4b9ff9e938794ba5d741a713fc4f24a4a603c6f6
Rob Buels <rmb32@cornell.edu>
2010-02-03 21:07:49 +0000

    fixed view_rflp.pl, converted it to mason
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/sgn/trunk@17009 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- lib/SGN/Controller/Marker.pm
+++ lib/SGN/Controller/Marker.pm
@@ -1,4 +1,6 @@
 package SGN::Controller::Marker;
+use namespace::autoclean;
+use File::Spec::Functions;
 
 =head2 rflp_image_link
 
@@ -12,19 +14,16 @@ an RFLP image, or undef if there is no image.
 =cut
 
 sub rflp_image_link {
-    my $class       = shift;
-    my $marker      = shift;
-    my $conf        = shift;
+    my ( $class, $c, $marker ) = @_;
     my $marker_name = uc $marker->name_that_marker();
     my ( $dir ) = $marker_name =~ /^(CD|CT|PC|PG|TG|PCD2)/
 	or return;
 
-    my $source = $conf->{'image_path'} . "/rflp/$dir/$marker_name.jpg";
+    my $source = catfile( $c->get_conf('image_path'), 'rflp', $dir, "$marker_name.jpg" );
 
-    return unless -f $source;
+    #return unless -f $source;
 
-    return $conf->{'static_datasets_url'}
-	. "/images/rflp/$dir/$marker_name.jpg";
+    return catfile( $c->get_conf('static_datasets_url'), 'images','rflp', $dir, "$marker_name.jpg" );
 }
 
 
