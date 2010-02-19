7abd00f443bffc9d6b67ffb5ef53bbb23ea39817
Isaak Yosief Tecle <iyt2@cornell.edu>
2010-02-19 10:38:35 +0000

    merged sgn/branches/tecle into trunk [17264:17265]
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/sgn/trunk@17268 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

---
+++ cgi-bin/phenome/organism_browser.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl -wT
+
+
+=head1 SYNOPSIS
+A simple script that retrieves common names and their ids, and 
+writes out id of the selected organism into a file.
+It is used to work with the Ajax form for the QTL webform.
+
+
+
+=head1 AUTHOR
+
+Isaak Y Tecle iyt2@cornell.edu 
+
+=cut
+
+
+
+use strict;
+use warnings;
+
+use CXGN::Scrap::AjaxPage;
+use CXGN::DB::Connection;
+use CXGN::Login;
+use CXGN::Phenome::Qtl;
+
+
+my $doc = CXGN::Scrap::AjaxPage->new();
+$doc->send_http_header();
+
+
+my ($common_name, $common_name_id, $type, $sp_person_id) = $doc->get_encoded_arguments("organism", "organism_id", "type", "sp_person_id");
+ 
+print STDERR "common_name : $common_name\n";
+print STDERR "common_name_id : $common_name_id\n";
+print STDERR "type: $type\n";
+
+#$organism_id =~/(\d+)/;
+#$organism_id =$1;
+#$organism_id =~ s/\s//;
+
+my $dbh = CXGN::DB::Connection->new();
+my ($login_person_id, $login_user_type)=CXGN::Login->new($dbh)->verify_session();
+
+
+if ($login_user_type eq 'curator' || $login_user_type eq 'submitter' || $login_user_type eq 'sequencer') {
+
+    if ($type eq 'browse') {
+	&browse_organisms($dbh, $type, $common_name);
+    }
+    elsif ($type eq 'associate') {
+	&associate_organism($dbh, $type, $common_name_id, $login_person_id);
+    }
+
+}         
+ 
+sub browse_organisms {
+    my $dbh = shift;
+    my $type = shift;
+    my $common_name = shift;
+    
+    print STDERR "common_name : $common_name";  
+    my $organisms;
+    
+    if ($type eq 'browse') {
+
+	if ($common_name) { 
+	    my $sth = $dbh->prepare ("SELECT common_name_id, common_name 
+                                              FROM sgn.common_name                                                                                             WHERE common_name ILIKE '$common_name%'"
+                                    );
+	    $sth->execute();
+
+	    while (my ($common_name_id, $common_name)	= $sth->fetchrow_array()) {		
+		$organisms .= "$common_name_id*$common_name_id -- $common_name |";
+		    
+	    }
+	
+	    print $organisms;
+	  }  
+	
+    }
+  
+}
+
+sub associate_organism {
+    my $dbh = shift;
+    my $type = shift;
+    my $common_name_id = shift;
+    my $sp_person_id = shift;
+    
+    my $qtl = CXGN::Phenome::Qtl->new($sp_person_id);
+    my ($temp_qtl_dir, $temp_user_dir) = $qtl->create_user_qtl_dir(); 
+    print STDERR "created user qtl dir...organism select..\n";
+
+    my $org_file = "$temp_user_dir/organism.txt";
+    open TXTFILE, ">$org_file" or die "Can't create file: $! \n";
+    print TXTFILE 'common_name_id' . "\t" . $common_name_id;
+    close TXTFILE;
+
+}
+
--- cgi-bin/phenome/pop_download.pl
+++ cgi-bin/phenome/pop_download.pl
@@ -1,3 +1,17 @@
+#!/usr/bin/perl -wT
+
+=head1 DESCRIPTION
+A small script for downloading population 
+phenotype raw data in tab delimited format.
+
+=head1 AUTHOR(S)
+
+Isaak Y Tecle (iyt2@cornell.edu)
+
+=cut
+
+
+
 use strict;
 
 use CXGN::DB::Connection;
@@ -16,7 +30,8 @@ my $name = $pop->get_name();
 
 
 
-print "Pragma: \"no-cache\"\nContent-Disposition: filename=population_data.txt \nContent-type: application/data\n\n";
+print "Pragma: \"no-cache\"\nContent-Disposition: filename=population_data.txt 
+               \nContent-type: application/data\n\n";
 
 #print "Content-Type: text/plain\n\n";
 
@@ -41,26 +56,26 @@ print "Lines \t";
 print join ("\t", @cvterms);
 
 
-	my ($pop_id, $pop_name, $ind_id, $ind_name, $obs_id, $cvterm, $definition, $value) = $pop->get_pop_raw_data($population_id);
+my ($pop_id, $pop_name, $ind_id, $ind_name, 
+$obs_id, $cvterm, $definition, $value) = $pop->get_pop_raw_data();
 
 
-	my $old_ind_id = "";
+my $old_ind_id = "";
 	
-	for (my $i=0; $i<@$pop_id; $i++) { 
-	    if ($old_ind_id ne $ind_id->[$i]) {
+for (my $i=0; $i<@$pop_id; $i++) { 
+    if ($old_ind_id ne $ind_id->[$i]) {
 
-		print "\n$ind_name->[$i]\t";
-	
-	    }
+	print "\n$ind_name->[$i]\t";	
+    }
 
-	    foreach my $t (@cvterms) { 
-		my $term = $cvterm->[$i];
-		if ($t =~ /^$term$/i) { 
-		    print "$value->[$i]\t";
-		}
-	    }
-	    $old_ind_id=$ind_id->[$i];
+    foreach my $t (@cvterms) { 
+	my $term = $cvterm->[$i];
+	if ($t =~ /^$term$/i) { 
+	    print "$value->[$i]\t";
 	}
+    }
+    $old_ind_id=$ind_id->[$i];
+}
 
 
 
