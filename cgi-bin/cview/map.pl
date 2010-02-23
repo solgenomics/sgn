410891adaf8ffae80fc1f11dab39682141b91517
Lukas Mueller <lam87@cornell.edu>
2010-02-23 23:58:27 +0000

    added a tempdir parameter to the script that is percolated through the mason components M    map.pl

--- cgi-bin/cview/map.pl
+++ cgi-bin/cview/map.pl
@@ -1,4 +1,7 @@
 
+use strict;
+use warnings;
+
 use CXGN::Page;
 use CXGN::DB::Connection;
 
@@ -7,9 +10,14 @@ my ($map_id, $map_version_id, $size, $hilite, $physical, $force) = $page->get_en
 
 my $dbh = CXGN::DB::Connection->new();
 
-my $referer = $page->get_request()->uri() ."?". $page->get_request->args();
 
-$c->forward_to_mason_view('/cview/map/index.mas', dbh=>$dbh, map_version_id=>$map_version_id, map_id=>$map_id, hilite=>$hilite, physical=>$physical, size=>$size, referer=>$referer, force=>$force);
+my $referer = ($page->get_request()->uri()) ."?". ($page->get_request->args());
+
+
+my $tempdir = $c->path_to($c->tempfiles_subdir('cview'));
+
+
+$c->forward_to_mason_view('/cview/map/index.mas', dbh=>$dbh, map_version_id=>$map_version_id, map_id=>$map_id, hilite=>$hilite, physical=>$physical, size=>$size, referer=>$referer, force=>$force, tempdir=>$tempdir);
 
 =head1 NAME
 
