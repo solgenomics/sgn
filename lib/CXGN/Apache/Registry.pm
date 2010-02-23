4f35f1e7d1f5b8ea9ae8306ebd655d8cd13eb373
Rob Buels <rmb32@cornell.edu>
2010-02-23 23:13:12 +0000

    simplified CXGN::Apache::Registry implementation, compile errors are going to the error log now, at least

--- lib/CXGN/Apache/Registry.pm
+++ lib/CXGN/Apache/Registry.pm
@@ -1,87 +1,17 @@
 package CXGN::Apache::Registry;
-
 use strict;
 use warnings FATAL => 'all';
 
 use base qw(ModPerl::RegistryPrefork);
-use CXGN::Apache::Error;
 use SGN::Context;
 
-# #########################################################################
-# # func: error_check
-# # dflt: error_check
-# # desc: checks $@ for errors and prints a pretty error page if present
-# # args: $self - registry blessed object
-# # rtrn: Apache2::Const::SERVER_ERROR if $@ is set, Apache2::Const::OK otherwise
-# #########################################################################
-
-# sub error_check {
-#     my $self = shift;
-
-#     require CXGN::Apache::Error;
-
-#     # ModPerl::Util::exit() throws an exception object whose rc is
-#     # ModPerl::EXIT
-#     # (see modperl_perl_exit() and modperl_errsv() C functions)
-#     if ($@ && !(ref $@ eq 'APR::Error' && $@ == ModPerl::EXIT)) {
-#         $self->log_error($@);
-# 	CXGN::Apache::Error::Purgatory::cxgn_die_handler('general error', $@ );
-#         return Apache2::Const::SERVER_ERROR;
-#     }
-#     return Apache2::Const::OK;
-# }
-
-
-# #########################################################################
-# # func: read_script
-# # dflt: read_script
-# # desc: reads the script in
-# # args: $self - registry blessed object
-# # rtrn: Apache2::Const::OK on success, some other code on failure
-# # efct: initializes the CODE field with the source script
-# #########################################################################
-
-    my $cxgn_script_header = <<'EOC';
+my $cxgn_script_header = <<'EOC';
 our $c = SGN::Context->instance;
 EOC
-sub read_script {
+sub get_mark_line {
     my $self = shift;
-
-    $self->{CODE} = eval { my $c = $cxgn_script_header.${$self->{REQ}->slurp_filename(0)}; \$c };
-    if ($@) {
-        $self->log_error("$@");
-
-        if (ref $@ eq 'APR::Error') {
-            return Apache2::Const::FORBIDDEN if APR::Status::is_EACCES($@);
-            return Apache2::Const::NOT_FOUND if APR::Status::is_ENOENT($@);
-        }
-
-        return Apache2::Const::SERVER_ERROR;
-    }
-
-    return Apache2::Const::OK;
+    return $cxgn_script_header.$self->SUPER::get_mark_line(@_);
 }
-
-
-sub compile {
-    my ($self, $eval) = @_;
-
-    ModPerl::Global::special_list_register(END => $self->{PACKAGE});
-    ModPerl::Global::special_list_clear(   END => $self->{PACKAGE});
-
-    {
-        # let the code define its own warn and strict level
-        no strict;
-        no warnings FATAL => 'all'; # because we use FATAL
-        eval $$eval;
-    }
-    if($@) {
-	CXGN::Apache::Error::compile_error_notify($@);
-    }
-
-    return $self->error_check;
-}
-
 
 1;
 __END__
