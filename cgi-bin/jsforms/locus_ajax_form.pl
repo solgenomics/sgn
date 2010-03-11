7c061f47ae7369fd90b67cc92cb77610e886dc83
Naama Menda <nm249@cornell.edu>
2010-03-11 06:06:32 +0000

    locus form server side script is a AjaxFormPage subclass

--- cgi-bin/jsforms/locus_ajax_form.pl
+++ cgi-bin/jsforms/locus_ajax_form.pl
@@ -1,12 +1,12 @@
 
+use strict;
+use warnings;
+
 my $locus_form = CXGN::Phenome::LocusForm->new();
 
 package CXGN::Phenome::LocusForm;
 
-use base qw/CXGN::Page::Form::AjaxFormPage  /; ##move all relevant functions to this parent class
-
-use strict;
-use warnings;
+use base qw/CXGN::Page::Form::AjaxFormPage  /; 
 
 use CXGN::Phenome::Locus;
 use CXGN::Phenome::Locus::LinkageGroup;
@@ -15,7 +15,6 @@ use CXGN::Tools::Organism;
 use CXGN::People::Person;
 use CXGN::Contact; 
 use CXGN::Feed;
-use CXGN::Debug;
 
 use Try::Tiny;
 
@@ -25,7 +24,7 @@ use JSON;
 sub new {
     my $class  = shift;
     my $self   = $class->SUPER::new(@_);
-    $self->get_ajax_page()->send_http_header();
+###    $self->get_ajax_page()->send_http_header();
     
     return $self;
 }
@@ -36,35 +35,38 @@ sub define_object {
     my %args      = $self->get_args();
     my $locus_id  = $args{locus_id} || $args{object_id};
     my $user_type = $self->get_user()->get_user_type();
+    my %json_hash= $self->get_json_hash();
+    
     $self->set_object_id($locus_id);
+    $self->set_object_name('Locus'); #this is useful for email messages
     $self->set_object(
         CXGN::Phenome::Locus->new( $self->get_dbh(), $self->get_object_id() ) );
     if ( $self->get_object()->get_obsolete() eq 't' && $user_type ne 'curator' )
     {
-	$self->set_json_hash({error=>'Locus $locus_id is obsolete!'});
-	$self->return_json();
+	##print STDERR "ERROR:: Locus $locus_id is obsolete!";
+	$json_hash{error}="Locus $locus_id is obsolete!";
     }
-    unless ( ( $locus_id =~ m /^\d+$/ ) || $args{action} eq 'new' ) {
-        $self->set_json_hash({error=>'No locus exists for identifier $locus_id'});
-	$self->return_json();
+    unless ( ( $locus_id =~ m /^\d+$/ || !$locus_id  )  ) {
+        print STDERR "ERROR:: 'No locus exists for identifier $locus_id'\n\n";
+	$json_hash{error}="No locus exists for identifier $locus_id";
     }
+    $self->set_json_hash(%json_hash);
     $self->set_primary_key("locus_id");
     $self->set_owners( $self->get_object()->get_owners() );
+    $self->return_json() if $json_hash{error};
 }
 
 sub display_form {
     my $self = shift;
     my %json_hash = $self->get_json_hash();
-   
     
-    if (!($json_hash{html}) ) { $json_hash{html} = $self->get_form()->as_table_string() ;
-    }		
+    if (!($json_hash{html}) ) { $json_hash{html} = $self->get_form()->as_table_string() ; }		
     
     $json_hash{"user_type"} = $self->get_user()->get_user_type();
     $json_hash{"is_owner"} = $self->get_is_owner();
     $json_hash{"editable_form_id"} = $self->get_form()->get_form_id();
-
-
+   
+    
     $self->set_json_hash(%json_hash);
     $self->return_json();
 }
@@ -76,28 +78,38 @@ sub store {
     my $locus_id = $self->get_object_id();
     my %args     = $self->get_args();
     my %json_hash = $self->get_json_hash();
-    
+    my $initial_locus_id = $locus_id;
+   
+    my $error;
+    $locus->set_common_name_id($args{common_name_id});
     
     my ($message) =
 	$locus->exists_in_database( $args{locus_name}, $args{locus_symbol} );
-    
+    my $validate;
     if ($message) {
-	$json_hash{error} = " Locus $args{locus_name} (symbol=  $args{locus_symbol} ) already exists in the database ";
-	
+	$error = " Locus $args{locus_name} (symbol=  $args{locus_symbol} ) already exists in the database ";
     }else {
 	try{
-	    $self->SUPER::store();
-	    %json_hash=$self->get_json_hash();
-	    
+	    $self->SUPER::store(); #this sets $json_hash{validate} if the form validation failed.
+	    $locus_id = $locus->get_locus_id() ;
 	} catch { 
-	    $json_hash{error} = " An error occurred. Cannot store to the database\n An  email message has been sent to the SGN development team";
-	    CXGN::Contact::send_email('locus_ajax_form.pl died',$json_hash{"error"} . "\n" . $_ , 'sgn-bugs@sgn.cornell.edu');
+	    $error = " An error occurred. Cannot store to the database\n An  email message has been sent to the SGN development team";
+	    CXGN::Contact::send_email('locus_ajax_form.pl died', $error . "\n" . $_ , 'sgn-bugs@sgn.cornell.edu');
 	};
     }
-    $self->send_form_email({subject=>'[New locus details stored] locus $locus_id', mailing_list=>'sgn-db-curation@sgn.cornell.edu', referring_page=>'www.solgenomics.net/phenome/locus_display.pl?locus_id=$locus_id'});
-    $self->set_json_hash(%json_hash);
-    $self->return_json();
+    #the validate field is false is validation passed for all fields, true if did not pass and the form is re-printed
+    #$json_hash{validate}= $validate;
+    %json_hash= $self->get_json_hash();
+    $validate= $json_hash{validate};
+    $json_hash{error} = $error if $error;
     
+    my $referring_page="/phenome/locus_display.pl?locus_id=$locus_id";
+    $self->send_form_email({subject=>"[New locus details stored] locus $locus_id", mailing_list=>'sgn-db-curation@sgn.cornell.edu', referring_page=>"www.solgenomics.net".$referring_page}) if (!$validate && !$json_hash{error});
+    $json_hash{referring_page}=$referring_page if !$initial_locus_id && !$validate && !$error;
+    
+    $self->set_json_hash(%json_hash);
+    
+    $self->return_json();
 }
 
 
@@ -105,10 +117,15 @@ sub store {
 sub delete {
     ##Delete the locus (actually set obsolete = 't')
     my $self = shift;
-    $self->check_modify_privileges();
+    my $check = $self->check_modify_privileges();
+    $self->return_json() if $check ; #error or no user privileges
+    
     my $locus      = $self->get_object();
     my $locus_name = $locus->get_locus_name();
+    my $locus_id = $locus->get_locus_id();
     my %json_hash= $self->get_json_hash();
+    my $referring_page="/phenome/locus_display.pl?locus_id=$locus_id";
+    
     if (!$json_hash{error} ) {
 	try {
 	    $locus->delete();
@@ -117,26 +134,10 @@ sub delete {
 	};
 	$json_hash{reload} = 1;
     }
-    $self->send_locus_email('delete') if (!$json_hash{error});
+    $self->send_form_email({subject=>"Locus obsoleted ($locus_name)", mailing_list=>'sgn-db-curation@sgn.cornell.edu', referring_page=>"www.solgenomics.net".$referring_page, action=>'delete'}) if (!$json_hash{error});
     $self->set_json_hash(%json_hash);
     $self->return_json();
 }
-
-
-
-sub return_json {
-    my $self=shift;
-    my %results= $self->get_json_hash();
-    
-    if ($results{die_error} ) { 
-	CXGN::Contact::send_email('locus_ajax_form.pl died',$results{die_error}, 'sgn-bugs@sgn.cornell.edu');
-    }
-    
-    my $json = JSON->new();
-    my $jobj = $json->encode(\%results);
-    print  $jobj;
-}
-
 
 
 
@@ -150,7 +151,7 @@ sub generate_form {
     my %args  = $self->get_args();
     my $form = $self->get_form();
     my $dbh = $self->get_dbh();
-
+    
     my ( $organism_names_ref, $organism_ids_ref ) =
 	CXGN::Tools::Organism::get_all_organisms( $self->get_dbh() );
     my ($lg_names_ref) =
@@ -158,8 +159,7 @@ sub generate_form {
     my ($lg_arms_ref) =
 	CXGN::Phenome::Locus::LinkageGroup::get_lg_arms( $self->get_dbh() );
     
- 
-    if ( $self->get_action =~ /new/ ) {
+    if ( $self->get_action =~ /new|store/ ) {
 	$self->get_form->add_select(
 	    display_name       => "Organism ",
 	    field_name         => "common_name_id",
@@ -275,49 +275,9 @@ sub generate_form {
 	
     }
     elsif ( $self->get_action() =~ /store/ ) {
-	#print STDERR "getting form from request ...\n\n\n\n";
 	$form->from_request( %args );
     }
 }
 
 
 
-sub send_locus_email {
-    my $self   = shift;
-    my $action = shift;
-    my $user=$self->get_user();
-    my $locus= $self->get_object();
-    
-    my $locus_id = $locus->get_locus_id();
-    my $name     = $locus->get_locus_name();
-    my $symbol   = $locus->get_locus_symbol();
-
-    my $subject = "[New locus details stored] locus $locus_id";
-    my $username =
-        $user->get_first_name() . " "
-	. $user->get_last_name();
-    my $sp_person_id = $user->get_sp_person_id();
-    
-    my $locus_link =
-	qq |http://www.sgn.cornell.edu/phenome/locus_display.pl?locus_id=$locus_id|;
-    my $user_link =
-	qq |http://www.sgn.cornell.edu/solpeople/personal-info.pl?sp_person_id=$sp_person_id|;
-    
-    my $usermail = $user->get_private_email();
-    my $fdbk_body;
-    if ( $action eq 'delete' ) {
-        $fdbk_body =
-	    "$username ($user_link) has obsoleted locus  $name ($locus_link) \n  $usermail";
-    }
-    elsif ( $locus_id == 0 ) {
-        $fdbk_body =
-	    "$username ($user_link) has submitted a new locus  \n$name ($locus_link)\nLocus symbol: $symbol\n $usermail ";
-    }
-    else {
-        $fdbk_body =
-	    "$username ($user_link) has submitted data for locus $name ($locus_link) \nLocus symbol: $symbol\n $usermail";
-    }
-    
-    CXGN::Contact::send_email( $subject, $fdbk_body,'sgn-db-curation@sgn.cornell.edu' );
-    CXGN::Feed::update_feed( $subject, $fdbk_body );
-}
