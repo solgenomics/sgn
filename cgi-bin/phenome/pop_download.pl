178fd79a15febd69c4ad3562200464162c1eacd4
Isaak Yosief Tecle <iyt2@cornell.edu>
2010-03-23 12:46:38 +0000

    perltidy on pop_download.pl
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/sgn/trunk@17730 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- cgi-bin/phenome/pop_download.pl
+++ cgi-bin/phenome/pop_download.pl
@@ -10,8 +10,6 @@ Isaak Y Tecle (iyt2@cornell.edu)
 
 =cut
 
-
-
 use strict;
 
 use CXGN::DB::Connection;
@@ -19,29 +17,25 @@ use CXGN::Phenome::Population;
 use CXGN::Phenome::Individual;
 use CXGN::Scrap;
 
-
 my $scrap = CXGN::Scrap->new();
-my $dbh = CXGN::DB::Connection->new();
+my $dbh   = CXGN::DB::Connection->new();
 
 my ($population_id) = $scrap->get_encoded_arguments("population_id");
 
-my $pop = CXGN::Phenome::Population->new($dbh, $population_id);
+my $pop = CXGN::Phenome::Population->new( $dbh, $population_id );
 my $name = $pop->get_name();
 
-
-
-print "Pragma: \"no-cache\"\nContent-Disposition:filename=population_data.txt\nContent-type:application/data\n\n";
+print
+"Pragma: \"no-cache\"\nContent-Disposition:filename=population_data.txt\nContent-type:application/data\n\n";
 
 #print "Content-Type: text/plain\n\n";
 
 print "Population: $name\t";
 print "\n";
 
-
 my @individuals = $pop->get_individuals();
 
-my (@pop_id, @name, @obs_id, @cvterm, @definition, @value);
-
+my ( @pop_id, @name, @obs_id, @cvterm, @definition, @value );
 
 my $individual_obj = $individuals[1];
 
@@ -49,42 +43,31 @@ my $indv_id = $individual_obj->get_individual_id();
 
 my @cvterms = $individual_obj->get_unique_cvterms($indv_id);
 
-
 print "Lines \t";
-print join ("\t", @cvterms);
+print join( "\t", @cvterms );
 
-
-my ($pop_id, $pop_name, $ind_id, $ind_name, 
-$obs_id, $cvterm, $definition, $value) = $pop->get_pop_raw_data();
-
+my ( $pop_id, $pop_name, $ind_id, $ind_name, $obs_id, $cvterm, $definition,
+     $value )
+  = $pop->get_pop_raw_data();
 
 my $old_ind_id = "";
-	
-for (my $i=0; $i<@$pop_id; $i++) { 
-    if ($old_ind_id ne $ind_id->[$i]) {
 
-	print "\n$ind_name->[$i]\t";	
+for ( my $i = 0 ; $i < @$pop_id ; $i++ )
+{
+    if ( $old_ind_id ne $ind_id->[$i] )
+    {
+
+        print "\n$ind_name->[$i]\t";
     }
 
-    foreach my $t (@cvterms) { 
-	my $term = $cvterm->[$i];
-	if ($t =~ /^$term$/i) { 
-	    print "$value->[$i]\t";
-	}
+    foreach my $t (@cvterms)
+    {
+        my $term = $cvterm->[$i];
+        if ( $t =~ /^$term$/i )
+        {
+            print "$value->[$i]\t";
+        }
     }
-    $old_ind_id=$ind_id->[$i];
+    $old_ind_id = $ind_id->[$i];
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
