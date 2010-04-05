43c790ac3186691281160a4d88a042672e6a8e30
Naama Menda <nm249@cornell.edu>
2010-04-05 17:50:12 +0000

    trying to print a phylo tree image

--- cgi-bin/sequencing/sol100.pl
+++ cgi-bin/sequencing/sol100.pl
@@ -12,6 +12,9 @@ use CXGN::People;
 use CXGN::Chado::Organism;
 use Bio::Chado::Schema;
 
+use CXGN::Phylo::Tree;
+use CXGN::Phylo::Node;
+
 my $page   =  CXGN::Page->new("SOL100 sequencing project","Naama");
 my $dbh = CXGN::DB::Connection->new();
 
@@ -22,18 +25,37 @@ my $schema= Bio::Chado::Schema->connect(  sub { $dbh->get_actual_dbh() },
 my @species= ('Solanum lycopersicum', 'Solanum pennellii', 'Solanum pimpinellifolium',  'Solanum galapagense');
 my $info;
 my @details;
+my $root= 'Solanaceae';
+my $root_o = CXGN::Chado::Organism->new_with_species($schema, $root);
+my $root_o_id = $root_o->get_organism_id();
+my $organism_link =   "/chado/organism.pl?organism_id="; 
+
+our %nodes={};
+my @unique_nodes=();
+
+my $tree =  CXGN::Phylo::Tree->new(); # $newick_string ) ;
+
+my $root_node = $tree->get_root();#CXGN::Phylo::Node->new();
+
+#$root_node->set_name('Solanaceae');
+#$root_node->set_link($organism_link . $root_o_id);
+
+
+
 foreach my $s (@species ) {
     
-    my $o =  CXGN::Chado::Organism::get_organism_by_species($s, $schema);
+    my $o =  CXGN::Chado::Organism->new_with_species($schema, $s);
     if ($o) {
-	my $organism_id = $o->organism_id();
-	my $organism_link .= qq| <a href="/chado/organism.pl?organism_id=$organism_id">$s</a> |; 
+	my $organism_id = $o->get_organism_id();
+	$nodes{$organism_id}=$o;
+
+	find_recursive_parent($o);
 	
 	
 	push @details,
 	[
 	 map { $_ } (
-	     $organism_link,  "PERSON/GROUP INFO",
+	     "<a href=\"$organism_link" . $organism_id . "\">$s</a> " ,  "PERSON/GROUP INFO",
 	     "PROJECT METADATA",
 	 )
 	];
@@ -43,6 +65,47 @@ foreach my $s (@species ) {
 	print STDERR "NO ORGANISM FOUND FOR SPECIES $s  !!!!!!!!!!!\n\n";
     }
 }
+
+@unique_nodes = values %nodes;
+foreach (keys %nodes) { 
+    if ($nodes{$_} ) {
+	print STDERR "Found organism " . $nodes{$_}->get_species() . " (id = $_ ) \n" ; 
+    }
+}
+
+
+recursive_children( $nodes{$root_o_id}, $root_node ) ;
+
+
+
+my $newick= $tree->generate_newick();
+#my $file =$c->get_conf("tempfiles_subdir") . "/tree_browser/tmpfile.png"; 
+
+my $file = $c->tempfile( TEMPLATE =>
+				 ['tree_browser',
+				  'tree-XXXXXX'
+				 ],
+				 SUFFIX   => '.png',
+    );
+
+my $uri = $c->uri_for_file($file); 
+
+my $filename = $file->filename();
+print STDERR "\n\nfilename = $filename, uri = $uri\n\n";
+
+$tree->render_png($uri);
+
+# foreach (@unique_nodes) {
+    
+#     my $node= CXGN::Phylo::Node->new();
+#     $node->set_name( $_->get_species() );
+#     $node->set_link($organism_link . $_->get_organism_id());
+    
+#     $node->set_tree($tree);
+    
+#     my $newick= $tree->generate_newick();
+#     print STDERR $newick . "\n" ; 
+# }
 
 
 $info = columnar_table_html(
@@ -61,9 +124,55 @@ print page_title_html("SOL100 sequencing project\n");
 
 print info_section_html(
     title       => 'Species',
-    contents     => $info,
+    contents     => $info ,
     collapsible => 1,
     );
 
+print info_section_html(
+    title       => 'Tree',
+    contents     =>  $newick . $uri . qq| <img src="$file" border="1" alt="tree_browser" />|,
+    collapsible => 1,
+    );
 
 $page->footer();
+
+
+sub recursive_children {
+    my $o= shift; #CXGN::Chado::Organism object
+    my $n = shift; # CXGN::Phylo::Node object
+    my @children = $o->get_direct_children;
+    foreach my $child (@children) {
+	
+	if ( exists( $nodes{$child->get_organism_id() } ) && defined( $nodes{$child->get_organism_id()} ) ) {
+	    my $new_node=$n->add_child();
+	   # $new_node->set_tree($n->get_tree());
+
+	    print STDERR "Found child id " . $child->get_organism_id() . " name = " . $child->get_species() . " for parent node " . $n->get_name() . "\n\n";
+	    $new_node->set_name($child->get_species());
+	    $new_node->set_link("/chado/organism.pl?organism_id=" . $child->get_organism_id());
+	    #$new_node->set_hide_label(0);
+	   
+	    recursive_children($child, $new_node);
+	}
+    }
+}
+
+
+sub find_recursive_parent {
+    my $organism=shift ; 
+    my $parent = $organism->get_parent;
+    if ($parent) {
+	my $id = $parent->get_organism_id();
+	
+	if (!$nodes{$id} ) {
+	    print STDERR "Found parent id $id for organism " . $parent->get_species() . "\n";
+	    $nodes{$id} = $parent ;
+	    find_recursive_parent($parent);
+	} 
+    }
+    else { 
+	print STDERR "NO PARENT FOR ORGANISM " . $organism->get_species() . " This must be the root of your tree :-) \n\n";
+	return;
+    }
+    
+}
