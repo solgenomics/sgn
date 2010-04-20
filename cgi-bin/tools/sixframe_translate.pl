3778e4ceca7a50ea885235643cc3576426b9373b
Rob Buels <rmb32@cornell.edu>
2010-04-20 21:33:50 +0000

    cleanup of sixframe_translate.pl, merged CXGN::Garbage::Sequence into here to remove dependency on it

--- cgi-bin/tools/sixframe_translate.pl
+++ cgi-bin/tools/sixframe_translate.pl
@@ -1,18 +1,17 @@
 #!/usr/bin/perl -w
 use strict;
 use CXGN::Page;
-use CXGN::Garbage::Sequence;
 use CXGN::Page::FormattingHelpers qw/page_title_html blue_section_html html_break_string/;
 use CXGN::DB::Connection;
 
 
-our $page = CXGN::Page->new( "Simple N->AA Translate", "Dan/Koni");
+my $page = CXGN::Page->new( "Simple N->AA Translate", "Dan/Koni");
 my $dbh = CXGN::DB::Connection->new();
 
-my $memberq = $dbh->prepare_cached("SELECT nr_members FROM unigene WHERE unigene_id=?");
-my $consensusq = $dbh->prepare_cached("SELECT seq FROM unigene INNER JOIN unigene_consensi USING (consensi_id) WHERE unigene_id=?");
-my $unigene_estq = $dbh->prepare_cached("SELECT COALESCE(SUBSTRING(seq, hqi_start::integer+1, hqi_length::integer), seq) FROM unigene LEFT JOIN unigene_member USING (unigene_id) LEFT JOIN est USING (est_id) LEFT JOIN qc_report USING (est_id) WHERE unigene.unigene_id=?");
-my $estq  = $dbh->prepare_cached("SELECT COALESCE(SUBSTRING(seq, hqi_start::integer+1, hqi_length::integer), seq) FROM est LEFT JOIN qc_report USING (est_id) where est.est_id=?");
+my $memberq = $dbh->prepare("SELECT nr_members FROM unigene WHERE unigene_id=?");
+my $consensusq = $dbh->prepare("SELECT seq FROM unigene INNER JOIN unigene_consensi USING (consensi_id) WHERE unigene_id=?");
+my $unigene_estq = $dbh->prepare("SELECT COALESCE(SUBSTRING(seq, hqi_start::integer+1, hqi_length::integer), seq) FROM unigene LEFT JOIN unigene_member USING (unigene_id) LEFT JOIN est USING (est_id) LEFT JOIN qc_report USING (est_id) WHERE unigene.unigene_id=?");
+my $estq  = $dbh->prepare("SELECT COALESCE(SUBSTRING(seq, hqi_start::integer+1, hqi_length::integer), seq) FROM est LEFT JOIN qc_report USING (est_id) where est.est_id=?");
 
 
 my ($seq, $est_id, $unigene_id) = 
@@ -72,7 +71,7 @@ for ($j=0;$j<3;$j++){
     substr($frames[$j],0,$j,'');
     my $i=0;
     for (;$i<(length $frames[$j]);$i+=3){
-	$frames_translation[$j].=CXGN::Garbage::Sequence::translate_3n_1letter(substr($frames[$j],$i,3));
+	$frames_translation[$j].= translate_3n_1letter(substr($frames[$j],$i,3));
     }
 }
 
@@ -89,7 +88,7 @@ for ($j=3;$j<6;$j++){
     $frames[$j]=$complement;
     substr($frames[$j],0,($j-3),'');
     for ($i=0;$i<(length $frames[$j]);$i+=3){
-	$frames_translation[$j].=CXGN::Garbage::Sequence::translate_3n_1letter(substr($frames[$j],$i,3));
+	$frames_translation[$j].= translate_3n_1letter(substr($frames[$j],$i,3));
     }
 }
 
@@ -196,3 +195,96 @@ EOF
     exit(0);
 }
 
+
+no strict;
+
+#ugly-looking code to create the translation hash. 
+#auto-generated from a translation table
+
+our %trans_hash;
+
+$trans_hash{AAA}=['K','Lys'];
+$trans_hash{AAC}=['N','Asn'];
+$trans_hash{AAG}=['K','Lys'];
+$trans_hash{ATA}=['I','Ile'];
+$trans_hash{ATC}=['I','Ile'];
+$trans_hash{CCA}=['P','Pro'];
+$trans_hash{ATG}=['M','Met'];
+$trans_hash{CCC}=['P','Pro'];
+$trans_hash{CCG}=['P','Pro'];
+$trans_hash{CGA}=['R','Arg'];
+$trans_hash{AAT}=['N','Asn'];
+$trans_hash{CGC}=['R','Arg'];
+$trans_hash{GCA}=['A','Ala'];
+$trans_hash{GCC}=['A','Ala'];
+$trans_hash{CGG}=['R','Arg'];
+$trans_hash{ATT}=['I','Ile'];
+$trans_hash{GCG}=['A','Ala'];
+$trans_hash{TCA}=['S','Ser'];
+$trans_hash{CCT}=['P','Pro'];
+$trans_hash{GGA}=['G','Gly'];
+$trans_hash{TCC}=['S','Ser'];
+$trans_hash{GGC}=['G','Gly'];
+$trans_hash{TCG}=['S','Ser'];
+$trans_hash{GGG}=['G','Gly'];
+$trans_hash{TGA}=['*','Ter'];
+$trans_hash{CGT}=['R','Arg'];
+$trans_hash{TGC}=['C','Cys'];
+$trans_hash{GCT}=['A','Ala'];
+$trans_hash{TGG}=['W','Trp'];
+$trans_hash{TCT}=['S','Ser'];
+$trans_hash{GGT}=['G','Gly'];
+$trans_hash{TGT}=['C','Cys'];
+$trans_hash{ACA}=['T','Thr'];
+$trans_hash{CAA}=['Q','Gln'];
+$trans_hash{ACC}=['T','Thr'];
+$trans_hash{CAC}=['H','His'];
+$trans_hash{ACG}=['T','Thr'];
+$trans_hash{CTA}=['L','Leu'];
+$trans_hash{CAG}=['Q','Gln'];
+$trans_hash{AGA}=['R','Arg'];
+$trans_hash{CTC}=['L','Leu'];
+$trans_hash{AGC}=['S','Ser'];
+$trans_hash{CTG}=['L','Leu'];
+$trans_hash{AGG}=['R','Arg'];
+$trans_hash{GAA}=['E','Glu'];
+$trans_hash{GAC}=['D','Asp'];
+$trans_hash{ACT}=['T','Thr'];
+$trans_hash{GTA}=['V','Val'];
+$trans_hash{GAG}=['E','Glu'];
+$trans_hash{CAT}=['H','His'];
+$trans_hash{TAA}=['*','Ter'];
+$trans_hash{GTC}=['V','Val'];
+$trans_hash{TAC}=['Y','Tyr'];
+$trans_hash{TAG}=['*','Ter'];
+$trans_hash{AGT}=['S','Ser'];
+$trans_hash{GTG}=['V','Val'];
+$trans_hash{CTT}=['L','Leu'];
+$trans_hash{TTA}=['L','Leu'];
+$trans_hash{TTC}=['F','Phe'];
+$trans_hash{GAT}=['D','Asp'];
+$trans_hash{TTG}=['L','Leu'];
+$trans_hash{GTT}=['V','Val'];
+$trans_hash{TAT}=['Y','Tyr'];
+$trans_hash{TTT}=['F','Phe'];
+#sequence translator
+#########################
+sub translate_3n_1letter{
+    my ($triplet)=@_;
+    my $toreturn=$trans_hash{$triplet}[0];
+    unless($toreturn){
+	(length $triplet)==3
+	    and $toreturn='X';
+    }
+    return $toreturn;
+}
+
+sub translate_3n_3letter{
+    my ($triplet)=@_;
+    return $trans_hash{$triplet}[0];
+}
+
+sub translate_3n_both{
+    my ($triplet)=@_;
+    return @{$trans_hash{$triplet}};
+}
