abe06dc413453a6a95d383339fb78f65712faa82
Lukas Mueller <lam87@cornell.edu>
2010-05-03 18:50:08 +0000

    added a missing database handle argument in the controller script. AM + markers/markerinfo-old.pl M    markers/markerinfo.pl
    git-svn-id: svn+ssh://svn.sgn.cornell.edu/cxgn/sgn/trunk@18130 b4ddb4e3-33f1-0310-aa89-e703427ae9b8

--- cgi-bin/search/markers/markerinfo.pl
+++ cgi-bin/search/markers/markerinfo.pl
@@ -2,9 +2,11 @@
 use strict;
 
 use CXGN::Page;
+use CXGN::DB::Connection;
 
 my $page = CXGN::Page->new();
+my $dbh = CXGN::DB::Connection->new();
 
 my $marker_id = $page->get_arguments("marker_id");
 
-$c->forward_to_mason_view('/markers/index.mas', marker_id=>"marker_id");
+$c->forward_to_mason_view('/markers/index.mas', marker_id=>"marker_id", dbh=>$dbh);
