04a8ba7e73684a2c306b0ddd5c9f42e5bfaba71c
Duke Leto <jonathan@leto.net>
2010-06-11 11:54:26 -0700

    Convert 2-arg open's in CXGN::Genefamily to slurp

--- lib/CXGN/Genefamily.pm
+++ lib/CXGN/Genefamily.pm
@@ -3,6 +3,7 @@
 package CXGN::Genefamily;
 
 use Moose;
+use File::Slurp qw/slurp/;
 
 has 'name' => (is=>'rw', required=>1);
 
@@ -10,30 +11,16 @@ has 'members' => (is=>'rw', isa=>'ArrayRef', default=>sub { [] });
 
 has 'files_dir' => (is=>'rw');
 
-sub get_alignment { 
+sub get_alignment {
     my $self = shift;
-
-    my $a = "";
-    
     my $file = $self->files_dir()."/alignments/family_".$self->name().".fa.align";
-    open (my $F, "<$file") || die "Can't open ".$file;
-    while (<$F>) { 
-	$a .= $_;
-    }
-    close($F);
-    return $a;
+    return slurp($file);
 }
 
-sub get_fasta { 
+sub get_fasta {
     my $self = shift;
-    
     my $file = $self->files_dir()."/fasta/".$self->name().".fa";
-    open (my $F, "<$file") || die "Can't open ".$file;
-    while (<$F>) { 
-	$a .= $_;
-    }
-    close($F);
-    return $a;
+    return slurp($file);
 }
 
 sub get_member_ids { 
