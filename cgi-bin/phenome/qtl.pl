59b466bff296c5e37ed105cde5658bb93a2c65db
Isaak Y Tecle <iyt2@cornell.edu>
2010-06-14 17:07:38 +0200

    added cM positions to QTL markers' detail section...

--- cgi-bin/phenome/qtl.pl
+++ cgi-bin/phenome/qtl.pl
@@ -30,6 +30,7 @@ use CXGN::Map;
 use CXGN::DB::Connection;
 use CXGN::Chado::Cvterm;
 use List::MoreUtils qw /uniq/;
+use Math::Round::Var;
 
 my $page = CXGN::Page->new( "qtl", "isaak" );
 my ( $pop_id, $trait_id, $lg, $l_m, $p_m, $r_m, $lod, $qtl_image ) =
@@ -46,7 +47,7 @@ my $trait_name   = &trait_name( $pop, $trait_id );
 my $genetic_link = &genetic_map($pop);
 my $cmv_link     = &marker_positions( $pop, $lg, $l_m, $p_m, $r_m );
 my $gbrowse_link = &genome_positions( $l_m, $p_m, $r_m );
-my $marker_link  = &marker_detail( $l_m, $p_m, $r_m );
+my $marker_link  = &marker_detail($pop, $l_m, $p_m, $r_m);
 my $legend       = &legend();
 my $comment      = &comment();
 
@@ -98,21 +99,35 @@ sub genetic_map
 
 sub marker_detail
 {
-    my @markers = @_;
-    my ( $m_link, $desc );
+    my ($pop, $l_m, $p_m, $r_m) = @_;
+    my @markers = uniq ($l_m, $p_m, $r_m);
+    my $mapv_id = $pop->mapversion_id();
+
+    my @marker_html;
+    my $rnd = Math::Round::Var->new(0.01);
     for ( my $i = 0 ; $i < @markers ; $i++ )
     {
         my $marker = CXGN::Marker->new_with_name( $dbh, $markers[$i] );
-        my $m_id = $marker->marker_id() unless !$marker;
-        if ( $i == 0 ) { $desc = "Left flanking marker:"; }
-        if ( $i == 1 ) { $desc = "Peak (<i>or the closest</i>) marker:"; }
-        if ( $i == 2 ) { $desc = "Right flanking marker:"; }
-        $m_link .=
-qq |<br/>$desc <a href="/search/markers/markerinfo.pl?marker_id=$m_id">$markers[$i]</a>|
-          unless !$marker;
-    }
+	my ($m_id, $m_pos);
+	
+	unless (!$marker) 
+	{
+	    $m_id = $marker->marker_id();
+	    my $m_pos = $pop->get_marker_position( $mapv_id, $markers[$i] );
+	    
+	    push @marker_html, 
+	    [ 
+	      map {$_} 
+	      ( 
+		qq | <a href="/search/markers/markerinfo.pl?marker_id=$m_id">$markers[$i]</a>|, $rnd->round($m_pos)
+	      ) 
+	    ];       
+	} 
 
-    return $m_link;
+    }  
+
+    return \@marker_html;
+
 }
 
 sub trait_name
