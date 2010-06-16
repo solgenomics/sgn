ec7ff1545890e641fcf0b5a48da6a9065a19d29b
Robert Buels <rmb32@cornell.edu>
2010-06-16 12:19:13 -0700

    hoverscript.js, removed windows line endings, tweaked a little code formatting

--- js/hoverScript.js
+++ js/hoverScript.js
@@ -2,10 +2,10 @@ sfHover = function() {
 	var sfEls = document.getElementById("nav").getElementsByTagName("LI");
 	for (var i=0; i<sfEls.length; i++) {
 		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
+			this.className += " sfhover";
 		}
 		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+			this.className = this.className.replace(new RegExp(" sfhover\\b"), "");
 		}
 	}
 }
