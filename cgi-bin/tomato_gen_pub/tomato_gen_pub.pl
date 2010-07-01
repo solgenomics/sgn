1b7fd517f9aeec44deeea4a67a1630fd84227c88
Jean Hooi Lee <jhl233@cornell.edu>
2010-07-01 13:52:16 -0400

    Added my $schema = $c->dbic_schema to tomato_gen_pub.pl (removed from tomato_gen_pub.mas)

--- cgi-bin/tomato_gen_pub/tomato_gen_pub.pl
+++ cgi-bin/tomato_gen_pub/tomato_gen_pub.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
-$c->forward_to_mason_view('/tomato_gen_pub/tomato_gen_pub.mas');
+my $schema = $c->dbic_schema('Bio::Chado::Schema');
+$c->forward_to_mason_view('/tomato_gen_pub/tomato_gen_pub.mas', schema => $schema);
 
 1;
