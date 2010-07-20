28b717d9e33b44570266fa556b7e7c7122abc5c1
Robert Buels <rmb32@cornell.edu>
2010-07-20 15:33:40 -0700

    don't need a shebang in our cgi scripts

--- cgi-bin/genomes/Solanum_lycopersicum/publications.pl
+++ cgi-bin/genomes/Solanum_lycopersicum/publications.pl
@@ -1,9 +1,5 @@
-#!/usr/bin/perl
-
 use strict;
 use warnings;
 
 my $schema = $c->dbic_schema('Bio::Chado::Schema');
 $c->forward_to_mason_view('/genomes/Solanum_lycopersicum/publications.mas', schema => $schema);
-
-1;
