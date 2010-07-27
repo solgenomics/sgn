9ee0787b0e75870f8c5eedb58f434762fb757c38
Robert Buels <rmb32@cornell.edu>
2010-07-26 18:29:05 -0700

    added caching of species hash, fixed bugs with directory paths provided to webimagecache

--- cgi-bin/content/sgn_data.pl
+++ cgi-bin/content/sgn_data.pl
@@ -6,78 +6,91 @@ use Cache::File;
 use CXGN::Chado::Organism;
 
 
-
-
 my $schema   = $c->dbic_schema( 'Bio::Chado::Schema', 'sgn_chado' );
 my @families_to_display = qw( Solanaceae  Rubiaceae  Plantaginaceae );
 
 my $cache = Cache::File->new(
-    cache_root      => $c->path_to( $c->tempfiles_subdir('sgn_data') ),
-    default_expires => '16000 sec',
+
+    cache_root      => $c->path_to( $c->tempfiles_subdir('sgn_data_pl') ),
+    default_expires => '1 day',
+
     load_callback   => sub {
         my $cache_entry = shift;
-        my $org    = CXGN::Chado::Organism->new( $schema, $cache_entry->key );
-        $cache_entry->set( join '', map "?$_", (
-            "Name: ".$org->get_species(),
-            "Common Name: ".$org->get_group_common_name(),
-            "Loci Num: ".$org->get_loci_count(),
-            "Phenotype Count: ".$org->get_phenotype_count(),
-            "Maps Available: ".$org->has_avail_map(),
-            "Genome Information: ".$org->has_avail_genome(),
-            "Library Num: ".scalar( $org->get_library_list ),
-           )
-        );
+
+        # we will store the species hash in the cache also, under a special key
+        if( $cache_entry->key eq 'species_data' ) {
+            $cache_entry->freeze( _make_species_hashref( $schema, @families_to_display ) );
+        } else {
+            my $org = CXGN::Chado::Organism->new( $schema, $cache_entry->key );
+            $cache_entry->set( join '', map "?$_", (
+                "Name: ".$org->get_species(),
+                "Common Name: ".$org->get_group_common_name(),
+                "Loci Num: ".$org->get_loci_count(),
+                "Phenotype Count: ".$org->get_phenotype_count(),
+                "Maps Available: ".$org->has_avail_map(),
+                "Genome Information: ".$org->has_avail_genome(),
+                "Library Num: ".scalar( $org->get_library_list ),
+               )
+            );
+        }
     },
+
    );
 
-# hash like:
+
+$c->forward_to_mason_view( "/content/sgn_data.mas",
+    schema  => $schema,
+    cache   => $cache,
+    species => $cache->thaw('species_data'),
+);
+
+####### helper subs ##########
+
+# returns hashref like:
 #   'Solanaceae' => { arguments for organism_tree.mas },
 #   'Rubiaceae' => (same),
 #   .....
+sub _make_species_hashref {
+    my ( $schema, @families ) = @_;
 
-my %species;
-for my $family ( @families_to_display ) {
+    my %species;
 
-    # find the phylonode(s) for this family
-    my $family_phylonodes =
-        $schema->resultset('Organism::Organism')
-               ->search({ 'me.species' => $family })
-               ->search_related('phylonode_organisms')
-               ->search_related('phylonode',
-                    { 'cv.name' => 'taxonomy' },
-                    { join => { type => 'cv' } },
-                 );
+    for my $family ( @families ) {
 
-    # find the child organisms of this family that are also web-visible
-    my $organisms =
-        _child_phylonodes( $family_phylonodes )
-            ->search_related('phylonode_organism')
-            ->search_related('organism',
-                             { 'cv.name' => 'local',
-                               'type.name' => 'web visible',
-                             },
-                             { join => { organismprops => { type => 'cv' }}},
-                            );
+        # find the phylonode(s) for this family
+        my $family_phylonodes =
+            $schema->resultset('Organism::Organism')
+                   ->search({ 'me.species' => $family })
+                   ->search_related('phylonode_organisms')
+                   ->search_related('phylonode',
+                        { 'cv.name' => 'taxonomy' },
+                        { join => { type => 'cv' } },
+                     );
 
-    $species{$family} = {
-        schema => $schema,
-        root   => $family,
-        speciesinfo_cache => $cache,
-        uri_dir => $c->tempfiles_subdir('sgn_data'),
-        tmp_dir => $c->path_to( $c->tempfiles_subdir('sgn_data') ),
-    };
+        # find the child organisms of this family that are also web-visible
+        my $organisms =
+            _child_phylonodes( $family_phylonodes )
+                ->search_related('phylonode_organism')
+                ->search_related('organism',
+                                 { 'cv.name' => 'local',
+                                   'type.name' => 'web visible',
+                                 },
+                                 { join => { organismprops => { type => 'cv' }}},
+                                );
 
-    while( my $organism = $organisms->next ) {
-        $species{$family}{species_hashref}{ $organism->species } = $organism->organism_id;
+        $species{$family} = {
+            root   => $family,
+            uri_dir => $c->tempfiles_subdir('sgn_data_pl'),
+            tmp_dir => $c->path_to(),
+        };
+
+        while( my $organism = $organisms->next ) {
+            $species{$family}{species_hashref}{ $organism->species } = $organism->organism_id;
+        }
     }
+
+    return \%species;
 }
-
-#use Data::Dumper;
-#print "Content-type: text/html\n\n".'<pre>'.Dumper(\%species).'</pre>';
-
-$c->forward_to_mason_view( "/content/sgn_data.mas", species => \%species );
-
-####### helper subs ##########
 
 # take a resultset of phylonodes, construct a resultset of the child
 # phylonodes.  this method should be integrated into
