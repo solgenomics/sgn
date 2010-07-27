5b77fe7540689614536480ee4345e0329849bb60
dmb387 <dmb387@cornell.edu>
2010-07-27 17:07:51 -0400

    sol100 page

--- cgi-bin/sequencing/sol100.pl
+++ cgi-bin/sequencing/sol100.pl
@@ -1,29 +1,76 @@
 use strict;
+use warnings;
 
 use CXGN::Page;
 
-use CXGN::Chado::Organism;
-use CXGN::DB::DBICFactory;
 
-my $page   =  CXGN::Page->new("SOL100 sequencing project","Naama");
+my $page = CXGN::Page->new("SOL100 sequencing project","Naama");
 
-my $schema =  
-    CXGN::DB::DBICFactory
-    ->open_schema( 'Bio::Chado::Schema' );
+my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
 
-my @species= ('Solanum lycopersicum', 'Solanum pennellii', 'Solanum pimpinellifolium',  'Solanum galapagense');
+#my @species= ('Solanum lycopersicum', 'Solanum pennellii', 'Solanum pimpinellifolium', 'Solanum galapagense');
+
+my $cvterm = $schema->resultset("Cv::Cvterm")->search( { name => 'sol100' } );
+
+my $o_props;
+my $organism_res;
+
+$o_props= $cvterm->search_related('organismprops') if $cvterm;
 
 my $sol=();
 
-foreach my $s (@species) {
-    my $organism= CXGN::Chado::Organism->new_with_species($schema, $s);
-    my $organism_id = $organism->get_organism_id();
-    $sol->{$s}= $organism_id ;
+while(my $o_prop=$o_props->next){
+    print STDERR "$o_prop\n\n\n";
+    my ($organism)=$o_prop->search_related('organism');
+    
+    my $organism_id = $organism->organism_id;
+    print STDERR "$organism_id\n\n\n";
+    $sol->{$organism->species}= $organism_id;
 }
 
-##########
+
+# if ($organism_res) {
+#     my @organisms = $organism_res->all();
+#     foreach my $o (@organisms){
+# 	if (defined $o){
+# 	    my $organism_id = $o->organism_id();
+# 	    $sol->{$species}= $organism_id ;
+# 	}
+#     }
 
 $c->forward_to_mason_view("/sequencing/sol100.mas" , schema=>$schema, sol=>$sol );
 
+
+
+##########
+
+
+
+
+
+
+
+
+
+# use strict;
+# use CXGN::Page;
+# use CXGN::Chado::Organism;
+# use CXGN::DB::DBICFactory;
+
+# my $page   =  CXGN::Page->new("SOL100 sequencing project","Dean");
+
+# my $schema =  CXGN::DB::DBICFactory->open_schema( 'Bio::Chado::Schema' );
+
+# my @species= ('Solanum lycopersicum', 'Solanum pennellii', 'Solanum pimpinellifolium',); #'Solanum galapagense');
+
+# my $sol=();
+
+# foreach my $s (@species) {
+#     my $organism= CXGN::Chado::Organism->new_with_species($schema, $s);
+#     my $organism_id = $organism->get_organism_id();
+#     $sol->{$s}= $organism_id ;
+# }
+
+# $c->forward_to_mason_view("/sequencing/sol100.mas" , schema=>$schema, sol=>$sol );
 
 
