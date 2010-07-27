2aa36e7f22ecc64a5df2f7ac9a58450fd9bedb97
dmb387 <dmb387@cornell.edu>
2010-07-27 17:09:36 -0400

    Add organism to organismprop table

--- cgi-bin/chado/add_organismprop.pl
+++ cgi-bin/chado/add_organismprop.pl
@@ -17,15 +17,13 @@ my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
 my %status;
 
 try{
-
-print STDERR "In the beginning of the catch block\n\n"; 
+ 
     my $org = $schema->resultset('Organism::Organism')->find({species=>$species});
     $org->create_organismprops({'sol100'=>'1'},{autocreate=>1});
     $status{"pass"} = "Success, the object was added to the table.";
 
 }catch{
     $status{"fail"} = "Error: The organism does not exist. Please select an organism from the suggestion list.";
-
 };
 
 my $jobj = $json->encode(\%status);
