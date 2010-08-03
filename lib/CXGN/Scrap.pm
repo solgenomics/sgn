600b759bd3ff5623c5dcf6f68378e9b995424989
Robert Buels <rmb32@cornell.edu>
2010-08-03 16:31:10 -0700

    deprecated CXGN::Scrap, reimplemented a lot of methods to use the catalyst context object instead of CGI

--- lib/CXGN/Scrap.pm
+++ lib/CXGN/Scrap.pm
@@ -4,14 +4,21 @@ package CXGN::Scrap;
 
  CXGN::Scrap
 
-=head1 DESCRIPTION
+=head1 DEPRECATED
 
- Simplified page object, now a superclass of CXGN::Page.  Also changed
- to be a singleton class.  Provides the bindings for argument
- retrieval, but doesn't do much else.  The motivation for this module
- was to create a subclass of a simple page object for AJAX requests.
+Deprecated.  Do not use in new code.
 
 =cut
+
+
+# =head1 DESCRIPTION
+
+#  Simplified page object, now a superclass of CXGN::Page.  Also changed
+#  to be a singleton class.  Provides the bindings for argument
+#  retrieval, but doesn't do much else.  The motivation for this module
+#  was to create a subclass of a simple page object for AJAX requests.
+
+# =cut
 
 
 use strict;
@@ -19,41 +26,40 @@ use warnings;
 use Clone;
 use HTML::Entities;
 use Carp;
-use CGI;
 
 use File::Path ();
 
 use JSAN::ServerSide;
 use SGN::Context;
 
-=head1 OBJECT METHODS
+use CatalystX::GlobalContext '$c'; our $c;
 
-=head2 new
+# =head1 OBJECT METHODS
 
-Creates returns the singleton page-scrap object.
+# =head2 new
 
-#Example
-my $scrap=CXGN::Scrap->new();
+# Creates returns the singleton page-scrap object.
 
-=cut
+# #Example
+# my $scrap=CXGN::Scrap->new();
+
+# =cut
 
 sub new {
   my $class=shift;
   my $self = bless {},$class;
   $self->{content_type} = 'text/html';
-  $self->{vhost} = SGN::Context->instance;
-  $self->{cgi} = CGI->new;
   return $self;
 }
 
-=head2 get_encoded_arguments
+# =head2 get_encoded_arguments
 
-Gets arguments which are being sent in via GET or POST (doesn't matter which). Encodes the HTML entities in those arguments to keep clients from being able to submit evil HTML, javascript, etc. to be printed on our pages.
+# Gets arguments which are being sent in via GET or POST (doesn't matter which). Encodes the HTML entities in those arguments to keep clients from being able to submit evil HTML, javascript, etc. to be printed on our pages.
 
-	#Example
-	my($id,$name)=$scrap->get_encoded_arguments("id","name");
+# 	#Example
+# 	my($id,$name)=$scrap->get_encoded_arguments("id","name");
 
-=cut
+# =cut
 
 # use this one for all alphanumeric arguments, unless for some reason you don't
 # want to filter out possibly evil characters (see below)
@@ -66,47 +72,45 @@ sub get_encoded_arguments {
 }
 
 
-=head2 get_all_encoded_arguments
+# =head2 get_all_encoded_arguments
 
-Args: none
-Ret : hash of ( argument name => HTML-encoded value of that argument )
+# Args: none
+# Ret : hash of ( argument name => HTML-encoded value of that argument )
 
-WARNING: this method does not work for POSTs of type multipart/form-data, particularly with file uploads.  This method only works with GET and other POST requests.
+# WARNING: this method does not work for POSTs of type multipart/form-data, particularly with file uploads.  This method only works with GET and other POST requests.
 
-=cut
+# =cut
 
 sub get_all_encoded_arguments {
   my ($self) = @_;
-  my %vars = %{$self->{cgi}->Vars};
+  my %vars = %{$c->req->params};
   $_ = HTML::Entities::encode_entities($_,"<>&'\";") for values %vars;
   return %vars;
 }
 
 
-=head2 get_upload
+# =head2 get_upload
 
-Get the L<Apache2::Upload> object for the currently uploaded file, if any.
+# Get the L<Catalyst::Request::Upload> object for the currently uploaded file, if any.
+# This is compatible with L<Apache2::Upload>.
 
-=cut
+# =cut
 
 sub get_upload {
-  my $self = shift;
-  my @upload_names = $self->{apache_request}->upload();
-  if( wantarray ) {
-      return map $self->{apache_request}->upload($_), @upload_names;
-  } else {
-      return $self->{apache_request}->upload( $upload_names[0] );
-  }
+  my ($field)  = $c->req->upload
+      or return;
+  my @uploads = $c->req->upload($field);
+  return wantarray ? @uploads : $uploads[0];
 }
 
-=head2 get_arguments
+# =head2 get_arguments
 
-Gets arguments which are being sent in via GET or POST (doesn't matter which). DOES NOT encode the HTML entities in those arguments, so be careful because it IS possible for clients to submit evil HTML, javascript, etc.
+# Gets arguments which are being sent in via GET or POST (doesn't matter which). DOES NOT encode the HTML entities in those arguments, so be careful because it IS possible for clients to submit evil HTML, javascript, etc.
 
-	#Example
-	my($fasta_file)=$scrap->get_arguments("fasta_file");
+# 	#Example
+# 	my($fasta_file)=$scrap->get_arguments("fasta_file");
 
-=cut
+# =cut
 
 # only use this method if you need unfiltered arguments with weird characters
 # in them, like passwords and fasta file data. be aware that the user's agent
@@ -115,7 +119,7 @@ Gets arguments which are being sent in via GET or POST (doesn't matter which). D
 sub get_arguments {
 	my($self,@items)=@_;
 	my @args = map {
-	  my @p = $self->{cgi}->param($_);
+	  my @p = $c->req->param($_);
 	  if(@p > 1) {
 	    carp "WARNING: multiple parameters returned for argument '$_'";
 	    \@p
@@ -129,39 +133,39 @@ sub get_arguments {
 	return $args[0];
 }
 
-=head2 jsan_use
+# =head2 jsan_use
 
-  Usage: $scrap->jsan_use('MyModule.Name');
-  Desc : add a javascript module (and its dependent javascript
-         modules) to the list of js modules needed by this page scrap
-  Args : list of module names in My.Module.Name form
-  Ret  : nothing meaningful
+#   Usage: $scrap->jsan_use('MyModule.Name');
+#   Desc : add a javascript module (and its dependent javascript
+#          modules) to the list of js modules needed by this page scrap
+#   Args : list of module names in My.Module.Name form
+#   Ret  : nothing meaningful
 
-=cut
+# =cut
 
 sub jsan_use {
   my ($self,@uses) = @_;
   $self->_jsan->add(my $a = $_) foreach @uses;
 }
 
-=head2 jsan_render_includes
+# =head2 jsan_render_includes
 
-  Usage: my $str = $scrap->jsan_render_includes
-  Desc : render HTML script-tag includes for javascript
-         modules required with jsan_use(), plus the globally-used
-         javascript modules defined herein, currently:
+#   Usage: my $str = $scrap->jsan_render_includes
+#   Desc : render HTML script-tag includes for javascript
+#          modules required with jsan_use(), plus the globally-used
+#          javascript modules defined herein, currently:
 
-           CXGN.Effects
-           CXGN.Page.FormattingHelpers
-           CXGN.Page.Toolbar
-           CXGN.UserPrefs
+#            CXGN.Effects
+#            CXGN.Page.FormattingHelpers
+#            CXGN.Page.Toolbar
+#            CXGN.UserPrefs
 
-  Args : none
-  Ret  : a string containing zero or more newline-separated
-         include statements
-  Side Effects: none
+#   Args : none
+#   Ret  : a string containing zero or more newline-separated
+#          include statements
+#   Side Effects: none
 
-=cut
+# =cut
 
 my @global_js = qw(
 		   CXGN.Effects
@@ -190,86 +194,87 @@ sub _jsan {
 	return $self->{jsan_serverside} ||= JSAN::ServerSide->new( %{ $self->{context}->_jsan_params } );
 }
 
-=head2 cgi_params
+# =head2 cgi_params
 
-Wrapper for CGI->new()->Vars(). Used when you have many arguments with the same name.
+# Wrapper for CGI->new()->Vars(). Used when you have many arguments with the same name.
 
-	#Example
-	my %params=$scrap->params();
+# 	#Example
+# 	my %params=$scrap->params();
 
-=cut
+# =cut
 
 # if you have lists of same-named parameters you will probably want to
 # handle that yourself using this function which returns a hash (not
 # hash reference)
 sub cgi_params {
-	return shift->{cgi}->Vars();
+	return %{$c->req->params};
 }
 
-=head2 get_hostname
+# =head2 get_hostname
 
-  Usage: my $hostname = $page->hostname();
-  Desc : get the hostname in the current page request (from
-         CGI::server_name())
-  Args : none
-  Ret  : hostname string
-  Side Effects: none
+#   Usage: my $hostname = $page->hostname();
+#   Desc : get the hostname in the current page request (from
+#          CGI::server_name())
+#   Args : none
+#   Ret  : hostname string
+#   Side Effects: none
 
-=cut
+# =cut
 
 sub get_hostname {
-  shift->{cgi}->server_name();
+    my (undef,$n) = split m| [:/]+ |x, $c->req->base;
+    return $n;
 }
 
-=head2 get_request
+# =head2 get_request
 
-  Usage: my $req = $page->get_request();
-  Desc : get the CGI object for the current page
-         request
-  Args : none
-  Ret  : an Apache object
-  Side Effects: none
+#   Usage: my $req = $page->get_request();
+#   Desc : get the CGI object for the current page
+#          request
+#   Args : none
+#   Ret  : an Apache object
+#   Side Effects: none
 
-=cut
+# =cut
 
 sub get_request {
-  shift->{cgi};
+    $c->req
 }
 
 
-=head2 is_bot_request
+# =head2 is_bot_request
 
-  Usage: print "it's a bot" if $page->is_bot_request;
-  Desc : return true if this page request is probably coming from a
-         web-crawling robot
-  Args : none
-  Ret  : boolean
-  Side Effects: none
+#   Usage: print "it's a bot" if $page->is_bot_request;
+#   Desc : return true if this page request is probably coming from a
+#          web-crawling robot
+#   Args : none
+#   Ret  : boolean
+#   Side Effects: none
 
-=cut
+# =cut
 
 sub is_bot_request {
-  my $user_agent = shift->get_request->user_agent;
+  my $user_agent = $c->req->user_agent;
 
   return 1 if
-    ( $user_agent =~ m|bot/\d|i #< will get google, msn
-      || $user_agent =~ /Yahoo!?\s+Slurp/i #< will get yahoo
-    );
+      $user_agent =~ m|bot/\d|i #< will get google, msn
+   || $user_agent =~ /Yahoo!?\s+Slurp/i #< will get yahoo
+       ;
 
   return;
 }
 
 
-=head2 send_content_type_header
+# =head2 send_content_type_header
 
-  Usage: $page->send_content_type_header()
-  Desc : set an http content-type header
-  Args : optional string content type to send,
-         defaults to 'text/html'
-  Ret  : nothing meaningful
-  Side Effects: dies on error
+#   Usage: $page->send_content_type_header()
+#   Desc : set an http content-type header
+#   Args : optional string content type to send,
+#          defaults to 'text/html'
+#   Ret  : nothing meaningful
+#   Side Effects: dies on error
 
-=cut
+# =cut
 
 
 sub send_content_type_header {
@@ -278,58 +283,50 @@ sub send_content_type_header {
 }
 
 
-=head2 path_to
+# =head2 path_to
 
-  Usage: $page->path_to('/something/somewhere.txt');
-  Desc : get the full path to a file relative to the
-         base path of the web server
-  Args : file path relative to the site root
-  Ret  : absolute file path
-  Side Effects: dies on error
+#   Usage: $page->path_to('/something/somewhere.txt');
+#   Desc : get the full path to a file relative to the
+#          base path of the web server
+#   Args : file path relative to the site root
+#   Ret  : absolute file path
+#   Side Effects: dies on error
 
-=cut
+# =cut
 
 sub path_to {
-    shift->{vhost}->path_to(@_)
+    $c->path_to(@_)
 }
 
 
-=head2 tempfiles_subdir
+# =head2 tempfiles_subdir
 
-  Usage: my $dir = $page->tempfiles_subdir('some','dir');
-  Desc : get the path to this site's web-server-accessible tempfiles directory.
-  Args : (optional) one or more directory names to append onto the tempdir root
-  Ret  : full filesystem pathname to the relevant dir
-  Side Effects: dies on error
+#   Usage: my $dir = $page->tempfiles_subdir('some','dir');
+#   Desc : get the path to this site's web-server-accessible tempfiles directory.
+#   Args : (optional) one or more directory names to append onto the tempdir root
+#   Ret  : full filesystem pathname to the relevant dir
+#   Side Effects: dies on error
 
-=cut
+# =cut
 
 sub tempfiles_subdir {
-    shift->{vhost}->tempfiles_subdir(@_);
+    shift;
+    $c->tempfiles_subdir(@_);
 }
 
 
-=head2 get_conf
+# =head2 get_conf
 
-  convenience method, forwards to to CXGN::VHost::get_conf();
+#   convenience method, forwards to to CXGN::VHost::get_conf();
 
-  equivalent to doing CXGN::VHost->new->get_conf(@_);
+#   equivalent to doing CXGN::VHost->new->get_conf(@_);
 
-=cut
+# =cut
 
 sub get_conf {
   my $self = shift;
-  return $self->{vhost}->get_conf(@_);
+  return $c->get_conf(@_);
 }
-
-
-=head1 AUTHORS
-
-John Binns - John Binns <zombieite@gmail.com>  (methods)
-
-Christopher Carpita  <csc32@cornell.edu> (constructor/org)
-
-=cut
 
 ####
 1; # do not remove
