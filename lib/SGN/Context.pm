eac4c1d4568cc2f12c32e3ca9ea20acd31f84e6d
Robert Buels <rmb32@cornell.edu>
2010-08-04 14:39:53 -0700

    deprecated SGN::Context

--- lib/SGN/Context.pm
+++ lib/SGN/Context.pm
@@ -1,29 +1,35 @@
 =head1 NAME
 
-SGN::Context - configuration and context object, meant to export a
-similar interface to the Catalyst context object, to help smooth our
-transition to Catalyst.
-
-=head1 SYNOPSIS
-
-  my $c = SGN::Context->new;
-  my $c = SGN::Context->instance; # new() and instance() do the same thing
-
-  # Catalyst-compatible
-  print "my_conf_variable is ".$c->get_conf('my_conf_variable');
-
-=head1 DESCRIPTION
-
-Note that this object is a singleton, based on L<MooseX::Singleton>.
-There is only ever 1 instance of it.
-
-=head1 ROLES
-
-Does: L<SGN::SiteFeatures>, L<SGN::Site>
-
-=head1 OBJECT METHODS
+SGN::Context - deprecated, do not use in new code
 
 =cut
+
+# =head1 NAME
+
+# SGN::Context - configuration and context object, meant to export a
+# similar interface to the Catalyst context object, to help smooth our
+# transition to Catalyst.
+
+# =head1 SYNOPSIS
+
+#   my $c = SGN::Context->new;
+#   my $c = SGN::Context->instance; # new() and instance() do the same thing
+
+#   # Catalyst-compatible
+#   print "my_conf_variable is ".$c->get_conf('my_conf_variable');
+
+# =head1 DESCRIPTION
+
+# Note that this object is a singleton, based on L<MooseX::Singleton>.
+# There is only ever 1 instance of it.
+
+# =head1 ROLES
+
+# Does: L<SGN::SiteFeatures>, L<SGN::Site>
+
+# =head1 OBJECT METHODS
+
+# =cut
 
 package SGN::Context;
 use MooseX::Singleton;
@@ -41,8 +47,6 @@ use Storable ();
 use URI ();
 
 use JSAN::ServerSide;
-
-
 
 =head2 path_to
 
@@ -116,14 +120,14 @@ sub js_import_uris {
 }
 
 
-=head2 req
+# =head2 req
 
-  Usage: $c->req->param('foo')
-  Desc : get a CGI-compatible query object for the current request
-  Args : none
-  Ret  : a CGI-compatible query object
+#   Usage: $c->req->param('foo')
+#   Desc : get a CGI-compatible query object for the current request
+#   Args : none
+#   Ret  : a CGI-compatible query object
 
-=cut
+# =cut
 
 sub req {
     CGI->new
