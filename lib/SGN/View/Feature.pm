77af48e1c2c89d61353e89e3777ef5aea41dbcec
Jonathan "Duke" Leto <jonathan@leto.net>
2010-08-04 11:20:53 -0700

    Move related_stats to SGN::View::Feature and update tests

--- lib/SGN/View/Feature.pm
+++ lib/SGN/View/Feature.pm
@@ -1,8 +1,19 @@
-use MooseX::Declare;
-
-class SGN::View::Feature {
-    method render($c,$name) {
-        return "Render feature $name\n";
+package SGN::View::Feature;
+use base 'Exporter';
+our @EXPORT_OK = qw/related_stats/;
+sub related_stats {
+    my ($features) = @_;
+    my $stats = { };
+    my $total = scalar @$features;
+    for my $f (@$features) {
+            $stats->{$f->type->name}++;
     }
-
+    my $data = [ ];
+    for my $k (sort keys %$stats) {
+        push @$data, [ $k => $stats->{$k} ];
+    }
+    push @$data, [ "Total" => $total ];
+    return $data;
 }
+
+1;
