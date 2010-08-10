48896d71abce375a48acc8654b2a9f928e5ca11c
isaak <iyt2@cornell.edu>
2010-08-10 13:30:10 +0200

    R code tidying...

--- cgi-bin/phenome/cvterm_qtl.r
+++ cgi-bin/phenome/cvterm_qtl.r
@@ -20,57 +20,102 @@ options(echo = FALSE)
 library(qtl)
 
 allargs<-commandArgs()
-warning()
 
-infile <- grep("infile_list", allargs, ignore.case=TRUE, perl=TRUE, value=TRUE)
+infile <- grep("infile_list",
+               allargs, ignore.case=TRUE,
+               perl=TRUE,
+               value=TRUE
+               )
 
-outfile<-grep("outfile_list", allargs, ignore.case=TRUE, perl=TRUE, value=TRUE)
+outfile<-grep("outfile_list",
+              allargs,
+              ignore.case=TRUE,
+              perl=TRUE,
+              value=TRUE
+              )
 
-statfile<-grep("stat", allargs, ignore.case=TRUE, perl=TRUE, value=TRUE)
+statfile<-grep("stat",
+               allargs,
+               ignore.case=TRUE,
+               perl=TRUE,
+               value=TRUE
+               )
 
 
 ##### stat files
-statfiles<-scan(statfile, what="character")
-#print(statfiles)
+statfiles<-scan(statfile,
+                what="character"
+                )
+
 
 ###### QTL mapping method ############
-qtlmethodfile<-grep("stat_qtl_method", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-qtlmethod<-scan(qtlmethodfile, what="character", sep="\n")
+qtlmethodfile<-grep("stat_qtl_method",
+                    statfiles,
+                    ignore.case=TRUE,
+                    fixed = FALSE,
+                    value=TRUE
+                    )
+qtlmethod<-scan(qtlmethodfile,
+                what="character",
+                sep="\n"
+                )
 #print(qtlmethod)
-if (qtlmethod == "Maximum Likelihood") {
+if (qtlmethod == "Maximum Likelihood")
+{
   qtlmethod<-c("em")
-  #print(qtlmethod)
-
 } else
 
-if (qtlmethod == "Haley-knott Regression") {
+if (qtlmethod == "Haley-knott Regression")
+{
   qtlmethod<-c("hk")
 
 } else
 
-if (qtlmethod == "Multiple Imputation") {
+if (qtlmethod == "Multiple Imputation")
+{
   qtlmethod<-c("imp")
 
 } else
-if (qtlmethod == "Marker Regression") {
+if (qtlmethod == "Marker Regression")
+{
   qtlmethod<-c("mr")
 }
 
 ###### QTL model ############
-qtlmodelfile<-grep("stat_qtl_model", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-qtlmodel<-scan(qtlmodelfile, what="character", sep="\n")
-if (qtlmodel == "Single-QTL Scan") {
+qtlmodelfile<-grep("stat_qtl_model",
+                   statfiles,
+                   ignore.case=TRUE,
+                   fixed = FALSE,
+                   value=TRUE
+                   )
+qtlmodel<-scan(qtlmodelfile,
+               what="character",
+               sep="\n"
+               )
+if (qtlmodel == "Single-QTL Scan")
+{
   qtlmodel<-c("scanone")
-
 } else
-if  (qtlmodel == "Two-QTL Scan") {
+if  (qtlmodel == "Two-QTL Scan")
+{
   qtlmodel<-c("scantwo")
 }
 
 ###### permutation############
-userpermufile<-grep("stat_permu_test", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-userpermuvalue<-scan(userpermufile, what="numeric", dec = ".", sep="\n")
-if (userpermuvalue == "None") {
+userpermufile<-grep("stat_permu_test",
+                    statfiles,
+                    ignore.case=TRUE,
+                    fixed = FALSE,
+                    value=TRUE
+                    )
+userpermuvalue<-scan(userpermufile,
+                     what="numeric",
+                     dec = ".",
+                     sep="\n"
+                     )
+
+if (userpermuvalue == "None")
+{
   userpermuvalue<-c(0)
 }
 userpermuvalue<-as.numeric(userpermuvalue)
@@ -81,42 +126,94 @@ userpermuvalue<-as.numeric(userpermuvalue)
 
 
 ######genome step size############
-stepsizefile<-grep("stat_step_size", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-stepsize<-scan(stepsizefile, what="numeric", dec = ".", sep="\n")
-if (stepsize == "zero") {
+stepsizefile<-grep("stat_step_size",
+                   statfiles,
+                   ignore.case=TRUE,
+                   fixed = FALSE,
+                   value=TRUE)
+stepsize<-scan(stepsizefile,
+               what="numeric",
+               dec = ".",
+               sep="\n"
+               )
+if (stepsize == "zero")
+{
   stepsize<-c(0)
 }
 stepsize<-as.numeric(stepsize)
 
 ######genotype calculation method############
-genoprobmethodfile<-grep("stat_prob_method", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-genoprobmethod<-scan(genoprobmethodfile, what="character", dec = ".", sep="\n")
+genoprobmethodfile<-grep("stat_prob_method",
+                         statfiles,
+                         ignore.case=TRUE,
+                         fixed = FALSE,
+                         value=TRUE
+                         )
+genoprobmethod<-scan(genoprobmethodfile,
+                     what="character",
+                     dec = ".",
+                     sep="\n"
+                     )
 
 
 ########No. of draws for sim.geno method###########
 drawsnofile<-c()
-if (is.logical(grep("stat_no_draws", statfiles))==TRUE) {
-  drawsnofile<-(grep("stat_no_drwas", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE))
+if (is.logical(grep("stat_no_draws",
+                    statfiles))==TRUE
+    )
+{
+  drawsnofile<-(grep("stat_no_drwas",
+                     statfiles,
+                     ignore.case=TRUE,
+                     fixed = FALSE,
+                     value=TRUE)
+                )
 }
     
-if (is.logical(drawsnofile) ==TRUE) {
-  drawsno<-scan(drawsnofile, what="numeric", dec = ".", sep="\n")
+if (is.logical(drawsnofile) ==TRUE)
+{
+  drawsno<-scan(drawsnofile,
+                what="numeric",
+                dec = ".",
+                sep="\n"
+                )
   drawsno<-as.numeric(drawsno)
 }
-########significance level for genotype probablity calculation###########
-genoproblevelfile<-grep("stat_prob_level", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-genoproblevel<-scan(genoproblevelfile, what="numeric", dec = ".", sep="\n")
+########significance level for genotype
+#######probablity calculation
+genoproblevelfile<-grep("stat_prob_level",
+                        statfiles,
+                        ignore.case=TRUE,
+                        fixed = FALSE,
+                        value=TRUE
+                        )
+genoproblevel<-scan(genoproblevelfile,
+                    what="numeric",
+                    dec = ".",
+                    sep="\n"
+                    )
 genoproblevel<-as.numeric(genoproblevel)
 
 
-########significance level for permutation test###########
-permuproblevelfile<-grep("stat_permu_level", statfiles, ignore.case=TRUE, fixed = FALSE, value=TRUE)
-permuproblevel<-scan(permuproblevelfile, what="numeric", dec = ".", sep="\n")
+########significance level for permutation test
+permuproblevelfile<-grep("stat_permu_level",
+                         statfiles,
+                         ignore.case=TRUE,
+                         fixed = FALSE,
+                         value=TRUE
+                         )
+permuproblevel<-scan(permuproblevelfile,
+                     what="numeric",
+                     dec = ".",
+                     sep="\n"
+                     )
 permuproblevel<-as.numeric(permuproblevel)
 
 
 #########
-infile<-scan(file=infile,  what="character")#container for the ff
+infile<-scan(file=infile,
+             what="character"
+             )#container for the ff
 
 cvtermfile<-infile[1]#file that contains the cvtername
 popid<-infile[2]#population dataset identifier
@@ -125,62 +222,123 @@ phenodata<-infile[4] #file name for phenotype dataset
 permufile<-infile[5]
 crossfile<-infile[6]
 
-print(crossfile)
-cross<-scan(crossfile, what="character", sep="\n")
+
+cross<-scan(crossfile,
+            what="character",
+            sep="\n"
+            )
 
 popdata<-c()
-if (cross == "f2") {
-  popdata<- read.cross("csvs", genfile=genodata, phefile=phenodata, na.strings=c("NA"), genotypes=c("1", "2", "3", "4", "5"), estimate.map=TRUE, convertXdata=TRUE)
+if (cross == "f2")
+{
+  popdata<- read.cross("csvs",
+                       genfile=genodata,
+                       phefile=phenodata,
+                       na.strings=c("NA", "-"),
+                       genotypes=c("1", "2", "3", "4", "5"),
+                       )
+
   popdata<-jittermap(popdata)
 } else
-if (cross == "bc") {
-  popdata<- read.cross("csvs", genfile=genodata, phefile=phenodata, na.strings=c("NA"), genotypes=c("1", "2"), estimate.map=TRUE, convertXdata=TRUE)
+if (cross == "bc")
+{
+  popdata<- read.cross("csvs",
+                       genfile=genodata,
+                       phefile=phenodata,
+                       na.strings=c("NA", "-"),
+                       genotypes=c("1", "2"),                      
+                       )
+
   popdata<-jittermap(popdata)
 }  
 
 
-if (qtlmethod != "mr") {
-  if (genoprobmethod == "Calculate") {
-    popdata<-calc.genoprob(popdata, step=stepsize, error.prob=genoproblevel)
-  #calculates the qtl genotype probablity at the specififed step size and probability level
+if (qtlmethod != "mr")
+{
+  if (genoprobmethod == "Calculate")
+    {
+    popdata<-calc.genoprob(popdata,
+                           step=stepsize,
+                           error.prob=genoproblevel
+                           )
+  #calculates the qtl genotype probablity at
+  #the specififed step size and probability level
   } else
-  if (genoprobmethod == "Simulate") {
-    popdata<-sim.genoprob(popdata, n.draws= drawsno, step=stepsize, error.prob=genoproblevel, stepwidth="fixed")
-  #calculates the qtl genotype probablity at the specififed step size
+  if (genoprobmethod == "Simulate")
+    {
+    popdata<-sim.genoprob(popdata,
+                          n.draws= drawsno,
+                          step=stepsize,
+                          error.prob=genoproblevel,
+                          stepwidth="fixed"
+                          ) 
   }
 }
 
 
-cvterm<-scan(file=cvtermfile, what="character") #reads the cvterm
-cv<-find.pheno(popdata, cvterm)#returns the col no. of the cvterm
+cvterm<-scan(file=cvtermfile,
+             what="character"
+             ) #reads the cvterm
+cv<-find.pheno(popdata,
+               cvterm
+               )#returns the col no. of the cvterm
 
 
-permuvalues<-scan(file=permufile, what="character")
+permuvalues<-scan(file=permufile,
+                  what="character"
+                  )
 
 permuvalue1<-permuvalues[1]
 permuvalue2<-permuvalues[2]
-if ((is.logical(permuvalue1) == FALSE)) {
-  if (qtlmodel == "scanone") {
-    if (userpermuvalue == 0 ) {
-      popdataperm<-scanone(popdata, pheno.col=cv, model="normal",  method=qtlmethod)
-      #permu<-summary(popdataperm, alpha=c(0.05))
+if ((is.logical(permuvalue1) == FALSE))
+{
+  if (qtlmodel == "scanone")
+    {
+    if (userpermuvalue == 0 )
+      {
+      popdataperm<-scanone(popdata,
+                           pheno.col=cv,
+                           model="normal",
+                           method=qtlmethod
+                           )
+      
     } else
-    if (userpermuvalue != 0) {
-      popdataperm<-scanone(popdata, pheno.col=cv, model="normal", n.perm = userpermuvalue, method=qtlmethod)
-      permu<-summary(popdataperm, alpha=permuproblevel)
-      #print(permu)
+    if (userpermuvalue != 0)
+      {
+      popdataperm<-scanone(popdata,
+                           pheno.col=cv,
+                           model="normal",
+                           n.perm = userpermuvalue,
+                           method=qtlmethod
+                           )
+      permu<-summary(popdataperm,
+                     alpha=permuproblevel
+                     )
     }
   }else
-  if (qtlmethod != "mr") {
-    if (qtlmodel == "scantwo") {
-      if (userpermuvalue == 0 ) {
-        popdataperm<-scantwo(popdata, pheno.col=cv, model="normal", method=qtlmethod)
-        #permu<-summary(popdataperm)
+  if (qtlmethod != "mr")
+    {
+    if (qtlmodel == "scantwo")
+      {
+      if (userpermuvalue == 0 )
+        {
+        popdataperm<-scantwo(popdata,
+                             pheno.col=cv,
+                             model="normal",
+                             method=qtlmethod
+                             )
       } else
-      if (userpermuvalue != 0) {
-        popdataperm<-scantwo(popdata, pheno.col=cv, model="normal", n.perm = userpermuvalue, method=qtlmethod)
-        permu<-summary(popdataperm, alpha=permuproblevel)
-        #print(permu) 
+      if (userpermuvalue != 0)
+        {
+        popdataperm<-scantwo(popdata,
+                             pheno.col=cv,
+                             model="normal",
+                             n.perm = userpermuvalue,
+                             method=qtlmethod
+                             )
+        permu<-summary(popdataperm,
+                       alpha=permuproblevel
+                       )
     
       }
     }
@@ -190,21 +348,43 @@ if ((is.logical(permuvalue1) == FALSE)) {
 
 chrlist<-c("chr1")
 
-for (no in 2:12) {
-  chr<-paste("chr", no, sep="")
-  chrlist<-append(chrlist, chr)
+for (no in 2:12)
+{
+  chr<-paste("chr",
+             no,
+             sep=""
+             )
+  chrlist<-append(chrlist,
+                  chr
+                  )
 }
 
-chrdata<-paste(cvterm, popid, "chr1", sep="_")
+chrdata<-paste(cvterm,
+               popid,
+               "chr1",
+               sep="_"
+               )
 chrtest<-c("chr1")
+
 for (ch in chrlist) {
   if (ch=="chr1"){
-    chrdata<-paste(cvterm, popid, ch, sep="_");
+    chrdata<-paste(cvterm,
+                   popid,
+                   ch,
+                   sep="_"
+                   )
   }
   
-  else {
-    n<-paste(cvterm, popid, ch, sep="_");
-    chrdata<-append(chrdata, n)
+  else
+    {
+    n<-paste(cvterm,
+             popid,
+             ch,
+             sep="_"
+             )
+    chrdata<-append(chrdata,
+                    n
+                    )
     
   }
 } 
@@ -214,51 +394,95 @@ chrno<-1
 datasummary<-c()
 confidenceints<-c()
 
-
-for (i in chrdata){
- 
+for (i in chrdata)
+{  
+  filedata<-paste(cvterm,
+                  popid,
+                  chrno,
+                  sep="_"
+                  )
+  filedata<-paste(filedata,
+                  "txt",
+                  sep="."
+                  )
   
-  filedata<-paste(cvterm, popid, chrno, sep="_");
-  filedata<-paste(filedata,"txt", sep=".");
-  
-  i<-scanone(popdata, chr=chrno, pheno.col=cv)
-  position<-max(i, chr=chrno)
+  i<-scanone(popdata,
+             chr=chrno,
+             pheno.col=cv
+             )
+  position<-max(i,
+                chr=chrno
+                )
   
   p<-position[2]
   p<-p[1, ]
-  peakmarker<-find.marker(popdata, chr=chrno, pos=p)  
-  confidenceint<-bayesint(i, chr=chrno, prob=0.95, expandtomarkers=TRUE)
+  peakmarker<-find.marker(popdata,
+                          chr=chrno,
+                          pos=p
+                          )  
+  confidenceint<-bayesint(i,
+                          chr=chrno,
+                          prob=0.95,
+                          expandtomarkers=TRUE
+                          )
   confidenceint<-rownames(confidenceint)
   print (confidenceint)
-  confidenceint<-c(chrno, confidenceint, peakmarker)
+  confidenceint<-c(chrno,
+                   confidenceint,
+                   peakmarker
+                   )
   print(confidenceint)
-  if (chrno==1) { 
+  if (chrno==1)
+    { 
     datasummary<-i
     confidenceints<-confidenceint
-}
-  if (chrno > 1 ) {
-    datasummary<-rbind(datasummary, i)
-    confidenceints<-rbind(confidenceints, confidenceint)   
+  }
+  if (chrno > 1 )
+    {
+    datasummary<-rbind(datasummary,
+                       i
+                       )
+    confidenceints<-rbind(confidenceints,
+                          confidenceint
+                          )   
   }
 
 chrno<-chrno + 1;
 
 }
-#print("cis")
-#print(confidenceints)
-#print("data summary")
-#print(datasummary)
 
-outfiles<-scan(file=outfile,  what="character")
+outfiles<-scan(file=outfile,
+               what="character"
+               )
 qtlfile<-outfiles[1]
 confidenceintfile<-outfiles[2]
 
-write.table(datasummary, file=qtlfile, sep="\t", col.names=NA, quote=FALSE, append=FALSE)
-write.table(confidenceints, file=confidenceintfile, sep="\t", col.names=NA, quote=FALSE, append=FALSE)
+write.table(datasummary,
+            file=qtlfile,
+            sep="\t",
+            col.names=NA,
+            quote=FALSE,
+            append=FALSE
+            )
+write.table(confidenceints,
+            file=confidenceintfile,
+            sep="\t",
+            col.names=NA,
+            quote=FALSE,
+            append=FALSE
+            )
 
-if (userpermuvalue != 0) {
-  if ((is.logical(permuvalue1) == FALSE)) {
-    write.table(permu, file=permufile, sep="\t", col.names=NA, quote=FALSE, append=FALSE)
+if (userpermuvalue != 0)
+{
+  if ((is.logical(permuvalue1) == FALSE))
+    {
+    write.table(permu,
+                file=permufile,
+                sep="\t",
+                col.names=NA,
+                quote=FALSE,
+                append=FALSE
+                )
   }
 }
 
