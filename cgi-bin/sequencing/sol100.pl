4d0832e67d406cc0dc65f78ad888dd63ba2f215c
Naama Menda <nm249@cornell.edu>
2010-08-11 11:46:57 -0400

    removed redundant code and print STDERR

--- cgi-bin/sequencing/sol100.pl
+++ cgi-bin/sequencing/sol100.pl
@@ -1,33 +1,27 @@
 use strict;
 use warnings;
-use CXGN::Page;
 use CXGN::Login;
 use CXGN::Chado::Organism;
-use CXGN::DB::DBICFactory;
 
 
-my $page = CXGN::Page->new("SOL100 sequencing project","Naama");
-my $dbh = $page->get_dbh;
+
+our $c;
+
+my $dbh = $c->dbc->dbh;
 my ($person_id, $user_type)=CXGN::Login->new($dbh)->has_session();
 my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
 my $cvterm = $schema->resultset("Cv::Cvterm")->search( { name => 'sol100' } );
 my $o_props;
 my $organism_res;
 
-my $page = CXGN::Page->new("SOL100 sequencing project","Naama");
-
-my $schema = CXGN::DB::DBICFactory->open_schema( 'Bio::Chado::Schema' );
-
 $o_props= $cvterm->search_related('organismprops') if $cvterm;
 
 my $sol=();
 
 while(my $o_prop=$o_props->next){
-    print STDERR "$o_prop\n\n\n";
     my ($organism)=$o_prop->search_related('organism');
     
     my $organism_id = $organism->organism_id;
-    print STDERR "$organism_id\n\n\n";
     $sol->{$organism->species}= $organism_id;
 }
 
