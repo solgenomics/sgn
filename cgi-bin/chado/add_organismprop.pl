306f0d692d734d67b82a94384ad79d4ecd96d73e
Naama Menda <nm249@cornell.edu>
2010-08-12 15:32:28 -0400

    test for add_organismprop.pl, and made the script pass prop_name and value as parameters to the server side script

--- cgi-bin/chado/add_organismprop.pl
+++ cgi-bin/chado/add_organismprop.pl
@@ -4,26 +4,39 @@ use warnings;
 use JSON;
 use CXGN::Scrap::AjaxPage;
 use Try::Tiny;
+use CXGN::Login;
 
-my $json = JSON->new();
 
-my $doc = CXGN::Scrap::AjaxPage->new();
-$doc->send_http_header();
-
-my ($species) = $doc->get_encoded_arguments("species");
-
-my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
+my $dbh = $c->dbc->dbh;
 
 my %status;
+my $json = JSON->new();
+my $doc = CXGN::Scrap::AjaxPage->new();
 
-try{
-    my $org = $schema->resultset('Organism::Organism')->find({species=>$species});
-    $org->create_organismprops({'sol100'=>'1'},{autocreate=>1});
-    $status{"pass"} = "Success, the object was added to the table.";
+$doc->send_http_header();
 
-}catch{
-    $status{"fail"} = "Error: The organism does not exist. Please select an organism from the suggestion list.";
-};
+my ( $person_id, $user_type ) =
+    CXGN::Login->new($dbh)->has_session();
 
+my ($species, $prop_name, $prop_value) = $doc->get_encoded_arguments("species", "prop_name", "prop_value");
+
+#if (!$prop_name || !$prop_value) { die "Must pass prop_name and prop_value! \n"; }
+    
+    
+if ( grep { /^$user_type$/ } ('curator', 'submitter', 'sequencer') ) {
+    
+    my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
+    
+    try{
+	my $org = $schema->resultset('Organism::Organism')->find({species=>$species});
+	$org->create_organismprops({$prop_name=>$prop_value},{autocreate=>1});
+	$status{"pass"} = "Success, the object was added to the table.";
+	
+    }catch{
+	$status{"fail"} = "Error: The organism does not exist. Please select an organism from the suggestion list.";
+    };
+} else { 
+    $status{"fail"} = 'You don\'t have the right privileges for adding a new organism to the sol100 project. Please contact sgn-feedback@solgenomics.net for more info.';
+}
 my $jobj = $json->encode(\%status);
 print "$jobj";
