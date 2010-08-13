0eed09fa90b9b7ecabc84041958232f251da46f3
Robert Buels <rmb32@cornell.edu>
2010-08-13 11:48:04 -0700

    clean up sol100.pl, consolidate queries

--- cgi-bin/sequencing/sol100.pl
+++ cgi-bin/sequencing/sol100.pl
@@ -3,26 +3,22 @@ use warnings;
 use CXGN::Login;
 use CXGN::Chado::Organism;
 
-
-
 our $c;
 
 my $dbh = $c->dbc->dbh;
-my ($person_id, $user_type)=CXGN::Login->new($dbh)->has_session();
+my ($person_id, $user_type) = CXGN::Login->new($dbh)->has_session();
+
 my $schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
-my $cvterm = $schema->resultset("Cv::Cvterm")->search( { name => 'sol100' } );
-my $o_props;
-my $organism_res;
 
-$o_props= $cvterm->search_related('organismprops') if $cvterm;
+my $sol100_organisms =
+    $schema->resultset( "Cv::Cvterm" )
+           ->search({ name => 'sol100' })
+           ->search_related( 'organismprops' )
+           ->search_related( 'organism' );
 
-my $sol=();
-
-while(my $o_prop=$o_props->next){
-    my ($organism)=$o_prop->search_related('organism');
-    
-    my $organism_id = $organism->organism_id;
-    $sol->{$organism->species}= $organism_id;
-}
-
-$c->forward_to_mason_view("/sequencing/sol100.mas" , user_type=>$user_type, schema=>$schema, sol=>$sol );
+$c->forward_to_mason_view(
+    "/sequencing/sol100.mas",
+    user_type => $user_type,
+    schema    => $schema,
+    sol       => { map { $_->species => $_->organism_id } $sol100_organisms->all },
+   );
