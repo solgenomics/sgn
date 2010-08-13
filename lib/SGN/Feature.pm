1c90a064bdd5faab360aaa46c7019e35df26a179
Robert Buels <rmb32@cornell.edu>
2010-08-12 19:25:00 -0700

    tweak what SGN::Feature will accept as a context object/class

--- lib/SGN/Feature.pm
+++ lib/SGN/Feature.pm
@@ -7,7 +7,7 @@ use File::Spec;
 # our context object
 has 'context' => ( documentation => 'our context class',
     is => 'ro',
-    isa => 'ClassName',
+    does => 'SGN::Role::Site::SiteFeatures',
     required => 1,
    );
 has 'enabled' => ( documentation => 'boolean flag, whether this feature is enabled',
