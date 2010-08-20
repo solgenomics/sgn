36156f8d1257388616c87fb55dfb7b441e4a48da
Jonathan "Duke" Leto <jonathan@leto.net>
2010-08-20 12:47:25 -0700

    Squash warnings in the horrific tools/blast/watch/delete.pl

--- cgi-bin/tools/blast/watch/delete.pl
+++ cgi-bin/tools/blast/watch/delete.pl
@@ -7,35 +7,34 @@ use CXGN::BlastWatch;
 use CXGN::DB::Connection;
 use CXGN::Page::FormattingHelpers qw/page_title_html modesel info_table_html hierarchical_selectboxes_html simple_selectbox_html/;
 
-my $page = CXGN::Page->new("", "Adri");
 use CatalystX::GlobalContext qw( $c );
 
 # get query_id and person_id from toplevel.pl
+my $page = CXGN::Page->new("", "Adri");
 
-&post_only unless $c->req->method eq 'POST';
+throw_error() unless $c->req->method eq 'POST';
 
 my $params = $c->req->params;
-
 my @bw_query_ids = $params->{'bw_query_id'};
-
 my $sp_person_id = $params->{'sp_person_id'};
 
 my $dbh = CXGN::DB::Connection->new("public");
 
 my $deleted = CXGN::BlastWatch::delete_query($dbh,$sp_person_id,@bw_query_ids);
 
-unless ($deleted) { &user_error("ERROR: Sequence does not exist or you do not have permission to remove it.") }
+unless ($deleted) { user_error($page, "ERROR: Sequence does not exist or you do not have permission to remove it.") }
 
 $dbh->disconnect(42);
 
-&website;
+website($page);
 
-sub post_only {
+sub throw_error {
     $c->throw( message => 'BLAST subsystem can only accept HTTP POST requests',
                is_error => 0 );
 }
 
 sub website {
+    my ($page) = @_;
     $page->header();
     print page_title_html('Success!');
     
@@ -47,6 +46,7 @@ EOF
 }
 
 sub user_error {
+    my ($page) = @_;
 
     my $reason = shift;
     
@@ -58,6 +58,5 @@ sub user_error {
 EOF
 
     $page->footer();
-    exit(0);
 }
 
