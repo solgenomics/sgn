8722a48de25272c05e9dcca96891396989576d2a
Jonathan "Duke" Leto <jonathan@leto.net>
2010-08-23 13:06:09 -0700

    Make SGN::Test diag SGN_TEST_SERVER

--- t/lib/SGN/Test.pm
+++ t/lib/SGN/Test.pm
@@ -16,6 +16,7 @@ use Exporter;
 BEGIN {
     BAIL_OUT "You need to define SGN_TEST_SERVER environment variable"
         unless $ENV{SGN_TEST_SERVER};
+    diag "Using server $ENV{SGN_TEST_SERVER}";
 }
 
 sub make_dump_tempdir {
