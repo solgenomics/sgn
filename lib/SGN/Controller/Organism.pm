94fb27ae3f64d0bed5d09f7db744f459fd6053c1
Robert Buels <rmb32@cornell.edu>
2010-08-24 13:33:18 -0700

    warning squash

--- lib/SGN/Controller/Organism.pm
+++ lib/SGN/Controller/Organism.pm
@@ -212,7 +212,7 @@ sub view_sol100 :Path('sol100/view') :Args(0) {
             image_uri => $c->uri_for( $self->action_for('organism_tree_image'), ['sol100'] ),
         },
 
-        show_org_add_form         => ( any {$user_type eq $_} qw( curator submitter sequencer ) ),
+        show_org_add_form         => ( $user_type && any {$user_type eq $_} qw( curator submitter sequencer ) ),
         organism_add_uri          => $c->uri_for( $self->action_for('add_sol100_organism')),
         organism_autocomplete_uri => $c->uri_for( $self->action_for('autocomplete'), ['Solanaceae']),
 
