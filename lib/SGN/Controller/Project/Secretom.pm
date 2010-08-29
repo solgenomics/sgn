e32ba493f00836b7724b6d9da5575f46d40860fc
Robert Buels <rmb32@cornell.edu>
2010-08-28 22:05:28 -0700

    secretome predictions page mostly done

--- lib/SGN/Controller/Project/Secretom.pm
+++ lib/SGN/Controller/Project/Secretom.pm
@@ -23,6 +23,9 @@ Catalyst Controller.
 
 =head2 index
 
+Does nothing, just forwards to the default template
+(/secretom/index.mas), see L<Catalyst::Action::RenderView>.
+
 =cut
 
 sub index :Path :Args(0) {
@@ -31,9 +34,33 @@ sub index :Path :Args(0) {
     #$c->response->body('Matched SGN::Controller::Project::Secretom in Project::Secretom.');
 }
 
+=head2 default
+
+Just forwards to the template indicated by the request path.
+
+=cut
+
 sub default :Path {
     my ( $self, $c, @args ) = @_;
-    $c->stash->{template} = (join '/', $self->action_namespace, @args).'.mas';
+    my $path = join '/', $self->action_namespace, @args;
+    if( $path =~ s/\.pl$// ) { #< attempt to su
+        $c->res->redirect( $path, 301 );
+    }
+    $c->stash->{template} = "$path.mas"
+}
+
+=head2 auto
+
+Sets some config needed by the templates.
+
+=cut
+
+sub auto :Private {
+    my ( $self, $c, @args ) = @_;
+
+    # set the root dir for secretom static files.  used by some of the
+    # templates.
+    $c->stash->{static_dir} = $c->path_to( $c->config->{root}, 'data' );
 }
 
 
