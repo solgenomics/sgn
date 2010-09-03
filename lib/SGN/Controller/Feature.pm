f5bb33274f6bd1a6aab849afcf5d249fe0c6ccf1
Jonathan "Duke" Leto <jonathan@leto.net>
2010-09-03 13:44:45 -0700

    Render a basic feature search page

--- lib/SGN/Controller/Feature.pm
+++ lib/SGN/Controller/Feature.pm
@@ -10,16 +10,109 @@ features
 use Moose;
 use namespace::autoclean;
 
+use HTML::FormFu;
+use URI::FromHash 'uri';
+use YAML::Any;
+
+has 'schema' => (
+is => 'ro',
+isa => 'DBIx::Class::Schema',
+required => 0,
+);
+
+has 'default_page_size' => (
+is => 'ro',
+default => 20,
+);
+
+
 BEGIN { extends 'Catalyst::Controller' }
 with 'Catalyst::Component::ApplicationAttribute';
 
 
 sub search :Path('/feature/search') Args(0) {
     my ( $self, $c ) = @_;
-    $c->stash({
-        template => '/feature/search.mas',
-    });
+
+    my $req = $c->req;
+    my $form = $self->_build_form;
+    $form->process( $req );
+
+    my $results;
+    if( $form->submitted_and_valid ) {
+        $results = $self->_make_feature_search_rs( $c, $form );
+    }
+
+    $c->forward_to_mason_view(
+        '/feature/search.mas',
+        form => $form,
+        results => $results,
+        pagination_link_maker => sub {
+            return uri( query => { %{$form->params}, page => shift } );
+        },
+    );
 }
+
+
+sub _build_form {
+    my ($self) = @_;
+
+    my $form = HTML::FormFu->new(Load(<<EOY));
+      method: POST
+      attributes:
+        name: feature_search_form
+        id: feature_search_form
+      elements:
+          - type: Text
+            name: feature_name
+            label: Feature Name
+            size: 30
+
+          - type: Text
+            name: feature_type
+            label: Feature Type
+            size: 30
+
+        # hidden form values for page and page size
+          - type: Hidden
+            name: page
+            value: 1
+
+          - type: Hidden
+            name: page_size
+            default: 20
+
+          - type: Submit
+            name: submit
+EOY
+
+    # set the feature type multi-select choices from the db
+    # $form->get_element({ name => 'feature_type'})->options( $self->_feature_types );
+
+    return $form;
+}
+
+# assembles a DBIC resultset for the search based on the submitted
+# form values
+sub _make_feature_search_rs {
+    my ( $self, $c, $form ) = @_;
+
+    my $rs = $self->schema->resultset('Sequence::Feature');
+
+    if( my $name = $form->param_value('feature_name') ) {
+        $rs = $rs->search({ 'lower(name)' => { like => '%'.lc( $name ).'%' }});
+    }
+
+    # page number and page size, and order by species name
+    $rs = $rs->search( undef,
+                       { page => $form->param_value('page') || 1,
+                         rows => $form->param_value('page_size') || $self->default_page_size,
+                         order_by => 'name',
+                       },
+                     );
+
+    return $rs;
+}
+
 
 __PACKAGE__->meta->make_immutable;
 1;
