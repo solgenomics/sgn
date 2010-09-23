417f095629b722d2be1a760536aa63a6e4a0d49f
Robert Buels <rmb32@cornell.edu>
2010-09-23 11:55:44 -0700

    thread necessary basepath and tempfiles_subdir vars through mason components for bac_by_bac progress overview on tomato genome page

--- cgi-bin/genomes/Solanum_lycopersicum/index.pl
+++ cgi-bin/genomes/Solanum_lycopersicum/index.pl
@@ -6,5 +6,9 @@ use CXGN::DB::Connection;
 
 my $dbh = CXGN::DB::Connection->new();
 
-if ($dbh->isa('CXGN::DB::Connection')) { print STDERR "marker 1. yes.\n"; }
-$c->forward_to_mason_view('/genomes/Solanum_lycopersicum.mas', dbh=>$dbh, basepath=>$c->get_conf('basepath') );
+$c->forward_to_mason_view(
+    '/genomes/Solanum_lycopersicum.mas',
+    dbh      => $dbh,
+    basepath => $c->get_conf('basepath'),
+    cview_tempfiles_subdir => $c->tempfiles_subdir('cview'),
+   );
