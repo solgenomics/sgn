5abd44ed14f72a8c79d5f039278b6804b3b0b5af
Robert Buels <rmb32@cornell.edu>
2010-10-19 08:48:31 -0700

    formatting cleanups to new image controller

--- lib/SGN/Controller/Image.pm
+++ lib/SGN/Controller/Image.pm
@@ -1,6 +1,3 @@
-
-use strict;
-
 package SGN::Controller::Image;
 use Moose;
 use namespace::autoclean;
@@ -9,11 +6,7 @@ use CXGN::Login;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
-with 'Catalyst::Component::ApplicationAttribute';
-
-###__PACKAGE__->config( uploadtmp => '/static/documents/tempfiles/images' );
-
-sub view :Path('/image/view/') Args(1) { 
+sub view :Path('/image/view/') Args(1) {
     my $self = shift;
     my $c = shift;
     my $image_id = shift;
@@ -23,75 +16,75 @@ sub view :Path('/image/view/') Args(1) {
     my $person_id = CXGN::Login->new($dbh)->has_session();
 
     $c->forward_to_mason_view('/image/index.mas',
-			      object_id=>$image_id, 
-			      dbh => $dbh,
-			      person_id => $person_id,
-	);
+                              object_id=>$image_id,
+                              dbh => $dbh,
+                              person_id => $person_id,
+        );
 
 }
 
-sub add :Path('/image/add') Args(0) { 
+sub add :Path('/image/add') Args(0) {
     my ($self, $c) = @_;
     my $refering_page;
     $c->forward_to_mason_view('/image/add_image.mas',
-			      refering_page => $c->req->referer(),
-			      type          => $c->req->param('type'),
-			      type_id       => $c->req->param('type_id'),
-	);
-    
+                              refering_page => $c->req->referer(),
+                              type          => $c->req->param('type'),
+                              type_id       => $c->req->param('type_id'),
+        );
+
 
 }
 
-sub confirm :Path('/image/confirm') { 
+sub confirm :Path('/image/confirm') {
     my ($self, $c) = @_;
-    
-    my $sp_person_id =CXGN::Login->new($c->dbc->dbh)->has_session();
-    if (!$sp_person_id) { 
-	die "Need to be logged in!";
+
+    my $sp_person_id = CXGN::Login->new($c->dbc->dbh)->has_session();
+    if (!$sp_person_id) {
+        die "Need to be logged in!";
 
     }
-    my $upload = $c->req()->upload('file');
-    
-    
+    my $upload = $c->req->upload('file');
+
+
     my $filename = $upload->filename();
     my $tempfile = $upload->tempname();
-    print STDERR "FILENAME: $filename TEMPNAME: $tempfile\n";
-   
-    if (! -e $tempfile) { 
-	die "No tempfile $tempfile\n";
+    #print STDERR "FILENAME: $filename TEMPNAME: $tempfile\n";
+
+    if (! -e $tempfile) {
+        die "No tempfile $tempfile\n";
     }
- 
+
     my $filename_validation_msg =  $self->validate_image_filename(basename($filename));
     if ( $filename_validation_msg )  { #if non-blank, there is a problem with Filename, print messages
 #print STDERR "Invalid Upload Filename Attempt: $temp_file_base, $filename_validation_msg \n";
-	print qq { There is a problem with the image file you selected: $filename <br />};
-	print qq { Error:  };
-	print $filename_validation_msg; 
-	print qq {<br />};
-	unlink $tempfile;  # remove upload! prevents more errors on item we have rejected
-	
-	
+        print qq { There is a problem with the image file you selected: $filename <br />};
+        print qq { Error:  };
+        print $filename_validation_msg;
+        print qq {<br />};
+        unlink $tempfile;  # remove upload! prevents more errors on item we have rejected
+
+
     }
     my $image_url = $c->tempfiles_subdir('image')."/".basename($tempfile);
     my $confirm_filename = $c->get_conf('basepath')."/".$image_url;
     if (! -e $tempfile) { die "Temp file does not exit $tempfile\n"; }
-    if (!$upload->copy_to( $confirm_filename  )) { 
-	die "Error copying $tempfile to $confirm_filename\n";
+    if (!$upload->copy_to( $confirm_filename  )) {
+        die "Error copying $tempfile to $confirm_filename\n";
     }
-	    
+
     $c->{stash}->{type}          = $c->req()->param('type');
     $c->{stash}->{refering_page} = $c->req->param('refering_page');
     $c->{stash}->{type_id}       = $c->req()->param('type_id');
     $c->{stash}->{filename}      = $filename;
     $c->{stash}->{tempfile}      = basename($tempfile);
     $c->{stash}->{image_url}     = $image_url;
-    $c->{stash}->{sp_person_id}  = $sp_person_id; 
+    $c->{stash}->{sp_person_id}  = $sp_person_id;
 }
 
 sub store :Path('/image/store') {
     my $self = shift;
     my $c = shift;
-    
+
     my $image = SGN::Image->new($c->dbc->dbh());
 
     my $tempfile = $c->req()->param('tempfile');
@@ -102,31 +95,30 @@ sub store :Path('/image/store') {
     my $sp_person_id = CXGN::Login->new($c->dbc->dbh())->has_session();
 
     my $temp_image_dir = $c->get_conf("basepath")."/".$c->tempfiles_subdir('image');
-   
-    $image -> set_sp_person_id($sp_person_id);
 
-    if ((my $err = $image->process_image($temp_image_dir."/".$tempfile, $type, $type_id))<=0) { 
-	die "An error occurred during the upload. Is the file you are uploading an image file? [$err] ";
+    $image->set_sp_person_id($sp_person_id);
+
+    if ((my $err = $image->process_image($temp_image_dir."/".$tempfile, $type, $type_id))<=0) {
+        die "An error occurred during the upload. Is the file you are uploading an image file? [$err] ";
 
     }
-    
+
     # set some image attributes...
     # the image owner...
     #print STDERR "Setting the submitter information in the image object...\n";
-    
-    $image -> set_name($filename);
-    
-    
+
+    $image->set_name($filename);
+
+
     $image->store();
-    
+
    # send_image_email($c, "store", $image, $sp_person_id, $refering_page, $type, $type_id);
     #remove the temp_file
     #
-    unlink ($temp_image_dir."/".$tempfile);
+    unlink $temp_image_dir."/".$tempfile;
 
-    my $image_id=$image->get_image_id();
-   
-    
+    my $image_id = $image->get_image_id();
+
     # go to the image detail page
     # open for editing.....
     $c->redirect("/image/view/$image_id");
@@ -138,36 +130,34 @@ sub store :Path('/image/store') {
 sub validate_image_filename :Private {
     my $self = shift;
     my $fn = shift;
-    my %file_types = ( '.jpg' => 'JPEG file',
-		       '.jpeg' => 'JPEG file',
-		       '.gif' => 'GIF file',
-		       '.pdf' => 'PDF file',
-		       '.ps'  => 'PS file',
-		       '.eps' => 'EPS file',
-		       '.png' => 'PNG file');
-    
+    my %file_types = ( '.jpg'  => 'JPEG file',
+                       '.jpeg' => 'JPEG file',
+                       '.gif'  => 'GIF file',
+                       '.pdf'  => 'PDF file',
+                       '.ps'   => 'PS file',
+                       '.eps'  => 'EPS file',
+                       '.png'  => 'PNG file');
 
-    
     # first test is non-acceptable characters in filename
     my $OK_CHARS='-a-zA-Z0-9_.@\ '; # as recommend by CERT, test for what you will allow
     my $test_fn = $fn;
     $test_fn =~ s/[^$OK_CHARS]/_/go;
     if ( $fn ne $test_fn ) {
-	#print STDERR "Upload Attempt with bad shell characters: $fn \n";
-	return "Invalid characters found in filename, must not contain
-	characters <b>\& ; : \` \' \\ \| \* \? ~ ^ < > ( ) [ ] { } \$</b>" ;
+        #print STDERR "Upload Attempt with bad shell characters: $fn \n";
+        return "Invalid characters found in filename, must not contain
+        characters <b>\& ; : \` \' \\ \| \* \? ~ ^ < > ( ) [ ] { } \$</b>" ;
     }
-    
-    my $ext; 
+
+    my $ext;
     if ($fn =~ m/^(.*)(\.\S{1,4})\r*$/) {
-	$ext = lc ($2);
-	#print STDERR "Upload Attempt with disallowed filename extension: $fn Extension: $ext\n";
-	return "File Type must be one of: .png, .jpg, .jpeg, .gif, .pdf, .ps, or .eps" unless exists $file_types{$ext};
+        $ext = lc ($2);
+        #print STDERR "Upload Attempt with disallowed filename extension: $fn Extension: $ext\n";
+        return "File Type must be one of: .png, .jpg, .jpeg, .gif, .pdf, .ps, or .eps" unless exists $file_types{$ext};
     } else {
-	#print STDERR "Upload Attempt with filename extension we could not parse: $fn \n";
-	return "File Type must be one of: .png, .jpg, .jpeg, .gif, .pdf, .ps, or .eps";
+        #print STDERR "Upload Attempt with filename extension we could not parse: $fn \n";
+        return "File Type must be one of: .png, .jpg, .jpeg, .gif, .pdf, .ps, or .eps";
     }
-    
+
     return 0;  # FALSE, if passes all tests
 }
 
@@ -183,24 +173,24 @@ sub send_image_email :Private {
     my $type_id = shift; #the database id of the refering object (locus..)
 
     my $image_id = $image->get_image_id();
-   
+
     my $person= CXGN::People::Person->new($c->dbc->dbh, $sp_person_id);
     my $user=$person->get_first_name()." ".$person->get_last_name();
-    
+
     my $type_link;
-    
-    
+
+
     my $user_link = qq | http://sgn.cornell.edu/solpeople/personal-info.pl?sp_person_id=$sp_person_id|;
     my $usermail=$person->get_contact_email();
     my $image_link = qq |http://sgn.cornell.edu/image/?image_id=$image_id|;
     if ($type eq 'locus') {
-	$type_link = qq | http://sgn.cornell.edu/phenome/locus_display.pl?locus_id=$type_id|;
+        $type_link = qq | http://sgn.cornell.edu/phenome/locus_display.pl?locus_id=$type_id|;
     }
 #    elsif ($type eq 'allele') {
-#	$type_link = qq | http://sgn.cornell.edu/phenome/allele.pl?allele_id=$type_id|;
+#       $type_link = qq | http://sgn.cornell.edu/phenome/allele.pl?allele_id=$type_id|;
 #     }
 #     elsif ($type eq 'population') {
-# 	$type_link = qq | http://sgn.cornell.edu/phenome/population.pl?population_id=$type_id|;
+#       $type_link = qq | http://sgn.cornell.edu/phenome/population.pl?population_id=$type_id|;
 #     }
 
     my $fdbk_body;
@@ -209,21 +199,19 @@ sub send_image_email :Private {
     if ($action eq 'store') {
 
         $subject="[New image associated with $type: $type_id]";
-	$fdbk_body="$user ($user_link) has associated image $image_link \n with $type: $type_link"; 
+        $fdbk_body="$user ($user_link) has associated image $image_link \n with $type: $type_link";
    }
     elsif($action eq 'delete') {
-	
-	
-	$subject="[A image-$type association removed from $type: $type_id]";
-	$fdbk_body="$user ($user_link) has removed publication $image_link \n from $type: $type_link"; 
+
+
+        $subject="[A image-$type association removed from $type: $type_id]";
+        $fdbk_body="$user ($user_link) has removed publication $image_link \n from $type: $type_link";
     }
-    
+
     CXGN::Contact::send_email($subject,$fdbk_body, 'sgn-db-curation@sgn.cornell.edu');
-    
+
 }
 
 
 
 1;
-
-
