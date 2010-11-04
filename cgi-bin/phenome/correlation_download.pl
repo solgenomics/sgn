25e3b62aa85c41e1c36b403b29b87c6e0643c6d4
Robert Buels <rmb32@cornell.edu>
2010-11-03 18:11:32 -0700

    fix content-type and file attachment headers of correlation_download

--- cgi-bin/phenome/correlation_download.pl
+++ cgi-bin/phenome/correlation_download.pl
@@ -15,36 +15,41 @@ use strict;
 
 use CXGN::DB::Connection;
 use CXGN::Phenome::Population;
-use CGI qw /:cgi-lib/ ;
-
+use CGI ();
 
 my $cgi = CGI->new();
 my %args = $cgi->Vars();
-my $pop_id = $args{'population_id'};
+my $pop_id = $args{'population_id'} + 0;
 my $corre_file = $args{'corre_file'};
 
 my $dbh = CXGN::DB::Connection->new();
 my $pop = CXGN::Phenome::Population->new( $dbh, $pop_id );
 my $name = $pop->get_name();
 
-
-print
-"Pragma: \"no-cache\"\nContent-Disposition:filename=corre_data_${pop_id}.txt\nContent-type:application/data\n\n";
-
-
 if (-e $corre_file) {
-    
+
+    print $cgi->header(
+        -type => 'text/plain',
+        '-Content-Disposition' => "attachment; filename=corre_data_${pop_id}.txt",
+       );
+
     print 'Pearson correlation coefficients and their 
            corresponding p-values for all traits in 
            population'	.  $name . "\n\n\n";
- 
 
     open my $f, "<$corre_file" or die "can't open file $corre_file: $!\n";
-    
+
     my $cols = <$f>;
     print "Traits\t" . $cols;
-    
+
     while (my $row=<$f>) {
 	print $row;
     }
+} else {
+
+    print $cgi->header(
+        -status => 404,
+        -type   => 'text/plain',
+       );
+
 }
