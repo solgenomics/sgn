d81a8cf59d9cd53298c460188020a962465f4b35
Robert Buels <rmb32@cornell.edu>
2010-11-17 17:24:06 -0800

    clean up SQL formatting

--- db/AddUserRoles.pm
+++ db/AddUserRoles.pm
@@ -26,13 +26,11 @@ sub patch {
     print "\nExecuting the SQL commands.\n";
 
     $self->dbh->do(<<EOSQL);
---do your SQL here
---
 
 CREATE TABLE sgn_people.sp_roles (
     sp_role_id serial primary key,
     name varchar(20)
-);
+  );
 
 CREATE TABLE sgn_people.sp_person_roles (
     sp_person_role_id serial primary key,
@@ -45,7 +43,10 @@ CREATE TABLE sgn_people.sp_person_roles (
     INSERT INTO sgn_people.sp_roles(name) VALUES ('submitter');
     INSERT INTO sgn_people.sp_roles(name) VALUES ('user');
 
-    INSERT INTO sgn_people.sp_person_roles (sp_role_id, sp_person_id) SELECT sp_role_id, sp_person_id FROM sgn_people.sp_person JOIN sgn_people.sp_roles ON (user_type=name);
+    INSERT INTO sgn_people.sp_person_roles (sp_role_id, sp_person_id)
+       SELECT sp_role_id, sp_person_id
+       FROM sgn_people.sp_person
+       JOIN sgn_people.sp_roles ON (user_type=name);
 
     GRANT select, update, insert, delete  ON sgn_people.sp_roles to postgres, web_usr;
     GRANT select, update, insert, delete  ON sgn_people.sp_person_roles to postgres, web_usr;
