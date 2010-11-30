d62d38b996671147d21d668a7fe11d5942d6afb5
Jonathan "Duke" Leto <jonathan@leto.net>
2010-11-29 19:21:31 -0800

    Cache featurelocs in the Catalyst stash

--- lib/SGN/Controller/Feature.pm
+++ lib/SGN/Controller/Feature.pm
@@ -12,17 +12,16 @@ use namespace::autoclean;
 use HTML::FormFu;
 use URI::FromHash 'uri';
 use YAML::Any;
-use SGN::View::Feature qw/feature_link/;
 
 has 'schema' => (
-is => 'rw',
-isa => 'DBIx::Class::Schema',
-required => 0,
+    is       => 'rw',
+    isa      => 'DBIx::Class::Schema',
+    required => 0,
 );
 
 has 'default_page_size' => (
-is => 'ro',
-default => 20,
+    is      => 'ro',
+    default => 20,
 );
 
 
@@ -36,7 +35,8 @@ sub delegate_component
     my $type_name = $feature->type->name;
     my $template  = "/feature/dhandler";
 
-    $c->stash->{feature}  = $feature;
+    $c->stash->{feature}     = $feature;
+    $c->stash->{featurelocs} = $feature->featureloc_features;
 
     # look up site xrefs for this feature
     my @xrefs = $c->feature_xrefs( $feature->name, { exclude => 'featurepages' } );
@@ -44,7 +44,7 @@ sub delegate_component
         @xrefs = map {
             $c->feature_xrefs( $_->srcfeature->name.':'.($_->fmin+1).'..'.$_->fmax, { exclude => 'featurepages' } )
         }
-        $feature->featureloc_features
+        $c->stash->{featurelocs}
     }
     $c->stash->{xrefs} = \@xrefs;
 
