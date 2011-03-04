8897fa4ae659398218a69a6347c77b0e4620ad8e
mueller <lam87@cornell.edu>
2011-03-04 09:10:41 -0500

    bare bones version of loading script. Need to add publications and GO terms.

--- bin/load_qtl_loci.pl
+++ bin/load_qtl_loci.pl
@@ -2,8 +2,9 @@
 use Modern::Perl;
 
 use CXGN::Phenome::Locus;
-use CXGN::DB::InsertDBH;
+use CXGN::Phenome::LocusMarker;
 
+use CXGN::DB::InsertDBH;
 use Getopt::Std;
 
 our(%opts);
@@ -37,22 +38,31 @@ while (<$F>) {
 	$l->set_locus_symbol($qtl);
 	$l->set_common_name_id($opts{c});
 	print STDERR "Setting description...\n";
-	$l->set_description("description");
+	$l->set_description($description);
 	$l->set_sp_person_id(222);
-	my $sth = $dbh -> prepare("SELECT marker_id FROM sgn.marker_alias WHERE alias =?");
-	$sth->execute($qtl);
 	
-	my ($marker_id) = $sth->fetchrow_array();
-	
-	if (!$marker_id) { die "Couldn't fine a marker for $qtl!"; }
-	print STDERR "Found marker $marker_id. Associating it...\n";
-	$l->add_locus_marker($marker_id);
-
 	if (!$opts{t}) { 
 	    print STDERR "Storing...\n";
-	    $l->store(); 
+	    my $locus_id = $l->store();
 	    
-	    $l->add_owner($opts{p}, 222);
+	    $l->add_owner($opts{p});
+	    
+	    my $sth = $dbh -> prepare("SELECT marker_id FROM sgn.marker_alias WHERE alias =?"); 
+	$sth->execute($qtl);
+	
+	    my ($marker_id) = $sth->fetchrow_array();
+	
+	    if (!$marker_id) { die "Couldn't find a marker for $qtl!"; }
+	    print STDERR "Found marker $marker_id. Associating it...\n";
+	    
+	    my $lm = CXGN::Phenome::LocusMarker->new($dbh);
+	    $lm->set_marker_id($marker_id);
+	    $lm->set_locus_id($locus_id);
+	    $lm->store();
+	    
+	    $l->add_locus_marker($lm);
+	    
+	    
 	} 
 
 	else { 
