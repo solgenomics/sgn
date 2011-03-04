f9d934e46597b6a910052e5b7f152009f23ff6e4
mueller <lam87@cornell.edu>
2011-03-04 09:13:59 -0500

    adding a cview controller and re-mangling some of the mason components.

---
+++ lib/SGN/Controller/Cview.pm
@@ -0,0 +1,261 @@
+
+use Modern::Perl;
+
+package SGN::Controller::Cview;
+
+use base "Catalyst::Controller"; 
+
+use Cache::File;
+use File::Spec;
+use URI::Escape;
+use CXGN::Cview::MapFactory;
+use CXGN::Cview::Map::Tools;
+use CXGN::Cview::MapOverviews::Generic;
+use CXGN::Phenome::Population;
+use CXGN::People::Person;
+use CXGN::Login;
+use CXGN::Map;
+
+
+sub auto :Args(0) { 
+    my ($self, $c) = @_;
+    print STDERR "auto: Setting some defaults...\n";
+
+    # push some useful stuff on the stash
+    #
+    $c->stash->{dbh} = $c->dbc->dbh();
+    $c->stash->{map_link} = '/cview/map.pl';
+    $c->stash->{chr_link} = '/cview/view_chromosome.pl';
+    $c->stash->{comp_map_link} = '/cview/view_maps.pl';
+    $c->stash->{default_map_id} = $c->get_conf('cview_default_map_id');
+    $c->stash->{referer} = $c->req->referer();
+    $c->stash->{tempdir} = $c->get_conf("tempfiles_subdir")."/cview";
+    $c->stash->{basepath} = $c->get_conf("basepath");
+    print STDERR "BASEPATH: ".($c->stash->{basepath})."\n";
+    return 1;
+}
+
+sub index :Path("/cview") :Args(0) { 
+    my ($self, $c) = @_;
+    
+    $c->stash->{template} = '/cview/index.mas';
+
+    my $map_factory = CXGN::Cview::MapFactory->new($c->dbc->dbh);
+    my @maps = $map_factory->get_system_maps();
+    
+    my %map_by_species;
+
+    foreach my $map (@maps) {	
+	my $species = $map->get_common_name();
+	my $long_name = $map->get_long_name();
+	my $short_name = $map->get_short_name();
+	my $id = $map->get_id();
+	push @{$map_by_species{$species} } ,  qq{ <a href="/cview/map.pl?map_version_id=$id">$short_name</a>: $long_name\n } ;
+    }
+    $c->stash->{map_by_species} = \%map_by_species;    
+}
+
+sub map :Path("/cview/map") :Args(0) { 
+    my ($self, $c) = @_;
+    
+    my @params = qw | map_id map_version_id size hilite physical force map_items |;
+
+    foreach my $param (@params) { 
+	print STDERR "map: Processing $param\n";
+	$c->stash->{$param} = $c->req->param($param);
+    }
+
+    my %marker_info;
+
+    # set up a cache for the map statistics, using Cache::File
+    #
+    my $cache_file_dir = File::Spec->catfile($c->stash->{basepath}, 
+					     $c->stash->{tempdir}, "cache_file");
+
+    tie %marker_info, 'Cache::File', { cache_root => $cache_file_dir };
+
+    # report some unusual conditions to the user.
+    #
+    my $message = ""; 
+
+    # if the map_id was supplied, convert immediately to map_version_id
+    #
+    if ($c->stash->{map_id} && !$c->stash->{map_version_id}) { 
+	$c->stash->{map_version_id} = CXGN::Cview::Map::Tools::find_current_version($c->stash->{dbh}, $c->stash->{map_id});
+    }
+    # get the map data using the CXGN::Map API.
+    #
+    my $map_factory = CXGN::Cview::MapFactory->new($c->stash->{dbh});
+    my $map = $map_factory ->create({ map_version_id => $c->stash->{map_version_id} });
+
+    if (!$map) {
+	$c->stash->{template} = '/cview/map/missing.mas';
+	return;
+    }
+
+    my $private = data_is_private($c->stash->{dbh}, $c->stash->{map_version_id});
+
+    $c->stash->{long_name} = $map->get_long_name();
+    $c->stash->{short_name} = $map->get_short_name();
+
+
+    my @hilite_markers = split /\s+|\,\s*/, $c->{stash}->{hilite};
+
+    $c->stash->{hilite_markers} = \@hilite_markers;
+
+    # calculate the size of the image based on the size parameter
+    #
+    my $image_height = 160;
+    my $size = $c->stash->{size};
+
+    if ($size < 0) { $size =0; }
+    if ($size > 10) { $size = 10;}
+
+    $c->stash->{size} = $size;
+
+    $image_height = $image_height + $image_height * $size /2;
+    my $image_width = 820;
+
+    # create an appropriate overview diagram - physical or generic
+    # (the generic will also provide an appropriate overview for the fish map).
+    #
+    my $map_overview = CXGN::Cview::MapOverviews::Generic ->
+      new($map,
+	  {
+	   force            => $c->stash->{force},
+	   basepath         => $c->stash->{basepath},
+	   tempfiles_subdir => $c->stash->{tempdir},
+	   dbh              => $c->stash->{dbh},
+	  });
+
+    $map_overview->set_image_height($image_height);
+    $map_overview->set_image_width($image_width);
+
+    # deal with marker names to be highlighted on the overview diagram
+    # (the ones to be requested to be hilited using the hilite feature)
+    #
+    my @map_items = split /\n/, $c->stash->{map_items};
+
+    $map_overview->get_map()->set_map_items(@map_items);
+    foreach my $hm (@hilite_markers) {
+      #print STDERR "Hilite marker $hm...\n";
+      $map_overview -> hilite_marker($hm);
+    }
+
+    # generate the marker list for use in the URL links
+    #
+    my $hilite_encoded = URI::Escape::uri_escape(join (" ", @hilite_markers)); 
+
+    # render the map and get the imagemap
+    #
+    $map_overview -> render_map();
+
+    $c->stash->{overview_image} = $map_overview->get_image_html();
+
+    # get the markers that could not be hilited
+    #
+    my @markers_not_found = $map_overview -> get_markers_not_found();
+
+    if (@markers_not_found) {
+      $message .= "The following markers requested for hiliting were not found on this map (click to search on other maps):<br />";
+      foreach my $m (@markers_not_found) {
+	$message .= "&nbsp;&nbsp;<a href=\"/search/markers/markersearch.pl?searchtype=exactly&amp;name=$m\">$m</a>";
+      }
+      $message .= "<br />\n";
+    }
+
+    # get chromosome stats and cache them
+    #
+    my %marker_info = ();
+    my @chr_names = $map->get_chromosome_names();
+    my $total_markers=0;
+    for (my $i=0; $i<@chr_names; $i++) {
+      my $hash_key = $c->stash->{map_version_id}."-".$i;
+      if (!exists($marker_info{$hash_key}) || $c->stash->{force}) { 
+	$marker_info{$hash_key} = $map->get_marker_count($chr_names[$i]);
+      }
+    }
+
+    my @chromosome_stats = map { $marker_info{$c->stash->{map_version_id}."-".$_}}  $map->get_chromosome_names();
+
+    $c->stash->{message} = $message;
+    $c->stash->{abstract} = $map->get_abstract();
+    $c->stash->{can_overlay} = $map->can_overlay();
+    $c->stash->{marker_stats} = $map->get_marker_type_stats();
+    $c->stash->{chromosome_stats} = \@chromosome_stats;
+    $c->stash->{template} = "/cview/map/index.mas";
+}
+
+
+sub data_is_private {
+    my $dbh = shift;
+    my $map_version_id = shift;
+
+    if ($map_version_id!~/^\d+$/) { return ''; }
+    my $genetic_map = CXGN::Map->new($dbh, { map_version_id=>$map_version_id}); 
+    my $pop_id = $genetic_map->get_population_id();
+    my $pop = CXGN::Phenome::Population->new($dbh, $pop_id);
+    my $is_public = $pop->get_privacy_status();
+    
+    my ($login_id, $user_type) = CXGN::Login->new($dbh)->has_session();
+    
+    print STDERR "$is_public\n";
+    
+    if ($is_public ||             
+	    $user_type eq 'curator' || 
+	    $login_id == 
+	$pop->get_sp_person_id() 
+       )  {
+      return undef;
+    } 
+    else {
+      
+	   my $owner_id = $pop->get_sp_person_id(); 
+    
+
+	   my $submitter = CXGN::People::Person->new($dbh, $owner_id);
+    	   my $submitter_name = $submitter->get_first_name()." ".$submitter->get_last_name();
+    	   my $submitter_link = qq |<a href="/solpeople/personal-info.pl?sp_person_id=$owner_id">$submitter_name</a> |;
+       
+	   my $private = qq | <p>This genetic map is not public yet. 
+       	              If you would like to know more about this data, 
+                      please contact the owner of the data: <b>$submitter_link</b> 
+                      or email to SGN:
+                      <a href=mailto:sgn-feedback\@sgn.cornell.edu>
+                      sgn-feedback\@sgn.cornell.edu</a>.
+                    </p> |;
+
+         return $private;
+    } 
+    
+}
+
+
+sub chromosome :Path("/cview/chr") :Args(0) { 
+    my ($self, $c) = @_;
+
+    my @params = qw | map_id map_version_id chr_nr cM zoom show_physical show_ruler show_IL comp_map_id comp_map_version_id comp_chr color_model map_chr_select size hilite cM_start cM_end confidence show_zoomed marker_type show_offsets force clicked |;
+    
+    foreach my $param (@params) { 
+	print STDERR "Preparing param $param...\n";
+	$c->stash->{$param}= $c->req->param($param);
+    }
+
+    $c->stash->{template} = '/cview/chr/index.mas';
+}
+
+
+sub maps :Path("/cview/view_maps.pl") :Args(0) { 
+    my ($self, $c) = @_;
+
+    $c->stash->{template} = '/cview/map/comparison.mas';
+    $c->stash->{dbh} = $c->dbc->dbh();
+
+    my @params = qw | center_map center_map_version_id show_physical show_ruler show_IL left_map left_map_version_id right_map right_map_version_id color_model |; 
+    foreach my $param (@params) { 
+	$c->stash->{$param} = $c->req->param($param);
+    }
+}
+
+
+1;
