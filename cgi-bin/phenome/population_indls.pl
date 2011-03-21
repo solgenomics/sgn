239fa0291b1af2cc2fc406d54158d98446bd5bc5
isaak <iyt2@cornell.edu>
2011-03-21 12:40:57 +0100

    removed not used code...

--- cgi-bin/phenome/population_indls.pl
+++ cgi-bin/phenome/population_indls.pl
@@ -8,7 +8,6 @@ a redirect for phenome/population_indls.pl to phenome/qtl_analysis.pl: for links
 use strict;
 use CGI;
 
-use CatalystX::GlobalContext qw( $c );
 
 my $cgi = CGI->new();
 my $pop_id = $cgi->param('population_id');
@@ -18,8 +17,4 @@ my $term_id = $cgi->param('cvterm_id');
 print $cgi->redirect("qtl_analysis.pl?population_id=$pop_id&cvterm_id=$term_id", 301);
 
 
-#$c->forward_to_mason_view('/qtl/qtl_analysis.mas',
-#                          url           => '/phenome/qtl_analysis.pl',
-#                          population_id => $pop_id,
-#                          cvterm_id     => $term_id
-#    );
+
