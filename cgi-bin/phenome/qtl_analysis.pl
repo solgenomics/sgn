c2e3af9164f2502e1ae42f7c36d4730f3633c4d1
isaak <iyt2@cornell.edu>
2011-03-23 09:48:05 +0100

    some nominal changes..

--- cgi-bin/phenome/qtl_analysis.pl
+++ cgi-bin/phenome/qtl_analysis.pl
@@ -15,10 +15,10 @@ Isaak Y Tecle (iyt2@cornell.edu)
 use strict;
 use warnings;
 
-my $population_indls_detail_page =
-  CXGN::Phenome::PopulationIndlsDetailPage->new();
+my $qtl_analysis_detail_page =
+  CXGN::Phenome::QtlAnalysisDetailPage->new();
 
-package CXGN::Phenome::PopulationIndlsDetailPage;
+package CXGN::Phenome::QtlAnalysisDetailPage;
 
 
 
@@ -68,7 +68,7 @@ sub new
 {
     my $class = shift;
     my $self  = $class->SUPER::new(@_);
-    $self->set_script_name("population_indls.pl");
+    $self->set_script_name("qtl_analysis.pl");
 
     return $self;
 }
@@ -265,7 +265,7 @@ EOS
 
     
     my $page =
-"../phenome/population_indls.pl?population_id=$population_id&amp;cvterm_id=$term_id";
+"../phenome/qtl_analysis.pl?population_id=$population_id&amp;cvterm_id=$term_id";
     $args{calling_page} = $page;
 
     my $pubmed;
