5ea17af91ed8eb1b3717a6e265aa90953a1c8b8b
Robert Buels <rmb32@cornell.edu>
2011-03-24 10:25:26 -0700

    make t/test_all.pl run prove inline, and propagate MyDevLibs uses to it

--- t/test_all.pl
+++ t/test_all.pl
@@ -2,19 +2,37 @@
 use strict;
 use warnings;
 use LWP::Simple;
+use App::Prove;
 
-use lib 'lib';
 use Catalyst::ScriptRunner;
 
-my $server_pid;
-if( $server_pid = fork ) {
+use lib 'lib';
+use SGN::Devel::MyDevLibs;
+
+my @test_paths = @ARGV;
+@test_paths = ('t') unless @test_paths;
+
+if( my $server_pid = fork ) {
+
+    # testing process
     sleep 1 until get 'http://localhost:3000';
     $ENV{SGN_TEST_SERVER}='http://localhost:3000';
-    system("prove -lr t");
+    my $app = App::Prove->new;
+    $app->process_args(
+        '-lrwv',
+        ( map { -I => $_ } @INC ),
+        @test_paths
+        );
+    exit( $app->run ? 0 : 1 );
+
+    END { kill 15, $server_pid if $server_pid }
+
 } else {
+
+    # server process
     $ENV{SGN_TEST_MODE} = 1;
     Catalyst::ScriptRunner->run('SGN', 'Server');
     exit;
+
 }
 
-END { kill 15, $server_pid if $server_pid }
