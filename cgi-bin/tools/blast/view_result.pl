d4d7a3474a0160d4ae121d6a29a9a0771806f79d
Robert Buels <rmb32@cornell.edu>
2011-04-07 15:48:04 -0700

    improve layout of match-linking dialog in blast tool

--- cgi-bin/tools/blast/view_result.pl
+++ cgi-bin/tools/blast/view_result.pl
@@ -65,16 +65,15 @@ $page->header();
 # stuff to support AJAXy disambiguation of site xrefs
 print <<EOJS;
 <div id="xref_menu_popup" title="Match information">
-  <h1 class="popup_title"></h1>
   <dl>
-    <dt>Subject details</dt>
-      <dd class="identifier_link"></dd>
-    <dt>Subject sequence</dt>
-      <dd><a class="match_details" href="">view matched sequence</a></dd>
-    <dt>Related pages</dt>
+    <dt>Hit region <span class="region_string"></span></dt>
       <dd>
-       <div class="xref_content"></div>
-     </dd>
+        <div style="margin: 0.5em 0"><a class="match_details" href="">View matched sequence</a></div>
+        <div class="hit_region_xrefs"></div>
+      </dd>
+    <dt>Subject sequence <span class="sequence_name"></span></dt>
+      <dd class="subject_sequence_xrefs">
+      </dd>
   </dl>
 </div>
 <script>
@@ -82,30 +81,38 @@ print <<EOJS;
   function resolve_blast_ident( id, id_region, match_detail_url, identifier_url ) {
     var popup = jQuery( "#xref_menu_popup" );
 
-    var popup_title = popup.children('.popup_title');
-    var identifier_link_area = popup.find('.identifier_link');
-
+    var sequence_name = popup.find('.sequence_name');
     if( identifier_url == null ) {
-       popup_title.html( 'Subject: ' + id );
-       identifier_link_area.html( '<span class="ghosted">not available</span>' );
+       sequence_name.html( id );
     } else {
-       popup_title.html( 'Subject: <a href="' + identifier_url + '">' + id + '</a>' );
-       identifier_link_area.html( '<a href="' + identifier_url + '">view ' + id + ' details</a>' );
+       sequence_name.html( '<a href="' + identifier_url + '">' + id + '</a>' );
     }
 
+    popup.find('.region_string').html( id_region );
+
     popup.find('a.match_details').attr( 'href', match_detail_url );
-    var content = popup.find('div.xref_content');
-    content.html( '<img src="/img/throbber.gif" /> searching for additional related pages ...' );
-    content.load( '/api/v1/feature_xrefs?q='+id+';q='+id_region );
+
+    // look up xrefs for overall subject sequence
+    var subj = popup.find('.subject_sequence_xrefs');
+    subj.html( '<img src="/img/throbber.gif" /> searching ...' );
+    subj.load( '/api/v1/feature_xrefs?q='+id );
+
+    // look up xrefs for the hit region
+    var region = popup.find('.hit_region_xrefs');
+    region.html( '<img src="/img/throbber.gif" /> searching ...' );
+    region.load( '/api/v1/feature_xrefs?q='+id_region );
+
     popup.dialog( 'open' );
     jQuery( "body .ui-widget-overlay").click( function() { popup.dialog( "close" ); } );
+
+    popup.find('a').blur();
 
     return false;
   }
 
   jQuery( "#xref_menu_popup" ).dialog({
           autoOpen: false,
-          height: 300,
+          height: 400,
           width: 680,
           modal: true
   });
