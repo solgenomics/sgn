ff37f833b5a2a79dde2b8aa6dd92d74d0bc8bf96
isaak <iyt2@cornell.edu>
2011-04-08 09:58:25 +0200

    redirects to qtl_analysis.pl. was lost due to merging confict of topic/qtl_effects

---
+++ cgi-bin/phenome/population_indls.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+=head1 DESCRIPTION
+redirects to qtl_analysis.pl
+
+=cut
+
+use strict;
+use CGI;
+
+my $cgi = CGI->new();
+my $pop_id  = $cgi->param('population_id');
+my $term_id = $cgi->param('cvterm_id');
+
+print $cgi->redirect("qtl_analysis.pl?population_id=$pop_id&cvterm_id=$term_id", 301);
