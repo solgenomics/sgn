d75d21fee6540e13a6787e44e98017b8fb260fba
Robert Buels <rmb32@cornell.edu>
2011-04-22 10:15:56 -0700

    increase size of gbrowse search text boxes on tomato genome data page

--- cgi-bin/genomes/Solanum_lycopersicum/genome_data.pl
+++ cgi-bin/genomes/Solanum_lycopersicum/genome_data.pl
@@ -111,7 +111,7 @@ sub gb_searchbox {
     my ($gb_root) = @_;
     return
         start_form( -style => 'display: inline', -action => $gb_root )
-       .textfield( -name => 'name', -value => 'search (e.g. SGN-M7588 or lycopene cyclase)', -onfocus => "this.value = ''", -size => 30)
+       .textfield( -name => 'name', -value => 'search (e.g. SGN-M7588 or lycopene cyclase)', -onfocus => "this.value = ''", -size => 50)
        .submit('Search','Search')
        .end_form()
 }
