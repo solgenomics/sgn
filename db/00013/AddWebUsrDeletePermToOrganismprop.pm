3b0da82d8b8b0235373ace60d55ba487b2796d1d
Robert Buels <rmb32@cornell.edu>
2011-05-13 08:48:55 -0700

    remove reams of inaccurate boilerplate from db patch

--- db/00013/AddWebUsrDeletePermToOrganismprop.pm
+++ db/00013/AddWebUsrDeletePermToOrganismprop.pm
@@ -1,54 +1,8 @@
 #!/usr/bin/env perl
-
-
-=head1 NAME
-
- SampleDbpatchMoose.pm
-
-=head1 SYNOPSIS
-
-mx-run ThisPackageName [options] -H hostname -D dbname -u username [-F]
-    
-this is a subclass of L<CXGN::Metadata::Dbpatch>
-see the perldoc of parent class for more details.
-    
-=head1 DESCRIPTION
-
-This is a test dummy patch. 
-This subclass uses L<Moose>. The parent class uses L<MooseX::Runnable>
-    
-=head1 AUTHOR
-
- Naama Menda<nm249@cornell.edu>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2010 Boyce Thompson Institute for Plant Research
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
-
-
 package AddWebUsrDeletePermToOrganismprop;
 
 use Moose;
 extends 'CXGN::Metadata::Dbpatch';
-
-
-sub init_patch {
-    my $self=shift;
-    my $name = __PACKAGE__;
-    print "dbpatch name is ':" .  $name . "\n\n";
-    my $description = 'Testing a Moose dbpatch';
-    my @previous_requested_patches = (); #ADD HERE 
-    
-    $self->name($name);
-    $self->description($description);
-    $self->prereq(\@previous_requested_patches);
-    
-}
 
 sub patch {
     my $self=shift;
