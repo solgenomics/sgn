c51d2ca168cb323da5aa4348a6f38320d0d89c97
isaak <iyt2@cornell.edu>
2011-06-20 16:26:02 +0200

    cleaning up dead code and more....

--- cgi-bin/phenome/organism_browser.pl
+++ cgi-bin/phenome/organism_browser.pl
@@ -1,4 +1,4 @@
-use CatalystX::GlobalContext qw( $c );
+
 #!/usr/bin/perl -wT
 
 
@@ -27,20 +27,14 @@ use CXGN::DB::Connection;
 use CXGN::Login;
 use CXGN::Phenome::Qtl;
 
+use CatalystX::GlobalContext qw( $c );
+
 my $doc = CXGN::Scrap::AjaxPage->new();
 $doc->send_http_header();
 
 
 my ($common_name, $common_name_id, $type, $sp_person_id) = $doc->get_encoded_arguments("organism", "organism_id", "type", "sp_person_id");
  
-print STDERR "common_name : $common_name\n";
-print STDERR "common_name_id : $common_name_id\n";
-print STDERR "type: $type\n";
-
-#$organism_id =~/(\d+)/;
-#$organism_id =$1;
-#$organism_id =~ s/\s//;
-
 my $dbh = CXGN::DB::Connection->new();
 my ($login_person_id, $login_user_type)=CXGN::Login->new($dbh)->verify_session();
 
@@ -57,31 +51,25 @@ if ($login_user_type eq 'curator' || $login_user_type eq 'submitter' || $login_u
 }         
  
 sub browse_organisms {
-    my $dbh = shift;
-    my $type = shift;
-    my $common_name = shift;
-    
-    print STDERR "common_name : $common_name";  
+    my ($dbh, $type, $common_name) = @_;   
     my $organisms;
     
-    if ($type eq 'browse') {
-
-	if ($common_name) { 
+    if ($type eq 'browse') 
+    {
+	if ($common_name) 
+        { 
 	    my $sth = $dbh->prepare ("SELECT common_name_id, common_name 
                                               FROM sgn.common_name                                                                                             WHERE common_name ILIKE '$common_name%'"
                                     );
 	    $sth->execute();
 
-	    while (my ($common_name_id, $common_name)	= $sth->fetchrow_array()) {		
-		$organisms .= "$common_name_id*$common_name_id -- $common_name |";
-		    
-	    }
-	
+	    while (my ($common_name_id, $common_name)	= $sth->fetchrow_array()) 
+            {		
+		$organisms .= "$common_name_id*$common_name_id -- $common_name |";		    
+	    }	
 	    print $organisms;
-	  }  
-	
-    }
-  
+	  }  	
+    }  
 }
 
 sub associate_organism {
