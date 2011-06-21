a2efcf8605e203010f48ff41459505aa72d3208e
isaak <iyt2@cornell.edu>
2011-06-21 17:28:00 +0200

    a server side script for creating a file for storing on whether to use default or user specificied statistical parameters

---
+++ cgi-bin/phenome/qtl_stat_options.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+use CXGN::Phenome::Qtl;
+use Path::Class;
+use CatalystX::GlobalContext qw( $c );
+
+my $login_id = $c->user()->get_object->get_sp_person_id() if $c->user;
+my $pop_id = $c->request->param('pop_id');
+
+if ($login_id) {
+
+    my $qtl = CXGN::Phenome::Qtl->new($login_id);
+    my ($temp_qtl_dir, $temp_user_dir) = $qtl->create_user_qtl_dir($c); 
+                     
+    my $f = file( $temp_user_dir, "stat_options_${pop_id}.txt" )->openw
+        or die "Can't create file: $! \n";
+    $f->print( "default parameters\tYes" );
+  
+}                
