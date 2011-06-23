f02c32acca340e92ced746e00f5fb88d68fcf6f6
isaak <iyt2@cornell.edu>
2011-06-23 15:47:18 +0200

    stores the current chosen option (default or user parameters) for the QTL analysis

--- cgi-bin/phenome/qtl_stat_options.pl
+++ cgi-bin/phenome/qtl_stat_options.pl
@@ -7,6 +7,7 @@ use CatalystX::GlobalContext qw( $c );
 
 my $login_id = $c->user()->get_object->get_sp_person_id() if $c->user;
 my $pop_id = $c->request->param('pop_id');
+my $stat_params = $c->request->param('stat_params');
 
 if ($login_id) {
 
@@ -15,6 +16,14 @@ if ($login_id) {
                      
     my $f = file( $temp_user_dir, "stat_options_${pop_id}.txt" )->openw
         or die "Can't create file: $! \n";
-    $f->print( "default parameters\tYes" );
+
+    if ( $stat_params eq 'default' ) 
+    {
+        $f->print( "default parameters\tYes" );
+    } 
+    else 
+    {
+        $f->print( "default parameters\tNo" );
+    }
   
 }                
