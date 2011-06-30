ca7f861fadda0174f914a8959cc6439039b08624
mueller <lam87@cornell.edu>
2011-06-30 14:47:30 -0400

    Adding a page about AFRI-SOL, and adding a Controller for dispatching to static pages.

---
+++ lib/SGN/Controller/Static.pm
@@ -0,0 +1,32 @@
+
+
+=head1 NAME
+
+SGN::Controller::Static - a controller for dispatching to static pages
+
+=head1 DESCRIPTION
+
+SGN has some static pages that don't merit their own controller. The purpose of this one is to have one common controller for all the static pages that don't fit anywhere else.
+
+Please feel free to add your own pages. The actions should essentially just link to a mason component for display.
+
+=head1 AUTHOR
+
+Lukas Mueller <lam87@cornell.edu>
+
+=cut
+
+
+package SGN::Controller::Static;
+
+use Moose;
+
+use base 'Catalyst::Controller';
+
+
+sub solanaceae_project_afri :Path('/solanaceae-project/afri-sol/') { 
+    my ($self, $c) = @_;
+    $c->stash->{template} = '/links/afri_sol.mas';
+}
+
+1;
