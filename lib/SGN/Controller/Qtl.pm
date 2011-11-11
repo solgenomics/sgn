c97f2efcc7a23793accca269b62d0c5f44873888
isaak <iyt2@cornell.edu>
2011-11-11 11:06:58 +0100

    added new action item that maps to a new url for qtl traits list

--- lib/SGN/Controller/Qtl.pm
+++ lib/SGN/Controller/Qtl.pm
@@ -9,6 +9,7 @@ package SGN::Controller::Qtl;
 use Moose;
 use namespace::autoclean;
 use File::Spec::Functions;
+use List::MoreUtils qw /uniq/;
 use File::Temp qw / tempfile /;
 use File::Path qw / mkpath  /;
 use File::Copy;
@@ -644,6 +645,61 @@ sub mark_qtl_traits {
 }
 
 
+sub qtl_traits : PathPart('qtl/traits') Chained Args(1) {
+    my ($self, $c, $index) = @_;
+    
+    my $qtl_tools = CXGN::Phenome::Qtl::Tools->new();
+    my ( $all_traits, $all_trait_d ) = $qtl_tools->all_traits_with_qtl_data();
+  
+    my  @all_traits = sort { $a <=> $b } uniq (@{$all_traits});
+
+    my @index_traits;
+    
+    foreach my $trait (@all_traits)
+    {
+        if ( $trait =~ /^$index/i )
+        {
+            push @index_traits, $trait;
+        }
+    }
+
+    my @traits_list;
+    if (@index_traits)
+    {
+        foreach my $trait (@index_traits)
+        {
+            my $cvterm = CXGN::Chado::Cvterm::get_cvterm_by_name( $c->dbc->dbh, $trait );
+            my $cvterm_id = $cvterm->get_cvterm_id();
+            if ($cvterm_id)
+            {
+                push @traits_list,
+                [
+                 map { $_ } 
+                 (
+                  qq |<a href=/chado/cvterm.pl?cvterm_id=$cvterm_id>$trait</a> |
+                 )
+                ];
+            }
+            else
+            {
+                my $t = CXGN::Phenome::UserTrait->new_with_name( $c->dbc->dbh, $trait );
+                my $trait_id = $t->get_user_trait_id();
+                push @traits_list,
+                [
+                 map { $_ } 
+                 (
+                  qq |<a href=/phenome/trait.pl?trait_id=$trait_id>$trait</a> |
+                 )
+                ];
+            }
+        }
+    }
+
+    $c->stash( template => '/qtl/traits/index.mas',
+               index    => $index,
+               traits_list => \@traits_list
+             );
+}
 
 
 
