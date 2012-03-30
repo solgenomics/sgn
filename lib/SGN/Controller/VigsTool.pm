7394ee6686724fe43b6706ecc588e88c642388a3
Lukas Mueller <lam87@cornell.edu>
2012-03-30 10:12:10 -0400

    initial simple version of vigs tool.

--- lib/SGN/Controller/VigsTool.pm
+++ lib/SGN/Controller/VigsTool.pm
@@ -7,6 +7,7 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 use File::Basename;
 use File::Slurp;
+use File::Spec;
 use File::Temp qw | tempfile |; 
 use POSIX;
 use Storable qw | nstore |;
@@ -27,12 +28,7 @@ sub input :Path('/tools/vigs/') :Args(0) {
     my $dbh = CXGN::DB::Connection->new;
     our $prefs = CXGN::Page::UserPrefs->new( $dbh );
     
-    print STDERR "Generating the blast select list\n";
-
     my ($databases,$programs,$programs_js) = blast_db_prog_selects($c,$prefs);
-
-
-    print STDERR "stashing stuff...\n";
 
     $c->stash->{template} = '/tools/vigs/input.mas';
 
@@ -45,66 +41,63 @@ sub input :Path('/tools/vigs/') :Args(0) {
 #
 sub blast_db_prog_selects {
     my ($c,$prefs) = @_;
-
-  sub opt {
-    my $db = shift;
-    print STDERR "processing $db...\n";
-    my $timestamp = $db->file_modtime
-      or return '';
-    $timestamp = strftime(' &nbsp;(%m-%d-%y)',gmtime $db->file_modtime);
-    my $seq_count = $db->sequences_count;
-
-    [$db->blast_db_id, $db->title.$timestamp]
-  }
-
-  my @db_choices = map {
-      
-      print STDERR $c->get_conf('blast_db_path')." ". $_->name ."= ". join (",", $_->blast_dbs( web_interface_visible=>'t'))."\n\n";
-
-      my @web_visible = $_->blast_dbs( web_interface_visible=>'t');
-      my @dbs = map [$_,opt($_)], grep $_->file_modtime, $_->blast_dbs( web_interface_visible => 't');
-      
-      @dbs || print STDERR "No databases available...". $_->name."\n";
-      @dbs ? ('__'.$_->name, @dbs) : ()
-  } CXGN::BlastDB::Group->search_like(name => '%',{order_by => 'ordinal, name'});
-
-  my @ungrouped_dbs = grep $_->file_modtime,CXGN::BlastDB->search( blast_db_group_id => undef, web_interface_visible => 't', {order_by => 'title'} );
-  if(@ungrouped_dbs) {
-    push @db_choices, '__Other',  map [$_,opt($_)], @ungrouped_dbs;
-  }
-
-  @db_choices or return '<span class="ghosted">The BLAST service is temporarily unavailable, we apologize for the inconvenience</span>';
-
-  my $selected_db_file_base = $prefs->get_pref('last_blast_db_file_base');
-  #warn "got pref last_blast_db_file_base '$selected_db_file_base'\n";
-
-  my %prog_descs = ( blastn  => 'BLASTN (nucleotide to nucleotide)',
-		     blastx  => 'BLASTX (nucleotide to protein; query translated to protein)',
-		     blastp  => 'BLASTP (protein to protein)',
-		     tblastx => 'TBLASTX (protein to protein; both database and query are translated)',
-		     tblastn => 'TBLASTN (protein to nucleotide; database translated to protein)',
-		   );
-
-  my @program_choices = map {
-    my ($db) = @$_;
-    if($db->type eq 'protein') {
-      [map [$_,$prog_descs{$_}], 'blastx','blastp']
-    } else {
-      [map [$_,$prog_descs{$_}], 'blastn','tblastx','tblastn']
+    
+    sub opt {
+	my $db = shift;
+	
+	my $timestamp = $db->file_modtime
+	    or return '';
+	$timestamp = strftime(' &nbsp;(%m-%d-%y)',gmtime $db->file_modtime);
+	my $seq_count = $db->sequences_count;
+	
+	[$db->blast_db_id, $db->title.$timestamp]
     }
-  } grep ref, @db_choices;
-
-  @db_choices = map {ref($_) ? $_->[1] : $_} @db_choices;
-
-  return hierarchical_selectboxes_html( parentsel => { name => 'database',
-						       choices =>
-						       \@db_choices,
-						       $selected_db_file_base ? (selected => $selected_db_file_base) : (),
-						     },
-					childsel  => { name => 'program',
-						     },
-					childchoices => \@program_choices
-				      );
+    
+    my @db_choices = map {
+	my @web_visible = $_->blast_dbs( web_interface_visible=>'t');
+	my @dbs = map [$_,opt($_)], grep $_->file_modtime, $_->blast_dbs( web_interface_visible => 't');
+	
+	@dbs || print STDERR "No databases available...". $_->name."\n";
+	@dbs ? ('__'.$_->name, @dbs) : ()
+    } CXGN::BlastDB::Group->search_like(name => '%',{order_by => 'ordinal, name'});
+    
+    my @ungrouped_dbs = grep $_->file_modtime,CXGN::BlastDB->search( blast_db_group_id => undef, web_interface_visible => 't', {order_by => 'title'} );
+    if(@ungrouped_dbs) {
+	push @db_choices, '__Other',  map [$_,opt($_)], @ungrouped_dbs;
+    }
+    
+    @db_choices or return '<span class="ghosted">The BLAST service is temporarily unavailable, we apologize for the inconvenience</span>';
+    
+    my $selected_db_file_base = $prefs->get_pref('last_blast_db_file_base');
+    #warn "got pref last_blast_db_file_base '$selected_db_file_base'\n";
+    
+    my %prog_descs = ( blastn  => 'BLASTN (nucleotide to nucleotide)',
+		       blastx  => 'BLASTX (nucleotide to protein; query translated to protein)',
+		       blastp  => 'BLASTP (protein to protein)',
+		       tblastx => 'TBLASTX (protein to protein; both database and query are translated)',
+		       tblastn => 'TBLASTN (protein to nucleotide; database translated to protein)',
+	);
+    
+    my @program_choices = map {
+	my ($db) = @$_;
+	if($db->type eq 'protein') {
+	    [map [$_,$prog_descs{$_}], 'blastx','blastp']
+	} else {
+	    [map [$_,$prog_descs{$_}], 'blastn','tblastx','tblastn']
+    }
+    } grep ref, @db_choices;
+    
+    @db_choices = map {ref($_) ? $_->[1] : $_} @db_choices;
+    
+    return hierarchical_selectboxes_html( parentsel => { name => 'database',
+							 choices =>
+							     \@db_choices,
+							     $selected_db_file_base ? (selected => $selected_db_file_base) : (),
+					  },
+					  childsel  => { name => 'program',
+					  },
+					  childchoices => \@program_choices
+	);
 }
 
 
@@ -118,11 +111,6 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 
 
     my $params = $c->req->body_params();
-
-#     print STDERR "PARAMETERS: \n";
-#     foreach my $k (keys(%$params)) { 
-# 	print STDERR "$k: $params->{$k}\n";
-#     }
 
     # processing the primers 
     #
@@ -147,61 +135,22 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 	user_error(join("<br />" , @errors));
     }
     
+    my ($seq_fh, $seq_filename) = tempfile( "seqXXXXXX",
+					    DIR=> $c->config->{'cluster_shared_tempdir'},
+	);
+    
+    my $seq = Bio::Seq->new(-seq=>$sequence, -id=>"temp");
+    my $io = Bio::SeqIO->new(-format=>'fasta', -file=>">".$seq_filename);
+    
+    $io->write_seq($seq);
+    $io->close();
+
     my %arg_handlers =
 	(
 	 
 	 sequence=>
-	 sub {
-
-	     # generate a file with fragments of 'sequence' of length 'fragment_size'
-	     #
-	     my ($seq_fh, $seq_filename) = tempfile( "seqXXXXXX",
-						     DIR=> $c->config->{'cluster_shared_tempdir'},
-		 );
-	     
-	     
-	     my $seq = Bio::Seq->new(-seq=>$sequence, -id=>"temp");
-	     my $io = Bio::SeqIO->new(-format=>'fasta', -file=>">".$seq_filename);
-	     
-	     #foreach my $i (1..$seq->length()-$fragment_size) { 
-		 
-	#	 my $subseq = $seq->subseq($i, $i + $fragment_size -1);
-	#	 my $subseq_obj = Bio::Seq->new(-seq=>$subseq, -display_id=>"temp-$i");
-	#	 $io->write_seq($subseq_obj);
-		 
-	#     }
-	#     $io->close();
-	     $io->write_seq($seq);
-	     $io->close();
-
+	 sub {	
 	     return "-i" => $seq_filename;
-	     
-# 	     my $sequence = $blast_seq_in;
-# 	     #$sequence =s/^\s+|\s+$|\n\s*\n//g;
-	     
-# 	     #make a tempfile that has our sequence(s) in it
-# 	     my ($seq_fh, $seq_filename) = tempfile( "seqXXXXXX",
-# 						     DIR=> $c->config->{'cluster_shared_tempdir'},
-# 		 );
-# 	     print $seq_fh $sequence;
-	     
-	     
-	     
-	     
-# 	     if(my $file_upload = $page->get_upload) {
-# 		 if ( my $fh = $file_upload->fh ) {
-# 		     print $seq_fh $_ while <$fh>;
-# 		 }
-# 	     }
-	     
-# 	     seek $seq_fh,0,0; #< rewind the filehandle
-# 	     #count the number of seqs in the file
-# 	     while(<$seq_fh>) {
-# 		 $seq_count++ if index($_,'>') != -1;
-# 	     }
-	     
-	     
-# 	     return -i => $seq_filename
 	 },
 
 	 matrix =>
@@ -236,7 +185,7 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 	     
 	     #     warn "setting pref last_blast_db_fil
 	     #database object for specific ID_No
-	     
+	     print STDERR "DATABASE SELECTED: $params->{database}\n";
 	     my $bdb = CXGN::BlastDB->from_id($params->{database});
 	     print STDERR "\n\n\n".$bdb->dbpath()."\n\n\n";
 	     my $basename = $bdb->full_file_basename;
@@ -284,9 +233,9 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 					    }
 	);
 #$job->do_not_cleanup(1);
-    
     my $job_file_tempdir = $c->path_to( $c->tempfiles_subdir('blast') );
     my (undef,$job_file) = tempfile( DIR => $job_file_tempdir, TEMPLATE=>"object_XXXXXX");
+    
     
     nstore($job, $job_file)
 	or die 'could not serialize job object';
@@ -297,7 +246,7 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 #delete $params{sequence};
     
     my $pass_back = "/tools/vigs/view?".
-	hash2param(%$params, seq_count => $seq_count, 
+	hash2param(%$params, seq_count => $seq_count, query_file => $urlencode{$seq_filename},
 		   "report_file");
 #$pass_back =~ s/&/&amp;/g;
     
@@ -306,8 +255,6 @@ sub calculate :Path('/tools/vigs/result') :Args(0) {
 #warn "redirecting to '$redir_url'";
     
     $c->response->redirect($redir_url);  # should be fully qualified URL -- fix.
-    
-    
 }
 
 
@@ -333,13 +280,23 @@ sub hash2param {
 sub view :Path('/tools/vigs/view') :Args(0) { 
     my ($self, $c) = @_;
 
-    
+    my $file = $c->request->param('report_file');
+    my $fragment_size = $c->request->param('fragment_size');
+    my $query_file = $c->request->param('query_file');
 
+    $c->stash->{query_file} = $query_file;
+
+    $query_file =~ s/\%2F/\//g;
+
+    my $query_io = Bio::SeqIO->new(-format=>'fasta', -file=>$query_file);
+
+    my %seqs = ();
+    while (my $s = $query_io->next_seq()) { 
+	$seqs{$s->id()} = $s->seq();
+    }
 
     $c->stash->{template} = '/tools/vigs/view.mas';
     my $job_file_tempdir = $c->path_to( $c->tempfiles_subdir('blast') );
-    my $file = $c->request->param('report_file');
-    my $fragment_size = $c->request->param('fragment_size');
     
     open(my $F, "<", $job_file_tempdir."/".$file) || die "Can't open file $file.";
     my %matches;
@@ -350,53 +307,57 @@ sub view :Path('/tools/vigs/view') :Args(0) {
 #####################
 
     #graph variables for just Evan's graph package
-    my $graph_img_filename = $c->tempname() . ".png";
+    my $graph_img_filename = basename($c->tempfile(TEMPLATE=>'imgXXXXXX', UNLINK=>0) . ".png");
+    my $graph_img_path = File::Spec->catfile($c->config->{basepath}, $c->tempfiles_subdir('vigs'), $graph_img_filename);
+    my $graph_img_url  = File::Spec->catfile($c->tempfiles_subdir('vigs'), $graph_img_filename);
 
-    my $tempfiles_subdir = $c->{config}->{tempfiles_subdir};
-
-    my $graph2 = CXGN::Graphics::BlastGraph->new( blast_outfile => $file,
-                                                graph_outfile => $tempfiles_subdir."/$graph_img_filename",
-                                              );
-
-    return 'graphical display not available BLAST reports larger than 1 MB' if -s $file > 1_000_000;
+    my $raw_blast_path = File::Spec->catfile($job_file_tempdir, $file);
+    my $raw_blast_url  = File::Spec->catfile($c->tempfiles_subdir('blast'), $file);
     
+    print STDERR "TEMP FILE PATH FOR PNG = $graph_img_path\n";
+    print STDERR "RAW BLAST PATH = $raw_blast_path\n";
+
+    my $graph2 = CXGN::Graphics::BlastGraph->new( 
+	blast_outfile => $raw_blast_path,
+	graph_outfile => $graph_img_path,
+    );
+	
+    my @single_coverage_regions = $graph2->get_regions(1);
+    my @double_coverage_regions = $graph2->get_regions(2);
+
+    $c->stash->{error} = 'graphical display not available BLAST reports larger than 1 MB' if -s $raw_blast_path > 1_000_000;
+	
     my $errstr = $graph2->write_img();
     $errstr and die "<b>ERROR:</b> $errstr";
     
-    
-    return join '',
-    ( <<EOH,
-      <center><b>Conservedness Histogram</b></center>
-      <p>The histogram shows a count of hits <i>for each base</i> in the query sequence,
-but counts <i>only the domains BLAST finds</i>, meaning this is really more a function of region than of individual base.
-      Within the graph, green shows exact base matches within conserved regions; blue shows non-matching bases within conserved regions. Gaps introduced into the query by BLAST are ignored; gaps introduced into target sequences are not.</p>
+    $c->stash->{single_coverage_regions} = \@single_coverage_regions;
+    $c->stash->{double_coverage_regions} = \@double_coverage_regions;
 
-EOH
+    $c->stash->{coverage_graph_url} = $graph_img_url;
+    $c->stash->{click_map_html} = $graph2->get_map_html(), #code for map element (should have the name used below in the image)
+  
+    $c->stash->{raw_blast_url} = $raw_blast_url;
 
-      $graph2->get_map_html(), #code for map element (should have the name used below in the image)
-      qq|<div align="center" style="color: #777777">|,
-      qq|<img src="$tempfiles_subdir/$graph_img_filename" border="2" usemap="#graph2map" alt="" />|,
-      qq|</div>\n\n|,
-    );
+    open(my $F, "<", $raw_blast_path) || die "Can't open the raw blast output file ($raw_blast_path)\n";
 
+    my $old_query = "";
+    while (<$F>) { 
+ 	chomp;
+ 	my ($query, $subject, $percent, $length, $mismatches, $gaps) = split/\t/;
+ 	if (($percent == 100) && ($length >= $fragment_size)) { 
+ 	    $matches{$query}++;
+ 	}
+ 	if ($query ne $old_query) { push @queries, $query; }
+ 	$old_query = $query;
+     }
+     my @blast_results = ();
+     for (my $i=0; $i<@queries; $i++) { 
+ 	push @blast_results, [$i, $matches{$queries[$i]} ];
+     }
 
+    $c->stash->{blast_matches} = \%matches;
+    $c->stash->{fragment_size} = $fragment_size;
 
-
-
-#     my $old_query = "";
-#     while (<$F>) { 
-# 	chomp;
-# 	my ($query, $subject, $percent, $length, $mismatches, $gaps) = split/\t/;
-# 	if (($percent == 100) && ($length == $fragment_size)) { 
-# 	    $matches{$query}++;
-# 	}
-# 	if ($query ne $old_query) { push @queries, $query; }
-# 	$old_query = $query;
-#     }
-#     my @blast_results = ();
-#     for (my $i=0; $i<@queries; $i++) { 
-# 	push @blast_results, [$i, $matches{$queries[$i]} ];
-#     }
 
     ## Create rbase:
 
