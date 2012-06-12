c0bcf1b8da0033f58ae17f8ec743b3a0562739ff
Lukas Mueller <lam87@cornell.edu>
2012-06-12 18:02:07 -0400

    add an experimental barcode controller.

--- lib/SGN/Controller/Barcode.pm
+++ lib/SGN/Controller/Barcode.pm
@@ -6,6 +6,9 @@ use Moose;
 use GD;
 use Barcode::Code128;
 use GD::Barcode::QRcode;
+use Tie::UrlEncoder;
+
+our %urlencode;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -27,12 +30,40 @@ sub code128_png :Path('/barcode/code128png') :Args(2) {
     my  $barcode = $barcode_object ->gd_image();
     my $text_width = gdLargeFont->width()*length($text);
     $barcode->string(gdLargeFont,int(($barcode->width()-$text_width)/2),10,$text, $barcode->colorAllocate(0, 0, 0));
-    
     $c->res->headers->content_type('image/png');
     
     $c->res->body($barcode->png());    
 }
 
+# a barcode element for a continuous barcode
+
+sub barcode_element : Path('/barcode/element/') :Args(2) { 
+    my $self = shift;
+    my $c = shift;
+    my $text = shift;
+    my $height = shift;
+
+    my $barcode_object = Barcode::Code128->new();
+    $barcode_object->barcode($text);
+    $barcode_object->height(100);
+    #$barcode_object->width(200);
+    $barcode_object->font('large');
+    $barcode_object->border(0);
+    $barcode_object->top_margin(0);
+    my  $barcode = $barcode_object ->gd_image();
+
+    my $barcode_slice = GD::Image->new($barcode->width, $height);
+    my $white = $barcode_slice->colorAllocate(255,255,255);
+    $barcode_slice->filledRectangle(0, 0, $barcode->width, $height, $white);
+
+    print STDERR "Creating barcode with width ".($barcode->width)." and height $height\n";
+    $barcode_slice->copy($barcode, 0, 0, 0, 0, $barcode->width, $height);
+
+    $c->res->headers->content_type('image/png');
+    
+    $c->res->body($barcode_slice->png());    
+
+}
 
 sub qrcode_png :Path('/barcode/qrcodepng') :Args(2) { 
     my $self = shift;
@@ -50,5 +81,84 @@ sub qrcode_png :Path('/barcode/qrcodepng') :Args(2) {
     $c->res->body($image->png());			      
 }
 
+sub barcode_tool :Path('/barcode/tool') Args(2) { 
+    my $self = shift;
+    my $c = shift;
+    my $cvterm = shift;
+    my $values = shift;
+
+    my ($db, $accession) = split /\:/, $cvterm;
+
+    print STDERR "Searching $cvterm, DB $db...\n";
+    my ($db_row) = $c->dbic_schema('Bio::Chado::Schema')->resultset('General::Db')->search( { name => $db } );
+
+    print STDERR $db_row->db_id;
+    print STDERR "DB_ID for $db: $\n";
+
+
+    my $dbxref_rs = $c->dbic_schema('Bio::Chado::Schema')->resultset('General::Dbxref')->search_rs( { accession=>$accession, db_id=>$db_row->db_id } );
+    
+    my $cvterm_rs = $c->dbic_schema('Bio::Chado::Schema')->resultset('Cv::Cvterm')->search( { dbxref_id => $dbxref_rs->first->dbxref_id });
+
+    my $cvterm_id = $cvterm_rs->first()->cvterm_id();
+   my $cvterm_synonym_rs = ""; #$c->dbic_schema('Bio::Chado::Schema')->resultset('Cv::Cvtermsynonym')->search->( { cvterm_id=>$cvterm_id });
+
+    $c->stash->{cvterm} = $cvterm;
+    $c->stash->{cvterm_name} = $cvterm_rs->first()->name();
+    $c->stash->{cvterm_definition} = $cvterm_rs->first()->definition();
+    $c->stash->{cvterm_values} = $values;
+    $c->stash->{tool_version} = "1.0";
+    $c->stash->{template} = '/barcode/tool.mas';
+#    $c->stash->{cvterm_synonym} = $cvterm_synonym_rs->synonym();
+    $c->forward('View::Mason');
+    
+
+}
+
+sub continuous_scale : Path('/barcode/continuous_scale') Args(0) { 
+    my $self = shift;
+    my $c = shift;
+    my $start = $c->req->param("start");
+    my $end = $c->req->param("end");
+    my $step = $c->req->param("step");
+    my $height = $c->req->param("height");
+
+    my @barcodes = ();
+
+    # barcodes all have to have the same with - use with of end value
+    my $text_width = length($end); 
+
+    for(my $i = $start; $i <= $end; $i += $step) { 
+	my $text = $urlencode{sprintf "%".$text_width."d", $i};
+	print STDERR "TEXT: $text\n";
+	push @barcodes, qq { <img src="/barcode/element/$i/$height" align="right" /> };
+
+    }
+
+    $c->res->body("<table cellpadding=\"0\" cellspacing=\"0\">". (join "\n", (map { "<tr><td>$_</td></tr>"}  @barcodes)). "</table>");
+
+    
+}
+
+sub continuous_scale_form : Path('/barcode/continuous_scale/input') :Args(0) { 
+    my $self = shift;
+    my $c = shift;
+    
+    my $form = <<HTML;
+<h1>Create continuous barcode</h1>
+
+<form action="/barcode/continuous_scale">
+Start value <input name="start" /><br />
+End value <input name="end" /><br />
+Step size <input name="step" /><br />
+Increment (pixels) <input name="height" />
+
+<input type="submit"  />
+</form>
+
+HTML
+
+$c->res->body($form);
+}
 
 1;
