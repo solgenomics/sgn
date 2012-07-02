c449927e151784bce3921e5e14a775ab90e39dd7
Lukas Mueller <lam87@cornell.edu>
2012-07-02 09:40:15 -0400

    experimental breeders toolbox functionality.

--- lib/SGN/Controller/BreedersToolbox.pm
+++ lib/SGN/Controller/BreedersToolbox.pm
@@ -7,12 +7,73 @@ use URI::FromHash 'uri';
 BEGIN { extends 'Catalyst::Controller'; }
 
 
-sub make_cross :Path("/stock/cross/new") :Args(0) { 
-
+sub make_cross_form :Path("/stock/cross/new") :Args(0) { 
     my ($self, $c) = @_;
-
     $c->stash->{template} = '/stock/cross.mas';
 }
+
+sub make_cross :Path("/stock/cross/generate") :Args(0) { 
+    my ($self, $c) = @_;
+    my $trial_id = $c->req->param('trial_id');
+    my $location = $c->req->param('location_id');
+    my $maternal = $c->req->param('maternal');
+    my $paternal = $c->req->param('paternal');
+    my $private_to_role = $c->req->param('private_to_role');
+    my $progeny_count = $c->req->param('progeny');
+    
+    # get ID for $maternal
+    my $schema = $c->dbic_schema('Bio::Chado::Schema');
+    
+    eval { 
+	my $maternal_id = $schema->resultset('Stock::Stock')->search( { stock_name => $maternal })->first->stock_id();
+	
+	my $paternal_id = $schema->resultset('Stock::Stock')->search( { stock_name => $paternal })->first->stock_id();
+    };
+    if ($@) { 
+    }
+
+    
+    
+
+
+}
+
+sub insert_new_location : Path("/breeders/location/insert") : Args(0) { 
+    my ($self, $c) = @_;
+    
+    my $description => $c->req->param("description");
+    my $longitude => $c->req->param("longitude");
+    my $latitude  => $c->req->param("latitude");
+
+
+    if (! $c->user()) { # redirect
+	$c->res->redirect( uri( path => '/solpeople/login.pl', query => { goto_url => $c->req->uri->path_query } ) );
+	return;
+    }
+
+    my $schema = $c->dbic_schema('Bio::Chado::Schema');
+
+    my $exists = $schema->resultset('NaturalDiversity::NdGeolocation')->search( { description => $description } )->count();
+
+    if ($exists > 0) { 
+	$c->res->body("The location \'$description\' already exists!");
+	return;
+    }
+
+    my $new_row = $schema->resultset('NaturalDiversity::NdGeolocation')->new( { 
+	description => $description,
+	longitude => $longitude,
+	latitude  => $latitude,
+							      });
+
+    $new_row->insert();
+
+    $c->res->body("Everything OK\n");
+
+   #$c->res->redirect( uri( path => '/breeders/home', query => { goto_url => $c->req->uri->path_query } ) );
+    
+}
+    
      
 sub breeder_home :Path("/breeders/home") Args(0) { 
     my ($self , $c) = @_;
@@ -74,6 +135,7 @@ sub breeder_home :Path("/breeders/home") Args(0) {
     else {
 	# redirect to login page
 	$c->res->redirect( uri( path => '/solpeople/login.pl', query => { goto_url => $c->req->uri->path_query } ) );
+      
 
     }
 }
