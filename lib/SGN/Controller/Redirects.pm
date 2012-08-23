34a2196ae0c4c0840ee054fe8e0c9f7909416998
isaak <iyt2@cornell.edu>
2012-08-23 11:06:38 +0200

    redirecting variations of the urls to the est search page, in response to bug reports..

--- lib/SGN/Controller/Redirects.pm
+++ lib/SGN/Controller/Redirects.pm
@@ -28,6 +28,10 @@ my %paths = (
         /search/template_experiment_platform   /search/expression/template
         /search/experiment                     /search/expression/experiment
         /search/est                            /search/transcripts/est
+        /search/est                            /search/transcripts/est
+        /search/ests                            /search/transcripts/est
+        /search/EST                            /search/transcripts/est
+        /search/ESTs                            /search/transcripts/est
         /search/est_library                    /search/transcripts/est_library
         /search/library                        /search/transcripts/est_library
         /search/bacs                           /search/genomic/clones
