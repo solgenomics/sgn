e63d5607964e58dcd5d479f2e8ec2ca666b6e058
nmenda <nm249@cornell.edu>
2012-11-15 14:02:14 -0500

    making a synchronous request

--- js/CXGN/AJAX/Ontology.js
+++ js/CXGN/AJAX/Ontology.js
@@ -23,9 +23,10 @@ var Ontology = {
         var evidence_with = jQuery('#evidence_with_select').val();
         var reference = jQuery('#reference_select').val();
         jQuery.ajax({
-                type: 'POST',
+		url: cvterm_add_uri ,
+		    type: 'POST',
                     dataType: "json",
-                    url: cvterm_add_uri ,
+                    async: false,
                     data: 'term_name='+jQuery('#term_name').val()+'&object_id='+object_id+'&relationship='+relationship+'&evidence_code='+evidence_code+'&evidence_description='+evidence_description+'&evidence_with='+evidence_with+'&reference='+reference ,
                     success: function(response) {
                     var error = response.error;
