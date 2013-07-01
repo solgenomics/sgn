17b33c810f638d6b340dfb2507f62e80254ead3e
Lukas Mueller <lam87@cornell.edu>
2013-07-01 00:12:08 -0400

    convert tomato genome ids to fasta.

--- lib/CXGN/Blast/SeqQuery/Plugin/TomatoGenomeIds.pm
+++ lib/CXGN/Blast/SeqQuery/Plugin/TomatoGenomeIds.pm
@@ -9,12 +9,15 @@ sub name {
 
 sub validate { 
     my $self = shift;
+    my $c = shift;
     my $input = shift;
+
+
     my @ids = split /\s+/, $input; 
     
     my @errors = ();
     foreach my $id (@ids){
-	if($id !~ m/solyc\d{1,2}g\d{6}/){
+	if($id !~ m/solyc\d{1,2}g\d{6}/i){
 	    push @errors, $id;
 	}
     }
@@ -29,7 +32,29 @@ sub validate {
 
 sub process { 
     my $self = shift;
+    my $c = shift;
     my $input = shift;
+    
+    my @ids = split /\s+/, $input; 
+
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+    my $rna_id = $schema->resultset("Cv::Cvterm")->find( { name=>'mRNA' })->cvterm_id();
+    print STDERR "RNA: $rna_id\n";
+    my @seqs = ();
+    foreach my $id (@ids) { 
+	my $rs = $schema->resultset("Sequence::Feature")->search( { type_id=>$rna_id, name => "$id" } );
+	if (my $row = $rs->next()) { 
+	    
+	    push @seqs, ">".$row->name."\n".$->residues();
+	}
+	else { 
+	    	    die "ID $id does not exist!";
+	}
+    }
+    my $sequence =  join "\n", @seqs;
+    print STDERR "SEQUENCE = $sequence\n";
+
+    return $sequence;
     
     
 }
