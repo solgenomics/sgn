1af3d35a3afb21afaf178a5ebcf0a6c8748e36d4
Lukas Mueller <lam87@cornell.edu>
2013-07-01 20:04:30 -0400

    also populate the new linking table with the legacy data from the blast_db table.

--- db/00027/AddBlastDbBlastDbGroupLinkingTable.pm
+++ db/00027/AddBlastDbBlastDbGroupLinkingTable.pm
@@ -64,6 +64,9 @@ create table blast_db_blast_db_group (
     blast_db_group_id bigint REFERENCES blast_db_group
 );
 
+-- populate table with current data from blast_db table
+insert into sgn.blast_db_blast_db_group (blast_db_id, blast_db_group_id) SELECT blast_db_id, blast_db_group_id FROM sgn.blast_db;
+
 EOSQL
 
 print "You're done!\n";
