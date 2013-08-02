80eb86ae778f011b4d5b11a70b9be6f737232ee2
Lukas Mueller <lam87@cornell.edu>
2013-08-02 17:38:25 -0400

    fix sequence parsing.

--- lib/SGN/Controller/AJAX/Blast.pm
+++ lib/SGN/Controller/AJAX/Blast.pm
@@ -29,18 +29,6 @@ __PACKAGE__->config(
     map       => { 'application/json' => 'JSON', 'text/html' => 'JSON' },
    );
 
-# sub init_run : Path('/tools/blast/init') Args(0) { 
-#     my $self = shift;
-#     my $c = shift;
-
-#     my ($seq_fh, $seqfile) = tempfile( "blastquery_XXXXXX",
-# 					    DIR=> $c->get_conf('cluster_shared_tempdir'),
-# 	);
-
-#     my $jobid = basename($seqfile);
-#     $c->stash->{rest} = { jobid =>  basename($jobid), };
-# }
-
 sub run : Path('/tools/blast/run') Args(0) { 
     my $self = shift;
     my $c = shift;
@@ -58,6 +46,8 @@ sub run : Path('/tools/blast/run') Args(0) {
     
     $params->{sequence} = $input_query->process($c, $params->{input_options}, $params->{sequence});
 
+    print STDERR "SEQUENCE now : ".$params->{sequence}."\n";
+
     my ($seq_fh, $seqfile) = tempfile( 
 	"blast_XXXXXX",
 	DIR=> $c->get_conf('cluster_shared_tempdir'),
@@ -65,73 +55,77 @@ sub run : Path('/tools/blast/run') Args(0) {
     
     my $jobid = basename($seqfile);
 
+    print STDERR "JOB ID CREATED: $jobid\n";
+
     my $seq_count;
 
-        my $schema = $c->dbic_schema("SGN::Schema");
-
+    my $schema = $c->dbic_schema("SGN::Schema");
+    
     my %arg_handlers =
 	(
-##	 interface_type =>
-#	 sub { ""}, #< does nothing to blast command
-	 
+
 	 sequence =>
 	 sub {
 	     my $sequence = $params->{sequence};
-	     if( $sequence ) {
-		 $sequence =~ s/^\s+|\s+$|\n\s*\n//g; #< trim out leading and trailing whitespace and blank lines
-		 if ($sequence !~ /^\s*>/) {
-		     $sequence = ">WEB-USER-SEQUENCE (Unknown)\n$sequence";
-		 }
-		 $sequence .= "\n"; #< add a final newline
-	     }
-	         
-	     open(my $FH, ">", $seqfile) || die "Can't open file for query ($seqfile)\n";
-	     print $FH $sequence if $sequence;
-	     close($FH);
+	      if( $sequence ) {
+	      	 $sequence =~ s/^\s+|\s+$|\n\s*\n//g; #< trim out leading and trailing whitespace and blank lines
+	     # 	 if ($sequence !~ /^\s*>/) {
+	      #	     $sequence = ">WEB-USER-SEQUENCE (Unknown)\n$sequence";
+	      #	 }
+	      #	 $sequence .= "\n"; #< add a final newline
+	 
+	         print STDERR "Opening file for sequence ($seqfile)... ";
+		 open(my $FH, ">", $seqfile) || die "Can't open file for query ($seqfile)\n";
+		 print $FH $sequence if $sequence;
+		 close($FH);
+		 
+		 print STDERR "Done.\n";
+		 
 #	     if(my $file_upload = $page->get_upload) {
 #		 if ( my $fh = $file_upload->fh ) {
 #		     print $seq_fh $_ while <$fh>;
 #		 }
 #	     }
 	     
-
-	     my $i = Bio::SeqIO->new(
-		 -format   => 'fasta',
-		 -file       => $seqfile,
-		 );
-
-	     try {
-		 while ( my $s = $i->next_seq ) {
-		     $seq_count++ if $s->length;
-		     $self->validate_seq($c, $s, $params->{program} );
-		     $s->length or $c->throw(
-			 message  => 'Sequence '.encode_entities('"'.$s->id.'"').' is empty, this is not allowed by BLAST.',
-			 is_error => 0, 
-			 );
-		 }
-	     } catch {
-		 print STDERR $@;
-		 die $_ if ref; #< throw it onward if it's an exception
-		 my $full_error = $_;
-		 if( /MSG:([^\n]+)/ ) {
-		     $_ = $1;
-		 }
-		 s/at \/[\-\w \/\.]+ line \d+.+//; # remove anything resembling backtraces
-		 $c->throw( message  => $_,
-			    is_error => 0,
-			    developer_message => $full_error,
+		 print STDERR "Paring with bioperl... ";
+		 my $i = Bio::SeqIO->new(
+		     -format   => 'fasta',
+		     -file       => $seqfile,
 		     );
-	     };
+		 
+		 try {
+		     while ( my $s = $i->next_seq ) {
+			 $seq_count++ if $s->length;
+			 #$self->validate_seq($c, $s, $params->{program} );
+			 $s->length or $c->throw(
+			     message  => 'Sequence '.encode_entities('"'.$s->id.'"').' is empty, this is not allowed by BLAST.',
+			     is_error => 0, 
+			     );
+		     }
+		 } catch {
+		     print STDERR $@;
+		     die $_ if ref; #< throw it onward if it's an exception
+		     my $full_error = $_;
+		     if( /MSG:([^\n]+)/ ) {
+			 $_ = $1;
+		     }
+		     s/at \/[\-\w \/\.]+ line \d+.+//; # remove anything resembling backtraces
+		     $c->throw( message  => $_,
+				is_error => 0,
+				developer_message => $full_error,
+			 );
+		 };
 	     
-	     $seq_count >= 1 or $c->throw( message => 'no sequence submitted, cannot run BLAST',
-					   is_error => 0,
-					   developer_message => Data::Dumper::Dumper({
-					       '$seq_count' => $seq_count,
-					       '$seq_filename' => $seqfile,
-										     }),
-		 );
-	     
- 	     return -i => $seqfile;
+		 $seq_count >= 1 or $c->throw( message => 'no sequence submitted, cannot run BLAST',
+					       is_error => 0,
+					       developer_message => Data::Dumper::Dumper({
+						   '$seq_count' => $seq_count,
+						   '$seq_filename' => $seqfile,
+											 }),
+		     );
+		 
+		 return -i => $seqfile;
+	      }
 	 },
 	 
 	 matrix =>
@@ -193,13 +187,14 @@ sub run : Path('/tools/blast/run') Args(0) {
     #
     my @command = ('blastall');
     foreach my $k (keys %arg_handlers) { 
+	
 	print STDERR "evaluating $k..."; 
 	my @x = $arg_handlers{$k}->(); 
-	print STDERR "  ", (join ",", @x)."\n"; 
+	print STDERR "component:
+  ", (join ",", @x)."\n"; 
 	@command = (@command, @x);
     } 
 	
-
     print STDERR "COMMAND: ".join(" ", @command);
     print STDERR "\n";
     
@@ -221,17 +216,20 @@ sub run : Path('/tools/blast/run') Args(0) {
 		max_cluster_jobs => 1_000_000_000,
 	    }
 	    );
+	 
+	 print STDERR "Saving job state to $seqfile.job for id ".$job->job_id()."\n";
+
+	 nstore( $job, $seqfile.".job" )
+	     or die 'could not serialize job object';
+
     };
 
-    print STDERR "Saving job state to $seqfile.job for id ".$job->job_id()."\n";
-
-    nstore( $job, $seqfile.".job" )
-	or die 'could not serialize job object';
-
     if ($@) { 
+	print STDERR "An error occurred! $@\n";
 	$c->stash->{rest} = { error => $@ };
     }
     else { 
+	print STDERR "Passing jobid code ".(basename($jobid))."\n";
 	$c->stash->{rest} = { jobid =>  basename($jobid), };
     }
 }
@@ -284,16 +282,16 @@ sub check : Path('/tools/blast/check') Args(1) {
 }
 
 # fetch some html/js required for displaying the parse report
-sub get_prereqs : Path('/tools/blast/prereqs') Args(1) { 
-    my $self = shift;
-    my $c = shift;
-    my $jobid = shift;
+# sub get_prereqs : Path('/tools/blast/prereqs') Args(1) { 
+#     my $self = shift;
+#     my $c = shift;
+#     my $jobid = shift;
 
-    my $format=$c->req->param('format');
-    my $parser = CXGN::Blast::Parse->new();
-    my $prereqs = $parser->prereqs($format);
-    $c->stash->{rest} = { prereqs => $prereqs };
-}
+#     my $format=$c->req->param('format');
+#     my $parser = CXGN::Blast::Parse->new();
+#     my $prereqs = $parser->prereqs($format);
+#     $c->stash->{rest} = { prereqs => $prereqs };
+# }
 
 sub get_result : Path('/tools/blast/result') Args(1) { 
     my $self = shift;
@@ -311,96 +309,96 @@ sub get_result : Path('/tools/blast/result') Args(1) {
     my $db = $schema->resultset("BlastDb")->find($db_id);
     if (!$db) { die "Can't find database with id $db_id"; }
     my $parser = CXGN::Blast::Parse->new();
-    my $parsed_file = $parser->parse($format, $result_file, $db);
+    my $parsed_data = $parser->parse($format, $result_file, $db);
 
-    $c->stash->{rest} = { blast_report => '<pre>'.(join("\n", read_file($parsed_file))).'</pre>', };
-
-}
-
-sub blast_overview_graph : Path('/tools/blast/overview') Args(1) { 
-    my $self = shift;
-    my $c = shift;
-    my $jobid = shift;
-
+    $c->stash->{rest} = $parsed_data; # { blast_report => '<pre>'.(join("\n", read_file($parsed_file))).'</pre>', };
 
 }
 
-sub blast_coverage_graph : Path('/tools/blast/coverage') Args(1) { 
-    my $self = shift;
-    my $c = shift;
-    my $jobid = shift;
+# sub blast_overview_graph : Path('/tools/blast/overview') Args(1) { 
+#     my $self = shift;
+#     my $c = shift;
+#     my $jobid = shift;
 
 
-}
+# }
+
+# sub blast_coverage_graph : Path('/tools/blast/coverage') Args(1) { 
+#     my $self = shift;
+#     my $c = shift;
+#     my $jobid = shift;
+
+
+# }
     
 
-# validate the given sequence as input for the given blast program
-sub validate_seq : Path('/tools/blast/validate') Args(0) {
-    my $self = shift;
-    my $c = shift;
-    my $s = shift;
-    my $program = shift;
+# # validate the given sequence as input for the given blast program
+# sub validate_seq : Path('/tools/blast/validate') Args(0) {
+#     my $self = shift;
+#     my $c = shift;
+#     my $s = shift;
+#     my $program = shift;
 
-    my %alphabets = (
-        ( map { $_ => 'protein' } 'tblastn', 'blastp'            ),
-        ( map { $_ => 'DNA'     } 'blastn',  'blastx', 'tblastx' ),
-       );
+#     my %alphabets = (
+#         ( map { $_ => 'protein' } 'tblastn', 'blastp'            ),
+#         ( map { $_ => 'DNA'     } 'blastn',  'blastx', 'tblastx' ),
+#        );
 
-    #my $alphabet = $alphabets{$program}
-    #or $c->throw( message => 'invalid program!',
-    #                  is_error => 1,
-    #                  developer_message => "program was '$program'",
-    #                 );
-    if (!exists($alphabets{$program})) { 
-	$c->stash->{rest} = { 
-	    validated => 0, 
-	    error => "Invalid program '$program'. Please choose another program.", 
-	};
-	return;
-    }
-    my $alphabet = $alphabets{$program};
+#     #my $alphabet = $alphabets{$program}
+#     #or $c->throw( message => 'invalid program!',
+#     #                  is_error => 1,
+#     #                  developer_message => "program was '$program'",
+#     #                 );
+#     if (!exists($alphabets{$program})) { 
+# 	$c->stash->{rest} = { 
+# 	    validated => 0, 
+# 	    error => "Invalid program '$program'. Please choose another program.", 
+# 	};
+# 	return;
+#     }
+#     my $alphabet = $alphabets{$program};
 
-    if (!$s->validate_seq) {  #< bioperl must think it's OK
-	$c->stash->{rest} = { 
-	    validated => 0, 
-	    error => 'Not a legal sequence', 
-	};
-	return;
-    }
+#     if (!$s->validate_seq) {  #< bioperl must think it's OK
+# 	$c->stash->{rest} = { 
+# 	    validated => 0, 
+# 	    error => 'Not a legal sequence', 
+# 	};
+# 	return;
+#     }
 
-    my %not_iupac_pats = ( DNA     => qr/([^ACGTURYKMSWBDHVN]+)/i,
-			   protein => qr/([^GAVLIPFYCMHKRWSTDENQBZ\.X\*]+)/i,
-			   rna     => qr/([^ACGTURYKMSWBDHVN]+)/i,
-			 );
+#     my %not_iupac_pats = ( DNA     => qr/([^ACGTURYKMSWBDHVN]+)/i,
+# 			   protein => qr/([^GAVLIPFYCMHKRWSTDENQBZ\.X\*]+)/i,
+# 			   rna     => qr/([^ACGTURYKMSWBDHVN]+)/i,
+# 			 );
 
-    my $val_pat = $not_iupac_pats{$alphabet};
-    if (!$val_pat) { 
-	$c->stash->{rest} = { 
-	    validated => 0, 
-	    error => "Invalid alphabet ($alphabet)",
-	};
-	return;
-    }
-        # or $c->throw( message => 'invalid alphabet!',
-        #               is_error => 1,
-        #               developer_message => "alphabet was '$alphabet'",
-        #              );
-    if ($s->seq =~ $val_pat) { 
-        #and $c->throw(
-        #    message => encode_entities('Sequence "'.$s->id.qq|" contains invalid $alphabet characters "$1"| ),
-        #    is_error => 0,
-        #   );
-	$c->stash->{rest} = { 
-	    validated => 1,
-	    error => encode_entities('Sequence "'.$s->id.qq|" contains invalid $alphabet characters "$1"| ),
-	};
-	return;
-    }
-    $c->stash->{rest} = { 
-	validated => 1,
-	error => '',
-    };
-}
+#     my $val_pat = $not_iupac_pats{$alphabet};
+#     if (!$val_pat) { 
+# 	$c->stash->{rest} = { 
+# 	    validated => 0, 
+# 	    error => "Invalid alphabet ($alphabet)",
+# 	};
+# 	return;
+#     }
+#         # or $c->throw( message => 'invalid alphabet!',
+#         #               is_error => 1,
+#         #               developer_message => "alphabet was '$alphabet'",
+#         #              );
+#     if ($s->seq =~ $val_pat) { 
+#         #and $c->throw(
+#         #    message => encode_entities('Sequence "'.$s->id.qq|" contains invalid $alphabet characters "$1"| ),
+#         #    is_error => 0,
+#         #   );
+# 	$c->stash->{rest} = { 
+# 	    validated => 1,
+# 	    error => encode_entities('Sequence "'.$s->id.qq|" contains invalid $alphabet characters "$1"| ),
+# 	};
+# 	return;
+#     }
+#     $c->stash->{rest} = { 
+# 	validated => 1,
+# 	error => '',
+#     };
+# }
 
 sub jobid_to_file { 
     my $self = shift;
