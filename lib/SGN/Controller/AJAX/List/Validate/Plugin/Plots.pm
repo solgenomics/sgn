a8b30646ed13b66288e9f140bb3a84fbb1aa2865
Lukas Mueller <lam87@cornell.edu>
2013-09-28 21:28:29 -0400

    add AJAX-based interface for validation of lists.

---
+++ lib/SGN/Controller/AJAX/List/Validate/Plugin/Plots.pm
@@ -0,0 +1,29 @@
+
+package SGN::Controller::AJAX::List::Validate::Plugin::Plots;
+
+use Moose;
+
+sub validate { 
+    my $self = shift;
+    my $c = shift;
+    my $list = shift;
+
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+    
+    my $type_id = $schema->resultset("Cvterm::Cvterm")->search({ name=>"plot" })->first->type_id();
+    
+    my @missing = ();
+    foreach my $l (@$list) { 
+	my $rs = $schema->resultset("Stock")->search(
+	    { 
+		type_id=>$type_id,
+		name => $l, 
+	    });	
+	if (!$rs) { 
+	    push @missing, $l;
+	}
+    }
+    return @missing;
+}
+
+1;
