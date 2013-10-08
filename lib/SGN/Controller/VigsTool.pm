cd57cf981a7606a6857b8a455c2e71eeca9461f7
noefp <noeisneo@gmail.com>
2013-10-08 22:05:56 +0000

    jquery code structure

--- lib/SGN/Controller/VigsTool.pm
+++ lib/SGN/Controller/VigsTool.pm
@@ -3,10 +3,10 @@ package SGN::Controller::VigsTool;
 
 use Moose;
 
-use CXGN::DB::Connection;
+#use CXGN::DB::Connection;
 use CXGN::BlastDB;
-use CXGN::Page::FormattingHelpers qw| page_title_html info_table_html hierarchical_selectboxes_html |;
-use CXGN::Page::UserPrefs;
+#use CXGN::Page::FormattingHelpers qw| page_title_html info_table_html hierarchical_selectboxes_html |;
+#use CXGN::Page::UserPrefs;
 
 
 BEGIN { extends 'Catalyst::Controller'; }
@@ -14,8 +14,8 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 sub input :Path('/tools/vigs/')  :Args(0) { 
     my ($self, $c) = @_;
-    my $dbh = CXGN::DB::Connection->new;
-    our $prefs = CXGN::Page::UserPrefs->new( $dbh );
+#    my $dbh = CXGN::DB::Connection->new;
+#    our $prefs = CXGN::Page::UserPrefs->new( $dbh );
 
     # get database ids from a string in the configuration file 
     my @database_ids = split /\s+/, $c->config->{vigs_tool_blast_datasets};
@@ -26,6 +26,7 @@ sub input :Path('/tools/vigs/')  :Args(0) {
     my @databases;
     foreach my $d (@database_ids) { 
 	my $bdb = CXGN::BlastDB->from_id($d);
+
 	if ($bdb) { push @databases, $bdb; }
     }
 
