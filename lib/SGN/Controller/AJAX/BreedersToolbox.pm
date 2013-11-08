430e6e787b3e4b0b4dea657f6f178586699c3f2a
Lukas Mueller <lam87@cornell.edu>
2013-11-08 08:08:16 -0500

    require login to add a breeding program.

--- lib/SGN/Controller/AJAX/BreedersToolbox.pm
+++ lib/SGN/Controller/AJAX/BreedersToolbox.pm
@@ -141,6 +141,11 @@ sub new_breeding_program :Path('/breeders/program/new') Args(0) {
     my $c = shift;
     my $name = $c->req->param("name");
     my $desc = $c->req->param("desc");
+
+    if (!($c->user() || $c->user()->check_roles('submitter'))) { 
+	$c->stash->{rest} = { error => 'You need to be logged in and have sufficient privileges to add a breeding program.' };
+    }
+	    
        
     my $p = CXGN::BreedersToolbox::Projects->new( { schema => $c->dbic_schema("Bio::Chado::Schema") });
 
