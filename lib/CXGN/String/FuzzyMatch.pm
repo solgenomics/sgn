f1a069a8cab21c3c2d32dbe472b9df1770669bb1
Jeremy D. Edwards <jde22@cornell.edu>
2013-11-13 14:12:20 -0500

    backend to do fuzzy string matching

---
+++ lib/CXGN/String/FuzzyMatch.pm
@@ -0,0 +1,37 @@
+package CXGN::String::FuzzMatch;
+
+=head1 NAME
+
+CXGN::String::FuzzMatch - an object to find approximate matches to a query string in an array of strings.
+
+=head1 USAGE
+
+ my $fuzzy_string_search = CXGN::String::FuzzyMatch->new();
+ my @string_matches = $fuzzy_string_search->get_matches($query_string, \@string_array, $max_distance);
+
+=head1 DESCRIPTION
+
+
+=head1 AUTHORS
+
+ Jeremy D. Edwards (jde22@cornell.edu)
+
+=cut
+
+use strict;
+use warnings;
+use String::Approx;
+
+sub get_matches {
+    my $self = shift;
+    my $query_string = shift;
+    my $string_array_ref = shift;
+    my $max_distance = shift;
+    my @matches;
+    my @string_array = @{$string_array_ref};
+    return @matches;
+}
+
+###
+1;
+###
