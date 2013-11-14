e9917f9278e35b40c9b2bb5b4f9a045bdb2cac83
Jeremy D. Edwards <jde22@cornell.edu>
2013-11-13 23:03:52 -0500

    select list for adding accessions

--- js/CXGN/BreedersToolbox/Accessions.js
+++ js/CXGN/BreedersToolbox/Accessions.js
@@ -13,28 +13,26 @@ Jeremy D. Edwards <jde22@cornell.edu>
 
 */
 
+jQuery(document).ready(function () {
 
-window.onload = function initialize() { 
-    jQuery(document).ready(function () {
-
-	jQuery( "#add_accessions_dialog" ).dialog({
-	    autoOpen: false,
-	    modal: true,
-	    autoResize:true,
-            width: 500,
-            position: ['top', 150],
-	    buttons: {
-		Ok: function() {
-		    jQuery( this ).dialog( "close" );
-		    location.reload();
-		}
+    jQuery( "#add_accessions_dialog" ).dialog({
+	autoOpen: false,
+	modal: true,
+	autoResize:true,
+        width: 500,
+        position: ['top', 150],
+	buttons: {
+	    Ok: function() {
+		jQuery( this ).dialog( "close" );
+		location.reload();
 	    }
-	});
-
-	jQuery('#add_accessions_link').click(function () {
-            jQuery('#add_accessions_dialog').dialog("open");
-	});
-
+	}
     });
-}
 
+    jQuery('#add_accessions_link').click(function () {
+        jQuery('#add_accessions_dialog').dialog("open");
+	var list = new CXGN.List();
+	jQuery("#list_div").append(list.listSelect("select_list"));
+    });
+
+});
