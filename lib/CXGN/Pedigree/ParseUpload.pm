2fd53c416da3afa588b4ef0103eb111ed9a1f802
Jeremy D. Edwards <jde22@cornell.edu>
2014-01-14 03:13:12 -0500

    plugin for parsing crosses in excel

---
+++ lib/CXGN/Pedigree/ParseUpload.pm
@@ -0,0 +1,35 @@
+package CXGN::Pedigree::ParseUpload;
+
+use Moose;
+
+use Module::Pluggable require => 1;
+
+sub validate {
+    my $self = shift;
+    my $type = shift;
+    my $filename = shift;
+    my $validate_result;
+
+    foreach my $p ($self->plugins()) {
+        if ($type eq $p->name()) {
+	     $validate_result = $p->validate($filename);
+	}
+    }
+    return $validate_result;
+}
+
+sub parse {
+    my $self = shift;
+    my $type = shift;
+    my $filename = shift;
+    my $parse_result;
+
+    foreach my $p ($self->plugins()) {
+        if ($type eq $p->name()) {
+	     $parse_result = $p->parse($filename);
+	}
+    }
+    return $parse_result;
+}
+
+1;
