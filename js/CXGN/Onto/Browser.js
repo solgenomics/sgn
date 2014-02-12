1e5cba30febe71633574747e9f11fc8a54edba5b
Lukas Mueller <lam87@cornell.edu>
2014-02-12 11:00:21 -0500

    add unique ids to links in the ontology browser to make testing with selenium2 easier.

--- js/CXGN/Onto/Browser.js
+++ js/CXGN/Onto/Browser.js
@@ -928,7 +928,7 @@ Node.prototype = {
 	}
 
 	var relType=this.getRelType() || '';
-	return relType + ' <span style="'+hiliteStyle+'"><a href="/chado/cvterm?action=view&amp;cvterm_id='+this.getCVtermID()+'">'+this.getAccession() + '</a> ' + this.getName() + ' ' + link +'</span><br />';
+	return relType + ' <span style="'+hiliteStyle+'"><a id="cvterm_id_'+this.getCVtermID()+'" href="/chado/cvterm?action=view&amp;cvterm_id='+this.getCVtermID()+'">'+this.getAccession() + '</a> ' + this.getName() + ' ' + link +'</span><br />';
     },
 
     setHilite: function(h) { 
