21ca988aa0a8b176fa38be670b3f60e249f5877a
Jeremy D. Edwards <jde22@cornell.edu>
2014-02-26 15:40:58 -0500

    links to upload trial dialog and spreadsheet format info

--- js/CXGN/BreedersToolbox/UploadTrial.js
+++ js/CXGN/BreedersToolbox/UploadTrial.js
@@ -20,9 +20,42 @@ var $j = jQuery.noConflict();
 
 jQuery(document).ready(function ($) {
 
+
+    $("#upload_trial_dialog").dialog({
+	autoOpen: false,	
+	modal: true,
+	autoResize:true,
+        width: 500,
+        position: ['top', 75],
+	buttons: {
+            "Cancel": function () {
+                $('#upload_trial_dialog').dialog("close");
+            },
+	    "Add": function () {
+		alert("adding trial");
+	    },
+	}
+    });
+
+    function open_upload_trial_dialog() {
+	$('#upload_trial_dialog').dialog("open");
+    }
+
     $('#upload_trial_link').click(function () {
-	alert("upload dialog happens");
+	//alert("upload dialog happens");
         open_upload_trial_dialog();
+    });
+
+    $("#trial_upload_spreadsheet_format_info").click( function () { 
+	$("#trial_upload_spreadsheet_info_dialog" ).dialog("open");
+    });
+
+    $("#trial_upload_spreadsheet_info_dialog").dialog( {
+	autoOpen: false,
+	buttons: { "OK" :  function() { $("#trial_upload_spreadsheet_info_dialog").dialog("close"); },},
+	modal: true,
+	width: 900,
+	autoResize:true,
     });
 
 });
