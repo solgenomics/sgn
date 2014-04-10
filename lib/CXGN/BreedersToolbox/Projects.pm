a3372833244b50db4edfe49287b52b280a715a37
Jeremy D. Edwards <jde22@cornell.edu>
2014-04-10 13:50:41 -0400

    add function to get genotyping trials

--- lib/CXGN/BreedersToolbox/Projects.pm
+++ lib/CXGN/BreedersToolbox/Projects.pm
@@ -9,9 +9,9 @@ has 'schema' => ( isa => 'Bio::Chado::Schema',
                   is => 'rw');
 
 
-sub get_breeding_programs { 
+sub get_breeding_programs {
     my $self = shift;
-    
+
 
     my $breeding_program_cvterm_id = $self->get_breeding_program_cvterm_id();
 
@@ -40,10 +40,7 @@ sub get_breeding_program_by_name {
 
 }
 
-
-
-
-sub _get_all_trials_by_breeding_program { 
+sub _get_all_trials_by_breeding_program {
     my $self = shift;
     my $breeding_project_id = shift;
     my $dbh = $self->schema->storage->dbh();
@@ -84,7 +81,7 @@ sub get_trials_by_breeding_program {
     my %project_name;
     my %project_description;
 
-    while (my ($id, $name, $desc, $prop, $propvalue) = $h->fetchrow_array()) { 
+    while (my ($id, $name, $desc, $prop, $propvalue) = $h->fetchrow_array()) {
 	#push @$trials, [ $id, $name, $desc ];
       $project_name{$id} = $name;
       $project_description{$id} = $desc;
@@ -122,8 +119,7 @@ sub get_genotyping_trials_by_breeding_program {
     my %project_name;
     my %project_description;
 
-    while (my ($id, $name, $desc, $prop, $propvalue) = $h->fetchrow_array()) { 
-	#push @$trials, [ $id, $name, $desc ];
+    while (my ($id, $name, $desc, $prop, $propvalue) = $h->fetchrow_array()) {
       $project_name{$id} = $name;
       $project_description{$id} = $desc;
       if ($prop == $cross_cvterm_id) {
@@ -141,7 +137,9 @@ sub get_genotyping_trials_by_breeding_program {
 
     foreach my $id_key (@sorted_by_year_keys) {
       if (!$projects_that_are_crosses{$id_key}) {
-	push @$trials, [ $id_key, $project_name{$id_key}, $project_description{$id_key}];
+	if ($projects_that_are_genotyping_trials{$id_key}) {
+	  push @$trials, [ $id_key, $project_name{$id_key}, $project_description{$id_key}];
+	}
       }
     }
 
@@ -158,14 +156,14 @@ sub get_locations_by_breeding_program {
 
     my $type_id = $self->schema->resultset('Cv::Cvterm')->search( { 'name'=>'plot' })->first->cvterm_id;
 
-    if ($breeding_program_id) { 
+    if ($breeding_program_id) {
 	my $q = "SELECT distinct(nd_geolocation_id), nd_geolocation.description, count(distinct(stock.stock_id)) FROM project JOIN project_relationship on (project_id=object_project_id) JOIN project as trial ON (subject_project_id=trial.project_id) JOIN nd_experiment_project ON (trial.project_id=nd_experiment_project.project_id) JOIN nd_experiment USING (nd_experiment_id) JOIN nd_experiment_stock ON (nd_experiment.nd_experiment_id=nd_experiment_stock.nd_experiment_id) JOIN stock ON (nd_experiment_stock.stock_id=stock.stock_id) JOIN nd_geolocation USING (nd_geolocation_id) WHERE project.project_id=? and stock.type_id=? GROUP BY nd_geolocation.nd_geolocation_id, nd_experiment.nd_geolocation_id, nd_geolocation.description";
 
 	$h = $self->schema()->storage()->dbh()->prepare($q);
 	$h->execute($breeding_program_id, $type_id);
 
     }
-    else { 
+    else {
 	my $q = "SELECT distinct(nd_geolocation_id), nd_geolocation.description FROM nd_geolocation LEFT JOIN nd_experiment USING(nd_geolocation_id) where nd_experiment_id IS NULL";
 
 	$h = $self->schema()->storage()->dbh()->prepare($q);
@@ -173,62 +171,62 @@ sub get_locations_by_breeding_program {
     }
 
     my @locations;
-    while (my ($id, $name, $plot_count) = $h->fetchrow_array()) { 
+    while (my ($id, $name, $plot_count) = $h->fetchrow_array()) {
 	push @locations, [ $id, $name, $plot_count ];
     }
     return \@locations;
 }
 
-sub get_accessions_by_breeding_program { 
+sub get_accessions_by_breeding_program {
 
 
 }
 
 
-sub new_breeding_program { 
+sub new_breeding_program {
     my $self= shift;
     my $name = shift;
     my $description = shift;
 
     my $type_id = $self->get_breeding_program_cvterm_id();
 
-    my $rs = $self->schema()->resultset("Project::Project")->search( 
-	{ 
+    my $rs = $self->schema()->resultset("Project::Project")->search(
+	{
 	    name => $name,
 	});
-    if ($rs->count() > 0) { 
+    if ($rs->count() > 0) {
 	return "A breeding program with name '$name' already exists.";
     }
 
     eval { 
-	my $row = $self->schema()->resultset("Project::Project")->create( 
-	    { 
+	my $row = $self->schema()->resultset("Project::Project")->create(
+	    {
 		name => $name,
 		description => $description,
 	    });
-	
+
 	$row->insert();
 
 	my $prop_row = $self->schema()->resultset("Project::Projectprop")->create(
-	    { 
+	    {
 		type_id => $type_id,
 		project_id => $row->project_id(),
-		
+
 	    });
 	$prop_row->insert();
 
     };
-    if ($@) { 
+    if ($@) {
 	return "An error occurred while generating a new breeding program. ($@)";
     }
 
 }
 
 
-sub delete_breeding_program { 
+sub delete_breeding_program {
     my $self = shift;
     my $project_id = shift;
-    
+
     my $type_id = $self->get_breeding_program_cvterm_id();
 
     # check if this project entry is of type 'breeding program'
@@ -236,23 +234,23 @@ sub delete_breeding_program {
 	type_id => $type_id,
 	project_id => $project_id,
 	);
-    
-    if ($prop->count() == 0) { 
+
+    if ($prop->count() == 0) {
 	return 0; # wrong type, return 0.
     }
 
     $prop->delete();
 
-    my $rs = $self->schema->resultset("Project::Project")->search( 
+    my $rs = $self->schema->resultset("Project::Project")->search(
 	project_id => $project_id,
 	);
 
-    if ($rs->count() > 0) { 
+    if ($rs->count() > 0) {
 	my $pprs = $self->schema->resultset("Project::ProjectRelationship")->search(
 	    object_project_id => $project_id,
 	    );
 
-	if ($pprs->count()>0) { 
+	if ($pprs->count()>0) {
 	    $pprs->delete();
 	}
 	$rs->delete();
@@ -261,7 +259,7 @@ sub delete_breeding_program {
     return 0;
 }
 
-sub get_breeding_program_with_trial { 
+sub get_breeding_program_with_trial {
     my $self = shift;
     my $trial_id = shift;
 
@@ -277,28 +275,28 @@ sub get_breeding_program_with_trial {
     return $breeding_projects;
 }
 
-sub associate_breeding_program_with_trial { 
+sub associate_breeding_program_with_trial {
     my $self = shift;
     my $breeding_project_id = shift;
     my $trial_id = shift;
 
     my $breeding_trial_cvterm_id = $self->get_breeding_trial_cvterm_id();
-    
+
     # to do: check if the two provided IDs are of the proper type
 
-    eval { 
-	my $breeding_trial_assoc = $self->schema->resultset("Project::ProjectRelationship")->update_or_new( 
-	    { 
+    eval {
+	my $breeding_trial_assoc = $self->schema->resultset("Project::ProjectRelationship")->update_or_new(
+	    {
 		object_project_id => $breeding_project_id,
 		subject_project_id => $trial_id,
 		type_id => $breeding_trial_cvterm_id,
 	    }
 	    );
-	
+
 	if (! $breeding_trial_assoc->in_storage()) { $breeding_trial_assoc->insert(); }
 
     };
-    if ($@) { 
+    if ($@) {
 	print STDERR "ERROR: $@\n";
 	return { error => "An error occurred while storing the breeding program - trial relationship." };
     }
@@ -309,22 +307,22 @@ sub remove_breeding_program_from_trial {
     my $self = shift;
     my $breeding_program_id = shift;
     my $trial_id = shift;
-    
+
     my $breeding_trial_cvterm_id = $self->get_breeding_trial_cvterm_id();
 
-    eval { 
-	my $breeding_trial_assoc_rs = $self->schema->resultset("Project::ProjectRelationship")->search( 
-	    { 
+    eval {
+	my $breeding_trial_assoc_rs = $self->schema->resultset("Project::ProjectRelationship")->search(
+	    {
 		object_project_id => $breeding_program_id,
 		subject_project_id => $trial_id,
 		type_id => $breeding_trial_cvterm_id,
 	    }
 	    );
-	if (my $row = $breeding_trial_assoc_rs->first()) { 
+	if (my $row = $breeding_trial_assoc_rs->first()) {
 	    $row->delete();
 	}
     };
-    
+
     if ($@) { 
 	return { error => "An error occurred while deleting a breeding program - trial association. $@" };
     }
@@ -334,14 +332,14 @@ sub remove_breeding_program_from_trial {
 
 sub get_breeding_program_cvterm_id {
     my $self = shift;
-    
+
     my $breeding_program_cvterm_rs = $self->schema->resultset('Cv::Cvterm')->search( { name => 'breeding_program' });
 
     my $row;
 
-    if ($breeding_program_cvterm_rs->count() == 0) { 
-	$row = $self->schema->resultset('Cv::Cvterm')->create_with( 
-	    { 
+    if ($breeding_program_cvterm_rs->count() == 0) {
+	$row = $self->schema->resultset('Cv::Cvterm')->create_with(
+	    {
 		name => 'breeding_program',
 		cv   => 'local',
 		db   => 'local',
@@ -349,23 +347,23 @@ sub get_breeding_program_cvterm_id {
 	    });
 
     }
-    else { 
+    else {
 	$row = $breeding_program_cvterm_rs->first();
     }
 
     return $row->cvterm_id();
 }
- 
-sub get_breeding_trial_cvterm_id { 
+
+sub get_breeding_trial_cvterm_id {
     my $self = shift;
 
     my $cv_id = $self->schema->resultset('Cv::Cv')->find( { name => 'local' } )->cv_id();
 
     my $breeding_trial_cvterm_row = $self->schema->resultset('Cv::Cvterm')->find( { name => 'breeding_program_trial_relationship' });
-    
-    if (!$breeding_trial_cvterm_row) { 
-	my $row = $self->schema->resultset('Cv::Cvterm')->create_with( 
-	    { 
+
+    if (!$breeding_trial_cvterm_row) {
+	my $row = $self->schema->resultset('Cv::Cvterm')->create_with(
+	    {
 		name => 'breeding_program_trial_relationship',
 		cv   => 'local',
 		db   => 'local',
@@ -376,21 +374,30 @@ sub get_breeding_trial_cvterm_id {
     return $breeding_trial_cvterm_row->cvterm_id();
 }
 
-sub get_cross_cvterm_id { 
+sub get_cross_cvterm_id {
     my $self = shift;
     my $cv_id = $self->schema->resultset('Cv::Cv')->find( { name => 'stock type' } )->cv_id();
     my $cross_cvterm_row = $self->schema->resultset('Cv::Cvterm')->find( { name => 'cross', cv_id=> $cv_id });
     return $cross_cvterm_row->cvterm_id();
 }
 
-sub get_genotyping_trial_cvterm_id { 
+sub get_genotyping_trial_cvterm_id {
     my $self = shift;
-    my $cv_id = $self->schema->resultset('Cv::Cv')->find( { name => 'stock type' } )->cv_id();
-    my $cross_cvterm_row = $self->schema->resultset('Cv::Cvterm')->find( { name => 'cross', cv_id=> $cv_id });
-    return $cross_cvterm_row->cvterm_id();
+    my $cv = $self->schema->resultset("Cv::Cv")
+      ->find_or_create({
+			name => 'trial type',
+		       });
+    my $cvterm_id = $self->schema->resultset("Cv::Cvterm")
+      ->create_with({
+		     name   => 'genotyping trial',
+		     cv     => 'trial type',
+		     db     => 'null',
+		     dbxref => 'genotyping trial',
+		    });
+    return $cvterm_id;
 }
 
-sub get_project_year_cvterm_id { 
+sub get_project_year_cvterm_id {
     my $self = shift;
     my $year_cvterm_row = $self->schema->resultset('Cv::Cvterm')->find( { name => 'project year' });
     return $year_cvterm_row->cvterm_id();
