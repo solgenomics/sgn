f0ecc5c7380f53a72ab88103ba5ac6f994ed16c6
isaak <iyt2@cornell.edu>
2014-06-03 14:26:45 +0200

    refined process and error messaging

--- js/solGS/searchTrials.js
+++ js/solGS/searchTrials.js
@@ -14,26 +14,37 @@ jQuery(document).ready(function(){
 
 
 function searchTrials() {
- 
-    jQuery.ajax({  
+   
+    jQuery.ajax({
         type: 'POST',
         dataType: "json",
         url: '/solgs/search/trials/',
         success: function(res) {                                                                                
             var trialsList = listTrials(res.trials);
             var pagination = res.pagination;
-            jQuery("#homepage_trials_list").html(trialsList + pagination).show();            
+            jQuery("#homepage_message").hide();
+            jQuery("#homepage_trials_list").html(trialsList + pagination).show();           
         },
         error: function() {               
-            alert('error fetching first 10 trials');
+            jQuery("#homepage_message").html('Error occured fetching the first set of GS trials.').show();
+          
+        },
+        complete: function() {         
+            showListPops();
         }
-    });
 
+    });
+    
+  
     jQuery("#homepage_trials_list").on('click', "div.paginate_nav a", function(e) { 
         var page = jQuery(this).attr('href');
-  
+       
+        jQuery("#homepage_trials_list").empty();
+        jQuery("#combine").hide();
+        jQuery("#homepage_message").html('Fechting more GS trials..').show(); 
+ 
         if (page) {
-            jQuery.ajax({  
+            jQuery.ajax({ 
                 type: 'POST',
                 dataType: "json",
                 url: page,
@@ -41,12 +52,13 @@ function searchTrials() {
                     var trialsList = listTrials(res.trials);
                     var pagination = res.pagination;
                     jQuery("#homepage_trials_list").empty();
+                    jQuery("#homepage_message").hide(); 
                     jQuery("#homepage_trials_list").html(trialsList + pagination).show();
                   
                 },
                 
                 error: function() {
-                    alert('error fetching trials next pages.');
+                    jQuery("#homepage_message").html('Error occured fetching the next set of GS trials.').show();
                 }                    
             });                
         }
