f1bf243216d3a0e7f12a6e2c1ef67b8c7f2250b4
Lukas Mueller <lam87@cornell.edu>
2015-02-13 08:11:09 -0500

    remove alert.

--- js/CXGN/BreedersToolbox/HTMLSelect.js
+++ js/CXGN/BreedersToolbox/HTMLSelect.js
@@ -5,7 +5,6 @@ function get_select_box(type, div_id, name) {
 	url: '/ajax/html/select/'+type,
 	data: { 'name' : name },
 	success: function(response) { 
-	    alert(response.select);
 	    jQuery('#'+div_id).html(response.select);
 	},
 	error: function(response) { 
