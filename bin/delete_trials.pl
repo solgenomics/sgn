20be7a17257bf79e213f367730d396e90dbfc991
Lukas Mueller <lam87@cornell.edu>
2015-02-16 15:22:27 -0500

    add -n option for non-interactive mode.

--- bin/delete_trials.pl
+++ bin/delete_trials.pl
@@ -25,13 +25,14 @@ use CXGN::Phenome::Schema;
 use CXGN::DB::InsertDBH;
 use CXGN::Trial;
 
-our ($opt_H, $opt_D, $opt_i);
+our ($opt_H, $opt_D, $opt_i, $opt_n);
 
-getopts('H:i:D:');
+getopts('H:i:D:n');
 
 my $dbhost = $opt_H;
 my $dbname = $opt_D;
 my $trial_id = $opt_i;
+my $non_interactive = $opt_n;
 
 my $dbh = CXGN::DB::InsertDBH->new( { dbhost=>$dbhost,
 				      dbname=>$dbname,
@@ -47,17 +48,20 @@ my $phenome_schema = CXGN::Phenome::Schema->connect( sub { $dbh->get_actual_dbh(
 print STDERR "Retrieving trial information...\n";
 my $t = CXGN::Trial->new( { bcs_schema => $schema , trial_id => $trial_id } );
 
-print $t->get_name().", ".$t->get_description().". Delete? ";
-my $answer = <>;
-if ($answer =~ m/^y/i) { 
+my $answer = "";
+if (!$non_interactive) { 
+    print $t->get_name().", ".$t->get_description().". Delete? ";
+    $answer = <>;
+}
+if ($non_interactive || $answer =~ m/^y/i) { 
     print STDERR "Delete metadata...\n";
     $t->delete_metadata($metadata_schema, $phenome_schema);
-     print STDERR "Deleting phenotypes...\n";
-     $t->delete_phenotype_data();
-     print STDERR "Deleting layout...\n";
-     $t->delete_field_layout();
-     print STDERR "Delete project entry...\n";
-     $t->delete_project_entry();
+    print STDERR "Deleting phenotypes...\n";
+    $t->delete_phenotype_data();
+    print STDERR "Deleting layout...\n";
+    $t->delete_field_layout();
+    print STDERR "Delete project entry...\n";
+    $t->delete_project_entry();
 }
 
 print STDERR "Done.\n";
