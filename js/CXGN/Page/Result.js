ce208ce3d809e7b4826d0487cbade46ae904efb9
Lukas Mueller <lam87@cornell.edu>
2015-02-23 15:34:17 -0500

    use datatables for the trial search.

--- js/CXGN/Page/Result.js
+++ js/CXGN/Page/Result.js
@@ -2,22 +2,22 @@
 
 function display_results(headers, results, page_number, page_size, total_results) { 
 
-    var html = 'Total matches: <b>'+total_results + '</b>  Total pages: <b>'+ Math.floor((total_results / page_size)+1) + '</b>' ;
-    html += '<table  alt="search results" cellpadding="4" >';
+    var html = '<table id="results_table" class="display" cellspacing="0" width="100%" >';
 
-    html += '<tr>';
+    html += '<thead><tr>';
     for(var n=0; n<headers.length; n++) { 
-	html += '<td>'+headers[n]+'</td>';
+	html += '<th>'+headers[n]+'</th>';
     }
-    html += '</tr>';
+    html += '</tr></thead><tbody>';
     for(var n=0; n<results.length; n++) { 
 	html += '<tr>';
 
 	for(var i=0; i<results[n].length; i++) { 
 	    html += '<td>'+results[n][i]+'</td>';
 	}
-	html += '</tr>';
+	html += '</td><td></td></tr>';
     }
-    html += '</table>';
+    html += '</tbody></table>';
+    alert(html);
     return html;
 }
