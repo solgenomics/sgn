4a7e13b027d1362a25398851e0d7ab66a8b50c4f
Naama Menda <naama@localhost.localdomain>
2015-05-08 16:02:02 -0400

    add matchtype param to search

--- lib/SGN/Controller/AJAX/Search/Loci.pm
+++ lib/SGN/Controller/AJAX/Search/Loci.pm
@@ -36,32 +36,42 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
     
     my $matchtype = $params->{any_name_matchtype};
     my $any_name  = $params->{any_name};
-# contains    'ilike', '%' . $any_name . '%' 
-# exactly     'ilike , $any_name
-# starts_with 'ilike , $any_name . '%'
-# ends_with   'ilike , '%'.$any_name 
- 
-my $or_conditions = [ 
-     { locus         => {'ilike', '%'.$params->{any_name}.'%'} },  
-     { locus_name    => {'ilike', '%'.$params->{any_name}.'%'} },  
-     { locus_symbol  => {'ilike', '%'.$params->{any_name}.'%'} }, 
-     { description   => {'ilike', '%'.$params->{any_name}.'%'} },  
-     { gene_activity => {'ilike', '%'.$params->{any_name}.'%'} } 
-    ] ; 
 
+    my ($or_conditions, $and_conditions);
+    if (exists($params->{"any_name"} ) && $params->{"any_name"} ) {
+	my $start = '%';
+	my $end = '%';
+	if ( $matchtype eq 'exactly' ) {
+	    $start = '';
+	    $end = '';
+	} elsif ( $matchtype eq 'starts_with' ) {
+	    $start = '';
+	} elsif ( $matchtype eq 'ends_with' ) {
+	    $end = '';
+	}
 
-my $and_conditions = {
-    common_name_id      => $params->{organism},
-    linkage_group => $params->{linkage_group},
-    #locus_editor  => {'ilike', '%'.$params->{locus_editor}.'%' }, # join sgn.sgn_people
-    #phenotype     => {'ilike', '%'.$params->{phenotype}.'%' },    # join phenome.allele
-    #ontology_term => {'ilike', '%'.$params->{ontology_term}.'%'}, # join locus_dbxref->public.dbxref->public.cvterm  ( [ { dbxref.accession => {'ilike', '%'.$ontology_term.'%'} , {cvterm.name => 'ilike' , '%'.$ontology_term.'%'} ] ),
-    #dbxref.accession => {'ilike', '%'.$params->{genbank_accession}.'%'}, # join locus_dbxref->dbxref 
-    #has_sequence  # join locus_dbxref->dbxref->db  genbank, sgn unigene , or solyc id 
-    #has_marker    # join locus_marker
-    #has_annotation # join locus_dbxref->dbxref->db where db = GO || PO || SP
-};
-    
+	$or_conditions = [ 
+	    { locus         => {'ilike', $start.$params->{any_name}.$end} },  
+	    { locus_name    => {'ilike', $start.$params->{any_name}.$end} },  
+	    { locus_symbol  => {'ilike', $start.$params->{any_name}.$end} }, 
+	    { description   => {'ilike', $start.$params->{any_name}.$end} },  
+	    { gene_activity => {'ilike', $start.$params->{any_name}.$end} } 
+	    ] ; 
+    }
+    ###############
+    if (exists($params->{organism} ) && $params->{organism} ) {
+	$and_conditions = {
+	    common_name_id      => $params->{organism},
+	    linkage_group => $params->{linkage_group},
+	    #locus_editor  => {'ilike', '%'.$params->{locus_editor}.'%' }, # join sgn.sgn_people
+	    #phenotype     => {'ilike', '%'.$params->{phenotype}.'%' },    # join phenome.allele
+	    #ontology_term => {'ilike', '%'.$params->{ontology_term}.'%'}, # join locus_dbxref->public.dbxref->public.cvterm  ( [ { dbxref.accession => {'ilike', '%'.$ontology_term.'%'} , {cvterm.name => 'ilike' , '%'.$ontology_term.'%'} ] ),
+	    #dbxref.accession => {'ilike', '%'.$params->{genbank_accession}.'%'}, # join locus_dbxref->dbxref 
+	    #has_sequence  # join locus_dbxref->dbxref->db  genbank, sgn unigene , or solyc id 
+	    #has_marker    # join locus_marker
+	    #has_annotation # join locus_dbxref->dbxref->db where db = GO || PO || SP
+	};
+    }
 ###############
     foreach my $k ( qw |  locus_name locus_symbol locus description gene_activity  | ) { 
 	if (exists($params->{$k}) && $params->{$k}) { 
