0d5b85fd063ad1764686974be312bb6495780320
Naama Menda <naama@localhost.localdomain>
2015-05-27 11:54:21 -0400

    display distinct list of loci with a string of allele symbols

--- lib/SGN/Controller/AJAX/Search/Loci.pm
+++ lib/SGN/Controller/AJAX/Search/Loci.pm
@@ -46,23 +46,23 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
 	}
 
 	$or_conditions = [ 
-	    { 'locus_id.locus'         => {'ilike', $start.$params->{any_name}.$end} },  
-	    { 'locus_id.locus_name'    => {'ilike', $start.$params->{any_name}.$end} },  
-	    { 'locus_id.locus_symbol'  => {'ilike', $start.$params->{any_name}.$end} }, 
-	    { 'locus_id.description'   => {'ilike', $start.$params->{any_name}.$end} },  
-	    { 'locus_id.gene_activity' => {'ilike', $start.$params->{any_name}.$end} } 
+	    { 'locus'         => {'ilike', $start.$params->{any_name}.$end} },  
+	    { 'locus_name'    => {'ilike', $start.$params->{any_name}.$end} },  
+	    { 'locus_symbol'  => {'ilike', $start.$params->{any_name}.$end} }, 
+	    { 'description'   => {'ilike', $start.$params->{any_name}.$end} },  
+	    { 'gene_activity' => {'ilike', $start.$params->{any_name}.$end} } 
 	    ] ; 
     } else { 
-	$or_conditions = [ { 'locus_id.locus_name' => { '!=', undef } } ];
+	$or_conditions = [ { 'locus_name' => { '!=', undef } } ];
     }
     
     ###############
     if (exists($params->{common_name} ) && $params->{common_name} ) {
-	$and_conditions->{'locus_id.common_name_id'} = $params->{common_name} ;
+	$and_conditions->{'common_name_id'} = $params->{common_name} ;
 	
     }
     if (exists($params->{linkage_group} ) && $params->{linkage_group} ) {
-	$and_conditions->{'locus_id.linkage_group'} = $params->{linkage_group} ;
+	$and_conditions->{'linkage_group'} = $params->{linkage_group} ;
 	
     }
     if (exists($params->{phenotype} ) && $params->{phenotype} ) {
@@ -120,7 +120,7 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
 
     if (exists($params->{has_marker} ) && $params->{has_marker} eq "true"  ) {
 	my $m_rs = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("LocusMarker")->search( {} );
-	$and_conditions->{'locus_id.locus_id'} = { -in => $m_rs->get_column('locus_id')->as_query }; 
+	$and_conditions->{'locus_id'} = { -in => $m_rs->get_column('locus_id')->as_query }; 
     }
 
     if (exists($params->{has_annotation} ) && $params->{has_annotation} eq "true"  ) {
@@ -146,7 +146,7 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
 
 
     # get the count first
-    my $rs = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("Allele")->search( 
+    my $rs = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("Locus")->search( 
 	{
 	    -and => [
 		 $or_conditions,
@@ -154,7 +154,8 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
 		],
 	},
 	{
-	    join => { 'locus_id' => [ 'locus_owners' , 'locus_dbxrefs' ] },
+	    join =>  [ 'locus_owners' , 'locus_dbxrefs' , 'alleles' ] ,
+	    prefetch => 'alleles',
 	} 
 	);
 
@@ -163,7 +164,7 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
     #print STDERR "RECORDS TOTAL: $records_total\n";
     ## then get the data
     #
-    my $rs2 = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("Allele")->search(   
+    my $rs2 = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("Locus")->search(   
 	{
 	    -and => [
 		 $or_conditions,
@@ -171,26 +172,31 @@ sub locus_search :Path('/ajax/search/loci') Args(0) {
 		],
 	} ,
 	{ 
-	    join => { 'locus_id' => [ 'locus_owners' , 'locus_dbxrefs' ] },
-	    '+select' => [ 'locus_id.locus_name', 'locus_id.locus', 'locus_id.locus_symbol', 'locus_id.common_name_id' ],
-	    page => $page, 
-	    rows => $rows, 
-	    order_by => 'locus_name' 
+	    join      =>  [ 'locus_owners' , 'locus_dbxrefs' , 'alleles' ] ,
+	    '+select' => [ 'locus_name', 'locus', 'locus_symbol', 'common_name_id' ],
+	    '+as'     => [qw/ locus_name genome_locus locus_symbol common_name_id  / ],
+	    distinct  => 1,
+	    page      => $page, 
+	    rows      => $rows, 
+	    order_by  => 'locus_name' 
 	} 
 	);
 	
-
+    
     my @result;
-    while (my $a = $rs2->next()) { 
-	my $common_name_id = $a->locus_id->common_name_id;
-	my $locus_id = $a->get_column("locus_id") ;
-	my $locus_name = $a->locus_id->locus_name;
-	my $locus = $a->locus_id->locus;
-	my $locus_symbol = $a->locus_id->locus_symbol;
-	my $allele_name = $a->allele_name;
-	my $allele_symbol = $a->allele_symbol;
-	my $allele_phenotype = $a->allele_phenotype;
-	push @result, [ $common_name_id, "<a href=\"/locus/$locus_id/view\">$locus_name</a>", $locus_symbol, $allele_name, $allele_symbol, $allele_phenotype ];
+    while (my $l = $rs2->next()) { 
+	my $common_name_id = $l->common_name_id;
+	my $locus_id = $l->get_column("locus_id") ;
+	my $locus_name = $l->locus_name;
+	my $locus = $l->locus;
+	my $locus_symbol = $l->locus_symbol;
+	my $allele_string;
+	my $alleles = $l->alleles;
+	while ( my $a = $alleles->next() ) {
+	    $allele_string .= $a->allele_symbol . ", ";
+	}
+	$allele_string = substr($allele_string, 0, -2) ;
+	push @result, [ $common_name_id, "<a href=\"/locus/$locus_id/view\">$locus_name</a>", $locus_symbol, $allele_string ];
     }
 
     $c->stash->{rest} = { data => [ @result ], draw => $draw, recordsTotal => $records_total,  recordsFiltered => $records_total };
