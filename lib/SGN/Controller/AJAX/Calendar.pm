f0b87f649cbdc20506ccd2829324b1077923d5ae
nmorales3142 <nmorales3142@gmail.com>
2015-07-17 19:28:16 +0000

    added FullCalendar jquery files

---
+++ lib/SGN/Controller/AJAX/Calendar.pm
@@ -0,0 +1,57 @@
+
+=head1 NAME
+
+SGN::Controller::AJAX::Calendar - a REST controller class to provide the
+backend for displaying events on the calendar
+
+=head1 DESCRIPTION
+
+The FullCalendar Event call sends a GET request with a start, end, and _ value. These values can be used to query specific date ranges. 
+Using REST, json values for FullCalendar Event Object properties can be sent to be displayed, simply by stash->{rest}
+
+=head1 AUTHOR
+
+Nicolas Morales <nm529@cornell.edu>
+
+=cut
+
+
+package SGN::Controller::AJAX::Calendar;
+
+use strict;
+use Moose;
+use JSON;
+
+BEGIN { extends 'Catalyst::Controller::REST' }
+
+__PACKAGE__->config(
+    default   => 'application/json',
+    stash_key => 'rest',
+    map       => { 'application/json' => 'JSON', 'text/html' => 'JSON' },
+   );
+
+sub get_calendar_events : Path('/ajax/calendar/populate') : ActionClass('REST') { }
+
+sub get_calendar_events_GET { 
+    my $self = shift;
+    my $c = shift;
+    my $start = $c->req->param("start");
+    my $end = $c->req->param("end");
+
+    #cvterm names of interest:  "project year", "project fertilizer date", "project planting date"
+
+    my $q = "SELECT c.name, a.value, b.name FROM ((projectprop as a INNER JOIN cvterm as b on (a.type_id=b.cvterm_id)) INNER JOIN project as c on (a.project_id=c.project_id)) WHERE b.name='project planting date'";
+    my $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+
+    my @results;
+    while (my ($project_name, $project_date, $project_prop) = $sth->fetchrow_array ) {
+	push(@results, {title=>$project_name.' '.$project_prop, start=>$project_date});
+    }
+    push(@results, {title=>"Test Event", start=>"2015-07-16"});
+    push(@results, {title=>"Test 2", start=>"2015-07-10"});
+    $c->stash->{rest} = \@results;
+
+}
+
+1;
--- lib/SGN/Controller/Calendar.pm
+++ lib/SGN/Controller/Calendar.pm
@@ -1,10 +1,27 @@
+
+=head1 NAME
+
+SGN::Controller::Calendar - a controller class to handle calendar related methods, such as editing, viewing, modifying, deleting, and adding of events.
+
+=head1 DESCRIPTION
+
+For display and processing of calendar. AJAX calendar requests are located in SGN::Controller::AJAX::Calendar
+
+=head1 AUTHOR
+
+Nicolas Morales <nm529@cornell.edu>
+
+=cut
+
+
 package SGN::Controller::Calendar;
 
 use Moose;
+use JSON;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
-#this function maps the url /cassbasetest/test_page/ to test_page.mas
+#this function maps the url /calender/test_page/ to test_page.mas
 sub test_page :Path('/calendar/test_page/') :Args(0) { 
     my $self = shift;
     my $c = shift;
