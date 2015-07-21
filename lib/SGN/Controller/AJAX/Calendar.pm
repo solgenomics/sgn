2834734e90fa4b017cd19aaf6f8a9a999160b6e5
nmorales3142 <nmorales3142@gmail.com>
2015-07-21 21:24:27 +0000

    adding events works

--- lib/SGN/Controller/AJAX/Calendar.pm
+++ lib/SGN/Controller/AJAX/Calendar.pm
@@ -50,13 +50,7 @@ sub get_calendar_events_GET {
     while (my ($projectprop_id, $project_name, $project_date, $project_prop) = $sth->fetchrow_array ) {
 	push(@results, {projectprop_id=>$projectprop_id, title=>$project_name, property=>$project_prop, start=>$project_date, save=>$project_date});
     }
-
-    #Add some dummy test values
-    push(@results, {projectprop_id=>'9000', title=>"Populate Test 1", property=>"Test Date", start=>"2015-07-16"});
-    push(@results, {projectprop_id=>'9001', title=>"Populate Test 2", property=>"Test Date", start=>"2015-07-10"});
-    push(@results, {projectprop_id=>'9002', title=>"Populate Test 3", property=>"Test Date", start=>"2015-08-02"});
     $c->stash->{rest} = \@results;
-
 }
 
 sub drag_events : Path('/ajax/calendar/drag') : ActionClass('REST') { }
@@ -67,7 +61,16 @@ sub drag_events_POST {
     my $start = $c->req->param("save");
     my $projectprop_id = $c->req->param("projectprop_id");
     my $delta = $c->req->param("delta");
-    my $dt = Time::Piece->strptime($start, '%Y-%b-%d');
+    my $dt;
+    if ($start =~ /^\d{4}-\d\d-\d\d$/) {
+	$dt = Time::Piece->strptime($start, '%Y-%m-%d');
+    }
+    if ($start =~ /^(3[0-1]|2[0-9]|1[0-9]|0[1-9])[\s{1}|\/|-](Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[\s{1}|\/|-]\d{4}$/) {
+	$dt = Time::Piece->strptime($start, '%d-%b-%Y');
+    }
+    if ($start =~ /^\d{4}[\s{1}|\/|-](Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[\s{1}|\/|-](3[0-1]|2[0-9]|1[0-9]|0[1-9])$/) {
+	$dt = Time::Piece->strptime($start, '%Y-%b-%d');
+    }
     $dt += ONE_DAY * $delta;
     my $newdate = $dt->strftime('%Y-%b-%d');
     
@@ -77,5 +80,6 @@ sub drag_events_POST {
     
     $c->stash->{rest} = {success => "1",};
 }
+
 
 1;
--- lib/SGN/Controller/Calendar.pm
+++ lib/SGN/Controller/Calendar.pm
@@ -30,20 +30,78 @@ sub test_page :Path('/calendar/test_page/') :Args(0) {
     my $q = "SELECT a.projectprop_id, c.name, a.value, b.name FROM ((projectprop as a INNER JOIN cvterm as b on (a.type_id=b.cvterm_id)) INNER JOIN project as c on (a.project_id=c.project_id))";
     my $sth = $c->dbc->dbh->prepare($q);
     $sth->execute();
-
     my @results;
     while (my ($projectprop_id, $project_name, $project_date, $project_prop) = $sth->fetchrow_array ) {
 	push(@results, {projectprop_id=>$projectprop_id, title=>$project_name, property=>$project_prop, start=>$project_date, save=>$project_date});
     }
 
-    #Add some dummy test values
-    push(@results, {projectprop_id=>'9000', title=>"Populate Test 1", property=>"Test Date", start=>"2015-07-16"});
-    push(@results, {projectprop_id=>'9001', title=>"Populate Test 2", property=>"Test Date", start=>"2015-07-10"});
-    push(@results, {projectprop_id=>'9002', title=>"Populate Test 3", property=>"Test Date", start=>"2015-08-02"});
+    $q = "SELECT DISTINCT b.cvterm_id, b.name FROM (projectprop as a INNER JOIN cvterm as b on (a.type_id=b.cvterm_id))";
+    $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+    my @projectprop_types;
+    while (my ($cvterm_id, $cvterm_name) = $sth->fetchrow_array ) {
+	push(@projectprop_types, {cvterm_id=>$cvterm_id, cvterm_name=>$cvterm_name});
+    }
+
+    $q = "SELECT DISTINCT project_id, name FROM project";
+    $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+    my @projects;
+    while (my ($project_id, $project_name) = $sth->fetchrow_array ) {
+	push(@projects, {project_id=>$project_id, project_name=>$project_name});
+    }
+
+    $c->stash->{projects} = \@projects;
+    $c->stash->{projectprop_types} = \@projectprop_types;
     $c->stash->{project_dates_data} = \@results;
     $c->stash->{template} = '/calendar/test_page.mas';
     $c->stash->{static_content_path} = $c->config->{static_content_path};
 }
 
+sub add_event :Path('/calendar/add_event') :Args(0) { 
+    my $self = shift;
+    my $c = shift;
+    my $project_id = $c->req->param("event_project");
+    my $cvterm_id = $c->req->param("event_type");
+    my $date = $c->req->param("event_start");
+    my $count = $c->dbc->dbh->selectrow_array("SELECT count(projectprop_id) FROM projectprop WHERE project_id='$project_id' and type_id='$cvterm_id'");
+    if ($count > 0){
+      $c->stash->{add_event} = 'false';
+    } else {
+      my $q = "INSERT INTO projectprop (project_id, type_id, value) VALUES (?, ?, ?)";
+      my $sth = $c->dbc->dbh->prepare($q);
+      $sth->execute($project_id, $cvterm_id, $date);
+      $c->stash->{add_event} = 'true';
+    }
+
+    my $q = "SELECT a.projectprop_id, c.name, a.value, b.name FROM ((projectprop as a INNER JOIN cvterm as b on (a.type_id=b.cvterm_id)) INNER JOIN project as c on (a.project_id=c.project_id))";
+    my $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+    my @results;
+    while (my ($projectprop_id, $project_name, $project_date, $project_prop) = $sth->fetchrow_array ) {
+	push(@results, {projectprop_id=>$projectprop_id, title=>$project_name, property=>$project_prop, start=>$project_date, save=>$project_date});
+    }
+
+    $q = "SELECT DISTINCT b.cvterm_id, b.name FROM (projectprop as a INNER JOIN cvterm as b on (a.type_id=b.cvterm_id))";
+    $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+    my @projectprop_types;
+    while (my ($cvterm_id, $cvterm_name) = $sth->fetchrow_array ) {
+	push(@projectprop_types, {cvterm_id=>$cvterm_id, cvterm_name=>$cvterm_name});
+    }
+
+    $q = "SELECT DISTINCT project_id, name FROM project";
+    $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute();
+    my @projects;
+    while (my ($project_id, $project_name) = $sth->fetchrow_array ) {
+	push(@projects, {project_id=>$project_id, project_name=>$project_name});
+    }
+
+    $c->stash->{projects} = \@projects;
+    $c->stash->{projectprop_types} = \@projectprop_types;
+    $c->stash->{project_dates_data} = \@results;
+    $c->stash->{template} = '/calendar/test_page.mas';
+}
 
 1;
