20c57059e4f402aa621ff90f50eac878280459fa
noefp <noeisneo@gmail.com>
2015-07-28 16:12:21 +0000

    blast table result width to 700px

--- lib/CXGN/Blast/Parse/Plugin/Table.pm
+++ lib/CXGN/Blast/Parse/Plugin/Table.pm
@@ -46,7 +46,7 @@ sub parse {
   open (my $table_fh, ">", $blast_table_file);
   open (my $table_html_fh, ">", $blast_table_html);
   
-  print $table_html_fh "<style>\n#blast_table_div {\nwidth:700px\noverflow: scroll;\nborder:solid #ccf 1px;\n}\n#blast_table {\ntext-align:right;\nwhite-space: nowrap;\npadding:5px\n}\n#blast_table td {\npadding-left: 5px;\n}\n.aln_l {\ntext-align:left;\n}\n</style>\n";
+  print $table_html_fh "<style>\n#blast_table_div {\nwidth:700px;\noverflow: scroll;\nborder:solid #ccf 1px;\n}\n#blast_table {\ntext-align:right;\nwhite-space: nowrap;\npadding:5px\n}\n#blast_table td {\npadding-left: 5px;\n}\n.aln_l {\ntext-align:left;\n}\n</style>\n";
   print $table_html_fh "<div id=\"blast_table_div\">\n<center><table id=\"blast_table\" border=\"0\"><tr>\n";
   print $table_html_fh "<tr><th>QueryId</th><th>SubjectId</th><th>id%</th><th>Aln</th><th>Mm</th><th>Gaps</th><th>qstart</th><th>qend</th><th>sstart</th><th>send</th><th>e-value</th><th>Score</th><th class=\"aln_l\">Description</th></tr>\n";
   
