27168854fcad5b3503e8f51c4f4c9e5872111f50
Isaak <iyt2@cornell.edu>
2015-08-04 12:00:38 +0200

    pass analysis output filearg to reporter module...

--- lib/SGN/Controller/solGS/AnalysisProfile.pm
+++ lib/SGN/Controller/solGS/AnalysisProfile.pm
@@ -7,7 +7,7 @@ use File::Spec::Functions qw / catfile catdir/;
 use File::Temp qw / tempfile tempdir /;
 use File::Slurp qw /write_file read_file :edit prepend_file/;
 use JSON;
-use solGS::AnalysisProfile;
+use solGS::AnalysisReport;
 use CXGN::Tools::Run;
 use Try::Tiny;
 
@@ -117,13 +117,17 @@ sub run_saved_analysis :Path('/solgs/run/saved/analysis/') Args(0) {
     $c->stash->{analysis_page} = $analysis_page;
    
     $self->run_analysis($c);
+    
+    my $output_file = $c->stash->{gebv_kinship_file};
 
-    my $file = '/export/prod/tmp/localhost/solgs/cache/gebv_kinship_PSH_143';
- 
+    print STDERR "\nanalysis output file: $output_file\n";
+
     $c->stash->{r_temp_file} = 'analysis-status';
     $c->controller('solGS::solGS')->create_cluster_acccesible_tmp_files($c);
-    my $out_temp_file = $c->stash->{out_temp_file};
-    
+    my $out_temp_file = $c->stash->{out_file_temp};
+    my $err_temp_file = $c->stash->{err_file_temp};
+
+    print STDERR "\n error file: $err_temp_file\n\n";
     $self->create_profiles_dir($c);
     my $temp_dir = $c->stash->{solgs_tempfiles_dir};
 
@@ -131,35 +135,35 @@ sub run_saved_analysis :Path('/solgs/run/saved/analysis/') Args(0) {
     { 
         my $r_process = CXGN::Tools::Run->run_cluster_perl({
            
-            method => ['solGS::AnalysisProfile' => 'check_process_status'],
-	    args => [$file],
-	    run_opts => {
-		out_file => $out_temp_file,
-                working_dir =>$temp_dir,
-		load_packages=>['solGS::AnalysisProfile', ],
-                max_cluster_jobs => 1_000_000_000,
+            method        => ["solGS::AnalysisReport" => "check_analysis_status"],
+    	    args          => [$output_file],
+    	    load_packages => ['solGS::AnalysisReport' ],
+    	    run_opts      => {
+    		              out_file    => $out_temp_file,
+			      err_file    => $err_temp_file,
+    		              working_dir => $temp_dir,
+			      max_cluster_jobs => 1_000_000_000,
+	    },
+	   });
 
-	    }},
-            );
     }
     catch 
     {
-         my $err = $_;
-         $err =~ s/\n at .+//s; 
+	my $err = $_;
+	$err =~ s/\n at .+//s; 
         
       	try
-        { 
+        {  
             $err .= "\n=== R output ===\n"
-		.file($out_temp_file)->slurp
-		."\n=== end R output ===\n"; 
-         };
-            
+    		.file($out_temp_file)->slurp
+    		."\n=== end R output ===\n"; 
+	};            
     };
-
-   
-    $self->update_analysis_progress($c);
-
-    $self->notify_user($c);
+ 
+   # $self->update_analysis_progress($c);
+   # $self->notify_user($c);
+  #  my $status_check = solGS::AnalysisProfile->new();
+ #   $status_check->check_process_status($file);
 } 
 
 
@@ -174,13 +178,12 @@ sub run_analysis {
     $analysis_page =~ s/$base/\//;
   
     $self->create_profiles_dir($c);
-   # $c->stash->{on_completion} = 'test on completion'; 
- 
+  
     $c->req->path($analysis_page);
     $c->prepare_action;
     $c->action ? $c->forward( $c->action ) : $c->dispatch;
     my @error = @{$c->error};
-     
+    
     if ($error[0]) 
     {
 	$c->stash->{status} = 'Error';
@@ -214,8 +217,18 @@ sub update_analysis_progress {
 
 }
 
+
 sub notify_user {
   my ($self, $c) = @_;
+
+  $c->stash->{email} = {
+      to => 'user@email.com',
+      cc => 'sgn-db-curation@sgn.cornell.edu',
+      subject => 'solGS: analysis status update',
+      body => 'links to output page'
+  };
+
+  $c->forward( $c->view('Email') );
   #email analysis completion/or error.
 }
 
@@ -238,6 +251,15 @@ sub analysis_profile_file {
 
 }
 
+
+sub analysis_result_file {
+    my ($self, $analysis_page) = @_;
+
+    
+
+
+
+}
 
 sub confirm_request :Path('/solgs/confirm/request/') Args(0) {
     my ($self, $c) = @_;
