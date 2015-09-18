278af974adb9f8ae0ceb0ade4441206325db2ae7
Isaak <iyt2@cornell.edu>
2015-09-18 17:46:36 +0200

    typo fix and cleanup...

--- js/solGS/solGS.js
+++ js/solGS/solGS.js
@@ -278,7 +278,7 @@ function getProfileForm (args) {
 	+'<table>'
 	+  '<tr>'
      	+  '<td>name:</td>'
-     	+  '<td><input type="text" name="user_email" id="user_email" value=\"' + userName + '\"/></td>' 
+     	+  '<td><input type="text" name="user_name" id="user_name" value=\"' + userName + '\"/></td>' 
      	+  '</tr>'
 	+  '<tr>'
 	+  '<td>analysis name:</td>'
@@ -340,10 +340,6 @@ jQuery(document).ready(function (){
 
 function saveAnalysisProfile (profile) {
     
-    //make an ajax request
-    //save analysis details in a file
-    //run analysis, when complete, retrieve analysis owner and send an email
-   
     jQuery.ajax({
 	type    : 'POST',
 	dataType: 'json',
@@ -427,101 +423,4 @@ jQuery.fn.alternateFunctions = function(a, b) {
         });
     });
 };
-//
 
-
-
- //  $(function() {
-//     var dialog, form,
- 
-//       // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-//       emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-//       name = $( "#name" ),
-//       email = $( "#email" ),
-//       password = $( "#password" ),
-//       allFields = $( [] ).add( name ).add( email ).add( password ),
-//       tips = $( ".validateTips" );
- 
-//     function updateTips( t ) {
-//       tips
-//         .text( t )
-//         .addClass( "ui-state-highlight" );
-//       setTimeout(function() {
-//         tips.removeClass( "ui-state-highlight", 1500 );
-//       }, 500 );
-//     }
- 
-//     function checkLength( o, n, min, max ) {
-//       if ( o.val().length > max || o.val().length < min ) {
-//         o.addClass( "ui-state-error" );
-//         updateTips( "Length of " + n + " must be between " +
-//           min + " and " + max + "." );
-//         return false;
-//       } else {
-//         return true;
-//       }
-//     }
- 
-//     function checkRegexp( o, regexp, n ) {
-//       if ( !( regexp.test( o.val() ) ) ) {
-//         o.addClass( "ui-state-error" );
-//         updateTips( n );
-//         return false;
-//       } else {
-//         return true;
-//       }
-//     }
- 
-//     function addUser() {
-//       var valid = true;
-//       allFields.removeClass( "ui-state-error" );
- 
-//       valid = valid && checkLength( name, "username", 3, 16 );
-//       valid = valid && checkLength( email, "email", 6, 80 );
-//       valid = valid && checkLength( password, "password", 5, 16 );
- 
-//       valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-//       valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-//       valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
- 
-//       if ( valid ) {
-//         $( "#users tbody" ).append( "<tr>" +
-//           "<td>" + name.val() + "</td>" +
-//           "<td>" + email.val() + "</td>" +
-//           "<td>" + password.val() + "</td>" +
-//         "</tr>" );
-//         dialog.dialog( "close" );
-//       }
-//       return valid;
-//     }
- 
-//     dialog = $( "#dialog-form" ).dialog({
-//       autoOpen: false,
-//       height: 300,
-//       width: 350,
-//       modal: true,
-//       buttons: {
-//         "Create an account": addUser,
-//         Cancel: function() {
-//           dialog.dialog( "close" );
-//         }
-//       },
-//       close: function() {
-//         form[ 0 ].reset();
-//         allFields.removeClass( "ui-state-error" );
-//       }
-//     });
- 
-//     form = dialog.find( "form" ).on( "submit", function( event ) {
-//       event.preventDefault();
-//       addUser();
-//     });
- 
-//     $( "#create-user" ).button().on( "click", function() {
-//       dialog.dialog( "open" );
-//     });
-//   });
-//   </script>
-// </head>
-// <body>
- 
