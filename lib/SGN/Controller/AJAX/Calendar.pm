88bc323a13ca83ddbe3b1c8363c7c36808996d3d
nmorales3142 <nmorales3142@gmail.com>
2015-09-21 16:11:29 +0000

    calendar shows trials and projects only associated with a user's role

--- lib/SGN/Controller/AJAX/Calendar.pm
+++ lib/SGN/Controller/AJAX/Calendar.pm
@@ -29,6 +29,7 @@ use JSON;
 use Time::Piece;
 use Time::Seconds;
 use Data::Dumper;
+use CXGN::Login;
 
 BEGIN { extends 'Catalyst::Controller::REST' }
 
@@ -60,8 +61,19 @@ sub calendar_events_agendaWeek_GET {
     $c->stash->{rest} = populate_calendar_events($search_rs, $view);
 }
 
+sub calendar_events_month_role  : Path('/ajax/calendar/populate/month/role') : ActionClass('REST') { }
+
+#When the month view of the calendar is loaded and when controls (such as next month or year) are used, this function is called to get date data.
+sub calendar_events_month_role_GET { 
+    my $self = shift;
+    my $c = shift;
+    my $search_rs = get_calendar_events_role($c);
+    my $view = 'month';
+    $c->stash->{rest} = populate_calendar_events($search_rs, $view);
+}
+
+
 sub get_calendar_events {
-    #cvterm names of interest:  "project year", "project fertilizer date", "project planting date"
     #Calendar event info is retrieved using DBIx class.
     my $c = shift;
     my $schema = $c->dbic_schema('Bio::Chado::Schema');
@@ -75,6 +87,64 @@ sub get_calendar_events {
 	}
     );
     #$schema->storage->debug(1);
+    return $search_rs;
+}
+
+sub get_calendar_events_role {
+    my $c = shift;
+    my $person_id = CXGN::Login->new($c->dbc->dbh)->verify_session();
+
+    my @roles;
+    my $q = "SELECT sgn_people.sp_roles.name FROM sgn_people.sp_person JOIN sgn_people.sp_person_roles using(sp_person_id) join sgn_people.sp_roles using(sp_role_id) WHERE sp_person_id=?";
+    my $sth = $c->dbc->dbh->prepare($q);
+    $sth->execute($person_id);
+    while (my ($role) = $sth->fetchrow_array ) {
+	push(@roles, $role);
+    }
+
+    my @allowed_roles = ('IITA','NRCRI','NaCRRI','ARI Tanzania','NaCRRI Germplasm Collection','CIAT','CARI','Rayong','KU','CSIR','TestBootstrap2');
+    my %allowed_roles_hash = map { $_ => 1 } @allowed_roles;
+
+    my @search_project_ids = '-1';
+    foreach (@roles) {
+	if(exists($allowed_roles_hash{$_})) {
+	    
+	    my $q="SELECT project_id FROM project WHERE name=?";
+	    my $sth = $c->dbc->dbh->prepare($q);
+	    $sth->execute($_);
+	    while (my ($project_id) = $sth->fetchrow_array ) {
+
+		push(@search_project_ids, $project_id);
+
+		my $q="SELECT subject_project_id FROM project_relationship JOIN cvterm ON (type_id=cvterm_id) WHERE object_project_id=? and cvterm.name='breeding_program_trial_relationship'";
+		my $sth = $c->dbc->dbh->prepare($q);
+		$sth->execute($project_id);
+		while (my ($trial) = $sth->fetchrow_array ) {
+		    push(@search_project_ids, $trial);
+		}
+	    }
+	    
+	    print STDERR @search_project_ids;
+	}
+    }
+
+    my @search_project_ids = map{$_='me.project_id='.$_; $_} @search_project_ids;
+    my $search_projects = join(" OR ", @search_project_ids);
+
+    #Calendar event info is retrieved using DBIx class.
+    my $schema = $c->dbic_schema('Bio::Chado::Schema');
+
+    my $search_rs = $schema->resultset('Project::Project')->search(
+	undef,
+	{join=>{'projectprops'=>{'type'=>'cv'}},
+	'+select'=> ['projectprops.projectprop_id', 'type.name', 'projectprops.value', 'type.cvterm_id'],
+	'+as'=> ['pp_id', 'cv_name', 'pp_value', 'cv_id'],
+	}
+    );
+
+    $search_rs = $search_rs->search([$search_projects]);
+
+    $schema->storage->debug(1);
     return $search_rs;
 }
 
@@ -138,9 +208,14 @@ sub populate_calendar_events {
 sub check_value_format {
     #Check if value is in the {"2015-08-01T00:00:00","2015-08-01T00:00:00","description","url"} format.
     my $value = shift;
-    if ($value =~ /^{"\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d","\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d","/) {
-	return 1;
-    } else {
+    if ($value) {
+	if ($value =~ /^{"\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d","\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d","/) {
+	    return 1;
+	} else {
+	    return -1;
+	}
+    }
+    else {
 	return -1;
     }
 }
@@ -376,25 +451,18 @@ sub edit_event_POST {
     if ($description eq '') {$description = 'N/A';}
     if ($url eq '') {$url = '#';} elsif ($url eq '#') {$url = '#';} else {$url = 'http://www.'.$url;}
 
-    #Check if the projectprop unique (project_id, type_id, rank) constraint will cause the insert to fail.
     my $schema = $c->dbic_schema('Bio::Chado::Schema');
-    my $result_set = $schema->resultset('Project::Projectprop');
-    my $count = $result_set->search({project_id=>$project_id, type_id=>$cvterm_id, rank=>0})->count;
-    if ($count == 0) {
-	$schema->storage->txn_begin;
-	if (my $update_rs = $schema->resultset('Project::Projectprop')->find({projectprop_id=>$projectprop_id}, columns=>['project_id', 'type_id', 'value'])->update({project_id=>$project_id, type_id=>$cvterm_id, value=>[$start, $end, $description, $url]})) {
+    $schema->storage->txn_begin;
+    if (my $update_rs = $schema->resultset('Project::Projectprop')->find({projectprop_id=>$projectprop_id}, columns=>['project_id', 'type_id', 'value'])->update({project_id=>$project_id, type_id=>$cvterm_id, value=>[$start, $end, $description, $url]})) {
 	
-	    #The transaction changes are commited.
-	    $schema->storage->txn_commit;
-	    $c->stash->{rest} = {status => 1,};
-	} else {
-
-	    #The transaction is rolled back.
-	    $schema->storage->txn_rollback;
-	    $c->stash->{rest} = {error => 1,};
-	}
+	#The transaction changes are commited.
+	$schema->storage->txn_commit;
+	$c->stash->{rest} = {status => 1,};
     } else {
-	$c->stash->{rest} = {status => 0,};
+
+	#The transaction is rolled back.
+	$schema->storage->txn_rollback;
+	$c->stash->{rest} = {error => 1,};
     }
 }
 
