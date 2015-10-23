0329ee7fc996d5bb2ede2d88174a7e4a6ae591b1
bellerbrock <bje24@cornell.edu>
2015-10-23 14:23:08 +0000

    add script to create trial jbrowses

--- bin/jbrowse_vcf_tools/gen_trial_jbrowse.pl
+++ bin/jbrowse_vcf_tools/gen_trial_jbrowse.pl
@@ -2,20 +2,21 @@
 
 =head1
 
-gen_accession_jbrowse.pl - creates jbrowse instances that include base tracks and accession vcf tracks for each accession name supplied
+gen_trial_jbrowse.pl - creates jbrowse instances that include base tracks and accession vcf tracks for each trial name supplied
 
 =head1 SYNOPSIS
 
-    gen_accession_jbrowse.pl -v [absolute path through base instance to dir containing vcfs] -n [list of accession names]
+    gen_trial_jbrowse.pl -v [absolute path through base instance to dir containing vcfs] -n [list of trial names]
 
 =head1 COMMAND-LINE OPTIONS
  
  -v absolute path through base instance to dir containing vcfs
- -n list of accession names 
+ -n list of trial names 
 
 =head1 DESCRIPTION
 
-Takes a directory containing individual vcf files, including filtered and imputed versions, and creates jbrowse instances by symlinking to all necessary files in a base instance,  then generating a uniq tracks.conf file and appending dataset info to jbrowse.conf. Should be run from /data/json/accessions dir in jbrowse instance.
+Takes a directory containing individual vcf files, including filtered and imputed versions, and creates jbrowse instances by symlinking to all necessary files in a base instance,  then generating a uniq tracks.conf file and appending dataset info to jbrowse.conf. Should be run from /data/json/trials dir in jbrowse instance
+
 =head1 AUTHOR
 
 Bryan Ellerbrock (bje24@cornell.edu) - Oct 2015
@@ -27,10 +28,6 @@ use File::Slurp;
 use Getopt::Std;
 
 our ($opt_v, $opt_n);
-
-#-----------------------------------------------------------------------
-# define paths & array of vcf_file names. Open file handles to read accession lists and append datasets to jbrowse.conf 
-#-----------------------------------------------------------------------
 
 getopts('v:n:');
 
@@ -46,10 +43,6 @@ my @files = ` dir -GD -1 --hide *.tbi $vcf_dir_path ` ;
 open (CONF, ">>", "../../../jbrowse.conf") || die "Can't open conf file jbrowse.conf!\n";
 open (NAMES, "<", $names) || die "Can't open names file $names! \n";
 
-#-----------------------------------------------------------------------
-# for each accession name, locate matching indiv vcf files and construct necessary text for tracks.conf
-#-----------------------------------------------------------------------
-
 while (<NAMES>) {
     chomp (my $name = $_);
     $out = "$name/tracks.conf";
@@ -63,7 +56,7 @@ while (<NAMES>) {
         #next if !m/$name/;
 	$filt_test = $name . "_filtered";
 	$imp_test = $name . "_imputed";
-	if ($_ eq $filt_test) {                            #Handle filtered vcf files
+	if ($_ eq $filt_test) {
 	    print STDERR "File = $file \n" ;
 	    my $path = $url_path . "/" . $file ; 
 	    $mod_file = $file;
@@ -94,7 +87,7 @@ type = JBrowse/View/Track/HTMLVariants
 metadata.Description = Homozygous reference: Green	Heterozygous: Red		Homozygous alternate: Blue	Filtered to remove SNPs with a depth of 0 and SNPs with 2 or more alt alleles.
 label = ' . $key  . '
 ' ;
-	} elsif ($_ eq $imp_test) {                        #Handle imputed vcf files
+	} elsif ($_ eq $imp_test) {
 	    print STDERR "File = $file \n" ;
 	    my $path = $url_path . "/" . $file ; 
 	    $mod_file = $file;
@@ -141,28 +134,14 @@ label = ' . $key  . '
 	}
 }
 
-#-----------------------------------------------------------------------
-# if matching vcf files not found, skip to next accession name
-#-----------------------------------------------------------------------
-
-next if !$track_2;    
-
-#-----------------------------------------------------------------------
-# create dir for new jbrowse instance; create symlinks to files in base jbrowse instance
-#-----------------------------------------------------------------------
-
+next if !$track_2;
     `sudo rm -r $name; sudo mkdir $name`;
     `sudo ln -sf $link_path/data_files $name/data_files`;
     `sudo ln -sf $link_path/seq $name/seq`;
     `sudo ln -sf $link_path/tracks $name/tracks`;
     `sudo ln -sf $link_path/trackList.json $name/trackList.json`;
     `sudo ln -sf $link_path/readme $name/readme`;
-
-#-----------------------------------------------------------------------
-# append dataset info to jbrowse.conf, and create and populate accession specific tracks.conf file
-#-----------------------------------------------------------------------
-
-my $dataset = "[datasets.$name]\n" . "url  = ?data=data/json/accessions/$name\n" . "name = $name\n\n";
+my $dataset = "[datasets.$name]\n" . "url  = ?data=data/json/trials/$name\n" . "name = $name\n\n";
 print CONF $dataset;
     open (OUT, ">", $out) || die "Can't open out file $out! \n";
     print OUT $header;
