66a631d7dd879a594ed59749bc47583d28672760
Isaak <iyt2@cornell.edu>
2015-11-18 16:45:47 +0100

    make population data download notifications work..

--- lib/solGS/AnalysisReport.pm
+++ lib/solGS/AnalysisReport.pm
@@ -22,10 +22,10 @@ sub check_success {
     my ($self, $output_details) = @_;
     
     my $analysis_profile = $output_details->{analysis_profile};
-    
+  
     if ( $analysis_profile->{analysis_type} =~ /population download/ ) 
     {	
-	$self->check_population_download($output_details);
+	$output_details = $self->check_population_download($output_details);
     }
     elsif ( $analysis_profile->{analysis_type} =~ /(single|multiple) model/ )
     {
@@ -188,75 +188,78 @@ sub check_population_download {
     my ($self, $output_details) = @_;
 
     my $job_tempdir = $output_details->{r_job_tempdir};
-          
+    
     foreach my $k (keys %{$output_details})
     {
 	my $pheno_file;
 	my $geno_file;
+
 	if (ref $output_details->{$k} eq 'HASH') 
-	{
-	   $pheno_file = $output_details->{$k}->{phenotype_file}; 
-	   $geno_file  = $output_details->{$k}->{genotype_file}; 
-	} 
-	
-	if ($pheno_file && $geno_file) 
-	{
-	    my $pheno_size;
-	    my $geno_size;
-	    my $died_file;
-	
-	    while (1) 
-	    {
-		sleep 5;
-		$pheno_size = -s $pheno_file;
-		$geno_size  = -s $geno_file;
-		
-		if ($pheno_size) 
-		{
-		    $output_details->{$k}->{pheno_success} = 1;		
-		}
+	{ 
+	    if ($output_details->{$k}->{population_id})
+	    { 
+		$pheno_file = $output_details->{$k}->{phenotype_file}; 
+		$geno_file  = $output_details->{$k}->{genotype_file}; 
 
-		if ($geno_size) 
+		if ($pheno_file && $geno_file) 
 		{
-		    $output_details->{$k}->{geno_success} = 1;		
-		}
-
-		if ($pheno_size && $geno_size) 
-		{		    
-		   $output_details->{$k}->{success} = 1;
-		   last;
-		}
-		
-		else
-		{
-		    if ($job_tempdir) 
+		    my $pheno_size;
+		    my $geno_size;
+		    my $died_file;
+	
+		    while (1) 
 		    {
-			$died_file = $self->get_file($job_tempdir, 'died');
-			if ($died_file) 
+			sleep 5;
+			$pheno_size = -s $pheno_file;
+			$geno_size  = -s $geno_file;
+
+			unless (!$pheno_size) 
 			{
-			    $output_details->{$k}->{pheno_success}   = 0;
-			    $output_details->{$k}->{geno_success}    = 0;
-			    $output_details->{$k}->{success} = 0;
-			    last;
+			    $output_details->{$k}->{pheno_success} = 1;	
 			}
+
+			unless (!$geno_size) 
+			{
+			    $output_details->{$k}->{geno_success} = 1;		
+			}
+
+			if ($pheno_size && $geno_size) 
+			{		    
+			    $output_details->{$k}->{success} = 1;
+			    last;
+			}		
+			else
+			{
+			    if ($job_tempdir) 
+			    {
+				$died_file = $self->get_file($job_tempdir, 'died');
+				if ($died_file) 
+				{
+				    $output_details->{$k}->{pheno_success}   = 0;
+				    $output_details->{$k}->{geno_success}    = 0;
+				    $output_details->{$k}->{success} = 0;
+				    last;
+				}
+			    }
+			}	    
+		    }	   	    
+		}
+		else 
+		{  
+		    if (ref $output_details->{$k} eq 'HASH')	
+		    {   
+			$output_details->{$k}->{pheno_success}   = 0;
+			$output_details->{$k}->{geno_success}    = 0;
+			$output_details->{$k}->{success} = 0;
 		    }
-		}	    
-	    }	   	    
+		} 
+	    } 
 	}
-	else 
-	{  
-	    if (ref $output_details->{$k} eq 'HASH')	
-	    {   
-		$output_details->{$k}->{pheno_success}   = 0;
-		$output_details->{$k}->{geno_success}    = 0;
-		$output_details->{$k}->{success} = 0;
-	    }
-	}  
     }
-
+    
     return $output_details;
+    
 }
-
 
 
 sub get_file {
@@ -405,10 +408,13 @@ sub population_download_message {
     my ($self, $output_details) = @_;
     
     my $message;
+     
     foreach my $k (keys %{$output_details}) 
     {
 	if (ref $output_details->{$k} eq 'HASH')
 	{
+	     if ($output_details->{$k}->{population_id})
+	    {
 		my $pop_name = uc($output_details->{$k}->{population_name});
 		my $pop_page = $output_details->{$k}->{population_page};
 		
@@ -423,10 +429,11 @@ sub population_download_message {
 		    $message = "Downloading phenotype and genotype data for $pop_name failed."
 			."\nWe are troubleshooting the cause. " 
 			. "We will contact you when we find out more.";	 
-		}		
+		}
+	    }		
 	}
     }
-
+  
     return  $message;
 }
 
