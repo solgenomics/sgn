db7e1bf6d3103157f8441a692f112aaa67f29fd0
bellerbrock <bje24@cornell.edu>
2015-12-02 16:02:48 -0500

    rework get_genotyping_protocols_select method to better handle locally defined default and case of no protocols in the database

--- lib/SGN/Controller/AJAX/HTMLSelect.pm
+++ lib/SGN/Controller/AJAX/HTMLSelect.pm
@@ -97,23 +97,28 @@ sub get_genotyping_protocols_select : Path('/ajax/html/select/genotyping_protoco
     my $id = $c->req->param("id") || "gtp_select";
     my $name = $c->req->param("name") || "genotyping_protocol_select";
     my $empty = $c->req->param("empty") || "";
-    
+    my $default_gtp;
+    my %gtps;
+
     my $gt_protocols = CXGN::BreedersToolbox::Projects->new( { schema => $c->dbic_schema("Bio::Chado::Schema") } )->get_gt_protocols();
 
-    my $default_gtp = $c->config->{default_genotyping_protocol};
-    my %gtps = map { @$_[1] => @$_[0] } @$gt_protocols;
+    if (@$gt_protocols) {
+	$default_gtp = $c->config->{default_genotyping_protocol};
+	%gtps = map { @$_[1] => @$_[0] } @$gt_protocols;
 
-    if(!exists($gtps{$default_gtp})) {
-	die "The default genotyping protocol: \"$default_gtp\" in sgn_local.conf doesn't match any protocols in the database";
+	if(!exists($gtps{$default_gtp}) && !($default_gtp =~ /^none$/)) {
+	    die "The conf variable default_genotyping_protocol: \"$default_gtp\" does not match any protocols in the database. Set it in sgn_local.conf using a protocol name from the nd_protocol table, or set it to 'none' to silence this error.";
+	}
+    } else {
+	$gt_protocols = ["No genotyping protocols found"];
     }
-
-    my $html = simple_selectbox_html(
-	name => $name,
-	id => $id,
-	choices => $gt_protocols,
-	selected => $gtps{$default_gtp} 
-	);
-    $c->stash->{rest} = { select => $html };
+	my $html = simple_selectbox_html(
+	    name => $name,
+	    id => $id,
+	    choices => $gt_protocols,
+	    selected => $gtps{$default_gtp} 
+	    );
+	$c->stash->{rest} = { select => $html };
 }
 
 1;
