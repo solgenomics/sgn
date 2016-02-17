60ed54e6a93855c63bbc9a14d4c83fd40060c134
noefp <noeisneo@gmail.com>
2016-02-17 17:28:49 -0500

    singleton unigene ids on blast

--- lib/CXGN/Blast/SeqQuery/Plugin/Unigene.pm
+++ lib/CXGN/Blast/SeqQuery/Plugin/Unigene.pm
@@ -46,21 +46,37 @@ sub process {
 
     my $dbh = $c->dbc->dbh();
 
-    my $query = "SELECT unigene_id, unigene_consensi.seq FROM sgn.unigene JOIN sgn.unigene_consensi using(consensi_id) WHERE unigene_id=?";
+    my $query = "SELECT unigene_id, nr_members, unigene_consensi.seq FROM sgn.unigene JOIN sgn.unigene_consensi using(consensi_id) WHERE unigene_id=?";
     my $h = $dbh->prepare($query);
     
     my @seqs = ();
-    foreach my $id (@ids) { 
-	my $numeric_id = $id;
-	$numeric_id=~s/\D//g;
-	$h->execute($numeric_id);
-	if (my ($unigene_id, $seq) = $h->fetchrow_array()) { 
-	    
-	    push @seqs, ">".$id."\n".$seq;
-	}
-	else { 
-	    	    die "ID $id does not exist!";
-	}
+    foreach my $id (@ids) {
+      my $numeric_id = $id;
+      $numeric_id=~s/\D//g;
+      $h->execute($numeric_id);
+      
+      if (my ($unigene_id, $nr_members, $seq) = $h->fetchrow_array()) {
+      
+        if ($nr_members > 1) {
+          push @seqs, ">".$id."\n".$seq;
+        }
+        elsif ($nr_members == 1) {
+          my $query = "SELECT unigene_id, seq FROM sgn.unigene_member JOIN sgn.est using(est_id) WHERE unigene_id=?";
+          my $h = $dbh->prepare($query);
+          $h->execute($numeric_id);
+          
+          if (my ($unigene_id, $seq) = $h->fetchrow_array()) {
+            push @seqs, ">".$id."\n".$seq;
+          }
+          else { 
+            die "Unigene $id singleton could not be found!";
+          }
+          
+        }
+      }
+      else { 
+        die "ID $id does not exist!";
+      }
     }
     my $sequence =  join "\n", @seqs;
     print STDERR "SEQUENCE = $sequence\n";
