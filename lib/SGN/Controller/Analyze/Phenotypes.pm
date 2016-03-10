03ce696ff120704b23f61738ec04ae2f2ee33108
bellerbrock <bje24@cornell.edu>
2016-03-10 15:34:49 -0500

    fleshing out analyze phenotypes page

--- lib/SGN/Controller/Analyze/Phenotypes.pm
+++ lib/SGN/Controller/Analyze/Phenotypes.pm
@@ -12,131 +12,89 @@ use Data::Dumper;
 use CXGN::Trial;
 use CXGN::Trial::TrialLayout;
 use CXGN::Trial::Download;
+use CXGN::BreederSearch;
 use POSIX qw(strftime);
 
-sub load_analyze_phenotypes : Path('/analyze/phenotypes') Args(0) { 
+sub load_analyze_phenotypes : Path('/analyze/phenotypes') Args(0) {
     my $self = shift;
     my $c = shift;
 
-    if (!$c->user()) { 	
-	# redirect to login page
-	$c->res->redirect( uri( path => '/solpeople/login.pl', query => { goto_url => $c->req->uri->path_query } ) ); 
-	return;
-    }
-    
     $c->stash->{template} = '/analyze/phenotypes.mas';
 }
 
-sub analyze_phenotypes_action : Path('/analyze/phenotypes/action') Args(0) { 
-    my $self = shift;
-    my $c = shift;
+sub analyze_trial_phenotypes : Path('/analyze/phenotypes/trials') Args(1) {
+  my $self = shift;
+  my $c = shift;
+  my $trial_ids = shift;
+  my $design = $c->req->param("design");
+  my $analysis_type = $c->req->param("analysis_type");
 
-    my $accession_list_id = $c->req->param("accession_list_list_select");
-    my $trial_list_id     = $c->req->param("trial_list_list_select");
-    my $trait_list_id     = $c->req->param("trait_list_list_select");
-    my $format            = $c->req->param("format");
-    my $cookie_value      = $c->req->param("analyze_token_value"); 
+  $self->trial_download_log($c, $trial_ids, "trial analyze phenotypes");
 
-    my $accession_data;
-    if ($accession_list_id) { 
-	$accession_data = SGN::Controller::AJAX::List->retrieve_list($c, $accession_list_id);
-    }
+  my @trial_ids = split ",", $trial_ids;
+  my $trial_sql = join ",", map { "\'$_\'" } @trial_ids;
 
-    my $trial_data;
-    if ($trial_list_id) { 
-	$trial_data = SGN::Controller::AJAX::List->retrieve_list($c, $trial_list_id);
-    }
+  my $bs = CXGN::BreederSearch->new( { dbh=>$c->dbc->dbh() });
+  my @data = $bs->get_extended_phenotype_info_matrix(undef,$trial_sql, undef);
+  my $schema = $c->dbic_schema("Bio::Chado::Schema");
 
-    my $trait_data;
-    if ($trait_list_id) { 
-	$trait_data = SGN::Controller::AJAX::List->retrieve_list($c, $trait_list_id);
-    }
+  my $csv = $self->retrieve_phenotypes_csv($c, '', '', '', '', \@data);
 
-    my @accession_list = map { $_->[1] } @$accession_data;
-    my @trial_list = map { $_->[1] } @$trial_data;
-    my @trait_list = map { $_->[1] } @$trait_data;
+  print STDERR "path to temp file that R will analyze is: $csv \n";
 
-    my $tf = CXGN::List::Transform->new();
-    
-    my $unique_transform = $tf->can_transform("accession_synonyms", "accession_names");
-    
-    my $unique_list = $tf->transform($c->dbic_schema("Bio::Chado::Schema"), $unique_transform, \@accession_list);
-    
-    # get array ref out of hash ref so Transform/Plugins can use it
-    my %unique_hash = %$unique_list;
-    my $unique_accessions = $unique_hash{transform};
+  my $time_stamp = strftime "%Y-%m-%dT%H%M%S", localtime();
+  my $dir = $c->tempfiles_subdir('analyze'); # make sure analyze dir exists
+  my $file_name = $time_stamp . $analysis_type. "XXXX";
+  my $relative_path = $c->tempfile( TEMPLATE => "analyze/$file_name");
+  my $output_path = $c->config->{basepath}."/".$relative_path;
 
-    my $bs = CXGN::BreederSearch->new( { dbh=>$c->dbc->dbh() });
+  print STDERR "path to R output is: $output_path \n";
 
-    my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
-    my $t = CXGN::List::Transform->new();
-    
-    my $acc_t = $t->can_transform("accessions", "accession_ids");
-    my $accession_id_data = $t->transform($schema, $acc_t, $unique_accessions);
+  # run different R scripts based on supplied analysis and design type
+  #if ($analysis_type eq 'ANOVA') {
+  #   if ($design eq 'RCBD') {
+       #run analysis here, with CXGN:Tools Run
+  #   }
+  #}
 
-    my $trial_t = $t->can_transform("trials", "trial_ids");
-    my $trial_id_data = $t->transform($schema, $trial_t, \@trial_list);
-    
-    my $trait_t = $t->can_transform("traits", "trait_ids");
-    my $trait_id_data = $t->transform($schema, $trait_t, \@trait_list);
+  #just temporarily till R output is working
+  $output_path = '/home/bje24/cxgn/sgn/static/documents/Rplot.pdf';
 
-    my $accession_sql = join ",", map { "\'$_\'" } @{$accession_id_data->{transform}};
-    my $trial_sql = join ",", map { "\'$_\'" } @{$trial_id_data->{transform}};
-    my $trait_sql = join ",", map { "\'$_\'" } @{$trait_id_data->{transform}};
-
-    my $output = "";
-    my $result = $bs->get_phenotype_info($accession_sql, $trial_sql, $trait_sql);
-    my @data = @$result;
-
-	    # if xls or csv, create tempfile name and place to save it
-	    my $what = "analyze_phenotype";
-	    my $time_stamp = strftime "%Y-%m-%dT%H%M%S", localtime();
-	    my $dir = $c->tempfiles_subdir('analyze');
-	    my $temp_file_name = $time_stamp . "$what" . "XXXX";
-	    my $rel_file = $c->tempfile( TEMPLATE => "analyze/$temp_file_name");
-	    my $tempfile = $c->config->{basepath}."/".$rel_file;
-	    my @col_names = qw/year project_name stock_name location trait value plot_name cv_name cvterm_accession rep block_number/;
-	    
-	    if ($format eq ".csv") {
-		
-		#build csv with column names
-		open(CSV, ">", $tempfile) || die "Can't open file $tempfile\n";
-		print CSV join(",", @col_names);
-		print CSV "\n"; 
-		for (my $line =0; $line< @data; $line++) { 
-		    my @columns = @{$data[$line]};
-		    print CSV join(",", @columns);
-		    print CSV "\n";
-		}
-		close CSV;
-		
-	    } else {
-		
-		#build excel file; include column names
-		my $ss = Spreadsheet::WriteExcel->new($tempfile);
-		my $ws = $ss->add_worksheet();
-		
-		for (my $column =0; $column< @col_names; $column++) {
-		    $ws->write(0, $column, $col_names[$column]);
-		}
-		for (my $line =0; $line < @data; $line++) {
-		    my @columns = @{$data[$line]};
-		    for(my $col = 0; $col<@columns; $col++) { 
-			$ws->write(($line+1), $col, $columns[$col]);
-		    }
-		}
-		$ss ->close();
-		$format = ".xls";
-	    }
-	    
-	    #Using tempfile and new filename,send file to client
-	    my $file_name = $time_stamp . "$what" . "$format";
-	    $c->res->content_type('Application/'.$format);
-	    $c->res->cookies->{fileDownloadToken} = { value => $cookie_value};
-	    $c->res->header('Content-Disposition', qq[attachment; filename="$file_name"]);
-	    $output = read_file($tempfile);
-	    $c->res->body($output);   
+  # when analysis is complete, display output graphic
+  if ( -e $output_path) {
+    return "<img src=\"$output_path\" /><br>";
+  }
+  else {
+    return "Error in R script R/Anova.R<br>\n";
+  }
 }
 
+  sub retrieve_phenotypes_csv {
+      my $self = shift;
+      my $c = shift;
+      my $trial_id = shift;
+      my $program_name = shift;
+      my $location = shift;
+      my $year = shift;
+      my $dataref = shift;
+      my @data = @$dataref;
+
+      my ($fh, $tempfile) = $c->tempfile(TEMPLATE=>"data_export/trial_".$program_name."_phenotypes_".$location."_".$trial_id."_XXXXX");
+
+      close($fh);
+      my $file_path = $c->config->{basepath}."/".$tempfile.".csv";
+      move($tempfile, $file_path);
+
+      open(my $F, ">", $file_path) || die "Can't open file $file_path\n";
+      for (my $line =0; $line< @data; $line++) {
+  	my @columns = split /\t/, $data[$line];
+
+  	print $F join(",", @columns);
+  	print $F "\n";
+      }
+
+      close($F);
+      return $file_path;
+  }
 
 1;
