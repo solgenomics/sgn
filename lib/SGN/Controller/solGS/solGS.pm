145e0256ba90326de1360825f63fdd45625fdbeb
Isaak <iyt2@cornell.edu>
2016-04-04 11:23:11 +0200

    add db patch for granting permission to web_usr to matviews..

--- lib/SGN/Controller/solGS/solGS.pm
+++ lib/SGN/Controller/solGS/solGS.pm
@@ -4034,15 +4034,20 @@ sub all_gs_traits_list {
 
     $self->trial_compatibility_file($c);
     my $file = $c->stash->{trial_compatibility_file};
-   
+    
     my $traits;
-    my $matview = $c->model('solGS::solGS')->check_matview_exists('all_gs_traits');
+
+    my $mv_name = 'all_gs_traits';
+    my $matview = $c->model('solGS::solGS')->check_matview_exists($mv_name);
    
+    if (!$matview)
+    {
+        $c->model('solGS::solGS')->materialized_view_all_gs_traits();
+	$c->model('solGS::solGS')->insert_matview_public($mv_name);
+    }
+    
     if (!-s $file) 
     {
-	my $mv_name = 'all_gs_traits';
-	my $matview = $c->model('solGS::solGS')->check_matview_exists($mv_name);
-
 	if ($matview)
 	{
 	    $c->model('solGS::solGS')->refresh_materialized_view_all_gs_traits();
