da2ea570484a7dc0c3edbcc34ef7602b150fc821
nickmorales <nmorales3142@gmail.com>
2016-04-25 15:15:45 -0400

    change trial and folder names

--- js/CXGN/Trial.js
+++ js/CXGN/Trial.js
@@ -557,29 +557,6 @@ function trial_detail_page_setup_dialogs() {
 	}
     });
 
-    jQuery('#edit_trial_name').click( function () {
-	jQuery('#edit_trial_name_dialog').dialog("open");
-
-    });
-
-    jQuery('#edit_trial_name_dialog').dialog( {
-	autoOpen: false,
-	height: 200,
-	width: 300,
-	modal: true,
-	title: "Change trial name",
-	buttons: {
-	    cancel: { text: "Cancel",
-                      click: function() { jQuery( this ).dialog("close"); },
-                      id: "edit_name_cancel_button"
-		    },
-	    save:   { text: "Save",
-                      click: function() { save_trial_name(); },
-                      id: "edit_name_save_button"
-		    }
-	}
-    });
-
     jQuery('#change_trial_location_dialog').dialog( {
 	autoOpen: false,
 	height: 200,
@@ -606,50 +583,6 @@ function trial_detail_page_setup_dialogs() {
 
 }
 
-function display_trial_name(trial_id) {
-    jQuery.ajax( {
-	url: '/ajax/breeders/trial/'+trial_id+'/names',
-	success: function(response) {
-            if (response.error) {
-		jQuery('#trial_name').html('[ An error occurred fetching the trial name ]');
-	    }
-            else {
-		jQuery('#trial_name').html(response.names);
-		jQuery('#trial_name_input').html(response.names);
-            }
-	},
-	error: function(response) {
-	    jQuery('#trial_name').html('[ A protocol error occurred ]');
-	}
-    });
-}
-
-
-function save_trial_name(names) {
-	var trial_id = parseInt(jQuery('#trialIDDiv').text());
-	//var trial_id = get_trial_id();
-	var names = jQuery('#trial_name_input').val();
-	alert('New name = '+names);
-	jQuery.ajax( {
-		url: '/ajax/breeders/trial/'+trial_id+'/names/',
-		type: 'POST',
-		data: {'names' : names},
-		success: function(response) {
-			if (response.error) {
-				alert(response.error);
-			}
-			else {
-				alert("Successfully updated trial name");
-				jQuery('#edit_trial_name_dialog').dialog("close");
-				display_trial_name(trial_id);
-			}
-		},
-		error: function(response) {
-			alert("An error occurred updating the trial name");
-		},
-	});
-
-}
 
 function save_trial_type(type) {
     var trial_id = get_trial_id();
