180e47b1905667ade39d300e26d7261c8e5b4118
bellerbrock <bje24@cornell.edu>
2016-06-01 00:51:40 +0200

    trim whitespace from accession search input string unless exact match is specified. closes #495

--- lib/SGN/Controller/AJAX/Search/Stock.pm
+++ lib/SGN/Controller/AJAX/Search/Stock.pm
@@ -16,10 +16,10 @@ __PACKAGE__->config(
    );
 
 
-sub stock_search :Path('/ajax/search/stocks') Args(0) { 
+sub stock_search :Path('/ajax/search/stocks') Args(0) {
     my $self = shift;
     my $c = shift;
-    
+
     my $params = $c->req->params() || {};
 
     my %query;
@@ -28,11 +28,15 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
     my $matchtype = $params->{any_name_matchtype};
     my $any_name  = $params->{any_name};
 
+    unless ($matchtype eq 'exactly') { #trim whitespace from both ends unless exact search was specified
+      $any_name =~ s/^\s+|\s+$//g;
+    }
+
     my $schema = $c->dbic_schema("Bio::Chado::Schema", 'sgn_chado');
-    
+
     my ($or_conditions, $and_conditions);
     $or_conditions->{'me.stock_id'} =  { '>' => 0 };
-    $and_conditions->{'me.stock_id'} = { '>' => 0 }; 
+    $and_conditions->{'me.stock_id'} = { '>' => 0 };
     if (exists($params->{any_name} ) && $params->{any_name} ) {
 	my $start = '%';
 	my $end = '%';
@@ -45,17 +49,17 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
 	    $end = '';
 	}
 
-	$or_conditions = [ 
-	    { 'me.name'          => {'ilike', $start.$params->{any_name}.$end} },  
-	    { 'me.uniquename'    => {'ilike', $start.$params->{any_name}.$end} },  
-	    { 'me.description'   => {'ilike', $start.$params->{any_name}.$end} },
-	    { 'stockprops.value'   => {'ilike', $start.$params->{any_name}.$end} } 
-	    ] ; 
-    } else { 
+	$or_conditions = [
+	    { 'me.name'          => {'ilike', $start.$any_name.$end} },
+	    { 'me.uniquename'    => {'ilike', $start.$any_name.$end} },
+	    { 'me.description'   => {'ilike', $start.$any_name.$end} },
+	    { 'stockprops.value'   => {'ilike', $start.$any_name.$end} }
+	    ] ;
+    } else {
 	$or_conditions = [ { 'me.uniquename' => { '!=', undef } } ];
     }
-    
-    
+
+
     ###############
     if (exists($params->{organism} ) && $params->{organism} ) {
 	$and_conditions->{'me.organism_id'} = $params->{organism} ;
@@ -71,7 +75,7 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
         $first_name =~ s/\s+//g;
         $last_name =~ s/\s+//g;
 
-	my $p_rs = $c->dbic_schema("CXGN::People::Schema")->resultset("SpPerson")->search(  
+	my $p_rs = $c->dbic_schema("CXGN::People::Schema")->resultset("SpPerson")->search(
 	    {
 		first_name => { 'ilike' , '%'.$first_name.'%' } ,
 		last_name  => { 'ilike' , '%'.$last_name.'%' }
@@ -79,7 +83,7 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
 	    );
 
 	my $stock_owner_rs = $c->dbic_schema("CXGN::Phenome::Schema")->resultset("StockOwner")->search(
-	    { 
+	    {
 		sp_person_id => { -in  => $p_rs->get_column('sp_person_id')->as_query },
 	    });
 	my @stock_ids;
@@ -94,7 +98,7 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
     if (exists($params->{trait} ) && $params->{trait} ) {
 	$and_conditions->{ 'observable.name' }  = $params->{trait} ;
     }
-    
+
     if (exists($params->{project} ) && $params->{project} ) {
 	$and_conditions->{ 'lower(project.name)' } = { -like  => lc($params->{project} ) } ;
     }
@@ -121,7 +125,7 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
     my $page = int($start / $rows)+1;
 
     # get the count first
-    my $rs = $schema->resultset("Stock::Stock")->search( 
+    my $rs = $schema->resultset("Stock::Stock")->search(
 	{
 	    -and => [
 		 $or_conditions,
@@ -136,30 +140,30 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
 
 
     my $records_total = $rs->count();
-    
+
     #
-    my $rs2 = $schema->resultset("Stock::Stock")->search(   
+    my $rs2 = $schema->resultset("Stock::Stock")->search(
 	{
 	    -and => [
 		 $or_conditions,
-		 $and_conditions  
+		 $and_conditions
 		],
 	} ,
-	{ 
+	{
 	    join => ['type', 'organism', 'stockprops', { nd_experiment_stocks => { nd_experiment => {'nd_experiment_phenotypes' => {'phenotype' => 'observable' }}}} ,  { nd_experiment_stocks => { nd_experiment => { 'nd_experiment_projects' => { 'project' => ['projectprops', 'project_relationship_subject_projects'] } } } } , { nd_experiment_stocks => { nd_experiment => 'nd_geolocation' } } ],
 
 	    '+select' => [ 'type.name' , 'organism.species' ],
 	    '+as'     => [ 'cvterm_name' , 'species' ],
-	    page      => $page, 
-	    rows      => $rows, 
+	    page      => $page,
+	    rows      => $rows,
 	    order_by  => 'me.name',
 	    distinct  => 1,
-	} 
+	}
 	);
-	
+
 
     my @result;
-    while (my $a        = $rs2->next()) { 
+    while (my $a        = $rs2->next()) {
 	my $uniquename  = $a->uniquename;
 	my $type_id     = $a->type_id ;
 	my $type        = $a->get_column('cvterm_name');
@@ -170,8 +174,8 @@ sub stock_search :Path('/ajax/search/stocks') Args(0) {
     }
 
     $c->stash->{rest} = { data => [ @result ], draw => $draw, recordsTotal => $records_total,  recordsFiltered => $records_total };
-    
-    
+
+
 }
 
 1;
