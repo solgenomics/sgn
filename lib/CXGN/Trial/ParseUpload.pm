6d1ab26a8e00e8da6cc198a3980a80eddbdfa966
bellerbrock <bje24@cornell.edu>
2016-06-03 14:50:14 +0200

    Dump trial upload errors

--- lib/CXGN/Trial/ParseUpload.pm
+++ lib/CXGN/Trial/ParseUpload.pm
@@ -1,6 +1,7 @@
 package CXGN::Trial::ParseUpload;
 
 use Moose;
+use Data::Dumper;
 use MooseX::FollowPBP;
 use Moose::Util::TypeConstraints;
 
@@ -38,21 +39,24 @@ sub parse {
   my $self = shift;
 
   if (!$self->_validate_with_plugin()) {
-    print STDERR "\nCould not validate trial file: ".$self->get_filename()."\nError:".@$self->get_parse_errors()."\n";
+		my $errors = $self->get_parse_errors();
+    print STDERR "\nCould not validate trial file: ".$self->get_filename()."\nError:".Dumper($errors)."\n";
     return;
   }
 
   print STDERR "Check 3.1: ".localtime();
 
   if (!$self->_parse_with_plugin()) {
-    print STDERR "\nCould not parse trial file: ".$self->get_filename()."\nError:".@$self->get_parse_errors()."\n";
+		my $errors = $self->get_parse_errors();
+    print STDERR "\nCould not parse trial file: ".$self->get_filename()."\nError:".Dumper($errors)."\n";
     return;
   }
 
   print STDERR "Check 3.2: ".localtime();
 
   if (!$self->_has_parsed_data()) {
-    print STDERR "\nNo parsed data for trial file: ".$self->get_filename()."\nError:".@$self->get_parse_errors()."\n";
+		my $errors = $self->get_parse_errors();
+    print STDERR "\nNo parsed data for trial file: ".$self->get_filename()."\nError:".Dumper($errors)."\n";
     return;
   } else {
     return $self->_parsed_data();
@@ -60,7 +64,8 @@ sub parse {
 
   print STDERR "Check 3.3: ".localtime();
 
-  print STDERR "\nError parsing trial file: ".$self->get_filename()."\nError:".@$self->get_parse_errors()."\n";
+	my $errors = $self->get_parse_errors();
+  print STDERR "\nError parsing trial file: ".$self->get_filename()."\nError:".Dumper($errors)."\n";
   return;
 }
 
