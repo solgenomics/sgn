54463871202488b8a76df7ea2a88df4f1e65a4e9
Isaak <iyt2@cornell.edu>
2016-08-18 14:59:28 +0200

    get traits details with trait name or id..

--- lib/SGN/Controller/solGS/solGS.pm
+++ lib/SGN/Controller/solGS/solGS.pm
@@ -2005,15 +2005,39 @@ sub solgs_details_trait :Path('/solgs/details/trait/') Args(1) {
 
 
 sub get_trait_details {
-    my ($self, $c, $trait_id) = @_;
+    my ($self, $c, $trait) = @_;
+    
+    $trait = $c->stash->{trait_id} if !$trait;
+    
+    die "Can't get trait details with out trait id or name: $!\n" if !$trait;
 
-    $trait_id = $c->stash->{trait_id} if !$trait_id;
+    my ($trait_name, $trait_def, $trait_id, $trait_abbr);
 
-    my $trait_name = $c->model('solGS::solGS')->trait_name($trait_id); 
-    my $abbr = $self->abbreviate_term($trait_name);
+    if ($trait =~ /^\d+$/) 
+    {
+	$trait = $c->model('solGS::solGS')->trait_name($trait_id);	
+    }
+    
+    if ($trait) 
+    {
+	my $rs = $c->model('solGS::solGS')->trait_details($trait);
+	
+	while (my $row = $rs->next)
+	{
+	    $trait_id   = $row->id;
+	    $trait_name = $row->name;
+	    $trait_def  = $row->definition;
+	    $trait_abbr = $self->abbreviate_term($trait_name);
+
+	    print STDERR "\n $trait_id - $trait_name - $trait_def - $trait_abbr\n";
+	}	
+    } 
    
+    my $abbr = $self->abbreviate_term($trait_name);
+       
     $c->stash->{trait_id}   = $trait_id;
     $c->stash->{trait_name} = $trait_name;
+    $c->stash->{trait_def}  = $trait_def;
     $c->stash->{trait_abbr} = $abbr;
 
 }
@@ -4417,31 +4441,45 @@ sub traits_starting_with {
 sub hyperlink_traits {
     my ($self, $c, $traits) = @_;
 
-    my @traits_urls;
-    foreach my $tr (@$traits)
+    if (ref($traits) eq 'ARRAY') 
     {
-        push @traits_urls, [ qq | <a href="/solgs/search/result/traits/$tr">$tr</a> | ];
+	my @traits_urls;
+	foreach my $tr (@$traits)
+	{	
+	    push @traits_urls, [ qq | <a href="/solgs/search/result/traits/$tr">$tr</a> | ];
+	}
+
+	$c->stash->{traits_urls} = \@traits_urls;
     }
-
-    $c->stash->{traits_urls} = \@traits_urls;
-
+    else 
+    {    
+	$c->stash->{traits_urls} = qq | <a href="/solgs/search/result/traits/$traits">$traits</a> |;
+    }
 }
 
 
 sub gs_traits : Path('/solgs/traits') Args(1) {
     my ($self, $c, $index) = @_;
     
+    my @traits_list;
+
     if ($index =~ /^\w{1}$/) 
     {
         $self->traits_starting_with($c, $index);
         my $traits_gr = $c->stash->{trait_subgroup};
         
-        $self->hyperlink_traits($c, $traits_gr);
-        my $traits_urls = $c->stash->{traits_urls};
-        
-        $c->stash( template    => $self->template('/search/traits/list.mas'),
+	foreach my $trait (@$traits_gr)
+	{
+	    $self->hyperlink_traits($c, $trait);
+	    my $trait_url = $c->stash->{traits_urls};
+	   
+	    $self->get_trait_details($c, $trait);	    
+	    push @traits_list, [$trait_url, $c->stash->{trait_def}];	    
+	}       
+       
+	$c->stash( template    => $self->template('/search/traits/list.mas'),
                    index       => $index,
-                   traits_list => $traits_urls
+                   traits_list => \@traits_list
             );
     }
     else 
