998468a9b9538ff522d1e22c9ef61c091ca5badd
nickmorales <nmorales3142@gmail.com>
2016-08-30 18:28:59 +0000

    cleaner passing of nd_geolocation from url to mason page

--- lib/SGN/Controller/Search/Trial.pm
+++ lib/SGN/Controller/Search/Trial.pm
@@ -9,6 +9,7 @@ sub trial_search_page : Path('/search/trials/') Args(0) {
     my $self = shift;
     my $c = shift;
 
+    $c->stash->{nd_geolocation} = $c->req->param('nd_geolocation') || 'not_provided';
     $c->stash->{template} = '/search/trials.mas';
 
 }
