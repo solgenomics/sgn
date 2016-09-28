4d30d5052118794cc96ba99ccc63c7c8e5e2b715
nmenda <nm249@cornell.edu>
2016-09-28 14:57:11 -0400

    fix conflict

--- db/00064/AddNdprotocolDescriptionChado.pm
+++ db/00064/AddNdprotocolDescriptionChado.pm
@@ -15,6 +15,9 @@ see the perldoc of parent class for more details.
 =head1 DESCRIPTION
 
 Add a description column to the Chado table nd_protocol
+This change will go into Chado version 1.4. See GMOD git repo for details.
+DO NOT ALTER CHADO TABLES WITHOUT COORDINATING WITH GMOD FIRST!
+
 This subclass uses L<Moose>. The parent class uses L<MooseX::Runnable>
 
 =head1 AUTHOR
@@ -39,6 +42,7 @@ extends 'CXGN::Metadata::Dbpatch';
 
 has '+description' => ( default => <<'' );
 patch for adding descriotion column to chado table nd_protocol. This change will go into Chado version 1.4. DO NOT ALTER CHADO TABLES WITHOUT COORDINATING FIRST WITH GMOD!
+
 
 has '+prereq' => (
     default => sub {
