7d3fd2ddfd08bb21520cb043c80a880e9b57e4f4
Lukas Mueller <lam87@cornell.edu>
2017-02-16 09:18:26 -0500

    clean up code

--- R/analyze_phenotype.r
+++ R/analyze_phenotype.r
@@ -1,26 +1,21 @@
 
-# R CMD BATCH --no-save --no-restore '--args phenotype_file="blabla.txt" output_file="blalba.png" trait="CO:0029202" ' analyze_phenotype.r output.txt
+# R CMD BATCH --no-save --no-restore '--args phenotype_file="blabla.txt" output_file="blalba.png" ' analyze_phenotype.r output.txt
 
 args=(commandArgs(TRUE))
 
-
-trait = ''
-
 if(length(args)==0){
-    print("No arguments supplied.")
-    ##supply default values
-    phenotype_file = 'phenotypes.txt'
-    output_file = paste0(phenotype_file, ".png", sep="")
-    trait = ''
-}else{
-    for(i in 1:length(args)){
-      eval(parse(text=args[[i]]))
-    }
+   print("No arguments supplied.")
+   ##supply default values
+   phenotype_file = 'phenotypes.txt'
+   output_file = paste0(phenotype_file, ".png", sep="")
+} else {
+   for(i in 1:length(args)){
+       eval(parse(text=args[[i]]))
+   }
 }
 
 write(paste("phenotype file: ", phenotype_file), stderr())
 write(paste("output_file: ", output_file), stderr())
-write(paste("trait: ", trait), stderr())
 
 errorfile = paste(phenotype_file, ".err", sep="");
 
@@ -38,52 +33,35 @@ all_accessions = unique(phenodata$germplasmName)
 datamatrix = matrix(nrow = length(all_accessions), ncol=length(studyNames)) # * length(blocks))
 
 for (i in 1:(length(studyNames))) {
-   # for (n in 1:length(blocks)) { 
-        print(paste("StudyName: ", studyNames[i]))
-        trialdata = phenodata[phenodata[,"studyName"]==studyNames[i], ] # & phenodata[,"blockNumber"]==n, ]
-		show(trialdata)
-       for (m in 1:length(all_accessions)) { 
-	
-	    acc_slice = trialdata[trialdata[,"germplasmName"] ==  as.character(all_accessions[m] ), 16 ]
-	    
-	    acc_avg  = mean(as.numeric(acc_slice))
-	    print(paste("Average for accession", all_accessions[m]))
-            show(acc_avg)
-	    #	    col = (i-1) * length(blocks) + n;
-	    col = i;
-	    print(paste("row: ", m, "col", col, "avg", acc_avg))
-            datamatrix[m, col] = acc_avg
-	}
+   trialdata = phenodata[phenodata[,"studyName"]==studyNames[i], ] # & phenodata[,"blockNumber"]==n, ]
 
-	#	colname = paste(studyNames[i],"_B",blocks[n], sep="")
-	colname = studyNames[i]
-        datasetnames = c(datasetnames, colname)
-    #}		
+   for (m in 1:length(all_accessions)) { 
+	
+      acc_slice = trialdata[trialdata[,"germplasmName"] ==  as.character(all_accessions[m] ), 16 ]
+	    
+      acc_avg  = mean(as.numeric(acc_slice))
+      col = i;
+      datamatrix[m, col] = acc_avg
+   }
 }
 
-
-colnames(datamatrix) <- datasetnames
+colnames(datamatrix) <- studyNames
 rownames(datamatrix) <- all_accessions
-
-show(datamatrix)
 
 # remove columns containing only NULL values
 #
 dims = dim(datamatrix)
-print(paste("Dimension: ", dims))
+
 empty_cols <- c()
 for (i in 1:dims[2]) { 
     legal_values = datamatrix[ is.finite(datamatrix[,i]), i ]
-    show(legal_values)
+
     if (length(legal_values) == 0) { 
        print(paste("empty values", length(legal_values), "dims", dims[1]))
        print(paste("found empty col ", i))
        empty_cols <- c(empty_cols, i)
     }
 }
-
-print("empty cols")
-show(empty_cols)
 
 #for (i in 1:length(empty_cols)) { 
 #   datamatrix <- datamatrix[,-empty_cols[i]]
@@ -114,12 +92,8 @@ if (ncol(datamatrix) < 2) {
    write("No data. Try again", file = errorfile);
 }
 
-print("Datamatrix:")
-show(datamatrix)
-#write(datamatrix, file=errorfile, append=TRUE);
-
-#correlation
-
+# correlation
+#
 panel.cor <- function(x, y, digits=2, cex.cor)
 {
    usr <- par("usr"); on.exit(par(usr))
