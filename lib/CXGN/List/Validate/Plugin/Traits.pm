5a412db8fcc50c66c8c0829391125bfb0b0d2354
nickmorales <nmorales3142@gmail.com>
2017-02-24 17:19:25 -0500

    working on postcomposed trait validation

--- lib/CXGN/List/Validate/Plugin/Traits.pm
+++ lib/CXGN/List/Validate/Plugin/Traits.pm
@@ -12,50 +12,77 @@ sub validate {
     my $self = shift;
     my $schema = shift;
     my $list = shift;
+    my @missing = ();
 
 #    print STDERR "LIST: ".Data::Dumper::Dumper($list);
 
-    my @missing = ();
-    my $trait_has_components;
     foreach my $term (@$list) {
-        my @components = split /\|\|/, $term;
-        $trait_has_components = 1 ? scalar(@components) > 1 : 0;
-        foreach (@components) {
-            my ($trait_name, $full_accession) = split (/\|/, $_);
-            my ($db_name, $accession) = split ":", $full_accession;
+        my ($trait_name, $full_accession) = split (/\|/, $term);
+        my ($db_name, $accession) = split ":", $full_accession;
 
-            if ($accession) {
-                $accession =~ s/\s+$//;
-                $accession =~ s/^\s+//;
-            }
-            if ($db_name) {
-                $db_name  =~ s/\s+$//;
-                $db_name  =~ s/^\s+//;
-            }
+        if ($accession) {
+            $accession =~ s/\s+$//;
+            $accession =~ s/^\s+//;
+        }
+        if ($db_name) {
+            $db_name  =~ s/\s+$//;
+            $db_name  =~ s/^\s+//;
+        }
 
-            my $db_rs = $schema->resultset("General::Db")->search( { 'me.name' => $db_name });
-            if ($db_rs->count() == 0) {
+        my $db_rs = $schema->resultset("General::Db")->search( { 'me.name' => $db_name });
+        if ($db_rs->count() == 0) {
+            push @missing, $_;
+        } else {
+            my $db = $db_rs->first();
+            my $rs = $schema->resultset("Cv::Cvterm")->search( {
+            'dbxref.db_id' => $db->db_id(),
+            'dbxref.accession'=>$accession }, {
+                'join' => 'dbxref' }
+            );
+
+            if ($rs->count == 0) {
                 push @missing, $_;
             } else {
-                my $rs = $schema->resultset("Cv::Cvterm")->search( {
-                'dbxref.db_id' => $db_rs->first()->db_id(),
-                'dbxref.accession'=>$accession }, {
-                    'join' => 'dbxref' }
-                );
+            #        my $rs_var = $rs->search_related('cvterm_relationship_subjects', {'type.name' => 'VARIABLE_OF'}, { 'join' => 'type'});
+            #        if ($rs_var->count == 0) {
+            #            push @missing, $_;
+            #        }
+            
+            if ($db->name eq 'COMP'){
 
-                if ($rs->count == 0) {
-                    push @missing, $_;
-                } else {
+                my $object_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'object_ontology', 'composable_cvtypes')->cvterm_id;
+                my $attribute_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'attribute_ontology', 'composable_cvtypes')->cvterm_id;
+                my $method_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'method_ontology', 'composable_cvtypes')->cvterm_id;
+                my $trait_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'trait_ontology', 'composable_cvtypes')->cvterm_id;
+                my $unit_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'unit_ontology', 'composable_cvtypes')->cvterm_id;
+                my $time_ont_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'time_ontology', 'composable_cvtypes')->cvterm_id;
+                my %cvtypes = { $object_ont_cvterm_id => 1, $attribute_ont_cvterm_id => 1, $method_ont_cvterm_id => 1, $trait_ont_cvterm_id => 1, $unit_ont_cvterm_id => 1, $time_ont_cvterm_id => 1};
 
-                #    if (!$trait_has_components) {
-                #        my $rs_var = $rs->search_related('cvterm_relationship_subjects', {'type.name' => 'VARIABLE_OF'}, { 'join' => 'type'});
-                #        if ($rs_var->count == 0) {
-                #            push @missing, $_;
-                #        }
-                #    }
+                my $cvterm_rs = $schema->resultset("Cv::Cvterm")->search({ name => $term });
+                if ($cvterm_rs->count == 0){
+                    push @missing, $term;
                 }
+                if ($cvterm_rs->count > 1){
+                    die "ERROR: $term has been stored multiple times in cvterm!!\n";
+                }
+
+                my $contains_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'contains', 'relationship')->cvterm_id();
+
+                my $component_rs = $cvterm_rs->first->search_related('cvterm_relationship_objects', {'cvterm_relationship_objects.type_id' => $contains_cvterm_id});
+                while(my $r = $component_rs->next){
+                    my $component_cvterm = $r->subject();
+                    my $component_cvprops = $component_cvterm->cv_props;
+                    while(my $cvprop = $component_cvprops->next){
+                        if (!exists($cvtypes{$cvprop->type_id})){
+                            print STDERR "Component not part of allowed cv ontologies\n";
+                            push @missing, $term;
+                        }
+                    }
+                }
+
             }
         }
+
     }
     return { missing => \@missing };
 }
