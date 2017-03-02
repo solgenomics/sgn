b4b10a360ae044b8db37bd0f559bffb4f288f3b5
titima15 <tt15@cornell.edu>
2017-03-02 16:37:33 +0000

    add field_layout type_id to make the query more specific

--- lib/SGN/Controller/AJAX/Accession_usage.pm
+++ lib/SGN/Controller/AJAX/Accession_usage.pm
@@ -20,16 +20,18 @@ __PACKAGE__->config(
       my $schema = $c->dbic_schema("Bio::Chado::Schema", 'sgn_chado');
       my $accession_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
       my $plot_of_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plot_of', 'stock_relationship')->cvterm_id();
+      my $field_layout_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'field_layout', 'experiment_type')->cvterm_id();
 
       my $dbh = $schema->storage->dbh();
 
-      my $q = "SELECT DISTINCT stock.stock_id, stock.uniquename, COUNT (DISTINCT project.project_id) AS trials, COUNT (DISTINCT stock_relationship.subject_id)
+      my $q = "SELECT DISTINCT stock.stock_id, stock.uniquename, COUNT(DISTINCT project.project_id) AS trials, COUNT(stock_relationship.subject_id)
                FROM stock JOIN stock_relationship on (stock.stock_id=stock_relationship.object_id) AND stock_relationship.type_id=?
-               JOIN nd_experiment_stock ON (stock_relationship.subject_id=nd_experiment_stock.stock_id) JOIN nd_experiment_project USING (nd_experiment_id)
-               JOIN project USING (project_id) WHERE stock.type_id =? GROUP BY stock.stock_id ORDER BY trials DESC";
+               JOIN nd_experiment_stock ON (stock_relationship.subject_id=nd_experiment_stock.stock_id) AND nd_experiment_stock.type_id =?
+               JOIN nd_experiment_project USING (nd_experiment_id)
+               JOIN project USING (project_id) WHERE stock.type_id =? GROUP BY stock.stock_id ORDER BY trials DESC LIMIT 200";
 
       my $h = $dbh->prepare($q);
-      $h->execute($plot_of_type_id, $accession_type_id);
+      $h->execute($plot_of_type_id, $field_layout_type_id, $accession_type_id);
 
       my@accessions_trials =();
       while (my ($accession_id, $accession_name, $trial_count, $plot_count) = $h->fetchrow_array()){
@@ -54,7 +56,8 @@ __PACKAGE__->config(
 
         my $q = "SELECT DISTINCT female_parent.stock_id, female_parent.uniquename, COUNT (DISTINCT cross_id.stock_id) AS cross_number
                  FROM stock as female_parent JOIN stock_relationship ON (female_parent.stock_id=stock_relationship.subject_id) AND stock_relationship.type_id=?
-                 JOIN stock AS cross_id ON (cross_id.stock_id=stock_relationship.object_id) GROUP BY female_parent.stock_id ORDER BY cross_number DESC";
+                 JOIN stock AS cross_id ON (cross_id.stock_id=stock_relationship.object_id)
+                 GROUP BY female_parent.stock_id ORDER BY cross_number DESC LIMIT 200";
 
         my $h = $dbh->prepare($q);
         $h->execute($female_parent_typeid);
@@ -80,7 +83,8 @@ __PACKAGE__->config(
 
       my $q = "SELECT DISTINCT male_parent.stock_id, male_parent.uniquename, COUNT (DISTINCT cross_id.stock_id) AS cross_number
                FROM stock as male_parent JOIN stock_relationship ON (male_parent.stock_id=stock_relationship.subject_id) AND stock_relationship.type_id=?
-               JOIN stock AS cross_id ON (cross_id.stock_id=stock_relationship.object_id) GROUP BY male_parent.stock_id ORDER BY cross_number DESC";
+               JOIN stock AS cross_id ON (cross_id.stock_id=stock_relationship.object_id)
+               GROUP BY male_parent.stock_id ORDER BY cross_number DESC LIMIT 200";
 
       my $h = $dbh->prepare($q);
       $h->execute($male_parent_typeid);
