580a2a8fa52ae4feda49d10aa7f53f4b542073b7
bellerbrock <bje24@cornell.edu>
2017-04-19 20:23:19 +0000

    included add to list in new trait modal

--- lib/CXGN/Onto.pm
+++ lib/CXGN/Onto.pm
@@ -173,8 +173,9 @@ sub compose_trait {
     $h = $dbh->prepare($refresh2);
     $h->execute();
 
-    #return $new_term->cvterm_id();
-    return $new_term;
+    return { cvterm_id => $new_term->cvterm_id(),
+            name => $new_term->name().'|COMP:'.sprintf("%07d",$accession)
+        };
 }
 
 
