a856e5551db9507dbbd2f7b9b0901fd9bdac4d44
nickmorales <nmorales3142@gmail.com>
2017-05-02 10:35:17 -0400

    stock search includes a search over any of the editable_stock_props

--- lib/SGN/Controller/Stock.pm
+++ lib/SGN/Controller/Stock.pm
@@ -48,6 +48,7 @@ has 'default_page_size' => (
 
 sub stock_search :Path('/search/stocks') Args(0) {
     my ($self, $c ) = @_;
+    my @editable_stock_props = split ',',$c->get_conf('editable_stock_props');
     $c->stash(
 	template => '/search/stocks.mas',
 
@@ -58,6 +59,7 @@ sub stock_search :Path('/search/stocks') Args(0) {
         onto_autocomplete_uri      => '/ajax/cvterm/autocomplete',
 	trait_db_name              => $c->get_conf('trait_ontology_db_name'),
 	breeding_programs          => breeding_programs($self->schema),
+    editable_stock_props => \@editable_stock_props
 	);
 
 }
