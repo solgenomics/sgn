54d9b046168d380b753726eaa06d5c8d0fd66505
Isaak <iyt2@cornell.edu>
2017-05-31 08:59:34 +0200

    update phenotype data cleaning and structuring for BrAPI like headers..

--- lib/SGN/Controller/solGS/solGS.pm
+++ lib/SGN/Controller/solGS/solGS.pm
@@ -631,6 +631,7 @@ sub get_project_details {
 sub project_description {
     my ($self, $c, $pr_id) = @_;
 
+    $c->stash->{pop_id} = $pr_id;
     $c->stash->{uploaded_reference} = 1 if ($pr_id =~ /uploaded/);
 
     my $protocol = $c->config->{default_genotyping_protocol};
@@ -674,31 +675,24 @@ sub project_description {
 	@geno_lines = read_file($geno_file);
 	$markers_no = scalar(split ('\t', $geno_lines[0])) - 1;	
     }
-
+   
     $self->trait_phenodata_file($c);
     my $trait_pheno_file  = $c->stash->{trait_phenodata_file};
     my @trait_pheno_lines = read_file($trait_pheno_file) if $trait_pheno_file;
  
     my $stocks_no = @trait_pheno_lines ? scalar(@trait_pheno_lines) - 1 : scalar(@geno_lines) - 1;
     
-    $self->phenotype_file($c);
-    my $pheno_file = $c->stash->{phenotype_file};
-    my @phe_lines  = read_file($pheno_file);   
-    my $traits     = $phe_lines[0];
-
-    $self->filter_phenotype_header($c);
-    my $filter_header = $c->stash->{filter_phenotype_header};
-   
-    $traits       =~ s/($filter_header\t)//g;
-
-    my @traits    =  split (/\t/, $traits);    
-    my $traits_no = scalar(@traits);
-   
+    $self->traits_acronym_file($c);
+    my $traits_file = $c->stash->{traits_acronym_file};
+    my @lines = read_file($traits_file);
+    my $traits_no = scalar(@lines) - 1;
+       
     $c->stash(markers_no => $markers_no,
               traits_no  => $traits_no,
               stocks_no  => $stocks_no,
 	      protocol   => $protocol,
         );
+
 }
 
 
@@ -4199,8 +4193,9 @@ sub analyzed_traits {
 sub filter_phenotype_header {
     my ($self, $c) = @_;
        
-    my $meta_headers = "uniquename\tobject_name\tobject_id\tstock_id\tstock_name\tdesign\tblock\treplicate";
+    my @headers = ( 'studyYear', 'studyDbId', 'studyName', 'studyDesign', 'locationDbId', 'locationName', 'germplasmDbId', 'germplasmName', 'germplasmSynonyms', 'observationLevel', 'observationUnitDbId', 'observationUnitName', 'replicate', 'blockNumber', 'plotNumber' );
 
+    my $meta_headers = join("\t", @headers);
     if ($c) 
     {
 	$c->stash->{filter_phenotype_header} = $meta_headers;
@@ -4628,13 +4623,9 @@ sub format_phenotype_dataset {
 sub format_phenotype_dataset_rows {
     my ($self, $data_rows) = @_;
     
-    foreach (@$data_rows)
-    {
-        $_ =~ s/\s+plot//g;
-        $_ .= "\n";
-    }
-    
-    return $data_rows;
+    my $data = join("\n", @$data_rows);
+
+    return $data;
     
 }
 
@@ -4642,8 +4633,7 @@ sub format_phenotype_dataset_rows {
 sub format_phenotype_dataset_headers {
     my ($self, $raw_headers, $traits_file) = @_;
 
-    $raw_headers =~ s/SP:\d+\|//g;  
-    $raw_headers =~ s/\w+:\w+\|//g;  
+    $raw_headers =~ s/\|\w+:\d+//g;
     $raw_headers =~ s/\n//g;  
 
     my $meta_headers=  $self->filter_phenotype_header();
