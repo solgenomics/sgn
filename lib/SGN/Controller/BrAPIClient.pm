b4cdf8a308ad509604ed521c7f6b8e0c6cd79480
nickmorales <nmorales3142@gmail.com>
2017-06-10 22:51:10 -0400

    brapi phenotype handhelds demo interface

--- lib/SGN/Controller/BrAPIClient.pm
+++ lib/SGN/Controller/BrAPIClient.pm
@@ -17,6 +17,12 @@ sub germplasm : Path('/brapihome/germplasm') Args(0) {
     $c->stash->{template} = 'brapiclient/germplasm_search.mas';
 }
 
+sub phenotyping_handhelds : Path('/brapihome/phenotyping_handhelds') Args(0) {
+    my $self = shift;
+    my $c = shift;
+    $c->stash->{template} = 'brapiclient/phenotyping_handhelds.mas';
+}
+
 sub index : Path('/brapiclient/comparegenotypes') Args(0) { 
     my $self = shift;
     my $c = shift;
