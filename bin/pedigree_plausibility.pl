50f3edeb84231c550bf06a21147dd7f97cf8229c
bellerbrock <bje24@cornell.edu>
2017-06-14 19:42:06 +0000

    read and print lines of file

--- bin/pedigree_plausibility.pl
+++ bin/pedigree_plausibility.pl
@@ -2,17 +2,29 @@
 
 use strict;
 use warnings;
-use Getopt::Std;
+#use Getopt::Std;
 
 
-my $opt_D;
+#my $opt_D;
+my @genotypes;
+my $marker;
+my $counter;
 
-getopts('D:');
+#getopts('D:');
+my $file = $ARGV[0];
 
-open(my $GENOS, "<", $opt_D) || die "Can't open  dosage file $opt_D";
-
-
-
+open(my $GENOS, "<", $file) || die "Can't open  dosage file $file";
+while (my $row = <$GENOS>) {
+	chomp $row;
+    if ($counter == 0) { # is header
+        print "Header: $row\n";
+    }
+    if ($counter % 1000) {
+        ($marker, @genotypes) = split /\t/, $row;
+        print "At line $counter Marker =$marker and genotypes are @genotypes\n";
+    }
+    $counter++;
+}
 
 sub compare_parental_genotypes {
     my $self = shift;
