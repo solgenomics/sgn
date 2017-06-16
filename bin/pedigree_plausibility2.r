23f44769d3ffaa960e4081c3bab787992886afa3
Kyndra <zacherl008@knights.gannon.edu>
2017-06-16 12:24:22 -0400

    rough working script

--- bin/pedigree_plausibility2.r
+++ bin/pedigree_plausibility2.r
@@ -1,44 +1,59 @@
-full_data <- read.table ("/home/bioinfo/host/test/genotypes.txt", header = TRUE, sep = "\t")
-parents <- mother + father
+genotype_data <- read.table ("/home/klz26/host/test/genotypes.txt", header = TRUE)
+pedigree_data <- read.table ("/home/klz26/host/test/Pedigrees.txt", header = TRUE, sep = "\t")
 
-for (i in file)
+colnames(pedigree_data)[1] <- 'child_name'
+colnames(pedigree_data)[2] <- 'mother_name'
+colnames(pedigree_data)[3] <- 'father_name'
+
+q <- 1
+x <- 1
+y <- 1
+z <- 1
+
+for(i in pedigree_data)
+{
+pedigree_data [x, "child_name"]
+test_child_name <- .Last.value
+x <- x+1
+
+pedigree_data [x, "mother_name"]
+test_mother_name <- .Last.value
+y <- y+1
+
+pedigree_data [x, "father_name"]
+test_father_name <- .Last.value
+z <- z+1
+
+
+for (i in genotype_data)
+{
+  genotype_data [q, test_child_name]
+  child_score <- .Last.value
+
+  genotype_data [q, test_mother_name]
+  mother_score <- .Last.value
+
+  genotype_data [q, test_father_name]
+  father_score <- .Last.value
+
+  q <- q + 1
+  data$parent_score <- data$mother_score + data$father_score
+
   if(
-    child > parents
-    [ 1 ][ 0 ][ 2 ] |
-    [ 0 ][ 0 ][ 1 ] |
-    [ 0 ][ 0 ][ 2 ] |
-    [ 0 ][ 1 ][ 2 ] |
-    mother and father = 2 and child doesn't
-    [ 2 ][ 2 ][ 0 ] |
-    [ 2 ][ 2 ][ 1 ] |
-    child = 0 and mother or father = 2
-    [ 2 ][ 1 ][ 0 ] |
-    [ 1 ][ 2 ][ 0 ] |
-    [ 2 ][ 0 ][ 0 ] |
-    [ 0 ][ 2 ][ 0 ] |
-    mother or father and child = 2
-    [ 0 ][ 2 ][ 2 ] |
-    [ 2 ][ 0 ][ 2 ])
+    if (child_score > parent_score)
+    {implausibility_count <- implausibility_count + 1}
 
-    {possibility <- 0};
+    else if (mother_score & father_score = 2) & (child_score != 2))
+    {implausibility_count <- implausibility_count + 1}
 
-  else if(
-          [ 1 ][ 0 ][ 0 ] |
-          [ 1 ][ 0 ][ 1 ] |
-      	  [ 1 ][ 1 ][ 0 ] |
-      	  [ 1 ][ 1 ][ 1 ] |
-      	  [ 1 ][ 2 ][ 1 ] |
-      	  [ 1 ][ 2 ][ 2 ] |
-          [ 0 ][ 2 ][ 1 ] |
-      	  [ 2 ][ 0 ][ 1 ] |
-          [ 0 ][ 1 ][ 1 ] |
-          [ 2 ][ 1 ][ 1 ] |
-      	  [ 2 ][ 1 ][ 2 ] |
-          [ 1 ][ 1 ][ 2 ] |
-      	  [ 2 ][ 2 ][ 2 ] |
-          [ 0 ][ 0 ][ 0 ] |
-          [ 0 ][ 1 ][ 0 ])
-          {possibility <- 1};
+    else if (mother_score | father_score = 2) & (child_score = 0))
+    {implausibility_count <- implausibility_count + 1}
 
-))
-  )
+    else if (mother_score | father_score = 2) & (child_score = 2))
+    {implausibility_count <- implausibility_count + 1}
+    )
+  else (
+    {implausibility_count <- implausibility_count + 0}
+       )
+}
+}
