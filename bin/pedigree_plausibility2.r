6644a9c9f9fb6f0e34ca6a43658d53d33b72c9a5
klz26 <zacherl008@knights.gannon.edu>
2017-06-16 16:29:39 -0400

    bug fixes for pedigree script

--- bin/pedigree_plausibility2.r
+++ bin/pedigree_plausibility2.r
@@ -1,4 +1,4 @@
-genotype_data <- read.table ("/home/klz26/host/test/genotypes.txt", header = TRUE)
+genotype_data <- read.table ("/home/klz26/host/test/red_genos.txt", header = TRUE)
 pedigree_data <- read.table ("/home/klz26/host/test/Pedigrees.txt", header = TRUE, sep = "\t")
 
 colnames(pedigree_data)[1] <- 'child_name'
@@ -7,53 +7,62 @@ colnames(pedigree_data)[3] <- 'father_name'
 
 q <- 1
 x <- 1
-y <- 1
-z <- 1
 
-for(i in pedigree_data)
+length_g <- length(genotype_data[,1])
+length_p <- length(pedigree_data[,1])
+implausibility_count <- 0
+
+while (x <= length_g)
 {
-pedigree_data [x, "child_name"]
-test_child_name <- .Last.value
+
+row_vector <- as.vector(pedigree_data[x,])
+
+test_child_name <- row_vector[1]
+colnames(test_child_name) <- c()
+rownames(test_child_name) <- c()
+
+test_mother_name <- row_vector[2]
+colnames(test_mother_name) <- c()
+rownames(test_mother_name) <- c()
+
+test_father_name <- row_vector[3]
+colnames(test_father_name) <- c()
+rownames(test_father_name) <- c()
+
 x <- x+1
 
-pedigree_data [x, "mother_name"]
-test_mother_name <- .Last.value
-y <- y+1
-
-pedigree_data [x, "father_name"]
-test_father_name <- .Last.value
-z <- z+1
-
-
-for (i in genotype_data)
+while (q <= length(genotype_data[,1]))
 {
-  genotype_data [q, test_child_name]
+  genotype_data[q, "test_child_name"]
   child_score <- .Last.value
+  colnames(child_score) <- c()
+  rownames(child_score) <- c()
 
-  genotype_data [q, test_mother_name]
+  genotype_data[q, "test_mother_name"]
   mother_score <- .Last.value
+  colnames(mother_score) <- c()
+  rownames(mother_score) <- c()
 
-  genotype_data [q, test_father_name]
+  genotype_data[q, "test_father_name"]
   father_score <- .Last.value
+  colnames(father_score) <- c()
+  rownames(father_score) <- c()
 
   q <- q + 1
-  data$parent_score <- data$mother_score + data$father_score
+  parent_score <- mother_score + father_score
 
-  if(
-    if (child_score > parent_score)
-    {implausibility_count <- implausibility_count + 1}
+    if (child_score > parent_score) {
+        implausibility_count <- implausibility_count + 1
 
-    else if (mother_score & father_score = 2) & (child_score != 2))
-    {implausibility_count <- implausibility_count + 1}
+    }  else if (mother_score == 2 & father_score == 2 & child_score != 2) {
+       implausibility_count <- implausibility_count + 1
 
-    else if (mother_score | father_score = 2) & (child_score = 0))
-    {implausibility_count <- implausibility_count + 1}
+    }  else if (mother_score == 2 | father_score == 2 & child_score == 0) {
+       implausibility_count <- implausibility_count + 1
 
-    else if (mother_score | father_score = 2) & (child_score = 2))
-    {implausibility_count <- implausibility_count + 1}
-    )
-  else (
-    {implausibility_count <- implausibility_count + 0}
-       )
+    }  else if (mother_score == 2 | father_score == 2 & child_score == 2) {
+       implausibility_count <- implausibility_count + 1
+    }
 }
 }
+print (implausibility_count)
