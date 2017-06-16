ca5213d670e62401dc6d8b83584370528517f80f
bioinfo <bioinfo@biodebian.redrover.cornell.edu>
2017-06-16 09:40:09 -0400

    pedigree loop

---
+++ bin/pedigree_plausibility2.r
@@ -0,0 +1,44 @@
+full_data <- read.table ("/home/bioinfo/host/test/genotypes.txt", header = TRUE, sep = "\t")
+parents <- mother + father
+
+for (i in file)
+  if(
+    child > parents
+    [ 1 ][ 0 ][ 2 ] |
+    [ 0 ][ 0 ][ 1 ] |
+    [ 0 ][ 0 ][ 2 ] |
+    [ 0 ][ 1 ][ 2 ] |
+    mother and father = 2 and child doesn't
+    [ 2 ][ 2 ][ 0 ] |
+    [ 2 ][ 2 ][ 1 ] |
+    child = 0 and mother or father = 2
+    [ 2 ][ 1 ][ 0 ] |
+    [ 1 ][ 2 ][ 0 ] |
+    [ 2 ][ 0 ][ 0 ] |
+    [ 0 ][ 2 ][ 0 ] |
+    mother or father and child = 2
+    [ 0 ][ 2 ][ 2 ] |
+    [ 2 ][ 0 ][ 2 ])
+
+    {possibility <- 0};
+
+  else if(
+          [ 1 ][ 0 ][ 0 ] |
+          [ 1 ][ 0 ][ 1 ] |
+      	  [ 1 ][ 1 ][ 0 ] |
+      	  [ 1 ][ 1 ][ 1 ] |
+      	  [ 1 ][ 2 ][ 1 ] |
+      	  [ 1 ][ 2 ][ 2 ] |
+          [ 0 ][ 2 ][ 1 ] |
+      	  [ 2 ][ 0 ][ 1 ] |
+          [ 0 ][ 1 ][ 1 ] |
+          [ 2 ][ 1 ][ 1 ] |
+      	  [ 2 ][ 1 ][ 2 ] |
+          [ 1 ][ 1 ][ 2 ] |
+      	  [ 2 ][ 2 ][ 2 ] |
+          [ 0 ][ 0 ][ 0 ] |
+          [ 0 ][ 1 ][ 0 ])
+          {possibility <- 1};
+
+))
+  )
