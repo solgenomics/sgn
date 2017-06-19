b5d435c978a3c1b1dcd9213fc2afac6953fc5a58
klz26 <zacherl008@knights.gannon.edu>
2017-06-19 14:09:34 -0400

    added dosage score calculator

--- bin/pedigree_plausibility2.r
+++ bin/pedigree_plausibility2.r
@@ -1,18 +1,17 @@
 genotype_data <- read.table ("/home/klz26/host/test/red_genos.txt", header = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
 pedigree_data <- read.table ("/home/klz26/host/test/Pedigrees.txt", header = TRUE, sep = "\t", check.names = FALSE, stringsAsFactors = FALSE)
 
-colnames(pedigree_data)[1] <- 'child_name'
-colnames(pedigree_data)[2] <- 'mother_name'
-colnames(pedigree_data)[3] <- 'father_name'
-
+colnames(pedigree_data)[1] <- "Name"
+colnames(pedigree_data)[2] <- "Mother"
+colnames(pedigree_data)[3] <- "Father"
+pedigree_data["Pedigree Conflict"] <- NA
 
 length_g <- length(genotype_data[,1])
 length_p <- length(pedigree_data[,1])
 implausibility_count <- 0
 
-for (x in 1:length_g)
+for (x in 1:length_p)
 {
-
   row_vector <- as.vector(pedigree_data[x,])
 
   test_child_name <- pedigree_data[x,1]
@@ -23,37 +22,44 @@ for (x in 1:length_g)
   print ("Genotypes not all present, skipping analysis")
   }
 
-  for (q in 1:length(genotype_data[,1]))
+  for (q in 1:length_g)
   {
     genotype_data[q, test_child_name]
     child_score <- .Last.value
-  
+    child_score <- round (child_score, digits = 0)
+    
     genotype_data[q, test_mother_name]
     mother_score <- .Last.value
-
+    mother_score <- round(father_score, digits = 0)
+    
     genotype_data[q, test_father_name]
     father_score <- .Last.value
+    father_score <- round(father_score, digits = 0)
 
     parent_score <- mother_score + father_score
 
     if (child_score > parent_score) {
         implausibility_count <- implausibility_count + 1
+        print ("line" + test_child_name + "shows a pedigree conflict")
       } else if (mother_score == 2 & father_score == 2 & child_score != 2) {
        implausibility_count <- implausibility_count + 1
+       print ("line" + test_child_name + "shows a pedigree conflict")
       } else if (mother_score == 2 || father_score == 2 & child_score == 0) {
        implausibility_count <- implausibility_count + 1
+       print ("line" + test_child_name + "shows a pedigree conflict")
       } else if ((xor(mother_score == 2, father_score == 2)) & (xor(mother_score == 0, 
        father_score == 0)) & child_score == 2) {
        implausibility_count <- implausibility_count + 1
+       print ("line" + test_child_name + "shows a pedigree conflict")
       }
-  
-    if (implausibility_count == 1) {
-      print (implausibility_count + "line shows a pedigree conflict")
-      } else {
-      print (implausibility_count + "lines show pedigree conflicts")
-      }
+    dosage_score <- implausibility_count / length_g
+    dosage_score <- round(dosage_score, digits = 1)
+    pedigree_data [x, 4] <- dosage_score
+    implausibility_count <- 0
   }
 }
-print (implausibility_count)
-
-
+if (implausibility_count == 1) {
+  print (implausibility_count + "line shows a pedigree conflict")
+} else {
+  print (implausibility_count + "lines show pedigree conflicts")
+}
