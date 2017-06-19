715828adb5974629745bcb18dab780127017af9a
klz26 <zacherl008@knights.gannon.edu>
2017-06-19 13:31:47 -0400

    fixed bug in loops

--- bin/pedigree_plausibility2.r
+++ bin/pedigree_plausibility2.r
@@ -13,44 +13,46 @@ implausibility_count <- 0
 for (x in 1:length_g)
 {
 
-row_vector <- as.vector(pedigree_data[x,])
+  row_vector <- as.vector(pedigree_data[x,])
 
-test_child_name <- pedigree_data[x,1]
+  test_child_name <- pedigree_data[x,1]
+  test_mother_name <- pedigree_data[x,2]
+  test_father_name <- pedigree_data[x,3]
 
-test_mother_name <- pedigree_data[x,2]
+  if (test_father_name == "NULL" || test_child_name == "NULL" || test_mother_name == "NULL"){
+  print ("Genotypes not all present, skipping analysis")
+  }
 
-test_father_name <- pedigree_data[x,3]
-
-if (test_father_name == "NULL" || test_child_name == "NULL" || test_mother_name == "NULL") 
-  {print ("Genotypes not all present, skipping analysis")}
-
-for (q in 1:length(genotype_data[,1]))
-{
-  genotype_data[q, test_child_name]
-  child_score <- .Last.value
+  for (q in 1:length(genotype_data[,1]))
+  {
+    genotype_data[q, test_child_name]
+    child_score <- .Last.value
   
-  genotype_data[q, test_mother_name]
-  mother_score <- .Last.value
+    genotype_data[q, test_mother_name]
+    mother_score <- .Last.value
 
-  genotype_data[q, test_father_name]
-  father_score <- .Last.value
+    genotype_data[q, test_father_name]
+    father_score <- .Last.value
 
-  parent_score <- mother_score + father_score
+    parent_score <- mother_score + father_score
 
     if (child_score > parent_score) {
         implausibility_count <- implausibility_count + 1
-
-    }  else if (mother_score == 2 & father_score == 2 & child_score != 2) {
+      } else if (mother_score == 2 & father_score == 2 & child_score != 2) {
        implausibility_count <- implausibility_count + 1
-
-    }  else if (mother_score == 2 || father_score == 2 & child_score == 0) {
+      } else if (mother_score == 2 || father_score == 2 & child_score == 0) {
        implausibility_count <- implausibility_count + 1
-
-    }  else if ((xor(mother_score == 2, father_score == 2)) & (xor(mother_score == 0, 
-                father_score == 0)) & child_score == 2) {
+      } else if ((xor(mother_score == 2, father_score == 2)) & (xor(mother_score == 0, 
+       father_score == 0)) & child_score == 2) {
        implausibility_count <- implausibility_count + 1
-    }
-}
+      }
+  
+    if (implausibility_count == 1) {
+      print (implausibility_count + "line shows a pedigree conflict")
+      } else {
+      print (implausibility_count + "lines show pedigree conflicts")
+      }
+  }
 }
 print (implausibility_count)
 
