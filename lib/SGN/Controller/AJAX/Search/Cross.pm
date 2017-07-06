3f967fce0981c9169d768d957d1c225dd7c90769
titima15 <tt15@cornell.edu>
2017-07-06 20:24:32 +0000

    refactoring sub for retrieving progenies

--- lib/SGN/Controller/AJAX/Search/Cross.pm
+++ lib/SGN/Controller/AJAX/Search/Cross.pm
@@ -31,7 +31,7 @@ sub search_cross_male_parents :Path('/ajax/search/cross_male_parents') :Args(0){
     INNER JOIN stock_relationship AS stock_relationship1 ON (female_parent.stock_id=stock_relationship1.subject_id)
     INNER JOIN stock AS check_type ON (stock_relationship1.object_id=check_type.stock_id)
     LEFT JOIN stock_relationship AS stock_relationship2 ON (stock_relationship1.object_id = stock_relationship2.object_id)
-    INNER JOIN stock AS male_parent ON (male_parent.stock_id=stock_relationship2.subject_id)
+    LEFT JOIN stock AS male_parent ON (male_parent.stock_id=stock_relationship2.subject_id)
     WHERE female_parent.uniquename = ? AND stock_relationship1.type_id = ? AND check_type.type_id = ? AND stock_relationship2.type_id = ?
     ORDER BY male_parent.uniquename ASC";
 
@@ -63,7 +63,6 @@ sub search_cross_info : Path('/ajax/search/cross_info') Args(0) {
 
     my $schema = $c->dbic_schema("Bio::Chado::Schema");
 
-    #my $cross_search = CXGN::Cross->new({bcs_schema => $schema, female_parent => $female_parent, male_parent=>$male_parent});
     my $result = CXGN::Cross->get_cross_info($schema, $female_parent, $male_parent);
     my @cross_info;
     foreach my $r (@$result){
@@ -89,7 +88,6 @@ sub search_all_crosses : Path('/ajax/search/all_crosses') Args(0) {
 
     my $schema = $c->dbic_schema("Bio::Chado::Schema");
 
-    #my $cross_search = CXGN::Cross->new({bcs_schema => $schema, female_parent => $female_parent});
     my $result = CXGN::Cross->get_cross_info($schema, $female_parent);
     my @cross_info;
     foreach my $r (@$result){
@@ -120,9 +118,8 @@ sub search_pedigree_male_parents :Path('/ajax/search/pedigree_male_parents') :Ar
     my $dbh = $schema->storage->dbh();
 
     my $q = "SELECT DISTINCT female_parent.stock_id, male_parent.stock_id, male_parent.uniquename FROM stock as female_parent
-    INNER JOIN stock_relationship AS stock_relationship1 ON (female_parent.stock_id=stock_relationship1.subject_id)
-    AND stock_relationship1.type_id= ? INNER JOIN stock AS check_type ON (stock_relationship1.object_id=check_type.stock_id)
-    AND check_type.type_id = ?
+    INNER JOIN stock_relationship AS stock_relationship1 ON (female_parent.stock_id=stock_relationship1.subject_id) AND stock_relationship1.type_id= ?
+    INNER JOIN stock AS check_type ON (stock_relationship1.object_id=check_type.stock_id) AND check_type.type_id = ?
     INNER JOIN stock_relationship AS stock_relationship2 ON (stock_relationship1.object_id=stock_relationship2.object_id) AND stock_relationship2.type_id = ?
     INNER JOIN stock AS male_parent ON (male_parent.stock_id=stock_relationship2.subject_id)
     WHERE female_parent.uniquename= ? ORDER BY male_parent.uniquename ASC";
@@ -147,30 +144,18 @@ sub search_all_progenies : Path('/ajax/search/all_progenies') Args(0) {
     my $c = shift;
 
     my $pedigree_female_parent = $c->req->param("pedigree_female_parent");
+
     my $schema = $c->dbic_schema("Bio::Chado::Schema");
-    my $female_parent_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'female_parent', 'stock_relationship')->cvterm_id();
-    my $male_parent_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'male_parent', 'stock_relationship')->cvterm_id();
-    my $accession_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
-    my $dbh = $schema->storage->dbh();
+    my $result = CXGN::Cross->get_progeny_info($schema, $pedigree_female_parent);
+    my @all_progenies;
+    foreach my $r(@$result){
+      print STDERR Dumper $r;
 
-    my $q = "SELECT DISTINCT female_parent.stock_id, female_parent.uniquename, male_parent.stock_id, male_parent.uniquename, progeny.stock_id, progeny.uniquename, stock_relationship1.value
-      FROM stock_relationship as stock_relationship1 INNER JOIN stock AS female_parent ON (stock_relationship1.subject_id = female_parent.stock_id)
-      INNER JOIN stock AS progeny ON (stock_relationship1.object_id = progeny.stock_id)
-      LEFT JOIN stock_relationship AS stock_relationship2 ON (progeny.stock_id = stock_relationship2.object_id)
-      INNER JOIN stock AS male_parent ON (stock_relationship2.subject_id = male_parent.stock_id)
-      WHERE female_parent.uniquename = ? AND stock_relationship1.type_id = ? AND stock_relationship2.type_id = ? AND progeny.type_id = ?
-      ORDER BY male_parent.uniquename";
-
-    my$h = $dbh->prepare($q);
-    $h->execute($pedigree_female_parent, $female_parent_typeid, $male_parent_typeid, $accession_typeid);
-
-    my@all_progenies=();
-    while (my($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $progeny_id, $progeny_name, $cross_type) = $h->fetchrow_array()){
-
+    my ($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $progeny_id, $progeny_name, $cross_type) = @$r;
     push @all_progenies, [ qq{<a href="/stock/$female_parent_id/view">$female_parent_name</a>},
       qq{<a href="/stock/$male_parent_id/view">$male_parent_name</a>},
       qq{<a href="/stock/$progeny_id">$progeny_name</a}, $cross_type];
-      print STDERR "All_progenies =" . Dumper(@all_progenies) . "\n";
+      print STDERR Dumper @all_progenies;
     }
 
     $c->stash->{rest}={ data=> \@all_progenies};
@@ -178,40 +163,29 @@ sub search_all_progenies : Path('/ajax/search/all_progenies') Args(0) {
     }
 
 
-sub search_pedigree_info : Path('/ajax/search/pedigree_info') Args(0) {
+sub search_progenies : Path('/ajax/search/progenies') Args(0) {
     my $self = shift;
     my $c = shift;
 
     my $pedigree_female_parent = $c->req->param("pedigree_female_parent");
     my $pedigree_male_parent = $c->req->param("pedigree_male_parent");
+
     my $schema = $c->dbic_schema("Bio::Chado::Schema");
-    my $female_parent_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'female_parent', 'stock_relationship')->cvterm_id();
-    my $male_parent_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'male_parent', 'stock_relationship')->cvterm_id();
-    my $accession_typeid = $c->model("Cvterm")->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
-    my $dbh = $schema->storage->dbh();
+    my $result = CXGN::Cross->get_progeny_info($schema, $pedigree_female_parent, $pedigree_male_parent);
+    my @progenies;
+    foreach my $r(@$result){
+      print STDERR Dumper $r;
 
-    my $q = "SELECT DISTINCT female_parent.stock_id, female_parent.uniquename, male_parent.stock_id, male_parent.uniquename, progeny.stock_id, progeny.uniquename, stock_relationship1.value
-        FROM stock_relationship as stock_relationship1 INNER JOIN stock AS female_parent ON (stock_relationship1.subject_id = female_parent.stock_id)
-        INNER JOIN stock AS progeny ON (stock_relationship1.object_id = progeny.stock_id)
-        LEFT JOIN stock_relationship AS stock_relationship2 ON (progeny.stock_id = stock_relationship2.object_id)
-        INNER JOIN stock AS male_parent ON (stock_relationship2.subject_id = male_parent.stock_id)
-        WHERE female_parent.uniquename = ? AND male_parent.uniquename = ? AND stock_relationship1.type_id = ? AND stock_relationship2.type_id = ? AND progeny.type_id = ?";
+    my ($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $progeny_id, $progeny_name, $cross_type) = @$r;
+    push @progenies, [ qq{<a href="/stock/$female_parent_id/view">$female_parent_name</a>},
+      qq{<a href="/stock/$male_parent_id/view">$male_parent_name</a>},
+      qq{<a href="/stock/$progeny_id">$progeny_name</a}, $cross_type];
+      print STDERR Dumper @progenies;
+    }
 
-        my$h = $dbh->prepare($q);
-        $h->execute($pedigree_female_parent, $pedigree_male_parent, $female_parent_typeid, $male_parent_typeid, $accession_typeid);
+    $c->stash->{rest}={ data=> \@progenies};
 
-        my@pedigree_info=();
-        while (my($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $progeny_id, $progeny_name, $cross_type) = $h->fetchrow_array()){
-
-        push @pedigree_info, [ qq{<a href="/stock/$female_parent_id/view">$female_parent_name</a>},
-          qq{<a href="/stock/$male_parent_id/view">$male_parent_name</a>},
-          qq{<a href="/stock/$progeny_id">$progeny_name</a}, $cross_type];
-          print STDERR "Pedigree_info =" . Dumper(@pedigree_info) . "\n";
-        }
-
-        $c->stash->{rest}={ data=> \@pedigree_info};
-
-        }
+    }
 
 
 
