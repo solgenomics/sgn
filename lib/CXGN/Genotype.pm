f5de97ff1358122c2c9b97b966bafbc46e61cc31
bellerbrock <bje24@cornell.edu>
2017-07-10 12:57:42 -0400

    fix two impossible parent/child geno combos

--- lib/CXGN/Genotype.pm
+++ lib/CXGN/Genotype.pm
@@ -205,10 +205,10 @@ sub compare_parental_genotypes {
 
 	$matrix[ 2 ][ 0 ][ 2 ] =0;
 	$matrix[ 2 ][ 0 ][ 0 ] =0;
+	$matrix[ 0 ][ 2 ][ 0 ] =0;
+	$matrix[ 0 ][ 2 ][ 2 ] =0;
 
 	$matrix[ 0 ][ 2 ][ 1 ] =1;
-	$matrix[ 0 ][ 2 ][ 0 ] =1;
-	$matrix[ 0 ][ 2 ][ 2 ] =1;
 	$matrix[ 0 ][ 0 ][ 0 ] =1;
 	$matrix[ 1 ][ 0 ][ 0 ] =1;
 	$matrix[ 1 ][ 0 ][ 1 ] =1;
