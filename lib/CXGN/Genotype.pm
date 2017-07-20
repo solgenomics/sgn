ec332773bd706e5988917f3183591b2ab72aba85
Bryan Ellerbrock <bje24@cornell.edu>
2017-07-20 15:50:04 -0400

    Update Genotype.pm
    test more stringent genotype codes

--- lib/CXGN/Genotype.pm
+++ lib/CXGN/Genotype.pm
@@ -193,36 +193,34 @@ sub compare_parental_genotypes {
 
 	my @matrix; #mom, dad, self, 1=possible 0=impossible
 
-	$matrix[ 0 ][ 0 ][ 1 ] =0;
 	$matrix[ 0 ][ 0 ][ 2 ] =0;
-	$matrix[ 0 ][ 1 ][ 2 ] =0;
-	$matrix[ 1 ][ 0 ][ 2 ] =0;
 	$matrix[ 2 ][ 2 ][ 0 ] =0;
-	$matrix[ 2 ][ 2 ][ 1 ] =0;
-
-	$matrix[ 2 ][ 1 ][ 0 ] =0;
-	$matrix[ 1 ][ 2 ][ 0 ] =0;
-
-	$matrix[ 2 ][ 0 ][ 2 ] =0;
-	$matrix[ 2 ][ 0 ][ 0 ] =0;
-	$matrix[ 0 ][ 2 ][ 0 ] =0;
-	$matrix[ 0 ][ 2 ][ 2 ] =0;
-
-	$matrix[ 0 ][ 2 ][ 1 ] =1;
 	$matrix[ 0 ][ 0 ][ 0 ] =1;
-	$matrix[ 1 ][ 0 ][ 0 ] =1;
-	$matrix[ 1 ][ 0 ][ 1 ] =1;
-	$matrix[ 1 ][ 1 ][ 0 ] =1;
-	$matrix[ 1 ][ 1 ][ 1 ] =1;
-	$matrix[ 1 ][ 1 ][ 2 ] =1;
-	$matrix[ 1 ][ 2 ][ 1 ] =1;
-	$matrix[ 1 ][ 2 ][ 2 ] =1;
-	$matrix[ 0 ][ 1 ][ 0 ] =1;
-	$matrix[ 0 ][ 1 ][ 1 ] =1;
-	$matrix[ 2 ][ 0 ][ 1 ] =1;
-  $matrix[ 2 ][ 1 ][ 1 ] =1;
-	$matrix[ 2 ][ 1 ][ 2 ] =1;
 	$matrix[ 2 ][ 2 ][ 2 ] =1;
+
+	$matrix[ 0 ][ 0 ][ 1 ] =-1;
+	$matrix[ 0 ][ 1 ][ 2 ] =-1;
+	$matrix[ 1 ][ 0 ][ 2 ] =-1;
+	$matrix[ 2 ][ 2 ][ 1 ] =-1;
+	$matrix[ 2 ][ 1 ][ 0 ] =-1;
+	$matrix[ 1 ][ 2 ][ 0 ] =-1;
+	$matrix[ 2 ][ 0 ][ 2 ] =-1;
+	$matrix[ 2 ][ 0 ][ 0 ] =-1;
+	$matrix[ 0 ][ 2 ][ 0 ] =-1;
+	$matrix[ 0 ][ 2 ][ 2 ] =-1;
+	$matrix[ 0 ][ 2 ][ 1 ] =-1;
+	$matrix[ 1 ][ 0 ][ 0 ] =-1;
+	$matrix[ 1 ][ 0 ][ 1 ] =-1;
+	$matrix[ 1 ][ 1 ][ 0 ] =-1;
+	$matrix[ 1 ][ 1 ][ 1 ] =-1;
+	$matrix[ 1 ][ 1 ][ 2 ] =-1;
+	$matrix[ 1 ][ 2 ][ 1 ] =-1;
+	$matrix[ 1 ][ 2 ][ 2 ] =-1;
+	$matrix[ 0 ][ 1 ][ 0 ] =-1;
+	$matrix[ 0 ][ 1 ][ 1 ] =-1;
+	$matrix[ 2 ][ 0 ][ 1 ] =-1;
+  	$matrix[ 2 ][ 1 ][ 1 ] =-1;
+	$matrix[ 2 ][ 1 ][ 2 ] =-1;
 
 	#print "self markers". Dumper $self_markers;
 
