357189effcc3bd766bbf11a295aba031d1964c79
zacherl008 <vagrant@contrib-jessie.raw>
2017-07-21 16:45:28 +0000

    merge two scripts

--- bin/pedigreeplausibility.pl
+++ bin/pedigreeplausibility.pl
@@ -66,63 +66,64 @@ while (my $row = $stock_rs->next()) {
 
     if ($parents->{'mother'} && $parents->{'father'}) {
 
-	  my $gts = CXGN::Genotype::Search->new( {
-	    bcs_schema => $schema,
-	    accession_list => [ $row->stock_id ],
-	    protocol_id => $protocol_id,
-							    });
+      my $gts = CXGN::Genotype::Search->new( {
+          bcs_schema => $schema,
+          accession_list => [ $row->stock_id ],
+          protocol_id => $protocol_id,
+      });
+      my @self_gts = $gts->get_genotype_info_as_genotype_objects();
 
-  my @self_gts = $gts->get_genotype_info_as_genotype_objects();
-	$gts = CXGN::Genotype::Search->new( {
-	    bcs_schema => $schema,
-	    accession_list => [ $parents[0]->[0]],
-	    protocol_id => $protocol_id,
-							    });
-  my @mom_gts;
-	@mom_gts = $gts->get_genotype_info_as_genotype_objects();
-  #if (@mom_gts) { print $OUT  Dumper @mom_gts;}
+      if (!@self_gts) {
+        print STDERR "Genotype of accession ".$row->uniquename()." not available. Skipping...\n";
+        next;
+    }
 
-	$gts = CXGN::Genotype::Search->new( {
-	    bcs_schema => $schema,
-	    accession_list => [ $parents[1]->[0]],
-	    protocol_id => $protocol_id,
-							    });
+      my $mom_gts = CXGN::Genotype::Search->new( {
+        bcs_schema => $schema,
+        accession_list => [$parents->{'mother_id'}],
+        protocol_id => $protocol_id,
+      });
+    my @mom_gts = $mom_gts->get_genotype_info_as_genotype_objects();
 
+    if (!@mom_gts) {
+        print STDERR "Genotype of female parent missing. Skipping.\n";
+        next;
+    }
+    my $dad_gts;
+    my @dad_gts;
 
-	my (@dad_gts) = $gts->get_genotype_info_as_genotype_objects();
+    if ($parents->{'father_id'} ==  $parents->{'mother_id'}){
+        @dad_gts = @mom_gts;
+    }
+    else{
+    $dad_gts = CXGN::Genotype::Search->new({
+        bcs_schema => $schema,
+        accession_list => [$parents->{'father_id'}],
+        protocol_id => $protocol_id,
+    });
+    @dad_gts = $dad_gts->get_genotype_info_as_genotype_objects();
+    }
 
-	if (! (@self_gts)) {
-	    print STDERR "Genotype of accession ".$row->uniquename()." not available. Skipping...\n";
-	    next;
-	}
-	if (!@mom_gts) {
-	    print STDERR "Genotype of female parent missing. Skipping.\n";
-	    next;
-	}
-	if (! @dad_gts) {
-	    print STDERR "Genotype of male parent missing. Skipping.\n";
-	    next;
-	}
-      open($OUT, '>', $opt_o);
+    if (!@dad_gts) {
+        print STDERR "Genotype of male parent missing. Skipping.\n";
+        next;
+    }
 
 #print $OUT "d child genos".$self_gts;
   #print $OUT "d father genos".$dad_gts;
   #print $OUT "d mother genos".$mom_gts;
 ##check length
 ##index of array for
-my $s;
-my $m;
-my $d;
-$s = shift @self_gts;
-    #if (@mom_gts) { print $OUT  Dumper @mom_gts;}
-$m = shift @mom_gts;
-$d = shift @dad_gts;
-		    my ($concordant, $discordant, $non_informative) = $s->compare_parental_genotypes($m, $d);
-		    my $score = 1 - ($concordant / ($concordant + $discordant));
-		    #push @scores, $score;
-        print STDERR "scores are". $score. "\n";
-		    print $OUT join "\t", map { ($_->name(), $_->id()) } ($s, $m, $d);
-		    print $OUT "\t$score\n";
+    my $s = shift @self_gts;
+    my $m = shift @mom_gts;
+    my $d = shift @dad_gts;
+
+    my ($concordant, $discordant, $non_informative) = $s->compare_parental_genotypes($m, $d);
+    my $score = 1- ($concordant / ($concordant + $discordant));
+
+    print STDERR "scores are". $score. "\n";
+    print $OUT join "\t", map { ($_->name(), $_->id()) } ($s, $m, $d);
+    print $OUT "\t$score\n";
 }
 }
 
