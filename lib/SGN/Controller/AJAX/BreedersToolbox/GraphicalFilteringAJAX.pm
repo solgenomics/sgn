8a4866edf2a790db5dc6aba89c0e1c46ba274cc4
dauglyon <dauglyon@users.noreply.github.com>
2017-08-07 13:40:36 -0400

    added plot list filtering and url parameters to determine the list to filter

--- lib/SGN/Controller/AJAX/BreedersToolbox/GraphicalFilteringAJAX.pm
+++ lib/SGN/Controller/AJAX/BreedersToolbox/GraphicalFilteringAJAX.pm
@@ -24,8 +24,8 @@ has 'schema' => (
 		 lazy_build => 1,
 		);
 
-sub common_plot_traits : Path('/ajax/trial/common_plot_traits') : ActionClass('REST') {}
-sub common_plot_traits_GET : Args(0) {
+sub common_traits_by_trial_list : Path('/ajax/plot/common_traits_by/trial_list') : ActionClass('REST') {}
+sub common_traits_by_trial_list_GET : Args(0) {
    my $self = shift;
    my $c = shift;
 
@@ -47,18 +47,133 @@ sub common_plot_traits_GET : Args(0) {
 
    #get all common trial traits
    my $breedersearch =  CXGN::BreederSearch->new({"dbh"=>$c->dbc->dbh});
-   my @criteria2 = ['trials','traits'];
-   my $dataref2 = {
+   my @trait_criteria = ['trials','traits'];
+   my $trait_dataref = {
       'traits' => {
          'trials' => $trials_string
       }
    };
-   my $queryref2 = {
+   my $trait_queryref = {
       'traits' => {
          'trials' => 1
       }
    };
-   my $traits_results_ref = $breedersearch->metadata_query(@criteria2, $dataref2, $queryref2);
+   my $traits_results_ref = $breedersearch->metadata_query(@trait_criteria, $trait_dataref, $trait_queryref);
+   my @trait_list = map { $$_[0] } @{$$traits_results_ref{results}};
+   my $traits_string = "\'".join( "\',\'",@trait_list)."\'";
+
+   #get all plots with every trait of each trial
+   my @criteria = ['trials','traits','plots'];
+   my $dataref = {
+      'plots' => {
+         'trials' => $trials_string,
+         'traits' => $traits_string
+      }
+   };
+   my $queryref = {
+      'plots' => {
+         'trials' => 0,
+         'traits' => 1
+      }
+   };
+   my $plots_results_ref = $breedersearch->metadata_query(@criteria, $dataref, $queryref);
+
+   my @plot_list = map { $$_[0] } @{$$plots_results_ref{results}};
+
+
+   my $phenotypes_search = CXGN::Phenotypes::PhenotypeMatrix->new(
+      bcs_schema=> $schema,
+      search_type => "Native",
+      data_level => "plot",
+      trait_list=> \@trait_list,
+      plot_list=>  \@plot_list
+   );
+   my @data = $phenotypes_search->get_phenotype_matrix();
+
+   $c->stash->{rest} = {
+      traits => $traits_results_ref,
+      data => \@data
+   };
+}
+
+sub common_traits_by_plot_list : Path('/ajax/plot/common_traits_by/plot_list') : ActionClass('REST') {}
+sub common_traits_by_plot_list_GET : Args(0) {
+   my $self = shift;
+   my $c = shift;
+
+   #get list ID from url param
+   my $plot_list_id = $c->request->param('plot_list_id');
+
+   #get userinfo from db
+   my $schema = $c->dbic_schema("Bio::Chado::Schema");
+   my $user = $c->user();
+   my $user_id = $user->get_object()->get_sp_person_id();
+
+   #get list contents
+   my $dbh = $schema->storage()->dbh();
+   my $tl = CXGN::List->new({ dbh => $dbh, list_id => $plot_list_id, owner => $user_id });
+   my $plots = $tl->elements();
+   my $plot_id_rs = $schema->resultset("Stock::Stock")->search( { uniquename => { in => [ @$plots ]} });
+   my @plot_list = map { $_->stock_id() } $plot_id_rs->all();
+   my $plots_string = "\'".join( "\',\'",@plot_list)."\'";
+
+   #get all common plot traits
+   my $breedersearch =  CXGN::BreederSearch->new({"dbh"=>$c->dbc->dbh});
+   my @criteria = ['plots','traits'];
+   my $dataref = {
+      'traits' => {
+         'plots' => $plots_string
+      }
+   };
+   my $queryref = {
+      'traits' => {
+         'plots' => 1
+      }
+   };
+   my $traits_results_ref = $breedersearch->metadata_query(@criteria, $dataref, $queryref);
+   my @trait_list = map { $$_[0] } @{$$traits_results_ref{results}};
+
+   my $phenotypes_search = CXGN::Phenotypes::PhenotypeMatrix->new(
+      bcs_schema=> $schema,
+      search_type => "Native",
+      data_level => "plot",
+      trait_list=> \@trait_list,
+      plot_list=>  \@plot_list
+   );
+   my @data = $phenotypes_search->get_phenotype_matrix();
+
+   $c->stash->{rest} = {
+      traits => $traits_results_ref,
+      data => \@data
+   };
+}
+
+sub common_traits_by_trials : Path('/ajax/plot/common_traits_by/trials') : ActionClass('REST') {}
+sub common_traits_by_trials_GET : Args(0) {
+   my $self = shift;
+   my $c = shift;
+
+   #get schema
+   my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+   #parse trial params
+   my $trial_ids = $c->request->parameters->{'trial_id'};
+   my $trials_string = "\'".join( "\',\'",@$trial_ids)."\'";
+
+   #get all common trial traits
+   my $breedersearch =  CXGN::BreederSearch->new({"dbh"=>$c->dbc->dbh});
+   my @trait_criteria = ['trials','traits'];
+   my $trait_dataref = {
+      'traits' => {
+         'trials' => $trials_string
+      }
+   };
+   my $trait_queryref = {
+      'traits' => {
+         'trials' => 1
+      }
+   };
+   my $traits_results_ref = $breedersearch->metadata_query(@trait_criteria, $trait_dataref, $trait_queryref);
    my @trait_list = map { $$_[0] } @{$$traits_results_ref{results}};
    my $traits_string = "\'".join( "\',\'",@trait_list)."\'";
 
