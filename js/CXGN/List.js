aa7235fb9bd249977dbe1d25708f9b5ebbdcc2eb
nickmorales <nmorales3142@gmail.com>
2017-08-09 16:39:05 -0400

    empty element can be useful for unselecting a value

--- js/CXGN/List.js
+++ js/CXGN/List.js
@@ -631,7 +631,7 @@ CXGN.List.prototype = {
 
         var html = '<select class="form-control input-sm" id="'+div_name+'_list_select" name="'+div_name+'_list_select" >';
         if (empty_element) {
-            html += '<option value="" disabled>'+empty_element+'</option>\n';
+            html += '<option value="" >'+empty_element+'</option>\n';
         }
         html += '<option disabled>--------YOUR LISTS BELOW--------</option>';
         for (var n=0; n<lists.length; n++) {
