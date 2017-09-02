f89aad6bbf2bfe4bd73b7615b6308359ea6b55e0
titima15 <tt15@cornell.edu>
2017-09-02 21:37:48 +0000

    add location, description and fix add_crossingtrial function

--- js/CXGN/BreedersToolbox/Crosses.js
+++ js/CXGN/BreedersToolbox/Crosses.js
@@ -40,18 +40,36 @@ jQuery(document).ready(function($) {
 
           var crossingTrialName = $("#crossingtrial_name").val();
           if (!crossingTrialName) {
-              alert("A crossing trial name is required");
+              alert("Crossing trial name is required");
               return;
           }
 
           var breeding_program_id = $("#program").val();
           if (!breeding_program_id) {
-              alert("A breeding program is required");
+              alert("Breeding program is required");
               return;
           }
 
           var visibleToRole = $("#visible_to_role").val();
+
           var location = $("#location").val();
+          if (!location) {
+              alert ("Location is required");
+              return;
+          }
+
+          var year = $("#add_project_year").val();
+          if (!year) {
+              alert ("Year is required");
+              return;
+          }
+
+          var project_description = $('textarea#add_project_description').val();
+          if (!project_description) {
+              alert ("Description is required");
+              return;
+          }
+
           var folder_name = $("#crossingtrial_folder_name").val();
           var folder_id;
           if (folder_name) {  // get id if folder with this name already exisits
@@ -60,7 +78,7 @@ jQuery(document).ready(function($) {
           else {
               folder_id = $("#crossingtrial_folder_id").val();
           }
-          add_crossingtrial(crossingTrialName, breeding_program_id, visibleToRole, location, folder_name, folder_id);
+          add_crossingtrial(crossingTrialName, breeding_program_id, visibleToRole, location, year, project_description, folder_name, folder_id);
 
       });
 
@@ -374,22 +392,24 @@ jQuery(document).ready(function($) {
         return names;
     }
 
-    function add_crossingtrial(crossingTrialName, breeding_program_id, visibleToRole, location, folder_name, folder_id) {
+    function add_crossingtrial(crossingTrialName, breeding_program_id, visibleToRole, location, year, project_description, folder_name, folder_id) {
         $.ajax({
             url: '/ajax/cross/add_crossingtrial',
-          timeout: 3000000,
-          dataType: "json",
-          type: 'POST',
-          data: 'crossingTrialName=' + crossingtrial_name +  '&paternal=' + paternal + '&maternal_parents=' + maternal_parents + '&paternal_parents=' + paternal_parents + '&progeny_number=' + progenyNumber + '&flower_number=' + flowerNumber + '&fruit_number=' + fruitNumber + '&seed_number=' + seedNumber + '&prefix=' + prefix + '&suffix=' + suffix + '&visible_to_role' + visibleToRole + '&breeding_program_id=' + breeding_program_id + '&location=' + location + '&folder_name=' + folder_name + '&folder_id=' + folder_id,
-          beforeSend: function() {
-              jQuery("#create_cross").modal("hide");
+            timeout: 3000000,
+            dataType: "json",
+            type: 'POST',
+            data: 'crossingtrial_name=' + crossingTrialName + '&program=' + breeding_program_id + '&visible_to_role=' + visibleToRole
+            + '&location=' + location + '&add_project_year=' + year + '&add_project_description=' + project_description
+            + '&add_cross_folder_name=' + folder_name +'&add_cross_folder_id=' + folder_id,
+            beforeSend: function() {
+              jQuery("#create_crossingtrial").modal("hide");
               jQuery("#working_modal").modal("show");
           },
           error: function(response) {
-              alert("An error occurred. Please try again later!" + JSON.stringify(response));
+              alert("An error occurred!" + JSON.stringify(response));
           },
           parseerror: function(response) {
-              alert("A parse error occurred. Please try again." + response);
+              alert("A parse error occurred!" + response);
           },
           success: function(response) {
               if (response.error) {
