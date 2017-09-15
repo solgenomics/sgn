7c504800cc76ca8070db11e294f2c623c59a0fb9
aco46 <aco46@cornell.edu>
2017-09-15 21:56:24 +0000

    retrieved trial traits assayed

--- lib/CXGN/Phenotypes/TrialPhenotype.pm
+++ lib/CXGN/Phenotypes/TrialPhenotype.pm
@@ -1,6 +1,29 @@
 
 package CXGN::Phenotypes::TrialPhenotype;
 
+
+=head1 NAME
+
+CXGN::Phenotypes::TrialPhenotype - an object to handle retrieving of trial phenotype and field information.
+
+=head1 USAGE
+
+my $phenotypes_heatmap = CXGN::Phenotypes::TrialPhenotype->new(
+	bcs_schema=>$schema,
+	trial_id=>$trial_id
+);
+my @phenotype = $phenotypes_heatmap->get_trial_phenotypes_heatmap();
+
+=head1 DESCRIPTION
+
+
+=head1 AUTHORS
+
+
+=cut
+
+
+
 use strict;
 use warnings;
 use Moose;
@@ -11,11 +34,23 @@ use CXGN::Phenotypes::SearchFactory;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
+has 'bcs_schema' => (
+	isa => 'Bio::Chado::Schema',
+	is => 'rw',
+	required => 1,
+);
+
+has 'trial_id' => (
+	isa => 'Int',
+	is => 'rw',
+    required => 1,
+);
 
 
-
-sub trial_phenotypes_heatmap {
+sub get_trial_phenotypes_heatmap {
     my $self = shift;
+    my $schema = $self->bcs_schema;
+    my $trial_id = $self->trial_id;
     
     return ;
 }
