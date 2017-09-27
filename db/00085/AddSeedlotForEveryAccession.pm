92694a2e4cad539748c697e115fbe3b7ff839dc9
nickmorales <nmorales3142@gmail.com>
2017-09-27 09:12:38 -0400

    better assignment of breeding program for seedlots created from patch

--- db/00085/AddSeedlotForEveryAccession.pm
+++ db/00085/AddSeedlotForEveryAccession.pm
@@ -41,6 +41,7 @@ use SGN::Model::Cvterm;
 use CXGN::Stock::Seedlot;
 use CXGN::Stock::Seedlot::Transaction;
 use Data::Dumper;
+use CXGN::BreedersToolbox::Projects;
 
 extends 'CXGN::Metadata::Dbpatch';
 
@@ -50,33 +51,47 @@ Update cassava_trait db prefix
 
 has '+prereq' => (
 	default => sub {
-        [],
+        ['AddSeedlotCurrentCountCvterm'],
     },
 
   );
 
 sub patch {
-    my $self=shift;
+	my $self=shift;
 
-    print STDOUT "Executing the patch:\n " .   $self->name . ".\n\nDescription:\n  ".  $self->description . ".\n\nExecuted by:\n " .  $self->username . " .";
+	print STDOUT "Executing the patch:\n " .   $self->name . ".\n\nDescription:\n  ".  $self->description . ".\n\nExecuted by:\n " .  $self->username . " .";
 
-    print STDOUT "\nChecking if this db_patch was executed before or if previous db_patches have been executed.\n";
+	print STDOUT "\nChecking if this db_patch was executed before or if previous db_patches have been executed.\n";
 
-    print STDOUT "\nExecuting the SQL commands.\n";
-    my $schema = Bio::Chado::Schema->connect( sub { $self->dbh->clone } );
+	print STDOUT "\nExecuting the SQL commands.\n";
+	my $schema = Bio::Chado::Schema->connect( sub { $self->dbh->clone } );
 
 	my $accession_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
 	my $seedlot_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'seedlot', 'stock_type')->cvterm_id();
 	my $plot_of_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plot_of', 'stock_relationship')->cvterm_id();
 	my $exp_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'field_layout', 'experiment_type')->cvterm_id();
 	my $bp_rel_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'breeding_program_trial_relationship', 'project_relationship')->cvterm_id();
-    my $seedlot_rs = $schema->resultset("Stock::Stock")->search({
+	my $seedlot_rs = $schema->resultset("Stock::Stock")->search({
 		type_id=>$seedlot_type_id
 	});
+
+	# If the patch is unable to determine to which breeding program the new seedlot belongs to, the new seedlot will be assigned to this default breeding program. Please change it here in the file to match your database.
+	my $userdefined_default_breeding_program = 'test';
+	my $default_breeding_program_id = $schema->resultset('Project::Project')->find({name=>$userdefined_default_breeding_program})->project_id();
+
+	# FIRST, ADDS current_count property to seedlots already in database.
+	print STDOUT "Adding current_count to any existing seedlots in database\n";
+
 	my %existing_seedlots;
 	while(my $r=$seedlot_rs->next){
 		$existing_seedlots{$r->uniquename}++;
+
+		my $sl = CXGN::Stock::Seedlot->new(schema => $schema, seedlot_id=>$r->stock_id);
+		$sl->set_current_count_property();
 	}
+
+	# SECOND, DETERMINES which breeding program an accession has been used most in. This will be used to assign the seedlot later on.
+	print STDOUT "Determining which breeding program an accession has been used most in. This will be used to assign the seedlot later on.\n";
 
 	my $accession_rs = $schema->resultset("Stock::Stock")->search(
 		{
@@ -105,24 +120,70 @@ sub patch {
 		$highest_accession_bp_hash{$k} = $most_used;
 	}
 	#print STDERR Dumper \%highest_accession_bp_hash;
-	
 
-	while (my ($k,$v) = each %accession_hash){
-		my $accession_uniquename = $k;
-		my $accession_stock_id = $v;
+	# THIRD, DETERMINES if an accession has an organization associated that is actually a breeding program. If so, this breeding program will be assigned to the new seedlot.
+	print STDOUT "Determining if an accession has an organization associated that is actually a breeding program. If so, this breeding program will be assigned to the new seedlot.\n";
+
+	my $p = CXGN::BreedersToolbox::Projects->new({schema=>$schema});
+	my $breeding_programs = $p->get_breeding_programs();
+	my %available_breeding_program_names;
+	foreach (@$breeding_programs){
+		$available_breeding_program_names{$_->[1]}++;
+	}
+	
+	my $organization_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'organization', 'stock_property')->cvterm_id();
+	my $organization_accession_rs = $schema->resultset("Stock::Stock")->search(
+		{
+			'me.type_id'=>$accession_type_id,
+			'stockprops.type_id'=>$organization_cvterm_id
+		},
+		{
+			'join'=>{'stockprops'=>'type'},
+			'+select'=>['stockprops.value'],
+			'+as'=>['organization']
+		}
+	);
+	my %accession_organization_hash;
+	while (my $r=$organization_accession_rs->next){
+		my $organization = $r->get_column('organization');
+		if (exists($available_breeding_program_names{$organization})){
+			$accession_organization_hash{$r->uniquename} = $organization;
+		}
+	}
+
+
+	# THIRD, CREATES a seedlot for every accession in the database. Attempts to assign breeding program to seedlot based on accession's usage or accession's organization. If neither of these are found, then USERDEFINED default is assigned.
+	print STDOUT "Creating seedlots\n";
+
+	my $full_accession_rs = $schema->resultset("Stock::Stock")->search({
+		type_id=>$accession_type_id
+	});
+
+	while (my $r = $full_accession_rs->next){
+		my $accession_uniquename = $r->uniquename;
+		my $accession_stock_id = $r->stock_id;
 		my $seedlot_uniquename = $accession_uniquename."_001";
-		
+
 		if(exists($existing_seedlots{$seedlot_uniquename})){
 			next;
 		}
-	
+
+		my $seedlot_bp_id;
+		if (exists($accession_organization_hash{$accession_uniquename})){
+			$seedlot_bp_id = $accession_organization_hash{$accession_uniquename};
+		} elsif (exists($highest_accession_bp_hash{$accession_uniquename})){
+			$seedlot_bp_id = $highest_accession_bp_hash{$accession_uniquename};
+		} else {
+			$seedlot_bp_id = $default_breeding_program_id;
+		}
+
 		my $sl = CXGN::Stock::Seedlot->new(schema => $schema);
 		$sl->uniquename($seedlot_uniquename);
 		$sl->location_code("NA");
 		$sl->accession_stock_ids([$accession_stock_id]);
 		#$sl->organization_name();
 		#$sl->population_name($population_name);
-		$sl->breeding_program_id($highest_accession_bp_hash{$accession_uniquename});
+		$sl->breeding_program_id($seedlot_bp_id);
 		$sl->check_name_exists(0);
 		#TO DO
 		#$sl->cross_id($cross_id);
@@ -139,11 +200,11 @@ sub patch {
 		$transaction->description("Auto generated seedlot from accession. DbPatch 00085");
 		$transaction->operator('nmorales');
 		$transaction->store();
-        $sl->set_current_count_property();
+		$sl->set_current_count_property();
 	}
 	
 
-    print "You're done!\n";
+	print "You're done!\n";
 }
 
 
