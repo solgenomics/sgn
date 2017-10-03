2f9f11c7b8290d6c52cb34847828f43bed584b9f
aco46 <aco46@cornell.edu>
2017-10-03 18:28:20 +0000

    position text on label

--- lib/SGN/Controller/StockBarcode.pm
+++ lib/SGN/Controller/StockBarcode.pm
@@ -200,7 +200,7 @@ sub download_pdf_labels :Path('/barcode/stock/download/pdf') :Args(0) {
           $fdata_block = "blk:".$stockprop_hash{$stock_id}->{'block'};
           $fdata_rep = "rep:".$stockprop_hash{$stock_id}->{'replicate'};
           $fdata_plot = "plot:".$stockprop_hash{$stock_id}->{'plot number'};
-          $musa_row_col_number = "row:".$stockprop_hash{$stock_id}->{'row_number'}.' '."col:".$stockprop_hash{$stock_id}->{'col_number'};
+          $musa_row_col_number = "row no:".$stockprop_hash{$stock_id}->{'row_number'}.' '."column no:".$stockprop_hash{$stock_id}->{'col_number'};
           $row_col_number = "rw/cl:".$stockprop_hash{$stock_id}->{'row_number'}."/".$stockprop_hash{$stock_id}->{'col_number'};
           my $h_acc = $dbh->prepare("select stock.uniquename, stock.stock_id FROM stock join stock_relationship on (stock.stock_id = stock_relationship.object_id) where stock_relationship.subject_id =? and stock.type_id=?;");
 
@@ -583,40 +583,36 @@ sub download_pdf_labels :Path('/barcode/stock/download/pdf') :Args(0) {
            my $xposition = $left_margin + ($label_count -1) * $final_barcode_width;
            my $yposition = $ypos -7;
            my $label_text = $found[$i]->[1];
-           if ($found[$i]->[5] eq 'plot'){
-               $label_text = $found[$i]->[2];
-           }
-           my $label_size =  7;
+        #    if ($found[$i]->[5] eq 'plot'){
+        #        $label_text = $found[$i]->[2];
+        #    }
+           my $label_size =  8;
            my $label_size_stock =  10;
-           my $yposition_8 = $ypos + 2;
            my $yposition_2 = $ypos - 10;
            my $yposition_3 = $ypos - 20;
            my $yposition_4 = $ypos - 30;
            my $yposition_5 = $ypos - 40;
            my $yposition_6 = $ypos - 50;
            my $yposition_7 = $ypos - 60;
-           $label_text_6 = $found[$i]->[2];
+           my $yposition_8 = $ypos - 70;
+           $label_text_6 = "accession: ".$found[$i]->[2];
+           my $parents_20A4 = "pedigree: ".$parents;
            $label_text_5 = $found[$i]->[11];
-           $label_text_4 = $found[$i]->[12];
+           $label_text_4 = $found[$i]->[8];
            $label_text_8 = $found[$i]->[10];
-           $pages[$page_nr-1]->string($font, $label_size_stock, $xposition, $yposition_8, $label_text);
-           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_4, $label_text_8);
-           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_3, $label_text_4);
-           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_2, $label_text_5);
+           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_2, $label_text);
+           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_3, $label_text_6);
+           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_5, $label_text_5);
+           $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_4, $label_text_4);
+            $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_6, $label_text_8);
            if ($found[$i]->[5] eq 'accession'){
-               $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_6, $parents);
+               $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_7, $parents_20A4);
            }else{
-                $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_6, $parents);
+                $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_7, $parents_20A4);
                 #$pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_7, $parents);
-                $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_5, $added_text);
+                $pages[$page_nr-1]->string($font, $label_size, $xposition, $yposition_8, $added_text);
            }
-           
-           if ($found[$i]->[5] eq 'accession'){
-               $pages[$page_nr-1]->image(image=>$image, xpos=>$left_margin + 200 + ($label_count -1) * $final_barcode_width, ypos=>$ypos, xalign=>0, yalign=>2, xscale=>$scalex, yscale=>$scaley);
-           }else{
-               $pages[$page_nr-1]->image(image=>$image, xpos=>$left_margin + 200 + ($label_count -1) * $final_barcode_width, ypos=>$ypos, xalign=>0, yalign=>2, xscale=>$scalex, yscale=>$scaley);
-           }
-           
+           $pages[$page_nr-1]->image(image=>$image, xpos=>$left_margin + 200 + ($label_count -1) * $final_barcode_width, ypos=>$ypos, xalign=>0, yalign=>2, xscale=>$scalex, yscale=>$scaley);
          }
      }
      
