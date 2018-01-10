5377dc0242ed02124595d418c6f6488fd13bcdc8
David Lyon <dauglyon@users.noreply.github.com>
2018-01-10 14:04:46 -0800

    Fixes BrAPI Progeny call null values bug
    Array was slicing past end causing null values to be returned when the number of fetched items was less than the page size.

--- lib/CXGN/BrAPI/v1/Germplasm.pm
+++ lib/CXGN/BrAPI/v1/Germplasm.pm
@@ -267,7 +267,11 @@ sub germplasm_progeny {
         }
     }
 	my $total_count = scalar @{$result->{data}};
-	my $result->{data} = [@{$result->{data}}[$page_size*$page .. $page_size*($page+1)-1]];
+	my $last_item = $page_size*($page+1)-1;
+	if($last_item > $total_count-1){
+	    $last_item = $total_count-1
+	}
+	my $result->{data} = [@{$result->{data}}[$page_size*$page .. $last_item]];
 	my @data_files;
 	my $pagination = CXGN::BrAPI::Pagination->pagination_response($total_count,$page_size,$page);
 	return CXGN::BrAPI::JSONResponse->return_success($result, $pagination, \@data_files, $status, 'Germplasm progeny result constructed');
