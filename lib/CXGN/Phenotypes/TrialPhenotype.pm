c467f0632873cf83b916964f39b59308c2f243b1
aco46 <aco46@cornell.edu>
2018-01-16 17:54:10 +0000

    sort field info by plot_number if row and col are missing

--- lib/CXGN/Phenotypes/TrialPhenotype.pm
+++ lib/CXGN/Phenotypes/TrialPhenotype.pm
@@ -101,7 +101,7 @@ sub get_trial_phenotypes_heatmap {
 
 	my $from_clause = $columns{'from_clause'};
 
-	my $order_clause = " ORDER BY 7, 8 ASC";
+	my $order_clause = " ORDER BY 4, 7, 8 ASC";
 	my $numeric_regex = '^[0-9]+([,.][0-9]+)?$';
 	my $numeric_regex_2 = '/^\s*$/';
 
