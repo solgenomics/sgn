5e1a28f24fea934182fdf320664ca929a5d3f194
nickmorales <nmorales3142@gmail.com>
2018-02-06 11:07:15 -0500

    fix merge

--- lib/SGN/Controller/AJAX/TrialMetadata.pm
+++ lib/SGN/Controller/AJAX/TrialMetadata.pm
@@ -1191,14 +1191,6 @@ sub delete_field_coord : Path('/ajax/phenotype/delete_field_coords') Args(0) {
     my $trial_layout = CXGN::Trial::TrialLayout->new({ schema => $schema, trial_id => $trial_id, experiment_type => 'field_layout' });
     $trial_layout->generate_and_cache_layout();
 
-    my $dbh = $c->dbc->dbh();
-    my $bs = CXGN::BreederSearch->new( { dbh=>$dbh, dbname=>$c->config->{dbname}, } );
-    my $refresh = $bs->refresh_matviews($c->config->{dbhost}, $c->config->{dbname}, $c->config->{dbuser}, $c->config->{dbpass}, 'stockprop');
-    if ($refresh->{error}) {
-        $c->stash->{rest} = { error => $refresh->{'error'} };
-        $c->detach();
-    }
-
     $c->stash->{rest} = {success => 1};
 }
 
