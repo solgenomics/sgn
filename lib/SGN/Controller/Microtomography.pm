ccc4d514ca47011cefad8b5244ce461d271b1d01
Surya Saha <ss2489@cornell.edu>
2018-03-10 21:02:42 -0500

    added controller for microct pages

---
+++ lib/SGN/Controller/Microtomography.pm
@@ -0,0 +1,28 @@
+package SGN::Controller::Microtomography;
+
+use Moose;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+SGN::Controller::Microtomography - show Microtomography pages for CG.org. add subroutines for other pages in Microtomography menu
+
+=cut
+
+
+sub microtomography_index :Path('/microtomography/index') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/microtomography/index.mas';
+}
+
+sub microtomography_javier_bruker_2017 :Path('/microtomography/javier_bruker_2017') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/microtomography/javier_bruker_2017.mas';
+}
+
+1;
