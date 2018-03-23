9e71ab43d2e195d12c531607b49e4dc54d9253d3
Isaak <iyt2@cornell.edu>
2018-03-23 12:50:22 +0100

    clean up..

--- R/anova.r
+++ R/anova.r
@@ -2,7 +2,6 @@
 
  #runs ANOVA.
  
-
  #AUTHOR
  # Isaak Y Tecle (iyt2@cornell.edu)
 
@@ -25,107 +24,106 @@ inputFiles  <- scan(grep("input_files", allArgs, value = TRUE),
 
 phenoDataFile <- grep("phenotype_data", inputFiles, value = TRUE)
 message('pheno file: ', phenoDataFile)
+
 traitsFile <- grep("traits", inputFiles, value = TRUE)
 message('traits file: ', traitsFile)
 
 #phenoDataFile <- '/export/prod/tmp/localhost/GBSApeKIgenotypingv4/solgs/cache/phenotype_data_139.txt';
 #phenoDataFile <- '/mnt/hgfs/cxgn/phenotype_data_RCBD_1596.txt';
 #phenoDataFile <- '/mnt/hgfs/cxgn/1-block_rcbd.txt'
+
 phenoData <- fread(phenoDataFile,
                      na.strings=c("NA", "-", " ", ".", ".."))
 
 phenoData <- data.frame(phenoData)
-#head(phenoData)
+
 traits  <- scan(traitsFile,
                 what = "character")
 print(traits)
 
 traits  <- strsplit(traits, "\t")
-print(traits)
+
+#traits <- 'dry.yield.CO_334.0000014'
+
 #needs more work for multi traits anova
-
 for (trait in traits) {
-#trait <- 'DY'
-message('trait: ', trait)
-  anovaFiles     <- grep("anova_table",
-                         outputFiles,
-                         value = TRUE)
 
-  message('anova file: ', anovaFiles)
-  anovaHtmlFile  <- grep("html",
-                         anovaFiles,
-                         value = TRUE)
-
-  message('anova html file: ', anovaHtmlFile)
-  anovaTxtFile   <- grep("txt",
-                         anovaFiles,
-                         value = TRUE)
-
-  message('anova txt file: ', anovaTxtFile)
-  modelSummFile <- grep("anova_model",
-                        outputFiles,
-                        value = TRUE)
-
-  message('model file: ', modelSummFile)
-  adjMeansFile  <- grep("adj_means",
-                        outputFiles,
-                        value = TRUE)
-
-  message('means file: ', adjMeansFile)
-
-
-  diagnosticsFile  <- grep("anova_diagnostics",
-                        outputFiles,
+    message('trait: ', trait)
+    anovaFiles     <- grep("anova_table",
+                           outputFiles,
                            value = TRUE)
 
-errorFile  <- grep("anova_error",
-                        outputFiles,
-                        value = TRUE)
+    message('anova file: ', anovaFiles)
+    anovaHtmlFile  <- grep("html",
+                           anovaFiles,
+                           value = TRUE)
 
- # anovaOut   <- runAnova(phenoData, 'dry.yield.CO_334.0000014')
+    message('anova html file: ', anovaHtmlFile)
+    anovaTxtFile   <- grep("txt",
+                           anovaFiles,
+                           value = TRUE)
 
-  anovaOut <- runAnova(phenoData, trait)
-  if (class(anovaOut)[1] == 'merModLmerTest') {
-    
-      png(diagnosticsFile, 960, 480)
-      par(mfrow=c(1,2))
-      plot(fitted(anovaOut), resid(anovaOut),
-           xlab="Fitted values",
-           ylab="Residuals",
-           main="Fitted values vs Residuals") 
-      abline(0,0)
-      qqnorm(resid(anovaOut))      
-      dev.off()
+    message('anova txt file: ', anovaTxtFile)
+    modelSummFile <- grep("anova_model",
+                          outputFiles,
+                          value = TRUE)
+
+    message('model file: ', modelSummFile)
+    adjMeansFile  <- grep("adj_means",
+                          outputFiles,
+                          value = TRUE)
+
+    message('means file: ', adjMeansFile)
+
+
+    diagnosticsFile  <- grep("anova_diagnostics",
+                             outputFiles,
+                             value = TRUE)
+
+    errorFile  <- grep("anova_error",
+                       outputFiles,
+                       value = TRUE)
+
  
-      anovaTable <- getAnovaTable(anovaOut,
-                                  tableType="html",
-                                  traitName=trait,
-                                  out=anovaHtmlFile)
+    anovaOut <- runAnova(phenoData, trait)
+    
+    if (class(anovaOut)[1] == 'merModLmerTest') {
+    
+        png(diagnosticsFile, 960, 480)
+        par(mfrow=c(1,2))
+        plot(fitted(anovaOut), resid(anovaOut),
+             xlab="Fitted values",
+             ylab="Residuals",
+             main="Fitted values vs Residuals") 
+        abline(0,0)
+        qqnorm(resid(anovaOut))      
+        dev.off()
+ 
+        anovaTable <- getAnovaTable(anovaOut,
+                                    tableType="html",
+                                    traitName=trait,
+                                    out=anovaHtmlFile)
 
-      anovaTable <- getAnovaTable(anovaOut,
-                                  tableType="text",
-                                  traitName=trait,
-                                  out=anovaTxtFile)
+        anovaTable <- getAnovaTable(anovaOut,
+                                    tableType="text",
+                                    traitName=trait,
+                                    out=anovaTxtFile)
       
-      adjMeans   <- getAdjMeans(phenoData, trait)
+        adjMeans   <- getAdjMeans(phenoData, trait)
 
-      fwrite(adjMeans,
-             file      = adjMeansFile,
-             row.names = FALSE,
-             sep       = "\t",
-             quote     = FALSE,
-             )
+        fwrite(adjMeans,
+               file      = adjMeansFile,
+               row.names = FALSE,
+               sep       = "\t",
+               quote     = FALSE,
+               )
 
-      sink(modelSummFile)
-      print(anovaOut)
-      sink()
-  } else {
-
-      print(anovaOut)
-      #anovaOut <- paste0('<p> ', anovaOut, ' </p>')
-      cat(anovaOut, file=errorFile)
-      
-  }
+        sink(modelSummFile)
+        print(anovaOut)
+        sink()
+    } else {
+        cat(anovaOut, file=errorFile)        
+    }
   
 }
 
