c86a1f459977b12ae818d7b15e49186b10bd075f
nickmorales <nmorales3142@gmail.com>
2018-03-27 13:28:59 -0400

    unneeded js

--- js/CXGN/GuidedHelp.js
+++ js/CXGN/GuidedHelp.js
@@ -4,12 +4,6 @@ jQuery(document).ready(function(){
         jQuery('#guided_help_begin_dialog').modal('show');
     });
 
-    jQuery('#guided_help_upload_trial_button').click(function(){
-        jQuery('#guided_help_upload_trial_dialog').modal('show');
-    });
-    jQuery('#guided_help_create_trial_button').click(function(){
-        jQuery('#guided_help_create_trial_dialog').modal('show');
-    });
     jQuery('#guided_help_seedlot_inventory_button').click(function(){
         jQuery('#guided_help_seedlot_inventory_dialog').modal('show');
     });
