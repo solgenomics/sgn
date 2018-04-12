fef14a464a3ff16d4709981b53340c621e2aa299
nickmorales <nmorales3142@gmail.com>
2018-04-12 10:55:36 -0400

    add genotyping trial to homepage guided help

--- js/CXGN/GuidedHelp.js
+++ js/CXGN/GuidedHelp.js
@@ -10,9 +10,6 @@ jQuery(document).ready(function(){
     jQuery('#guided_help_upload_phenotypes_button').click(function(){
         jQuery('#guided_help_upload_phenotypes_dialog').modal('show');
     });
-    jQuery('#guided_help_genotyping_trial_button').click(function(){
-        jQuery('#guided_help_genotyping_trial_dialog').modal('show');
-    });
     jQuery('#guided_help_barcode_trial_button').click(function(){
         jQuery('#guided_help_trial_barcoding_dialog').modal('show');
     });
