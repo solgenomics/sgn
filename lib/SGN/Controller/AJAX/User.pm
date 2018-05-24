39c1f343d505360ef7857b06f4d21cf7f9c764b0
Lukas Mueller <lam87@cornell.edu>
2018-05-24 16:04:34 -0400

    Remove protocol from link, as it should be already in the config variable.

--- lib/SGN/Controller/AJAX/User.pm
+++ lib/SGN/Controller/AJAX/User.pm
@@ -149,7 +149,7 @@ This message is sent to confirm the email address for community user
 Please click (or cut and paste into your browser) the following link to
 confirm your account and email address:
 
-https://$host/solpeople/account-confirm.pl?username=$username&confirm=$confirm_code
+  $host/solpeople/account-confirm.pl?username=$username&confirm=$confirm_code
 
 Thank you,
 Sol Genomics Network
