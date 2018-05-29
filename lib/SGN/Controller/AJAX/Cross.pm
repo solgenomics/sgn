956341ed6f7d4ef1e7af55d69764d11a246e6c2b
titima15 <tt15@cornell.edu>
2018-05-29 17:18:57 +0000

    fix indentation for upload_cross_file

--- lib/SGN/Controller/AJAX/Cross.pm
+++ lib/SGN/Controller/AJAX/Cross.pm
@@ -56,43 +56,42 @@ __PACKAGE__->config(
     default   => 'application/json',
     stash_key => 'rest',
     map       => { 'application/json' => 'JSON', 'text/html' => 'JSON' },
-   );
+);
 
 sub upload_cross_file : Path('/ajax/cross/upload_crosses_file') : ActionClass('REST') { }
 
 sub upload_cross_file_POST : Args(0) {
-  my ($self, $c) = @_;
-  #print STDERR Dumper $c->req->params();
-  my $chado_schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
-  my $metadata_schema = $c->dbic_schema("CXGN::Metadata::Schema");
-  my $phenome_schema = $c->dbic_schema("CXGN::Phenome::Schema");
-  my $dbh = $c->dbc->dbh;
-  my $crossing_trial_id = $c->req->param('cross_upload_crossing_trial');
-  my $location = $c->req->param('cross_upload_location');
-  my $crosses_plantplot_upload = $c->req->upload('crosses_plantplot_upload_file');
-  my $crosses_simple_upload = $c->req->upload('crosses_simple_upload_file');
-  my $prefix = $c->req->param('upload_prefix');
-  my $suffix = $c->req->param('upload_suffix');
-  my $parser;
-  my $parsed_data;
-  my $upload_original_name = $upload->filename();
-  my $upload_tempfile = $upload->tempname;
-  my $subdirectory = "cross_upload";
-  my $archived_filename_with_path;
-  my $md5;
-  my $validate_file;
-  my $parsed_file;
-  my $parse_errors;
-  my %parsed_data;
-  my %upload_metadata;
-  my $time = DateTime->now();
-  my $timestamp = $time->ymd()."_".$time->hms();
-  my $user_role;
-  my $user_id;
-  my $user_name;
-  my $owner_name;
-  my $upload_file_type = "crosses excel";#get from form when more options are added
-  my $session_id = $c->req->param("sgn_session_id");
+    my ($self, $c) = @_;
+    my $chado_schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
+    my $metadata_schema = $c->dbic_schema("CXGN::Metadata::Schema");
+    my $phenome_schema = $c->dbic_schema("CXGN::Phenome::Schema");
+    my $dbh = $c->dbc->dbh;
+    my $crossing_trial_id = $c->req->param('cross_upload_crossing_trial');
+    my $location = $c->req->param('cross_upload_location');
+    my $crosses_plantplot_upload = $c->req->upload('crosses_plantplot_upload_file');
+    my $crosses_simple_upload = $c->req->upload('crosses_simple_upload_file');
+    my $prefix = $c->req->param('upload_prefix');
+    my $suffix = $c->req->param('upload_suffix');
+    my $parser;
+    my $parsed_data;
+    my $upload_original_name = $upload->filename();
+    my $upload_tempfile = $upload->tempname;
+    my $subdirectory = "cross_upload";
+    my $archived_filename_with_path;
+    my $md5;
+    my $validate_file;
+    my $parsed_file;
+    my $parse_errors;
+    my %parsed_data;
+    my %upload_metadata;
+    my $time = DateTime->now();
+    my $timestamp = $time->ymd()."_".$time->hms();
+    my $user_role;
+    my $user_id;
+    my $user_name;
+    my $owner_name;
+    my $upload_file_type = "crosses excel";#get from form when more options are added
+    my $session_id = $c->req->param("sgn_session_id");
 
     if ($session_id){
         my $dbh = $c->dbc->dbh;
@@ -115,30 +114,29 @@ sub upload_cross_file_POST : Args(0) {
         $user_role = $c->user->get_object->get_user_type();
     }
 
-  my $uploader = CXGN::UploadFile->new({
-    tempfile => $upload_tempfile,
-    subdirectory => $subdirectory,
-    archive_path => $c->config->{archive_path},
-    archive_filename => $upload_original_name,
-    timestamp => $timestamp,
-    user_id => $user_id,
-    user_role => $user_role
+    my $uploader = CXGN::UploadFile->new({
+        tempfile => $upload_tempfile,
+        subdirectory => $subdirectory,
+        archive_path => $c->config->{archive_path},
+        archive_filename => $upload_original_name,
+        timestamp => $timestamp,
+        user_id => $user_id,
+        user_role => $user_role
+    });
 
-  });
+    ## Store uploaded temporary file in arhive
+    $archived_filename_with_path = $uploader->archive();
+    $md5 = $uploader->get_md5($archived_filename_with_path);
+    if (!$archived_filename_with_path) {
+        $c->stash->{rest} = {error => "Could not save file $upload_original_name in archive",};
+        return;
+    }
+    unlink $upload_tempfile;
 
-  ## Store uploaded temporary file in arhive
-  $archived_filename_with_path = $uploader->archive();
-  $md5 = $uploader->get_md5($archived_filename_with_path);
-  if (!$archived_filename_with_path) {
-      $c->stash->{rest} = {error => "Could not save file $upload_original_name in archive",};
-      return;
-  }
-  unlink $upload_tempfile;
-
-  $upload_metadata{'archived_file'} = $archived_filename_with_path;
-  $upload_metadata{'archived_file_type'}="cross upload file";
-  $upload_metadata{'user_id'}=$user_id;
-  $upload_metadata{'date'}="$timestamp";
+    $upload_metadata{'archived_file'} = $archived_filename_with_path;
+    $upload_metadata{'archived_file_type'}="cross upload file";
+    $upload_metadata{'user_id'}=$user_id;
+    $upload_metadata{'date'}="$timestamp";
 
     my $cross_properties_json = $c->config->{cross_properties};
     my @properties = split ',', $cross_properties_json;
@@ -152,9 +150,10 @@ sub upload_cross_file_POST : Args(0) {
         $upload_type = 'CrossesExcelFormat'
     }
 
-    if ($crosses_simple_upload) [
+    if ($crosses_simple_upload) {
         $upload_type = 'CrossesSimpleExcel'
-    ]
+    }
+
     $parser->load_plugin($upload_type);
     $parsed_data = $parser->parse();
     #print STDERR "Dumper of parsed data:\t" . Dumper($parsed_data) . "\n";
@@ -176,67 +175,66 @@ sub upload_cross_file_POST : Args(0) {
         $c->detach();
     }
 
-  my $cross_add = CXGN::Pedigree::AddCrosses
-    ->new({
-	   chado_schema => $chado_schema,
-	   phenome_schema => $phenome_schema,
-	   metadata_schema => $metadata_schema,
-	   dbh => $dbh,
-	   location => $location,
-     crossing_trial_id => $crossing_trial_id,
-	   crosses =>  $parsed_data->{crosses},
-	   owner_name => $user_name
+    my $cross_add = CXGN::Pedigree::AddCrosses->new({
+        chado_schema => $chado_schema,
+        phenome_schema => $phenome_schema,
+        metadata_schema => $metadata_schema,
+        dbh => $dbh,
+        location => $location,
+        crossing_trial_id => $crossing_trial_id,
+        crosses =>  $parsed_data->{crosses},
+        owner_name => $user_name
 	  });
 
-  #validate the crosses
-  if (!$cross_add->validate_crosses()){
-    $c->stash->{rest} = {error_string => "Error validating crosses",};
-    return;
-  }
-
-  #add the crosses
-  if (!$cross_add->add_crosses()){
-    $c->stash->{rest} = {error_string => "Error adding crosses",};
-    return;
-  }
-
-  #add the progeny
-  if ($parsed_data->{number_of_progeny}) {
-  my %progeny_hash = %{$parsed_data->{number_of_progeny}};
-  foreach my $cross_name_key (keys %progeny_hash){
-    my $progeny_number = $progeny_hash{$cross_name_key};
-    my $progeny_increment = 1;
-    my @progeny_names;
-
-    #create array of progeny names to add for this cross
-    while ($progeny_increment < $progeny_number + 1) {
-      $progeny_increment = sprintf "%03d", $progeny_increment;
-      my $stock_name = $cross_name_key.$prefix.$progeny_increment.$suffix;
-      push @progeny_names, $stock_name;
-      $progeny_increment++;
+    #validate the crosses
+    if (!$cross_add->validate_crosses()){
+        $c->stash->{rest} = {error_string => "Error validating crosses",};
+        return;
     }
 
-    #add array of progeny to the cross
-    my $progeny_add = CXGN::Pedigree::AddProgeny
-      ->new({
-	     chado_schema => $chado_schema,
-	     phenome_schema => $phenome_schema,
-	     dbh => $dbh,
-	     cross_name => $cross_name_key,
-	     progeny_names => \@progeny_names,
-	     owner_name => $owner_name,
-	    });
-    if (!$progeny_add->add_progeny()){
-      $c->stash->{rest} = {error_string => "Error adding progeny",};
-      #should delete crosses and other progeny if add progeny fails?
-      return;
+    #add the crosses
+    if (!$cross_add->add_crosses()){
+        $c->stash->{rest} = {error_string => "Error adding crosses",};
+        return;
     }
-  }
-}
+
+    #add the progeny
+    if ($parsed_data->{number_of_progeny}) {
+        my %progeny_hash = %{$parsed_data->{number_of_progeny}};
+
+        foreach my $cross_name_key (keys %progeny_hash) {
+            my $progeny_number = $progeny_hash{$cross_name_key};
+            my $progeny_increment = 1;
+            my @progeny_names;
+
+            #create array of progeny names to add for this cross
+            while ($progeny_increment < $progeny_number + 1) {
+                $progeny_increment = sprintf "%03d", $progeny_increment;
+                my $stock_name = $cross_name_key.$prefix.$progeny_increment.$suffix;
+                push @progeny_names, $stock_name;
+                $progeny_increment++;
+            }
+
+            #add array of progeny to the cross
+            my $progeny_add = CXGN::Pedigree::AddProgeny->new ({
+                chado_schema => $chado_schema,
+                phenome_schema => $phenome_schema,
+                dbh => $dbh,
+                cross_name => $cross_name_key,
+                progeny_names => \@progeny_names,
+                owner_name => $owner_name,
+            });
+            if (!$progeny_add->add_progeny()){
+                $c->stash->{rest} = {error_string => "Error adding progeny",};
+                #should delete crosses and other progeny if add progeny fails?
+                return;
+            }
+        }
+    }
 
     while (my $info_type = shift (@properties)){
         if ($parsed_data->{$info_type}) {
-        print STDERR "Handling info type $info_type\n";
+            print STDERR "Handling info type $info_type\n";
             my %info_hash = %{$parsed_data->{$info_type}};
             foreach my $cross_name_key (keys %info_hash) {
                 my $value = $info_hash{$cross_name_key};
