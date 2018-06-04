7442e940605f25a4cbd17e27cc11ecafc7ac2d3c
nickmorales <nmorales3142@gmail.com>
2018-06-04 19:56:13 +0000

    materialized phenotype table search

---
+++ lib/CXGN/Phenotypes/Search/MaterializedViewTable.pm
@@ -0,0 +1,338 @@
+package CXGN::Phenotypes::Search::MaterializedViewTable;
+
+=head1 NAME
+
+CXGN::Phenotypes::Search::MaterializedViewTable - an object to handle searching phenotypes across database. called from factory CXGN::Phenotypes::SearchFactory. Processes phenotype search against cxgn schema.
+
+=head1 USAGE
+
+my $phenotypes_search = CXGN::Phenotypes::SearchFactory->instantiate(
+    'MaterializedViewTable',    #can be either 'MaterializedView', or 'Native', or 'MaterializedViewTable'
+    {
+        bcs_schema=>$schema,
+        data_level=>$data_level,
+        trait_list=>$trait_list,
+        trial_list=>$trial_list,
+        year_list=>$year_list,
+        location_list=>$location_list,
+        accession_list=>$accession_list,
+        plot_list=>$plot_list,
+        plant_list=>$plant_list,
+        subplot_list=>$subplot_list,
+        exclude_phenotype_outlier=>0,
+        include_timestamp=>$include_timestamp,
+        include_row_and_column_numbers=>0,
+        trait_contains=>$trait_contains,
+        phenotype_min_value=>$phenotype_min_value,
+        phenotype_max_value=>$phenotype_max_value,
+        limit=>$limit,
+        offset=>$offset
+    }
+);
+my @data = $phenotypes_search->search();
+
+=head1 DESCRIPTION
+
+
+=head1 AUTHORS
+
+
+=cut
+
+use strict;
+use warnings;
+use Moose;
+use Try::Tiny;
+use Data::Dumper;
+use SGN::Model::Cvterm;
+use CXGN::Stock::StockLookup;
+use CXGN::Trial::TrialLayout;
+use CXGN::Calendar;
+use JSON;
+
+has 'bcs_schema' => ( isa => 'Bio::Chado::Schema',
+    is => 'rw',
+    required => 1,
+);
+
+#(plot, plant, or all)
+has 'data_level' => (
+    isa => 'Str|Undef',
+    is => 'ro',
+);
+
+has 'trial_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'trait_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'accession_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'plot_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'plant_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'subplot_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'location_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'year_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'rw',
+);
+
+has 'exclude_phenotype_outlier' => (
+    isa => 'Bool|Undef',
+    is => 'ro',
+    default => 0
+);
+
+has 'include_timestamp' => (
+    isa => 'Bool|Undef',
+    is => 'ro',
+    default => 0
+);
+
+has 'include_row_and_column_numbers' => (
+    isa => 'Bool|Undef',
+    is => 'ro',
+    default => 0
+);
+
+has 'trait_contains' => (
+    isa => 'ArrayRef[Str]|Undef',
+    is => 'rw'
+);
+
+has 'phenotype_min_value' => (
+    isa => 'Str|Undef',
+    is => 'rw'
+);
+
+has 'phenotype_max_value' => (
+    isa => 'Str|Undef',
+    is => 'rw'
+);
+
+has 'limit' => (
+    isa => 'Int|Undef',
+    is => 'rw'
+);
+
+has 'offset' => (
+    isa => 'Int|Undef',
+    is => 'rw'
+);
+
+sub search {
+    my $self = shift;
+    my $schema = $self->bcs_schema();
+    print STDERR "Search Start:".localtime."\n";
+
+    my $include_timestamp = $self->include_timestamp;
+    my $numeric_regex = '^[0-9]+([,.][0-9]+)?$';
+
+    my $stock_lookup = CXGN::Stock::StockLookup->new({ schema => $schema} );
+    my %synonym_hash_lookup = %{$stock_lookup->get_synonym_hash_lookup()};
+
+    my $select_clause = "SELECT observationunit_stock_id, observationunit_uniquename, observationunit_type_name, germplasm_uniquename, germplasm_stock_id, rep, block, plot_number, row_number, col_number, plant_number, is_a_control, trial_id, trial_name, trial_description, plot_width, plot_length, field_size, field_trial_is_planned_to_be_genotyped, field_trial_is_planned_to_cross, breeding_program_id, breeding_program_name, breeding_program_description, year, design, location_id, planting_date, harvest_date, folder_id, folder_name, folder_description, treatments, observations, count(observationunit_stock_id) OVER() AS full_count FROM materialized_phenotype_jsonb_table ";
+    my $order_clause = " ORDER BY trial_name, observationunit_uniquename";
+
+    my @where_clause;
+
+    if (($self->plot_list && scalar(@{$self->plot_list})>0) && ($self->plant_list && scalar(@{$self->plant_list})>0) && ($self->subplot_list && scalar(@{$self->subplot_list})>0)) {
+        my $plot_and_plant_and_subplot_sql = _sql_from_arrayref($self->plot_list) .",". _sql_from_arrayref($self->plant_list) .",". _sql_from_arrayref($self->subplot_list);
+        push @where_clause, "observationunit_stock_id in ($plot_and_plant_and_subplot_sql)";
+    } elsif (($self->plot_list && scalar(@{$self->plot_list})>0) && ($self->plant_list && scalar(@{$self->plant_list})>0)) {
+        my $plot_and_plant_sql = _sql_from_arrayref($self->plot_list) .",". _sql_from_arrayref($self->plant_list);
+        push @where_clause, "observationunit_stock_id in ($plot_and_plant_sql)";
+    } elsif (($self->plot_list && scalar(@{$self->plot_list})>0) && ($self->subplot_list && scalar(@{$self->subplot_list})>0)) {
+        my $plot_and_subplot_sql = _sql_from_arrayref($self->plot_list) .",". _sql_from_arrayref($self->subplot_list);
+        push @where_clause, "observationunit_stock_id in ($plot_and_subplot_sql)";
+    } elsif (($self->plant_list && scalar(@{$self->plant_list})>0) && ($self->subplot_list && scalar(@{$self->subplot_list})>0)) {
+        my $plant_and_subplot_sql = _sql_from_arrayref($self->plant_list) .",". _sql_from_arrayref($self->subplot_list);
+        push @where_clause, "observationunit_stock_id in ($plant_and_subplot_sql)";
+    } elsif ($self->plot_list && scalar(@{$self->plot_list})>0) {
+        my $plot_sql = _sql_from_arrayref($self->plot_list);
+        push @where_clause, "observationunit_stock_id in ($plot_sql)";
+    } elsif ($self->plant_list && scalar(@{$self->plant_list})>0) {
+        my $plant_sql = _sql_from_arrayref($self->plant_list);
+        push @where_clause, "observationunit_stock_id in ($plant_sql)";
+    } elsif ($self->subplot_list && scalar(@{$self->subplot_list})>0) {
+        my $subplot_sql = _sql_from_arrayref($self->subplot_list);
+        push @where_clause, "observationunit_stock_id in ($subplot_sql)";
+    }
+
+    if ($self->trial_list && scalar(@{$self->trial_list})>0) {
+        my $trial_sql = _sql_from_arrayref($self->trial_list);
+        push @where_clause, "trial_id in ($trial_sql)";
+    }
+    if ($self->location_list && scalar(@{$self->location_list})>0) {
+        my $arrayref = $self->location_list;
+        my $sql = join ("','" , @$arrayref);
+        my $location_sql = "'" . $sql . "'";
+        push @where_clause, "location_id in ($location_sql)";
+    }
+    if ($self->year_list && scalar(@{$self->year_list})>0) {
+        my $arrayref = $self->year_list;
+        my $sql = join ("','" , @$arrayref);
+        my $year_sql = "'" . $sql . "'";
+        push @where_clause, "year in ($year_sql)";
+    }
+    if ($self->data_level ne 'all') {
+        push @where_clause, "observationunit_type_name = '".$self->data_level."'"; #ONLY plot or plant or subplot
+    } else {
+        push @where_clause, "(observationunit_type_name = 'plot' OR observationunit_type_name = 'plant' OR observationunit_type_name = 'subplot')"; #plots AND plants AND subplots
+    }
+
+    if ($self->trait_list && scalar(@{$self->trait_list})>0) {
+        foreach (@{$self->trait_list}){
+            push @where_clause, "observations @> '[{\"trait_id\" : $_}]'";
+        }
+    }
+    if ($self->trait_contains && scalar(@{$self->trait_contains})>0) {
+        foreach (@{$self->trait_contains}) {
+            push @where_clause, "observations @> '[{\"trait_name\" : $_}]'";
+        }
+    }
+    #if ($self->phenotype_min_value && !$self->phenotype_max_value) {
+    #    push @where_clause, 'JSON_EXISTS(observations, \'$[*] ? (@.value >= '.$self->phenotype_min_value.')\')'; 
+    #}
+    # if ($self->phenotype_max_value && !$self->phenotype_min_value) {
+    #     push @where_clause, 'JSON_EXISTS(observations, \'$[*] ? (@.value <= '.$self->phenotype_max_value.')\')'; 
+    # }
+    # if ($self->phenotype_max_value && $self->phenotype_min_value) {
+    #     push @where_clause, 'JSON_EXISTS(observations, \'$[*] ? (@.value >= '.$self->phenotype_min_value.' && @.value <= '.$self->phenotype_max_value.')\')'; 
+    # }
+    # 
+    if ($self->exclude_phenotype_outlier){
+        push @where_clause, "observations !@> '[{\"outlier\" : 1}]'";;
+    }
+
+    my $where_clause = " WHERE " . (join (" AND " , @where_clause));
+
+    my $offset_clause = '';
+    my $limit_clause = '';
+    if ($self->limit){
+        $limit_clause = " LIMIT ".$self->limit;
+    }
+    if ($self->offset){
+        $offset_clause = " OFFSET ".$self->offset;
+    }
+
+    my  $q = $select_clause . $where_clause . $order_clause . $limit_clause . $offset_clause;
+
+    print STDERR "QUERY: $q\n\n";
+
+    my $location_rs = $schema->resultset('NaturalDiversity::NdGeolocation')->search();
+    my %location_id_lookup;
+    while( my $r = $location_rs->next()){
+        $location_id_lookup{$r->nd_geolocation_id} = $r->description;
+    }
+
+    my $h = $schema->storage->dbh()->prepare($q);
+    $h->execute();
+    my @result;
+
+    my $calendar_funcs = CXGN::Calendar->new({});
+
+    while (my ($observationunit_stock_id, $observationunit_uniquename, $observationunit_type_name, $germplasm_uniquename, $germplasm_stock_id, $rep, $block, $plot_number, $row_number, $col_number, $plant_number, $is_a_control, $trial_id, $trial_name, $trial_description, $plot_width, $plot_length, $field_size, $field_trial_is_planned_to_be_genotyped, $field_trial_is_planned_to_cross, $breeding_program_id, $breeding_program_name, $breeding_program_description, $year, $design, $location_id, $planting_date, $harvest_date, $folder_id, $folder_name, $folder_description, $treatments, $observations, $full_count) = $h->fetchrow_array()) {
+        my $harvest_date_value = $calendar_funcs->display_start_date($harvest_date);
+        my $planting_date_value = $calendar_funcs->display_start_date($planting_date);
+        my $synonyms = $synonym_hash_lookup{$germplasm_uniquename};
+        my $location_name = $location_id ? $location_id_lookup{$location_id} : '';
+        my $observations = decode_json $observations;
+        my $treatments = decode_json $treatments;
+
+        if ($include_timestamp){
+            foreach (@$observations){
+                my $timestamp_value;
+                my $operator_value;
+                my $phenotype_uniquename = $_->{uniquename};
+                if ($phenotype_uniquename){
+                    my ($p1, $p2) = split /date: /, $phenotype_uniquename;
+                    if ($p2){
+                        my ($timestamp, $operator_value) = split /  operator = /, $p2;
+                        if ( $timestamp =~ m/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})(\S)(\d{4})/) {
+                            $timestamp_value = $timestamp;
+                        }
+                    }
+                }
+                $_->{timestamp} = $timestamp_value;
+                $_->{operator} = $operator_value;
+            }
+        }
+
+        push @result, {
+            observationunit_stock_id => $observationunit_stock_id,
+            observationunit_uniquename => $observationunit_uniquename,
+            observationunit_type_name => $observationunit_type_name,
+            germplasm_uniquename => $germplasm_uniquename,
+            germplasm_stock_id => $germplasm_stock_id,
+            germplasm_synonyms => $synonyms,
+            obsunit_rep => $rep,
+            obsunit_block => $block,
+            obsunit_plot_number => $plot_number,
+            obsunit_row_number => $row_number,
+            obsunit_col_number => $col_number,
+            obsunit_plant_number => $plant_number,
+            obsunit_is_a_control => $is_a_control,
+            trial_id => $trial_id,
+            trial_name => $trial_name,
+            trial_description => $trial_description,
+            plot_width => $plot_width,
+            plot_length => $plot_length,
+            field_size => $field_size,
+            field_trial_is_planned_to_be_genotyped => $field_trial_is_planned_to_be_genotyped,
+            field_trial_is_planned_to_cross => $field_trial_is_planned_to_cross,
+            breeding_program_id => $breeding_program_id,
+            breeding_program_name => $breeding_program_name,
+            breeding_program_description => $breeding_program_description,
+            year => $year,
+            design => $design,
+            trial_location_id => $location_id,
+            trial_location_name => $location_name,
+            planting_date => $planting_date_value,
+            harvest_date => $harvest_date_value,
+            folder_id => $folder_id,
+            folder_name => $folder_name,
+            folder_description => $folder_description,
+            treatments => $treatments,
+            observations => $observations,
+            full_count => $full_count,
+        };
+    }
+    #print STDERR Dumper \@result;
+
+    print STDERR "Search End:".localtime."\n";
+    return \@result;
+}
+
+sub _sql_from_arrayref {
+    my $arrayref = shift;
+    my $sql = join ("," , @$arrayref);
+    return $sql;
+}
+
+
+1;
