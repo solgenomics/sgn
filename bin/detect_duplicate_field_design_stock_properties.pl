fba8ac37ff2b320f64589152f79c0da52872c444
nickmorales <nmorales3142@gmail.com>
2018-06-05 17:11:01 +0000

    script to detect duplicate design props

---
+++ bin/detect_duplicate_field_design_stock_properties.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+
+=head1
+
+detect_duplicate_field_design_stock_properties.pl - backend script for detecting and solving issue of plots with multiple plot_numbers
+
+=head1 SYNOPSIS
+
+    detect_duplicate_field_design_stock_properties.pl -H [dbhost] -D [dbname] -U [dbuser] -P [dbpass] -t [test]
+=head1 COMMAND-LINE OPTIONS
+  ARGUMENTS
+ -H host name (required) e.g. "localhost"
+ -D database name (required) e.g. "cxgn_cassava"
+ -U database username (required)
+ -P database userpass (required)
+ -t only show duplicates
+
+=head1 DESCRIPTION
+
+perl bin/detect_duplicate_field_design_stock_properties.pl -D cass -H localhost -U postgres -P postgres 
+
+This script will detect and correct (if t is passed) the issue of plots having multiple plot_numbers associated.
+
+=head1 AUTHOR
+
+ Nicolas Morales (nm529@cornell.edu)
+
+=cut
+
+use strict;
+
+use Getopt::Std;
+use Data::Dumper;
+use Carp qw /croak/ ;
+use Pod::Usage;
+use DateTime;
+use Bio::Chado::Schema;
+use SGN::Model::Cvterm;
+
+our ($opt_H, $opt_D, $opt_U, $opt_P, $opt_t);
+
+getopts('H:D:U:P:t');
+
+if (!$opt_H || !$opt_D || !$opt_U ||!$opt_P ) {
+    die "Must provide options -H (hostname), -D (database name), -U (database user), -P (database password)\n";
+}
+
+my $schema = Bio::Chado::Schema->connect(
+    "dbi:Pg:database=$opt_D;host=$opt_H", # DSN Line
+    $opt_U,                    # Username
+    $opt_P           # Password
+);
+
+my $plot_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plot number', 'stock_property')->cvterm_id();
+my $block_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'block', 'stock_property')->cvterm_id();
+my $row_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'row_number', 'stock_property')->cvterm_id();
+my $col_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'col_number', 'stock_property')->cvterm_id();
+my $is_a_control_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'is a control', 'stock_property')->cvterm_id();
+my $plant_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plant_index_number', 'stock_property')->cvterm_id();
+my $subplot_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'subplot_index_number', 'stock_property')->cvterm_id();
+my $tissue_sample_number_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'tissue_sample_index_number', 'stock_property')->cvterm_id();
+my $replicate_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'replicate', 'stock_property')->cvterm_id();
+my $range_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'range', 'stock_property')->cvterm_id();
+my @type_ids_of_interest = ($plot_number_type_id, $block_type_id, $row_number_type_id, $col_number_type_id, $is_a_control_type_id, $plant_number_type_id, $subplot_number_type_id, $tissue_sample_number_type_id, $replicate_type_id, $range_type_id);
+
+my $stockprops = $schema->resultset("Stock::Stockprop")->search(
+    {type_id => {-in => \@type_ids_of_interest}},
+    {
+        'join' => 'type',
+        '+select' => ['type.name'],
+        '+as' => ['type']
+    }
+);
+my %results;
+while (my $r = $stockprops->next){
+    push @{$results{$r->stock_id}->{$r->get_column('type')}}, $r->value;
+}
+
+while (my ($k, $v) = each %results){
+    while (my ($t, $z) = each %$v){
+        if (scalar(@{$z}) > 1){
+            my $values = join ',', @$z;
+            print STDERR "More than one for stock $k for type $t with $values\n";
+        }
+    }
+}
+
+print STDERR "Script Complete.\n";
