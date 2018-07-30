916ff348e0cdd3c9fc8f17f826db29602cffb30d
nickmorales <nmorales3142@gmail.com>
2018-07-30 21:55:14 +0000

    genotype search or new format

--- lib/CXGN/Genotype/Search.pm
+++ lib/CXGN/Genotype/Search.pm
@@ -9,6 +9,7 @@ CXGN::Genotype::Search - an object to handle searching genotypes for stocks
 my $genotypes_search = CXGN::Genotype::Search->new({
     bcs_schema=>$schema,
     accession_list=>$accession_list,
+    tissue_sample_list=>$tissue_sample_list,
     trial_list=>$trial_list,
     protocol_id=>$protocol_id
 });
@@ -34,8 +35,15 @@ use Try::Tiny;
 use Data::Dumper;
 use SGN::Model::Cvterm;
 use CXGN::Trial;
+use JSON;
 
 has 'bcs_schema' => ( isa => 'Bio::Chado::Schema',
+    is => 'rw',
+    required => 1,
+);
+
+has 'protocol_id' => (
+    isa => 'Int',
     is => 'rw',
     required => 1,
 );
@@ -45,15 +53,19 @@ has 'accession_list' => (
     is => 'ro',
 );
 
+has 'tissue_sample_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'ro',
+);
+
 has 'trial_list' => (
     isa => 'ArrayRef[Int]|Undef',
     is => 'ro',
 );
 
-has 'protocol_id' => (
-    isa => 'Int',
-    is => 'rw',
-    required => 1,
+has 'genotype_data_project_list' => (
+    isa => 'ArrayRef[Int]|Undef',
+    is => 'ro',
 );
 
 has 'limit' => (
@@ -78,12 +90,19 @@ sub get_genotype_info {
     my $trial_list = $self->trial_list;
     my $protocol_id = $self->protocol_id;
     my $accession_list = $self->accession_list;
+    my $tissue_sample_list = $self->tissue_sample_list;
     my $limit = $self->limit;
     my $offset = $self->offset;
     my @data;
     my %search_params;
+    my @where_clause;
 
     my $snp_genotyping_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'snp genotyping', 'genotype_property')->cvterm_id();
+    my $vcf_snp_genotyping_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'vcf_snp_genotyping', 'genotype_property')->cvterm_id();
+    my $vcf_map_details_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'vcf_map_details', 'protocol_property')->cvterm_id();
+    my $igd_genotypeprop_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'igd number', 'genotype_property')->cvterm_id();
+    my $accession_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'accession', 'stock_type')->cvterm_id();
+    my $tissue_sample_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'tissue_sample', 'stock_type')->cvterm_id();
 
     my @trials_accessions;
     foreach (@$trial_list){
@@ -102,51 +121,92 @@ sub get_genotype_info {
     #For projects inserted into database during the addition of genotypes and genotypeprops
     if (scalar(@trials_accessions)==0){
         if ($trial_list && scalar(@$trial_list)>0) {
-            $search_params{'nd_experiment_projects.project_id'} = { -in => $trial_list };
+            my $trial_sql = join ("," , @$trial_list);
+            push @where_clause, "project.project_id in ($trial_sql)";
         }
     }
 
-    $search_params{'genotypeprops.type_id'} = $snp_genotyping_cvterm_id;
-    $search_params{'nd_protocol.nd_protocol_id'} = $protocol_id;
+    push @where_clause, "nd_protocol.nd_protocol_id = $protocol_id";
     if ($accession_list && scalar(@$accession_list)>0) {
-        $search_params{'stock.stock_id'} = { -in => $accession_list };
+        my $accession_sql = join ("," , @$accession_list);
+        push @where_clause, "stock.stock_id in ($accession_sql)";
+        push @where_clause, "stock.type_id = $accession_cvterm_id";
+    }
+    if ($tissue_sample_list && scalar(@$tissue_sample_list)>0) {
+        my $stock_sql = join ("," , @$tissue_sample_list);
+        push @where_clause, "stock.stock_id in ($stock_sql)";
+        push @where_clause, "stock.type_id = $tissue_sample_cvterm_id";
     }
 
-    my @select_list = ('genotypeprops.genotypeprop_id', 'genotypeprops.value', 'nd_protocol.name', 'stock.stock_id', 'stock.uniquename', 'genotype.uniquename');
-    my @select_as_list = ('genotypeprop_id', 'value', 'protocol_name', 'stock_id', 'uniquename', 'genotype_uniquename');
-    #$self->bcs_schema->storage->debug(1);
-    my $rs = $self->bcs_schema->resultset('NaturalDiversity::NdExperiment')->search(
-        \%search_params,
-        {join=> [{'nd_experiment_genotypes' => {'genotype' => 'genotypeprops'} }, {'nd_experiment_protocols' => 'nd_protocol' }, 'nd_experiment_projects', {'nd_experiment_stocks' => 'stock'} ],
-        select=> \@select_list,
-        as=> \@select_as_list,
-        order_by=>{ -asc=>'genotypeprops.genotypeprop_id' }
-        }
-    );
+    my $where_clause = " WHERE " . (join (" AND " , @where_clause));
 
-    if ($rs) {
-        if ($limit && defined($offset)){
-            my $rs_slice = $rs->slice($offset, $limit);
-            $rs = $rs_slice;
-        }
-        while (my $row = $rs->next()) {
-            my $genotype_json = $row->get_column('value');
-            my $genotype = JSON::Any->decode($genotype_json);
+    my $offset_clause = '';
+    my $limit_clause = '';
+    if ($limit){
+        $limit_clause = " LIMIT $limit ";
+    }
+    if ($offset){
+        $offset_clause = " OFFSET $offset ";
+    }
 
-            push @data, {
-                markerProfileDbId => $row->get_column('genotypeprop_id'),
-                germplasmDbId => $row->get_column('stock_id'),
-                germplasmName => $row->get_column('uniquename'),
-                genotypeUniquename => $row->get_column('genotype_uniquename'),
-                analysisMethod => $row->get_column('protocol_name'),
-                genotype_hash => $genotype,
-                resultCount => scalar(keys(%$genotype))
-            };
+    my $q = "SELECT genotype_values.genotypeprop_id, genotype_values.value, igd_number_genotypeprop.value, nd_protocol.nd_protocol_id, nd_protocol.name, nd_protocolprop.value, stock.stock_id, stock.uniquename, stock.type_id, stock_cvterm.name, genotype.genotype_id, genotype.uniquename, project.project_id, project.name, count(genotype_values.genotypeprop_id) OVER() AS full_count
+        FROM stock
+        JOIN cvterm AS stock_cvterm ON(stock.type_id = stock_cvterm.cvterm_id)
+        JOIN nd_experiment_stock USING(stock_id)
+        JOIN nd_experiment USING(nd_experiment_id)
+        JOIN nd_experiment_protocol USING(nd_experiment_id)
+        JOIN nd_experiment_project USING(nd_experiment_id)
+        JOIN nd_experiment_genotype USING(nd_experiment_id)
+        JOIN nd_protocol USING(nd_protocol_id)
+        LEFT JOIN nd_protocolprop ON(nd_protocolprop.nd_protocol_id = nd_protocol.nd_protocol_id AND nd_protocolprop.type_id = $vcf_map_details_cvterm_id)
+        JOIN genotype USING(genotype_id)
+        LEFT JOIN genotypeprop AS igd_number_genotypeprop ON(igd_number_genotypeprop.genotype_id = genotype.genotype_id AND igd_number_genotypeprop.type_id = $igd_genotypeprop_cvterm_id)
+        JOIN genotypeprop AS genotype_values ON(genotype_values.genotype_id = genotype.genotype_id AND genotype_values.type_id = $vcf_snp_genotyping_cvterm_id)
+        JOIN project USING(project_id)
+        $where_clause
+        ORDER BY genotype_values.genotypeprop_id ASC
+        $limit_clause
+        $offset_clause;";
+
+    print STDERR Dumper $q;
+    my $h = $schema->storage->dbh()->prepare($q);
+    $h->execute();
+
+    my $total_count = 0;
+    while (my ($genotypeprop_id, $genotypeprop_json, $igd_number_json, $protocol_id, $protocol_name, $protocolprop_json, $stock_id, $stock_name, $stock_type_id, $stock_type_name, $genotype_id, $genotype_uniquename, $project_id, $project_name, $full_count) = $h->fetchrow_array()) {
+        my $genotype = decode_json $genotypeprop_json;
+        my $protocol = $protocolprop_json ? decode_json $protocolprop_json : undef;
+        my $all_protocol_marker_names = $protocol ? $protocol->{'marker_names'} : undef;
+        my $igd_number_hash = $igd_number_json ? decode_json $igd_number_json : undef;
+        my $igd_number = $igd_number_hash ? $igd_number_hash->{'igd number'} : undef;
+
+        my %dosage_hash;
+        while(my($marker_name, $val) = each %$genotype) {
+            $dosage_hash{$marker_name} = $val->{'DS'};
         }
+
+        push @data, {
+            markerProfileDbId => $genotypeprop_id,
+            germplasmDbId => $stock_id,
+            germplasmName => $stock_name,
+            stock_type_id => $stock_type_id,
+            stock_type_name => $stock_type_name,
+            genotypeDbId => $genotype_id,
+            genotypeUniquename => $genotype_uniquename,
+            analysisMethodDbId => $protocol_id,
+            analysisMethod => $protocol_name,
+            genotypingDataProjectDbId => $project_id,
+            genotypingDataProjectName => $project_name,
+            genotype_hash => \%dosage_hash,
+            full_genotype_hash => $genotype,
+            full_protocol_hash => $protocol,
+            all_protocol_marker_names => $all_protocol_marker_names,
+            igd_number => $igd_number,
+            resultCount => scalar(keys(%$genotype))
+        };
+        $total_count = $full_count;
     }
     #print STDERR Dumper \@data;
-
-    my $total_count = $rs->count();
 
     return ($total_count, \@data);
 }
