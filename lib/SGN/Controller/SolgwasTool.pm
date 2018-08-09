67ed0a08fa476ac3fc873c7b89712d4331db520f
afpowell <afp43@cornell.edu>
2018-08-09 16:32:16 +0000

    set new login redirect

--- lib/SGN/Controller/SolgwasTool.pm
+++ lib/SGN/Controller/SolgwasTool.pm
@@ -9,28 +9,30 @@ use List::Util qw/sum/;
 use Bio::SeqIO;
 use CXGN::Tools::Text qw/ sanitize_string /;
 #use SGN::Schema;
+use URI::FromHash 'uri';
 use CXGN::Blast;
 use CXGN::Blast::SeqQuery;
 
 
 BEGIN { extends 'Catalyst::Controller'; }
 
-sub AUTO { 
+sub AUTO {
     my $self = shift;
     my $c = shift;
     SGN::Schema::BlastDb->dbpath($c->config->{blast_db_path});
 }
 
-sub index :Path('/tools/solgwas/') :Args(0) { 
+sub index :Path('/tools/solgwas/') :Args(0) {
   my $self = shift;
   my $c = shift;
-
-
-
+  if (! $c->user) {
+    $c->res->redirect(uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+    return;
+  }
   $c->stash->{template} = '/tools/solgwas/index.mas';
 }
 
 
-    
+
 
 1;
