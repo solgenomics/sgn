aeb3ccb3ab0216c43202b0e513ec3f6fdbe7dd15
afpowell <afp43@cornell.edu>
2018-08-10 18:47:22 +0000

    Add dereferencing to avoid sort keys blessed hash exception

--- lib/CXGN/Dataset/File.pm
+++ lib/CXGN/Dataset/File.pm
@@ -12,32 +12,32 @@ has 'file_name' => ( isa => 'Str',
 		     default => '/tmp/dataset_file',
     );
 
-override('retrieve_genotypes', 
-	 sub { 
+override('retrieve_genotypes',
+	 sub {
 	     my $self = shift;
 	     my $protocol_id = shift;
 	     my $file = shift || $self->file_name()."_genotype.txt";
 	     my $genotypes = $self->SUPER::retrieve_genotypes($protocol_id);
 	     my $genotype_string = "";
 	     my $genotype_example = $genotypes->[0];
-	     foreach my $key (sort keys $genotype_example->{genotype_hash}) {
+	     foreach my $key (sort keys %{$genotype_example->{genotype_hash}}) {
 		 $genotype_string .= $key."\t";
 	     }
 	     $genotype_string .= "\n";
 	     foreach my $element (@$genotypes) {
 		 my $genotype_id = $element->{germplasmDbId};
-		 my $genotype_data_string = "";		 
-		 foreach my $key (sort keys $element->{genotype_hash}) {
+		 my $genotype_data_string = "";
+		 foreach my $key (sort keys %{$element->{genotype_hash}}) {
 		     my $value = $element->{genotype_hash}->{$key};
 		     my $current_genotype = $value;
-		     $genotype_data_string .= $current_genotype."\t";		     
+		     $genotype_data_string .= $current_genotype."\t";
 		 }
 		 my $s = join "\t", $genotype_id;
 		 $genotype_string .= $s."\t".$genotype_data_string."\n";
 	     }
      	     write_file($file, $genotype_string);
 
-	     
+
 #	     my $genotype_json = JSON::Any->encode($genotypes);
 #	     write_file($file, $genotype_json);
 	     return $genotypes;
@@ -58,7 +58,7 @@ override('retrieve_phenotypes',
 	 });
 
 override('retrieve_accessions',
-	 sub { 
+	 sub {
 	     my $self = shift;
 	     my $file = shift || $self->file_name()."_accessions.txt";
 	     my $accessions = $self->SUPER::retrieve_accessions();
@@ -68,7 +68,7 @@ override('retrieve_accessions',
 	 });
 
 override('retrieve_plots',
-	 sub { 
+	 sub {
 	     my $self = shift;
 	     my $file = shift || $self->file_name()."_plots.txt";
 	     my $plots = $self->SUPER::retrieve_plots();
@@ -78,7 +78,7 @@ override('retrieve_plots',
 	 });
 
 override('retrieve_trials',
-	 sub { 
+	 sub {
 	     my $self = shift;
 	     my $file = shift || $self->file_name()."_trials.txt";
 	     my $trials = $self->SUPER::retrieve_trials();
@@ -88,7 +88,7 @@ override('retrieve_trials',
 	 });
 
 override('retrieve_traits',
-	 sub { 
+	 sub {
 	     my $self = shift;
 	     my $file = shift || $self->file_name()."_traits.txt";
 	     my $traits = $self->SUPER::retrieve_traits();
@@ -98,7 +98,7 @@ override('retrieve_traits',
 	 });
 
 override('retrieve_years',
-	 sub { 
+	 sub {
 	     my $self = shift;
 	     my $file = shift || $self->file_name()."_years.txt";
 	     my $years = $self->SUPER::retrieve_years();
