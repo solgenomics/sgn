940c93e5c4812b523f8325723754c0c65a2212de
Isaak <iyt2@cornell.edu>
2018-08-27 09:51:47 +0200

    allow multiple type dataset selection..

--- js/solGS/Dataset.js
+++ js/solGS/Dataset.js
@@ -4,6 +4,8 @@
 *
 */
 
+//JSAN('CXGN.Dataset');
+
 var solGS = solGS || function solGS () {};
 
 solGS.getDatasetData = function (id) {
@@ -12,10 +14,9 @@ solGS.getDatasetData = function (id) {
     var allDatasets = dataset.getDatasets();
     var data = {};
     
-    for (var i =0; i <allDatasets.length; i++) {
+    for (var i =0; i < allDatasets.length; i++) {
 	console.log(allDatasets[i][0] + ' ' + allDatasets[i][1]);
-
-	var name = allDatasets[i][1];
+	
 	if (allDatasets[i][0] == id) {
 	    data.name = allDatasets[i][1];
 	    data.id   = id;
@@ -28,21 +29,37 @@ solGS.getDatasetData = function (id) {
 
   
 solGS.getDatasetsMenu = function (dType) {
+
+    console.log(Array.isArray(dType))
     
+    if (!Array.isArray(dType)) {
+	dType = [dType];
+    }
+        
     var dataset = new CXGN.Dataset();
     var allDatasets = dataset.getDatasets();
     
     var sp = ' ----------- ';
     var dMenu = '<option disabled>' + sp +  'DATASETS' + sp + '</option>';
-    
-    for (var i =0; i <allDatasets.length; i++) {
-	var id = allDatasets[i][0];
-	var name = allDatasets[i][1];
 
-	var d = dataset.getDataset(id);
-	if (d.categories[dType].length){	  
-	    dMenu += '<option name="dataset" value=' + id + '>' + name + '</option>';
-	}	   	    
+    var dsIds = [];
+    for (var i=0; i < allDatasets.length; i++) {
+    	var id = allDatasets[i][0];
+    	var name = allDatasets[i][1];
+
+    	var d = dataset.getDataset(id);
+	
+	for (var j=0; j<dType.length; j++ ) {
+    	    if (d.categories[dType[j]].length) {
+
+		if (!dsIds.includes(id)) {
+		    dsIds.push(id);
+		    dMenu += '<option name="dataset" value=' + id + '>' + name + '</option>';
+		}
+	
+    	    }
+    	}
+	
     }
 
     return dMenu;
