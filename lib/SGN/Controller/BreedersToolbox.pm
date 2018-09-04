a6aeabec3a67137dd67b7d56e097b084df74a387
nickmorales <nmorales3142@gmail.com>
2018-09-03 21:05:37 -0400

    Starting manage drone imagery page

--- lib/SGN/Controller/BreedersToolbox.pm
+++ lib/SGN/Controller/BreedersToolbox.pm
@@ -681,5 +681,19 @@ sub manage_genotyping : Path("/breeders/genotyping") Args(0) {
     $c->stash->{template} = '/breeders_toolbox/manage_genotyping.mas';
 }
 
+sub manage_drone_imagery : Path("/breeders/drone_imagery") Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    if (!$c->user()) {
+        # redirect to login page
+        $c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+        return;
+    }
+
+    my $schema = $c->dbic_schema('Bio::Chado::Schema');
+    $c->stash->{template} = '/breeders_toolbox/manage_drone_imagery.mas';
+}
+
 
 1;
