efc2e329ceccdff4d292c32d3ae73f6592b3d923
titima15 <tt15@cornell.edu>
2018-10-01 16:43:03 +0000

    move search stocks to a new object

--- lib/CXGN/Genotype/Search.pm
+++ lib/CXGN/Genotype/Search.pm
@@ -43,7 +43,6 @@ use Try::Tiny;
 use Data::Dumper;
 use SGN::Model::Cvterm;
 use CXGN::Trial;
-use DBI;
 use JSON;
 use CXGN::Stock::Accession;
 
@@ -131,26 +130,6 @@ has 'limit' => (
 has 'offset' => (
     isa => 'Int',
     is => 'rw',
-);
-
-has 'protocol_id' => (
-    isa => 'Int',
-    is => 'rw',
-);
-
-has 'marker_name' => (
-    isa => 'Str',
-    is => 'rw',
-);
-
-has 'allele_dosage' => (
-    isa => 'Str',
-    is => 'rw',
-);
-
-has 'filtering_parameters' => (
-    isa => 'ArrayRef[Str]',
-    is => 'ro',
 );
 
 =head2 get_genotype_info
@@ -414,59 +393,6 @@ sub get_genotype_info {
     return ($total_count, \@data);
 }
 
-sub get_selected_accessions {
-    my $self = shift;
-    my $schema = $self->bcs_schema;
-    my $accession_list = $self->accession_list;
-    my $filtering_parameters = $self->filtering_parameters;
-    my @accessions = @{$accession_list};
-    my @parameters = @{$filtering_parameters};
-
-    my $genotyping_experiment_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'genotyping_experiment', 'experiment_type')->cvterm_id();
-
-    my @selected_accessions = ();
-    my %vcf_params;
-    my $protocol_id;
-
-    foreach my $param (@parameters){
-        my $param_ref = decode_json$param;
-        my %params = %{$param_ref};
-        my $marker_name = $params{marker_name};
-        my $allele_dosage = $params{allele_dosage};
-        my $genotyping_protocol_id = $params{genotyping_protocol_id};
-
-        if ($genotyping_protocol_id){
-            $protocol_id = $genotyping_protocol_id
-        }
-
-        if ($marker_name){
-            $vcf_params{$marker_name} = {'DS' => $allele_dosage};
-        }
-    }
-
-    my $vcf_params_string = encode_json \%vcf_params;
-
-#    print STDERR "VCF PARAMS JSON=" .Dumper($vcf_params_string). "\n";
-#    print STDERR "PROTOCOL_ID=" .Dumper($protocol_id). "\n";
-
-
-    my $q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
-        JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
-        JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
-        JOIN genotypeprop on (nd_experiment_genotype.genotype_id = genotypeprop.genotype_id)
-        WHERE genotypeprop.value @> ?
-        AND stock.stock_id IN (" . join(', ', ('?') x @accessions) . ")";
-
-    my $h = $schema->storage->dbh()->prepare($q);
-    $h->execute($genotyping_experiment_cvterm_id, $protocol_id, $vcf_params_string, @accessions);
-
-    while (my ($selected_id, $selected_uniquename) = $h->fetchrow_array()){
-        push @selected_accessions, [$selected_id, $selected_uniquename, $vcf_params_string]
-    }
-
-    return \@selected_accessions;
-
-}
 
 
 1;
