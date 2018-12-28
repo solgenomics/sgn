ffc47b6d0d4bd0525e33188e71860d933c2eb249
Isaak <iyt2@cornell.edu>
2018-12-28 09:45:22 +0100

    use user k numbers for k-means clustering

--- R/solGS/cluster.r
+++ R/solGS/cluster.r
@@ -41,14 +41,21 @@ plotKmeansFile   <- grep("plot_kmeans", outputFiles, value = TRUE)
 message("k means result file: ", kResultFile)
 message("k means plot file: ", plotKmeansFile)
 optionsFile <- grep("options", inputFiles,  value = TRUE)
-message("clusterinf options file: ", optionsFile)
+message("cluster options file: ", optionsFile)
 
-clusterOptions <- read.table(optionsFile, header=TRUE, sep="\t")
+clusterOptions <- read.table(optionsFile,
+                             header=TRUE,
+                             sep="\t",
+                             stringsAsFactors=FALSE,
+                             na.strings = "")
+
 clusterOptions <- column_to_rownames(clusterOptions, var="Params")
 print(clusterOptions)
 
 userKNumbers <- clusterOptions["k numbers", 1]
+dataType     <- clusterOptions["data type", 1]
 message('userKNumbers ', userKNumbers)
+message('data type ', dataType)
 
 
 if (is.null(kResultFile))
@@ -57,64 +64,81 @@ if (is.null(kResultFile))
   q("no", 1, FALSE) 
 }
 
-genoData <- c()
-genoMetaData <- c()
 
-filteredGenoFile <- c()
+clusterData <- c()
+genoData    <- c()
 
-if (length(inputFiles) > 1 ) {   
-    allGenoFiles <- inputFiles
 
-    message('allGenoFiles: ', allGenoFiles)
-    genoData <- combineGenoData(allGenoFiles)
-    
-    genoMetaData   <- genoData$trial
-    genoData$trial <- NULL
- 
-} else {
-    genoDataFile <- grep("genotype_data", inputFiles,  value = TRUE)
-    genoData     <- fread(genoDataFile, na.strings = c("NA", " ", "--", "-", "."))
-    genoData     <- unique(genoData, by='V1')
- 
-   filteredGenoFile <- grep("filtered_genotype_data_",  genoDataFile, value = TRUE)
+extractGenotype <- function(inputFiles) {
 
-    if (!is.null(genoData)) { 
+     genoFiles <- grep("genotype_data", inputFiles,  value = TRUE)
 
-        genoData <- data.frame(genoData)
-        genoData <- column_to_rownames(genoData, 'V1')
-    
+    genoMetaData <- c()
+
+    filteredGenoFile <- c()
+
+    if (length(genoFiles) > 1 ) {   
+                                    
+        message('allGenoFiles: ', genoFiles)
+        genoData <- combineGenoData(genoFiles)
+        
+        genoMetaData   <- genoData$trial
+        genoData$trial <- NULL
+        
     } else {
-        genoData <- fread(filteredGenoFile)
+        genoFile <- genoFiles
+
+        message('geno file: ', genoFile)
+        genoData <- fread(genoFile, na.strings = c("NA", " ", "--", "-", "."))
+        genoData <- unique(genoData, by='V1')
+        
+        filteredGenoFile <- grep("filtered_genotype_data_",  genoFile, value = TRUE)
+
+        if (!is.null(genoData)) { 
+
+            genoData <- data.frame(genoData)
+            genoData <- column_to_rownames(genoData, 'V1')
+            
+        } else {
+            genoData <- fread(filteredGenoFile)
+        }
     }
-}
 
-if (is.null(genoData)) {
-  stop("There is no genotype dataset.")
-  q("no", 1, FALSE)
-}
+    if (is.null(genoData)) {
+        stop("There is no genotype dataset.")
+        q("no", 1, FALSE)
+    }
 
 
-genoDataMissing <- c()
-if (is.null(filteredGenoFile) == TRUE) {
-    ##genoDataFilter::filterGenoData
+    genoDataMissing <- c()
+    if (is.null(filteredGenoFile) == TRUE) {
+        ##genoDataFilter::filterGenoData
+        
+        genoData <- filterGenoData(genoData, maf=0.01)
+        genoData <- column_to_rownames(genoData, 'rn')
+
+        message("No. of geno missing values, ", sum(is.na(genoData)) )
+        if (sum(is.na(genoData)) > 0) {
+            genoDataMissing <- c('yes')
+            genoData <- na.roughfix(genoData)
+        }
+    }
+
+
+    genoData <- data.frame(genoData)
     
-    genoData <- filterGenoData(genoData, maf=0.01)
-    genoData <- column_to_rownames(genoData, 'rn')
+} 
 
-    message("No. of geno missing values, ", sum(is.na(genoData)) )
-    if (sum(is.na(genoData)) > 0) {
-        genoDataMissing <- c('yes')
-        genoData <- na.roughfix(genoData)
-    }
+if (dataType == 'Genotype') {
+    clusterData <- extractGenotype(inputFiles)   
 }
 
-
-genoData <- data.frame(genoData)
+print(clusterData[1:3, 1:5])
 
 set.seed(235)
 
 
-pca    <- prcomp(genoData, retx=TRUE)
+pca    <- prcomp(clusterData, retx=TRUE)
 pca    <- summary(pca)
 
 variances <- data.frame(pca$importance)
@@ -143,7 +167,19 @@ kMeansOut <- kmeansruns(scores, runs=10)
 recK <- paste0('Recommended number of clusters (k) for this data set is: ', kMeansOut$bestk)
 cat(recK, file=reportFile, sep="\n", append=TRUE)
 
-kCenters         <- kMeansOut$bestk
+kCenters <- kMeansOut$bestk
+
+if (!is.na(userKNumbers)) {
+
+    if (userKNumbers != 0) {
+        kCenters <- as.integer(userKNumbers)
+        userK <- paste0('However, Clustering was based on ', userKNumbers)
+        cat(userK, file=reportFile, sep="\n", append=TRUE)
+    }
+}
+
+print(scores[1:3, 1:5])
+
 kMeansOut        <- kmeans(scores, centers=kCenters, nstart=10)
 kClusters        <- data.frame(kMeansOut$cluster)
 kClusters        <- rownames_to_column(kClusters)
@@ -159,7 +195,7 @@ dev.off()
 #dev.off()
 
 
-if (length(inputFiles) > 1) {
+if (length(genoFiles) > 1) {
     fwrite(genoData,
        file      = combinedDataFile,
        sep       = "\t",
