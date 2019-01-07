3149be8f03cee1a10c49757a04b9f7aa5a0c3507
Isaak <iyt2@cornell.edu>
2019-01-07 09:59:31 +0100

    abstracted code to function in phenoAnalysis package

--- R/solGS/combine_gebvs_files.r
+++ R/solGS/combine_gebvs_files.r
@@ -1,68 +1,16 @@
 #combines gebvs of traits of a population
 # Isaak Y Tecle
 
+library(phenoAnalysis)
 
 options(echo = FALSE)
 
+allArgs    <- commandArgs()
+inFile     <- grep("gebv_files", allArgs, value = TRUE)
+outputFile <- grep("combined_gebvs", allArgs, value = TRUE)
+inputFiles <- scan(inFile, what = "character")
 
-allArgs <- commandArgs()
-
-inFile <- grep("gebv_files",
-               allArgs,
-               ignore.case = TRUE,
-               perl = TRUE,
-               value = TRUE
-               )
-
-outputFile <- grep("combined_gebvs",
-                allArgs,
-                ignore.case = TRUE,
-                perl = TRUE,
-                value = TRUE
-                )
-
-inputFiles <- scan(inFile,
-                   what = "character"
-                   )
-
-
-combinedGebvs <- c()
-count         <- 0
-
-for (i in inputFiles) {
-  
-  traitGebv <- read.table(i,
-                          header = TRUE,
-                          row.names = NULL,
-                          sep = "\t",
-                          dec = "."
-                          )
-
-  trait <- colnames(traitGebv)
-  count <- count + 1
- 
-  traitGebv <- traitGebv[order(traitGebv$X), ]
-
-  if(count == 1) {
-    combinedGebvs <- traitGebv
-  
-    row.names(combinedGebvs) <- combinedGebvs[, 1]
-    combinedGebvs[, 1] <- NULL
-  
-  } else {
-    row.names(traitGebv) <- traitGebv[, 1]
-    traitGebv[, 1] <- NULL
-
-    combinedGebvs <- merge(traitGebv,
-                           combinedGebvs,
-                           by=0,
-                           all=FALSE
-                           )
-   
-    rownames(combinedGebvs) <- combinedGebvs[, 1]      
-    combinedGebvs$Row.names <- NULL
-  }  
-}
+combinedGebvs <- mergeVariables(inputFiles)
 
 if (length(outputFile) != 0 ) {
   write.table(combinedGebvs,
