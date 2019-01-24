639a462830b753f77da7ded08b22cbad7424386b
Production User <production@cassava-test.sgn.cornell.edu>
2019-01-24 15:59:06 -0500

    make merge_stocks.pl use the new CXGN::Stock object.

--- bin/merge_stocks.pl
+++ bin/merge_stocks.pl
@@ -31,23 +31,26 @@ use CXGN::DB::InsertDBH;
 use CXGN::DB::Schemas;
 use CXGN::Stock;
 
+
 our($opt_H, $opt_D, $opt_x);
 getopts('H:D:x');
 
-my $dbh = CXGN::DB::InsertDBH->new( 
-    { 
-	dbhost => $opt_H,
-	dbname => $opt_D ,
-	dbargs => { 
-	    AutoCommit => 0,
-	    RaiseError => 1,
-	    limit_dialect => 'LimitOffset',
-	}
-    });
+
+print "Password for $opt_H / $opt_D: \n";
+my $pw = (<STDIN>);
+chomp($pw);
 
 my $delete_merged_stock = $opt_x;
 
 print STDERR "Note: -x: Deleting stocks that have been merged into other stocks.\n";
+
+print STDERR "Connecting to database...\n";
+my $dsn = 'dbi:Pg:database='.$opt_D.";host=".$opt_H.";port=5432";
+
+my $dbh = DBI->connect($dsn, "postgres", $pw, { AutoCommit => 0, RaiseError=>1 });
+
+print STDERR "Connecting to DBI schema...\n";
+my $bcs_schema = Bio::Chado::Schema->connect($dsn, "postgres", $pw);
 
 my $s = CXGN::DB::Schemas->new({ dbh => $dbh });
 my $schema = $s->bcs_schema();
@@ -56,12 +59,13 @@ my $file = shift;
 open(my $F, "<", $file) || die "Can't open file $file.\n";
 
 my $header = <$F>;
-
+print STDERR "Skipping header line $header\n";
 eval { 
     while (<$F>) { 
+        print STDERR "Read line: $_\n";
 	chomp;
 	my ($merge_stock_name, $good_stock_name) = split /\t/;
-	
+	print STDERR "bad name: $merge_stock_name, good name: $good_stock_name\n";
 	my $stock_row = $schema->resultset("Stock::Stock")->find( { uniquename => $good_stock_name } );
 	if (!$stock_row) { 
 	    print STDERR "Stock $good_stock_name not found. Skipping...\n";
@@ -75,11 +79,11 @@ eval {
 	    next();
 	}
 	
-	my $good_stock = CXGN::Chado::Stock->new($schema, $stock_row->stock_id);
-	my $merge_stock = CXGN::Chado::Stock->new($schema, $merge_row->stock_id);
+	my $good_stock = CXGN::Stock->new( { schema => $schema, stock_id => $stock_row->stock_id });
+	my $merge_stock = CXGN::Stock->new( { schema => $schema, stock_id => $merge_row->stock_id });
 	
 	print STDERR "Merging stock $merge_stock_name into $good_stock_name... ";
-	$good_stock->merge($merge_stock->get_stock_id(), $delete_merged_stock);
+	$good_stock->merge($merge_stock->stock_id(), $delete_merged_stock);
 	print STDERR "Done.\n";
     }
     
