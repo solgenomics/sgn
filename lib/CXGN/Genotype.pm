e5761201688cf8164d6fa3d767ee7b2a6aae1187
Lukas Mueller <lam87@cornell.edu>
2019-02-01 13:18:29 -0500

    adding a function for checking the concordance of parental phenotypes.

--- lib/CXGN/Genotype.pm
+++ lib/CXGN/Genotype.pm
@@ -213,6 +213,73 @@ sub scores_are_equal {
     return 0;
 }
     
+sub check_parental_genotype_concordance {
+   my $self = shift;
+   my $female_parent_genotype = shift;
+   my $male_parent_genotype = shift;
+
+   my $self_markers = $self->markerscores();
+   my $mom_markers = $female_parent_genotype->markerscores();
+   my $dad_markers = $male_parent_genotype->markerscores();
+
+   my $non_informative =0;
+   my $concordant =0;
+   my $non_concordant =0;
+   foreach my $m (keys %$self_markers) {
+    
+    my @matrix; #mom, dad, self, 1=possible 0=impossible
+    $matrix[ 0 ][ 0 ][ 0 ] =1;
+    $matrix[ 0 ][ 0 ][ 1 ] =0;
+    $matrix[ 0 ][ 0 ][ 2 ] =0;
+    $matrix[ 0 ][ 1 ][ 0 ] =1;
+    $matrix[ 0 ][ 1 ][ 1 ] =1;
+    $matrix[ 0 ][ 1 ][ 2 ] =0;
+    $matrix[ 0 ][ 2 ][ 1 ] =1;
+    $matrix[ 0 ][ 2 ][ 0 ] =1;
+    $matrix[ 0 ][ 2 ][ 2 ] =0;
+
+    $matrix[ 1 ][ 0 ][ 0 ] =1;
+    $matrix[ 1 ][ 0 ][ 1 ] =1;
+    $matrix[ 1 ][ 0 ][ 2 ] =0;
+    $matrix[ 1 ][ 1 ][ 0 ] =-1;
+    $matrix[ 1 ][ 1 ][ 1 ] =-1;
+    $matrix[ 1 ][ 1 ][ 2 ] =0;
+    $matrix[ 1 ][ 2 ][ 0 ] =0;
+    $matrix[ 1 ][ 2 ][ 1 ] =1;
+    $matrix[ 1 ][ 2 ][ 2 ] =1;
+    
+    $matrix[ 2 ][ 0 ][ 0 ] = 0;
+    $matrix[ 2 ][ 0 ][ 1 ] = 1;
+    $matrix[ 2 ][ 0 ][ 2 ] = 0;
+    $matrix[ 2 ][ 1 ][ 0 ] = 1;
+    $matrix[ 2 ][ 1 ][ 1 ] = 1;
+    $matrix[ 2 ][ 1 ][ 2 ] = 1;
+    $matrix[ 2 ][ 2 ][ 0 ] = 0;
+    $matrix[ 2 ][ 2 ][ 1 ] = 0;
+    $matrix[ 2 ][ 2 ][ 2 ] = 1;
+
+
+    if (defined($mom_markers->{$m}) && defined($dad_markers->{$m}) && defined($self_markers->{$m})) {
+        
+        my $score = $matrix[ round($mom_markers->{$m})]->[ round($dad_markers->{$m})]->[ round($self_markers->{$m})];
+        if ($score == 1) {
+        $concordant++;
+
+        }
+        elsif ($score == -1)  {
+        $non_informative++;
+        }
+        else {
+        
+        $non_concordant++;
+
+        }
+    }
+   }
+   return ($concordant, $non_concordant, $non_informative);
+}
+
+
 
 sub calculate_encoding_type { 
 
