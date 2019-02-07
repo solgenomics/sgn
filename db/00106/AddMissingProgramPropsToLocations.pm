fd2ac0335d6e03ea568bfce1b8faeb534e4b91fa
bellerbrock <bje24@cornell.edu>
2019-02-07 00:18:16 +0000

    add sql

--- db/00106/AddMissingProgramPropsToLocations.pm
+++ db/00106/AddMissingProgramPropsToLocations.pm
@@ -58,11 +58,45 @@ sub patch {
     print STDOUT "\nExecuting the SQL commands.\n";
     my $schema = Bio::Chado::Schema->connect( sub { $self->dbh->clone } );
 
+    my $coderef = sub {
+        print STDERR "Updating location breeding programs . . .\n";
 
-    print STDERR "FINDING LOCATIONS WITHOUT A PROGRAM PROP...\n";
+        my $query = "SELECT nd_geolocation.nd_geolocation_id, project.project_id
+        FROM nd_geolocation
+        JOIN nd_experiment using(nd_geolocation_id)
+        JOIN nd_experiment_project using(nd_experiment_id)
+        JOIN project_relationship on (nd_experiment_project.project_id = subject_project_id and project_relationship.type_id = (select cvterm_id from cvterm where name = 'breeding_program_trial_relationship'))
+        JOIN project on (project_relationship.object_project_id = project.project_id)
+        GROUP by 1,2";
 
-print "You're done!\n";
-}
+        my $h = $self->schema->storage->dbh->prepare($query);
+        $h->execute();
+
+        my $breeding_program_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'breeding_program', 'project_property')->cvterm_id();
+        my @locations;
+        while (my ($location_id, $program_id) = $h->fetchrow_array()) {
+
+            my $location = $self->bcs_schema->resultset("NaturalDiversity::NdGeolocation")->find( { nd_geolocation_id => $location_id });
+            my $rs = $self->bcs_schema()->resultset("NaturalDiversity::NdGeolocationprop")->search({ nd_geolocation_id=> $location_id, value => $program_id, type_id => $breeding_program_cvterm_id });
+
+            if($rs->next->value()) {
+                #skip, this program - location link is already stored in nd_geolocationprop
+            } else {
+                my $count = $self->bcs_schema()->resultset("NaturalDiversity::NdGeolocationprop")->search({ nd_geolocation_id=> $location_id, type_id => $breeding_program_cvterm_id })->count();
+                my $stored_location = $location->create_geolocationprops({ 'breeding_program' => $program_id }, {cv_name => $cv, rank => $count });
+            }
+        }
+    };
+
+    try {
+      $schema->txn_do($coderef);
+    } catch {
+      die " patch failed! " . $_ .  "\n" ;
+    };
+
+    print "You're done!\n";
+
+  }
 
 
 ####
