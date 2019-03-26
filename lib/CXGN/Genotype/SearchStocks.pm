9ede770c029280508f252ef967f71d79fa77b03c
titima15 <tt15@cornell.edu>
2019-03-26 21:19:40 +0000

    add query for homozygous NT

--- lib/CXGN/Genotype/SearchStocks.pm
+++ lib/CXGN/Genotype/SearchStocks.pm
@@ -117,7 +117,7 @@ sub get_accessions_using_snps {
     my $genotyping_experiment_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'genotyping_experiment', 'experiment_type')->cvterm_id();
 
     my @selected_accessions = ();
-    my %vcf_params;
+    my %homozygous_nt;
     my $protocol_id;
 
     print STDERR "ACCESSION LIST=" .Dumper(\@accessions). "\n";
@@ -134,19 +134,24 @@ sub get_accessions_using_snps {
             $protocol_id = $genotyping_protocol_id
         }
 
-
+        if ($marker_name){
+            if ($allele_1 eq $allele_2){
+                my $homozygous_param = $allele_1.'/'.$allele_2;
+                $homozygous_nt{$marker_name} = {'NT' => $homozygous_param}
+            }
+        }
     }
 
-    my $vcf_params_string;
-    if (%vcf_params){
-        $vcf_params_string = encode_json \%vcf_params;
+    my $homozygous_nt_string;
+    if (%homozygous_nt){
+        $homozygous_nt_string = encode_json \%homozygous_nt;
     }
 
-    print STDERR "VCF PARAMS JSON=" .Dumper($vcf_params_string). "\n";
-    print STDERR "HET PARAMS=" .Dumper(\@het_params). "\n";
+    print STDERR "HOMOZYGOUS NT JSON=" .Dumper($homozygous_nt_string). "\n";
+#    print STDERR "HET PARAMS=" .Dumper(\@het_params). "\n";
 
-    if ($vcf_params_string){
-        my @first_round_accessions;
+    if ($homozygous_nt_string){
+        my @homozygous_accessions;
         my $first_q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
             JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
             JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
@@ -155,36 +160,36 @@ sub get_accessions_using_snps {
             AND stock.stock_id IN (" . join(', ', ('?') x @accessions) . ")";
 
         my $h = $schema->storage->dbh()->prepare($first_q);
-        $h->execute($genotyping_experiment_cvterm_id, $protocol_id, $vcf_params_string, @accessions);
+        $h->execute($genotyping_experiment_cvterm_id, $protocol_id, $homozygous_nt_string, @accessions);
 
         while (my @row = $h->fetchrow_array()){
-            push @first_round_accessions, $row[0]
+            push @homozygous_accessions, $row[0]
         }
 
-        print STDERR "HOMOZYGOUS TEST=" .Dumper(\@first_round_accessions). "\n";
+        print STDERR "HOMOZYGOUS TEST=" .Dumper(\@homozygous_accessions). "\n";
     }
 
-    if (@het_params){
-        my @pair;
-        my $pair_ref = \@pair;
-        foreach my $pair_ref(@het_params){
-            my $next_q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
-                JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
-                JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
-                JOIN genotypeprop on (nd_experiment_genotype.genotype_id = genotypeprop.genotype_id)
-                WHERE genotypeprop.value @> ? OR genotypeprop.value @> ?
-                AND stock.stock_id IN (" . join(', ', ('?') x @accessions) . ")";
+#    if (@het_params){
+#        my @pair;
+#        my $pair_ref = \@pair;
+#        foreach my $pair_ref(@het_params){
+#            my $next_q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
+#                JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
+#                JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
+#                JOIN genotypeprop on (nd_experiment_genotype.genotype_id = genotypeprop.genotype_id)
+#                WHERE genotypeprop.value @> ? OR genotypeprop.value @> ?
+#                AND stock.stock_id IN (" . join(', ', ('?') x @accessions) . ")";
 
-            my $h = $schema->storage->dbh()->prepare($next_q);
-            $h->execute($genotyping_experiment_cvterm_id, $protocol_id, $pair_ref->[0], $pair_ref->[1], @accessions);
-            while (my @row = $h->fetchrow_array()){
-                push @selected_accessions, [$row[0]];
+#            my $h = $schema->storage->dbh()->prepare($next_q);
+#            $h->execute($genotyping_experiment_cvterm_id, $protocol_id, $pair_ref->[0], $pair_ref->[1], @accessions);
+#            while (my @row = $h->fetchrow_array()){
+#                push @selected_accessions, [$row[0]];
 
-            }
-        }
-        print STDERR "HETEROZYGOUS TEST=" .Dumper(\@selected_accessions). "\n";
+#            }
+#        }
+#        print STDERR "HETEROZYGOUS TEST=" .Dumper(\@selected_accessions). "\n";
 
-    }
+#    }
 
     return \@selected_accessions;
 
