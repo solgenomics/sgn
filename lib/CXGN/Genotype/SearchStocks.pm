63bbeb3c6e4317bf14ec6d6fbacc433185a14a40
titima15 <tt15@cornell.edu>
2019-04-04 16:58:43 +0000

    simplify codes for generating heterozygous param pairs and clean up

--- lib/CXGN/Genotype/SearchStocks.pm
+++ lib/CXGN/Genotype/SearchStocks.pm
@@ -116,16 +116,13 @@ sub get_accessions_using_snps {
 
     my $genotyping_experiment_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($self->bcs_schema, 'genotyping_experiment', 'experiment_type')->cvterm_id();
 
-    my @selected_accessions = ();
-    my %homozygous_nt;
-    my %heterozygous_nt1;
-    my %heterozygous_nt2;
     my $protocol_id;
-    my @het_array1;
-    my @het_array2;
+    my %homozygous_nt;
+    my @all_het_pairs;
     my @het_all_accessions;
     my @het_selected_accessions;
-    my %heterozygous_params;
+    my @selected_accessions;
+    my $all_markers_string;
 
 
     print STDERR "ACCESSION LIST=" .Dumper(\@accessions). "\n";
@@ -149,72 +146,38 @@ sub get_accessions_using_snps {
             }
 
             if ($allele_1 ne $allele_2){
-
+                my %heterozygous_nt1 = ();
+                my %heterozygous_nt2 = ();
+                my @het_pair = ();
                 my $heterozygous_param1 = $allele_1.'/'.$allele_2;
                 my $heterozygous_param2 = $allele_2.'/'.$allele_1;
                 $heterozygous_nt1{$marker_name} = {'NT' => $heterozygous_param1};
+                my $heterozygous_nt_string1 = encode_json \%heterozygous_nt1;
                 $heterozygous_nt2{$marker_name} = {'NT' => $heterozygous_param2};
-#                push @{$heterozygous_params{$marker_name}{"NT"}}, $heterozygous_param1, $heterozygous_param2;
+                my $heterozygous_nt_string2 = encode_json \%heterozygous_nt2;
 
+                push @het_pair, $heterozygous_nt_string1, $heterozygous_nt_string2;
+                push @all_het_pairs, \@het_pair;
             }
+
+            my %all_markers;
+            my $nt_value = $allele_1.'/'.$allele_2;
+            $all_markers{$marker_name} = {'NT' => $nt_value};
+            $all_markers_string = encode_json \%all_markers;
         }
     }
 
-#    print STDERR "HETEROZYGOUS PARAMS=" .Dumper(\%heterozygous_params). "\n";
+    my $het_param_count = @all_het_pairs;
 
     my $homozygous_nt_string;
-    my $heterozygous_nt_string;
-    my $all_nt_string;
     if (%homozygous_nt){
         $homozygous_nt_string = encode_json \%homozygous_nt;
     }
 
-    #for showing  all params in datatable
-    if (%heterozygous_nt1){
-        $heterozygous_nt_string = encode_json \%heterozygous_nt1;
-    }
 
-    #for showing all params in datatable
-    if ((%homozygous_nt) && (%heterozygous_nt1)){
-        my %all_params = (%homozygous_nt, %heterozygous_nt1);
-        $all_nt_string = encode_json \%all_params;
-    }
-
-#    print STDERR "HOMOZYGOUS NT JSON=" .Dumper($homozygous_nt_string). "\n";
-#    print STDERR "HETEROZYGOUS NT JSON=" .Dumper($heterozygous_nt_string). "\n";
-#    print STDERR "ALL NT JSON=" .Dumper($all_nt_string). "\n";
-
-    print STDERR "HET HASH_1=" .Dumper(\%heterozygous_nt1). "\n";
-    print STDERR "HET HASH_2=" .Dumper(\%heterozygous_nt2). "\n";
-
-
-    if (%heterozygous_nt1 && %heterozygous_nt2){
-        foreach my $key (keys%heterozygous_nt1){
-            my %each_hash1 = ($key, $heterozygous_nt1{$key});
-            my $het_nt_string = encode_json \%each_hash1;
-            push @het_array1, $het_nt_string;
-        }
-
-        foreach my $key (keys%heterozygous_nt2){
-            my %each_hash2 = ($key, $heterozygous_nt2{$key});
-            my $het_nt_string = encode_json \%each_hash2;
-            push @het_array2, $het_nt_string;
-        }
-    }
-
-    my @all_het_params = sort (@het_array1, @het_array2);
-    print STDERR "ALL HET JASON=" .Dumper(\@all_het_params). "\n";
-
-    my @het_pairs;
-    while (@all_het_params) {
-        push @het_pairs, [ splice @all_het_params, 0, 2 ];
-    }
-
-    my $het_param_count = @het_array1;
-
-    print STDERR "HET JASON PAIRS=" .Dumper(\@het_pairs). "\n";
-
-#    print STDERR "HET PARAM COUNT=" .Dumper($het_param_count). "\n";
+    print STDERR "HOMOZYGOUS NT JSON=" .Dumper($homozygous_nt_string). "\n";
+    print STDERR "ALL HET PAIRS=" .Dumper(\@all_het_pairs). "\n";
+    print STDERR "HET PARAM COUNT=" .Dumper($het_param_count). "\n";
 
     my @homozygous_accessions;
     if ($homozygous_nt_string){
@@ -230,15 +193,14 @@ sub get_accessions_using_snps {
 
         if ($het_param_count == 0){
             while (my ($selected_id, $selected_uniquename) = $h->fetchrow_array()){
-                push @selected_accessions, [$selected_id, $selected_uniquename, $homozygous_nt_string]
+                push @selected_accessions, [$selected_id, $selected_uniquename, $all_markers_string]
             }
         } elsif ($het_param_count != 0){
             while (my @row = $h->fetchrow_array()){
                 push @homozygous_accessions, $row[0]
             }
 
-
-            foreach my $pair(@het_pairs){
+            foreach my $pair(@all_het_pairs){
                 my $q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
                 JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
                 JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
@@ -265,11 +227,11 @@ sub get_accessions_using_snps {
             my $h = $schema->storage->dbh()->prepare($q);
             $h->execute(@het_selected_accessions);
             while (my ($selected_id, $selected_uniquename) = $h->fetchrow_array()){
-                push @selected_accessions, [$selected_id, $selected_uniquename, $all_nt_string]
+                push @selected_accessions, [$selected_id, $selected_uniquename, $all_markers_string]
             }
         }
     } elsif (($homozygous_nt_string == 0) && ($het_param_count != 0)){
-        foreach my $pair(@het_pairs){
+        foreach my $pair(@all_het_pairs){
             my $q = "SELECT DISTINCT stock.stock_id, stock.uniquename FROM stock JOIN nd_experiment_stock ON (stock.stock_id = nd_experiment_stock.stock_id)
                 JOIN nd_experiment_protocol ON (nd_experiment_stock.nd_experiment_id = nd_experiment_protocol.nd_experiment_id) AND nd_experiment_stock.type_id = ? AND nd_experiment_protocol.nd_protocol_id =?
                 JOIN nd_experiment_genotype on (nd_experiment_genotype.nd_experiment_id = nd_experiment_stock.nd_experiment_id)
@@ -299,7 +261,7 @@ sub get_accessions_using_snps {
         my $h = $schema->storage->dbh()->prepare($q);
         $h->execute(@het_selected_accessions);
         while (my ($selected_id, $selected_uniquename) = $h->fetchrow_array()){
-            push @selected_accessions, [$selected_id, $selected_uniquename, $heterozygous_nt_string]
+            push @selected_accessions, [$selected_id, $selected_uniquename, $all_markers_string]
         }
 
     }
