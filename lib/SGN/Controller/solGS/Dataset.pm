1e70bccf2e4d60ce164a5d59fca7966abc628528
Isaak <iyt2@cornell.edu>
2019-04-25 08:52:12 +0200

    add dataset population metadata functions

--- lib/SGN/Controller/solGS/Dataset.pm
+++ lib/SGN/Controller/solGS/Dataset.pm
@@ -7,11 +7,14 @@ use namespace::autoclean;
 #use File::Spec::Functions qw / catfile catdir/;
 #use File::Path qw / mkpath  /;
 #use File::Temp qw / tempfile tempdir /;
-#use File::Slurp qw /write_file read_file :edit prepend_file/;
+
 #use JSON;
 
 #use CXGN::List;
+
 use Carp qw/ carp confess croak /;
+use File::Slurp qw /write_file read_file :edit prepend_file/;
+use POSIX qw(strftime);
 
 #BEGIN { extends 'Catalyst::Controller' }
 
@@ -148,6 +151,88 @@ sub get_model {
     return $model;
 }
 
+
+
+sub dataset_population_summary {
+    my ($self, $c, $dataset_id) = @_;
+
+    $dataset_id = $c->stash->{dataset_id} if !$dataset_id;
+
+    $dataset_id =  'dataset_' . $dataset_id  if $dataset_id !~ /dataset_/;
+    
+    my $tmp_dir = $c->stash->{solgs_datasets_dir};
+   
+    if (!$c->user)
+    {
+	my $page = "/" . $c->req->path;
+	$c->res->redirect("/solgs/login/message?page=$page");
+	$c->detach;
+    }
+    else
+    {
+	my $user_name = $c->user->id;  
+        my $protocol  = $c->controller('solGS::solGS')->create_protocol_url($c);
+
+	if ($dataset_id) 
+	{
+	    $c->controller('solGS::Files')->population_metadata_file($c, $dataset_id, $tmp_dir);   
+	    my $metadata_file = $c->stash->{population_metadata_file};
+       
+	    my @metadata = read_file($metadata_file);
+       
+	    my ($key, $dataset_name, $desc);
+     
+	    ($desc)        = grep {/description/} @metadata;       
+	    ($key, $desc)  = split(/\t/, $desc);
+      
+	    ($dataset_name)       = grep {/dataset_name/} @metadata;      
+	    ($key, $dataset_name) = split(/\t/, $dataset_name); 
+	   
+	    $c->stash(project_id          => $dataset_id,
+		      project_name        => $dataset_name,
+		      prediction_pop_name => $dataset_name,
+		      project_desc        => $desc,
+		      owner               => $user_name,
+		      protocol            => $protocol,
+		);  
+	}
+    }
+}
+
+
+sub create_dataset_population_metadata {
+    my ($self, $c) = @_;
+    
+    my $metadata = 'key' . "\t" . 'value';
+    $metadata .= "\n" . 'user_id' . "\t" . $c->user->id;
+    $metadata .= "\n" . 'dataset_name' . "\t" . $c->{stash}->{dataset_name};
+    $metadata .= "\n" . 'description' . "\t" . 'Uploaded on: ' . strftime "%a %b %e %H:%M %Y", localtime;
+    
+    $c->stash->{dataset_metadata} = $metadata;
+  
+}
+
+
+sub create_dataset_population_metadata_file {
+    my ($self, $c, $dataset_id) = @_;
+
+    $dataset_id =  'dataset_' . $dataset_id  if $dataset_id !~ /dataset_/;
+    
+    my $user_id = $c->user->id;
+    my $tmp_dir = $c->stash->{solgs_datasets_dir};
+    
+    $c->controller('solGS::Files')->population_metadata_file($c, $dataset_id, $tmp_dir);   
+    my $file = $c->stash->{population_metadata_file};
+   
+    $self->create_dataset_population_metadata($c);
+    my $metadata = $c->stash->{dataset_metadata};
+    
+    write_file($file, $metadata);
+ 
+    $c->stash->{dataset_metadata_file} = $file;
+ 
+  
+}
 
 sub begin : Private {
     my ($self, $c) = @_;
