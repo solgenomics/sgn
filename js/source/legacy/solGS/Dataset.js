eda3a1d7224838200b7bbb88ff2eda5d63e2e1da
Isaak <iyt2@cornell.edu>
2019-05-03 10:23:30 +0200

    add dataset training pop ajax calls

--- js/source/legacy/solGS/Dataset.js
+++ js/source/legacy/solGS/Dataset.js
@@ -15,9 +15,7 @@ solGS.dataset = {
 	var allDatasets = dataset.getDatasets();
 	var data = {};
 	
-	for (var i =0; i < allDatasets.length; i++) {
-	    console.log(allDatasets[i][0] + ' ' + allDatasets[i][1]);
-	    
+	for (var i =0; i < allDatasets.length; i++) {	    
 	    if (allDatasets[i][0] == id) {
 		data.name = allDatasets[i][1];
 		data.id   = id;
@@ -30,9 +28,6 @@ solGS.dataset = {
 
   
     getDatasetsMenu: function (dType) {
-	console.log('forming datasets menu')
-	console.log(Array.isArray(dType))
-	
 	if (!Array.isArray(dType)) {
 	    dType = [dType];
 	}
@@ -47,13 +42,9 @@ solGS.dataset = {
 	for (var i=0; i < allDatasets.length; i++) {
     	    var id = allDatasets[i][0];
     	    var name = allDatasets[i][1];
-	    console.log('name ' + name)
     	    var d = dataset.getDataset(id);
 
 	    for (var j=0; j<dType.length; j++) {
-		console.log('dtype j ' + dType[j])
-		console.log('d.categories[dType[j]]: ' + d.categories[dType[j]])
-		//d.categories[dType[j]].length
     		if (d.categories[dType[j]] !== null && d.categories[dType[j]].length ) {
 
 		    if (!dsIds.includes(id)) {
@@ -87,35 +78,29 @@ solGS.dataset = {
 
 	if (d.categories['trials'] &&
 	    d.categories['plots'] === null) {
-	    console.log('this is trials dataset')
-	    console.log('category plots: ' + d.categories['plots'])
-	    
+	  	    
 	    this.datasetTrialsTrainingPop(datasetId);
 	} else if (d.categories['trials'] &&
 		   d.categories['plots'])  {
 
-	    console.log('this is plots dataset')
-	    this.datasetPlotsTrainingPop(datsetId)
-
+	    this.queueDatasetTrainingPop(datasetId);
 	}
 	   
     },
 
 
     datasetTrialsTrainingPop: function(datasetId) {
-	console.log('creating  datasetTrialsTrainingPop')
 	jQuery.ajax({  
             type: 'POST',
             dataType: "json",
             url: '/solgs/get/dataset/trials',
             data: {'dataset_id': datasetId},
             success: function(res) {
-		console.log('success fetching trials ids')
+		
 		var trialsIds = res.trials_ids;
 		var comboPopsId = res.combo_pops_id; 
-		if (trialsIds) {
-		    console.log('got dataset trials ids: ' + trialsIds)
 
+		if (trialsIds) {
 		    var args = {
 			'combo_pops_id'   : [ comboPopsId ],
 			'combo_pops_list' : trialsIds,
@@ -146,16 +131,14 @@ solGS.dataset = {
             url: '/solgs/dataset/plots/',
             data: {'dataset_id': datasetId},
             success: function(res) {
-		console.log('success fetching trials ids')
 		var trialsIds = res.trials_ids;
 		var comboPopsId = res.combo_pops_id; 
 		if (trialsIds) {
-		    console.log('got dataset trials ids: ' + trialsIds)
-
-		     var args = {
+		    var args = {
 			'combo_pops_id'   : [ comboPopsId ],
 			'combo_pops_list' : trialsIds,
-		     };
+		    };
+		    
 		    if (trialsIds.length > 1) {
 			goToCombinedTrialsTrainingPopPage(args);
 		    } else {
@@ -173,34 +156,36 @@ solGS.dataset = {
 
 
     queueDatasetTrainingPop: function (datasetId) {
- 
-    var args = createDatasetTrainingReqArgs(listId);
-    var modelId = args.training_pop_id;
-      
-    var hostName = window.location.protocol + '//' + window.location.host;    
-    var page     = hostName + '/solgs/population/' + modelId;
 
-    solGS.waitPage(page, args);
+	var args = this.createDatasetTrainingReqArgs(datasetId);
+	var modelId = args.training_pop_id;
+      	
+	var hostName = window.location.protocol + '//' + window.location.host;    
+	var page     = hostName + '/solgs/population/' + modelId;
+
+	solGS.waitPage(page, args);
 
     },
 
 
-    createdDatasetTrainingReqArgs: function (datsetId) {
+    createDatasetTrainingReqArgs: function (datasetId) {
 
-    var datasetName  = genoList.name;   
-    var popId     = 'dataset_' + dataset_id;
-    var popType = 'dataset_training';
+	var dataset = new CXGN.Dataset();
+	var d = dataset.getDataset(datasetId);
+	
+	var popId     = 'dataset_' + datasetId;
+	var popType = 'dataset_training';
 
-    var args = {
-	'datset_name'     : datsetName,
-	'dataset_id'      : datsetId,
-	'analysis_type'   : 'population download',
-	'data_set_type'   : 'single population',
-        'training_pop_id' : popId,
-	'population_type' : popType,
-    };  
+	var args = {
+	    //'dataset_name'    : datasetName,
+	    'dataset_id'      : datasetId,
+	    'analysis_type'   : 'population download',
+	    'data_set_type'   : 'single population',
+            'training_pop_id' : popId,
+	    'population_type' : popType,
+	};  
 
-    return args;
+	return args;
 
 }
 
