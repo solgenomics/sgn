90ef9bb27f77d7898bc9b7a6776011d986a2f06a
Lukas Mueller <lam87@cornell.edu>
2019-06-23 12:56:41 -0400

    move class functions from Project.pm class back to Trial.pm class.

--- lib/CXGN/Trial.pm
+++ lib/CXGN/Trial.pm
@@ -85,5 +85,121 @@ sub new {
     return $object;
 }
 
+=head2 class method get_all_locations()
+
+ Usage:        my $locations = CXGN::Trial::get_all_locations($schema)
+ Desc:
+ Ret:
+ Args:
+ Side Effects:
+ Example:
+
+=cut
+
+sub get_all_locations {
+    my $schema = shift;
+	my $location_id = shift;
+    my @locations;
+
+	my %search_params;
+	if ($location_id){
+		$search_params{'nd_geolocation_id'} = $location_id;
+	}
+
+    my $loc = $schema->resultset('NaturalDiversity::NdGeolocation')->search( \%search_params, {order_by => { -asc => 'nd_geolocation_id' }} );
+    while (my $s = $loc->next()) {
+        my $loc_props = $schema->resultset('NaturalDiversity::NdGeolocationprop')->search( { nd_geolocation_id => $s->nd_geolocation_id() }, {join=>'type', '+select'=>['me.value', 'type.name'], '+as'=>['value', 'cvterm_name'] } );
+
+		my %attr;
+        $attr{'geodetic datum'} = $s->geodetic_datum();
+
+        my $country = '';
+        my $country_code = '';
+        my $location_type = '';
+        my $abbreviation = '';
+        my $address = '';
+
+        while (my $sp = $loc_props->next()) {
+            if ($sp->get_column('cvterm_name') eq 'country_name') {
+                $country = $sp->get_column('value');
+            } elsif ($sp->get_column('cvterm_name') eq 'country_code') {
+                $country_code = $sp->get_column('value');
+            } elsif ($sp->get_column('cvterm_name') eq 'location_type') {
+                $location_type = $sp->get_column('value');
+            } elsif ($sp->get_column('cvterm_name') eq 'abbreviation') {
+                $abbreviation = $sp->get_column('value');
+            } elsif ($sp->get_column('cvterm_name') eq 'geolocation address') {
+                $address = $sp->get_column('value');
+            } else {
+                $attr{$sp->get_column('cvterm_name')} = $sp->get_column('value') ;
+            }
+        }
+
+        push @locations, [$s->nd_geolocation_id(), $s->description(), $s->latitude(), $s->longitude(), $s->altitude(), $country, $country_code, \%attr, $location_type, $abbreviation, $address],
+    }
+
+    return \@locations;
+}
+
+# CLASS METHOD!
+
+=head2 class method get_all_project_types()
+
+ Usage:        my @cvterm_ids = CXGN::Trial::get_all_project_types($schema)
+ Desc:
+ Ret:
+ Args:
+ Side Effects:
+ Example:
+
+=cut
+
+sub get_all_project_types {
+    ##my $class = shift;
+    my $schema = shift;
+    my $project_type_cv_id = $schema->resultset('Cv::Cv')->find( { name => 'project_type' } )->cv_id();
+    my $rs = $schema->resultset('Cv::Cvterm')->search( { cv_id=> $project_type_cv_id }, {order_by=>'me.cvterm_id'} );
+    my @cvterm_ids;
+    if ($rs->count() > 0) {
+	@cvterm_ids = map { [ $_->cvterm_id(), $_->name(), $_->definition ] } ($rs->all());
+    }
+    return @cvterm_ids;
+}
+
+
+=head2 function get_all_phenotype_metadata($schema, $n)
+
+ Note:         Class method!
+ Usage:        CXGN::Trial->get_phenotype_metadata($schema, 100);
+ Desc:         retrieves maximally $n metadata.md_file entries for the any trial . These entries are created during StorePhenotypes.
+ Ret:
+ Args:
+ Side Effects:
+ Example:
+
+=cut
+
+sub get_all_phenotype_metadata {
+    my $class = shift;
+    my $schema = shift;
+    my $n = shift || 200;
+    my @file_array;
+    my %file_info;
+    my $q = "SELECT file_id, m.create_date, p.sp_person_id, p.username, basename, dirname, filetype FROM nd_experiment_project JOIN nd_experiment_phenotype USING(nd_experiment_id) JOIN phenome.nd_experiment_md_files ON (nd_experiment_phenotype.nd_experiment_id=nd_experiment_md_files.nd_experiment_id) LEFT JOIN metadata.md_files using(file_id) LEFT JOIN metadata.md_metadata as m using(metadata_id) LEFT JOIN sgn_people.sp_person as p ON (p.sp_person_id=m.create_person_id) WHERE m.obsolete = 0 and NOT (metadata.md_files.filetype='generated from plot from plant phenotypes') and NOT (metadata.md_files.filetype='direct phenotyping') ORDER BY file_id ASC LIMIT $n";
+    my $h = $schema->storage()->dbh()->prepare($q);
+    $h->execute();
+    
+    while (my ($file_id, $create_date, $person_id, $username, $basename, $dirname, $filetype) = $h->fetchrow_array()) {
+	$file_info{$file_id} = [$file_id, $create_date, $person_id, $username, $basename, $dirname, $filetype];
+    }
+    foreach (keys %file_info){
+	push @file_array, $file_info{$_};
+    }
+    return \@file_array;
+}
+
+
+
+
 1;
 
