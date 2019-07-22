48f225f619a57e4eb2200481e7eaff2ff0fc4053
titima15 <tt15@cornell.edu>
2019-07-22 18:27:45 +0000

    modified family_names validate plugin to include synonym

--- lib/CXGN/List/Validate/Plugin/FamilyNames.pm
+++ lib/CXGN/List/Validate/Plugin/FamilyNames.pm
@@ -12,21 +12,29 @@ sub validate {
     my $schema = shift;
     my $list = shift;
 
-    my $type_id = $schema->resultset("Cv::Cvterm")->search({ name=>"family_name" })->first->cvterm_id();
-
-    #print STDERR "FAMILY NAME TYPE ID $type_id\n";
-
-    my @missing = ();
-    foreach my $l (@$list) {
-	my $rs = $schema->resultset("Stock::Stock")->search(
-	    {
-		type_id=>$type_id,
-		uniquename => $l,
-	    });
-	if ($rs->count() == 0) {
-	    push @missing, $l;
-	}
+    my %all_names;
+    my $synonym_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'stock_synonym', 'stock_property')->cvterm_id();
+    my $family_name_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'family_name', 'stock_type')->cvterm_id();
+    my $q = "SELECT stock.uniquename, stockprop.value, stockprop.type_id FROM stock LEFT JOIN stockprop USING(stock_id) WHERE stock.type_id=$family_name_type_id AND stock.is_obsolete = 'F';";
+    my $h = $schema->storage->dbh()->prepare($q);
+    $h->execute();
+    while (my ($uniquename, $synonym, $type_id) = $h->fetchrow_array()) {
+        $all_names{$uniquename}++;
+        if ($type_id) {
+            if ($type_id == $synonym_type_id) {
+                $all_names{$synonym}++;
+            }
+        }
     }
+
+    #print STDERR Dumper \%all_names;
+    my @missing;
+    foreach my $item (@$list) {
+        if (!exists($all_names{$item})) {
+            push @missing, $item;
+        }
+    }
+
     return { missing => \@missing };
 }
 
