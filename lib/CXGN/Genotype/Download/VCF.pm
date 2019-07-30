158b375ef3a43c3687785ffe1aec37b35fa5a8d4
nickmorales <nmorales3142@gmail.com>
2019-07-30 16:08:43 -0400

    working on tests

--- lib/CXGN/Genotype/Download/VCF.pm
+++ lib/CXGN/Genotype/Download/VCF.pm
@@ -193,7 +193,7 @@ sub download {
 
     #VCF should be sorted by chromosome and position
     no warnings 'uninitialized';
-    @all_marker_objects = sort { $a->{chrom} <=> $b->{chrom} || $a->{pos} <=> $b->{pos} } @all_marker_objects;
+    @all_marker_objects = sort { $a->{chrom} <=> $b->{chrom} || $a->{pos} <=> $b->{pos} || $a->{name} cmp $b->{name} } @all_marker_objects;
 
     my $tsv = Text::CSV->new({ sep_char => "\t", eol => $/ });
     my @header = ("#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT");
