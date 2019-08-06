23e688cfd78c76d1f1bc06c3c9d914e3b5997bb8
Isaak <iyt2@cornell.edu>
2019-08-06 10:08:55 +0200

    make queuing more generic and simpler

--- lib/solGS/asyncJob.pm
+++ lib/solGS/asyncJob.pm
@@ -21,13 +21,6 @@ with 'MooseX::Runnable';
 has "prerequisite_jobs" => (
     is       => 'ro',
     isa      =>  'Str',
-    default  => 0,
-    );
-
-has "prerequisite_type" => (
-    is       => 'ro',
-    isa      =>  'Str',
-    default  => 0,
     );
 
 has "dependent_jobs" => (
@@ -39,7 +32,7 @@ has "dependent_jobs" => (
 has "analysis_report_job" => (
     is       => 'ro',
     isa      => 'Str',
-    required => 1, 
+   # required => 1, 
     );
 
 has "temp_dir" => (
@@ -50,104 +43,63 @@ has "temp_dir" => (
 
 
 
+
 sub run {
     my $self = shift;
    
-    my $jobs = $self->run_job();
-    $self->send_analysis_report($jobs);
+    $self->run_jobs;
           
 }
 
 
-sub run_job {
+
+
+
+sub run_jobs {
     my $self = shift;
 
     my $jobs = [];
-    
-    eval
+      
+    my $prerequisite_jobs = $self->prerequisite_jobs;
+	     
+    if ($prerequisite_jobs !~ /none/)
     {
-	my $job;
-	my $prerequisite_type = $self->prerequisite_type;
-     
-	if ($prerequisite_type =~ /combine_populations/)
-	{
-	    $job = $self->run_combine_populations();
+	$jobs =  $self->run_prerequisite_jobs;    	  
+    }
 
-	    foreach my $job (@$job)
-	    {
-		while (1) 
-		{	 
-		    last if !$job->alive();
-		    sleep 30 if $job->alive();
-		}
-	    }
-
-	    $jobs = $self->run_model();
-     	  
+    foreach my $job (@$jobs)
+    {
+	while (1) 
+	{	  		  
+	    last if !$job->alive();
+	    sleep 30 if $job->alive();
 	}
-	elsif ($prerequisite_type =~ /selection_pop_download_data/) 
-	{      
-	    $job =  $self->query_genotype_data();
-	    
-	    if ($job)
-	    {	  
-		while (1)
-		{
-		    last if !$job->alive();
-		    sleep 30 if $job->alive();
-		}	             		
-	    }
+    }
 
-	    $jobs = $self->run_model();
-	} else {
-	    $self->run_any_job();
-	}
-              	
-    };
-
-    return $jobs;
+    $jobs = $self->run_dependent_jobs;  
+    $self->send_analysis_report($jobs);
     
 }
 
 
-sub run_any_job {
+sub run_prerequisite_jobs {
     my $self = shift;
 
-    my $args_file = $self->dependent_jobs;
-    my $any_jobs = retrieve($args_file);
-     
-    if (reftype $any_jobs ne 'ARRAY') 
+    my $jobs_file = $self->prerequisite_jobs;
+    my $jobs = retrieve($jobs_file);
+
+    print STDERR "\nrun_prerequisite_jobs\n";
+    if (reftype $jobs ne 'ARRAY') 
     {
-	$any_jobs = [$any_jobs];
+	$jobs = [$jobs];
     }
    
     my @jobs;
-    foreach my $any_job (@$any_jobs) 
-    {	
-	my $job = $self->submit_job($any_job);
-	push @jobs, $job;
-    }
-    
-    return \@jobs;
-        
-}
-
-
-sub run_combine_populations {
-    my $self = shift;
-
-    my $args_file = $self->prerequisite_jobs;
-    my $combine_jobs = retrieve($args_file);
-     
-    if (reftype $combine_jobs ne 'ARRAY') 
+    foreach my $job (@$jobs) 
     {
-	$combine_jobs = [$combine_jobs];
-    }
-   
-    my @jobs;
-    foreach my $combine_job (@$combine_jobs) 
-    {	
-	my $job = $self->submit_job($combine_job);
+	print STDERR "\nrun_prerequisite_jobs job $job->{cmd}\n";
+	
+	my $job = $self->submit_job($job);
 	push @jobs, $job;
     }
     
@@ -156,40 +108,25 @@ sub run_combine_populations {
 }
 
 
-sub query_genotype_data {
-    my $self = shift;
-
-    my $query_job_file = $self->prerequisite_jobs;
-    my $query_job = retrieve($query_job_file);
-   
-    my $genotype_file = $query_job->{genotype_file};
-    
-    my $job;
-   
-    if (!-s $genotype_file)
-    {
-	$job = $self->submit_job($query_job);
-    }
-    
-    return $job;    
-}
-
-
-sub run_model {
+sub run_dependent_jobs {
     my $self = shift;
     
-    my $model_job_file = $self->dependent_jobs;
-    my $model_jobs = retrieve($model_job_file);
+    my $jobs_file = $self->dependent_jobs;
+    my $jobs = retrieve($jobs_file);
 
-    if (reftype $model_jobs ne 'ARRAY') 
+    print STDERR "\nrun_dependent_jobs\n";
+    
+    if (reftype $jobs ne 'ARRAY') 
     {
-	$model_jobs = [$model_jobs];
+	$jobs = [$jobs];
     }
 
     my @jobs;
-    foreach my $model_job (@$model_jobs) 
-    {	
-	my $job = $self->submit_job($model_job);
+    foreach my $job (@$jobs) 
+    {
+	my $cmd =  $job->{cmd};
+	print STDERR "\nrun_dependent_jobs job $cmd\n";
+	my $job = $self->submit_job($job);
 	push @jobs, $job;
     }
     
@@ -212,18 +149,19 @@ sub send_analysis_report {
     {
 	while (1)
 	{
+	    print STDERR "\n checking if job is still alive...before triggering email\n";
 	    last if !$job->alive();
 	    sleep 30 if $job->alive();
 	}
     }
-     
+       print STDERR "\n All jobs done...triggering email\n";
     my $report_file    = $self->analysis_report_job;
-    my $report_job = retrieve($report_file);  
-
-    my $job = $self->submit_job($report_job);
-
-    return $job;
-
+    unless ($report_file =~ /none/) 
+    {
+	my $report_job = retrieve($report_file);  
+	my $job = $self->submit_job($report_job);
+	return $job;
+    }
 }
 
 
@@ -233,7 +171,7 @@ sub submit_job {
     my $job;
     
     eval 
-    {
+    {		
     	$job = CXGN::Tools::Run->new($args->{config});
     	$job->do_not_cleanup(1);
 	 
