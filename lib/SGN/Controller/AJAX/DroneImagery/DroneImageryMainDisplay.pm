39ae35ac75905d8e747a07e8f9aca4fca796ec84
nickmorales <nmorales3142@gmail.com>
2019-08-07 16:53:03 -0400

    docs

--- lib/SGN/Controller/AJAX/DroneImagery/DroneImageryMainDisplay.pm
+++ lib/SGN/Controller/AJAX/DroneImagery/DroneImageryMainDisplay.pm
@@ -145,7 +145,7 @@ sub raw_drone_imagery_summary_GET : Args(0) {
                 } elsif (!$v->{drone_run_processed_minimal_vi}) {
                     $drone_run_html .= '<button class="btn btn-default btn-sm" name="project_drone_imagery_standard_process_minimal_vi" data-drone_run_project_id="'.$k.'" data-drone_run_project_name="'.$v->{drone_run_project_name}.'" data-field_trial_id="'.$v->{trial_id}.'" data-field_trial_name="'.$v->{trial_name}.'" >Run Minimal Vegetitative Index Standard Process For<br/>'.$v->{drone_run_project_name}.'</button><br/><br/>';
                 } elsif (!$v->{drone_run_processed_extended}) {
-                    $drone_run_html .= '<button class="btn btn-default btn-sm" name="project_drone_imagery_standard_process_extended" data-drone_run_project_id="'.$k.'" data-drone_run_project_name="'.$v->{drone_run_project_name}.'" data-field_trial_id="'.$v->{trial_id}.'" data-field_trial_name="'.$v->{trial_name}.'" >Run Extended<br/>Standard Process For<br/>'.$v->{drone_run_project_name}.'</button><br/><br/>';
+                    $drone_run_html .= '<button class="btn btn-default btn-sm" name="project_drone_imagery_standard_process_extended" data-drone_run_project_id="'.$k.'" data-drone_run_project_name="'.$v->{drone_run_project_name}.'" data-field_trial_id="'.$v->{trial_id}.'" data-field_trial_name="'.$v->{trial_name}.'" >Run Extended Standard Process For<br/>'.$v->{drone_run_project_name}.'</button><br/><br/>';
                 }
             }
             $drone_run_html .= '<button class="btn btn-danger btn-sm" name="project_drone_imagery_delete_drone_run" data-drone_run_project_id="'.$k.'" data-drone_run_project_name="'.$v->{drone_run_project_name}.'" >Delete Imaging Event</button>';
