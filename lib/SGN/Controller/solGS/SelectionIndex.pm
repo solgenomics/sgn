719f0d1077ece31b798d224268e6f38233923d51
Isaak <iyt2@cornell.edu>
2019-08-21 09:21:01 +0200

    cache si files

--- lib/SGN/Controller/solGS/SelectionIndex.pm
+++ lib/SGN/Controller/solGS/SelectionIndex.pm
@@ -113,37 +113,36 @@ sub calculate_selection_index :Path('/solgs/calculate/selection/index') Args() {
 sub calc_selection_index {
     my ($self, $c) = @_;
 
-    my $training_pop_id  = $c->stash->{training_pop_id};
-    my $selection_pop_id = $c->stash->{selection_pop_id};
-
     my $input_files = join("\t", 
                            $c->stash->{rel_weights_file},
                            $c->stash->{gebv_files_of_traits}
         );
    
-    $c->controller('solGS::Files')->gebvs_selection_index_file($c);
-    $c->controller('solGS::Files')->selection_index_file($c);
+    $self->gebvs_selection_index_file($c);
+    $self->selection_index_file($c);
 
     my $output_files = join("\t",
                             $c->stash->{gebvs_selection_index_file},
                             $c->stash->{selection_index_only_file}
         );
     
-    my $pred_file_suffix;
-    $pred_file_suffix = '_' . $selection_pop_id  if $selection_pop_id;
+   
+    $c->controller('solGS::Files')->create_file_id($c);
+    my $file_id = $c->stash->{file_id};
     
-    my $name = "output_selection_index_${training_pop_id}${pred_file_suffix}";
-    my $temp_dir = $c->stash->{solgs_tempfiles_dir};
-    my $output_file = $c->controller('solGS::Files')->create_tempfile($temp_dir, $name);
+    my $out_name = "output_files_selection_index_${file_id}";
+    my $temp_dir = $c->stash->{selection_index_temp_dir};
+    my $output_file = $c->controller('solGS::Files')->create_tempfile($temp_dir, $out_name);
     write_file($output_file, $output_files);
        
-    $name = "input_selection_index_${training_pop_id}${pred_file_suffix}";
-    my $input_file = $c->controller('solGS::Files')->create_tempfile($temp_dir, $name);
+    my $in_name = "input_files_selection_index_${file_id}";
+    my $input_file = $c->controller('solGS::Files')->create_tempfile($temp_dir, $in_name);
     write_file($input_file, $input_files);
-  
+
+    $c->stash->{analysis_tempfiles_dir} = $c->stash->{selection_index_temp_dir};
     $c->stash->{output_files} = $output_file;
     $c->stash->{input_files}  = $input_file;   
-    $c->stash->{r_temp_file}  = "selection_index_${training_pop_id}${pred_file_suffix}";  
+    $c->stash->{r_temp_file}  = "selection_index_${file_id}";  
     $c->stash->{r_script}     = 'R/solGS/selection_index.r';
     
     $c->controller('solGS::solGS')->run_r_script($c);
@@ -166,11 +165,11 @@ sub get_top_10_selection_indices {
 
 sub gebv_rel_weights {
     my ($self, $c, $rel_wts) = @_;
-    
-    my $training_pop_id = $c->stash->{training_pop_id};
-    my $selection_pop_id = $c->stash->{selection_pop_id};
-  
+      
+   
+    my @si_id;
     my $rel_wts_txt = "trait" . "\t" . 'relative_weight' . "\n";
+    
     foreach my $tr (keys %$rel_wts)
     {      
         my $wt = $rel_wts->{$tr};
@@ -178,19 +177,78 @@ sub gebv_rel_weights {
         {
             $rel_wts_txt .= $tr . "\t" . $wt;
             $rel_wts_txt .= "\n";
+	    push @si_id, $tr, $wt;
         }
     }
-  
-    my $pred_file_suffix;
-    $pred_file_suffix = '_' . $selection_pop_id  if $selection_pop_id; 
-    
-    my $name = "rel_weights_${training_pop_id}${pred_file_suffix}";
-    my $temp_dir = $c->stash->{solgs_tempfiles_dir};
-    my $file = $c->controller('solGS::Files')->create_tempfile($temp_dir, $name);
+
+    my $si_id = join('-', @si_id);
+    $c->stash->{selection_index_id} = $si_id;
+
+    $self->rel_weights_file($c);
+    my $file = $c->stash->{rel_weights_file};
     write_file($file, $rel_wts_txt);
     
-    $c->stash->{rel_weights_file} = $file;
+   ### $c->stash->{rel_weights_file} = $file;
     
+}
+
+
+sub gebvs_selection_index_file {
+    my ($self, $c) = @_;
+
+   $c->controller('solGS::Files')->create_file_id($c);
+    my $file_id = $c->stash->{file_id};
+  
+    my $name = "gebvs_selection_index_${file_id}";
+    my $dir = $c->stash->{selection_index_cache_dir};
+ 
+    my $cache_data = { key       => $name, 
+		       file      => $name . '.txt',
+		       stash_key => 'gebvs_selection_index_file',
+		       cache_dir => $dir
+    };
+   
+    $c->controller('solGS::Files')->cache_file($c, $cache_data);
+    
+}
+
+
+sub selection_index_file {
+    my ($self, $c) = @_;
+
+    $c->controller('solGS::Files')->create_file_id($c);
+    my $file_id = $c->stash->{file_id};
+    
+    my $name = "selection_index_only_${file_id}";
+    my $dir = $c->stash->{selection_index_cache_dir};
+ 
+    my $cache_data = { key       => $name, 
+		       file      => $name . '.txt',
+		       stash_key => 'selection_index_only_file',
+		       cache_dir => $dir
+    };
+    
+    $c->controller('solGS::Files')->cache_file($c, $cache_data);
+    
+}
+
+
+sub rel_weights_file {
+    my ($self, $c) = @_;
+
+    $c->controller('solGS::Files')->create_file_id($c);
+    my $file_id = $c->stash->{file_id};
+
+    my $dir = $c->stash->{selection_index_cache_dir};
+    my $name =  "rel_weights_${file_id}";
+   
+    my $cache_data = { key       => $name, 
+    		       file      => $name . '.txt',
+    		       stash_key => 'rel_weights_file',
+    		       cache_dir => $dir
+    };
+    
+    $c->controller('solGS::Files')->cache_file($c, $cache_data);
 }
 
 
