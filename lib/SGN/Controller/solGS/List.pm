1148d89296a905b8622011d2b7bf9b35aa1758e3
Isaak <iyt2@cornell.edu>
2019-12-19 13:49:49 +0100

    make user list GS data visible to curators

--- lib/SGN/Controller/solGS/List.pm
+++ lib/SGN/Controller/solGS/List.pm
@@ -909,8 +909,26 @@ sub list_population_summary {
     my $list_id = $c->stash->{list_id};
     my $file_id =  $self->list_file_id($c);	
     my $tmp_dir = $c->stash->{solgs_lists_dir};
+    my $protocol_id = $c->stash->{genotyping_protocol_id};
+    
+    my $list = CXGN::List->new( { dbh => $c->dbc()->dbh(), list_id => $list_id });
+    my $list_name = $list->name;
+    my $desc = $list->description || 'NA';
+    my $owner_id = $list->owner;
+    
+    my $person = CXGN::People::Person->new($c->dbc()->dbh(), $owner_id);
+    my $owner = $person->get_first_name() . ' ' . $person->get_last_name();
 
-    if (!$c->user)
+    my $user_role;
+    my $user_id;
+ 
+    if ($c->user)
+    {
+	$user_role = $c->user->get_object->get_user_type();
+	$user_id = $c->user->get_object->get_sp_person_id();
+    }
+    
+    if (($user_role =~ /submitter|user/  &&  $user_id != $owner_id) || !$c->user)
     {
 	my $page = "/" . $c->req->path;
 	$c->res->redirect("/solgs/login/message?page=$page");
@@ -918,34 +936,17 @@ sub list_population_summary {
     }
     else
     {
-	my $user_name = $c->user->id; 
-        my $protocol = $c->controller('solGS::solGS')->create_protocol_url($c);
-
-	if ($file_id) 
-	{
-	    $c->controller('solGS::Files')->population_metadata_file($c, $tmp_dir, $file_id);   
-	    my $metadata_file = $c->stash->{population_metadata_file}; 
-       
-	    my @metadata = read_file($metadata_file);
-
-	    
-	    my ($key, $desc);
-     
-	    ($desc)        = grep {/description/} @metadata;       
-	    ($key, $desc)  = split(/\t/, $desc);
-	   
-	    my $list = CXGN::List->new( { dbh => $c->dbc()->dbh(), list_id => $list_id });
-	    my $list_name = $list->name;
-	  
-	    $c->stash(project_id          => $file_id,
-		      project_name        => $list_name,
-		      selection_pop_name  => $list_name,
-		      project_desc        => $desc,
-		      owner               => $user_name,
-		      protocol            => $protocol,
-		);  
-	}
+        my $protocol_url = $c->controller('solGS::solGS')->create_protocol_url($c, $protocol_id);
+	   	    
+	$c->stash(project_id          => $file_id,
+		  project_name        => $list_name,
+		  selection_pop_name  => $list_name,
+		  project_desc        => $desc,
+		  owner               => $owner,
+		  protocol            => $protocol_url,
+	    );  
     }
+
 }
 
 
