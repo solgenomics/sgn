53ccd40c77f3c4e36930006c58bd5adf2d787dd8
Mirella Flores <mrf252@cornell.edu>
2020-04-20 18:56:50 -0400

    fixing pagination calls

--- lib/CXGN/BrAPI/v2/Calls.pm
+++ lib/CXGN/BrAPI/v2/Calls.pm
@@ -22,9 +22,9 @@ sub search {
     my $sep_unphased = $inputs->{sep_unphased};
     my $unknown_string = $inputs->{unknown_string};
     my $expand_homozygotes = $inputs->{expand_homozygotes};
-    my $marker_id = $inputs->{variantDbId};
-    my $callset_id = $inputs->{callSetDbId};
-    my $variantset_id = $inputs->{variantSetDbId};
+    my $marker_id = $inputs->{variantDbId} || ($inputs->{variantDbIds} || ());
+    my $callset_id = $inputs->{callSetDbId} || ($inputs->{callSetDbIds} || ());
+    my $variantset_id = $inputs->{variantSetDbId} || ($inputs->{variantSetDbIds} || ());
     my @variantset_id;
 
     if ($sep_phased || $sep_unphased || $expand_homozygotes || $unknown_string){
@@ -59,8 +59,6 @@ sub search {
     });
     my $file_handle = $genotypes_search->get_cached_file_search_json($c, 0);
 
-    my $start_index = $page*$page_size;
-    my $end_index = $page*$page_size + $page_size - 1;
     my $counter = 0;
 
     open my $fh, "<&", $file_handle or die "Can't open output file: $!";
@@ -77,28 +75,26 @@ sub search {
         foreach my $m (@ordered_refmarkers) {
 
             if ( !$marker_id || grep{/^$m$/}@{$marker_id} ) {
-                if ($counter >= $start_index && $counter <= $end_index) {
-                    my $geno = '';
-                    if (exists($genotype->{$m}->{'NT'}) && defined($genotype->{$m}->{'NT'})){
-                        $geno = $genotype->{$m}->{'NT'};
-                    }
-                    elsif (exists($genotype->{$m}->{'GT'}) && defined($genotype->{$m}->{'GT'})){
-                        $geno = $genotype->{$m}->{'GT'};
-                    }
-                    elsif (exists($genotype->{$m}->{'DS'}) && defined($genotype->{$m}->{'DS'})){
-                        $geno = $genotype->{$m}->{'DS'};
-                    }
-                    push @data, {
-                        additionalInfo=>undef,
-                        variantName=>qq|$m|,
-                        variantDbId=>qq|$m|,
-                        callSetDbId=>qq|$gt->{stock_id}|,
-                        callSetName=>qq|$gt->{stock_name}|,
-                        genotype=>{values=>$geno},
-                        genotype_likelihood=>undef,
-                        phaseSet=>undef,
-                    };
+                my $geno = '';
+                if (exists($genotype->{$m}->{'NT'}) && defined($genotype->{$m}->{'NT'})){
+                    $geno = $genotype->{$m}->{'NT'};
                 }
+                elsif (exists($genotype->{$m}->{'GT'}) && defined($genotype->{$m}->{'GT'})){
+                    $geno = $genotype->{$m}->{'GT'};
+                }
+                elsif (exists($genotype->{$m}->{'DS'}) && defined($genotype->{$m}->{'DS'})){
+                    $geno = $genotype->{$m}->{'DS'};
+                }
+                push @data, {
+                    additionalInfo=>undef,
+                    variantName=>qq|$m|,
+                    variantDbId=>qq|$m|,
+                    callSetDbId=>qq|$gt->{stock_id}|,
+                    callSetName=>qq|$gt->{stock_name}|,
+                    genotype=>{values=>$geno},
+                    genotype_likelihood=>undef,
+                    phaseSet=>undef,
+                };
                 $counter++;
             }
         }
@@ -109,8 +105,6 @@ sub search {
                 sepPhased=>undef, 
                 sepUnphased=>undef, 
                 unknownString=>undef);
-
-
 
     my $pagination = CXGN::BrAPI::Pagination->pagination_response($counter,$page_size,$page);
     return CXGN::BrAPI::JSONResponse->return_success(\%result, $pagination, \@data_files, $status, 'VariantSets result constructed');
