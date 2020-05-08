00e03c672fa21e81d67387e5d960ef54fb972f3b
nickmorales <nmorales3142@gmail.com>
2020-05-08 12:28:04 -0400

    new necrosis image analysis api

--- lib/SGN/Controller/AJAX/ImageAnalysis.pm
+++ lib/SGN/Controller/AJAX/ImageAnalysis.pm
@@ -88,7 +88,7 @@ sub image_analysis_submit_POST : Args(0) {
         my $server_endpoint;
         my $image_type_name;
         if ($service eq 'necrosis') {
-            $server_endpoint = "http://18.219.45.102/necrosis/api2/";
+            $server_endpoint = "http://unet.mcrops.org/api/";
             $image_type_name = "image_analysis_necrosis_solomon_nsumba";
         }
         if ($service eq 'whitefly_count') {
