df8f977fecc503aced037756b507056f0b475756
titima15 <tt15@cornell.edu>
2020-06-24 21:47:11 +0000

    fixed tests and removed unused codes

--- lib/SGN/Controller/AJAX/Search/Cross.pm
+++ lib/SGN/Controller/AJAX/Search/Cross.pm
@@ -40,33 +40,7 @@ sub search_cross_female_parents :Path('/ajax/search/cross_female_parents') :Args
 }
 
 
-sub search_cross_details : Path('/ajax/search/cross_details') Args(0) {
-    my $self = shift;
-    my $c = shift;
-    my $female_parent = $c->req->param("female_parent");
-    my $male_parent = $c->req->param("male_parent");
-    my $schema = $c->dbic_schema("Bio::Chado::Schema");
-
-    my $result = CXGN::Cross->get_cross_details($schema, $female_parent, $male_parent);
-    my @cross_details;
-#    print STDERR "RESULTS =".Dumper($result)."\n";
-    foreach my $r (@$result){
-        my ($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $cross_entry_id, $cross_name, $cross_type, $family_id, $family_name, $project_id, $project_name) = @$r;
-        push @cross_details, [ qq{<a href="/stock/$female_parent_id/view">$female_parent_name</a>},
-            qq{<a href="/stock/$male_parent_id/view">$male_parent_name</a>},
-            qq{<a href="/cross/$cross_entry_id">$cross_name</a>},
-            $cross_type,
-            qq{<a href="/stock/$family_id/view">$family_name</a>},
-            qq{<a href="/breeders/trial/$project_id">$project_name</a>},
-        ];
-    }
-
-    $c->stash->{rest}={ data=> \@cross_details};
-
-}
-
-
-sub search_all_crosses : Path('/ajax/search/all_crosses') Args(0) {
+sub search_crosses : Path('/ajax/search/crosses') Args(0) {
     my $self = shift;
     my $c = shift;
 
@@ -114,28 +88,6 @@ sub search_pedigree_female_parents :Path('/ajax/search/pedigree_female_parents')
     my $pedigree_female_parents = CXGN::Cross->get_pedigree_female_parents($schema, $pedigree_male_parent);
 
     $c->stash->{rest} = {data=> $pedigree_female_parents};
-
-}
-
-
-sub search_all_progenies : Path('/ajax/search/all_progenies') Args(0) {
-    my $self = shift;
-    my $c = shift;
-
-    my $pedigree_female_parent = $c->req->param("pedigree_female_parent");
-    my $pedigree_male_parent = $c->req->param("pedigree_male_parent");
-
-    my $schema = $c->dbic_schema("Bio::Chado::Schema");
-    my $result = CXGN::Cross->get_progeny_info($schema, $pedigree_female_parent, $pedigree_male_parent);
-    my @all_progenies;
-    foreach my $r(@$result){
-        my ($female_parent_id, $female_parent_name, $male_parent_id, $male_parent_name, $progeny_id, $progeny_name, $cross_type) = @$r;
-        push @all_progenies, [ qq{<a href="/stock/$female_parent_id/view">$female_parent_name</a>},
-        qq{<a href="/stock/$male_parent_id/view">$male_parent_name</a>},
-        qq{<a href="/stock/$progeny_id/view">$progeny_name</a>}, $cross_type];
-    }
-
-    $c->stash->{rest}={ data=> \@all_progenies};
 
 }
 
