c97db01dcedbe73af1e8803f699bbeb42f1650d2
titima15 <tt15@cornell.edu>
2020-06-26 14:39:48 +0000

    reformatted progenies and crosses search page

--- lib/SGN/Controller/Search/Cross.pm
+++ lib/SGN/Controller/Search/Cross.pm
@@ -13,4 +13,41 @@ sub search_page : Path('/search/cross') Args(0) {
 
 }
 
+
+sub search_progenies_using_female : Path('/search/progenies_using_female') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/search/cross/progeny_search_using_female.mas';
+
+}
+
+
+sub search_progenies_using_male : Path('/search/progenies_using_male') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/search/cross/progeny_search_using_male.mas';
+
+}
+
+
+sub search_crosses_using_female : Path('/search/crosses_using_female') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/search/cross/cross_search_using_female.mas';
+
+}
+
+
+sub search_crosses_using_male : Path('/search/crosses_using_male') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    $c->stash->{template} = '/search/cross/cross_search_using_male.mas';
+
+}
+
+
 1;
