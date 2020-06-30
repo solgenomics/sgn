2b2ec5ca36b978575de0db45bd7a20e1ccb4ea82
titima15 <tt15@cornell.edu>
2020-06-30 20:37:49 +0000

    remove spaces from both ends of name strings

--- lib/CXGN/Pedigree/ParseUpload/Plugin/ProgeniesExcel.pm
+++ lib/CXGN/Pedigree/ParseUpload/Plugin/ProgeniesExcel.pm
@@ -79,12 +79,14 @@ sub _validate_with_plugin {
         if (!$cross_name || $cross_name eq '') {
             push @error_messages, "Cell A$row_name: cross unique id missing";
         } else {
+            $cross_name =~ s/^\s+|\s+$//g;
             $seen_cross_names{$cross_name}++;
         }
 
         if (!$progeny_name || $progeny_name eq '') {
             push @error_messages, "Cell B$row_name: progeny name missing";
         } else {
+            $progeny_name =~ s/^\s+|\s+$//g;
             $seen_progeny_names{$progeny_name}++;
         }
     }
@@ -142,9 +144,11 @@ sub _parse_with_plugin {
 
         if ($worksheet->get_cell($row,0)){
             $cross_name = $worksheet->get_cell($row,0)->value();
+            $cross_name =~ s/^\s+|\s+$//g;
         }
         if ($worksheet->get_cell($row,1)){
             $progeny_name = $worksheet->get_cell($row,1)->value();
+            $progeny_name =~ s/^\s+|\s+$//g;
         }
         #skip blank lines or lines with no name, type and parent
         if (!$cross_name && !$progeny_name) {
