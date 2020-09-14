d9c99856da13afdd02d4ef1aa5ad9498a929a875
titima15 <tt15@cornell.edu>
2020-09-14 21:17:39 +0000

    fixed ajax encoding

--- lib/SGN/Controller/AJAX/FamilyName.pm
+++ lib/SGN/Controller/AJAX/FamilyName.pm
@@ -1,54 +1,32 @@
 
 =head1 NAME
 
-SGN::Controller::AJAX::Cross - a REST controller class to provide the
-functions for adding crosses
+SGN::Controller::AJAX::FamilyName - a REST controller class to provide the
+functions for retrieving family name related info
 
 =head1 DESCRIPTION
 
-Add a new cross or upload a file containing crosses to add
 
 =head1 AUTHOR
 
-Jeremy Edwards <jde22@cornell.edu>
-Lukas Mueller <lam87@cornell.edu>
+Titima Tantikanjana <tt15@cornell.edu>
 
 =cut
 
-package SGN::Controller::AJAX::Cross;
+package SGN::Controller::AJAX::FamilyName;
 
 use Moose;
 use Try::Tiny;
-use DateTime;
-use Time::HiRes qw(time);
-use POSIX qw(strftime);
 use Data::Dumper;
-use File::Basename qw | basename dirname|;
-use File::Copy;
-use File::Slurp;
-use File::Spec::Functions;
-use Digest::MD5;
-use List::MoreUtils qw /any /;
-use CXGN::Stock::StockLookup;
 use CXGN::FamilyName;
-use Carp;
-use File::Path qw(make_path);
-use File::Spec::Functions qw / catfile catdir/;
-use CXGN::Cross;
 use JSON;
-use Tie::UrlEncoder; our(%urlencode);
-use LWP::UserAgent;
-use HTML::Entities;
-use URI::Encode qw(uri_encode uri_decode);
-use Sort::Key::Natural qw(natsort);
 
 BEGIN { extends 'Catalyst::Controller::REST' }
-
 __PACKAGE__->config(
     default   => 'application/json',
     stash_key => 'rest',
-    map       => { 'application/json' => 'JSON', 'text/html' => 'JSON' },
-);
+    map       => { 'application/json' => 'JSON' },
+    );
 
 
 sub get_family_parents :Path('/ajax/family/parents') :Args(1) {
