48ee85af9ede21654176723a59d64f7adb579667
jmh579 <jmh579@cornell.edu>
2020-10-01 14:47:27 -0400

    Fix bugs in NIRS upload R script

--- R/Nirs/nirs_upload_filter_aggregate.R
+++ R/Nirs/nirs_upload_filter_aggregate.R
@@ -18,7 +18,10 @@ library(jsonlite)
 args <- commandArgs(trailingOnly = TRUE)
 
 raw.spectra <- jsonlite::fromJSON(txt = args[1], flatten = T) %>%
-  type_convert(cols(.default = col_double(), observationUnitId = col_character()))
+  type_convert(cols(.default = col_double(), observationUnitId = col_character(),
+  device_type = col_character())) %>%
+  dplyr::select(observationUnitID, device_type, starts_with("nirs_spectra"))
+
 wls <- colnames(raw.spectra) %>%
   str_subset("nirs_spectra.") %>%
   str_remove("nirs_spectra.") %>%
@@ -27,8 +30,8 @@ wls <- colnames(raw.spectra) %>%
 #### Generate plot and identify outliers ####
 spec.plot <- raw.spectra %>%
   rename_at(vars(starts_with("nirs_spectra")), ~str_replace(., "nirs_spectra.", "")) %>%
-  #rownames_to_column(var = "unique.id") %>%
-  PlotSpectra(wavelengths = wls, num.col.before.spectra = 7, window.size = 15)
+  rownames_to_column(var = "unique.id") %>%
+  PlotSpectra(wavelengths = wls, num.col.before.spectra = 3, window.size = 15)
 
 #### Output plot ####
 ggsave(plot = spec.plot, filename = args[4], units = "in", height = 7, width = 10)
@@ -36,25 +39,28 @@ ggsave(plot = spec.plot, filename = args[4], units = "in", height = 7, width = 1
 #### Identify outliers ####
 chisq95 <- qchisq(.95, df = length(wls))
 spectra.tagged <- raw.spectra %>%
-  na.omit() %>%
+  drop_na(observationUnitID, starts_with("nirs_spectra")) %>% # allows for case that no device type is present
   FilterSpectra(., filter = F, return.distances = T,
-                num.col.before.spectra = 7,
+                num.col.before.spectra = 2, # observationUnitID, device_type
                 window.size = 15) %>% # TODO write trycatch with different window sizes?
   mutate(outlier = ifelse(.data$h.distances > chisq95, T, F)) %>%
-  dplyr::select(id:observationUnitId, outlier, starts_with("X"), -h.distances) #TODO change to starts_with("nirs_spectra.")
+  dplyr::select(observationUnitId, device_type, outlier, starts_with("nirs_spectra."))
 
 #### Generate CSV with outlier metadata ####
   if(sum(spectra.tagged$outlier > 0)){
-    outlier.df <- spectra.tagged %>% dplyr::filter(outlier) %>%
-            dplyr::select(-starts_with("X")) %>% distinct()
+    outlier.df <- spectra.tagged %>%
+            dplyr::filter(outlier) %>%
+            dplyr::select(observationUnitId, device_type, outlier) %>%
+            distinct()
     write.csv(outlier.df, file = args[5])
   } else{
-    cat("No outliers detected.\n") # TODO what is the best way to print this message to webpage?
+    cat("No outliers detected.\n") # controller will recognize that there is no file and display message for user
   }
 
 #### Output raw JSON with added outlier tags ####
 spectra.tagged %>%
-  nest(nirs_metadata = id:outlier, nirs_spectra = starts_with("X")) %>%
+  group_by(observationUnitID:outlier) %>%
+  nest(nirs_spectra = starts_with("nirs_spectra")) %>%
   jsonlite::toJSON() %>%
   jsonlite::write_json(x = ., path = args[3])
 
@@ -62,10 +68,11 @@ spectra.tagged %>%
 agg.function <- function(x) suppressWarnings(mean(as.numeric(as.character(x)), na.rm= T))
 agg.spectra <- spectra.tagged %>%
   dplyr::filter(!outlier) %>%
-  aggregate(by = c("observationUnitId"), FUN = agg.function)
+  aggregate(by = c("observationUnitId", "device_type", "outlier"), FUN = agg.function)
 
 #### Output filtered and aggregated JSON ####
 agg.spectra %>%
-  nest(nirs_metadata = id:outlier, nirs_spectra = starts_with("X")) %>%
+  group_by(observationUnitId:outlier)
+  nest(nirs_spectra = starts_with("X")) %>%
   jsonlite::toJSON() %>%
   jsonlite::write_json(x = ., path = args[2])
