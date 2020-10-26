d187960c1e43f043baa2cefb7efad0f61b6862b7
titima15 <tt15@cornell.edu>
2020-10-26 19:48:47 +0000

    restrict to uniquename

--- lib/CXGN/List/Validate/Plugin/AccessionsOrCrosses.pm
+++ lib/CXGN/List/Validate/Plugin/AccessionsOrCrosses.pm
@@ -14,32 +14,23 @@ sub validate {
     my $schema = shift;
     my $list = shift;
 
-    my %all_names;
-    my $synonym_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'stock_synonym', 'stock_property')->cvterm_id();
     my $accession_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
     my $cross_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'cross', 'stock_type')->cvterm_id();
-    my $q = "SELECT stock.uniquename, stockprop.value, stockprop.type_id FROM stock LEFT JOIN stockprop USING(stock_id) WHERE (stock.type_id=? OR stock.type_id=?) AND stock.is_obsolete = 'F';";
-    my $h = $schema->storage->dbh()->prepare($q);
-    $h->execute($accession_type_id, $cross_type_id);
-    my %result;
-    while (my ($uniquename, $synonym, $type_id) = $h->fetchrow_array()) {
-        $all_names{$uniquename}++;
-        if ($type_id) {
-            if ($type_id == $synonym_type_id) {
-                $all_names{$synonym}++;
-            }
-        }
-    }
 
-    #print STDERR Dumper \%all_names;
-    my @missing;
-    foreach my $item (@$list) {
-        if (!exists($all_names{$item})) {
-            push @missing, $item;
+    my @missing = ();
+    foreach my $l (@$list) {
+        my $rs = $schema->resultset("Stock::Stock")->search({
+            type_id=> [$accession_type_id, $cross_type_id],
+            uniquename => $l,
+            is_obsolete => {'!=' => 't'},
+        });
+        if ($rs->count() == 0) {
+            push @missing, $l;
         }
     }
 
     return { missing => \@missing };
 }
+
 
 1;
