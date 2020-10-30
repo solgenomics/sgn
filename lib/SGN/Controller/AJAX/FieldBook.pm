f469436f5550fa37c84e2d1efd1c97f971de369f
nickmorales <nmorales3142@gmail.com>
2020-10-29 20:17:53 -0400

    debug

--- lib/SGN/Controller/AJAX/FieldBook.pm
+++ lib/SGN/Controller/AJAX/FieldBook.pm
@@ -59,6 +59,7 @@ sub create_fieldbook_from_trial_POST : Args(0) {
   my $treatment_project_ids = $c->req->param('treatment_project_id') ? [$c->req->param('treatment_project_id')] : [];
   my $metadata_schema = $c->dbic_schema('CXGN::Metadata::Schema');
   my $phenome_schema = $c->dbic_schema('CXGN::Phenome::Schema');
+  print STDERR Dumper $c->req->params();
 
   chomp($trial_id);
   if (!$c->user()) {
