c5792868fa7ccbb33f7d60dad143d6d1594e1e29
titima15 <tt15@cornell.edu>
2020-11-16 15:32:01 +0000

    changed data structure

--- lib/SGN/Controller/AJAX/CrossWishlist.pm
+++ lib/SGN/Controller/AJAX/CrossWishlist.pm
@@ -200,6 +200,9 @@ sub create_cross_wishlist_submit_POST : Args(0) {
     my $test_ona_form_name = $c->config->{odk_crossing_data_test_form_name};
     my $separate_crosswishlist_by_location = $c->config->{odk_crossing_data_separate_wishlist_by_location};
 
+    print STDERR "CROSSES =".Dumper($data)."\n";
+    print STDERR "SELECT PLOT IDS =".Dumper($selected_plot_ids)."\n";
+
     #For test ona forms, the cross wishlists are combined irrespective of location. On non-test forms, the cross wishlists can be separated by location
     my $is_test_form;
     if ($ona_form_name eq $test_ona_form_name){
@@ -891,16 +894,11 @@ sub create_wishlist_by_uploading_POST : Args(0) {
         $c->stash->{rest}->{error} = "You must be logged in to actually create a cross wishlist.";
         $c->detach();
     }
-    my $user_id = $c->user()->get_object()->get_sp_person_id();
-    my $user_name = $c->user()->get_object()->get_username();
     my $site_name = $c->config->{project_name};
 
-    my $time = DateTime->now();
-    my $timestamp = $time->ymd()."_".$time->hms();
-    my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
-    my $people_schema = $c->dbic_schema('CXGN::People::Schema');
-    my $phenome_schema = $c->dbic_schema('CXGN::Phenome::Schema');
-    my $metadata_schema = $c->dbic_schema('CXGN::Metadata::Schema');
+    my $chado_schema = $c->dbic_schema('Bio::Chado::Schema', 'sgn_chado');
+    my $metadata_schema = $c->dbic_schema("CXGN::Metadata::Schema");
+    my $phenome_schema = $c->dbic_schema("CXGN::Phenome::Schema");
 
     my $female_trial_id = $c->req->param('cross_wishlist_upload_female_trial_id');
     my $male_trial_id = $c->req->param('cross_wishlist_upload_male_trial_id');
@@ -918,13 +916,103 @@ sub create_wishlist_by_uploading_POST : Args(0) {
 #    if ($ona_form_name eq $test_ona_form_name){
 #        $is_test_form = 1;
 #    }
-    print STDERR "ONA FORM NAME =".Dumper($ona_form_name)."\n";
 
-#    my $upload = $c->req->upload('wishlist_file');
+    my $dbh = $c->dbc->dbh;
+    my $upload = $c->req->upload('wishlist_file');
+    my $upload_type = 'WishlistExcel';
+    my $parser;
+    my $parsed_data;
+    my $upload_original_name = $upload->filename();
+    my $upload_tempfile = $upload->tempname;
+    my $subdirectory = "wishlist_upload";
+    my $archived_filename_with_path;
+    my $md5;
+    my $validate_file;
+    my $parsed_file;
+    my $parse_errors;
+    my %parsed_data;
+    my %upload_metadata;
+    my $time = DateTime->now();
+    my $timestamp = $time->ymd()."_".$time->hms();
+    my $user_role;
+    my $user_id;
+    my $user_name;
+    my $owner_name;
+    my $session_id = $c->req->param("sgn_session_id");
+
+    if ($session_id){
+        my $dbh = $c->dbc->dbh;
+        my @user_info = CXGN::Login->new($dbh)->query_from_cookie($session_id);
+        if (!$user_info[0]){
+            $c->stash->{rest} = {error=>'You must be logged in to upload wishlist!'};
+            $c->detach();
+        }
+        $user_id = $user_info[0];
+        $user_role = $user_info[1];
+        my $p = CXGN::People::Person->new($dbh, $user_id);
+        $user_name = $p->get_username;
+    } else{
+        if (!$c->user){
+            $c->stash->{rest} = {error=>'You must be logged in to upload wishlist!'};
+            $c->detach();
+        }
+        $user_id = $c->user()->get_object()->get_sp_person_id();
+        $user_name = $c->user()->get_object()->get_username();
+        $user_role = $c->user->get_object->get_user_type();
+    }
+
+    my $uploader = CXGN::UploadFile->new({
+        tempfile => $upload_tempfile,
+        subdirectory => $subdirectory,
+        archive_path => $c->config->{archive_path},
+        archive_filename => $upload_original_name,
+        timestamp => $timestamp,
+        user_id => $user_id,
+        user_role => $user_role
+    });
+
+    ## Store uploaded temporary file in arhive
+    $archived_filename_with_path = $uploader->archive();
+    $md5 = $uploader->get_md5($archived_filename_with_path);
+    if (!$archived_filename_with_path) {
+        $c->stash->{rest} = {error => "Could not save file $upload_original_name in archive",};
+        return;
+    }
+    unlink $upload_tempfile;
+
+    $upload_metadata{'archived_file'} = $archived_filename_with_path;
+    $upload_metadata{'archived_file_type'}="wishlist upload file";
+    $upload_metadata{'user_id'}=$user_id;
+    $upload_metadata{'date'}="$timestamp";
+    #parse uploaded file with appropriate plugin
+    $parser = CXGN::Pedigree::ParseUpload->new(chado_schema => $chado_schema, filename => $archived_filename_with_path);
+    $parser->load_plugin($upload_type);
+    $parsed_data = $parser->parse();
+    #print STDERR "Dumper of parsed data:\t" . Dumper($parsed_data) . "\n";
+
+    if (!$parsed_data){
+        my $return_error = '';
+        my $parse_errors;
+        if (!$parser->has_parse_errors() ){
+            $c->stash->{rest} = {error_string => "Could not get parsing errors"};
+        } else {
+            $parse_errors = $parser->get_parse_errors();
+            #print STDERR Dumper $parse_errors;
+
+            foreach my $error_string (@{$parse_errors->{'error_messages'}}){
+                $return_error .= $error_string."<br>";
+            }
+        }
+        $c->stash->{rest} = {error_string => $return_error};
+        $c->detach();
+    }
+
+#    $c->stash->{rest}->{data} = $cross_wishlist_plot_select_html;
 
 
 
     exit;
+
 
 }
 
