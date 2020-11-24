4995a174a3d82f6a791f8519e843bbf4b93fa1f2
titima15 <tt15@cornell.edu>
2020-11-24 21:07:53 +0000

    modified crossing experiment column

--- bin/change_accessions_to_crosses.pl
+++ bin/change_accessions_to_crosses.pl
@@ -16,7 +16,7 @@ change_accessions_to_crosses.pl -H [dbhost] -D [dbname] -i [infile]
 
 =head1 DESCRIPTION
 
-This script changes stocks with type accession to type cross and adds parents, cross type. The infile provided has 4 columns. The first column contains stock uniquenames stored as accessions. The second column contains female parent info and the third column contains male parent info. The forth column contains cross type info. There is no header on the infile and the infile is .xls.
+This script changes stocks with type accession to type cross, then links cross to parents,a cross type, an nd_experiment and a project.  The infile provided has 5 columns. The first column contains stock uniquenames stored as accession stock type. The second column contains female parent uniquenames. The third column contains male parent uniquenames. The forth column contains cross type info. The fifth column contains crossing experiment names. There is no header on the infile and the infile is .xls.
 =head1 AUTHOR
 
 Titima Tantikanjana <tt15@cornell.edu>
@@ -67,35 +67,29 @@ my $coderef = sub {
     my $female_parent_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'female_parent', 'stock_relationship')->cvterm_id();
     my $male_parent_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'male_parent', 'stock_relationship')->cvterm_id();
     my $cross_experiment_cvterm_id =  SGN::Model::Cvterm->get_cvterm_row($schema, 'cross_experiment', 'experiment_type')->cvterm_id();
+    my $project_location_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'project location', 'project_property')->cvterm_id();
 
-    my $crossing_experiment_name = $worksheet->get_cell(0,0)->value();
-    $crossing_experiment_name =~ s/^\s+|\s+$//g;
-
-    my $crossing_experiment_rs = $schema->resultset("Project::Project")->find( { name => $crossing_experiment_name });
-    if (!$crossing_experiment_rs) {
-	    print STDERR "Error! Crossing experiment: $crossing_experiment_name was not found in the database.\n";
-	    exit;
-	}
-    my $crossing_experiment_id = $crossing_experiment_rs->project_id();
-
-    my $project_location_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($chado_schema, 'project location', 'project_property')->cvterm_id();
-    my $geolocation_rs = $schema->resultset("Project::Projectprop")->find({project_id => $crossing_experiment_id, type_id => $project_location_cvterm_id});
-
-    for my $row ( 1 .. $row_max ) {
+    for my $row ( 0 .. $row_max ) {
 
     	my $stock_uniquename = $worksheet->get_cell($row,0)->value();
         $stock_uniquename =~ s/^\s+|\s+$//g;
+
     	my $female_parent_uniquename = $worksheet->get_cell($row,1)->value();
         $female_parent_uniquename =~ s/^\s+|\s+$//g;
+
         my $male_parent_uniquename = $worksheet->get_cell($row,2)->value();
         $male_parent_uniquename =~ s/^\s+|\s+$//g;
+
         my $cross_type = $worksheet->get_cell($row,3)->value();
         $cross_type =~ s/^\s+|\s+$//g;
 
+        my $crossing_experiment_name = $worksheet->get_cell($row,4)->value();
+        $crossing_experiment_name =~ s/^\s+|\s+$//g;
 
     	my $stock_rs = $schema->resultset('Stock::Stock')->find({ uniquename => $stock_uniquename, type_id => $accession_cvterm_id });
         my $female_rs = $schema->resultset('Stock::Stock')->find({ uniquename => $female_parent_uniquename, type_id => $accession_cvterm_id });
         my $male_rs = $schema->resultset('Stock::Stock')->find({ uniquename => $male_parent_uniquename, type_id => $accession_cvterm_id });
+        my $crossing_experiment_rs = $schema->resultset("Project::Project")->find( { name => $crossing_experiment_name });
 
         if (!$stock_rs) {
             print STDERR "Error! Stock with uniquename $stock_uniquename was not found in the database.\n";
@@ -109,10 +103,19 @@ my $coderef = sub {
             print STDERR "Error! Male parent with uniquename $male_parent_uniquename was not found in the database.\n";
             next();
         }
+        if (!$crossing_experiment_rs) {
+    	    print STDERR "Error! Crossing experiment: $crossing_experiment_name was not found in the database.\n";
+    	    next();
+    	}
 
+        my $crossing_experiment_id = $crossing_experiment_rs->project_id();
+        my $geolocation_rs = $schema->resultset("Project::Projectprop")->find({project_id => $crossing_experiment_id, type_id => $project_location_cvterm_id});
+
+        #update stock type
         my $cross_stock_rs = $stock_rs->update({ type_id => $cross_cvterm_id});
 
-        my $stock_female_relationship_rs = $self->schema->resultset("Stock::StockRelationship")->search({ object_id => $stock_rs->stock_id(), type_id => $female_parent_cvterm_id });
+         #link cross to female and male parents
+        my $stock_female_relationship_rs = $self->schema->resultset("Stock::StockRelationship")->search({ object_id => $cross_stock_rs->stock_id(), type_id => $female_parent_cvterm_id });
         if (!$stock_female_relationship_rs) {
             $cross_stock_rs->find_or_create_related('stock_relationship_objects', {
                 type_id => $female_parent_cvterm_id,
@@ -122,7 +125,7 @@ my $coderef = sub {
             });
         }
 
-        my $stock_male_relationship_rs = $self->schema->resultset("Stock::StockRelationship")->search({ object_id => $stock_rs->stock_id(), type_id => $male_parent_cvterm_id });
+        my $stock_male_relationship_rs = $self->schema->resultset("Stock::StockRelationship")->search({ object_id => $cross_stock_rs->stock_id(), type_id => $male_parent_cvterm_id });
         if (!$stock_male_relationship_rs) {
             $cross_stock_rs->find_or_create_related('stock_relationship_objects', {
                 type_id => $male_parent_cvterm_id,
@@ -143,7 +146,7 @@ my $coderef = sub {
               type_id  =>  $cross_experiment_cvterm_id,
             });
 
-        #link the experiment to the project
+        #link experiment to the project
         $experiment->find_or_create_related('nd_experiment_projects', {
             project_id => $crossing_experiment_id,
         });
