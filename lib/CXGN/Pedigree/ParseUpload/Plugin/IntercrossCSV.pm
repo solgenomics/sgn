a4200ea5df15011b6ea46a3e121b6da299287f7f
titima15 <tt15@cornell.edu>
2020-12-01 20:59:45 +0000

    validate parents

--- lib/CXGN/Pedigree/ParseUpload/Plugin/IntercrossCSV.pm
+++ lib/CXGN/Pedigree/ParseUpload/Plugin/IntercrossCSV.pm
@@ -15,102 +15,128 @@ sub validate {
     my $self = shift;
     my $filename = shift;
     my $timestamp_included = shift;
-    my $data_level = shift;
+    my $parent_type = shift;
     my $schema = shift;
+    my $delimiter = ',';
     my %parse_result;
-    my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
-                or die "Cannot use CSV: ".Text::CSV->error_diag ();
+    my @error_messages;
 
-    ## Check that the file can be read
-    my @file_lines = read_file($filename);
-    if (!@file_lines) {
+
+    my $csv = Text::CSV->new({ sep_char => ',' });
+
+    open(my $fh, '<', $filename)
+        or die "Could not open file '$filename' $!";
+
+    if (!$fh) {
         $parse_result{'error'} = "Could not read file.";
         print STDERR "Could not read file.\n";
         return \%parse_result;
     }
-    ## Check that the file has at least 2 lines;
-    if (scalar(@file_lines < 2)) {
-        $parse_result{'error'} = "File has less than 2 lines.";
-        print STDERR "File has less than 2 lines.\n";
+
+    my $header_row = <$fh>;
+    my @columns;
+    if ($csv->parse($header_row)) {
+        @columns = $csv->fields();
+    } else {
+        $parse_result{'error'} = "Could not parse header row.";
+        print STDERR "Could not parse header.\n";
         return \%parse_result;
     }
 
-        my $header = shift(@file_lines);
-        my $status  = $csv->parse($header);
-        my @header_row = $csv->fields();
-
-        if (!$header_row[1]) {
-            $parse_result{'error'} = "File has no header row.";
-            print STDERR "File has no header row.\n";
-            return \%parse_result;
-        }
-
-        #  Check headers
-        if ($header_row[0] ne 'crossDbId'){
+    #  Check headers
+    if ($columns[0] ne 'crossDbId'){
             $parse_result{'error'} = "File contents incorrect. The first column must be crossDbId.";
             return \%parse_result;
         }
 
-        if ($header_row[1] ne 'femaleObsUnitDbId'){
-            $parse_result{'error'} = "File contents incorrect. The second column must be femaleObsUnitDbId.";
+    if ($columns[1] ne 'femaleObsUnitDbId'){
+        $parse_result{'error'} = "File contents incorrect. The second column must be femaleObsUnitDbId.";
+        return \%parse_result;
+    }
+
+    if ($columns[2] ne 'maleObsUnitDbId'){
+        $parse_result{'error'} = "File contents incorrect. The third column must be maleObsUnitDbId.";
+        return \%parse_result;
+    }
+
+    if ($columns[3] ne 'timestamp'){
+        $parse_result{'error'} = "File contents incorrect. The fourth column must be timestamp.";
+        return \%parse_result;
+    }
+
+    if ($columns[4] ne 'person'){
+        $parse_result{'error'} = "File contents incorrect. The fifth column must be person.";
+        return \%parse_result;
+    }
+
+    if ($columns[5] ne 'experiment'){
+        $parse_result{'error'} = "File contents incorrect. The sixth column must be experiment.";
+        return \%parse_result;
+    }
+
+    if ($columns[6] ne 'type'){
+        $parse_result{'error'} = "File contents incorrect. The seventh column must be type.";
+        return \%parse_result;
+    }
+
+    if ($columns[7] ne 'fruits'){
+        $parse_result{'error'} = "File contents incorrect. The eighth column must be fruits.";
+        return \%parse_result;
+    }
+
+    if ($columns[8] ne 'flowers'){
+        $parse_result{'error'} = "File contents incorrect. The ninth column must be flowers.";
+        return \%parse_result;
+    }
+
+    if ($columns[9] ne 'seeds'){
+        $parse_result{'error'} = "File contents incorrect. The tenth column must be seeds.";
+        return \%parse_result;
+    }
+
+    if($parent_type ne 'accession' && $data_level ne 'plot' && $data_level ne 'plant'){
+        $parse_result{'error'} = "You must specify if the parents are accessions, plots or plants.";
+        return \%parse_result;
+    }
+
+    my %parent_names;
+    while ( my $row = <$fh> ){
+        my @column_values;
+        if ($csv->parse($row)) {
+            @column_values = $csv->fields();
+        } else {
+            $parse_result{'error'} = "Could not parse row $row.";
+            print STDERR "Could not parse row $row.\n";
             return \%parse_result;
         }
 
-        if ($header_row[2] ne 'maleObsUnitDbId'){
-            $parse_result{'error'} = "File contents incorrect. The third column must be maleObsUnitDbId.";
-            return \%parse_result;
-        }
+        my $female_parent = $column_values[1];
+        $parent_names{$female_parent}++;
 
-        if ($header_row[3] ne 'timestamp'){
-            $parse_result{'error'} = "File contents incorrect. The fourth must be timestamp.";
-            return \%parse_result;
-        }
+        my $male_parent = $column_values[2];
+        $parent_names{$male_parent}++;
 
-        if ($header_row[4] ne 'person'){
-            $parse_result{'error'} = "File contents incorrect. The fifth column must be person.";
-            return \%parse_result;
-        }
+    }
 
-        if ($header_row[5] ne 'experiment'){
-            $parse_result{'error'} = "File contents incorrect. The sixth column must be experiment.";
-            return \%parse_result;
-        }
+    my @parent_list = keys %parent_names;
+    my $parent_validator = CXGN::List::Validate->new();
 
-        if ($header_row[6] ne 'type'){
-            $parse_result{'error'} = "File contents incorrect. The seventh column must be type.";
-            return \%parse_result;
-        }
+    if($parent_type eq 'accessions') {
+        my @parents_missing = @{$parent_validator->validate($schema,'uniquenames',\@parent_list)->{'missing'}};
+    }
 
-        if ($header_row[7] ne 'fruits'){
-            $parse_result{'error'} = "File contents incorrect. The eighth column must be fruits.";
-            return \%parse_result;
-        }
+    if($parent_type eq 'plots') {
+        my @parents_missing = @{$parent_validator->validate($schema,'plots',\@parent_list)->{'missing'}};
+    }
 
-        if ($header_row[8] ne 'flowers'){
-            $parse_result{'error'} = "File contents incorrect. The ninth column must be flowers.";
-            return \%parse_result;
-        }
+    if($parent_type eq 'plants') {
+        my @parents_missing = @{$parent_validator->validate($schema,'plants',\@parent_list)->{'missing'}};
+    }
 
-        if ($header_row[8] ne 'seeds'){
-            $parse_result{'error'} = "File contents incorrect. The tenth column must be seeds.";
-            return \%parse_result;
-        }
+    if (scalar(@parents_missing) > 0) {
+        $parse_result{'error'} = "The following parents are not in the database, or are not in the database as uniquenames: ".join(',',@parents_missing);
+        return \%parse_result;
+    }
 
-
-        if($female_type ne 'accession' && $data_level ne 'plot' && $data_level ne 'plant'){
-            $parse_result{'error'} = "You must specify if the femaleObsUnitDbId information is accession, plot or plant level.";
-            return \%parse_result;
-        }
-
-        if($male_type ne 'accession' && $data_level ne 'plot' && $data_level ne 'plant'){
-            $parse_result{'error'} = "You must specify if the maleObsUnitDbId information is accession, plot or plant level.";
-            return \%parse_result;
-        }
-
-
-
-
-
-
-            return 1;
-        }
+    return 1;
+}
