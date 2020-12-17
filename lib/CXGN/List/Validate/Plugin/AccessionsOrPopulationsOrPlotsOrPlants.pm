8fea6fe61534fcef8afc233068b9cb582989a9f0
titima15 <tt15@cornell.edu>
2020-12-17 19:29:54 +0000

    support 4 cross types from intercross

--- lib/CXGN/List/Validate/Plugin/AccessionsOrPopulationsOrPlotsOrPlants.pm
+++ lib/CXGN/List/Validate/Plugin/AccessionsOrPopulationsOrPlotsOrPlants.pm
@@ -1,4 +1,4 @@
-package CXGN::List::Validate::Plugin::AccessionsOrPlotsOrPlants;
+package CXGN::List::Validate::Plugin::AccessionsOrPopulationsOrPlotsOrPlants;
 
 use Moose;
 
@@ -6,7 +6,7 @@ use Data::Dumper;
 use SGN::Model::Cvterm;
 
 sub name {
-    return "accessions_or_plots_or_plants";
+    return "accessions_or_populations_or_plots_or_plants";
 }
 
 sub validate {
@@ -15,13 +15,14 @@ sub validate {
     my $list = shift;
 
     my $accession_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
+    my $population_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'population', 'stock_type')->cvterm_id();
     my $plot_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plot', 'stock_type')->cvterm_id();
     my $plant_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plant', 'stock_type')->cvterm_id();
 
     my @missing = ();
     foreach my $l (@$list) {
         my $rs = $schema->resultset("Stock::Stock")->search({
-            type_id=> [$accession_type_id, $plot_type_id, $plant_type_id],
+            type_id=> [$accession_type_id, $population_type_id, $plot_type_id, $plant_type_id],
             uniquename => $l,
             is_obsolete => {'!=' => 't'},
         });
