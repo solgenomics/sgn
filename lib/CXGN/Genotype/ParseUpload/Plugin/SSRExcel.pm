c8da2f8be9aca90d312f5321125928ce95459e6a
titima15 <tt15@cornell.edu>
2021-01-12 22:10:07 +0000

    ssr plugin

--- lib/CXGN/Genotype/ParseUpload/Plugin/SSRExcel.pm
+++ lib/CXGN/Genotype/ParseUpload/Plugin/SSRExcel.pm
@@ -36,8 +36,8 @@ sub _validate_with_plugin {
 
     my ($row_min, $row_max) = $worksheet->row_range();
     my ($col_min, $col_max) = $worksheet->col_range();
-    if (($col_max - $col_min)  < 1 || ($row_max - $row_min) < 4 ) { #must have header and at least one row of progeny
-        push @error_messages, "Spreadsheet is missing header or no progeny data";
+    if (($col_max - $col_min)  < 1 || ($row_max - $row_min) < 2 ) { #must have marker names and product sizes and at least one row of data
+        push @error_messages, "Spreadsheet is missing marker name info, product size info or no data";
         $errors{'error_messages'} = \@error_messages;
         $self->_set_parse_errors(\%errors);
         return;
@@ -46,19 +46,18 @@ sub _validate_with_plugin {
     #get column headers
     my $sample_name_head;
 
-    if ($worksheet->get_cell(4,0)) {
-        $sample_name_head  = $worksheet->get_cell(4,0)->value();
+    if ($worksheet->get_cell(0,0)) {
+        $sample_name_head  = $worksheet->get_cell(0,0)->value();
     }
 
     if (!$sample_name_head || $sample_name_head ne 'sample_name' ) {
-        push @error_messages, "Cell A4:sample_name is missing from the header";
+        push @error_messages, "Cell A1:sample_name is missing from the header";
     }
 
     my %seen_sample_names;
 
-    for my $row (5 .. $row_max){
-        my $row_name = $row+1;
-        my sample_name;
+    for my $row (2 .. $row_max){
+        my $sample_name;
 
         if ($worksheet->get_cell($row,0)) {
             $sample_name = $worksheet->get_cell($row,0)->value();
@@ -71,6 +70,21 @@ sub _validate_with_plugin {
             $seen_sample_names{$sample_name}++;
         }
 
+        for my $column (1 .. $col_max) {
+            if ($worksheet->get_cell($row,$column)) {
+                my $ssr_data = $worksheet->get_cell($row,$column)->value();
+                if (!$ssr_data || $ssr_data eq '') {
+                    push @error_messages, "Row:$row Column:$column data missing";
+                }
+
+                if ($ssr_data) {
+                    if (($ssr_data ne '0') || ($ssr_data ne '1') {
+                        push @error_messages, "SSR data in Row:$row_name Column:$column should be either 0 or 1";
+                    }
+                }
+            }
+        }
+
     }
 
     my @samples = keys %seen_sample_names;
@@ -78,7 +92,7 @@ sub _validate_with_plugin {
     my @samples_missing = @{sample_validator->validate($schema,'accessions',\@samples)->{'missing'}};
 
     if (scalar(@samples_missing) > 0){
-        push @error_messages, "The following accessions are not in the database as uniquenames or synonyms: ".join(',',@samples_missing);
+        push @error_messages, "The following accessions are not in the database: ".join(',',@samples_missing);
     }
 
     #store any errors found in the parsed file to parse_errors accessor
@@ -111,27 +125,27 @@ sub _parse_with_plugin {
 
     my %sample_marker_hash;
 
-    for my $row (5 .. $row_max){
+    for my $row (2 .. $row_max){
         my $sample_name;
 
         if ($worksheet->get_cell($row,0)){
             $sample_name = $worksheet->get_cell($row,0)->value();
-            $sample_name =~ s/^\s+|\s+$//g;
+            if ($sample_name) {
+                $sample_name =~ s/^\s+|\s+$//g;
+            }
         }
 
         for my $column (1 .. $col_max ){
             if ($worksheet->get_cell($row,$column)) {
-
-
-
+                my $marker_name = $worksheet->get_cell(0,$column)->value();
+                my $product_size = $worksheet->get_cell(1,$column)->value();
+                $sample_marker_hash{$sample_name}{$marker_name}{$product_size} = $worksheet->get_cell($row,$column)->value();
             }
         }
     }
 
+    $self->_set_parsed_data(\%sample_marker_hash);
 
-
-
-    $self->_set_parsed_data(   );
     return 1;
 }
 
