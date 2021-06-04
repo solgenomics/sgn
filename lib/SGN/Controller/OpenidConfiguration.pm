9d214d38421f28488d6bcc8461296d0fd87517ab
bellerbrock <bje24@cornell.edu>
2021-06-04 16:17:16 -0400

    use file slurp to read and serve openid config

--- lib/SGN/Controller/OpenidConfiguration.pm
+++ lib/SGN/Controller/OpenidConfiguration.pm
@@ -1,18 +1,22 @@
 
 package SGN::Controller::OpenidConfiguration;
 
+use File::Slurp;
 use Moose;
 
 BEGIN { extends "Catalyst::Controller"; }
 
 
 
-sub openid_configuration : Path('/.well-known/openid-configuration') Args(0) { 
+sub openid_configuration : Path('/well-known/openid-configuration') Args(0) {
     my $self = shift;
     my $c = shift;
 
-    $c->stash->{site} =  $c->config->{main_production_site_url};
-    $c->stash->{template} = '/well-known/openid-configuration.txt';
+    my $json = read_file($c->get_conf('basepath') . '/static/documents/openid-configuration.json');
+    my $site = $c->config->{main_production_site_url};
+    $json =~ s/\<\% \$site \%>/$site/; 
+    $c->res->content_type("application/json");
+    $c->res->body($json);
 }
 
 
