af350d46de33214d99175e6fa27224c8e075e31c
afpowell <afp43@cornell.edu>
2021-06-07 13:48:30 +0000

    nirs brapi set up for single protocol, single accession

--- lib/CXGN/BrAPI/v2/Nirs.pm
+++ lib/CXGN/BrAPI/v2/Nirs.pm
@@ -17,11 +17,12 @@ sub search {
 	my $page = $self->page;
     my $status = $self->status;
 	my $schema = $self->bcs_schema();
+	my $stock_ids_arrayref = $params->{observationUnitDbId} || ($params->{observationUnitDbIds} || ());
 
 	my @nirs_protocol_ids;
 	my @data;
 	my @data_files;
-	my $total_count;
+	my $total_count = 1;
 
 	my $high_dim_nirs_protocol_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'high_dimensional_phenotype_nirs_protocol', 'protocol_type')->cvterm_id();
 
@@ -37,28 +38,33 @@ sub search {
 
 
 # 	my $accession_ids = $ds->accessions();
+#	my @accession_array = ['41786'];
+#	my $accession_ids = \@accession_array;
 #     my $plot_ids = $ds->plots();
 #     my $plant_ids = $ds->plants();
-	foreach (@nirs_protocol_ids){
-		print STDERR Dumper $_;
+#	foreach (@nirs_protocol_ids){
+#		print STDERR Dumper $_;
 
 		my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
 	        bcs_schema=>$schema,
-	        nd_protocol_id=>$_,
+#	        nd_protocol_id=>$_,
+	        nd_protocol_id=>'6',
 	        high_dimensional_phenotype_type=>'NIRS',
-#	        query_associated_stocks=>1,
-#	        accession_list=>undef,
-#	        plot_list=>undef,
-#	        plant_list=>undef
+	        query_associated_stocks=>1,
+	        accession_list=>$stock_ids_arrayref,
+	        plot_list=>undef,
+	        plant_list=>undef
 	    });
 		my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
-		print STDERR Dumper $identifer_names;
+		print STDERR Dumper $identifier_names;
 
 		push @data, {
-            name=>$identifier_names,
-            metadata=>$identifier_metadata,
+            device_type=>undef,
+			header_column_names=>$identifier_names,
+            header_column_details=>undef,
+
         };
-	}
+#	}
 	print STDERR Dumper @data;
 
 
@@ -73,26 +79,56 @@ sub search {
 
 #     }
 #
-#     my %result = (data => \@data);
-#     my $pagination = CXGN::BrAPI::Pagination->pagination_response($total_count,$page_size,$page);
-#    my @data_files;
-#    return CXGN::BrAPI::JSONResponse->return_success(\%result, $pagination, \@data_files, $status, 'Breeding methods result constructed');
+my %result = (data => \@data);
+my $pagination = CXGN::BrAPI::Pagination->pagination_response($total_count,$page_size,$page);
+return CXGN::BrAPI::JSONResponse->return_success(\%result, $pagination, \@data_files, $status, 'Nirs result constructed');
 }
 
-sub detail {
-    # my $self = shift;
-    # my $inputs = shift;
-    # my $page_size = $self->page_size;
-    # my $page = $self->page;
-    # my $status = $self->status;
-	#
-	#
-	#
-	#
-	#
-	#
-	# my %result = (data=>\@data_out);
-    # my @data_files;
-    # my $pagination = CXGN::BrAPI::Pagination->pagination_response(1,$page_size,$page);
-    # return CXGN::BrAPI::JSONResponse->return_success(\%result, $pagination, \@data_files, $status, 'Variants result constructed');
-}
+# sub nirs_detail {
+#     my $self = shift;
+#     my $stock_id = shift;
+#
+#     my $status = $self->status;
+#     my $page_size = $self->page_size;
+#     my $page = $self->page;
+#     my @data_files;
+#
+# 	foreach (@nirs_protocol_ids){
+# 		print STDERR Dumper $_;
+#
+# 		my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
+# 	        bcs_schema=>$schema,
+# 	        nd_protocol_id=>$_,
+# 	        high_dimensional_phenotype_type=>'NIRS',
+# 	        query_associated_stocks=>1,
+# 	        accession_list=>undef,
+# 	        plot_list=>undef,
+# 	        plant_list=>undef
+# 	    });
+# 		my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
+# 		print STDERR Dumper $identifier_names;
+#
+# 		push @data, {
+#             name=>$identifier_names,
+#             metadata=>$identifier_metadata,
+#         };
+# 	}
+#
+# 	my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
+# 		bcs_schema=>$schema,
+# 		nd_protocol_id=>$_,
+# 		high_dimensional_phenotype_type=>'NIRS',
+# 	    query_associated_stocks=>1,
+# 	    accession_list=>undef,
+# 	    plot_list=>undef,
+# 	    plant_list=>undef
+# 	});
+# 	my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
+#
+#
+#     my @result = _simple_search($self,[$stock_id]);
+#     my $total_count = scalar(@result);
+#
+#     my $pagination = CXGN::BrAPI::Pagination->pagination_response(1,$page_size,$page);
+#     return CXGN::BrAPI::JSONResponse->return_success(@result, $pagination, \@data_files, $status, 'Nirs detail result constructed');
+# }
