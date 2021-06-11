b7e8b859082e3d42bfa8725f3b9a43b8824680ba
Isaak <iyt2@cornell.edu>
2021-06-11 12:54:16 +0200

    make bin width dynamic, add outer tickvalue...

--- js/source/legacy/solGS/histogram.js
+++ js/source/legacy/solGS/histogram.js
@@ -1,4 +1,4 @@
-/** 
+/**
 * histogram plotting using d3
 * Isaak Y Tecle <iyt2@cornell.edu>
 *
@@ -24,28 +24,28 @@ solGS.histogram =  {
 		    var traitValues = response.data;
 		    var stat = response.stat;
 
-		    traitValues = traitValues.map( function (d) {		   
-			
+		    traitValues = traitValues.map( function (d) {
+
 			return  parseFloat(d[1]);
-		    });	
-		    
+		    });
+
 		    traitValues = traitValues.sort();
-		    traitValues = traitValues.filter( function(val) {		   
-			
-			return !(val === "" 
-				 || typeof val == "undefined" 
-				 || val === null 
+		    traitValues = traitValues.filter( function(val) {
+
+			return !(val === ""
+				 || typeof val == "undefined"
+				 || val === null
 				 || isNaN(val) == true
 				);
 		    });
 
 		    var obs = traitValues.length;
 		    var uniqueValues = solGS.histogram.getUnique(traitValues);
-		    
+
 		    if (uniqueValues.length === 1) {
-			jQuery("#histogram_message").html('<p> All of the valid observations ' 
+			jQuery("#histogram_message").html('<p> All of the valid observations '
 							  + '('+ obs +') ' + 'in this dataset have '
-							  + 'a value of ' + uniqueValues[0] 
+							  + 'a value of ' + uniqueValues[0]
 							  + '. No frequency distribution plot.</p>'
 							 );
 
@@ -55,47 +55,47 @@ solGS.histogram =  {
 			    'canvas' : 'trait_histogram_canvas',
 			    'plot_id': 'trait_histogram_plot'
 			};
-			
+
 			solGS.histogram.plotHistogram(args);
-			
+
 			jQuery("#histogram_message").empty();
-			//solGS.histogram.descriptiveStat(stat);	
+			//solGS.histogram.descriptiveStat(stat);
 		    }
-		} else {                
+		} else {
                     var errorMessage = "<p>This trait has no phenotype data to plot.</p>";
-                    jQuery("#histogram_message").html(errorMessage);  
+                    jQuery("#histogram_message").html(errorMessage);
 		}
-		
+
             },
             error: function(response) {
 		var errorMessage = "<p>Error occured plotting histogram for this trait dataset.</p>";
-		jQuery("#histogram_message").html(errorMessage);                  
+		jQuery("#histogram_message").html(errorMessage);
             }
 	});
-	
+
     },
 
-    
+
     getUnique: function (inputArray) {
-	
+
 	var outputArray = [];
 	for (var i = 0; i < inputArray.length; i++) {
 	    if ((jQuery.inArray(inputArray[i], outputArray)) == -1) {
 		outputArray.push(inputArray[i]);
 	    }
 	}
-	
+
 	return outputArray;
     },
 
     getHistogramParams: function () {
-	
-	var traitId      = jQuery("#trait_id").val();    
+
+	var traitId      = jQuery("#trait_id").val();
 	var population   = solGS.getPopulationDetails();
 	var populationId = population.training_pop_id;
 	var comboPopsId  = population.combo_pops_id;
-	
-	var params = { 
+
+	var params = {
 	    'trait_id'     : traitId,
 	    'training_pop_id': populationId,
 	    'combo_pops_id'  : comboPopsId
@@ -103,25 +103,25 @@ solGS.histogram =  {
 
 	return params;
     },
-    
+
 
     descriptiveStat: function (stat)  {
-	
+
 	var table = '<table style="margin-top: 40px;width:100%;text-align:left">';
 
 	for (var i=0; i < stat.length; i++) {
-	    
+
             if (stat[i]) {
 		table += '<tr>';
 		table += '<td>' + stat[i][0] + '</td>'  + '<td>' + stat[i][1] + '</td>';
 		table += '</tr>';
             }
 	}
-	
+
 	table += '</table>';
 
 	jQuery("#trait_histogram_canvas").append(table);
-	
+
 
     },
 
@@ -133,21 +133,21 @@ solGS.histogram =  {
 	var values = histo.values;
 	var xLabel = histo.x_label || 'Values';
 	var yLabel = histo.y_label || 'Frequency';
-	
+
 	var height = 300;
 	var width  = 500;
-	var pad    = {left:20, top:50, right:40, bottom: 50}; 
+	var pad    = {left:20, top:50, right:50, bottom: 50};
 	var totalH = height + pad.top + pad.bottom;
 	var totalW = width + pad.left + pad.right;
-	
+
 	uniqueValues = this.getUnique(values);
-	
+
 	var binNum;
-	
+
 	if ( uniqueValues.length > 9) {
 	    binNum = 10;
 	} else {
-	    binNum = uniqueValues.length;	
+	    binNum = uniqueValues.length;
 	}
 
 	var xRange;
@@ -168,7 +168,6 @@ solGS.histogram =  {
             .bins(binNum)
         (values);
 
-	
 	var xAxisScale = d3.scale.linear()
             .domain([xMin, xMax])
             .range([0, width]);
@@ -176,14 +175,13 @@ solGS.histogram =  {
 	var yAxisScale = d3.scale.linear()
             .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
             .range([0, height]);
-	
+
+   var tVals = d3.range(xMin, xMax, xRange / binNum);
+    tVals.push(xMax)
 	var xAxis = d3.svg.axis()
             .scale(xAxisScale)
             .orient("bottom")
-            .tickValues(d3.range(xMin, 
-				 xMax, 
-				 0.1 * xRange)
-                       );
+            .tickValues(tVals);
 
 	var yAxisLabel = d3.scale.linear()
             .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
@@ -197,7 +195,7 @@ solGS.histogram =  {
             .append("svg")
             .attr("height", totalH)
             .attr("width", totalW);
-        
+
 	var histogramPlot = svg.append("g")
             .attr("id", plotId)
             .attr("transform", "translate(" +  pad.left + "," + pad.top + ")");
@@ -209,13 +207,13 @@ solGS.histogram =  {
             .attr("class", "bar")
             .attr("transform", function(d) {
 		return "translate(" + xAxisScale(d.x)
-                    + "," + height - yAxisScale(d.y) + ")"; 
-            });     
-	
+                    + "," + height - yAxisScale(d.y) + ")";
+            });
+
 	bar.append("rect")
             .attr("x", function(d) { return 2*pad.left + xAxisScale(d.x); } )
-            .attr("y", function(d) {return height - yAxisScale(d.y); }) 
-            .attr("width", function(d) {return 0.1*width; })
+            .attr("y", function(d) {return height - yAxisScale(d.y); })
+            .attr("width", function(d) {return width / binNum; })
             .attr("height", function(d) { return yAxisScale(d.y); })
             .style("fill", "green")
 	    .style('stroke', 'white')
@@ -225,18 +223,18 @@ solGS.histogram =  {
             .on("mouseout", function() {
                 d3.select(this).style("fill", "green");
             });
-	
+
 	bar.append("text")
             .text(function(d) { return d.y; })
             .attr("y", function(d) {return height - (yAxisScale(d.y) + 10); } )
-            .attr("x",  function(d) { return 2*pad.left + xAxisScale(d.x) + 0.05*width; } )      
+            .attr("x",  function(d) { return 2*pad.left + xAxisScale(d.x) + 0.05*width; } )
             .attr("dy", ".6em")
-            .attr("text-anchor", "end")  
+            .attr("text-anchor", "end")
             .attr("font-family", "sans-serif")
             .attr("font-size", "12px")
             .attr("fill", "green")
             .attr("class", "histoLabel");
-        
+
 	histogramPlot.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(" + (2*pad.left) + "," + height +")")
@@ -244,11 +242,11 @@ solGS.histogram =  {
             .selectAll("text")
             .attr("y", 0)
             .attr("x", 10)
-            .attr("dy", ".1em")         
+            .attr("dy", ".1em")
             .attr("transform", "rotate(90)")
             .attr("fill", "purple")
-            .style({"text-anchor":"start", "fill": "green"});	
-        
+            .style({"text-anchor":"start", "fill": "green"});
+
 	histogramPlot.append("g")
             .attr("class", "y axis")
             .attr("transform", "translate(" + 2* pad.left +  "," + 0 + ")")
@@ -260,23 +258,23 @@ solGS.histogram =  {
             .style("fill", "green");
 
 	histogramPlot.append("g")
-            .attr("transform", "translate(" + (totalW * 0.5) + "," + (height + pad.bottom) + ")")        
+            .attr("transform", "translate(" + (totalW * 0.5) + "," + (height + pad.bottom) + ")")
             .append("text")
-            .text(xLabel)            
+            .text(xLabel)
             .attr("fill", "teal")
             .style("fill", "teal");
 
 	histogramPlot.append("g")
-            .attr("transform", "translate(" + 0 + "," + ( totalH*0.5) + ")")        
+            .attr("transform", "translate(" + 0 + "," + ( totalH*0.5) + ")")
             .append("text")
-            .text(yLabel)            
+            .text(yLabel)
             .attr("fill", "teal")
             .style("fill", "teal")
-            .attr("transform", "rotate(-90)");	
-    },   
+            .attr("transform", "rotate(-90)");
+    },
 
 
-//////    
+//////
 }
 //////
 
