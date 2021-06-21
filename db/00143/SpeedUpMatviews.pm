8c3ad64ce4ee9ac4a8360fbdd1eca55341b069b2
bellerbrock <bje24@cornell.edu>
2021-06-21 10:26:49 -0400

    fix column used as location_id

--- db/00143/SpeedUpMatviews.pm
+++ db/00143/SpeedUpMatviews.pm
@@ -62,7 +62,7 @@ DROP MATERIALIZED VIEW IF EXISTS public.materialized_phenoview CASCADE;
 CREATE MATERIALIZED VIEW public.materialized_phenoview AS
 SELECT
   breeding_program.project_id AS breeding_program_id,
-  location.type_id AS location_id,
+  location.value AS location_id,
   year.value AS year_id,
   trial.project_id AS trial_id,
   accession.stock_id AS accession_id,
