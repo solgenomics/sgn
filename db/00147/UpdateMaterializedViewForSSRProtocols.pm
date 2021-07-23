52b82e192dd57751c8984cd121e2212832fa21e0
titima15 <tt15@cornell.edu>
2021-07-23 08:26:58 +0000

    debugging

--- db/00147/UpdateMaterializedViewForSSRProtocols.pm
+++ db/00147/UpdateMaterializedViewForSSRProtocols.pm
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 
-
 =head1 NAME
 UpdatMaterializedViewForSSRProtocols.pm
 
@@ -13,7 +12,7 @@ see the perldoc of parent class for more details.
 
 =head1 DESCRIPTION
 
-This patch updates wizard materialized view to include SSR protocol:
+This patch updates wizard materialized view to include SSR protocols:
 
 =head1 AUTHOR
 
@@ -49,14 +48,15 @@ sub patch {
     $self->dbh->do(<<EOSQL);
 --do your SQL here
 
-DROP MATERIALIZED VIEW IF EXISTS public.genotyping_protocols CASCADE;
+DROP VIEW IF EXISTS public.genotyping_protocols CASCADE;
 CREATE VIEW public.genotyping_protocols AS
 SELECT nd_protocol.nd_protocol_id AS genotyping_protocol_id,
     nd_protocol.name AS genotyping_protocol_name
-   FROM nd_protocol
-   WHERE nd_protocol.type_id IN (SELECT cvterm_id from cvterm where cvterm.name = 'genotyping_experiment' OR cvterm.name = 'pcr_marker_protocol')
-  GROUP BY public.nd_protocol.nd_protocol_id, public.nd_protocol.name;
+    FROM nd_protocol
+    WHERE nd_protocol.type_id IN (SELECT cvterm_id from cvterm where cvterm.name = 'genotyping_experiment' OR cvterm.name = 'pcr_marker_protocol')
+GROUP BY public.nd_protocol.nd_protocol_id, public.nd_protocol.name;
 ALTER VIEW genotyping_protocols OWNER TO web_usr;
+
 
 EOSQL
 
