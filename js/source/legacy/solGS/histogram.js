c59d53fa30f4af2007a8eebc3d5013d823d7471f
Isaak <iyt2@cornell.edu>
2021-07-28 10:59:50 +0200

    make displaying bin elements for multiple histograms

--- js/source/legacy/solGS/histogram.js
+++ js/source/legacy/solGS/histogram.js
@@ -61,29 +61,39 @@ solGS.histogram =  {
 
     createBinElementsTable: function () {
 
-        var table = '<div class="bin_elements" style="display:none">'
-          + '<table class="table bin_elements_table" style="text-align: left;">'
+        var table = '<table class="table bin_elements_table" style="text-align: left;display:none">'
           + '<thead>'
           +  '<tr>'
           +  '<th>Bin elements</th>'
           +  '<th>Values</th>'
           +  '</tr>'
           + '</thead>'
-          + '</table>'
-          + '</div>';
+          + '</table>';
 
         return table;
     },
 
-    displayBinElements: function(data, canvas) {
-        var table;
-        if (!jQuery(canvas + ' .bin_elements').length) {
-            table = this.createBinElementsTable();
-            jQuery(canvas).append(table);
-        }
+    binElemsTable: function(plotId) {
+        return `${plotId} .bin_elements_table`;
 
-        jQuery(canvas + ' .bin_elements').show();
-        table = canvas  +  ' .bin_elements .bin_elements_table';
+    },
+
+    displayBinElements: function(binValues, namedValues, plotId) {
+
+       var binElems = [];
+       var binMin = d3.min(binValues);
+       var binMax = d3.max(binValues);
+
+       namedValues.forEach(function(el, idx) {
+          var ek = el[1];
+          if ( ek >= binMin && ek <= binMax) {
+              binElems.push(el);
+          }
+       });
+
+        var table = this.binElemsTable(plotId);
+        jQuery(table).show();
+
         if (jQuery.fn.DataTable.isDataTable(table) ) {
              jQuery(table).DataTable().destroy();
         }
@@ -97,7 +107,7 @@ solGS.histogram =  {
         });
 
         table.clear().draw();
-        table.rows.add(data).draw();
+        table.rows.add(binElems).draw();
 
     },
 
@@ -115,192 +125,190 @@ solGS.histogram =  {
     //
     plotHistogram: function (histo) {
 
-	var canvas = histo.canvas || 'histogram_canvas';
-	var plotId = histo.plot_id || 'histogram_plot';
-	var values = histo.values;
-    var namedValues = histo.namedValues;
+    	var canvas = histo.canvas || 'histogram_canvas';
+    	var plotId = histo.plot_id || 'histogram_plot';
+    	var values = histo.values;
+        var namedValues = histo.namedValues;
 
-    var barClr = histo.bar_color || '#9A2EFE';
-    var altBarClr = histo.alt_bar_color ||  '#C07CFE';
-    var caption = histo.caption;
+        var barClr = histo.bar_color || '#9A2EFE';
+        var altBarClr = histo.alt_bar_color ||  '#C07CFE';
+        var caption = histo.caption;
 
+        if (canvas.match(/#/) == null) {canvas = '#' + canvas;}
+        if (plotId.match(/#/) == null) {plotId = '#' + plotId;}
 
-    if (!values || !values[0]) {
-        values = this.extractValues(namedValues);
+        if (!values || !values[0]) {
+            values = this.extractValues(namedValues);
+        }
 
-    }
+        values = this.cleanData(values);
 
-    values = this.cleanData(values);
+    	var height = 300;
+    	var width  = 500;
+    	var pad    = {left:20, top:50, right:50, bottom: 50};
+    	var totalH = height + pad.top + pad.bottom;
+    	var totalW = width + pad.left + pad.right;
 
-	var height = 300;
-	var width  = 500;
-	var pad    = {left:20, top:50, right:50, bottom: 50};
-	var totalH = height + pad.top + pad.bottom;
-	var totalW = width + pad.left + pad.right;
+    	uniqueValues = this.getUnique(values);
 
-	uniqueValues = this.getUnique(values);
+    	var binNum;
 
-	var binNum;
+    	if ( uniqueValues.length > 9) {
+    	    binNum = 10;
+    	} else {
+    	    binNum = uniqueValues.length;
+    	}
 
-	if ( uniqueValues.length > 9) {
-	    binNum = 10;
-	} else {
-	    binNum = uniqueValues.length;
-	}
+    	var xRange;
+    	var xMin;
+    	var xMax;
 
-	var xRange;
-	var xMin;
-	var xMax;
+    	if (binNum == 1) {
+    	    xRange = values[0];
+    	    xMin   = 0;
+    	    xMax   = d3.max(values);
+    	} else {
+    	    xRange = d3.max(values) -  d3.min(values);
+    	    xMin   = d3.min(values);
+    	    xMax   = d3.max(values);
+    	}
 
-	if (binNum == 1) {
-	    xRange = values[0];
-	    xMin   = 0;
-	    xMax   = d3.max(values);
-	} else {
-	    xRange = d3.max(values) -  d3.min(values);
-	    xMin   = d3.min(values);
-	    xMax   = d3.max(values);
-	}
+    	var histogram = d3.layout.histogram()
+                .bins(binNum)
+            (values);
 
-	var histogram = d3.layout.histogram()
-            .bins(binNum)
-        (values);
+    	var xAxisScale = d3.scale.linear()
+                .domain([xMin, xMax])
+                .range([0, width]);
 
-	var xAxisScale = d3.scale.linear()
-            .domain([xMin, xMax])
-            .range([0, width]);
+    	var yAxisScale = d3.scale.linear()
+                .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
+                .range([0, height]);
 
-	var yAxisScale = d3.scale.linear()
-            .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
-            .range([0, height]);
+       var tVals = d3.range(xMin, xMax, xRange / binNum);
+        tVals.push(xMax)
+    	var xAxis = d3.svg.axis()
+                .scale(xAxisScale)
+                .orient("bottom")
+                .tickValues(tVals);
 
-   var tVals = d3.range(xMin, xMax, xRange / binNum);
-    tVals.push(xMax)
-	var xAxis = d3.svg.axis()
-            .scale(xAxisScale)
-            .orient("bottom")
-            .tickValues(tVals);
+    	var yAxisLabel = d3.scale.linear()
+                .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
+                .range([height, 0]);
 
-	var yAxisLabel = d3.scale.linear()
-            .domain([0, d3.max(histogram, ( function (d) {return d.y;}) )])
-            .range([height, 0]);
+    	var yAxis = d3.svg.axis()
+                .scale(yAxisLabel)
+                .orient("left");
 
-	var yAxis = d3.svg.axis()
-            .scale(yAxisLabel)
-            .orient("left");
+        var xLabel = histo.x_label || 'Values';
+    	var yLabel = histo.y_label || 'Frequency';
 
-    var xLabel = histo.x_label || 'Values';
-	var yLabel = histo.y_label || 'Frequency';
+    	var svg = d3.select(canvas)
+                .append("svg")
+                .attr("height", totalH)
+                .attr("width", totalW);
 
-	var svg = d3.select(canvas)
-            .append("svg")
-            .attr("height", totalH)
-            .attr("width", totalW);
+    	var histogramPlot = svg.append("g")
+                .attr("id", plotId)
+                .attr("transform", "translate(" +  pad.left + "," + pad.top + ")");
 
-    var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
-        var binElem = [];
-        var dMin = d3.min(d);
-        var dMax = d3.max(d);
+    	var bar = histogramPlot.selectAll(".bar")
+                .data(histogram)
+                .enter()
+                .append("g")
+                .attr("class", "bar")
+                .attr("transform", function(d) {
+    		return "translate(" + xAxisScale(d.x)
+                        + "," + height - yAxisScale(d.y) + ")";
+                });
 
-        namedValues.forEach(function(el, idx) {
-            var ek = el[1];
+    	bar.append("rect")
+                .attr("x", function(d) { return 2*pad.left + xAxisScale(d.x); } )
+                .attr("y", function(d) {return height - yAxisScale(d.y); })
+                .attr("width", function(d) {return width / binNum; })
+                .attr("height", function(d) { return yAxisScale(d.y); })
+                .style("fill", barClr)
+    	        .style('stroke', "#ffffff")
+                .on("mouseover", function(d) {
+                    //solGS.histogram.displayBinElements(d, namedValues, plotId);
+                    d3.select(this).style("fill", altBarClr);
+                })
+                .on("mouseout", function() {
+                    //jQuery(table).hide();
+                    d3.select(this).style("fill", barClr);
+                });;
 
-           if ( ek >= dMin && ek <= dMax) {
-               binElem.push(el);
-           }
-       });
+    	bar.append("text")
+                .text(function(d) { return d.y; })
+                .attr("y", function(d) {return height - (yAxisScale(d.y) + 10); } )
+                .attr("x",  function(d) { return 2*pad.left + xAxisScale(d.x) + 0.05*width; } )
+                .attr("dy", ".6em")
+                .attr("text-anchor", "end")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "12px")
+                .attr("fill", barClr)
+                .attr("class", "histoLabel");
 
-    solGS.histogram.displayBinElements(binElem, canvas);
+    	histogramPlot.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + (2*pad.left) + "," + height +")")
+                .call(xAxis)
+                .selectAll("text")
+                .attr("y", 0)
+                .attr("x", 10)
+                .attr("dy", ".1em")
+                .attr("transform", "rotate(90)")
+                .attr("fill", barClr)
+                .style({"text-anchor":"start", "fill": barClr});
 
-        return 'See below';
-    });
+    	histogramPlot.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + 2* pad.left +  "," + 0 + ")")
+                .call(yAxis)
+                .selectAll("text")
+                .attr("y", 0)
+                .attr("x", -10)
+                .attr("fill", barClr)
+                .style("fill", barClr);
 
-    svg.call(tip)
+    	histogramPlot.append("g")
+                .attr("transform", "translate(" + (totalW * 0.5) + "," + (height + pad.bottom) + ")")
+                .append("text")
+                .text(xLabel)
+                .attr("fill", barClr)
+                .style("fill", barClr);
 
-	var histogramPlot = svg.append("g")
-            .attr("id", plotId)
-            .attr("transform", "translate(" +  pad.left + "," + pad.top + ")");
+    	histogramPlot.append("g")
+                .attr("transform", "translate(" + 0 + "," + ( totalH*0.5) + ")")
+                .append("text")
+                .text(yLabel)
+                .attr("fill", barClr)
+                .style("fill", barClr)
+                .attr("transform", "rotate(-90)");
 
-	var bar = histogramPlot.selectAll(".bar")
-            .data(histogram)
-            .enter()
-            .append("g")
-            .attr("class", "bar")
-            .attr("transform", function(d) {
-		return "translate(" + xAxisScale(d.x)
-                    + "," + height - yAxisScale(d.y) + ")";
-            });
+        var plotDivId = plotId.replace('#', '')
+        jQuery(canvas).append(`<div id=${plotDivId}></div>`).show();
+        var table = this.binElemsTable(plotId);
 
-	bar.append("rect")
-            .attr("x", function(d) { return 2*pad.left + xAxisScale(d.x); } )
-            .attr("y", function(d) {return height - yAxisScale(d.y); })
-            .attr("width", function(d) {return width / binNum; })
-            .attr("height", function(d) { return yAxisScale(d.y); })
-            .style("fill", barClr)
-	        .style('stroke', "#ffffff")
-            .on("mouseover", function(d) {
-                tip.show(d);
-                d3.select(this).style("fill", altBarClr);
-            })
+        bar.on("mouseover", function(d) {
+            solGS.histogram.displayBinElements(d, namedValues, plotId);
+            //d3.select(this).style("fill", altBarClr);
+        })
             .on("mouseout", function() {
-                tip.hide();
-                jQuery('.bin_elements').hide();
-                d3.select(this).style("fill", barClr);
-            });
+                jQuery(table).hide();
+                //d3.select(this).style("fill", barClr);
+        });
 
-	bar.append("text")
-            .text(function(d) { return d.y; })
-            .attr("y", function(d) {return height - (yAxisScale(d.y) + 10); } )
-            .attr("x",  function(d) { return 2*pad.left + xAxisScale(d.x) + 0.05*width; } )
-            .attr("dy", ".6em")
-            .attr("text-anchor", "end")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "12px")
-            .attr("fill", barClr)
-            .attr("class", "histoLabel");
 
-	histogramPlot.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(" + (2*pad.left) + "," + height +")")
-            .call(xAxis)
-            .selectAll("text")
-            .attr("y", 0)
-            .attr("x", 10)
-            .attr("dy", ".1em")
-            .attr("transform", "rotate(90)")
-            .attr("fill", barClr)
-            .style({"text-anchor":"start", "fill": barClr});
+        if (!jQuery(table).length) {
+            table = this.createBinElementsTable(table);
+            jQuery(plotId).append(table);
+        }
 
-	histogramPlot.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + 2* pad.left +  "," + 0 + ")")
-            .call(yAxis)
-            .selectAll("text")
-            .attr("y", 0)
-            .attr("x", -10)
-            .attr("fill", barClr)
-            .style("fill", barClr);
-
-	histogramPlot.append("g")
-            .attr("transform", "translate(" + (totalW * 0.5) + "," + (height + pad.bottom) + ")")
-            .append("text")
-            .text(xLabel)
-            .attr("fill", barClr)
-            .style("fill", barClr);
-
-	histogramPlot.append("g")
-            .attr("transform", "translate(" + 0 + "," + ( totalH*0.5) + ")")
-            .append("text")
-            .text(yLabel)
-            .attr("fill", barClr)
-            .style("fill", barClr)
-            .attr("transform", "rotate(-90)");
-
-    if (caption) {
-        jQuery(canvas).append('<br/>' + caption);
-    }
+        if (caption) {
+            jQuery(canvas).append('<br/>' + caption);
+        }
     },
-
 
 }
 //////
