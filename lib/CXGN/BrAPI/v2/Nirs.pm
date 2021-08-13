80f0a550d7a392192e19e2dc816a89580a44b186
afpowell <afp43@cornell.edu>
2021-08-13 17:14:26 +0000

    updated high-dimensional nirs brapi observationUnitDbId calls

--- lib/CXGN/BrAPI/v2/Nirs.pm
+++ lib/CXGN/BrAPI/v2/Nirs.pm
@@ -11,16 +11,17 @@ extends 'CXGN::BrAPI::v2::Common';
 
 sub search {
 	my $self = shift;
-	my $params = shift;
+	my $inputs = shift;
 	my $c = shift;
 	my $page_size = $self->page_size;
 	my $page = $self->page;
     my $status = $self->status;
 	my $schema = $self->bcs_schema();
-	my $stock_ids_arrayref = $params->{observationUnitDbId} || ($params->{observationUnitDbIds} || ());
-	my $stock_id = @$stock_ids_arrayref[0];
-	print STDERR Dumper $stock_id;
+	my $stock_id_arrayref = $inputs->{observationUnitDbId} || ($inputs->{observationUnitDbIds} || ());
+#	my $stock_id = @$stock_ids_arrayref[0];
 	my @nirs_protocol_ids;
+	my @nirs_stock_ids;
+	my $nd_protocol_id;
 	my @data;
 	my @data_files;
 	my $total_count = 1;
@@ -37,6 +38,20 @@ sub search {
 	}
 	print STDERR Dumper @nirs_protocol_ids;
 
+	for $nd_protocol_id (@nirs_protocol_ids) {
+#	$nd_protocol_id = @nirs_protocol_ids[0];
+	print STDERR Dumper $nd_protocol_id;
+
+	if (! defined($stock_id_arrayref)) {
+	my $q = "SELECT stock_id FROM nd_protocol JOIN nd_experiment_protocol ON nd_protocol.nd_protocol_id = nd_experiment_protocol.nd_protocol_id JOIN nd_experiment_stock ON nd_experiment_protocol.nd_experiment_id=nd_experiment_stock.nd_experiment_id WHERE nd_protocol.nd_protocol_id = ?;";
+	my $h = $self->bcs_schema->storage()->dbh()->prepare($q);
+	$h->execute($nd_protocol_id);
+
+	while (my ($nirs_stock_id) = $h->fetchrow_array()) {
+		push @nirs_stock_ids, $nirs_stock_id;
+	}
+	$stock_id_arrayref = \@nirs_stock_ids;
+	}
 
 # 	my $accession_ids = $ds->accessions();
 #	my @accession_array = ['41786'];
@@ -49,23 +64,25 @@ sub search {
 		my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
 	        bcs_schema=>$schema,
 #	        nd_protocol_id=>$_,
-	        nd_protocol_id=>'6',
+	        nd_protocol_id=>$nd_protocol_id,
 	        high_dimensional_phenotype_type=>'NIRS',
 	        query_associated_stocks=>0,
-	        accession_list=>$stock_ids_arrayref,
+	        accession_list=>$stock_id_arrayref,
 	        plot_list=>undef,
 	        plant_list=>undef
 	    });
 		my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
-		print STDERR Dumper $data_matrix;
+#		my $example_stock = @nirs_stock_ids[0];
+		my $example_stock = @$stock_id_arrayref[0];
 		my %data_matrix = %$data_matrix;
 		push @data, {
-            device_type=>$data_matrix{$stock_id}->{device_type},
+			device_type=>$data_matrix{$example_stock}->{device_type},
 			header_column_names=>$identifier_names,
-            header_column_details=>undef,
+			protocol_id=>$nd_protocol_id,
+	#		header_column_details=>undef,
 
-        };
-#	}
+		};
+	}
 	print STDERR Dumper @data;
 
 	my %result = (data => \@data);
@@ -75,51 +92,74 @@ sub search {
 
 sub nirs_matrix {
 	my $self = shift;
+#	my $c = shift;
 	my $nd_protocol_id = shift;
-
+	my $inputs = shift;
+	my $stock_id_arrayref = $inputs->{observationUnitDbId} || ($inputs->{observationUnitDbIds} || ());
 	my $status = $self->status;
 	my $page_size = $self->page_size;
 	my $page = $self->page;
 	my @data_files;
 	my $schema = $self->bcs_schema();
-	my @nirs_protocol_ids;
+	my @nirs_stock_ids;
 	my @data;
 
-	my @protocol_id_array;
-	@protocol_id_array[0] = $nd_protocol_id;
-	my $protocol_id_arrayref = \@protocol_id_array;
+#	my @protocol_id_array;
+#	@protocol_id_array[0] = $nd_protocol_id;
+#	my $protocol_id_arrayref = \@protocol_id_array;
 
-	my $verify_id = $self->bcs_schema->resultset('Stock::Stock')->find({stock_id=>$stock_id});
-	if (!$verify_id) {
-		return CXGN::BrAPI::JSONResponse->return_error($status, 'ObservationUnitDbId does not exist in the database');
-	}
-
-	my $high_dim_nirs_protocol_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'high_dimensional_phenotype_nirs_protocol', 'protocol_type')->cvterm_id();
-
-	my $q = "SELECT nd_protocol_id FROM nd_protocol where type_id = ?;";
+#	my $high_dim_nirs_protocol_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'high_dimensional_phenotype_nirs_protocol', 'protocol_type')->cvterm_id();
+if (! defined($stock_id_arrayref)) {
+	my $q = "SELECT stock_id FROM nd_protocol JOIN nd_experiment_protocol ON nd_protocol.nd_protocol_id = nd_experiment_protocol.nd_protocol_id JOIN nd_experiment_stock ON nd_experiment_protocol.nd_experiment_id=nd_experiment_stock.nd_experiment_id WHERE nd_protocol.nd_protocol_id = ?;";
 	my $h = $self->bcs_schema->storage()->dbh()->prepare($q);
-	$h->execute($high_dim_nirs_protocol_cvterm_id);
+	$h->execute($nd_protocol_id);
 
-	while (my ($nirs_protocol_id) = $h->fetchrow_array()) {
-		push @nirs_protocol_ids, $nirs_protocol_id;
+	while (my ($nirs_stock_id) = $h->fetchrow_array()) {
+		push @nirs_stock_ids, $nirs_stock_id;
 	}
+	$stock_id_arrayref = \@nirs_stock_ids;
+}
+#	print STDERR Dumper \@nirs_stock_ids;
 
 	my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
 		bcs_schema=>$schema,
 #	        nd_protocol_id=>$_,
-		nd_protocol_id=>$protocol_id_arrayref,
+		nd_protocol_id=>$nd_protocol_id,
 		high_dimensional_phenotype_type=>'NIRS',
-		query_associated_stocks=>1,
-		accession_list=>undef,
+		query_associated_stocks=>0,
+		accession_list=>$stock_id_arrayref,
 		plot_list=>undef,
 		plant_list=>undef
 	});
 	my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
 
 	my %data_matrix = %$data_matrix;
-	push @data, {
-		data_matrix=>$data_matrix{$stock_id}->{spectra},
-	};
+
+	foreach (@$stock_id_arrayref) {
+		my @current_row_values;
+		# ordered keys was only included for verifying consistent order
+		#my @ordered_keys;
+		my $current_values = $data_matrix{$_}->{spectra};
+		my %current_values = %$current_values;
+		print STDERR Dumper $current_values;
+		print STDERR Dumper %current_values;
+		foreach my $name (sort keys %current_values) {
+			my $curr_val = $current_values{$name};
+			print STDERR Dumper $curr_val;
+			push @current_row_values, $curr_val;
+			# ordered keys was only included for verifying consistent order
+#			push @ordered_keys, $name;
+		}
+		print STDERR Dumper @current_row_values;
+		push @data, {
+#			data_matrix=>$data_matrix{$stock_id}->{spectra},
+		#	data_matrix=>$data_matrix,
+			key=>$_,
+			# ordered keys was only included for verifying consistent order
+#			labels=>\@ordered_keys,
+			row=>\@current_row_values,
+		};
+	}
 
 #	my $total_count = scalar(@result);
 	my $total_count = 1;
@@ -132,52 +172,51 @@ sub nirs_matrix {
 
 sub nirs_detail {
 	my $self = shift;
-	my $stock_id = shift;
+	my $nd_protocol_id = shift;
 
 	my $status = $self->status;
 	my $page_size = $self->page_size;
 	my $page = $self->page;
 	my @data_files;
 	my $schema = $self->bcs_schema();
-	my @nirs_protocol_ids;
+	my @nirs_stock_ids;
 	my @data;
 
-	my @stock_id_array;
-	@stock_id_array[0] = $stock_id;
-	my $stock_id_arrayref = \@stock_id_array;
-
-	my $verify_id = $self->bcs_schema->resultset('Stock::Stock')->find({stock_id=>$stock_id});
-	if (!$verify_id) {
-		return CXGN::BrAPI::JSONResponse->return_error($status, 'ObservationUnitDbId does not exist in the database');
-	}
-
-	my $high_dim_nirs_protocol_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'high_dimensional_phenotype_nirs_protocol', 'protocol_type')->cvterm_id();
-
-	my $q = "SELECT nd_protocol_id FROM nd_protocol where type_id = ?;";
+	my $q = "SELECT stock_id FROM nd_protocol JOIN nd_experiment_protocol ON nd_protocol.nd_protocol_id = nd_experiment_protocol.nd_protocol_id JOIN nd_experiment_stock ON nd_experiment_protocol.nd_experiment_id=nd_experiment_stock.nd_experiment_id WHERE nd_protocol.nd_protocol_id = ?;";
 	my $h = $self->bcs_schema->storage()->dbh()->prepare($q);
-	$h->execute($high_dim_nirs_protocol_cvterm_id);
+	$h->execute($nd_protocol_id);
 
-	while (my ($nirs_protocol_id) = $h->fetchrow_array()) {
-		push @nirs_protocol_ids, $nirs_protocol_id;
+	while (my ($nirs_stock_id) = $h->fetchrow_array()) {
+		push @nirs_stock_ids, $nirs_stock_id;
 	}
+
+#	my $high_dim_nirs_protocol_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'high_dimensional_phenotype_nirs_protocol', 'protocol_type')->cvterm_id();
+
+#	my $q = "SELECT nd_protocol_id FROM nd_protocol where type_id = ?;";
+#	my $h = $self->bcs_schema->storage()->dbh()->prepare($q);
+#	$h->execute($high_dim_nirs_protocol_cvterm_id);
+
+#	while (my ($nirs_protocol_id) = $h->fetchrow_array()) {
+#		push @nirs_protocol_ids, $nirs_protocol_id;
+#	}
 
 	my $phenotypes_search = CXGN::Phenotypes::HighDimensionalPhenotypesSearch->new({
 		bcs_schema=>$schema,
 #	        nd_protocol_id=>$_,
-		nd_protocol_id=>'6',
+		nd_protocol_id=>$nd_protocol_id,
 		high_dimensional_phenotype_type=>'NIRS',
 		query_associated_stocks=>0,
-		accession_list=>$stock_id_arrayref,
+		accession_list=>\@nirs_stock_ids,
 		plot_list=>undef,
 		plant_list=>undef
 	});
 	my ($data_matrix, $identifier_metadata, $identifier_names) = $phenotypes_search->search();
-
+	my $example_stock = @nirs_stock_ids[0];
 	my %data_matrix = %$data_matrix;
 	push @data, {
-		device_type=>$data_matrix{$stock_id}->{device_type},
+		device_type=>$data_matrix{$example_stock}->{device_type},
 		header_column_names=>$identifier_names,
-		header_column_details=>undef,
+#		header_column_details=>undef,
 
 	};
 
--- lib/SGN/Controller/AJAX/BrAPI.pm
+++ lib/SGN/Controller/AJAX/BrAPI.pm
@@ -5082,7 +5082,7 @@ sub nirs_GET {
 sub nirs_single  : Chained('brapi') PathPart('nirs') CaptureArgs(1) {
 	my $self = shift;
 	my $c = shift;
-	my $nd_protocol_id= shift;
+	my $nd_protocol_id = shift;
 
 	$c->stash->{nd_protocol_id} = $nd_protocol_id;
 }
@@ -5097,7 +5097,7 @@ sub nirs_detail_GET {
 	my $brapi = $self->brapi_module;
 	my $brapi_module = $brapi->brapi_wrapper('Nirs');
 	my $brapi_package_result = $brapi_module->nirs_detail(
-		$c->stash->{stock_id}
+		$c->stash->{nd_protocol_id}
 	);
 	_standard_response_construction($c, $brapi_package_result);
 }
@@ -5112,7 +5112,8 @@ sub nirs_matrix_GET {
 	my $brapi = $self->brapi_module;
 	my $brapi_module = $brapi->brapi_wrapper('Nirs');
 	my $brapi_package_result = $brapi_module->nirs_matrix(
-		$c->stash->{nd_protocol_id}
+		$c->stash->{nd_protocol_id},
+		$clean_inputs
 	);
 	_standard_response_construction($c, $brapi_package_result);
 }
