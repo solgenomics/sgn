24b85125c2c589d1d2a7dbdb94cd2f56c5808519
Lukas Mueller <lam87@cornell.edu>
2021-10-26 10:11:35 -0400

    move authorization token to the header as newly required by GitHub.

--- lib/SGN/Controller/AJAX/Contact.pm
+++ lib/SGN/Controller/AJAX/Contact.pm
@@ -50,8 +50,8 @@ sub submit_contact_form_POST : Args(0) {
 
     my $server_endpoint = "https://api.github.com/repos/solgenomics/contactform/issues?access_token=$github_access_token";
     my $req = HTTP::Request->new(POST => $server_endpoint);
-    $req->header('content-type' => 'application/json');
-
+    $req->header('content-type' => 'application/json', 'Authorization' => "token $github_access_token");
+    
     $body .= "\n\nSent from website: $website_name";
     $body .= "\n\nPlease remember to include the contact person's email in any replies which are directed to them. Please include the github.reply email address as a recipient in all messages, so that they are logged with the open ticket.";
     my $post_data = { "title"=>$title, "body"=> $body, "labels"=>[$website_name] };
