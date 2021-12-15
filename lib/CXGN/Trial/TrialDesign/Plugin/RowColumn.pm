5cdc3c0d591289946b4abf3b107aa382edb9b177
bellerbrock <bje24@cornell.edu>
2021-12-15 13:19:21 -0500

    add blocksdesign R code

--- lib/CXGN/Trial/TrialDesign/Plugin/RowColumn.pm
+++ lib/CXGN/Trial/TrialDesign/Plugin/RowColumn.pm
@@ -1,15 +1,16 @@
 
-package CXGN::Trial::TrialDesign::Plugin::RCBD;
+package CXGN::Trial::TrialDesign::Plugin::RCD;
 
 use Moose::Role;
 
 sub create_design {
   my $self = shift;
-  my %rcbd_design;
+  my %rcd_design;
   my $rbase = R::YapRI::Base->new();
   my @stock_list;
   my $number_of_blocks;
   my $stock_data_matrix;
+  my $control_data_matrix;
   my $r_block;
   my $result_matrix;
   my @plot_numbers;
@@ -17,7 +18,7 @@ sub create_design {
   my @control_names_crbd;
   my @block_numbers;
   my @converted_plot_numbers;
-  my @control_list_crbd;
+  my @control_list;
   my %control_names_lookup;
   my $fieldmap_row_number;
   my @fieldmap_row_numbers;
@@ -30,9 +31,9 @@ sub create_design {
   } else {
     die "No stock list specified\n";
   }
-  if ($self->has_control_list_crbd()) {
-    @control_list_crbd = @{$self->get_control_list_crbd()};
-    %control_names_lookup = map { $_ => 1 } @control_list_crbd;
+  if ($self->has_control_list()) {
+    @control_list = @{$self->get_control_list()};
+    %control_names_lookup = map { $_ => 1 } @control_list;
     $self->_check_controls_and_accessions_lists;
   }
   if ($self->has_number_of_blocks()) {
@@ -54,13 +55,23 @@ sub create_design {
   }
 
   $stock_data_matrix =  R::YapRI::Data::Matrix->new(
+                              {
+                           name => 'stock_data_matrix',
+                           rown => 1,
+                           coln => scalar(@stock_list),
+                           data => \@stock_list,
+                              }
+                             );
+
+  $control_data_matrix =  R::YapRI::Data::Matrix->new(
 						       {
-							name => 'stock_data_matrix',
+							name => 'control_data_matrix',
 							rown => 1,
-							coln => scalar(@stock_list),
-							data => \@stock_list,
+							coln => scalar(@control_list),
+							data => \@control_list,
 						       }
 						      );
+
 
   my $plot_start = $self->get_plot_start_number();
   my $serie;
@@ -74,64 +85,34 @@ sub create_design {
 
   $r_block = $rbase->create_block('r_block');
   $stock_data_matrix->send_rbase($rbase, 'r_block');
+  $control_data_matrix->send_rbase($rbase, 'r_block');
   $r_block->add_command('library(agricolae)');
-  $r_block->add_command('trt <- stock_data_matrix[1,]');
+  $r_block->add_command('library(blocksdesign)');
+  $r_block->add_command('treatments <- stock_data_matrix[1,]');
+  $r_block->add_command('controls <- control_data_matrix[1,]');
   $r_block->add_command('number_of_blocks <- '.$number_of_blocks);
-  $r_block->add_command('randomization_method <- "'.$self->get_randomization_method().'"');
-  if ($self->has_randomization_seed()){
-    $r_block->add_command('randomization_seed <- '.$self->get_randomization_seed());
-    $r_block->add_command('rcbd<-design.rcbd(trt,number_of_blocks,serie='.$serie.',kinds=randomization_method, seed=randomization_seed)');
-  }
-  else {
-    $r_block->add_command('rcbd<-design.rcbd(trt,number_of_blocks,serie='.$serie.',kinds=randomization_method)');
-  }
-  $r_block->add_command('rcbd<-rcbd$book'); #added for agricolae 1.1-8 changes in output
-  $r_block->add_command('rcbd<-as.matrix(rcbd)');
+  $r_block->add_command('number_of_rows <- '.$fieldmap_row_number);
+  $r_block->add_command('number_of_cols <- '.$fieldmap_col_number);
+
+  $r_block->add_command('RCDblocks <- data.frame(
+    block = gl(number_of_blocks,length(treatments)),
+    row = gl(number_of_rows,1),
+    col = gl(number_of_cols,number_of_rows)
+  )');
+  $r_block->add_command('RCD <- design(treatments, RCDblocks)$Design');
+  # $r_block->add_command('RCD <- transform(RCD, is_a_control = ifelse(RCD$treatments %in% controls, TRUE, FALSE))');
+
   $r_block->run_block();
-  $result_matrix = R::YapRI::Data::Matrix->read_rbase( $rbase,'r_block','rcbd');
-  #print STDERR Dumper $result_matrix;
+  $result_matrix = R::YapRI::Data::Matrix->read_rbase( $rbase,'r_block','RCD');
+  print STDERR Dumper $result_matrix;
   @plot_numbers = $result_matrix->get_column("plots");
-  #print STDERR Dumper \@plot_numbers;
+  print STDERR Dumper \@plot_numbers;
   @block_numbers = $result_matrix->get_column("block");
-  @stock_names = $result_matrix->get_column("trt");
-  # @converted_plot_numbers=@{$self->_convert_plot_numbers(\@plot_numbers, \@block_numbers, $number_of_blocks)};
+  @stock_names = $result_matrix->get_column("treatments");
+  @fieldmap_row_numbers = $result_matrix->get_column("row");
+  @col_number_fieldmaps = $result_matrix->get_column("col");
 
-  #generate col_number
-
-  if ($plot_layout_format eq "zigzag") {
-    if (!$fieldmap_col_number){
-      @col_number_fieldmaps = ((1..(scalar(@stock_list))) x $number_of_blocks);
-    } else {
-      @col_number_fieldmaps = ((1..$fieldmap_col_number) x $fieldmap_row_number);
-    }
-  }
-  elsif ($plot_layout_format eq "serpentine") {
-    if (!$fieldmap_row_number)  {
-      for my $rep (1 .. $number_of_blocks){
-        if ($rep % 2){
-          push @col_number_fieldmaps, (1..(scalar(@stock_list)));
-        } else {
-          push @col_number_fieldmaps, (reverse 1..(scalar(@stock_list)));
-        }
-      }
-    } else {
-      for my $rep (1 .. $fieldmap_row_number){
-        if ($rep % 2){
-          push @col_number_fieldmaps, (1..$fieldmap_col_number);
-        } else {
-          push @col_number_fieldmaps, (reverse 1..$fieldmap_col_number);
-        }
-      }
-    }
-  }
-
-  if ($plot_layout_format && !$fieldmap_col_number && !$fieldmap_row_number){
-    @fieldmap_row_numbers = (@block_numbers);
-  }
-  elsif ($plot_layout_format && $fieldmap_row_number){
-      @fieldmap_row_numbers = ((1..$fieldmap_row_number) x $fieldmap_col_number);
-      @fieldmap_row_numbers = sort {$a <=> $b} @fieldmap_row_numbers;
-    }
+  # alter plot numbers for serpentine or custom start number
 
     my %seedlot_hash;
     if($self->get_seedlot_hash){
@@ -156,10 +137,10 @@ sub create_design {
       $plot_info{'row_number'} = $fieldmap_row_numbers[$i];
       $plot_info{'col_number'} = $col_number_fieldmaps[$i];
     }
-    $rcbd_design{$plot_numbers[$i]} = \%plot_info;
+    $rcd_design{$plot_numbers[$i]} = \%plot_info;
   }
-  %rcbd_design = %{$self->_build_plot_names(\%rcbd_design)};
-  return \%rcbd_design;
+  %rcd_design = %{$self->_build_plot_names(\%rcd_design)};
+  return \%rcd_design;
 }
 
 1;
