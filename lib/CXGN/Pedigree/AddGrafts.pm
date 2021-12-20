3cfa3f475fc5bea130fe9eedd464b3484a1f5f2d
Lukas Mueller <lam87@cornell.edu>
2021-12-20 09:33:25 -0500

    add create grafts functionality.

---
+++ lib/CXGN/Pedigree/AddGrafts.pm
@@ -0,0 +1,177 @@
+
+
+=head1 NAME
+
+CXGN::Pedigree::AddPedigrees - a module to add pedigrees to accessions.
+
+=head1 USAGE
+
+ my $pedigree_add = CXGN::Stock::AddPedigrees->new({ schema => $schema, pedigrees => \@pedigrees} );
+ my $validated = $pedigree_add->validate_pedigrees(); #is true when all of the pedigrees are valid and the accessions they point to exist in the database.
+ $pedigree_add->add_pedigrees();
+
+=head1 DESCRIPTION
+
+Adds an array of pedigrees. The stock names used in the pedigree must already exist in the database, and the verify function does this check.   This module is intended to be used in independent loading scripts and interactive dialogs.
+
+=head1 AUTHORS
+
+ Jeremy D. Edwards (jde22@cornell.edu)
+
+=cut
+
+package CXGN::Pedigree::AddGrafts;
+
+use Moose;
+
+use Try::Tiny;
+use Data::Dumper;
+use CXGN::Stock::StockLookup;
+use SGN::Model::Cvterm;
+
+#class_type 'Pedigree', { class => 'Bio::GeneticRelationships::Pedigree' };
+has 'schema' => (
+		 is       => 'rw',
+		 isa      => 'DBIx::Class::Schema',
+		 predicate => 'has_schema',
+		 required => 1,
+		);
+
+=head2 get/set_pedigrees()
+
+ Usage:
+ Desc:         provide a hash of accession_names as keys and pedigree objects as values
+ Ret:
+ Args:
+ Side Effects:
+ Example:
+
+=cut
+
+has 'scion' => (isa => 'Str', is => 'rw');
+has 'rootstock' => (isa => 'Str', is => 'rw');
+
+sub add_grafts {
+    my $self = shift;
+    my $separator_string = shift || "+";
+    my $schema = $self->schema();
+    my %return_errors;
+
+    my $transaction_error = "";
+    my $graft;
+    my $coderef = sub {
+        #print STDERR "Getting cvterms...\n";
+        # get cvterms for scion and rootstock
+        my $scion_of_cvterm = SGN::Model::Cvterm->get_cvterm_row($self->schema(), 'scion_of', 'stock_relationship');
+        my $rootstock_of_cvterm = SGN::Model::Cvterm->get_cvterm_row($self->schema(), 'rootstock_of', 'stock_relationship');
+	my $accession_cvterm = SGN::Model::Cvterm->get_cvterm_row($self->schema(), 'accession', 'stock_type');
+	
+	my $scion_row = $schema->resultset('Stock::Stock')->find( { uniquename => $self->scion });
+
+	my $rootstock_row = $schema->resultset('Stock::Stock')->find( { uniquename => $self->rootstock() });
+
+	my @errors;
+
+	
+	if ($scion_row && $rootstock_row) {
+	    my $graft = join("", $self->scion(), $separator_string, $self->rootstock());
+
+	    my $graft_row = $schema->resultset('Stock::Stock')->find( { uniquename => $graft });
+
+	    if ($graft_row) {
+		push @errors, "The name for the graft $graft already exists in the database, not storing.\n";
+	    }
+	    else {
+	        $graft_row = $self->schema->resultset('Stock::Stock')->create(
+		    {
+			name => $graft,
+			uniquename => $graft,
+			type_id => $accession_cvterm->cvterm_id()
+		    });
+	    }
+
+	    my $sr = $self->schema()->resultset('Stock::StockRelationship')->find_or_create(
+		{
+		    type_id => $scion_of_cvterm->cvterm_id(),
+		    object_id => $graft_row->stock_id(),
+		    subject_id => $scion_row->stock_id(),
+		});
+
+	    my $sr2 = $self->schema()->resultset('Stock::StockRelationship')->find_or_create(
+		{
+		    type_id => $rootstock_of_cvterm->cvterm_id(),
+		    object_id => $graft_row->stock_id(),
+		    subject_id => $rootstock_row->stock_id(),
+		});
+	}
+	else {
+	    if (!$scion_row) {
+		die "The scion ".$self->scion()." does not exist in the database.";
+	    }
+	    if (!$rootstock_row) {
+		die "The rootstock ".$self->rootstock()." does not exist in the database.";
+	    }
+	}
+
+	## TO DO: Need to add an attribute that this is a graft
+    };
+
+    # try to add all crosses in a transaction
+    try {
+        print STDERR "Performing database operations... \n";
+        $self->schema()->txn_do($coderef);
+        print STDERR "Done.\n";
+    } catch {
+        $transaction_error =  $_;
+    };
+
+    if ($transaction_error) {
+        $return_errors{error} = "Transaction error creating pedigrees: $transaction_error";
+        print STDERR "Transaction error creating pedigrees: $transaction_error\n";
+        return \%return_errors;
+    }
+
+    return { errors => \%return_errors, graft => $graft };
+}
+
+sub validate_grafts {
+    my $self = shift;
+    my $separator_string = shift || '+';
+    
+    my $schema = $self->schema();
+    my %return_errors;
+
+    my $scion = $schema->resultset('Stock::Stock')->find( { uniquename => $self->scion });
+    
+    my $rootstock = $schema->resultset('Stock::Stock')->find( { uniquename => $self->rootstock() });
+    
+    my @errors;
+    my @messages;
+    
+    if ($scion && $rootstock) {
+	my $graft = join("", $scion, $separator_string, $rootstock);
+	
+	my $graft = $schema->resultset('Stock::Stock')->find( { uniquename => $graft });
+	
+	if ($graft) {
+	    push @messages, "The name for the graft $graft already exists in the database, not storing.\n";
+	}
+
+	if (!$scion) {
+	    push @errors,  "The scion $scion does not exist in the database.";
+	}
+	if (!$rootstock) {
+	    push @errors, "The rootstock $rootstock does not exist in the database.";
+	}
+
+	$return_errors{error} = join(", ", @errors);
+	
+	return { errors => \%return_errors, messages => \@messages };
+    }
+}
+
+
+
+#######
+1;
+#######
---
+++ lib/SGN/Controller/AJAX/Grafting.pm
@@ -0,0 +1,378 @@
+
+package SGN::Controller::AJAX::Grafting;
+
+use Moose;
+use List::Util qw | any |;
+use File::Slurp qw | read_file |;
+use Data::Dumper;
+use Bio::GeneticRelationships::Individual;
+use Bio::GeneticRelationships::Pedigree;
+use CXGN::Pedigree::AddGrafts;
+use CXGN::List::Validate;
+use SGN::Model::Cvterm;
+use utf8;
+
+BEGIN { extends 'Catalyst::Controller::REST'; }
+
+__PACKAGE__->config(
+    default   => 'application/json',
+    stash_key => 'rest',
+    map       => { 'application/json' => 'JSON', 'text/html' => 'JSON'  },
+   );
+
+has 'schema' => (
+		 is       => 'rw',
+		 isa      => 'DBIx::Class::Schema',
+		 lazy_build => 1,
+		);
+
+
+sub upload_grafts_verify : Path('/ajax/grafts/upload_verify') Args(0)  {
+    my $self = shift;
+    my $c = shift;
+
+    my $separator_string = $c->req->param("separator_string");
+    
+    if (!$c->user()) {
+	print STDERR "User not logged in... not uploading grafts.\n";
+	$c->stash->{rest} = {error => "You need to be logged in to upload grafts." };
+	return;
+    }
+
+    if (!any { $_ eq "curator" || $_ eq "submitter" } ($c->user()->roles)  ) {
+	$c->stash->{rest} = {error =>  "You have insufficient privileges to add grafts." };
+	return;
+    }
+
+    my $time = DateTime->now();
+    my $user_id = $c->user()->get_object()->get_sp_person_id();
+    my $user_name = $c->user()->get_object()->get_username();
+    my $timestamp = $time->ymd()."_".$time->hms();
+    my $subdirectory = 'graft_upload';
+
+    my $upload = $c->req->upload('graft_uploaded_file');
+    my $upload_tempfile  = $upload->tempname;
+
+    my $upload_original_name  = $upload->filename();
+
+    # check file type by file name extension
+    #
+    if ($upload_original_name =~ /\.xls$|\.xlsx/) {
+	$c->stash->{rest} = { error => "Grafting upload requires a tab delimited file. Excel files (.xls and .xlsx) are currently not supported. Please convert the file and try again." };
+	return;
+    }
+
+    my $md5;
+
+    my @user_roles = $c->user()->roles();
+    my $user_role = shift @user_roles;
+
+    my $params = {
+	tempfile => $upload_tempfile,
+	subdirectory => $subdirectory,
+	archive_path => $c->config->{archive_path},
+	archive_filename => $upload_original_name,
+	timestamp => $timestamp,
+	user_id => $user_id,
+	user_role => $user_role,
+    };
+
+    my $uploader = CXGN::UploadFile->new( $params );
+
+    my %upload_metadata;
+    my $archived_filename_with_path = $uploader->archive();
+
+    if (!$archived_filename_with_path) {
+	$c->stash->{rest} = {error => "Could not save file $upload_original_name in archive",};
+	return;
+    }
+
+    $md5 = $uploader->get_md5($archived_filename_with_path);
+    unlink $upload_tempfile;
+
+    # check if all accessions exist
+    #
+    open(my $F, "< :encoding(UTF-8)", $archived_filename_with_path) || die "Can't open archive file $archived_filename_with_path";
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+    my %stocks;
+
+    my $header = <$F>;
+    $header =~ s/\r//g;
+    chomp($header);
+    my ($scion_accession, $rootstock_accession, $type) =split /\t/, $header;
+
+    my %header_errors;
+
+    if ($scion_accession ne 'scion') {
+	$header_errors{'scion accession'} = "First column must have header 'scion accession' (not '$scion_accession'); ";
+    }
+
+    if ($rootstock_accession ne 'rootstock') {
+	$header_errors{'rootstock accession'} = "Second column must have header 'rootstock accession' (not '$rootstock_accession'); ";
+    }
+
+    if (%header_errors) {
+	my $error = join "<br />", values %header_errors;
+	$c->stash->{rest} = { error => $error, archived_filename_with_path => $archived_filename_with_path };
+	return;
+    }
+
+    
+    my %errors;
+
+    my ($scion, $rootstock);
+    
+    while (<$F>) {
+        chomp;
+        $_ =~ s/\r//g;
+	($scion, $rootstock) = split /\t/;
+	print STDERR "READ: $scion, $rootstock\n";
+        foreach my $acc ($scion, $rootstock) {
+	    if ($acc){
+		$acc =~ s/^\s+|\s+$//g; #trim whitespace from front and end...
+		$stocks{$acc}++;
+            }
+        }
+    }
+    close($F);
+    
+    my @unique_stocks = keys(%stocks);
+    my $accession_validator = CXGN::List::Validate->new();
+    my @accessions_missing = @{$accession_validator->validate($schema,'accessions_or_populations',\@unique_stocks)->{'missing'}};
+
+    if (scalar(@accessions_missing)>0){
+        $errors{"The following accessions could not be found in the database: ".(join ",", @accessions_missing)} = 1;
+    }
+
+    if (%errors) {
+        $c->stash->{rest} = { error => "There were problems loading the graft for the following accessions or populations: ".(join ",", keys(%errors)).". Please fix these errors and try again. (errors: ".(join ", ", values(%errors)).")" };
+        return;
+    }
+
+    print STDERR "UploadGraftCheck1".localtime()."\n";
+    my $grafts = _get_grafts_from_file($c, $archived_filename_with_path);
+    print STDERR "UploadGraftCheck2".localtime()."\n";
+
+    my $error = "";
+    foreach my $g (@$grafts) { 
+
+	my ($scion, $rootstock) = @$g;
+
+	if ($scion eq $rootstock) {
+	    $error .= "Scion and rootstock ($scion and $rootstock) designate the same accession. Not generating a graft.\n";
+
+	}
+	my $add = CXGN::Pedigree::AddGrafts->new({ schema=>$schema });
+	$add->scion($scion);
+	$add->rootstock($rootstock);
+      
+	my $error;
+	my $graft_check = $add->validate_grafts($separator_string);
+	print STDERR "UploadGraftCheck3".localtime()."Complete\n";
+	#print STDERR Dumper $graft_check;
+	if (!$graft_check){
+	    $error .= "There was a problem validating grafts. Grafts were not stored.";
+	}
+    }
+    if ($error){
+        $c->stash->{rest} = {error => $error, archived_file_name => $archived_filename_with_path};
+    } else {
+        $c->stash->{rest} = {archived_file_name => $archived_filename_with_path};
+    }
+}
+
+sub upload_grafts_store : Path('/ajax/grafts/upload_store') Args(0)  {
+    my $self = shift;
+    my $c = shift;
+    my $archived_file_name = $c->req->param('archived_file_name');
+    my $overwrite_grafts = $c->req->param('overwrite_grafts') ne 'false' ? $c->req->param('overwrite_grafts') : 0;
+    my $separator_string = $c->req->param('separator_string');
+
+    print STDERR "ARCHIVED FILE NAME = $archived_file_name\n";
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+
+    my $grafts = _get_grafts_from_file($c, $archived_file_name);
+
+    print STDERR "FILE CONTENTS: ".Dumper($grafts);
+
+    my @added_grafts;
+    my @already_existing_grafts;
+    my @error_grafts;
+    
+    my $error;
+    foreach my $g (@$grafts) {
+	my ($scion, $rootstock) = @$g;
+	print STDERR "Storing scion & rootstock: $scion & $rootstock.\n";
+	my $add = CXGN::Pedigree::AddGrafts->new({ schema=>$schema });
+	$add->scion($scion);
+	$add->rootstock($rootstock);
+		  
+	my $info = $add->add_grafts($separator_string);
+
+	print STDERR Dumper $info;
+
+	if ($info->{errors}){
+	    
+	    else {
+		push @error_grafts, $info->{error};
+	    }
+	}
+	else {
+	    push @added_grafts, $info->{graft};
+	}
+    }
+    	
+    if (@error_grafts){
+        $c->stash->{rest} = { error => join(", ",@error_grafts) };
+        $c->detach();
+    }
+    $c->stash->{rest} = { success => 1, added_grafts => \@added_grafts, already_existing_grafts =>\@already_existing_grafts };
+}
+
+sub validate_grafts {
+    my $self = shift;
+    my $c = shift;
+
+
+}
+
+sub _get_grafts_from_file {
+    my $c = shift;
+    my $archived_filename_with_path = shift;
+
+    open(my $F, "< :encoding(UTF-8)", $archived_filename_with_path) || die "Can't open file $archived_filename_with_path";
+    my $header = <$F>;
+    my @grafts;
+    my $line_num = 2;
+    while (<$F>) {
+	chomp;
+	my ($scion, $rootstock) = split /\t/;
+	push @grafts, [ $scion, $rootstock ];
+    }
+    return \@grafts;
+}
+
+=head2 get_full_graft
+
+Usage:
+    GET "/ajax/grafts/get_full?stock_id=<STOCK_ID>";
+
+Responds with JSON array containing graft relationship objects for the
+accession identified by STOCK_ID and all of its parents (recursively).
+
+=cut
+
+
+=head2 get_relationships
+
+Usage:
+    POST "/ajax/grafts/get_relationships";
+    BODY "stock_id=<STOCK_ID>[&stock_id=<STOCK_ID>...]"
+
+Responds with JSON array containing graft relationship objects for the
+accessions identified by the provided STOCK_IDs.
+
+=cut
+
+sub get_relationships : Path('/ajax/grafts/get_relationships') : ActionClass('REST') { }
+sub get_relationships_POST {
+    my $self = shift;
+    my $c = shift;
+    my $stock_ids = [];
+    my $s_ids = $c->req->body_params->{stock_id};
+    push @{$stock_ids}, (ref $s_ids eq 'ARRAY' ? @$s_ids : $s_ids);
+    my $schema = $c->dbic_schema("Bio::Chado::Schema");
+    my $mother_cvterm = SGN::Model::Cvterm->get_cvterm_row($schema, 'female_parent', 'stock_relationship')->cvterm_id();
+    my $father_cvterm = SGN::Model::Cvterm->get_cvterm_row($schema, 'male_parent', 'stock_relationship')->cvterm_id();
+    my $accession_cvterm = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
+    my $nodes = [];
+    while (@{$stock_ids}){
+        push @{$nodes}, _get_relationships($schema, $mother_cvterm, $father_cvterm, $accession_cvterm, (shift @{$stock_ids}));
+    }
+    $c->stash->{rest} = $nodes;
+}
+
+sub _get_relationships {
+    my $schema = shift;
+    my $mother_cvterm = shift;
+    my $father_cvterm = shift;
+    my $accession_cvterm = shift;
+    my $stock_id = shift;
+    my $name = $schema->resultset("Stock::Stock")->find({stock_id=>$stock_id})->uniquename();
+    my $parents = _get_graft_parents($schema, $mother_cvterm, $father_cvterm, $accession_cvterm, $stock_id);
+    my $children = _get_graft_children($schema, $mother_cvterm, $father_cvterm, $accession_cvterm, $stock_id);
+    return {
+        id => $stock_id,
+        name=>$name,
+        parents=> $parents,
+        children=> $children
+    };
+}
+
+sub _get_graft_parents {
+    my $schema = shift;
+    my $mother_cvterm = shift;
+    my $father_cvterm = shift;
+    my $accession_cvterm = shift;
+    my $stock_id = shift;
+    my $edges = $schema->resultset("Stock::StockRelationship")->search([
+        {
+            'me.object_id' => $stock_id,
+            'me.type_id' => $father_cvterm,
+            'subject.type_id'=> $accession_cvterm
+        },
+        {
+            'me.object_id' => $stock_id,
+            'me.type_id' => $mother_cvterm,
+            'subject.type_id'=> $accession_cvterm
+        }
+    ],{join => 'subject'});
+    my $parents = {};
+    while (my $edge = $edges->next) {
+        if ($edge->type_id==$mother_cvterm){
+            $parents->{mother}=$edge->subject_id;
+        } else {
+            $parents->{father}=$edge->subject_id;
+        }
+    }
+    return $parents;
+}
+
+sub _get_graft_children {
+    my $schema = shift;
+    my $mother_cvterm = shift;
+    my $father_cvterm = shift;
+    my $accession_cvterm = shift;
+    my $stock_id = shift;
+    my $edges = $schema->resultset("Stock::StockRelationship")->search([
+        {
+            'me.subject_id' => $stock_id,
+            'me.type_id' => $father_cvterm,
+            'object.type_id'=> $accession_cvterm
+        },
+        {
+            'me.subject_id' => $stock_id,
+            'me.type_id' => $mother_cvterm,
+            'object.type_id'=> $accession_cvterm
+        }
+    ],{join => 'object'});
+    my $children = {};
+    $children->{mother_of}=[];
+    $children->{father_of}=[];
+    while (my $edge = $edges->next) {
+        if ($edge->type_id==$mother_cvterm){
+            push @{$children->{mother_of}}, $edge->object_id;
+        } else {
+            push @{$children->{father_of}}, $edge->object_id;
+        }
+    }
+    return $children;
+}
+
+# sub _trait_overlay {
+#     my $schema = shift;
+#     my $node_list = shift;
+# }
+
+
+1;
