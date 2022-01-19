dbb6fd71717f9067169bbbc0f6ee7be0fbd36238
Isaak <iyt2@cornell.edu>
2022-01-19 15:41:04 +0100

    update...

--- t/lib/SGN/Test/solGSData.pm
+++ t/lib/SGN/Test/solGSData.pm
@@ -1,3 +1,55 @@
+=head1 NAME
+
+SGN::Test::solGSData - load lists, datasets into fixture db and delete at the end of a test
+
+=head1 SYNOPSIS
+
+
+my $solgs_data = SGN::Test::solGSData->new();
+
+To load lists and datasets to fixture db...
+my $accessions_list =  $solgs_data->load_accessions_list();
+my $accessions_list_name = $accessions_list->{list_name};
+my $accessions_list_id = $accessions_list->{list_id};
+
+my $plots_list =  $solgs_data->load_plots_list();
+my $plots_list_name = $plots_list->{list_name};
+my $plots_list_id = $plots_list->{list_id};
+
+my $trials_list =  $solgs_data->load_trials_list();
+my $trials_list_name = $trials_list->{list_name};
+my $trials_list_id = $trials_list->{list_id};
+
+my $trials_dt = $solgs_data->load_trials_dataset();
+my $trials_dt_name = $trials_dt->{dataset_name};
+my $trials_dt_id = $trials_dt->{dataset_id};
+
+my $accessions_dt = $solgs_data->load_accessions_dataset();
+my $accessions_dt_name = $accessions_dt->{dataset_name};
+my $accessions_dt_id = $accessions_dt->{dataset_id};
+
+
+my $plots_dt = $solgs_data->load_plots_dataset();
+my $plots_dt_name = $plots_dt->{dataset_name};
+my $plots_dt_id = $plots_dt->{dataset_id};
+
+To delete lists and datasets from the fixture db..
+
+foreach my $list_id ($trials_list_id, $accessions_list_id, $plots_list_id) {
+    $solgs_data->delete_list($list_id);
+}
+
+foreach my $dataset_id ($trials_dt_id, $accessions_dt_id, $plots_dt_id) {
+    $solgs_data->delete_dataset($dataset_id);
+}
+
+
+=head1 AUTHOR
+
+Isaak Y Tecle <iyt2@cornell.edu>
+
+=cut
+
 package SGN::Test::solGSData;
 
 
@@ -10,20 +62,60 @@ use CXGN::Dataset;
 
 has 'user_id' => (isa => 'Int',
     is => 'rw',
-    required => 1
+    required => 1,
+    default => 40
+);
+
+has 'trials_ids' => (isa => 'ArrayRef',
+    is => 'rw',
+    default => sub { [139, 141] }
+);
+
+has 'accessions_list_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'accessions list'
+);
+
+has 'plots_list_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'plots list'
+);
+
+has 'trials_list_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'trials list'
+);
+
+has 'accessions_dataset_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'accessions dataset'
+);
+
+has 'plots_dataset_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'plots dataset'
+);
+
+has 'trials_dataset_name' => (isa => 'Str',
+    is => 'rw',
+    default =>'trials dataset'
+);
+
+has 'genotyping_protocol_id' => (isa => 'Int',
+    is => 'rw',
+    default => 1
 );
 
 
-sub add_plots_list {
+sub load_plots_list {
     my $self = shift;
 
     my $list_id = $self->create_plots_list();
-    my $plots = $self->plots_list();
+    my $plots = $self->get_plots_names();
 
-    return  $self->add_list_elems($list_id, $plots, 'plots');
+    return  $self->load_list_elems($list_id, $plots, 'plots');
 
 }
-
 
 sub create_list {
     my $self = shift;
@@ -42,7 +134,7 @@ sub create_list {
 sub create_plots_list {
     my $self = shift;
 
-    my $name = '60 NaCRRI plots';
+    my $name = $self->plots_list_name;
     my $desc = 'solgs plots list';
 
     my $list_id = $self->create_list($name, $desc);
@@ -54,7 +146,7 @@ sub create_plots_list {
 sub create_trials_list {
     my $self = shift;
 
-    my $name = '2 NaCRRI trials';
+    my $name =$self->trials_list_name;
     my $desc = 'solgs nacrri list';
 
     my $list_id = $self->create_list($name, $desc);
@@ -66,7 +158,7 @@ sub create_trials_list {
 sub create_accessions_list {
     my $self = shift;
 
-    my $name = '34 accessions';
+    my $name = $self->accessions_list_name;
     my $desc = 'solgs clones list';
 
     my $list_id = $self->create_list($name, $desc);
@@ -75,41 +167,38 @@ sub create_accessions_list {
 
 }
 
-
-sub add_accessions_list {
+sub load_accessions_list {
     my $self = shift;
 
     my $list_id = $self->create_accessions_list();
-    my $accs = $self->accessions_list();
-    print STDERR "\nadd accessions: @$accs\n";
+    my $accs = $self->get_accessions_names();
+    #my @accs = $accs->[0..34];
 
-    return $self->add_list_elems($list_id, $accs, 'accessions');
+    return $self->load_list_elems($list_id, $accs, 'accessions');
 
 }
 
-sub add_trials_list {
+sub load_trials_list {
     my $self = shift;
 
     my $list_id = $self->create_trials_list();
     my $trials = $self->trials_list();
-    print STDERR "\nadd strials: @$trials\n";
 
-    return $self->add_list_elems($list_id, $trials, 'trials');
+    return $self->load_list_elems($list_id, $trials, 'trials');
 
 }
 
-sub add_list_elems {
+sub load_list_elems {
     my $self = shift;
     my $list_id = shift;
     my $elems = shift;
     my $type = shift;
 
-    my $dbh = $self->get_dbh();
+    my $list = $self->get_list_obj($list_id);
 
-    my $list = CXGN::List->new({dbh => $dbh, list_id => $list_id});
     $list->type($type);
 
-    print STDERR "\nadding $type...: @$elems\n";
+    # print STDERR "\nadding $type...: @$elems\n";
     my $res = $list->add_bulk($elems);
 
     return { list_id => $list_id,
@@ -118,36 +207,251 @@ sub add_list_elems {
 
 }
 
+sub get_list_obj {
+    my $self = shift;
+    my $list_id = shift;
+
+    my $dbh = $self->get_dbh();
+    my $list = CXGN::List->new({dbh => $dbh, list_id => $list_id});
+
+    return $list;
+
+}
+
+sub delete_list {
+    my $self = shift;
+    my $list_id = shift;
+
+    print STDERR "\nDELETING list $list_id\n";
+    CXGN::List::delete_list($self->get_dbh, $list_id);
+
+}
+
+
+sub delete_dataset {
+    my ($self, $dataset_id) = @_;
+
+    my $dataset = $self->get_dataset_obj();
+    $dataset->sp_dataset_id($dataset_id);
+    print STDERR "\nDELETING dataset $dataset_id\n";
+    my $res = $dataset->delete();
+
+}
+
+sub load_trials_dataset {
+    my $self = shift;
+
+    my $dt_name = $self->trials_dataset_name;
+    my $dataset = $self->create_dataset($dt_name);
+    $dataset->trials([139, 141]);
+    $dataset->store();
+
+    return {'dataset_name' => $dataset->name,
+        'dataset_id' =>$dataset->sp_dataset_id
+    };
+}
+
+sub load_accessions_dataset {
+    my $self = shift;
+
+    my $dt_name = $self->accessions_dataset_name;
+    my $dataset = $self->create_dataset($dt_name);
+    $dataset->trials([139]);
+
+    $dataset->accessions($self->get_accessions_ids);
+    $dataset->is_live(1);
+    $dataset->store();
+
+    return {'dataset_name' => $dataset->name,
+        'dataset_id' =>$dataset->sp_dataset_id
+    };
+}
+
+sub load_plots_dataset {
+    my $self = shift;
+
+    my $dt_name = $self->plots_dataset_name;
+    my $dataset = $self->create_dataset($dt_name);
+    $dataset->trials([139]);
+
+    $dataset->plots($self->get_plots_ids);
+    $dataset->is_live(1);
+    $dataset->store();
+
+    return {'dataset_name' => $dataset->name,
+        'dataset_id' =>$dataset->sp_dataset_id
+    };
+}
+
+sub get_dataset_obj {
+    my $self = shift;
+
+    my $dataset = CXGN::Dataset->new({
+	       schema => $self->bcs_schema,
+	       people_schema => $self->people_schema,
+	});
+
+    return $dataset;
+
+}
+
+sub create_dataset {
+    my $self = shift;
+    my $dataset_name = shift;
+
+    my $protocol_id = $self->genotyping_protocol_id;
+    my $dataset = $self->get_dataset_obj();
+
+    $dataset->sp_person_id($self->user_id);
+    $dataset->name($dataset_name);
+    $dataset->genotyping_protocols([$protocol_id]);
+
+    return $dataset;
+
+}
+
 sub get_dbh {
     my $self = shift;
 
-    my $fixture = SGN::Test::Fixture->new();
+    my $fixture = $self->get_fixture_obj();
     return  $fixture->dbh;
+
+}
+
+sub get_fixture_obj {
+    my $self = shift;
+    return SGN::Test::Fixture->new();
+}
+
+sub people_schema {
+    my $self = shift;
+
+    my $fixture = $self->get_fixture_obj();
+    return  $fixture->people_schema;
+
+}
+
+sub bcs_schema {
+    my $self = shift;
+
+    my $fixture = $self->get_fixture_obj();
+    return  $fixture->bcs_schema;
 
 }
 
 sub trials_list {
     my $self = shift;
-    my @trials = ('Kasese solgs trial', 'trial2 NaCRRI');
+
+    my @trials;
+    my $trials_ids = $self->trials_ids;
+
+    foreach my $trial_id (@$trials_ids) {
+        my $trial =  $self->get_trial_obj($trial_id);
+        push @trials, $trial->name;
+    }
 
     return \@trials;
+
 }
 
 sub accessions_list {
     my $self = shift;
-    my @accessions = ('UG120001', 'UG120002', 'UG120003', 'UG120004', 'UG120005', 'UG120006', 'UG120007', 'UG120008', 'UG120009', 'UG120010', 'UG120011', 'UG120012', 'UG120013', 'UG120014', 'UG120015', 'UG120016', 'UG120017', 'UG120018', 'UG120019', 'UG120020', 'UG120021', 'UG120022', 'UG120023', 'UG120024', 'UG120025', 'UG120026', 'UG120027', 'UG120028', 'UG120029', 'UG120030', 'UG120031', 'UG120032', 'UG120033', 'UG120034' );
+    my $trial_id = shift;
 
-    return \@accessions;
+    $trial_id = 139 if !$trial_id;
+
+    my $trial =  my $trial = $self->get_trial_obj($trial_id);
+    my $accessions = $trial->get_accessions();
+
+    return $accessions;
 
 }
 
 sub plots_list {
     my $self = shift;
-    my @plots_list = ('UG120001_block:1_plot:TP1_2012_NaCRRI','UG120002_block:1_plot:TP2_2012_NaCRRI','UG120003_block:1_plot:TP3_2012_NaCRRI', 'UG120004_block:1_plot:TP4_2012_NaCRRI', 'UG120005_block:1_plot:TP5_2012_NaCRRI', 'UG120006_block:1_plot:TP6_2012_NaCRRI', 'UG120007_block:1_plot:TP7_2012_NaCRRI', 'UG120008_block:1_plot:TP8_2012_NaCRRI', 'UG120009_block:1_plot:TP9_2012_NaCRRI', 'UG120010_block:1_plot:TP10_2012_NaCRRI', 'UG120011_block:1_plot:TP11_2012_NaCRRI', 'UG120012_block:1_plot:TP12_2012_NaCRRI', 'UG120013_block:1_plot:TP13_2012_NaCRRI', 'UG120014_block:1_plot:TP14_2012_NaCRRI', 'UG120015_block:1_plot:TP15_2012_NaCRRI', 'UG120016_block:1_plot:TP16_2012_NaCRRI', 'UG120017_block:1_plot:TP17_2012_NaCRRI', 'UG120018_block:1_plot:TP18_2012_NaCRRI', 'UG120019_block:1_plot:TP19_2012_NaCRRI', 'UG120020_block:1_plot:TP20_2012_NaCRRI', 'UG120021_block:1_plot:TP21_2012_NaCRRI', 'UG120022_block:1_plot:TP22_2012_NaCRRI', 'UG120023_block:2_plot:TP23_2012_NaCRRI', 'UG120024_block:2_plot:TP24_2012_NaCRRI', 'UG120025_block:2_plot:TP25_2012_NaCRRI', 'UG120026_block:2_plot:TP26_2012_NaCRRI', 'UG120027_block:2_plot:TP27_2012_NaCRRI', 'UG120028_block:2_plot:TP28_2012_NaCRRI', 'UG120029_block:2_plot:TP29_2012_NaCRRI', 'UG120030_block:2_plot:TP30_2012_NaCRRI', 'UG120031_block:2_plot:TP31_2012_NaCRRI', 'UG120032_block:2_plot:TP32_2012_NaCRRI', 'UG120033_block:2_plot:TP33_2012_NaCRRI', 'UG120034_block:2_plot:TP34_2012_NaCRRI', 'UG120035_block:2_plot:TP35_2012_NaCRRI', 'UG120036_block:2_plot:TP36_2012_NaCRRI', 'UG120037_block:2_plot:TP37_2012_NaCRRI', 'UG120038_block:2_plot:TP38_2012_NaCRRI', 'UG120039_block:2_plot:TP39_2012_NaCRRI', 'UG120040_block:2_plot:TP40_2012_NaCRRI', 'UG120041_block:2_plot:TP41_2012_NaCRRI', 'UG120042_block:2_plot:TP42_2012_NaCRRI', 'UG120043_block:2_plot:TP43_2012_NaCRRI', 'UG120044_block:2_plot:TP44_2012_NaCRRI', 'UG120045_block:3_plot:TP45_2012_NaCRRI', 'UG120046_block:3_plot:TP46_2012_NaCRRI', 'UG120047_block:3_plot:TP47_2012_NaCRRI', 'UG120048_block:3_plot:TP48_2012_NaCRRI', 'UG120049_block:3_plot:TP49_2012_NaCRRI', 'UG120050_block:3_plot:TP50_2012_NaCRRI', 'UG120051_block:3_plot:TP51_2012_NaCRRI', 'UG120052_block:3_plot:TP52_2012_NaCRRI', 'UG120053_block:3_plot:TP53_2012_NaCRRI', 'UG120054_block:3_plot:TP54_2012_NaCRRI', 'UG120055_block:3_plot:TP55_2012_NaCRRI', 'UG120056_block:3_plot:TP56_2012_NaCRRI', 'UG120057_block:3_plot:TP57_2012_NaCRRI', 'UG120058_block:3_plot:TP58_2012_NaCRRI', 'UG120059_block:3_plot:TP59_2012_NaCRRI', 'UG120060_block:3_plot:TP60_2012_NaCRRI');
+    my $trial_id = shift;
 
-    return \@plots_list;
+    $trial_id = 139 if !$trial_id;
+
+    my $trial = $self->get_trial_obj($trial_id);
+    my $plots =  $trial->get_plots();
+
+    return $plots;
 
 }
+
+sub get_trial_obj {
+    my ($self, $trial_id) = @_;
+
+    my $trial = CXGN::Trial->new({'bcs_schema' => $self->bcs_schema,
+        'trial_id' => $trial_id}
+        );
+
+    return $trial;
+
+}
+
+sub get_plots_ids {
+    my $self = shift;
+
+    my $trials_ids = $self->trials_ids;
+    my $plots = $self->plots_list($trials_ids->[0]);
+
+    my @plots_ids;
+    foreach my $pl (@$plots){
+        push @plots_ids, $pl->[0];
+    }
+
+    return \@plots_ids;
+
+}
+
+sub get_plots_names {
+    my $self = shift;
+
+    my $trials_ids = $self->trials_ids;
+    my $plots = $self->plots_list($trials_ids->[0]);
+
+    my @plots_names;
+    foreach my $pl (@$plots){
+        push @plots_names, $pl->[1];
+    }
+
+    return \@plots_names;
+
+}
+
+sub get_accessions_ids {
+    my $self = shift;
+
+    my $trials_ids = $self->trials_ids;
+    my $accessions = $self->accessions_list($trials_ids->[0]);
+
+    my @accessions_ids;
+    foreach my $acc (@$accessions){
+        push @accessions_ids, $acc->{stock_id};
+    }
+
+    return \@accessions_ids;
+
+}
+
+sub get_accessions_names {
+    my $self = shift;
+
+    my $trials_ids = $self->trials_ids;
+    my $accessions = $self->accessions_list($trials_ids->[0]);
+
+    my @accessions_names;
+    foreach my $acc (@$accessions){
+        push @accessions_names, $acc->{accession_name};
+    }
+
+    return \@accessions_names;
+
+}
+
+
 ###
 1;
 ###
