024c151d83da311ac0afd1f230914310ba7a4b84
Naftali Panitz <npanitz@pop-os.localdomain>
2022-01-19 20:27:43 -0500

    added regenerate cache line

--- lib/SGN/Controller/AJAX/Trial.pm
+++ lib/SGN/Controller/AJAX/Trial.pm
@@ -648,6 +648,10 @@ sub save_experimental_design_POST : Args(0) {
             }
         }
     }
+
+    my $trial_layout = CXGN::Trial::TrialLayout->new({ schema => $c->dbic_schema("Bio::Chado::Schema"), trial_id => $save->{'trial_id'}, experiment_type => 'field_layout' });
+    $trial_layout->generate_and_cache_layout();
+    
     my $bs = CXGN::BreederSearch->new( { dbh=>$dbh, dbname=>$c->config->{dbname}, } );
     my $refresh = $bs->refresh_matviews($c->config->{dbhost}, $c->config->{dbname}, $c->config->{dbuser}, $c->config->{dbpass}, 'phenotypes', 'concurrent', $c->config->{basepath});
     
