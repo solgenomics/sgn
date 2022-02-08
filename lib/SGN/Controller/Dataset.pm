91bbf38e97ecd3af8bab520bfc53dc8570eae7a0
Lukas Mueller <lam87@cornell.edu>
2022-02-08 14:09:28 -0500

    Update Dataset.pm

--- lib/SGN/Controller/Dataset.pm
+++ lib/SGN/Controller/Dataset.pm
@@ -8,7 +8,9 @@ BEGIN { extends 'Catalyst::Controller'; }
 sub dataset :Path('dataset') Args(1) {
     my $self = shift;
     my $c = shift;
-
-    $c->{template} = '/dataset/index.mas';
+    my $dataset_id = shift;
+    
+    $c->stash->{dataset_id} = $dataset_id;
+    $c->stash->{template} = '/dataset/index.mas';
     
 }
