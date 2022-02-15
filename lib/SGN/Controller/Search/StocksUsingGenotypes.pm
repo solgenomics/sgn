6230e3ed202ba7f8ee32c2117f9001ed853a72e6
titima15 <tt15@cornell.edu>
2022-02-15 19:05:30 +0000

    redirect to login dialog

--- lib/SGN/Controller/Search/StocksUsingGenotypes.pm
+++ lib/SGN/Controller/Search/StocksUsingGenotypes.pm
@@ -2,12 +2,19 @@
 package SGN::Controller::Search::StocksUsingGenotypes;
 
 use Moose;
+use URI::FromHash 'uri';
 
 BEGIN { extends 'Catalyst::Controller' };
 
 sub search_stocks_using_genotypes : Path('/search/stocks_using_genotypes') Args(0) {
     my $self = shift;
     my $c = shift;
+
+    if (!$c->user()) {
+        # redirect to login page
+        $c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+        return;
+    }
 
     $c->stash->{template} = '/search/stocks_using_genotypes.mas';
 
