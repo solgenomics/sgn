5d545a5ec6ba6311a399da5de0d5027fdc0a0657
Lukas Mueller <lam87@cornell.edu>
2022-02-24 20:52:32 -0500

    require login for the GCPC tool.

--- lib/SGN/Controller/GPCP.pm
+++ lib/SGN/Controller/GPCP.pm
@@ -3,6 +3,7 @@ package SGN::Controller::GPCP;
 
 use Moose;
 use Catalyst::Controller;
+use URI::FromHash 'uri';
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -10,6 +11,10 @@ sub gpcp_input :Path('/tools/gcpc') Args(0) {
     my $self = shift;
     my $c = shift;
 
+    if (!$c->user) { 
+	$c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+	return;
+    }
     $c->stash->{template} = '/tools/gcpc/index.mas';
 }
 
