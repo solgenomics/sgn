5fc86e59887e62b6afe2d0822099870cc1e90aec
Naftali Panitz <npanitz@pop-os.localdomain>
2022-03-04 04:38:10 -0500

    Loaded d3 bar graph with dummy data

--- js/source/entries/dataset_histogram.js
+++ js/source/entries/dataset_histogram.js
@@ -7,8 +7,8 @@ export function init() {
         render() {
             // set the dimensions and margins of the graph
             var margin = {top: 30, right: 30, bottom: 70, left: 60},
-                width = 460 - margin.left - margin.right,
-                height = 400 - margin.top - margin.bottom;
+                width = 560 - margin.left - margin.right,
+                height = 700 - margin.top - margin.bottom;
 
             // append the svg object to the body of the page
             var svg = d3.select("#trait_histogram")
