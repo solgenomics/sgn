e99a25e403c71379e1400cc2c0362cd7da1ca7d6
Naftali Panitz <npanitz@pop-os.localdomain>
2022-03-22 19:43:02 -0400

    changed to dataset id and added integration with real data

--- lib/SGN/Controller/Dataset.pm
+++ lib/SGN/Controller/Dataset.pm
@@ -2,6 +2,7 @@
 package SGN::Controller::Dataset;
 
 use Moose;
+use CXGN::Dataset;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
@@ -10,7 +11,15 @@ sub dataset :Chained('/') Path('dataset') Args(1) {
     my $c = shift;
     my $dataset_id = shift;
     
+    my $dataset = CXGN::Dataset->new({
+        schema => $c->dbic_schema("Bio::Chado::Schema"),
+        people_schema => $c->dbic_schema("CXGN::People::Schema"),
+        sp_dataset_id=> $dataset_id,
+    });
+
+    $c->stash->{dataset_name} = $dataset->name();
     $c->stash->{dataset_id} = $dataset_id;
+    print STDERR "dataset name ".$dataset->name();
     $c->stash->{template} = '/dataset/index.mas';
     
 }
