6d04a3da81811059ee7f3986559baa2055bff123
Isaak <iyt2@cornell.edu>
2022-04-18 14:21:07 +0200

    use stash_json_args fun

--- lib/SGN/Controller/solGS/List.pm
+++ lib/SGN/Controller/solGS/List.pm
@@ -66,18 +66,12 @@ sub generate_check_value :Path('/solgs/generate/checkvalue') Args(0) {
 sub check_predicted_list_selection :Path('/solgs/check/predicted/list/selection') Args(0) {
     my ($self, $c) = @_;
 
-    my $args = $c->req->param('arguments');
+    $c->controller('solGS::Utils')->stash_json_args($c, $c->req->param('arguments'));
 
-    my $json = JSON->new();
-    $args = $json->decode($args);
-
-    my $training_pop_id  = $args->{training_pop_id};
-    my $selection_pop_id = $args->{selection_pop_id};
-    $c->stash->{training_traits_ids} = $args->{training_traits_ids};
-    $c->stash->{genotyping_protocol_id} = $args->{genotyping_protocol_id};
-
+    my $training_pop_id  = $c->stash->{training_pop_id};
+    my $selection_pop_id = $c->stash->{selection_pop_id};
+   
     $c->controller('solGS::Download')->selection_prediction_download_urls($c, $training_pop_id, $selection_pop_id);
-
     my $ret->{output} = $c->stash->{selection_prediction_download};
 
     $ret = to_json($ret);
@@ -91,40 +85,15 @@ sub check_predicted_list_selection :Path('/solgs/check/predicted/list/selection'
 sub load_genotypes_list_selection :Path('/solgs/load/genotypes/list/selection') Args(0) {
     my ($self, $c) = @_;
 
-    my $args = $c->req->param('arguments');
-
-    my $json = JSON->new();
-    $args = $json->decode($args);
-
-    my $training_pop_id  = $args->{training_pop_id}[0];
-    my $selection_pop_id = $args->{selection_pop_id}[0];
-    my $trait_id         = $args->{trait_id}[0];
-    my $protocol_id      = $args->{genotyping_protocol_id};
-
-    $c->stash->{list}                = $args->{list};
-    $c->stash->{list_name}           = $args->{list_name};
-    $c->stash->{list_id}             = $args->{list_id};
-    $c->stash->{data_set_type}       = $args->{data_set_type};
-    $c->stash->{training_pop_id}     = $training_pop_id;
-    $c->stash->{model_id}            = $training_pop_id;
-    $c->stash->{pop_id}              = $training_pop_id;
-    $c->stash->{selection_pop_id}    = $selection_pop_id;
-    $c->stash->{list_prediction}     = $args->{population_type};
-    $c->stash->{trait_id}            = $trait_id;
-
-    $c->stash->{genotyping_protocol_id} = $protocol_id;
-
-    if ($args->{data_set_type} =~ /combined populations/)
-    {
-	 $c->stash->{combo_pops_id}  = $training_pop_id;
-    }
+    $c->controller('solGS::Utils')->stash_json_args($c, $c->req->param('arguments'));
 
     $self->get_genotypes_list_details($c);
-
     my $genotypes_list = $c->stash->{genotypes_list};
     my $genotypes_ids = $c->stash->{genotypes_ids};
 
-    $self->genotypes_list_genotype_file($c, $selection_pop_id, $protocol_id);
+    my $selection_pop_id = $c->stash->{selection_pop_id};
+    my $sel_pop_protocol_id = $c->stash->{selection_pop_genotyping_protocol_id};
+    $self->genotypes_list_genotype_file($c, $selection_pop_id, $sel_pop_protocol_id);
     my $genotype_file = $c->stash->{genotypes_list_genotype_file};
 
     $self->create_list_population_metadata_file($c, $selection_pop_id);
@@ -133,10 +102,9 @@ sub load_genotypes_list_selection :Path('/solgs/load/genotypes/list/selection') 
 
     if (-s $genotype_file)
     {
-	$self->predict_list_selection_gebvs($c);
-
+	    $self->predict_list_selection_gebvs($c);
         $ret->{status} = $c->stash->{status};
-	$ret->{output} = $c->stash->{selection_prediction_download};
+	    $ret->{output} = $c->stash->{selection_prediction_download};
     }
 
     $ret = to_json($ret);
