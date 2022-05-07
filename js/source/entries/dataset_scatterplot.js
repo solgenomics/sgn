dedf29e84c7078897feb5a7c0f2567130592640c
Naftali Panitz <npanitz@pop-os.localdomain>
2022-05-07 19:18:16 -0400

    started with storing outliers in backend but needs work

--- js/source/entries/dataset_scatterplot.js
+++ js/source/entries/dataset_scatterplot.js
@@ -12,9 +12,7 @@ export function init(dataset_id) {
             this.observations = {};
             this.traits = {};
             this.phenoIds = [];
-            this.traitVals
-            // this.xAxisData = [];
-            // this.yAxisData = [];
+            this.traitVals = [];
         }
 
         getPhenotypes() {
@@ -23,7 +21,6 @@ export function init(dataset_id) {
           new jQuery.ajax({
             url: '/ajax/dataset/retrieve/' + this.dataset_id + '/phenotypes?include_phenotype_primary_key=1',
             success: function(response) {
-              console.log(response);
               LocalThis.observations = response.phenotypes;
               LocalThis.setDropDownTraits();
             },
@@ -64,23 +61,23 @@ export function init(dataset_id) {
         }
 
         setData() {
-          // Gets a list of pheno ids, filters them so only the ones that have non null values are included and then sorts the ids by their value by looking up their values in traits hash.
-          this.phenoIds = Object.keys(this.traits[this.selection])
-            .filter((phenoId) => !isNaN(parseFloat(this.traits[this.selection][phenoId])))
-            .sort((a,b) => this.traits[this.selection][a] - this.traits[this.selection][b]);
+          if (this.selection != "default") {
+            // Gets a list of pheno ids, filters them so only the ones that have non null values are included and then sorts the ids by their value by looking up their values in traits hash.
+            this.phenoIds = Object.keys(this.traits[this.selection])
+              .filter((phenoId) => !isNaN(parseFloat(this.traits[this.selection][phenoId])))
+              .sort((a,b) => this.traits[this.selection][a] - this.traits[this.selection][b]);
 
-          console.log(this.phenoIds);
-          this.traitVals = this.phenoIds.map((id) => parseFloat(this.traits[this.selection][id]));
-          console.log(this.traitVals);
-          // console.log(this.traitVals);
-          // Debugging check: You should see a list of ids and the corresponding values, logs should be sorted by increasing values.
-          // for (let id of this.phenoIds) {
-          //   console.log(id, this.traits[this.selection][id].value);
-          // }
+            this.traitVals = this.phenoIds.map((id) => parseFloat(this.traits[this.selection][id]));
+            // console.log(this.traitVals);
+            // Debugging check: You should see a list of ids and the corresponding values, logs should be sorted by increasing values.
+            // for (let id of this.phenoIds) {
+            //   console.log(id, this.traits[this.selection][id].value);
+            // }
+          }
         }
 
         standardDeviation(values) {
-          function average(data) {
+          var average = function(data) {
             var sum = data.reduce(function(sum, value){
               return sum + value;
             }, 0);
@@ -120,81 +117,88 @@ export function init(dataset_id) {
             d3.select("svg").remove();
             LocalThis.selection = event.target.value;
             LocalThis.setData();
-            LocalThis.render(); 
+            if (!this.firstRefresh) {
+              LocalThis.render(); 
+            }
           });
 
-          let downloadOutliersButton = document.getElementById("download_outliers");
-          downloadOutliersButton.onclick = function() {
-            let csv = 'outliers\n' + LocalThis.outliers.map((outlier) => outlier + '\n');
-            var hiddenElement = document.createElement('a');
-            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-            hiddenElement.target = '_blank';
-            hiddenElement.download = `test.csv`;
-            hiddenElement.click();    
+          let storeOutliersButton = document.getElementById("store_outliers");
+          storeOutliersButton.onclick = function() {
+            console.log(LocalThis.outliers);
+            new jQuery.ajax({
+              type: 'POST',
+              url: '/ajax/dataset/store_outliers/' + LocalThis.dataset_id,
+              data: {outliers: LocalThis.outliers},
+              success: function(response) {
+                console.log('outliers successfully stored!');
+              },
+              error: function(response) {
+                alert('Error');
+              }
+            })
           }
-
-
         }
 
         render() {
           if (this.firstRefresh) {
             this.getPhenotypes();
             this.addEventListeners();
-          };
-          const LocalThis = this;
+          } else if (this.selection != "default") {
+            const LocalThis = this;
 
-          var margin = {top: 10, right: 30, bottom: 30, left: 60},
-              width = 1180 - margin.left - margin.right,
-              height = 600 - margin.top - margin.bottom;
-          
-          var svg = d3.select("#trait_graph")
-            .append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-              .attr("transform",
-                    "translate(" + margin.left + "," + margin.top + ")");
+            var margin = {top: 10, right: 30, bottom: 30, left: 60},
+                width = 1180 - margin.left - margin.right,
+                height = 600 - margin.top - margin.bottom;
+            
+            var svg = d3.select("#trait_graph")
+              .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform",
+                      "translate(" + margin.left + "," + margin.top + ")");
 
-          var isOutlier = function(unit, value, mean, stdDev) {
-              let filter = (LocalThis.stdDevMultiplier - 1) / 2 ;
-              if (value >= mean - stdDev * filter && value <= mean + stdDev * filter) {
-                return "green";
-              } else {
-                LocalThis.outliers.push(unit);
-                return "red";
-              }
-          }
-          console.log('traitVals', this.traitVals);
-          const [mean, stdDev] = this.standardDeviation(this.traitVals);
-          this.outliers = [];
-          // Add X axis
-          var x = d3.scaleLinear()
-          .domain([0, this.phenoIds.length])
-          .range([ 0, width]);
-          svg.append("g")
-          .style("font", "18px times")
-          .attr("transform", "translate(0," + height + ")")
-      
-          // Add Y axis
-          var y = d3.scaleLinear()
-          .domain([Math.min(...this.traitVals), Math.max(...this.traitVals)])
-          .range([ height, 0]);
-          svg.append("g")
-          .style("font", "18px times")
-          .call(d3.axisLeft(y))
+            var isOutlier = function(id, value, mean, stdDev) {
+                let filter = (LocalThis.stdDevMultiplier - 1) / 2 ;
+                if (value >= mean - stdDev * filter && value <= mean + stdDev * filter) {
+                  return "green";
+                } else {
+                  LocalThis.outliers.push(id);
+                  return "red";
+                }
+            }
 
-          svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
-            .attr("x",0 - (height / 2))
-            .attr("dy", ".65em")
-            .style("text-anchor", "middle")
+            const [mean, stdDev] = this.standardDeviation(this.traitVals);
+            
+            this.outliers = [];
+            
+            // Add X axis
+            var x = d3.scaleLinear()
+            .domain([0, this.phenoIds.length])
+            .range([ 0, width]);
+            svg.append("g")
             .style("font", "18px times")
-            .text("Trait Value");
+            .attr("transform", "translate(0," + height + ")")
+        
+            // Add Y axis
+            var y = d3.scaleLinear()
+            .domain([Math.min(...this.traitVals), Math.max(...this.traitVals)])
+            .range([ height, 0]);
+            svg.append("g")
+            .style("font", "18px times")
+            .call(d3.axisLeft(y))
 
-      
-          // Add dots
-          if (this.selection != null) {
+            svg.append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 0 - margin.left)
+              .attr("x",0 - (height / 2))
+              .attr("dy", ".65em")
+              .style("text-anchor", "middle")
+              .style("font", "18px times")
+              .text("Trait Value");
+
+        
+            // Add dots
 
             var tooltip = d3.select("#trait_graph")
             .append("div")
@@ -230,9 +234,7 @@ export function init(dataset_id) {
                 .style("stroke", "none")
                 .style("opacity", 0.8)
             }
-            
-
-
+             
             svg.append('g')
             .selectAll("dot")
             .data([...Array(this.phenoIds.length).keys()])
@@ -245,11 +247,7 @@ export function init(dataset_id) {
               .on("mouseover", mouseover)
               .on("mousemove", mousemove)
               .on("mouseleave", mouseleave);
-
           }
-
-
-          
         }
     }
     const dataset = new Dataset;    
