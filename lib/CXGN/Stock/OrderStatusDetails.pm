4d186e9659c88f17759b71535e29632ee0842e22
titima15 <tt15@cornell.edu>
2022-05-24 20:14:55 +0000

    orderStatusDetails obj

---
+++ lib/CXGN/Stock/OrderStatusDetails.pm
@@ -0,0 +1,26 @@
+package CXGN::Stock::OrderStatusDetails;
+
+use Moose;
+
+extends 'CXGN::JSONProp';
+
+has 'item_status' => (is => 'rw', isa => 'HashRef');
+
+sub BUILD {
+    my $self = shift;
+    my $args = shift;
+
+    $self->prop_table('sp_orderprop');
+    $self->prop_namespace('SpOrderprop');
+    $self->prop_primary_key('sp_orderprop_id');
+    $self->prop_type('order_progress_json');
+    $self->cv_name('sp_order_property');
+    $self->allowed_fields( [ qw | item_status | ] );
+    $self->parent_table('sp_order');
+    $self->parent_primary_key('sp_order_id');
+
+    $self->load();
+}
+
+
+1;
