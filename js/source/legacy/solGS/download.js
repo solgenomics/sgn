909e80bc01b622e32023b2b321c9c3dc7c977b46
Isaak <iyt2@cornell.edu>
2022-06-27 14:40:55 +0200

    add model input data requests

--- js/source/legacy/solGS/download.js
+++ js/source/legacy/solGS/download.js
@@ -64,8 +64,76 @@ solGS.download = {
       '<p style="margin-top: 20px">' + downloadLinks + "</p>"
     );
   },
+
+  getModelInputDataFiles: function () {
+    var args = {
+      training_pop_id: jQuery("#training_pop_id").val(),
+      genotyping_protocol_id: jQuery("#genotyping_protocol_id").val(),
+    };
+
+    var trainingTraitsIds = solGS.getTrainingTraitsIds();
+    console.log("training traits ids: " + trainingTraitsIds);
+    if (trainingTraitsIds) {
+      args["training_traits_ids"] = trainingTraitsIds;
+    }
+    args = JSON.stringify(args);
+
+    jQuery.ajax({
+      type: "POST",
+      dataType: "json",
+      data: {
+        arguments: args,
+      },
+      url: "/solgs/download/model/input/data",
+      success: function (res) {
+        var errorMsg = "Error occured getting model input data download links.";
+
+        if (res) {
+          solGS.download.createModelInputDownloadLinks(res);
+        } else {
+          jQuery("#download_message").html(errorMsg);
+        }
+      },
+      error: function (res) {
+        jQuery("#download_message").html("errorMsg");
+      },
+    });
+  },
+
+  createModelInputDownloadLinks: function (res) {
+    var genoFile = res.model_geno_data_file;
+    var phenoFile = res.model_pheno_data_file;
+
+    console.log("geno file: " + genoFile);
+    console.log("pheno file: " + phenoFile);
+
+    var genoFileName = genoFile.split("/").pop();
+    var genoFileLink =
+      '<a href="' + genoFile + '" download=' + genoFileName + '">' + "Genotype data" + "</a>";
+
+    var phenoFileName = phenoFile.split("/").pop();
+    var phenoFileLink =
+      '<a href="' + phenoFile + '" download=' + phenoFileName + '">' + "Phenotype data" + "</a>";
+
+    var downloadLinks =
+      " <strong>Download " + "Model" + " </strong>: " + genoFileLink + " | " + phenoFileLink;
+
+    jQuery("#model_input_data_download").prepend(
+      '<p style="margin-top: 20px">' + downloadLinks + "</p>"
+    );
+  },
 };
 
 jQuery(document).ready(function () {
-  solGS.download.getTrainingPopRawDataFiles();
+  solGS.checkPageType().done(function (res) {
+    if (res.page_type.match(/training population/)) {
+      solGS.download.getTrainingPopRawDataFiles();
+    } else if (res.page_type.match(/training model/)) {
+      solGS.download.getModelInputDataFiles();
+    }
+  });
+
+  solGS.checkPageType().fail(function (res) {
+    solGS.alertMessage("Error occured checking for page type.");
+  });
 });
