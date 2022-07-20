723dac8e6daaf538d87881b454954e94730e1156
Isaak <iyt2@cornell.edu>
2022-07-20 11:54:07 +0200

    add protocol id to selection gebvs file args list

--- lib/SGN/Controller/solGS/Files.pm
+++ lib/SGN/Controller/solGS/Files.pm
@@ -589,7 +589,7 @@ sub rrblup_training_gebvs_file {
 
 
 sub rrblup_selection_gebvs_file {
-    my ($self, $c, $training_pop_id, $selection_pop_id, $trait_id, $protocol_id) = @_;
+    my ($self, $c, $training_pop_id, $selection_pop_id, $trait_id) = @_;
 
     $c->controller('solGS::Trait')->get_trait_details($c, $trait_id);
     my $trait_abbr  = $c->stash->{trait_abbr};
