d6e1e9e717a51cdd5f65bcb0824ce9c07dd8f0d9
Lukas Mueller <mueller@pop-os.localdomain>
2022-07-21 09:23:54 -0400

    tweak seedlot loading script.

--- bin/load_seedlots.pl
+++ bin/load_seedlots.pl
@@ -5,6 +5,8 @@
 
 use strict;
 
+use CXGN::DB::InsertDBH;
+use SGN::View::Cvterm;
 use Getopt::Long;
 use CXGN::Stock::Seedlot;
 use CXGN::Stock::Seedlot::Transaction;
@@ -26,10 +28,13 @@ my $dbh = CXGN::DB::InsertDBH->new( { dbhost=>$dbhost,
 						 RaiseError => 1}
 				    }
     );
+
 my $schema= Bio::Chado::Schema->connect(  sub { $dbh->get_actual_dbh() } ,  { on_connect_do => ['SET search_path TO  public;'] }
 					  );
 my $phenome_schema= CXGN::Phenome::Schema->connect( sub { $dbh->get_actual_dbh } , { on_connect_do => ['set search_path to public,phenome;'] }  );
 
+my $seedlot_cvterm_id = SGN::View::Cvterm->get_cvterm_row('seedlot', 'stock_type')->cvterm_id();
+print STDERR "SEEDLOT CVTERM ID = $seedlot_cvterm_id\n";
 
 open(my $F, "<", $file) || die "Can't open $file\n";
 
@@ -38,13 +43,34 @@ while (<$F>) {
     
     my ($seedlot_name, $accession_name, $operator_name, $amount, $weight, $description, $box_name, $quality, $source) = split /\t/;
 
-    my $seedlot = CXGN::Stock::Seedlot->new( { schema => $schema });
 
+    my $accession_row = $schema->resultset('Stock::Stock')->find( { uniquename => $accession_name } );
+
+    if (!$accession_row) {
+	print STDERR "accession $accession_name not found. SKIPPING!\n";
+	next();
+    }
+
+    my $seedlot_row = $schema->resultset('Stock::Stock')->find( { uniquename => $seedlot_name });
+
+    if ($seedlot_row) {
+	print STDERR "Seedlot $seedlot_name already exists...\n";
+	if ($seedlot_row->type_id() != $seedlot_cvterm_id) {
+	    print STDERR "Seedlot $seedlot_name exists in the database, but has wrong type_id (".$seedlot_row->type_id().")\n";
+	    next();
+	}
+    }
+	
+    my $seedlot = CXGN::Stock::Seedlot->new(schema => $schema, seedlot_id => $seedlot_row->stock_id() );
+
+    my $accession_id = $accession_row->stock_id();
     $seedlot->uniquename($seedlot_name);
-    $seedlot->accessions( [$accession_name ]);
+    $seedlot->accession_stock_id();
     $seedlot->box_name($seedlot_name);
     $seedlot->description($description);
-    $seedlot->operator($operator_name);
+    $seedlot->location_code("Westchester");
+    $seedlot->breeding_program_id(325);
+    #$seedlot->operator($operator_name);
     $seedlot->store();
 
     
