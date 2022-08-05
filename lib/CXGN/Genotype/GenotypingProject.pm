d808d7b0c86924c4d5fdbe9c2570935b521a640a
titima15 <tt15@cornell.edu>
2022-08-05 21:26:45 +0000

    set up search

---
+++ lib/CXGN/Genotype/GenotypingProject.pm
@@ -0,0 +1,71 @@
+
+=head1 NAME
+
+CXGN::Genotype::GenotypingProject - an object representing a genotyping project in the database
+
+=head1 DESCRIPTION
+
+    my $genotyping_project = CXGN::Genotype::GenotypingProject->new( { schema => $schema, trial_id => 37347 });
+
+
+=head1 AUTHORS
+
+    Titima Tantikanjana
+
+=head1 METHODS
+
+=cut
+package CXGN::Genotype::GenotypingProject;
+
+use Moose;
+use SGN::Model::Cvterm;
+use Data::Dumper;
+use JSON;
+use CXGN::Trial::Search;
+
+has 'bcs_schema' => (
+    isa => 'Bio::Chado::Schema',
+    is => 'rw',
+    required => 1,
+);
+
+has 'project_id' => (
+    isa => 'Int',
+    is => 'rw',
+    required => 1,
+);
+
+
+sub BUILD {
+
+    my $self = shift;
+    my $schema = $self->bcs_schema();
+    my $genotyping_project_id = $self->project_id();
+
+    my $genotyping_project_relationship_cvterm = SGN::Model::Cvterm->get_cvterm_row($schema, 'genotyping_project_and_plate_relationship', 'project_relationship');
+    my $relationships_rs = $self->get_schema()->resultset("Project::ProjectRelationship")->search ({
+        object_project_id => $genotyping_project_id,
+        type_id => $genotyping_project_relationship_cvterm->cvterm_id()
+    });
+
+    my @plate_list;
+    if ($relationships_rs) {
+        while (my $each_relationship = $relationships_rs->next()) {
+    	    push @plate_list, $each_relationship->subject_project_id();
+        }
+    }
+
+    $self->genotyping_plate_list(\@plate_list);
+
+}
+
+
+sub get_plate_info {
+    my $self = shift;
+    my $schema = $self->bcs_schema();
+    my $plate_list = $self->genotyping_plate_list();
+
+}
+
+
+1;
