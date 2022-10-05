c40d991f67996784e3307d4b952aef508c86ea70
Lukas Mueller <mueller@pop-os.localdomain>
2022-10-05 14:37:00 -0400

    add to dos.

--- lib/SGN/Controller/SimSearch.pm
+++ lib/SGN/Controller/SimSearch.pm
@@ -23,16 +23,27 @@ sub process_file : Path('/tools/simsearch/processfile') {
     $c->stash->{template} = '/tools/simsearch/process_file.mas';
 
 
+    # -i required input file -r reference file (with -i only, input is also used as reference)
+    # need to add a pull down with current genotypes for each protocol
+    
     my $cmd = "../gtsimsrch/src/simsearch -i $filename -o $filename.out";
 
     print STDERR "running command $cmd...\n";
     system($cmd);
 
+
+    system("perl ../gtsimsrch/src/agmr_cluster.pl < $filename.out > $filename.out.clusters");
+
     my $results;
-    open(my $F , "<", $filename.".out") || die "Can't open file $filename.out";
+    open(my $F , "<", $filename.".out.clusters") || die "Can't open file $filename.out";
     while(<$F>) {
 	$results .= $_;
     }
+
+    # plot the agmr score distribution histogram using the 6th column in $filename.out
+    #
+    # (use gnuplot or R)
+    
     
     $c->body($results);
     
