02d8f9c2d4deb7c415d77b30ae2ebbc1a407d60d
Lukas Mueller <lam87@cornell.edu>
2022-11-28 13:14:41 -0500

    move simsearch functionality to restful interface.

--- lib/SGN/Controller/AJAX/SimSearch.pm
+++ lib/SGN/Controller/AJAX/SimSearch.pm
@@ -7,53 +7,14 @@ use File::Basename;
 
 BEGIN { extends 'Catalyst::Controller::REST'; }
 
-sub upload_file : Path('/ajax/tools/simsearch/upload_file') {
-    my $self = shift;
-    my $c = shift;
 
-    my $tempdir = $c->tempfiles_subdir("simsearch");
-    my $tempfile = $c->config->{basepath}."/".$c->tempfile( TEMPLATE => 'simsearch/simsearch-XXXXXX', UNLINK => 0 );
+__PACKAGE__->config(
+    default   => 'application/json',
+    stash_key => 'rest',
+    map       => { 'application/json' => 'JSON' },
+    );
 
-    
-    my $upload = $c->request->upload('upload_vcf_file');
 
-    my $filename = $upload->filename();
-    
-    print STDERR "UPLOAD FILENAME: $filename\n";
-
-    my $tempname = $upload->tempname();
-    print STDERR "TEMPNAME: $tempname\n";
-    
-    $c->stash->{filename} = $tempfile;
-    $c->stash->{template} = '/tools/simsearch/upload_file.mas';
-
-    my $simsearch_datadir = $c->config->{simsearch_datadir};
-
-    print STDERR "SIMSEARCH DATADIR = $simsearch_datadir\n";
-
-    $upload->copy_to($tempfile);
-    
-    my @files = glob "$simsearch_datadir/*";
-
-    print STDERR "FILES: ".join(",",@files)."\n";
-
-    
-    
-    my $pulldown = "<select id=\"reference_file\" >\n";
-    foreach my $f (@files) {
-	$pulldown .= "<option>".basename($f)."</option>";
-    }
-    $pulldown .="</select>";
-
-    # -i required input file -r reference file (with -i only, input is also used as reference)
-    # need to add a pull down with current genotypes for each protocol
-
-    print STDERR "COPIED TO $tempfile\n";
-    $c->stash->{reference_files_menu} = $pulldown;
-    $c->stash->{filename} = basename($tempfile);
-
-    $c->stash->{rest} = { filename => $tempfile };
-}
 
 sub process_file :Path('/ajax/tools/simsearch/process_file') :Args(0) {
     my $self = shift;
@@ -88,12 +49,14 @@ sub process_file :Path('/ajax/tools/simsearch/process_file') :Args(0) {
     my @line;
     my $html = "<table cellspacing=\"20\" cellpadding=\"20\" border=\"1\">";
     my $group =1;
+    
     while(<$F>) {
+	chomp;
 	if (/^#/) { next; }
 	@line = split /\s+/;
 	$html .= '<tr><td>'.$group.'</td><td>'. join('<br />', @line[4..@line-1])."</td></tr>\n";
 	$group++;
-	push @data, \@line;
+	push @data, [ $group, $line[3], join("<br />", @line[4..@line-1]) ];
     }
     $html.="</table>\n";
     close($F);
@@ -102,8 +65,7 @@ sub process_file :Path('/ajax/tools/simsearch/process_file') :Args(0) {
     #
     # (use gnuplot or R)
 
-    $c->stash->{results} = $html;
-    $c->stash->{template} = '/tools/simsearch/results.mas';
+#    $c->stash->{template} = '/tools/simsearch/results.mas';
 
     $c->stash->{rest} = { data => \@data };
     
