f1ae66e3689c6493725090f796d994fcd928e35e
bellerbrock <bje24@cornell.edu>
2022-12-22 13:54:33 -0500

    add xaxis label to boxplot test

--- js/source/legacy/brapi/BoxPlotter.js
+++ js/source/legacy/brapi/BoxPlotter.js
@@ -292,6 +292,13 @@ var BoxPlotter = function () {
         svg.select("g.axis").attr("transform", "translate(0," + (plt.vm - 5) + ")").call(plt.axis);
         svg.attr("width", svgbbox.width + svgbbox.x + 20);
         svg.attr("height", svgbbox.height + svgbbox.y + plt.vm * 2);
+
+        svg.append("text")
+            .attr("transform", "translate(0," + (plt.vm - 5) + ")")
+            .style("text-anchor", "middle")
+            .attr("fill", "black")
+            .text("Title");
+
       });
     }
   }, {
