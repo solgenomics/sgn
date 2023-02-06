51fb6e1d54c80adc1b9f493e93db108c5a7cc772
Amber Lockrow <awl67@cornell.edu>
2023-02-06 15:15:15 -0500

    Edit Login subs to get login info from cookie or logged_in page

--- lib/SGN/Controller/AJAX/Login.pm
+++ lib/SGN/Controller/AJAX/Login.pm
@@ -4,6 +4,7 @@ package SGN::Controller::AJAX::Login;
 use Moose;
 use CXGN::People::Login;
 use CXGN::People::Person;
+use CXGN::Login;
 
 BEGIN { extends 'Catalyst::Controller::REST' }
 
@@ -24,15 +25,15 @@ sub is_logged_in :Path('/user/logged_in') Args(0) {
     my $login = CXGN::Login->new($dbh);
     if (my ($person_id, $user_type) = $login->has_session()) {
         my $user_id = CXGN::People::Person->new($dbh,$person_id);
-    	$c->stash->{rest} = $login -> get_login_info;
-    	    # user_id => $user_id,
-    	    # username => $user->get_object->get_username(),
-    	    # first_name => $user->get_object->get_first_name(),
-    	    # last_name => $user->get_object->get_last_name(),
-    	# };
+        my $user = $c->user();
+    	$c->stash->{rest} = {first_name => $user->get_object->get_first_name(),
+    	    last_name => $user->get_object->get_last_name(),
+    	    username => $user->get_object->get_username(),
+            user_id => $person_id,
+    	};
     	return;
+    };
     $c->stash->{rest} = { user_id => 0 };
-    }
 }
 
 sub login_with_cookie :Path('/user/cookie_login') Args(1) {
@@ -42,17 +43,15 @@ sub login_with_cookie :Path('/user/cookie_login') Args(1) {
     $c->response->headers->header( "Access-Control-Allow-Methods" => "POST, GET, PUT, DELETE" );
     $c->response->headers->header( 'Access-Control-Allow-Headers' => 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range,Authorization');
     my $cookie = shift;
-    if (my $user = $c->user()) {
-        my $dbh = $c->dbic_schema("CXGN::People::Schema")->storage->dbh;
-        my $sp_person_id = CXGN::People::Login->get_login_by_token($dbh, $cookie);
-        my $user_id = CXGN::People::Person->new($dbh, $sp_person_id);
-        $c->stash->{rest} = {first_name => $user->get_object->get_first_name(),
-                            last_name => $user->get_object->get_last_name(),
-                            username => $user->get_object->get_username(),
-                            user_id => $sp_person_id,
-        };
-        return;
-    }
+    my $dbh = $c->dbc->dbh;
+    my @user_info = CXGN::Login->new($dbh)->query_from_cookie($cookie);
+    my $user_id = $user_info[0];
+    my $p = CXGN::People::Person->new($dbh, $user_id);
+    $c->stash->{rest} = {first_name => $p->get_first_name,
+	    last_name => $p->get_last_name,
+        username => $p->get_username,
+        user_id => $user_id,
+    };
 }
 
 sub get_roles :Path('/user/get_roles') Args(0) {
