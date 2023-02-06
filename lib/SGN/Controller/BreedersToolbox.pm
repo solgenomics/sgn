c3ef7de1500f1530961a22c143b973768dd25023
titima15 <tt15@cornell.edu>
2023-02-06 17:50:38 -0500

    manage inventories page

--- lib/SGN/Controller/BreedersToolbox.pm
+++ lib/SGN/Controller/BreedersToolbox.pm
@@ -879,4 +879,17 @@ sub manage_drone_imagery : Path("/breeders/drone_imagery") Args(0) {
     $c->stash->{template} = '/breeders_toolbox/manage_drone_imagery.mas';
 }
 
+sub manage_inventories : Path("/breeders/inventories") Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    if (!$c->user()) {
+        $c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+        return;
+    }
+
+    $c->stash->{template} = '/breeders_toolbox/manage_inventories.mas';
+}
+
+
 1;
