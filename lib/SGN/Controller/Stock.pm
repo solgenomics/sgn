4c47196ed4a76df5ef47200f130042dc20d3c51f
nmenda <nm249@cornell.edu>
2023-03-02 11:07:23 -0500

    reset search form params

--- lib/SGN/Controller/Stock.pm
+++ lib/SGN/Controller/Stock.pm
@@ -149,10 +149,10 @@ sub view_stock : Chained('get_stock') PathPart('view') Args(0) {
     my ( $self, $c, $action) = @_;
 
      if (!$c->user()) {
-	
-	my $url = '/' . $c->req->path;	
+
+	my $url = '/' . $c->req->path;
 	$c->res->redirect("/user/login?goto_url=$url");
-	
+
     } else {
 	$time = time();
 
@@ -305,7 +305,7 @@ Path Params:
     name = stock unique name
 
 Search for stock(s) matching the organism query and the stock unique name.
-If 1 match is found, display the stock detail page.  Display an error for 
+If 1 match is found, display the stock detail page.  Display an error for
 0 matches and a list of matches when multiple stocks are found.
 
 =cut
@@ -316,14 +316,14 @@ sub view_by_organism_name : Path('/stock/view_by_organism') Args(2) {
 }
 
 
-=head2 view_by_name 
+=head2 view_by_name
 
 Public Path: /stock/view_by_name/$name
 Path Params:
     name = stock unique name
 
 Search for stock(s) matching the stock unique name.
-If 1 match is found, display the stock detail page.  Display an error for 
+If 1 match is found, display the stock detail page.  Display an error for
 0 matches and a list of matches when multiple stocks are found.
 
 =cut
@@ -385,7 +385,7 @@ sub download_genotypes : Chained('get_stock') PathPart('genotypes') Args(0) {
 	return;
     }
 
-    
+
     my $stock_row = $c->stash->{stock_row};
     my $stock_id = $stock_row->stock_id;
     my $stock_name = $stock_row->uniquename;
@@ -396,10 +396,10 @@ sub download_genotypes : Chained('get_stock') PathPart('genotypes') Args(0) {
 	my $referer = $c->req->referer;
 	my $message = "<p>Genotype data download for the stock is missing an associated genotype id. <br/>"
 	    .  "<a href=\"$referer\">[ Go back ]</a></p>";
-	   
+
 	$c->stash->{message} = $message;
 	$c->stash->{template} = "/generic_message.mas";
-	
+
     } else {
 	my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
 	my $people_schema = $c->dbic_schema("CXGN::People::Schema");
@@ -449,7 +449,7 @@ sub download_genotypes : Chained('get_stock') PathPart('genotypes') Args(0) {
 	    $c->res->body($file_handle);
 	}
     }
-    
+
 }
 
 sub chr_sort {
@@ -501,7 +501,7 @@ sub get_stock : Chained('/')  PathPart('stock')  CaptureArgs(1) {
 sub search_stock : Private {
     my ( $self, $c, $organism_query, $stock_query ) = @_;
     my $rs = $self->schema->resultset('Stock::Stock');
-    
+
     my $matches;
     my $count = 0;
 
@@ -525,9 +525,9 @@ sub search_stock : Private {
     # Search by name
     elsif ( defined($stock_query) ) {
         $matches = $rs->search({
-                'UPPER(uniquename)' => uc($stock_query), 
+                'UPPER(uniquename)' => uc($stock_query),
                 is_obsolete => 'false'
-            }, 
+            },
             {join => 'organism'}
         );
         $count = $matches->count;
@@ -539,7 +539,7 @@ sub search_stock : Private {
         $c->stash->{template} = "generic_message.mas";
         $c->stash->{message} = "<strong>No Matching Stock Found</strong> ($stock_query $organism_query)<br />You can view and search for stocks from the <a href='/search/stocks'>Stock Search Page</a>";
     }
-    
+
     # MULTIPLE MATCHES FOUND
     elsif ( $count > 1 ) {
         my $list = "<ul>";
