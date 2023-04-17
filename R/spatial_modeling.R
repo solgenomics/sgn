69691e376eae143711ddaea4def05351d5d93392
Christine Nyaga <cmn92@cornell.edu>
2023-04-17 14:08:54 -0400

    adding the change on spatial importance

--- R/spatial_modeling.R
+++ R/spatial_modeling.R
@@ -6,7 +6,7 @@
 # 1. Load the software needed.
 # 2. Declare user-supplied variables.
 # 3. Process the phenotypic data.
-# 4. Fit the mixed models in sommer.
+# 4. Fit the two models with and without 2D Spline model in sommer
 # 5. Format the information needed for output.
 
 
@@ -15,7 +15,6 @@
 ################################################################################
 
 library(sommer)
-
 
 ################################################################################
 # 2. Declare user-supplied variables.
@@ -28,7 +27,6 @@ if (length(args) < 2) {
 phenotypeFile <- args[1]
 traits <- args[2]
 
-
 ################################################################################
 # 3. Process the phenotypic data.
 ################################################################################
@@ -39,8 +37,9 @@ userPheno <- read.delim(phenotypeFile, header = TRUE, sep = "\t", fill = TRUE)
 # The user should be able to select their response variables from a drop-down menu
 #    of the column names of the userPheno object. Then, those strings should be passed
 #    to this vector, 'userResponse'.
-
 userResponse <- unlist(strsplit(traits, split = ",", fixed = T))
+userResponse <- userResponse[!userResponse == "notes"] # x[ !x == 'A'] # remove notes from userResponse
+replicate <- "replicate"
 userID <- "germplasmName"
 row <- "rowNumber"
 col <- "colNumber"
@@ -48,30 +47,53 @@ userPheno$R <- as.factor(userPheno$rowNumber)
 userPheno$C <- as.factor(userPheno$colNumber)
 
 ################################################################################
-# 4. Fit the 2D Spline model in sommer
+# 4. Fit the two models with and without 2D Spline model in sommer
 ################################################################################
 # Make a list to save the models.
+userModelsWith <- list()
+userModelsWithout <- list()
 
-userModels <- list()
+# Make lists to save the outputs for AIC, blues and fitted values
+AIC_output <- data.frame()
 output <- data.frame()
+fitted_output <- data.frame()
 
+# Loop through the response variables running the model with and without spatial variation
 for (i in 2:length(userResponse)) {
+  write(paste("userResponse:", userResponse[i]), stderr())
+  # we want response variables with nas < 90% and more than one unique value
   nas <- colMeans(is.na(userPheno[userResponse[i]])) * 100
-  standarddev <- sapply(userPheno[userResponse[i]], sd)
+  standarddev <- sapply(userPheno[userResponse[i]], sd) # not used
   minimum <- min(userPheno[userResponse[i]], na.rm = TRUE)
   maximum <- max(userPheno[userResponse[i]], na.rm = TRUE)
-  write(paste("MINIMUM:", minimum), stderr())
+
+  # so if nas < 90% and there is more than one unique value, run the model
   if (nas < 90 && minimum != maximum) {
-    # if (standarddev!=0) {
+    write(paste("userResponse again:", userResponse[i]), stderr())
     write(paste("Percentage of Nas:", nas), stderr())
-    write(paste("userResponse:", userResponse[i]), stderr())
     write(paste("userResponsedata:", userPheno[userResponse[i]]), stderr())
+
+    ## Fit the model without spatial variation ##
+    modArg <- paste(userResponse[i], " ~ ", userID, " + ", replicate, sep = "")
+    write(paste("modArg:", modArg), stderr())
+    mod <- mmer(
+      as.formula(modArg),
+      data = userPheno,
+      verbose = FALSE
+    )
+
+    write(paste("modArg:", modArg), stderr())
+    userModelsWithout[[i]] <- mod
+
+    ## Fit the model with spatial variation ##
+
+    # create a formula for the fixed effects
     fixedArg <- paste(userResponse[i], " ~ ", "1 +", userID, sep = "")
     write(paste("fixedArg:", fixedArg), stderr())
-
+    # create a formula for the random effects
     randArg <- paste("~vsr(R)+vsr(C)+ spl2Da(", col, ",", row, ")", sep = "")
     write(paste("randArg:", randArg), stderr())
-
+    # the model
     m2.sommer <- mmer(
       fixed = as.formula(fixedArg),
       random = as.formula(randArg),
@@ -79,27 +101,60 @@ for (i in 2:length(userResponse)) {
       data = userPheno, verbose = FALSE
     )
 
-    # write(paste('model:', m2.sommer), stderr())
+    userModelsWith[[i]] <- m2.sommer
 
+    ## Obtaining the results from the models: AIC, blues and fitted values ##
+    # AIC
+    AIC.without <- summary(mod)$logo$AIC
+    AIC.with <- summary(m2.sommer)$logo$AIC
+    # check if the model with spatial variation is better than the model without spatial variation by difference of 5 in AIC
+    if (AIC.with < AIC.without - 5) {
+      AIC_output <- rbind(AIC_output, data.frame(Trait = userResponse[i], AIC.without, AIC.with, Spatial.Importance = "Yes"))
+    } else if (AIC.with < AIC.without) {
+      AIC_output <- rbind(AIC_output, data.frame(Trait = userResponse[i], AIC.without, AIC.with, Spatial.Importance = "Maybe"))
+    } else {
+      AIC_output <- rbind(AIC_output, data.frame(Trait = userResponse[i], AIC.without, AIC.with, Spatial.Importance = "No"))
+    }
 
-    userModels[[i]] <- m2.sommer
+    # blues
     blue <- summary(m2.sommer)$betas
-    # output[[i]] <- blue
     output <- rbind(output, blue)
 
-    # write(paste("blues:", blue), stderr())
+    # fitted values
+    fittedvals <- fitted(m2.sommer)
+    colname_fitted <- paste0(userResponse[i], ".fitted")
+    fitted_tab <- fittedvals$dataWithFitted[, c(userID, userResponse[i], colname_fitted)] # table with userID, trait values, and fitted values
+    colnames(fitted_tab) <- c("Name", "Phenotype_values", "Fitted_values")
+    fitted_tab$Phenotype_values <- round(fitted_tab$Phenotype_values, 2)
+    fitted_tab$Fitted_values <- round(fitted_tab$Fitted_values, 2)
+    # place userResponse[i] in first column
+    fitted_tab <- data.frame(Trait = userResponse[i], fitted_tab)
+    write(paste("colnames:", colnames(fitted_tab)), stderr())
+    fitted_output <- rbind(fitted_output, fitted_tab)
   }
 }
-print(colnames(output))
-colnames(output) <- c("ID", "Trait", "Genotype", "Estimate", "Std.Error")
-write(paste("blues:", output), stderr())
-BLUE <- as.data.frame(output)
-write(paste("BLUE:", BLUE), stderr())
 
-# adj = coef(m2.sommer)$Trait
-# outfile_blue <- paste(phenotypeFile, ".BLUEs", sep = "")
-outfile_blue <- paste(phenotypeFile, ".out", sep = "")
+################################################################################
+# 5. Write the results to files.
+################################################################################
+### deal with AIC output
+outfile_AIC <- paste(phenotypeFile, ".AIC", sep = "")
+write.table(AIC_output, outfile_AIC)
+
+## dealing with blues output
+print(colnames(output))
+colnames(output) <- c("ID", "Trait", "Name", "Estimate", "Std.Error")
+BLUE <- as.data.frame(output)
+outfile_blue <- paste(phenotypeFile, ".blues", sep = "")
 write.table(BLUE, outfile_blue)
+
+### deal with fitted values output
+write(paste("colnames fitted:", colnames(fitted_output)), stderr())
+write(paste("fitted_output:", head(fitted_output)), stderr())
+outfile_fitted <- paste(phenotypeFile, ".fitted", sep = "")
+write.table(fitted_output, outfile_fitted)
+
+
 
 ################################################################################
 # 5. Format the information needed for output.
@@ -123,3 +178,35 @@ write.table(BLUE, outfile_blue)
 #   #   outfile_blue = paste(phenotypeFile, ".BLUEs", sep="");
 #   #   write.table(BLUE, outfile_blue)
 # }
+
+
+# unique_ID <- unique(userPheno$germplasmName) # create vector of unique userID
+#     unique_row <- unique(userPheno$rowNumber) # create vector of unique row
+#     unique_col <- unique(userPheno$colNumber) # create vector of unique col
+#     df <- list(unique_ID, unique_row, unique_col) # create list of unique userID, row, and col
+#     # write(paste("df:", df), stderr())
+
+#     newdata <- expand.grid(... = df) # create data frame of all combinations of userID, row, and col
+#     names(newdata) <- c("userID", "row", "col") # name columns of new data frame
+
+#     # write(paste("newdata:", newdata), stderr())
+#     R_matrix <- diag(length(unique(userPheno$R))) # create diagonal matrix of length unique R
+#     C_matrix <- diag(length(unique(userPheno$C))) # create diagonal matrix of length unique C
+#     Z <- kronecker(R_matrix, C_matrix) # kronecker product of R and C
+
+#     num_combinations <- nrow(newdata) # number of combinations of userID, row, and col
+#     # write(paste("num_combinations:", num_combinations), stderr())
+#     # Z_list <- list(Z)
+#     Z_list <- vector("list", num_combinations) # create list of length num_combinations
+#     for (i in 1:num_combinations) {
+#       Z_list[[i]] <- Z # fill list with Z
+#     }
+#     Z <- do.call(rbind, Z_list) # combine list into matrix
+
+#     newdata$spl2Da <- with(newdata, spl2Da(col, row)) # add splines to new data frame
+#     newdata[, colnames(Z)] <- Z # add random effects matrix to new data frame
+#     # write(paste("newdata:", newdata), stderr())
+
+#     # adj_means <- predict(m2.sommer, newdata, type = "response")
+#     adj_means <- predict.mmer(m2.sommer, classify = userID)
+#     write(paste("adj_means:", adj_means), stderr())
--- lib/SGN/Controller/AJAX/SpatialModel.pm
+++ lib/SGN/Controller/AJAX/SpatialModel.pm
@@ -29,7 +29,7 @@ __PACKAGE__->config(
     map       => { 'application/json' => 'JSON' },
     );
 
-
+my $spatial_check;
 sub shared_phenotypes: Path('/ajax/spatial_model/shared_phenotypes') Args(0) {
     my $self = shift;
     my $c = shift;
@@ -76,35 +76,30 @@ sub extract_trait_data :Path('/ajax/spatial_model/getdata') Args(0) {
 
     $c->stash->{rest} = { data => \@data, trait => $trait};
 }
+#spatial_checking method
+sub spatial_checking: Path('/ajax/spatial_model/spatial_checking') Args(1) { # Args(1) is the trial id
+    my $self = shift; # $self is the controller object
+    my $c = shift; # $c is the Catalyst context object
+    my $trial_id = shift; # $trial_id is the trial id
 
-sub generate_results: Path('/ajax/spatial_model/generate_results') Args(1) {
-    my $self = shift;
-    my $c = shift;
-    my $trial_id = shift;
+    my $people_schema = $c->dbic_schema("CXGN::People::Schema"); # get the people schema
+    my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado"); # get the chado schema
 
-    print STDERR "TRIAL_ID: $trial_id\n";
+    my $file = $c->req->param("file"); # get the file name
+    my $si_traits = $c->req->param("si_traits"); # get the spatial interaction traits
 
-    $c->tempfiles_subdir("spatial_model_files");
-    my $spatial_model_tmp_output = $c->config->{cluster_shared_tempdir}."/spatial_model_files";
+    $c->tempfiles_subdir("spatial_model_files"); # set the tempfiles subdir to spatial model files
+    my $spatial_model_tmp_output = $c->config->{cluster_shared_tempdir}."/spatial_model_files"; # get the spatial model temp output directory
     print STDERR "spatial_model_tmp_output: $spatial_model_tmp_output\n";
-    mkdir $spatial_model_tmp_output if ! -d $spatial_model_tmp_output;
+    mkdir $spatial_model_tmp_output if ! -d $spatial_model_tmp_output; # create the spatial model temp output directory if it doesn't exist
     my ($tmp_fh, $tempfile) = tempfile(
       "spatial_model_download_XXXXX",
       DIR=> $spatial_model_tmp_output,
     );
     print STDERR "tempfile: $tempfile\n";
 
-    #my $temppath = $c->config->{basepath}."/".$tempfile;
-    #print STDERR "temppath: $temppath\n";
-
-    my $pheno_filepath = $tempfile . "_phenotype.txt";
-
+    my $pheno_filepath = $tempfile . "_phenotype.txt"; # create the phenotype file path
     print STDERR "pheno_filepath: $pheno_filepath\n";
-
-    my $people_schema = $c->dbic_schema("CXGN::People::Schema");
-    my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
-
-
 
     my $temppath =  $tempfile;
 
@@ -159,7 +154,7 @@ sub generate_results: Path('/ajax/spatial_model/generate_results') Args(1) {
 
     $cmd->run_cluster(
 	"Rscript ",
-	$c->config->{basepath} . "/R/spatial_modeling.R",
+	$c->config->{basepath} . "/R/spatial_checking.R",
 	$pheno_filepath.".clean",
 	"'".$si_traits."'",
 
@@ -169,9 +164,139 @@ sub generate_results: Path('/ajax/spatial_model/generate_results') Args(1) {
 	sleep(1);
     }
 
+
+     #getting the results
     my @data;
 
-    open(my $F, "<", $pheno_filepath.".clean.out") || die "Can't open result file $pheno_filepath".".clean.out";
+    open($spatial_check, "<", $pheno_filepath.".clean.spatialchecking") || die "Can't open result file $pheno_filepath".".clean.spatialchecking";
+    my $header = <$spatial_check>;
+    my @h = split(/\s+/, $header);
+    #my @h = split(',', $header);
+    my @spl;
+    foreach my $item (@h) {
+    push  @spl, {title => $item};
+  }
+    print STDERR "Header: ".Dumper(\@spl);
+    while (<$spatial_check>) {
+	chomp;
+	my @fields = split /\s+/;
+	foreach my $f (@fields) { $f =~ s/\"//g; }
+	push @data, \@fields;
+    }
+    # print STDERR "FORMATTED DATA: ".Dumper(\@data);
+
+    my $basename = basename($pheno_filepath.".clean.spatialchecking");
+
+    copy($pheno_filepath.".clean.spatialchecking", $c->config->{basepath}."/static/documents/tempfiles/spatial_model_files/".$basename);
+
+    my $download_url = '/documents/tempfiles/spatial_model_files/'.$basename;
+    my $download_link = "<a href=\"$download_url\">Download Results</a>";
+    $c->stash->{rest} = { 
+            data => \@data,
+            headers => \@spl,
+            basenamesp => $basename,
+    };
+
+}
+
+sub generate_results: Path('/ajax/spatial_model/generate_results') Args(1) {
+    my $self = shift;
+    my $c = shift;
+    my $trial_id = shift;
+    my $basenamesp = $c -> req -> param("basenamesp");
+
+    print STDERR "TRIAL_ID: $trial_id\n";
+
+    $c->tempfiles_subdir("spatial_model_files"); # set the tempfiles subdir to spatial model files
+    my $spatial_model_tmp_output = $c->config->{cluster_shared_tempdir}."/spatial_model_files"; # get the spatial model temp output directory
+    print STDERR "spatial_model_tmp_output: $spatial_model_tmp_output\n";
+    mkdir $spatial_model_tmp_output if ! -d $spatial_model_tmp_output; # create the spatial model temp output directory if it doesn't exist
+    my ($tmp_fh, $tempfile) = tempfile(
+      "spatial_model_download_XXXXX",
+      DIR=> $spatial_model_tmp_output,
+    );
+    print STDERR "tempfile: $tempfile\n";
+
+    #my $temppath = $c->config->{basepath}."/".$tempfile;
+    #print STDERR "temppath: $temppath\n";
+
+    my $pheno_filepath = $tempfile . "_phenotype.txt"; # create the phenotype file path
+    
+
+    print STDERR "pheno_filepath: $pheno_filepath\n";
+
+    my $people_schema = $c->dbic_schema("CXGN::People::Schema");
+    my $schema = $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
+
+    
+    my $temppath =  $tempfile;
+    my $ds = CXGN::Dataset::File->new(people_schema => $people_schema, schema => $schema,  file_name => $temppath, quotes=>0);
+    $ds -> trials([$trial_id]);
+    $ds -> retrieve_phenotypes($pheno_filepath);
+    open(my $PF, "<", $pheno_filepath) || die "Can't open pheno file $pheno_filepath";
+    open(my $CLEAN, ">", $pheno_filepath.".clean") || die "Can't open pheno_filepath clean for writing";
+
+    my $header = <$PF>;
+    chomp($header);
+
+    my @fields = split /\t/, $header;
+
+    my @file_traits = @fields[ 39 .. @fields-1 ];
+    my @other_headers = @fields[ 0 .. 38 ];
+
+
+
+    print STDERR "FIELDS: ".Dumper(\@file_traits);
+
+    foreach my $t (@file_traits) {
+	$t = make_R_trait_name($t);
+    }
+
+    my $si_traits = join(",", @file_traits);
+
+    print STDERR "FILE TRAITS: ".Dumper(\@file_traits);
+
+    my @new_header = (@other_headers, @file_traits);
+    print $CLEAN join("\t", @new_header)."\n";
+
+    my $last_index = scalar(@new_header)-1;
+
+
+    while(<$PF>) {
+	print $CLEAN $_;
+    }
+
+    close($PF);
+    close($CLEAN);
+
+    my $cmd = CXGN::Tools::Run->new({
+	backend => $c->config->{backend},
+	submit_host=>$c->config->{cluster_host},
+	temp_base => $c->config->{cluster_shared_tempdir} . "/spatial_model_files",
+	queue => $c->config->{'web_cluster_queue'},
+	do_cleanup => 0,
+	# don't block and wait if the cluster looks full
+	max_cluster_jobs => 1_000_000_000,
+    });
+
+
+    $cmd->run_cluster(
+	"Rscript ",
+	$c->config->{basepath} . "/R/spatial_modeling.R",
+	$pheno_filepath.".clean",
+	"'".$si_traits."'",
+    $spatial_check,
+
+	);
+
+    while ($cmd->alive) {
+	sleep(1);
+    }
+
+    #getting the blue results
+    my @data;
+
+    open(my $F, "<", $pheno_filepath.".clean.blues") || die "Can't open result file $pheno_filepath".".clean.blues";
     my $header = <$F>;
     my @h = split(/\s+/, $header);
     #my @h = split(',', $header);
@@ -187,19 +312,83 @@ sub generate_results: Path('/ajax/spatial_model/generate_results') Args(1) {
 	push @data, \@fields;
     }
 
-    print STDERR "FORMATTED DATA: ".Dumper(\@data);
+    # print STDERR "FORMATTED DATA: ".Dumper(\@data);
 
-    my $basename = basename($pheno_filepath.".clean.out");
+    my $basename = basename($pheno_filepath.".clean.blues");
 
-    copy($pheno_filepath.".clean.out", $c->config->{basepath}."/static/documents/tempfiles/spatial_model_files/".$basename);
+    copy($pheno_filepath.".clean.blues", $c->config->{basepath}."/static/documents/tempfiles/spatial_model_files/".$basename);
 
     my $download_url = '/documents/tempfiles/spatial_model_files/'.$basename;
     my $download_link = "<a href=\"$download_url\">Download Results</a>";
+
+
+
+
+    ##getting with fitted results
+    my @data_fitted;
+
+    open(my $F_fitted, "<", $pheno_filepath.".clean.fitted") || die "Can't open result file $pheno_filepath".".clean.fitted";
+    my $header_fitted = <$F_fitted>;
+    my @h_fitted = split(/\s+/, $header_fitted);
+    #my @h = split(',', $header);
+    my @spl_fitted;
+    foreach my $item_fitted (@h_fitted) {
+    push  @spl_fitted, {title => $item_fitted};
+  }
+    print STDERR "Header: ".Dumper(\@spl_fitted);
+    while (<$F_fitted>) {
+	chomp;
+	my @fields_fitted = split /\s+/;
+	foreach my $f_fitted (@fields_fitted) { $f_fitted =~ s/\"//g; }
+	push @data_fitted, \@fields_fitted;
+    }
+
+    # print STDERR "FORMATTED DATA: ".Dumper(\@data);
+
+    my $basename = basename($pheno_filepath.".clean.fitted");
+
+    copy($pheno_filepath.".clean.fitted", $c->config->{basepath}."/static/documents/tempfiles/spatial_model_files/".$basename);
+
+    my $download_url_fitted = '/documents/tempfiles/spatial_model_files/'.$basename;
+    my $download_link_fitted = "<a href=\"$download_url\">Download Fitted values</a>";
+
+    #getting the AIC results
+    my @data_AIC;
+
+    open(my $F_AIC, "<", $pheno_filepath.".clean.AIC") || die "Can't open result file $pheno_filepath".".clean.AIC";
+    my $header_AIC = <$F_AIC>;
+    my @h_AIC = split(/\s+/, $header_AIC);
+    #my @h = split(',', $header);
+    my @spl_AIC;
+    foreach my $item_AIC (@h_AIC) {
+    push  @spl_AIC, {title => $item_AIC};
+  }
+    print STDERR "Header: ".Dumper(\@spl_AIC);
+    while (<$F_AIC>) {
+	chomp;
+	my @fields_AIC = split /\s+/;
+	foreach my $f_AIC (@fields_AIC) { $f_AIC =~ s/\"//g; }
+	push @data_AIC, \@fields_AIC;
+    }
+
+    my $basename = basename($pheno_filepath.".clean.AIC");
+
+    copy($pheno_filepath.".clean.AIC", $c->config->{basepath}."/static/documents/tempfiles/spatial_model_files/".$basename);
+
+    my $download_url_AIC = '/documents/tempfiles/spatial_model_files/'.$basename;
+    my $download_link_AIC = "<a href=\"$download_url_AIC\">Download Results</a>";
+
 
     $c->stash->{rest} = {
 	data => \@data,
     headers => \@spl,
 	download_link => $download_link,
+    data_fitted => \@data_fitted,
+    headers_fitted => \@spl_fitted,
+    download_link_fitted => $download_link_fitted,
+    data_AIC => \@data_AIC,
+    headers_AIC => \@spl_AIC,
+    download_link_AIC => $download_link_AIC,
     };
 }
 
