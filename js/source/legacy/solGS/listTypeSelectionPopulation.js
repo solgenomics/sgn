dcd356ed377a600cc1acbd5ba3b382fa5b2f3be3
Isaak <iyt2@cornell.edu>
2023-06-07 14:34:00 +0200

    simplify and cleanup

--- js/source/legacy/solGS/listTypeSelectionPopulation.js
+++ js/source/legacy/solGS/listTypeSelectionPopulation.js
@@ -7,7 +7,7 @@ Isaak Y Tecle
 iyt2@cornell.edu
 */
 
-JSAN.use("jquery.blockUI");
+// JSAN.use("jquery.blockUI");
 
 var solGS = solGS || function solGS() {};
 
@@ -19,31 +19,14 @@ solGS.listTypeSelectionPopulation = {
     var args = this.createSelectionReqArgs(listId);
     args = JSON.stringify(args);
 
-    jQuery.ajax({
+    var checkPredicted = jQuery.ajax({
       type: "POST",
       dataType: "json",
       data: { arguments: args },
       url: "/solgs/check/predicted/list/selection",
-      success: function (response) {
-        args = JSON.parse(args);
-
-        if (response.output) {
-          solGS.listTypeSelectionPopulation.displayPredictedListTypeSelectionPops(
-            args,
-            response.output
-          );
-
-          if (document.URL.match(/solgs\/traits\/all\/|solgs\/models\/combined\//)) {
-            solGS.sIndex.populateSindexMenu();
-            solGS.correlation.populateGenCorrMenu();
-            solGS.geneticGain.ggSelectionPopulations();
-            solGS.cluster.listClusterPopulations();
-          }
-        } else {
-          solGS.listTypeSelectionPopulation.askSelectionJobQueueing(listId);
-        }
-      },
     });
+
+    return checkPredicted;
   },
 
   getSelectionListElementsNames: function (list) {
@@ -81,7 +64,8 @@ solGS.listTypeSelectionPopulation = {
     var protocolId = args.genotyping_protocol_id;
 
     var hostName = window.location.protocol + "//" + window.location.host;
-    var page = hostName + "/solgs/selection/" + selectionPopId + "/model/" + modelId;
+    var page =
+      hostName + "/solgs/selection/" + selectionPopId + "/model/" + modelId + "/gp/" + protocolId;
 
     solGS.waitPage(page, args);
   },
@@ -90,17 +74,14 @@ solGS.listTypeSelectionPopulation = {
     if (typeof listId == "number") {
       var genoList = this.getListTypeSelectionPopDetail(listId);
       var listName = genoList.name;
-      var list = genoList.list;
-      var modelId = this.getModelId();
-      var traitId = this.getTraitId();
 
-      var dataSetType = this.trainingDataSetType();
-
-      var popType = "list_prediction";
+      var modelArgs = solGS.getModelArgs();
+      var modelId = modelArgs.training_pop_id;
+      var dataSetType = modelArgs.data_set_type;
+      var popType = "list_selection";
 
       var selectionPopId = "list_" + listId;
-      var protocolId = jQuery("#genotyping_protocol_id").val();
-
+      var protocolId = modelArgs.genotyping_protocol_id;
       var trainingTraitsIds = solGS.getTrainingTraitsIds();
 
       var args = {
@@ -119,167 +100,25 @@ solGS.listTypeSelectionPopulation = {
     }
   },
 
-  getGenotypesList: function (listId) {
-    var list = new CXGN.List();
-    var genotypesList;
-
-    if (!listId == "") {
-      genotypesList = list.getListData(listId);
-    }
-
-    var listName = list.listNameById(listId);
-    var listType = list.getListType(listId);
-
-    return { name: listName, listId: listId, list_type: listType };
-  },
-
-  loadGenotypesListTypeSelectionPop: function (args) {
-    var listDetail = this.getListTypeSelectionPopDetail(args.list_id);
-
-    if (window.Prototype) {
-      delete Array.prototype.toJSON;
-    }
-
-    args = JSON.stringify(args);
-    var len = listDetail.elements_count;
-
-    if (len === 0) {
-      alert("The list is empty. Please select a list with content.");
-    } else {
-      jQuery.blockUI.defaults.applyPlatformOpacityRules = false;
-      jQuery.blockUI({ message: "Please wait.." });
-
-      jQuery.ajax({
-        type: "POST",
-        dataType: "json",
-        data: { arguments: args },
-        url: "/solgs/load/genotypes/list/selection",
-        success: function (response) {
-          if (response.status == "success") {
-            args = JSON.parse(args);
-            var modelId = args.training_pop_id;
-            var traitId = args.trait_id;
-            var selPopId = args.selection_pop_id;
-            var listId = args.list_id;
-
-            if (window.location.href.match(/solgs\/trait\//)) {
-              window.location =
-                "/solgs/selection/" + selPopId + "/model/" + modelId + "/trait/" + traitId;
-              jQuery.unblockUI();
-            } else if (window.location.href.match(/solgs\/model\/combined\/populations\//)) {
-              window.location =
-                "/solgs/selection/" + selPopId + "/model/combined/" + modelId + "/trait/" + traitId;
-              jQuery.unblockUI();
-            } else {
-              displayPredictedListTypeSelectionPops(args, response.output);
-              solGS.sIndex.populateSindexMenu();
-              solGS.correlation.populateGenCorrMenu();
-              jQuery.unblockUI();
-            }
-          } else {
-            alert("fail: Error occured while querying for the genotype data of the accessions.");
-            jQuery.unblockUI();
-          }
-        },
-        error: function (res) {
-          alert("Error occured while querying for the genotype data of the accessions.");
-          jQuery.unblockUI();
-        },
-      });
-    }
-  },
-
-  predictGenotypesListSelectionPop: function (args) {
-    var modelId = args.training_pop_id;
-    var traitId = args.trait_id;
-    var selPopId = args.selection_pop_id;
-
-    var url = "/solgs/selection/" + selPopId + "/model/" + modelId;
-
-    jQuery.ajax({
-      dataType: "json",
-      type: "POST",
-      data: { trait_id: traitId, list_source: "from_db", list_prediction: 1 },
-      url: url,
-      success: function (res) {
-        if (res.status == "success") {
-          window.location =
-            "/solgs/selection/" + selPopId + "/model/" + modelId + "/trait/" + traitId;
-        } else {
-          window.location = window.location.href;
-        }
-      },
-    });
-
-    jQuery.unblockUI();
-  },
-
-  getModelId: function () {
-    var modelId;
-    var modelIdExists = jQuery("#model_id").doesExist();
-    var comboPopsIdExists = jQuery("#combo_pops_id").doesExist();
-    var popIdExists = jQuery("#population_id").doesExist();
-
-    if (jQuery("#model_id").val()) {
-      modelId = jQuery("#model_id").val();
-    } else if (jQuery("#population_id").val()) {
-      modelId = jQuery("#population_id").val();
-    } else if (jQuery("#combo_pops_id").val()) {
-      modelId = jQuery("#combo_pops_id").val();
-    }
-
-    return modelId;
-  },
-
-  trainingDataSetType: function () {
-    var dataSetType;
-    var referer = document.URL;
-
-    if (referer.match(/\/combined\//)) {
-      dataSetType = "combined_populations";
-    } else {
-      dataSetType = "single_population";
-    }
-
-    return dataSetType;
-  },
-
-  getTraitId: function () {
-    var traitId;
-    var traitIdExists = jQuery("#trait_id").doesExist();
-
-    if (traitIdExists == true) {
-      traitId = jQuery("#trait_id").val();
-    }
-
-    return traitId;
-  },
-
   displayPredictedListTypeSelectionPops: function (args, output) {
     var listName = args.list_name;
     var listId = args.list_id;
-    var traitId = args.trait_id;
-    var selectionPopId = args.selection_pop_id;
-    var trainingPopId = args.training_pop_id;
 
-    var url = "/solgs/selection/" + selectionPopId + "/model/" + trainingPopId;
-    var listIdArg = "'" + listId + "'";
-    var listSource = "'from_db'";
     var popIdName = { id: "list_" + listId, name: listName, pop_type: "list_selection" };
     popIdName = JSON.stringify(popIdName);
     var hiddenInput = '<input type="hidden" value=\'' + popIdName + "'/>";
 
-    var predictedListTypeSelectionPops = jQuery("#list_type_selection_pops_table").doesExist();
+    var tableId = "list_type_selection_pops_table";
+    var trId = "list_selection_" + listId;
+    var predictedListTypeSelectionPops = jQuery(`#${tableId}`).doesExist();
 
     if (predictedListTypeSelectionPops == false) {
       var predictedListTypeSelectionTable =
-        '<table id="list_type_selection_pops_table" class="table"><thead><tr>' +
+        `<table id="${tableId}" class="table"><thead><tr>` +
         "<th>List-based selection population</th>" +
         "<th>View GEBVs</th>" +
         "</tr></thead><tbody>" +
-        '<tr id="list_prediction_output_' +
-        listId +
-        '">' +
+        `<tr id="${trId}">` +
         "<td>" +
         "<b>" +
         listName +
@@ -293,17 +132,9 @@ solGS.listTypeSelectionPopulation = {
 
       jQuery("#list_type_selection_pops_selected").append(predictedListTypeSelectionTable).show();
     } else {
-      var listIdArg = "'" + listId + "'";
-      var listSource = "'from_db'";
-
-      var popIdName = { id: "list_" + listId, name: listName, pop_type: "list_selection" };
-      popIdName = JSON.stringify(popIdName);
-      var hiddenInput = '<input type="hidden" value=\'' + popIdName + "'/>";
-
       var addRow =
-        '<tr id="list_prediction_output_' +
-        listId +
-        '"><td>' +
+        `<tr id="${trId}">` +
+        "<td>" +
         "<b>" +
         listName +
         "</td>" +
@@ -313,14 +144,13 @@ solGS.listTypeSelectionPopulation = {
         output +
         "</td></tr>";
 
-      var trId = "#list_prediction_output_" + listId;
-      var samePop = jQuery(trId).doesExist();
+      var samePop = jQuery(`#${trId}`).doesExist();
 
       if (samePop == false) {
-        jQuery("#list_type_selection_pops_table tr:last").after(addRow);
+        jQuery(`#${tableId} tr:last`).after(addRow);
       } else {
-        jQuery(trId).remove();
-        jQuery("#list_type_selection_pops_table").append(addRow).show();
+        jQuery(`#${trId}`).remove();
+        jQuery(`#${tableId}`).append(addRow).show();
       }
     }
   },
@@ -370,55 +200,6 @@ solGS.listTypeSelectionPopulation = {
     }
     return popsList;
   },
-
-  loadPredictionOutput: function (url, listId, listSource) {
-    var traitId = this.getTraitId();
-    var modelId = this.getModelId();
-
-    jQuery.blockUI.defaults.applyPlatformOpacityRules = false;
-    jQuery.blockUI({ message: "Please wait.." });
-
-    jQuery.ajax({
-      type: "POST",
-      url: url,
-      dataType: "json",
-      data: {
-        list_prediction: 1,
-        trait_id: traitId,
-        model_id: modelId,
-        prediction_id: listId,
-        list_source: listSource,
-      },
-
-      success: function (response) {
-        if (response.status == "success") {
-          var tdId = "#list_prediction_output_" + listId;
-          jQuery(tdId).html(response.output);
-
-          var page = document.URL;
-
-          if (page.match("/traits/all/population/") != null) {
-            solGS.sIndex.populateSindexMenu();
-            solGS.correlation.populateGenCorrMenu();
-          }
-
-          jQuery.unblockUI();
-        } else {
-          if (response.status == "failed") {
-            alert("Error occured while uploading the list of selection genotypes.");
-          } else {
-            alert(response.status);
-          }
-
-          jQuery.unblockUI();
-        }
-      },
-
-      error: function (response) {
-        alert("error: " + res.responseText);
-      },
-    });
-  },
 };
 
 jQuery.fn.doesExist = function () {
@@ -430,7 +211,8 @@ jQuery(document).ready(function () {
 });
 
 jQuery(document).ready(function () {
-  jQuery("#list_type_selection_pops_select").change(function () {
+  var menuId = solGS.listTypeSelectionPopulation.selectionPopsSelectMenuId;
+  jQuery(`${menuId}`).change(function () {
     var selectedPop = jQuery("option:selected", this).data("pop");
     if (selectedPop.id) {
       jQuery(" #list_type_selection_pop_go_btn").click(function () {
@@ -443,7 +225,30 @@ jQuery(document).ready(function () {
           );
 
           if (listDetail.type.match(/accessions/)) {
-            solGS.listTypeSelectionPopulation.checkPredictedListSelection(selectedPop.id);
+            solGS.listTypeSelectionPopulation
+              .checkPredictedListSelection(selectedPop.id)
+              .done(function (res) {
+                var args = solGS.listTypeSelectionPopulation.createSelectionReqArgs(selectedPop.id);
+
+                if (res.output) {
+                  solGS.listTypeSelectionPopulation.displayPredictedListTypeSelectionPops(
+                    args,
+                    res.output
+                  );
+
+                  if (document.URL.match(/solgs\/traits\/all\/|solgs\/models\/combined\//)) {
+                    solGS.sIndex.populateSindexMenu();
+                    solGS.correlation.populateGenCorrMenu();
+                    solGS.geneticGain.ggSelectionPopulations();
+                    solGS.cluster.listClusterPopulations();
+                  }
+                } else {
+                  solGS.listTypeSelectionPopulation.askSelectionJobQueueing(listId);
+                }
+              })
+              .fail(function () {
+                alert("Error occured checking if the selection population has predicted output.");
+              });
           } else {
             //TO-DO
             //	var trialsList = listDetail.list;
