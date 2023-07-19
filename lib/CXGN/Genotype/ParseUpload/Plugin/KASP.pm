8be92f4b3e5a9a3d21a473c7bd054c15cd4733f4
titima15 <tt15@cornell.edu>
2023-07-19 14:58:41 -0400

    validate info

--- lib/CXGN/Genotype/ParseUpload/Plugin/KASP.pm
+++ lib/CXGN/Genotype/ParseUpload/Plugin/KASP.pm
@@ -5,6 +5,7 @@ use SGN::Model::Cvterm;
 use Data::Dumper;
 use Text::CSV;
 use CXGN::Genotype::Protocol;
+use CXGN::List::Validate;
 
 sub _validate_with_plugin {
     my $self = shift;
@@ -40,14 +41,12 @@ sub _validate_with_plugin {
     my $marker_info_csv = Text::CSV->new({ sep_char => ',' });
     my $MI_F;
 
-    my %marker_names;
-    # Open Marker Info File and get headers
+    my %marker_names
     open($MI_F, "<", $marker_info_filename) || die "Can't open file $marker_info_filename\n";
 
         my $marker_info_header_row = <$MI_F>;
         my @marker_header_info;
 
-        # Get first row, which is the header
         if ($marker_info_csv->parse($marker_info_header_row)) {
             @marker_header_info = $marker_info_csv->fields();
         }
@@ -146,119 +145,116 @@ sub _validate_with_plugin {
 
     my $csv = Text::CSV->new({ sep_char => ',' });
     my $F;
+
+    my %seen_sample_ids;
     open($F, "<", $filename) || die "Can't open file $filename\n";
 
         my $header_row = <$F>;
         my @header_info;
 
-        # Get first row, which is the header
         if ($csv->parse($header_row)) {
             @header_info = $csv->fields();
         }
 
-        # Remove the first column from the header
-        my $unneeded_first_column = shift @header_info;
-        my @fields = ($unneeded_first_column);
-        my @markers = @header_info;
+        my $sample_id_header = $header_info[0];
+        $sample_id_header =~ s/^\s+|\s+$//g;
 
-        foreach (@markers) {
-            if (!exists($marker_names{$_})) {
-                push @error_messages, "Marker $_ in the SNP grid file is not found in the marker info file.";
-            }
+        my $snp_id_header = $header_info[1];
+        $snp_id_header =~ s/^\s+|\s+$//g;
+
+        my $snpcall_header = $header_info[2];
+        $SNPCall_header =~ s/^\s+|\s+$//g;
+
+        my $Xvalue_header = $header_info[2];
+        $SNPCall_header =~ s/^\s+|\s+$//g;
+
+        my $Yvalue_header = $header_info[2];
+        $SNPCall_header =~ s/^\s+|\s+$//g;
+
+        if ($sample_id_header ne 'SampleID'){
+            push @error_messages, 'Column 1 header must be "SampleID" in the KASP result File.';
         }
 
-        my @observation_unit_names;
-        # Iterate over all rows to get the sample ID and labID
+        if ($snp_id_header ne 'SNPID'){
+            push @error_messages, 'Column 2 header must be "SNPID" in the KASP result File.';
+        }
+
+        if ($snpcall_header ne 'SNPCall'){
+            push @error_messages, 'Column 3 header must be "SNPCall" in the KASP result File.';
+        }
+
+        if ($Xvalue_header ne 'Xvalue'){
+            push @error_messages, 'Column 4 header must be "Xvalue" in the KASP result File.';
+        }
+
+        if ($Yvalue_header ne 'Yvalue'){
+            push @error_messages, 'Column 5 header must be "Yvalue" in the KASP result File.';
+        }
+
+
         while (my $line = <$F>) {
             my @line_info;
             if ($csv->parse($line)) {
                 @line_info = $csv->fields();
             }
-            my $sample_name = $line_info[0];
-            chomp $sample_name;
-            $sample_name =~ s/^\s+|\s+$//g;
-            push @observation_unit_names, $sample_name;
+
+            my $sample_id = $line_info[0];
+            $sample_id =~ s/^\s+|\s+$//g;
+            my $snp_id = $line_info[1];
+            $snp_id =~ s/^\s+|\s+$//g;
+            my $snpcall = $line_info[2];
+            $snpcall =~ s/^\s+|\s+$//g;
+            my $xvalue = $line_info[3];
+            $xvalue =~ s/^\s+|\s+$//g;
+            my $yvalue = $line_info[4];
+            $yvalue =~ s/^\s+|\s+$//g;
+
+            if (!defined $sample_id){
+                push @error_messages, 'Sample id is required for all rows.';
+            } else {
+                $seen_sample_ids{$sample_id}++;
+            }
+
+            if (!defined $snp_id){
+                push @error_messages, 'SNP id is required for all rows.';
+            } elsif (!exists($marker_names{$snp_id})) {
+                push @error_messages, "Marker $snp_id in the result file is not found in the marker info file.";
+            }
+
+            if (!defined $snpcall){
+                push @error_messages, 'SNP call is required for all rows.';
+            }
+
+            if (!defined $xvalue){
+                push @error_messages, 'X value is required for all rows.';
+            }
+
+            if (!defined $yvalue){
+                push @error_messages, 'Y value is required for all value.';
+            }
         }
 
     close($F);
 
-    # Check that the first column in the header is equal to 'SampleName.LabID'
-    if ($fields[0] ne 'SampleName.LabID'){
-        push @error_messages, 'Column 1 header must be "SampleName.LabID" in the Grid File.';
-    }
-
-    my $number_observation_units = scalar(@observation_unit_names);
-    print STDERR "Number observation units: $number_observation_units...\n";
-
     my $stock_type = $self->get_observation_unit_type_name;
-    my @observation_units_names_trim;
-    # Separates sample name from lab id
-    foreach (@observation_unit_names) {
-        if ($stock_type eq 'accession'){
-            my ($observation_unit_name_with_accession_name, $lab_number) = split(/\./, $_, 2);
-            $observation_unit_name_with_accession_name =~ s/^\s+|\s+$//g;
-            my ($observation_unit_name, $accession_name) = split(/\|\|\|/, $observation_unit_name_with_accession_name);
-            push @observation_units_names_trim, $observation_unit_name;
-        }
-        else {
-            my ($observation_unit_name, $accession_name) = split(/\|\|\|/, $_);
-            push @observation_units_names_trim, $observation_unit_name;
-        }
-    }
-    my $observation_unit_names = \@observation_units_names_trim;
+    my $validate_type = $stock_type.'s';
+    print STDERR "VALIDATE TYPE =".Dumper($validate_type)."\n";
 
-    my $organism_id = $self->get_organism_id;
-    my $accession_cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
+    my @all_sample_ids = keys %seen_sample_ids;
+    my $sample_validator = CXGN::List::Validate->new();
+    my @sample_missing = @{$sample_validator->validate($schema,$validate_type,\@all_sample_ids)->{'missing'}};
 
-    # Validate that the sample names are in the database already
-    my @missing_stocks;
-    my $validator = CXGN::List::Validate->new();
-    if ($stock_type eq 'tissue_sample'){
-        @missing_stocks = @{$validator->validate($schema,'tissue_samples',$observation_unit_names)->{'missing'}};
-    } elsif ($stock_type eq 'accession'){
-        @missing_stocks = @{$validator->validate($schema,'accessions',$observation_unit_names)->{'missing'}};
-    } elsif ($stock_type eq 'stocks'){
-        @missing_stocks = @{$validator->validate($schema,'stocks',$observation_unit_names)->{'missing'}};
-    } else {
-        push @error_messages, "You can only upload genotype data for a tissue_sample OR accession (including synonyms) OR stocks!"
+    if (scalar(@sample_missing) > 0) {
+        push @error_messages, "The following sample ids are not in the database, or are not in the database as uniquenames: ".join(',',@sample_missing);
     }
 
-    my %unique_stocks;
-    foreach (@missing_stocks){
-        $unique_stocks{$_}++;
-    }
-
-    @missing_stocks = sort keys %unique_stocks;
-    my @missing_stocks_return;
-    # Optionally the missing sample ids can be created in the database as new accessions, but that is not recommended
-    foreach (@missing_stocks){
-        if (!$self->get_create_missing_observation_units_as_accessions){
-            push @missing_stocks_return, $_;
-            print STDERR "WARNING! Observation unit name $_ not found for stock type $stock_type. You can pass an option to automatically create accessions.\n";
-        } else {
-            my $stock = $schema->resultset("Stock::Stock")->create({
-                organism_id => $organism_id,
-                name       => $_,
-                uniquename => $_,
-                type_id     => $accession_cvterm_id,
-            });
-        }
-    }
-
-    # If there are missing sample names, they will be returned along with an error message
-    if (scalar(@missing_stocks_return)>0){
-        $errors{'missing_stocks'} = \@missing_stocks_return;
-        push @error_messages, "The following stocks are not in the database: ".join(',',@missing_stocks_return);
-    }
-
-    #store any errors found in the parsed file to parse_errors accessor
     if (scalar(@error_messages) >= 1) {
         $errors{'error_messages'} = \@error_messages;
         $self->_set_parse_errors(\%errors);
         return;
     }
 
-    return 1; #returns true if validation is passed
+    return 1; 
 }
 
 
