db5a781899edacd30698c4c69b00503c8ec2ee79
titima15 <tt15@cornell.edu>
2023-07-20 16:09:34 -0400

    aggregate results

--- lib/CXGN/Genotype/ParseUpload/Plugin/KASP.pm
+++ lib/CXGN/Genotype/ParseUpload/Plugin/KASP.pm
@@ -274,7 +274,7 @@ sub _parse_with_plugin {
     my $marker_info_csv = Text::CSV->new({ sep_char => ',' });
     my $MF;
     my %marker_info;
-    my %marker_info_nonseparated;
+    my %marker_info_details;
     my @marker_info_keys = qw(name intertek_name chrom pos alt ref);
 
     open($MF, "<", $marker_info_filename) || die "Can't open file $marker_info_filename\n";
@@ -339,7 +339,7 @@ sub _parse_with_plugin {
             }
 
             push @{$protocolprop_info{'marker_names'}}, $customer_snp_id;
-            $marker_info_nonseparated{$customer_snp_id} = \%marker;
+            $marker_info_details{$customer_snp_id} = \%marker;
 
             push @{$protocolprop_info{'markers_array'}->{$chromosome}}, \%marker;
             $marker_info{$chromosome}->{$customer_snp_id} = \%marker;
@@ -351,13 +351,122 @@ sub _parse_with_plugin {
     print STDERR "MARKER INFO =".Dumper(\%marker_info)."\n";
 
 
+    my %kasp_result;
 
+    my $csv = Text::CSV->new({ sep_char => ',' });
+    my $F;
 
+    my %seen_samples;
+    open($F, "<", $filename) || die "Can't open file $filename\n";
 
+        my $header_row = <$F>;
+        my @header_info;
 
+        if ($csv->parse($header_row)) {
+            @header_info = $csv->fields();
+        }
 
+        while (my $line = <$F>) {
+            my @line_info;
+            if ($csv->parse($line)) {
+                @line_info = $csv->fields();
+            }
+
+            my $sample_id = $line_info[0];
+            $sample_id =~ s/^\s+|\s+$//g;
+            my $snp_id = $line_info[1];
+            $snp_id =~ s/^\s+|\s+$//g;
+            my $snpcall = $line_info[2];
+            $snpcall =~ s/^\s+|\s+$//g;
+            my $xvalue = $line_info[3];
+            $xvalue =~ s/^\s+|\s+$//g;
+            my $yvalue = $line_info[4];
+            $yvalue =~ s/^\s+|\s+$//g;
+
+           $kasp_result{$snp_id}{$sample_id}{'call'} = $snpcall;
+           $kasp_result{$snp_id}{$sample_id}{'XV'} = $xvalue;
+           $kasp_result{$snp_id}{$sample_id}{'YV'} = $yvalue;
+           $seen_samples{$sample_id}++;
+        }
+
+    close($F);
+
+    my @observation_unit_names = keys %seen_samples;
+    my %genotype_info;
+
+    foreach my $marker_name (keys %kasp_result) {
+        my %sample_kasp_result = ();
+        my $ref = $marker_info_details{$marker_name}{'ref'};
+        my $alt = $marker_info_details{$marker_name}{'alt'};
+        my $chrom = $marker_info_details{$marker_name}{'chrom'};
+        my $sample_result = $kasp_result{$marker_name};
+        %sample_kasp_result = %{$sample_result};
+        foreach my $sample (keys %sample_kasp_result) {
+            my $snp_call = $sample->{call};
+            my $XV = $sample->{XV};
+            my @snp_alleles = split ":", $snp_call;
+
+            my $genotype_obj;
+            if ($ref && $alt) {
+                my @gt_vcf_genotype;
+                my @ref_calls;
+                my @alt_calls;
+                my $gt_dosage_alt = 0;
+                foreach my $a (@snp_alleles){
+                    if ($a eq $ref) {
+                        push @gt_vcf_genotype, 0;
+                        push @ref_calls, $a;
+                    }
+                    elsif ($a eq $alt) {
+                        push @gt_vcf_genotype, 1;
+                        push @alt_calls, $a;
+                        $gt_dosage_alt++;
+                    }
+                    elsif ($a eq '?' || $a eq 'Uncallable') {
+                        $gt_dosage_alt = 'NA';
+                        push @gt_vcf_genotype, './.';
+                        push @alt_calls, './.';
+                    } else {
+                        push @error_messages, "Allele Call Does Not Match Ref or Alt for Sample: $sample Marker: $marker_name Alt: $alt Ref: $ref Allele: $a";
+                    }
+                }
+
+                my @vcf_genotype = (@ref_calls, @alt_calls);
+                my $vcf_genotype_string = join ',', @vcf_genotype;
+                my $vcf_gt_genotype_string = join '/', @gt_vcf_genotype;
+                $genotype_obj = {
+                    'GT' => $vcf_gt_genotype_string,
+                    'NT' => $vcf_genotype_string,
+                    'DS' => "$gt_dosage_alt",
+                    'XV' => $XV,
+                    'YV' => $YV,
+                };
+            } else {
+                die "There should always be a ref and alt according to validation above\n";
+            }
+
+        }
+
+        $genotype_info{$sample}->{$chrom}->{$marker_name} = $genotype_obj;
+    }
+
+    if (scalar(@error_messages)>0) {
+        $errors{'error_messages'} = \@error_messages;
+        $self->_set_parse_errors(\%errors);
+        return;
+    }
+
+    my %parsed_data = (
+        protocol_info => \%protocolprop_info,
+        genotypes_info => \%genotype_info,
+        observation_unit_uniquenames => \@observation_unit_names,
+        marker_info_keys => \@marker_info_keys
+    );
+
+    $self->_set_parsed_data(\%parsed_data);
 
     return 1;
+
 }
 
 1;
