dbe77cbe9e6bd53cc0f0980df1b593f8d52b45a2
Srikanth <sk2783@cornell.edu>
2023-08-01 10:49:11 -0400

    indentation of project.pm

--- lib/CXGN/Project.pm
+++ lib/CXGN/Project.pm
@@ -1329,9 +1329,9 @@ sub get_transplanting_date {
     my $self = shift;
     my $transplanting_date_cvterm_id = $self->get_transplanting_date_cvterm_id();
     my $row = $self->bcs_schema->resultset('Project::Projectprop')->find({
-	    project_id => $self->get_trial_id(),
-	    type_id => $transplanting_date_cvterm_id,
-	});
+        project_id => $self->get_trial_id(),
+        type_id => $transplanting_date_cvterm_id,
+    });
 
     my $calendar_funcs = CXGN::Calendar->new({});
     if ($row){
@@ -1349,16 +1349,16 @@ sub set_transplanting_date {
     my $calendar_funcs = CXGN::Calendar->new({});
 
     if (my $transplanting_event = $calendar_funcs->check_value_format($transplanting_date)) {
-	    my $transplanting_date_cvterm_id = $self->get_transplanting_date_cvterm_id();
-	    my $row = $self->bcs_schema->resultset('Project::Projectprop')->find_or_create({
-		    project_id => $self->get_trial_id(),
-		    type_id => $transplanting_date_cvterm_id,
-		});
-	    $row->value($transplanting_event);
-	    $row->update();
+        my $transplanting_date_cvterm_id = $self->get_transplanting_date_cvterm_id();
+        my $row = $self->bcs_schema->resultset('Project::Projectprop')->find_or_create({
+            project_id => $self->get_trial_id(),
+            type_id => $transplanting_date_cvterm_id,
+        });
+        $row->value($transplanting_event);
+        $row->update();
     } 
     else{
-		print STDERR "date format did not pass check while preparing to set transplanting date: $transplanting_date \n";
+        print STDERR "date format did not pass check while preparing to set transplanting date: $transplanting_date \n";
     }
 }
 
@@ -1368,18 +1368,18 @@ sub remove_transplanting_date {
     my $calendar_funcs = CXGN::Calendar->new({});
     if (my $transplanting_event = $calendar_funcs->check_value_format($transplanting_date) ) {
         my $transplanting_date_cvterm_id = $self->get_transplanting_date_cvterm_id();
-		my $row = $self->bcs_schema->resultset('Project::Projectprop')->find_or_create({
-	    	project_id => $self->get_trial_id(),
-	    	type_id => $transplanting_date_cvterm_id,
-	    	value => $transplanting_event,
-		});
-    	if ($row){
-	    	print STDERR "Removing transplanting date $transplanting_event from trial ".$self->get_trial_id()."\n";
-	    	$row->delete();
-    	}
+        my $row = $self->bcs_schema->resultset('Project::Projectprop')->find_or_create({
+            project_id => $self->get_trial_id(),
+            type_id => $transplanting_date_cvterm_id,  
+            value => $transplanting_event,
+        });
+        if ($row){
+            print STDERR "Removing transplanting date $transplanting_event from trial ".$self->get_trial_id()."\n";
+            $row->delete();
+        }   
     } 
     else {
-		print STDERR "date format did not pass check while preparing to delete transplanting date: $transplanting_date  \n";
+        print STDERR "date format did not pass check while preparing to delete transplanting date: $transplanting_date  \n";
     }
 }
 
