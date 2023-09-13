a20f04cd4f8a8016f4756372c7f31e1152394478
Isaak <iyt2@cornell.edu>
2023-09-13 14:14:50 +0200

    merge phenotypic, genetic correlation code into one file

--- R/solGS/phenotypic_correlation.r
+++ R/solGS/phenotypic_correlation.r
@@ -1,6 +1,6 @@
  #SNOPSIS
 
- #runs phenotypic correlation analysis.
+ #runs correlation analysis.
  #Correlation coeffiecients are stored in tabular and json formats
 
  #AUTHOR
@@ -15,99 +15,147 @@ library(jsonlite)
 library(data.table)
 library(phenoAnalysis)
 library(dplyr)
+library(tibble)
 #library(rbenchmark)
 library(methods)
 
 allArgs <- commandArgs()
 
-
 outputFiles <- scan(grep("output_files", allArgs, value = TRUE),
                     what = "character")
-
 inputFiles  <- scan(grep("input_files", allArgs, value = TRUE),
                     what = "character")
 
 message('inputFiles: ', inputFiles)
 
-refererQtl <- grep("qtl", inputFiles, value=TRUE)
+correInputData <- c()
+correCoefsTableFile <- c()
+correCoefsJsonFile <- c()
 
-phenoDataFile      <- grep("\\/phenotype_data", inputFiles, value=TRUE)
-formattedPhenoFile <- grep("formatted_phenotype_data", inputFiles, fixed = FALSE, value = TRUE)
-metaFile       <-  grep("metadata", inputFiles, value=TRUE)
-message('metaFiles: ', metaFile)
-correCoefficientsFile     <- grep("pheno_corr_table", outputFiles, value=TRUE)
-correCoefficientsJsonFile <- grep("pheno_corr_json", outputFiles, value=TRUE)
+if (any(grepl("phenotype_data", inputFiles))) {
+  refererQtl <- grep("qtl", inputFiles, value=TRUE)
 
-formattedPhenoData <- c()
-phenoData          <- c()
+  phenoDataFile      <- grep("\\/phenotype_data", inputFiles, value=TRUE)
+  formattedPhenoFile <- grep("formatted_phenotype_data", inputFiles, fixed = FALSE, value = TRUE)
+  metaFile       <-  grep("metadata", inputFiles, value=TRUE)
+  correCoefsTableFile     <- grep("pheno_corr_table", outputFiles, value=TRUE)
+  correCoefsJsonFile <- grep("pheno_corr_json", outputFiles, value=TRUE)
 
+  formattedPhenoData <- c()
+  phenoData          <- c()
 
-if ( length(refererQtl) != 0 ) {
-   phenoDataFile      <- grep("\\/phenodata", inputFiles, value=TRUE)
+  if ( length(refererQtl) != 0 ) {
+    phenoDataFile      <- grep("\\/phenodata", inputFiles, value=TRUE)
+    phenoData <- data.frame(fread(phenoDataFile,
+          header=TRUE,
+          sep=",",
+          na.strings=c("NA", "-", " ", ".", "..")))
+  }
 
-   phenoData <- data.frame(fread(phenoDataFile,
-				header=TRUE,
-        sep=",",
-        na.strings=c("NA", "-", " ", ".", "..")))
-}
-# else {
-#
-#     phenoData <- data.frame(fread(phenoDataFile,
-#                                      header = TRUE,
-#                                      sep="\t",
-#                                      na.strings = c("NA", "", "--", "-", ".", "..")
-#                                    ))
-# }
+  metaData <- scan(metaFile, what="character")
 
-metaData <- scan(metaFile, what="character")
+  allTraitNames <- c()
+  nonTraitNames <- c()
+  naTraitNames  <- c()
 
-allTraitNames <- c()
-nonTraitNames <- c()
-naTraitNames  <- c()
-
-if (length(refererQtl) != 0) {
-
-  allNames      <- names(phenoData)
-  nonTraitNames <- c("ID")
-  allTraitNames <- allNames[! allNames %in% nonTraitNames]
-
-}
-
-correPhenoData <- c(0)
-
-if (length(refererQtl) == 0  ) {
-    averagedPhenoData <- cleanAveragePhenotypes(inputFiles, metaDataFile = metaFile)
-    allNames <- names(averagedPhenoData)
-    nonTraitNames <- metaData
+  if (length(refererQtl) != 0) {
+    allNames      <- names(phenoData)
+    nonTraitNames <- c("ID")
     allTraitNames <- allNames[! allNames %in% nonTraitNames]
+  }
 
-  rownames(averagedPhenoData) <- NULL
-  correPhenoData <- averagedPhenoData
-} else {
-  message("qtl stuff")
-  correPhenoData <- phenoData %>%
-                        group_by(ID) %>%
-                        summarise_if(is.numeric, mean, na.rm=TRUE) %>%
-                        select(-ID) %>%
-                        round(., 2) %>%
-                        data.frame
+  correPhenoData <- c()
+
+  if (length(refererQtl) == 0  ) {
+      averagedPhenoData <- cleanAveragePhenotypes(inputFiles, metaDataFile = metaFile)
+      allNames <- names(averagedPhenoData)
+      nonTraitNames <- metaData
+      allTraitNames <- allNames[! allNames %in% nonTraitNames]
+
+    rownames(averagedPhenoData) <- NULL
+    correPhenoData <- averagedPhenoData
+  } else {
+    message("qtl stuff")
+    correPhenoData <- phenoData %>%
+                          group_by(ID) %>%
+                          summarise_if(is.numeric, mean, na.rm=TRUE) %>%
+                          select(-ID) %>%
+                          round(., 2) %>%
+                          data.frame
+
+  }
+
+  correInputData <- correPhenoData
+print(head(correPhenoData))
+
+} else if (any(grepl("combined_gebvs", inputFiles)) || 
+              any(grepl("selection_index", inputFiles))) {
+
+    correCoefsTableFile <- grep("genetic_corr_table", outputFiles, value=TRUE)
+    correCoefsJsonFile  <- grep("genetic_corr_json", outputFiles, value=TRUE)
+    geneticDataFile <- grep("combined_gebvs", inputFiles, value=TRUE)
+    selectionIndexFile <- grep("selection_index", inputFiles, value=TRUE)
+
+    message('selectionIndexFile', selectionIndexFile)
+    message('geneticDataFile', geneticDataFile)
+
+    geneticData <- read.table(geneticDataFile,
+                              header = TRUE,
+                              row.names = 1,
+                              sep = "\t",
+                              na.strings = c("NA"),
+                              dec = "."
+                              )
+
+    indexData <- c()
+    if (length(selectionIndexFile) != 0
+        && file.info(selectionIndexFile)$size != 0) {
+        indexData <- read.table(selectionIndexFile,
+                                header = TRUE,
+                                row.names = 1,
+                                sep = "\t",
+                                na.strings = c("NA"),
+                                dec = "."
+                                )
+    }
+
+    corrData <- c()
+
+    if (!is.null(indexData)) {
+        geneticData <- rownames_to_column(geneticData, var="genotypes")    
+        indexData   <- rownames_to_column(indexData, var="genotypes")
+      
+        geneticData <- geneticData %>% arrange(genotypes)
+        indexData   <- indexData %>% arrange(genotypes)
+        
+        corrData <- full_join(geneticData, indexData)      
+        corrData <- column_to_rownames(corrData, var="genotypes")
+      
+    } else {
+        corrData <- geneticData
+    }
+
+    correInputData <- corrData
 
 }
 
-coefpvalues <- rcor.test(correPhenoData,
+if (is.null(correInputData)) {
+  stop("Can't run correlation analysis. There is no input data.")
+}
+
+coefpvalues <- rcor.test(correInputData,
                          method="pearson",
                          use="pairwise"
                          )
 
-coefficients <- coefpvalues$cor.mat
+coefs <- coefpvalues$cor.mat
 #remove rows and columns that are all "NA"
-coefficients <- data.frame(coefficients)
- if (any(is.na(coefficients))) {
-  coefficients <- coefficients[ , colSums(is.na(coefficients)) < nrow(coefficients)] 
+coefs <- data.frame(coefs)
+ if (any(is.na(coefs))) {
+  coefs <- coefs[ , colSums(is.na(coefs)) < nrow(coefs)] 
 }
 
- coefficients[upper.tri(coefficients)] <- NA
-
+coefs[upper.tri(coefs)] <- NA
 pvalues <- coefpvalues$cor.mat
 pvalues[lower.tri(pvalues)] <- coefpvalues$p.values[, 3]
 pvalues <- round(pvalues, 3)
@@ -119,25 +167,25 @@ allcordata[upper.tri(allcordata)] <- coefpvalues$p.values[, 3]
 diag(allcordata) <- 1
 allcordata   <- round(allcordata, 3)
 
-traits <- colnames(coefficients)
+traits <- colnames(coefs)
 
 correlationList <- list(
                      labels = traits,
-                    values  = coefficients,
+                    values  = coefs,
                     pvalues = pvalues
                    )
 
 correlationJson <- jsonlite::toJSON(correlationList)
 
 write.table(allcordata,
-       file = correCoefficientsFile,
+       file = correCoefsTableFile,
        sep  = "\t",
        row.names = TRUE,
        quote = FALSE
        )
 
 write(correlationJson,
-       file = correCoefficientsJsonFile)
+       file = correCoefsJsonFile)
 
 message("Done running correlation.")
 q(save = "no", runLast = FALSE)
