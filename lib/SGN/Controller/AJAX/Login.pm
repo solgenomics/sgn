e82aa9e60fba1d5c8ed1c3038201a06571807e29
Adrian Powell <afp43@cornell.edu>
2023-10-05 16:41:27 -0400

    add sp_person_id in Login ajax controller

--- lib/SGN/Controller/AJAX/Login.pm
+++ lib/SGN/Controller/AJAX/Login.pm
@@ -22,7 +22,8 @@ sub is_logged_in :Path('/user/logged_in') Args(0) {
     $c->response->headers->header( "Access-Control-Allow-Methods" => "POST, GET, PUT, DELETE" );
     $c->response->headers->header( 'Access-Control-Allow-Headers' => 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range,Authorization');
 
-    my $dbh = $c->dbic_schema("CXGN::People::Schema")->storage->dbh();
+    my $sp_person_id = $c->user->get_object()->get_sp_person_id();
+    my $dbh = $c->dbic_schema("CXGN::People::Schema", undef, $sp_person_id)->storage->dbh();
     
     my $login = CXGN::Login->new($dbh);
     
