9f8652e1ee36073a1e688f71de35b7fba97163b5
Adrian Powell <afp43@cornell.edu>
2023-10-18 10:38:07 -0400

    adding user_id in BrAPI v2 database connections

--- lib/CXGN/BrAPI/v2/Observations.pm
+++ lib/CXGN/BrAPI/v2/Observations.pm
@@ -257,7 +257,7 @@ sub observations_store {
         return CXGN::BrAPI::JSONResponse->return_error($status, 'Must have submitter privileges to upload phenotypes! Please contact us!', 403);
     }
 
-    my $p = $c->dbic_schema("CXGN::People::Schema")->resultset("SpPerson")->find({sp_person_id=>$user_id});
+    my $p = $c->dbic_schema("CXGN::People::Schema", undef, $user_id)->resultset("SpPerson")->find({sp_person_id=>$user_id});
     my $user_name = $p->username;
 
     ## Validate request structure and parse data
