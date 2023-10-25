99b80a9e440522d3d38296bf9ce4093c23b79ef9
Chris Simoes <ccs263@cornell.edu>
2023-10-24 23:24:51 -0400

    adding heritability error messages and improving R code

--- lib/SGN/Controller/AJAX/Heritability.pm
+++ lib/SGN/Controller/AJAX/Heritability.pm
@@ -110,10 +110,12 @@ sub generate_results: Path('/ajax/heritability/generate_results') : {
     my $c = shift;
     my $dataset_id = $c->req->param('dataset_id');
     my $trait_id = $c->req->param('trait_id');
-    print STDERR "****************************************************************************\n";
+
+
     print STDERR "The dataset is $dataset_id\n";
     print STDERR $dataset_id;
     print STDERR $trait_id;
+
     $c->tempfiles_subdir("heritability_files");
     my $heritability_tmp_output = $c->config->{cluster_shared_tempdir}."/heritability_files";
     mkdir $heritability_tmp_output if ! -d $heritability_tmp_output;
@@ -140,8 +142,6 @@ sub generate_results: Path('/ajax/heritability/generate_results') : {
 
     
     my $h2File = $tempfile . "_" . "h2File.json";
-    my $figure3file = $tempfile . "_" . "figure3.png";
-    my $figure4file = $tempfile . "_" . "figure4.png";
     my $errorFile = $tempfile . "_" . "error.txt";
 
 
@@ -166,8 +166,6 @@ sub generate_results: Path('/ajax/heritability/generate_results') : {
             $c->config->{basepath} . "/R/heritability/h2_blup_rscript.R",
             $pheno_filepath,
             $trait_id,
-            $figure3file,
-            $figure4file,
             $h2File,
             $errorFile
     );
@@ -175,24 +173,12 @@ sub generate_results: Path('/ajax/heritability/generate_results') : {
     $cmd->is_cluster(1);
     $cmd->wait;
 
-    # my $newpath = $c -> {basepath} . "/home/production/cxgn/sgn/documents/tempfiles/heritability_files";
-    # copy($h2File,$newpath) or die "Copy failed: $!";
-    # copy($figure3file,$newpath) or die "Copy failed: $!";
-    # copy($figure4file,$newpath) or die "Copy failed: $!";
    
     my $figure_path = $c->{basepath} . "./documents/tempfiles/heritability_files/";
     copy($h2File, $figure_path);
-    copy($figure3file, $figure_path);
-    copy($figure4file, $figure_path);
 
     my $h2Filebasename = basename($h2File);
     my $h2File_response = "/documents/tempfiles/heritability_files/" . $h2Filebasename;
-
-    my $figure3basename = basename($figure3file);
-    my $figure3_response = "/documents/tempfiles/heritability_files/" . $figure3basename;
-    
-    my $figure4basename = basename($figure4file);
-    my $figure4_response = "/documents/tempfiles/heritability_files/" . $figure4basename;
 
     my $errors;
     if ( -e $errorFile ) {
@@ -205,8 +191,6 @@ sub generate_results: Path('/ajax/heritability/generate_results') : {
         
     $c->stash->{rest} = {
         h2Table => $h2File_response,
-        figure3 => $figure3_response,
-        figure4 => $figure4_response,
         dummy_response => $dataset_id,
         error => $errors
         # dummy_response2 => $trait_id,
