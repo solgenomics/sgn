457740e5c9ffeb12f55bb88411843a4aa55e03de
Lukas Mueller <lam87@cornell.edu>
2024-01-19 16:12:36 -0500

    recent activity js code.

---
+++ js/source/entries/recent_activity.js
@@ -0,0 +1,30 @@
+
+import '../legacy/jquery.js';
+
+export function init() {
+
+
+    function update_tables(start_date, end_date, include_dateless_items) { 
+    
+    jQuery('#recent_activity_trials').DataTable(
+	ajax : '/
+	
+
+    );
+
+    jQuery('#recent_activity_phenotypes').DataTable(
+
+    );
+
+
+    jQuery('#recent_activity_accessions').DataTable(
+
+    );
+
+    jQuery('#recent_activity_plots').DataTable(
+
+    );
+
+
+}
+}
