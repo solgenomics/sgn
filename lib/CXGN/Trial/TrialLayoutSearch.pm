111d070e9179e8d35ef9892e632bd6de5e67fd86
Mirella <mirella@pop-os.localdomain>
2024-02-09 16:57:51 -0500

    clean obunits

--- lib/CXGN/Trial/TrialLayoutSearch.pm
+++ lib/CXGN/Trial/TrialLayoutSearch.pm
@@ -2,103 +2,31 @@ package CXGN::Trial::TrialLayoutSearch;
 
 =head1 NAME
 
-CXGN::Trial::TrialLayout - Module to get layout information about a trial
+CXGN::Trial::TrialLayoutSearch - Module to get layout information about one or more trials
 
 =head1 SYNOPSIS
 
-        This object has been converted to a factory object that will produce different classes
-        based on the experiment_type. The usage has been kept the same for backwards compatibility,
-        but a cleaner factory object implementation should be adopted in the future.
+    This object has been converted to a factory object that will produce different classes
+    based on the experiment_type. The usage has been kept the same for backwards compatibility,
+    but a cleaner factory object implementation should be adopted in the future.
+
+    It works similar to Phenotypes search but it doesn't merge with phenotypes table. Phenotypes table
+    will be joined only when IncludeObservations parameter is passed by as 1;
 
     my $trial_layout = CXGN::Trial::TrialLayout->new({
         schema => $schema,
-        trial_id => $trial_id,
-        experiment_type => $experiment_type #Either 'field_layout' or 'genotyping_layout'
+        data_level=>$data_level->[0],
+        trial_list=>$study_ids_arrayref,
+        include_observations=>$include_observations,
+        location_list=>$location_ids_arrayref,
+        accession_list=>$accession_ids_arrayref,
+        folder_list=>$folder_ids_arrayref,
+        program_list=>$program_ids_arrayref,
+        observation_unit_id_list=>$observation_unit_db_id,
+        observation_unit_names_list=>$observation_unit_names_list,
+        xref_id_list=>$reference_ids_arrayref,
+        xref_source_list=>$reference_sources_arrayref,
     });
-    my $tl = $trial_layout->get_design();
-
-    This module handles both retrieval of field_layout and genotyping_layout experiments.
-
-    If experiment_type is field_layout, get_design returns a hash representing
-    all the plots, with their design info and plant info and samples info. The return is
-    a HashRef of HashRef where the keys are the plot_number such as:
-
-    {
-        '1001' => {
-            "plot_name" => "plot1",
-            "plot_number" => 1001,
-            "plot_id" => 1234,
-            "accession_name" => "accession1",
-            "accession_id" => 2345,
-            "block_number" => 1,
-            "row_number" => 2,
-            "col_number" => 3,
-            "rep_number" => 1,
-            "is_a_control" => 1,
-            "seedlot_name" => "seedlot1",
-            "seedlot_stock_id" => 3456,
-            "num_seed_per_plot" => 12,
-            "seed_transaction_operator" => "janedoe",
-            "plant_names" => ["plant1", "plant2"],
-            "plant_ids" => [3456, 3457],
-            "plot_geo_json" => {}
-        }
-    }
-
-    If experiment_type is genotyping_layout, get_design returns a hash representing
-    all wells in a plate, with the tissue_sample name in each well and its accession.
-    The return is a HashRef of HashRef where the keys are the well number such as:
-
-    {
-        'A01' => {
-            "plot_name" => "mytissuesample_A01",
-            "stock_name" => "accession1",
-            "plot_number" => "A01",
-            "row_number" => "A",
-            "col_number" => "1",
-            "is_blank" => 0,
-            "concentration" => "2",
-            "volume" => "4",
-            "dna_person" => "nmorales",
-            "acquisition_date" => "2018/01/09",
-            "tissue_type" => "leaf",
-            "extraction" => "ctab",
-            "ncbi_taxonomy_id" => "1001",
-            "source_observation_unit_name" => "plant1",
-            "source_observation_unit_id" => "9091"
-        }
-    }
-
-    By using get_design(), this module attempts to get the design from a
-    projectprop called 'trial_layout_json', but if it cannot be found it calls
-    generate_and_cache_layout() to generate the design, return it, and store it
-    using that projectprop.
-
-    --------------------------------------------------------------------------
-
-    This can also be used the verify that a layout has a physical map covering all
-    plots, as well as verifying that the relationships between entities are valid
-    by doing:
-
-    my $trial_layout = CXGN::Trial::TrialLayout->new({
-        schema => $schema,
-        trial_id => $c->stash->{trial_id},
-        experiment_type => $experiment_type, #Either 'field_layout' or 'genotyping_layout'
-        verify_layout=>1,
-        verify_physical_map=>1
-    });
-    my $trial_errors = $trial_layout->generate_and_cache_layout();
-
-    If there are errors, $trial_errors is a HashRef like:
-
-    {
-        "errors" =>
-            {
-                "layout_errors" => [ "the accession between plot 1 and seedlot 1 is out of sync", "the accession between plot 1 and plant 1 is out of sync" ],
-                "seedlot_errors" => [ "plot1 does not have a seedlot linked" ],
-                "physical_map_errors" => [ "plot 1 does not have a row_number", "plot 1 does not have a col_number"]
-            }
-    }
 
 =head1 DESCRIPTION
 
@@ -116,7 +44,6 @@ use Try::Tiny;
 use Data::Dumper;
 use SGN::Model::Cvterm;
 use CXGN::Stock::StockLookup;
-use CXGN::Trial::TrialLayout;
 use CXGN::Calendar;
 use JSON;
 
@@ -152,7 +79,7 @@ has 'accession_list' => (
     is => 'rw',
 );
 
-has 'plot_list' => (
+has 'observation_unit_id_list' => (
     isa => 'ArrayRef[Int]|Undef',
     is => 'rw',
 );
@@ -259,8 +186,6 @@ sub search {
     }
 
     if ($self->observation_unit_names_list && scalar(@{$self->observation_unit_names_list})>0) {
-        # my $plot_names_sql = _sql_from_arrayref($self->observation_unit_names_list);
-        # push @where_clause, "observationunit.uniquename in ($plot_names_sql)";
         foreach (@{$self->observation_unit_names_list}) {
             push @where_clause, " observationunit.uniquename ilike '".($_)."'";
         }
@@ -268,8 +193,8 @@ sub search {
 
 
 
-    if ($self->plot_list && scalar(@{$self->plot_list})>0) {
-        my $plot_sql = _sql_from_arrayref($self->plot_list);
+    if ($self->observation_unit_id_list && scalar(@{$self->observation_unit_id_list})>0) {
+        my $plot_sql = _sql_from_arrayref($self->observation_unit_id_list);
         push @where_clause, "observationunit.stock_id in ($plot_sql)";
     }
 
