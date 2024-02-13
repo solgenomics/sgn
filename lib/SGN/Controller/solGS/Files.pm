2e5c3004db1fc1795a88f51f356f5deb30b4bcb6
Isaak <iyt2@cornell.edu>
2024-02-13 13:48:05 +0100

    fix gebvs file id

--- lib/SGN/Controller/solGS/Files.pm
+++ lib/SGN/Controller/solGS/Files.pm
@@ -617,15 +617,12 @@ sub gebvs_file_id {
     if ($type =~ /selection/) 
     {
         $identifier = "${identifier}-${selection_pop_id}";
+        my $sel_pop_protocol_id = $c->stash->{selection_pop_genotyping_protocol_id};
+        $protocol_id .= '-' . $sel_pop_protocol_id if $sel_pop_protocol_id;
     }
 
     $c->controller('solGS::Trait')->get_trait_details($c, $trait_id);
     my $trait_abbr  = $c->stash->{trait_abbr};
-
-    my $protocol_id = $c->stash->{genotyping_protocol_id};
-    my $sel_pop_protocol_id = $c->stash->{selection_pop_genotyping_protocol_id};
-    $protocol_id .= '-' . $sel_pop_protocol_id if $sel_pop_protocol_id;
-    my $file_id = "${training_pop_id}_${selection_pop_id}-${trait_abbr}-GP-${protocol_id}";
     my $file_id = "${identifier}-${trait_abbr}-GP-${protocol_id}";
 
     return $file_id;
