31e7c8ff9db7a7baafb7b67eb67f316908606e08
Lukas Mueller <lam87@cornell.edu>
2024-03-21 23:08:12 -0400

    add pod to replace_accessions_in_vcf.pl script.

--- bin/replace_accessions_in_vcf.pl
+++ bin/replace_accessions_in_vcf.pl
@@ -1,4 +1,24 @@
 
+=head1 NAME
+
+replace_accessions_in_vcf.pl - a script to replace the individual names in a vcf file
+
+=head1 SYNOPSYS
+
+perl replace_accessions_in_vcf.pl vcf_file.vcf equivalence_file.tab
+
+The equivalence file has the following columns:
+
+original_id new_id
+
+The original id should be in the file and will be replaced with the new_id.
+
+=head1 AUTHOR
+
+Original script by Isaak Tecle, POD by Lukas Mueller and slight changes to input file structure (please take note!).
+
+=cut
+
 use strict;
 
 use Hash::Case::Preserve;
@@ -13,7 +33,9 @@ tie my %ids, 'Hash::Case::Preserve';
 while (<$E>) {
     chomp;
 
-    my ($original_id, $modified_id, $new_id, $match_type, $stock_type) = split /\t/;
+    ###my ($original_id, $modified_id, $new_id, $match_type, $stock_type) = split /\t/;
+    my ($original_id, $new_id) = split /\t/;
+    
     $ids{$original_id} = $new_id;
 }
 
