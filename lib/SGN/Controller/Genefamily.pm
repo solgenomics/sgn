e2855b9dc262373726a0218d58d999d35daf4bf6
Lukas Mueller <lam87@cornell.edu>
2024-03-24 17:21:39 -0400

    remove login requirement.

--- lib/SGN/Controller/Genefamily.pm
+++ lib/SGN/Controller/Genefamily.pm
@@ -19,8 +19,8 @@ sub search : Path('/tools/genefamily/search') Args(0) {
     my $self = shift;
     my $c = shift;
 
-    if ($c->user()) { 
-	if (grep(/curator|genefamily_editor/, $c->user->get_object()->get_roles() )) { 
+#    if ($c->user()) { 
+#	if (grep(/curator|genefamily_editor/, $c->user->get_object()->get_roles() )) { 
 	    
 	    $c->stash->{genefamily_id} = $c->req->param("genefamily_id") || '';
 	    $c->stash->{build} = $c->req->param("build") || '';
@@ -28,17 +28,17 @@ sub search : Path('/tools/genefamily/search') Args(0) {
 	    $c->stash->{action} = $c->req->param("action") || '';
 	    
 	    $c->stash->{template} = '/tools/genefamily/search.mas';
-	}
-	else {
-	    $c->stash->{message} = "You do not have the necessary privileges to access this page.";
-	    $c->stash->{template} = "/generic_message.mas";
-	}
+#	}
+#	else {
+#	    $c->stash->{message} = "You do not have the necessary privileges to access this page.";
+#	    $c->stash->{template} = "/generic_message.mas";
+#	}
 
-    }
-    else {
-	$c->stash->{message} = "You need to be logged in to access this page.";
-	$c->stash->{template} = "/generic_message.mas";
-    }
+ #   }
+  #  else {
+#	$c->stash->{message} = "You need to be logged in to access this page.";
+#	$c->stash->{template} = "/generic_message.mas";
+ #   }
 	
 }
 
