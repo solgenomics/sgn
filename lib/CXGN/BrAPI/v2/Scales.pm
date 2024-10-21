4bba04a569170cb8ff4930981b029180b250ce2c
Lukas Mueller <lam87@cornell.edu>
2024-10-21 17:12:28 -0400

    add photo traits to the BrAPI trait export.

--- lib/CXGN/BrAPI/v2/Scales.pm
+++ lib/CXGN/BrAPI/v2/Scales.pm
@@ -432,15 +432,16 @@ sub _format_data_type {
     if ($value) {
         my %formats = (
 	    "categorical" => "Ordinal",
-            "numeric"  => "Numerical",
-            "qualitative"  => "Nominal",
-            "numerical"  => "Numerical",
-            "nominal"  => "Nominal",
+            "numeric" => "Numerical",
+            "qualitative" => "Nominal",
+            "numerical" => "Numerical",
+            "nominal" => "Nominal",
             "code" => "Code",
             "date" => "Date" ,
             "duration" => "Duration",
             "ordinal" => "Ordinal",
-            "text"=> "Text",
+            "text" => "Text",
+	    "photo" => "Photo",
         );
 
         $dataType = $formats{lc $value} ? $formats{lc $value} : $dataType;
