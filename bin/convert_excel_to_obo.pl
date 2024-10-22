241a091947cbbae31076f619b7d45ebcf0739b10
Lukas Mueller <lam87@cornell.edu>
2024-10-21 22:51:42 -0400

    partially implemented script.

--- bin/convert_excel_to_obo.pl
+++ bin/convert_excel_to_obo.pl
@@ -7,7 +7,7 @@ convert_excel_to_obo - a script to convert a spreadsheet based representation of
 
 Based on CXGN::File::Parse, this script can parse tab delimited or Excel formats (xls or xlsx) as follows:
 
-perl convert_excel_to_obo.pl file.xlsx
+perl convert_excel_to_obo.pl -n CO_999 -i file.xlsx -o ontology.obo
 
 =head1 AUTHOR
 
@@ -28,6 +28,10 @@ getopts('n:i:o:h');
 
 my $file = $opt_i;
 
+my $outfile = $file.".obo";
+
+open(my $F, ">", $outfile) || die "Can't open file $outfile\n";
+
 my @col_headers = ("Variable"," Trait name", "Trait class", "Trait description", "Trait synonyms", "Main trait abbreviation", "Entity", "Attribute", "Method Name", "Method class", "Method description", "Method Abbreviation", "Formula", "Scale name", "Scale abbreviation", "Scale class", "Category 1", "Category 2", "Category 3", "Category 4", "Category 5", "Category 6", "Category 7", "Category 8", "Category 9", "Category 10", "Category 11", "Category 12" );
 
 my $parser = CXGN::File::Parse->new( file => $file );
@@ -39,6 +43,20 @@ if ($parsed->{errors}) {
 }
 
 my $data = $parsed->{data};
+
+# get all the trait classes
+#
+my %trait_classes;
+
+foreach my $d (@$data) {
+    $trait_classes{$d->{Trait class}}->{count}++;
+}
+
+my $root_id = 0;
+my $count = $root_id;
+my $acc = sprintf "%07d", $count; # the number after the ontology name and a colon
+
+print STDERR "Starting at term $opt_n:$acc ...\n";
 
 # write header
 #
@@ -58,57 +76,116 @@ my $header = <$F>;
 print <<TERM;
 
 [Term]
-id: ROOT
+id: $acc
 name: ROOT
 
 TERM
 
-my %trait_classes;
 
+$count++;
 
+foreach my $k (keys %trait_classes) {
 
+    print format_trait(
+	$opt_n,
+	$count,
+	$k,
+	undef,
+	undef,
+	$opt_n,
+	$root_id,
+	);
 
-
- 
-
+    $trait_classes{$k}->{acc} = $count;
+    $trait_classes{$k}->{name} = $k;
     
-#    my ($curation, $variable_id, $variable_name, $variable_synonyms, $context_of_use, $growth_stage, $variable_status, $variable_xref, $institution, $scientist, $date, $language, $crop, $trait_id, $trait_name, $trait_class, $trait_description, $trait_synonyms, $main_trait_abbr, $alternative_trait_abbr, $entity, $attribute, $trait_status, $trait_xref, $method_id, $method_name, $method_class, $method_description, $formula, $method_reference, $scale_id, $scale_name, $scale_class, $decimal_places, $lower_limit, $upper_limit, $scale_xref, @categories)  = split /\t/;
+    $count++;
+    
+}
+
 
 foreach my $d (@$data) {
-    print STDERR "TRAIT CLASS = $trait_class\n";
-    $trait_classes{$trait_class}++;
-
-
-
+    print format_trait(
+	$ontology_code,
+	$count,
+	$d->{Trait name},
+	$d->{Trait description},
+	$d->{Trait synonyms},
+	$trait_classes{ $d->{Trait class} }->{id},
+	$trait_classes{ $d->{Trait class} }->{name},
+	);
     
-    print <<TERM;
-[Term]
-id: $trait_id
-name: $trait_name
-def: $trait_description
-is_a: $trait_class ! $trait_class
+    $count++;
 
+    print format_variable(
+	$ontology_code,
+	$count,
+	$d->{Variable},
+	$d->{Trait description},
+	$d->{Method name},
+	$d->{Entity},
+	$d->{Attribute}
+	);
+
+    $count++;
+	
+}
+
+
+
+sub format_ontology_id {
+    my $ontology_name = shift;
+    my $acc = shift;
+    
+    return $ontology_name.":".sprintf "%07d", $acc;
+}
+
+sub format_trait {
+    my $ontology_code = shift;
+    my $id = shift;
+    my $name = shift;
+    my $description = shift;
+    my $synonym = shift;
+    my $parent_class_id = shift;
+    my $parent_trait = shift;
+    
+    my $trait_id = format_ontology_id($ontology_code, $id);
+    my $parent_trait_id = format_ontology_id($ontology_code, $parent_class_id);
+
+    return <<TERM;
+    
 [Term]
-id: $variable_id
-name: $variable_name
-def: $trait_description $method_name $scale_name
-relationship: variable_of $trait_id ! $trait_name
+id: $ontology_code:$id
+name: $name
+def: "$description" []
+synonyms: $synonyms
+is_a: $parent_trait_id ! $parent_trait
 
 TERM
 
 }
 
 
-foreach my $k (keys %trait_classes) {
-
-    print <<TERM;
-
+sub format_variable {
+    my $ontology_code = shift;
+    my $id = shift;
+    my $name = shift;
+    my $description = shift;
+    my $method_name = shift;
+    my $scale_name = shift;
+    my $synonym = shift;
+    my $relationship = shift;
+    my $parent_trait_id = shift;
+    my $parent_trait_name = shift;
+    
+    return <<TERM;
+    
 [Term]
-id: $k
-name: $k
-is_a: ROOT ! ROOT
+id: $ontology_code:$id
+name: $name	
+def: "$description $method_name $scale_name" []
+relationship: variable_of $parent_trait_id ! $parent_trait_name
 
 TERM
-
 
 }
