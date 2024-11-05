4a7ffd921a9eb940e4c096fb4db3347019144415
Sri-2023 <sk2783@cornell.edu>
2024-11-05 16:50:01 -0500

    handle collect_date in Native search

--- lib/CXGN/Phenotypes/Search/Native.pm
+++ lib/CXGN/Phenotypes/Search/Native.pm
@@ -226,6 +226,8 @@ sub search {
     my $using_layout_hash;
     #For performance reasons the number of joins to stock can be reduced if a trial is given. If trial(s) given, use the cached layout from TrialLayout instead.
 
+    print STDERR "start date here: ".$self->start_date()." and the end date here: ".$self->end_date()."\n";
+
     if ($self->trial_list && scalar(@{$self->trial_list})>0) {
 
         $using_layout_hash = 1;
@@ -399,7 +401,7 @@ sub search {
     my $datelessq = "";
 
     if ($self->include_dateless_items()) {
-	$datelessq = " phenotype.create_date IS NULL OR ";
+	$datelessq = " phenotype.collect_date IS NULL OR ";
     }
 
     my ($start_date, $end_date);
@@ -407,13 +409,16 @@ sub search {
 	$start_date = $1;
     }
 
-    if ($self->end_date() =~ m/\d{4}\-\d{2}\-\d{2}/ ) {
+    if ($self->end_date() =~ m/(\d{4}\-\d{2}\-\d{2})/) {
 	$end_date = $1;
     }
 
-    if ($start_date && $end_date) {
-	push @where_clause, " ( $datelessq ( phenotype.create_date > $start_date and phenotype.create_date < $end_date ) ) ";
+    #print STDERR "the start date here: $start_date. And the end date here: $end_date\n";
 
+    if ($start_date && $end_date) {
+        #print STDERR "including the date query\n";
+	    push @where_clause, " ( $datelessq ( phenotype.collect_date >= '$start_date'::date and phenotype.collect_date <= '$end_date'::date ) ) ";
+        #push @where_clause, " ( $datelessq ( phenotype.collect_date >= $start_date and phenotype.collect_date <= $end_date ) ) ";
     }
 
 
