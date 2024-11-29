f42a5f60e6352ea507e22933ab4cccff1a471efa
Isaak <iyt2@cornell.edu>
2024-11-29 11:05:11 +0100

    reuse analysisSave methods

--- js/source/legacy/solGS/pcaSave.js
+++ js/source/legacy/solGS/pcaSave.js
@@ -1,54 +1,21 @@
 /*
- *Saves solgs modeling output
+ *Saves pca scores
  * Isaak Y Tecle <iyt2@cornell.edu>
  *
+ * requires solGS.analysisSave
  */
 
-// var solGS = solGS || function solGS() {};
-
 solGS.pca.save = {
-  checkStoredAnalysis: function (args) {
-    args = JSON.stringify(args);
-    var stored = jQuery.ajax({
-      dataType: "json",
-      type: "POST",
-      data: {'arguments': args},
-      url: "/solgs/check/stored/analysis/",
-    });
-
-    return stored;
-  },
-
-  getResultDetails: function (args) {
-    var args = JSON.stringify(args);
-    var details = jQuery.ajax({
-      dataType: "json",
-      type: "POST",
-      data: {'arguments': args},
-      url: "/solgs/analysis/result/details",
-    });
-
-    return details;
-  },
-
-  savePcaScores: function (args) {
-    var save = jQuery.ajax({
-      dataType: "json",
-      type: "POST",
-      data: args,
-      url: "/ajax/analysis/store/json",
-    });
-
-    return save;
-  },
-
-  checkUserStatus: function () {
-    return jQuery.ajax({
-      type: "POST",
-      dataType: "json",
-      url: "/solgs/check/user/login/",
-    });
-  },
+ savePcaScores: function (args) {
+        var saveReq = jQuery.ajax({
+          dataType: "json",
+          type: "POST",
+          data: args,
+          url: "/ajax/analysis/store/json",
+        });
+    
+        return saveReq;
+      },
 };
 
 jQuery(document).ready(function () {
@@ -56,25 +23,28 @@ jQuery(document).ready(function () {
     var elemId = e.target.id;
     if(elemId.match(/save_pcs_btn/)) {
         console.log(`saving pcs....elemId: ${elemId}`)
+        var savePcaBtnId = `#${elemId}`;
         var pcaArgs = solGS.pca.getSelectedPopPcaArgs(elemId);
         console.log(`saving pcs....elemId: ${elemId} -- pcaArgs: ${JSON.stringify(pcaArgs)}`)
         var pcaFileId = elemId.replace("save_pcs_btn_", '');
         console.log(`saving pcs....pcaFileId: ${pcaFileId}`)
         var pcaSaveMsgDiv = `#${pcaFileId}_pca_save_message`;
 
-        solGS.pca.save.checkStoredAnalysis(pcaArgs).done(function (res) {
+        solGS.save.checkStoredAnalysis(pcaArgs).done(function (res) {
             console.log(`checkStoredAnalysis res: ${JSON.stringify(res)}`)
+            console.log(`checkStoredAnalysis res.analysis_id: ${res.analysis_id}`)
+
             if (res.analysis_id) {
                 jQuery("#save_pcs_btn").hide();
                 var link = '<a href="/analyses/' + res.analysis_id + '">View stored PCs</a>';
-                jQuery("#download_").html(link);
+                jQuery(savePcaBtnId).html(link);
             } else {
                 jQuery(pcaSaveMsgDiv)
                 .html("Please wait...saving the PCs may take a few minutes.")
                 .show();
                 jQuery(`#${elemId}`).hide();
 
-                solGS.pca.save.checkUserStatus().done(function (res) {
+                solGS.save.checkUserStatus().done(function (res) {
                     console.log(`saving pcs....checkUserStatus loggedIn: ${res.loggedin}`)
                     if (!res.loggedin) {
                         solGS.submitJob.loginAlert();
@@ -83,7 +53,7 @@ jQuery(document).ready(function () {
                     solGS.alertMessage("Error occured checking for user status");
                 });
 
-                solGS.pca.save.getResultDetails(pcaArgs).done(function (res) {
+                solGS.save.getResultDetails(pcaArgs).done(function (res) {
                     console.log(`getResultDetails: res.analysis_details -- ${JSON.stringify(res.analysis_details)} `)
                     if (res.error) {
                         jQuery("#download_pca_output .multi-spinner-container").hide();
