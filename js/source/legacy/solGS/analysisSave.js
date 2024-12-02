25ce791e6dcf96b232f388a9628e17497d6d42cc
Isaak <iyt2@cornell.edu>
2024-12-02 15:55:44 +0100

    move gebvs saving code into gebvsSave.js

--- js/source/legacy/solGS/analysisSave.js
+++ js/source/legacy/solGS/analysisSave.js
@@ -1,5 +1,5 @@
 /*
- *Saves solgs modeling output
+ *Saves solGS modeling and other analysis output
  * Isaak Y Tecle <iyt2@cornell.edu>
  *
  */
@@ -12,7 +12,7 @@ solGS.save = {
     var stored = jQuery.ajax({
       dataType: "json",
       type: "POST",
-      data: {'arguments': args},
+      data: {'arguments': analysisArgs},
       url: "/solgs/check/stored/analysis/",
     });
 
@@ -64,67 +64,3 @@ solGS.save = {
   },
 };
 
-jQuery(document).ready(function () {
-  var analysisArgs = solGS.save.saveGebvsArgs();
-  solGS.save.checkStoredAnalysis(analysisArgs).done(function (res) {
-    if (res.analysis_id) {
-      jQuery("#save_gebvs").hide();
-      var link = '<a href="/analyses/' + res.analysis_id + '">View stored GEBVs</a>';
-      jQuery("#gebvs_output").append(link);
-      }
-  });
-
-  jQuery("#save_gebvs").click(function () {
-    jQuery("#gebvs_output .multi-spinner-container").show();
-    jQuery("#gebvs_save_message")
-      .html("Please wait...saving the GEBVs may take a few minutes.")
-      .show();
-    jQuery("#save_gebvs").hide();
-
-    solGS.save.checkUserStatus().done(function (res) {
-      if (!res.loggedin) {
-        solGS.submitJob.loginAlert();
-      } else {
-      }
-    });
-
-    solGS.save.checkUserStatus().fail(function () {
-      solGS.alertMessage("Error occured checking for user status");
-    });
-
-    solGS.save.getResultDetails(analysisArgs).done(function (res) {
-
-      if (res.error) {
-        jQuery("#gebvs_output .multi-spinner-container").hide();
-        jQuery("#gebvs_save_message")
-          .html(res.error + ". The logged info may not exist for the result.")
-          .show()
-          .fadeOut(50000);
-
-        jQuery("#save_gebvs").show();
-      } else {
-        var save = solGS.save.storeAnalysisResults(res.analysis_details);
-
-        save.done(function (res) {
-          jQuery("#gebvs_output .multi-spinner-container").hide();
-          if (res.error) {
-            jQuery("#gebvs_save_message").html(res.error).show().fadeOut(50000);
-
-            jQuery("#save_gebvs").show();
-          } else {
-            jQuery("#gebvs_save_message").hide();
-
-            var link = '<a href="/analyses/' + res.analysis_id + '">View stored GEBVs</a>';
-            jQuery("#gebvs_output").append(link);
-          }
-        });
-
-        save.fail(function (res) {
-          jQuery("#gebvs_output .multi-spinner-container").hide();
-          jQuery("#save_gebvs").show();
-          jQuery("#gebvs_save_message").html(res.error).show().fadeOut(50000);
-        });
-      }
-    });
-  });
-});
