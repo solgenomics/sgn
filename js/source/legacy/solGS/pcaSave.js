48bc51f283ccc1ebffddc3a62ed7198a7c33f5e9
Isaak <iyt2@cornell.edu>
2024-12-03 13:54:36 +0100

    use getPcaAnalysisArgs method

--- js/source/legacy/solGS/pcaSave.js
+++ js/source/legacy/solGS/pcaSave.js
@@ -9,18 +9,13 @@ jQuery(document).ready(function () {
   jQuery('body').on("click", ".download_pca_output", function (e) {
     var elemId = e.target.id;
     if(elemId.match(/save_pcs_btn/)) {
-        console.log(`saving pcs....elemId: ${elemId}`)
+        var pcaArgs = solGS.pca.getPcaAnalysisArgs(elemId);
+
         var savePcaBtnId = `#${elemId}`;
-        var pcaArgs = solGS.pca.getSelectedPopPcaArgs(elemId);
-        console.log(`saving pcs....elemId: ${elemId} -- pcaArgs: ${JSON.stringify(pcaArgs)}`)
         var pcaFileId = elemId.replace("save_pcs_btn_", '');
-        console.log(`saving pcs....pcaFileId: ${pcaFileId}`)
         var pcaSaveMsgDiv = `#${pcaFileId}_pca_save_message`;
 
         solGS.save.checkStoredAnalysis(pcaArgs).done(function (res) {
-            console.log(`checkStoredAnalysis res: ${JSON.stringify(res)}`)
-            console.log(`checkStoredAnalysis res.analysis_id: ${res.analysis_id}`)
-
             if (res.analysis_id) {
                 jQuery("#save_pcs_btn").hide();
                 var link = '<a href="/analyses/' + res.analysis_id + '">View stored PCs</a>';
@@ -32,7 +27,6 @@ jQuery(document).ready(function () {
                 jQuery(`#${elemId}`).hide();
 
                 solGS.save.checkUserStatus().done(function (res) {
-                    console.log(`saving pcs....checkUserStatus loggedIn: ${res.loggedin}`)
                     if (!res.loggedin) {
                         solGS.submitJob.loginAlert();
                     } 
@@ -41,7 +35,6 @@ jQuery(document).ready(function () {
                 });
 
                 solGS.save.getResultDetails(pcaArgs).done(function (res) {
-                    console.log(`getResultDetails: res.analysis_details -- ${JSON.stringify(res.analysis_details)} `)
                     if (res.error) {
                         jQuery("#download_pca_output .multi-spinner-container").hide();
                         jQuery(pcaSaveMsgDiv)
@@ -51,8 +44,6 @@ jQuery(document).ready(function () {
                         jQuery(`#${elemId}`).show();
                     } else {
                         solGS.save.storeAnalysisResults(res.analysis_details).done(function (res) {
-                            console.log(`savePcaScores: res -- ${JSON.stringify(res)} `)
-
                             jQuery("#download_pca_output .multi-spinner-container").hide();
                             if (res.error) {
                                 jQuery(pcaSaveMsgDiv).html(res.error).show().fadeOut(50000);
