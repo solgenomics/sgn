3efada4bacd0b333685bfc5538fd2a3fef337deb
Isaak <iyt2@cornell.edu>
2024-12-13 16:04:15 +0100

    move gebvs save logic to gebvsSave controller

--- lib/SGN/Controller/solGS/AnalysisSave.pm
+++ lib/SGN/Controller/solGS/AnalysisSave.pm
@@ -41,80 +41,6 @@ sub check_analysis_result :Path('/solgs/check/stored/analysis/') Args() {
 		
 }
 
-
-sub result_details :Path('/solgs/analysis/result/details') Args() {
-    my ($self, $c) = @_;
-
-	my $args = $c->req->param('arguments');
-    $c->controller('solGS::Utils')->stash_json_args($c, $args);
-
-	my $stored = $self->check_stored_analysis($c);
-
-	if (!$stored) {
-		my $params = decode_json($args);
-		my $analysis_details;
-
-		eval {
-			if ($c->stash->{analysis_type} =~ /pca/) {
-				$analysis_details = $self->structure_pca_result_details($c);
-			} else {
-				$analysis_details = $self->structure_gebvs_result_details($c, $params);
-			}
-		};
-
-		if ($@) {
-			print STDERR "\n$@\n";
-			$c->stash->{rest}{error} = 'Something went wrong structuring the analysis result';
-		} else {
-			$c->stash->{rest}{analysis_details} = $analysis_details;
-		}
-	}
-
-}
-
-
-sub structure_gebvs_result_details {
-	my ($self, $c, $params) = @_;
-
-	my $gebvs = $self->structure_gebvs_values($c, $params);
-	my @accessions = keys %$gebvs;
-
-	my $trait_names		= $self->analysis_traits($c);
-	my $model_details   = $self->model_details($c);
-	my $app_details		= $self->app_details();
-	my $log			    = $self->get_analysis_job_info($c);
-	my $analysis_name   = $log->{analysis_name};
-
-    my $details = {
-		'analysis_to_save_boolean' => 'yes',
-		'analysis_name' => $log->{analysis_name},
-		'analysis_description' => $log->{training_pop_desc},
-		'analysis_year' => $self->analysis_year($c),
-		'analysis_breeding_program_id' => $self->analysis_breeding_prog($c),
-		'analysis_protocol' => $model_details->{protocol},
-		'analysis_dataset_id' => $log->{dataset_id},
-		'analysis_accession_names' => encode_json(\@accessions),
-		'analysis_trait_names' => encode_json($trait_names),
-		'analysis_precomputed_design_optional' =>'',
-		'analysis_result_values' => to_json($gebvs),
-		'analysis_result_values_type' => 'analysis_result_values_match_accession_names',
-		'analysis_result_summary' => '',
-		'analysis_result_trait_compose_info' =>  "",
-		'analysis_statistical_ontology_term' =>  $model_details->{stat_ont_term},
-		'analysis_model_application_version' => $app_details->{version},
-		'analysis_model_application_name' => $app_details->{name},
-		'analysis_model_language' => $model_details->{model_lang},
-		'analysis_model_is_public' => 'yes',
-		'analysis_model_description' =>  $model_details->{model_desc},
-		'analysis_model_name' => $log->{analysis_name},
-		'analysis_model_type' => $model_details->{model_type},
-	};
-
-	return $details;
-
-}
-
-
 sub app_details {
 	my $self = shift;
 
@@ -185,52 +111,6 @@ sub analysis_breeding_prog {
 
 }
 
-sub pca_details {
-	my ($self, $c) = @_;
-
-	my $model_type = 'prcomp';
-	my $stat_ont_term = 'Principal component analysis using prcomp from base R package|SGNSTAT:0000043';
-	my $protocol = "prcomp method from base R Package";
-	my $log = $self->get_analysis_job_info($c);
-	my $model_page = $log->{analysis_page};
-	my $model_desc = qq | <a href="$model_page">Go to PCA detail page</a>|;
-
-	my $details = {
-		'model_type' => $model_type,
-		'model_page' => $model_page,
-		'model_desc' => $model_desc,
-		'model_lang' => 'R',
-		'stat_ont_term' => $stat_ont_term,
-		'protocol' => $protocol
-	};
-
-	return $details;
-}
-
-sub model_details {
-	my ($self, $c) = @_;
-
-	my $model_type = 'gblup_model_rrblup';
-	my $stat_ont_term = 'GEBVs using GBLUP from rrblup R package|SGNSTAT:0000038';
-	my $protocol = "GBLUP model from RRBLUP R Package";
-	my $log = $self->get_analysis_job_info($c);
-	my $model_page = $log->{analysis_page};
-	my $model_desc= qq | <a href="$model_page">Go to model detail page</a>|;
-	#my $model_desc = 'test desc';
-
-	my $details = {
-		'model_type' => $model_type,
-		'model_page' => $model_page,
-		'model_desc' => $model_desc,
-		'model_lang' => 'R',
-		'stat_ont_term' => $stat_ont_term,
-		'protocol' => $protocol
-	};
-
-	return $details;
-}
-
-
 sub analysis_year {
 	my ($self, $c) = @_;
 
@@ -243,7 +123,6 @@ sub analysis_year {
 	return $year;
 
 }
-
 
 sub check_stored_analysis {
 	my ($self, $c) = @_;
@@ -280,177 +159,6 @@ sub extended_trait_name {
 
 }
 
-
-sub gebvs_values {
-	my ($self, $c, $params) = @_;
-
-	my $training_pop_id = $params->{training_pop_id};
-	my $selection_pop_id = $params->{selection_pop_id};
-	my $trait_id = $params->{trait_id};
-	my $protocol_id = $params->{genotyping_protocol_id};
-
-	$c->stash->{genotyping_protocol_id} = $protocol_id;
-
-	my $analysis_page = $params->{analysis_page}; 
-	$analysis_page = $c->controller('solGS::Path')->page_type($c, $analysis_page);
-
-	my $gebvs_file;
-	if ($analysis_page =~ /training_model/) {
-		$gebvs_file = $c->controller('solGS::Files')->rrblup_training_gebvs_file($c, $training_pop_id, $trait_id);
-	}
-	elsif ($analysis_page =~ /selection_prediction/) {
-		$gebvs_file = $c->controller('solGS::Files')->rrblup_selection_gebvs_file($c, $training_pop_id, $selection_pop_id, $trait_id);
-	}
-
-	my $gebvs = $c->controller('solGS::Utils')->read_file_data($gebvs_file);
-
-	return $gebvs;
-
-}
-
-
-sub structure_gebvs_values {
-	my ($self, $c, $params) = @_;
-
-	my $trait_name = $self->extended_trait_name($c, $params->{trait_id});
-
-	my $gebvs = $self->gebvs_values($c, $params);
-	my $gebvs_ref = $c->controller('solGS::Utils')->convert_arrayref_to_hashref($gebvs);
-
-	my %gebvs_hash;
-	my $now = DateTime->now();
-	my $timestamp = $now->ymd()."T".$now->hms();
-
-	my $user = $c->controller('solGS::AnalysisQueue')->get_user_detail($c);
-	my $user_name = $user->{user_name};
-
-	my @accessions = keys %$gebvs_ref;
-
-	foreach my $accession (@accessions) {
-		$gebvs_hash{$accession} = {
-			$trait_name => [$gebvs_ref->{$accession}, $timestamp, $user_name, "", ""]
-		};
-	}
-
-	return \%gebvs_hash;
-
-}
-
-
-sub get_pca_scores {
-	my ($self, $c) = @_;
-
-    $c->controller('solGS::pca')->pca_scores_file($c);
-	my $scores_file     = $c->stash->{pca_scores_file};
-	my @cols = $c->controller('solGS::Utils')->get_data_col_headers($scores_file);
-	my @pc_cols = grep(/PC/i, @cols);
-
-	my $include_rownames = 'TRUE';
-	my $scores = $c->controller('solGS::Utils')->read_file_data_cols($scores_file, \@pc_cols, $include_rownames);
-	
-	#remove the first array with col headers
-	shift(@$scores);
-
-	return $scores;
-
-}
-
-
-sub structure_pca_scores {
-	my ($self, $c) = @_;
-
-	my $pcs_names = $self->get_pcs_names($c);
-	my $scores = $self->get_pca_scores($c);
-	my $scores_ref = $c->controller('solGS::Utils')->convert_arrayref_to_hashref($scores);
-	
-	my %scores_hash;
-	my $now = DateTime->now();
-	my $timestamp = $now->ymd()."T".$now->hms();
-
-	my $user = $c->controller('solGS::AnalysisQueue')->get_user_detail($c);
-	my $user_name = $user->{user_name};
-
-	my @accessions = keys %$scores_ref;
-	
-		foreach my $accession (@accessions) { 
-			for (my $i=0;  $i < scalar(@$pcs_names);  $i++) {
-				my $pc = $pcs_names->[$i];
-		
-				$scores_hash{$accession}{$pc} = 
-					[$scores_ref->{$accession}->[$i], $timestamp, $user_name, "", ""];
-			}
-		}
-
-	return \%scores_hash;
-
-}
-
-sub get_pcs_names {
-	my ($self, $c) = @_;
-
-	$c->controller('solGS::pca')->pca_scores_file($c);
-	my $scores_file     = $c->stash->{pca_scores_file};
-	my @cols = $c->controller('solGS::Utils')->get_data_col_headers($scores_file);
-	my @pc_cols = grep(/PC/i, @cols);
-
-	my $cv_name = "SGNStatistics_ontology";
-	my $schema = $self->schema($c);
-	my @extended_pc_names;
-
-	foreach my $pc (@pc_cols) {
-		my ($num) = $pc =~ /(\d+)/;
-		my $cvterm_name = 'Principal component '   . $num . ' (PC' . $num . ') scores';
-		my $cvterm_id = SGN::Model::Cvterm->get_cvterm_row($schema, $cvterm_name, $cv_name)->cvterm_id();
-		my $extended_name = SGN::Model::Cvterm::get_trait_from_cvterm_id($schema, $cvterm_id, 'extended');
-		
-		push @extended_pc_names, $extended_name;
-	}
-
-	return \@extended_pc_names;
-
-}
-
-sub structure_pca_result_details {
-	my ($self, $c) = @_;
-
-	my $scores = $self->structure_pca_scores($c);
-	my @accessions = keys %$scores;
-
-	my $pcs_names		= $self->get_pcs_names($c);
-	my $model_details   = $self->pca_details($c);
-	my $app_details		= $self->app_details();
-	my $log			    = $self->get_analysis_job_info($c);
-	my $analysis_name   = $log->{analysis_name};
-
-    my $details = {
-		'analysis_to_save_boolean' => 'yes',
-		'analysis_name' => $log->{analysis_name},
-		'analysis_description' => $log->{pca_desc} || 'test pca load',
-		'analysis_year' => $self->analysis_year($c),
-		'analysis_breeding_program_id' => $self->analysis_breeding_prog($c),
-		'analysis_protocol' => $model_details->{protocol},
-		'analysis_dataset_id' => $log->{dataset_id},
-		'analysis_accession_names' => encode_json(\@accessions),
-		'analysis_trait_names' => encode_json($pcs_names),
-		'analysis_precomputed_design_optional' =>'',
-		'analysis_result_values' => to_json($scores),
-		'analysis_result_values_type' => 'analysis_result_values_match_accession_names',
-		'analysis_result_summary' => '',
-		'analysis_result_trait_compose_info' =>  "",
-		'analysis_statistical_ontology_term' =>  $model_details->{stat_ont_term},
-		'analysis_model_application_version' => $app_details->{version},
-		'analysis_model_application_name' => $app_details->{name},
-		'analysis_model_language' => $model_details->{model_lang},
-		'analysis_model_is_public' => 'yes',
-		'analysis_model_description' =>  $model_details->{model_desc},
-		'analysis_model_name' => $log->{analysis_name},
-		'analysis_model_type' => $model_details->{model_type},
-	};
-
-	return $details;
-
-}
-
 sub get_analysis_job_info {
 	my ($self, $c) = @_;
 
@@ -477,7 +185,6 @@ sub get_analysis_job_info {
 
 }
 
-
 sub all_users_analyses_logs {
 	my ($self, $c) = @_;
 
@@ -490,13 +197,11 @@ sub all_users_analyses_logs {
 
 }
 
-
 sub schema {
 	my ($self, $c) = @_;
 
 	return  $c->dbic_schema("Bio::Chado::Schema", "sgn_chado");
 }
-
 
 sub begin : Private {
     my ($self, $c) = @_;
