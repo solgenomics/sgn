38a6d699f5c8d01835de1623fa5dee289f197746
Isaak <iyt2@cornell.edu>
2025-03-14 11:37:38 +0100

    add analysis_result to source stock types

--- lib/CXGN/Trial/TrialLayout/Analysis.pm
+++ lib/CXGN/Trial/TrialLayout/Analysis.pm
@@ -11,8 +11,10 @@ extends 'CXGN::Trial::TrialLayout::AbstractLayout';
 
 sub BUILD {
     my $self = shift;
+
+    print STDERR "BUILD CXGN::Trial::TrialLayout::Analysis...\n";
     $self->set_source_primary_stock_types( [ "accession", "analysis_result" ] );
-    $self->set_source_stock_types( [ "accession", "tissue_sample" ] );
+    $self->set_source_stock_types( [ "accession", "analysis_result", "tissue_sample" ] );
     $self->set_relationship_types( [ "analysis_of" ]);
     $self->set_target_stock_types( [ "analysis_instance" ]);
     $self->convert_source_stock_types_to_ids();
