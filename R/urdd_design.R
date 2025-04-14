b5f9a2c18579d51ee89908b0a411dfef24b437c3
chris263 <ccs263@cornell.edu>
2025-04-13 21:36:10 -0400

    fixing other lint warnings

--- R/urdd_design.R
+++ R/urdd_design.R
@@ -1,10 +1,10 @@
-## Script for Un-replicated Diagonal Designs
+## Script for unreplicated diagonal designs
 
 args <- commandArgs(TRUE)
 
 if (length(args) == 0) {
   message("No arguments supplied.")
-  paramfile <- ''
+  param_file <- ""
 } else {
   for (i in seq_along(args)) {
     message(paste("Processing arg", args[[i]]))
@@ -12,34 +12,34 @@ if (length(args) == 0) {
   }
 }
 
-source(paramfile)
+source(param_file)
 
-nStocks <- length(stocks)
-nChecks <- length(controls)
+n_stocks <- length(stocks)
+n_checks <- length(controls)
 
 ## Separating treatments from checks
 treatments <- stocks[!stocks %in% controls]
 
 ## Setting the same number of treatments per block
-if (is.null(nBlocks)) {
-  nInd <- nLines
-  nIndBlock <- nLines
+if (is.null(n_blocks)) {
+  n_ind <- n_lines
+  n_ind_block <- n_lines
 } else {
-  nInd <- nLines / nBlocks
-  nIndBlock <- rep(nInd, nBlocks)
+  n_ind <- n_lines / n_blocks
+  n_ind_block <- rep(n_ind, n_blocks)
 }
 
-nControls <- length(controls)
+n_controls <- length(controls)
 
-typeDesign <- if (nBlocks > 1) "DBUDC" else "SUDC"
-lType <- if (layout == "serpentine") "serpentine" else "cartesian"
+type_design <- if (n_blocks > 1) "DBUDC" else "SUDC"
+layout_type <- if (layout == "serpentine") "serpentine" else "cartesian"
 
 treatment_list <- data.frame(
-  ENTRY = seq_len(nStocks),
-  NAME = c(controls, treatments)
+  entry = seq_len(n_stocks),
+  name = c(controls, treatments)
 )
 
-startPlot <- switch(
+start_plot <- switch(
   as.character(serie),
   "1" = 1,
   "2" = 101,
@@ -48,32 +48,32 @@ startPlot <- switch(
 )
 
 ## Grant the right format
-nRow <- as.integer(nRow)
-nCol <- as.integer(nCol)
-nLines <- as.integer(nLines)
-nChecks <- as.integer(nChecks)
+n_row <- as.integer(n_row)
+n_col <- as.integer(n_col)
+n_lines <- as.integer(n_lines)
+n_checks <- as.integer(n_checks)
 
-basefile <- tools::file_path_sans_ext(paramfile)
+base_file <- tools::file_path_sans_ext(param_file)
 
-## FieldHub Design
+## FieldHub design
 output <- capture.output({
   multi_diag <- FielDHub::diagonal_arrangement(
-    nrows = nRow,
-    ncols = nCol,
-    lines = nLines,
-    planter = lType,
-    plotNumber = startPlot,
-    kindExpt = typeDesign,
-    blocks = nIndBlock,
-    checks = nChecks,
+    nrows = n_row,
+    ncols = n_col,
+    lines = n_lines,
+    planter = layout_type,
+    plotNumber = start_plot,
+    kindExpt = type_design,
+    blocks = n_ind_block,
+    checks = n_checks,
     l = 1,
     data = treatment_list
   )
 })
 
 if (any(grepl("Field dimensions do not fit", output))) {
-  errorFile <- paste0(basefile, ".design.error")
-  writeLines(output, con = errorFile)
+  error_file <- paste0(base_file, ".design.error")
+  writeLines(output, con = error_file)
   quit(status = 1)
 }
 
@@ -86,7 +86,7 @@ field_book$CHECKS[field_book$CHECKS > 0] <- 1
 print(head(field_book, 10))
 
 ## Save result files
-outfile <- paste0(basefile, ".design")
-sink(outfile)
-write.table(field_book, quote = FALSE, sep = '\t', row.names = FALSE)
+out_file <- paste0(base_file, ".design")
+sink(out_file)
+write.table(field_book, quote = FALSE, sep = "\t", row.names = FALSE)
 sink()
