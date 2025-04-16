fd0f5cb879f23565b6a2f0fb0932cda4f7ae121e
Lukas Mueller <lam87@cornell.edu>
2025-04-16 15:59:40 -0400

    trying to get unified javascript for project trees to work.

--- js/source/entries/htmltree.js
+++ js/source/entries/htmltree.js
@@ -4,31 +4,49 @@ import '../legacy/d3/d3Min.js';
 
 var version = '0.01';
 
-export function init(tree_type) {
+export function init(tree_type, hard_refresh) {
+
+    alert("STARTING..."+tree_type+" refresh="+hard_refresh);
+    
+    //init_events(tree_type);
+    // check if we have no last refresh date or hard_refresh is true
+    // fetch the tree from the back end and store it in the browser
+    //
+    alert("HARD REFRESH: "+hard_refresh);
     var last_refresh_date = localStorage.getItem(tree_type+'_last_refresh');
     alert("Last refresh timestamp  :-) = "+last_refresh_date);
 
-    if (last_refresh_date === null) { alert("refresh date is not set"); }
+    if (last_refresh_date === undefined || hard_refresh == 1) {
+	alert("refresh date is not set or hard_refresh is 1");
+	jQuery.ajax({ 
+	    url: '/ajax/breeders/recently_modified_projects',
+	    data: { 'type' : tree_type, 'hard_refresh' : hard_refresh },
+	}).then( function(r) {
+	    alert("NEW TRIALS: "+JSON.stringify(r));
 
-    alert('TREE TYPE: '+tree_type);
-    
-    jQuery.ajax( {
-    	url: '/ajax/breeders/recently_modified_projects',
-	data: { since_date: last_refresh_date, type: tree_type },
-    }).then( function(r) { alert("NEW TRIALS: "+JSON.stringify(r)); if (r.data.length > 0) { get_html_tree(tree_type).then( function(r) { alert("setting new tree"); format_html_tree(r.html, tree_type); } ) } else { alert("here!"); } });
-
-    alert("WHAT???");
-    
-
-    alert('get tree from local storage 2...');
-    var html = localStorage.getItem(tree_type);
-    if (html !== null) {
-	alert("HTML NOW 2: "+html);
-	format_html_tree(html, tree_type);
+	    if (r.data.length > 0) {
+		get_html_tree(tree_type).then( function(r) { alert("setting new tree 1"+JSON.stringify(html)); format_html_tree(html, tree_type); }, alert(r) )
+	    }    
+	});
     }
-
-    alert('Done with local storage...');
-    
+    else if(last_refresh_date !== undefined) { 
+	alert('TREE TYPE: '+tree_type);
+	
+	jQuery.ajax( {
+    	    url: '/ajax/breeders/recently_modified_projects',
+	    data: { since_date: last_refresh_date, type: tree_type },
+	}).then( function(r) { alert("NEW TRIALS: "+JSON.stringify(r)); if (r.data.length > 0) { get_html_tree(tree_type).then( function(r) { alert("setting new tree 2"); format_html_tree(r.html, tree_type); } ); } } );
+	
+	
+	alert('get tree from local storage 2...');
+	var html = localStorage.getItem(tree_type);
+	if (html !== null) {
+	    alert("HTML NOW 2: "+html);
+	    format_html_tree(html, tree_type);
+	}
+	
+	alert('Done with local storage...');
+    }	
     if (html === null) {
 	alert('HTML NOT DEFINED! FETCHING...');
 	get_html_tree(tree_type).then( function(r) { 
@@ -36,14 +54,13 @@ export function init(tree_type) {
 	    format_html_tree(html, tree_type);
 	});
     }
-
     alert('Done with init');
 }
 
 export function init_events(tree_type) { 
     jQuery('#refresh_'+tree_type+'_button').click(function(){
 	alert('hello!');
-	get_trial_tree(tree_type).then( function(r) { alert('now loading new tree'); format_html_tree(r.html, tree_type) });
+	get_html_tree(tree_type).then( function(r) { alert('now loading new tree'); format_html_tree(r.html, tree_type) });
 	
     });
 
@@ -67,7 +84,7 @@ export function init_events(tree_type) {
 
     jQuery('#'+tree_type+'_list').delegate("li", "dblclick", function(event){
       var node = jQuery('#'+tree_type+'_list').jstree("get_node", this);
-      //console.log(node);
+      console.log(node);
       if (node.id.substr(0,1) !== 'j') {
         if (node.type == 'folder') {
             window.open('/folder/'+node.id);
@@ -107,14 +124,27 @@ export function get_timestamp() {
 
     
 export function get_html_tree(tree_type) {
-    alert('get_trial_tree');
+    alert('get_html_tree');
     return jQuery.ajax( {
-	url: '/ajax/breeders/get_trials_with_folders_cached?type='+tree_type,  
+	url: '/ajax/breeders/get_trials_with_folders?type='+tree_type,  
     }).then(  function(r) {
-	alert("adding new html and timestamp to localstorage");
+	alert("adding new html and timestamp to localstorage" + r.html);
 	localStorage.setItem(tree_type, r.html);
 	localStorage.setItem(tree_type+'_last_refresh', get_timestamp());
-    });
+
+	return new Promise((resolve, reject) => {
+	    if (r.html) {
+		alert('RHTML IN GET_HTEML_TREE '+r.html);
+		resolve(r.html);
+	    }
+	    else {
+		alert("REJECTING EVERYTHING!!!!!");
+		reject('an error occurred in get_html_tree');
+	    }
+	}
+			  );
+    }
+	   );
 }
 
 export function format_html_tree(treehtml, tree_type) {
@@ -122,9 +152,12 @@ export function format_html_tree(treehtml, tree_type) {
     var html = '<ul>'+treehtml+'</ul>';
 
     alert('TREE TYPE = '+tree_type);
+
+    alert('html now: '+html);
+
     jQuery('#'+tree_type+'_list').html(html);
     
-    //console.log(html);
+    alert("NEW HTML = "+html);
     jQuery('#'+tree_type+'_list').jstree( {
 	"core": { 'themes': { 'name': 'proton', 'responsive': true}},
 	"valid_children" : [ "folder", "trial", "breeding_program", "analyses", "sampling_trial" ],
@@ -150,8 +183,7 @@ export function format_html_tree(treehtml, tree_type) {
 	},
 	"plugins" : ["html_data","types","search"],
 	
-    });
-    
+    });    
 }
 
 
