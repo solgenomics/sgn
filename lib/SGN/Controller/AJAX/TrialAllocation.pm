fea37adbe08acbf8089700273f8b21c3bdf8dbc1
chris263 <ccs263@cornell.edu>
2025-04-17 09:40:07 -0400

    adding controllers

--- lib/SGN/Controller/AJAX/TrialAllocation.pm
+++ lib/SGN/Controller/AJAX/TrialAllocation.pm
@@ -24,4 +24,41 @@ __PACKAGE__->config(
     stash_key => 'rest',
     map => { 'application/json' => 'JSON' },
    );
+
+
+sub accession_lists :Path('/ajax/trialallocation/accession_lists') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    my $dbh = $c->dbc->dbh;
+    my $schema = $c->dbic_schema('Bio::Chado::Schema');
+    my $sp_person_id = $c->user ? $c->user->get_object->get_sp_person_id : undef;
+
+    if (!$sp_person_id) {
+        $c->stash->{rest} = { error => 'Not logged in' };
+        return;
+    }
+
+    # Get cvterm_id for 'accessions' in 'list_types'
+    my $accession_type_id = $schema->resultset('Cv::Cvterm')->find({ name => 'accessions' })->cvterm_id;
+
+    # Use CXGN::List::all_lists
+    my $lists = CXGN::List::all_lists($dbh, $sp_person_id, 'accessions');
+
+    my @formatted = map {
+        {
+            list_id   => $_->[0],
+            name      => $_->[1],
+            desc      => $_->[2],
+            count     => $_->[3],
+            type_id   => $_->[4],
+            type_name => $_->[5],
+            is_public => $_->[6]
+        }
+    } @$lists;
+
+    $c->stash->{rest} = { success => 1, lists => \@formatted };
+}
+
+
 1;
