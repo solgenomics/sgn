9c4964c1a091144fece09cdfa635c4e4f3e8bb57
chris263 <ccs263@cornell.edu>
2025-04-21 00:05:08 -0400

    fixing R code for trial allocation, fixing controllers and still working on front end

---
+++ lib/CXGN/Trial/TrialAllocation.pm
@@ -0,0 +1,19 @@
+package CXGN::Trial::TrialAllocation;
+
+
+
+=head1 AUTHORS
+
+ Chris Simoes (ccs263@cornell.edu)
+
+=cut
+
+
+## Plot Numbering
+
+
+## Personalized Plot naming
+
+## Store Trial
+
+1;
