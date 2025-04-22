39c13d9a4c22cb696f4ff90b79ecc1b33cda6fc4
Isaak <iyt2@cornell.edu>
2025-04-22 08:31:05 +0200

    save gebvs and adjusted means as separate analysis instances

--- js/source/legacy/solGS/gebvsSave.js
+++ js/source/legacy/solGS/gebvsSave.js
@@ -9,6 +9,9 @@ jQuery(document).ready(function () {
     });
   
     jQuery("#save_gebvs").click(function () {
+        jQuery('#analysis_result_save_type').val('gebvs');
+        analysisArgs['analysis_result_save_type'] = 'gebvs';
+        console.log(`analysisArgs: ${JSON.stringify(analysisArgs)}`)
       jQuery("#gebvs_output .multi-spinner-container").show();
       jQuery("#gebvs_save_message")
         .html("Please wait...saving the GEBVs may take a few minutes.")
@@ -62,4 +65,74 @@ jQuery(document).ready(function () {
       });
     });
   });
+
+
+  jQuery(document).ready(function () {
+    var analysisArgs = solGS.analysisSave.saveGebvsArgs();
+      solGS.analysisSave.checkStoredAnalysis(analysisArgs).done(function (res) {
+      if (res.analysis_id) {
+        jQuery("#save_adjsuted_means").hide();
+        var link = '<a href="/analyses/' + res.analysis_id + '">View stored genetic values</a>';
+        jQuery("#gebvs_output").append(link);
+        }
+    });
+  
+    jQuery("#save_adjusted_means").click(function () {
+        analysisArgs['analysis_result_save_type'] = 'adjsuted_means';
+        jQuery('#analysis_result_save_type').val('adjsuted_means');
+
+      jQuery("#gebvs_output .multi-spinner-container").show();
+      jQuery("#gebvs_save_message")
+        .html("Please wait...saving the genetic values (adjusted means) may take a few minutes.")
+        .show();
+      jQuery("#save_adjsted_means").hide();
+  
+      solGS.analysisSave.checkUserStatus().done(function (res) {
+        if (!res.loggedin) {
+          solGS.submitJob.loginAlert();
+        } else {
+        }
+      });
+  
+      solGS.analysisSave.checkUserStatus().fail(function () {
+        solGS.alertMessage("Error occured checking for user status");
+      });
+  
+      solGS.analysisSave.getGebvsResultDetails(analysisArgs).done(function (res) {
+  
+        if (res.error) {
+          jQuery("#gebvs_output .multi-spinner-container").hide();
+          jQuery("#gebvs_save_message")
+            .html(res.error + ". The logged info may not exist for the result.")
+            .show()
+            .fadeOut(50000);
+  
+          jQuery("#save_adjusted_means").show();
+        } else {
+          var save = solGS.analysisSave.storeAnalysisResults(res.analysis_details);
+  
+          save.done(function (res) {
+            jQuery("#gebvs_output .multi-spinner-container").hide();
+            if (res.error) {
+              jQuery("#gebvs_save_message").html(res.error).show().fadeOut(50000);
+  
+              jQuery("#save_adjusted_means").show();
+            } else {
+              jQuery("#gebvs_save_message").hide();
+  
+              var link = '<a href="/analyses/' + res.analysis_id + '">View stored genetic values</a>';
+              jQuery("#gebvs_output").append(link);
+            }
+          });
+  
+          save.fail(function (res) {
+            jQuery("#gebvs_output .multi-spinner-container").hide();
+            jQuery("#save_adjusted_means").show();
+            jQuery("#gebvs_save_message").html(res.error).show().fadeOut(50000);
+          });
+        }
+      });
+    });
+  });
+  
   
