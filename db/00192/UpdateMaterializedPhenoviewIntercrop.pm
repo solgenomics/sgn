375def9e4bae23becaeb055f6dcb2dff79019c9d
David Waring <djw64@cornell.edu>
2025-05-01 13:58:01 -0400

    Traits MatView: allow multiple VARIABLE_OF terms

--- db/00192/UpdateMaterializedPhenoviewIntercrop.pm
+++ db/00192/UpdateMaterializedPhenoviewIntercrop.pm
@@ -209,7 +209,7 @@ CREATE VIEW public.traits AS
     JOIN cvterm ON(cvprop.cv_id = cvterm.cv_id)
       JOIN dbxref USING(dbxref_id)
     JOIN db ON(dbxref.db_id = db.db_id)
-    LEFT JOIN cvterm_relationship is_variable ON cvterm.cvterm_id = is_variable.subject_id AND is_variable.type_id = (SELECT cvterm_id FROM cvterm WHERE name = 'VARIABLE_OF')
+    LEFT JOIN cvterm_relationship is_variable ON cvterm.cvterm_id = is_variable.subject_id AND is_variable.type_id IN (SELECT cvterm_id FROM cvterm WHERE name = 'VARIABLE_OF')
     WHERE is_variable.subject_id IS NOT NULL
     GROUP BY 1,2
   UNION
