2e6515c628e42fb74ee2fe288a0f4b1e1482ff09
titima15 <tt15@cornell.edu>
2025-05-30 20:24:32 -0400

    get related trials

--- lib/CXGN/CrossingTrial.pm
+++ lib/CXGN/CrossingTrial.pm
@@ -25,13 +25,41 @@ sub get_field_trials_source_of_crossing_experiment {
     my $crossing_experiment_id = $self->get_trial_id();
 
     my $crossing_experiment = CXGN::Cross->new({schema=>$schema, trial_id => $crossing_experiment_id});
-    my $plots = $crossing_experiment->get_plots_used_in_crossing_experiment();
+    my $parent_plots = $crossing_experiment->get_plots_used_in_crossing_experiment();
     my $plots_of_plants = $crossing_experiment->get_plots_of_plants_used_in_crossing_experiment();
 
+    my %all_plots;
+    foreach my $plot (@$parent_plots) {
+        my $plot_id = $plot->[0];
+        $all_plots{$plot_id}++;
+    }
 
+    foreach my $plot_of_plant (@$plots_of_plants) {
+        my $plot_of_plant_id = $plot_of_plant->[0];
+        $all_plots{$plot_of_plant_id}++;
+    }
 
+    my @where_clause;
+    my @plot_ids = keys %all_plots;
+    my $plot_sql = join (",", @plot_ids);
+    push @where_clause, "nd_experiment_stock.stock_id IN ($plot_sql)";
+    my $where_clause = scalar(@where_clause)>0 ? " WHERE " . (join (" AND " , @where_clause)) : '';
 
-    my @field_trials;
+    my $nd_experiment_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, , 'field_layout', 'experiment_type')->cvterm_id();
+    my $q = "SELECT DISTINCT project.project_id, project.name
+        FROM nd_experiment_stock
+        JOIN nd_experiment_project ON (nd_experiment_project.nd_experiment_id = nd_experiment_stock.nd_experiment_id) AND nd_experiemnt_stock.type_id = ?
+        JOIN project ON (nd_experiment_project.project_id = project.project_id)
+        $where_clause";
+
+    my $h = $schema->storage->dbh()->prepare($q);
+    $h->execute($nd_experiment_type_id);
+
+    my @field_trials = ();
+    while(my($trial_id, $trial_name) = $h->fetchrow_array()){
+        push @field_trials, [$trial_id, $trial_name]
+    }
+
     return  \@field_trials;
 }
 
