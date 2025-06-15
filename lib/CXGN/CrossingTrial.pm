c995786879d9ad5d6a62b8ddb298b3f68f994b34
titima15 <tt15@cornell.edu>
2025-06-15 17:21:39 -0400

    initiate query only if param is defined

--- lib/CXGN/CrossingTrial.pm
+++ lib/CXGN/CrossingTrial.pm
@@ -44,25 +44,28 @@ sub get_field_trial_sources_of_crossing_experiment {
         }
     }
 
+    my @field_trials = ();
     my @where_clause;
     my @plot_ids = keys %all_plots;
-    my $plot_sql = join (",", @plot_ids);
-    push @where_clause, "nd_experiment_stock.stock_id IN ($plot_sql)";
-    my $where_clause = scalar(@where_clause)>0 ? " WHERE " . (join (" AND " , @where_clause)) : '';
 
-    my $nd_experiment_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, , 'field_layout', 'experiment_type')->cvterm_id();
-    my $q = "SELECT DISTINCT project.project_id, project.name
-        FROM nd_experiment_stock
-        JOIN nd_experiment_project ON (nd_experiment_project.nd_experiment_id = nd_experiment_stock.nd_experiment_id) AND nd_experiment_stock.type_id = ?
-        JOIN project ON (nd_experiment_project.project_id = project.project_id)
-        $where_clause";
+    if (scalar(@plot_ids) > 0) {
+        my $plot_sql = join (",", @plot_ids);
+        push @where_clause, "nd_experiment_stock.stock_id IN ($plot_sql)";
+        my $where_clause = scalar(@where_clause)>0 ? " WHERE " . (join (" AND " , @where_clause)) : '';
 
-    my $h = $schema->storage->dbh()->prepare($q);
-    $h->execute($nd_experiment_type_id);
+        my $nd_experiment_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, , 'field_layout', 'experiment_type')->cvterm_id();
+        my $q = "SELECT DISTINCT project.project_id, project.name
+            FROM nd_experiment_stock
+            JOIN nd_experiment_project ON (nd_experiment_project.nd_experiment_id = nd_experiment_stock.nd_experiment_id) AND nd_experiment_stock.type_id = ?
+            JOIN project ON (nd_experiment_project.project_id = project.project_id)
+            $where_clause";
 
-    my @field_trials = ();
-    while(my($trial_id, $trial_name) = $h->fetchrow_array()){
-        push @field_trials, [$trial_id, $trial_name]
+        my $h = $schema->storage->dbh()->prepare($q);
+        $h->execute($nd_experiment_type_id);
+
+        while(my($trial_id, $trial_name) = $h->fetchrow_array()){
+            push @field_trials, [$trial_id, $trial_name]
+        }
     }
 
     return  \@field_trials;
@@ -91,6 +94,7 @@ sub get_field_trials_for_evaluating_crosses {
     my $crossing_experiment = CXGN::Cross->new({schema=>$schema, trial_id => $crossing_experiment_id});
     my $crosses = $crossing_experiment->get_crosses_in_crossing_experiment();
 
+    my @field_trials = ();
     my @cross_ids;
     if ($crosses) {
         foreach my $cross (@$crosses) {
@@ -98,26 +102,28 @@ sub get_field_trials_for_evaluating_crosses {
         }
     }
 
-    my @where_clause;
-    my $cross_sql = join (",", @cross_ids);
-    push @where_clause, "stock_relationship.object_id IN ($cross_sql)";
-    my $where_clause = scalar(@where_clause)>0 ? " WHERE " . (join (" AND " , @where_clause)) : '';
+    if (scalar(@cross_ids) > 0) {
+        my @where_clause;
+        my $cross_sql = join (",", @cross_ids);
+        push @where_clause, "stock_relationship.object_id IN ($cross_sql)";
+        my $where_clause = scalar(@where_clause)>0 ? " WHERE " . (join (" AND " , @where_clause)) : '';
 
-    my $q = "SELECT DISTINCT project.project_id, project.name
-        FROM stock_relationship
-        JOIN nd_experiment_stock ON (nd_experiment_stock.stock_id = stock_relationship.subject_id) AND stock_relationship.type_id = ?
-        JOIN nd_experiment_project ON (nd_experiment_project.nd_experiment_id = nd_experiment_stock.nd_experiment_id) AND nd_experiment_stock.type_id = ?
-        JOIN project ON (nd_experiment_project.project_id = project.project_id)
-        $where_clause";
+        my $q = "SELECT DISTINCT project.project_id, project.name
+            FROM stock_relationship
+            JOIN nd_experiment_stock ON (nd_experiment_stock.stock_id = stock_relationship.subject_id) AND stock_relationship.type_id = ?
+            JOIN nd_experiment_project ON (nd_experiment_project.nd_experiment_id = nd_experiment_stock.nd_experiment_id) AND nd_experiment_stock.type_id = ?
+            JOIN project ON (nd_experiment_project.project_id = project.project_id)
+           $where_clause";
 
-    my $h = $schema->storage->dbh()->prepare($q);
-    $h->execute($plot_of_type_id, $nd_experiment_type_id);
+        my $h = $schema->storage->dbh()->prepare($q);
+        $h->execute($plot_of_type_id, $nd_experiment_type_id);
 
-    my @field_trials = ();
-    while(my($trial_id, $trial_name) = $h->fetchrow_array()){
-        push @field_trials, [$trial_id, $trial_name]
+        while(my($trial_id, $trial_name) = $h->fetchrow_array()){
+            push @field_trials, [$trial_id, $trial_name]
+        }
+
     }
-    print STDERR "FIELD TRIALS FOR CROSSES =".Dumper(\@field_trials)."\n";
+
     return  \@field_trials;
 }
 
