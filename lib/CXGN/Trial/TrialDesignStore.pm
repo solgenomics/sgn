e7f738055db98c3fe94842a5933825fae07844cd
alockrow <awl67@cornell.edu>
2025-07-03 16:31:46 -0400

    Add use strict to TrialDesignStore

--- lib/CXGN/Trial/TrialDesignStore.pm
+++ lib/CXGN/Trial/TrialDesignStore.pm
@@ -276,6 +276,7 @@ use CXGN::Trial::TrialDesignStore::GenotypingTrial;
 use CXGN::Trial::TrialDesignStore::Analysis;
 use CXGN::Trial::TrialDesignStore::CrossingTrial;
 use CXGN::Trial::TrialDesignStore::SamplingTrial;
+use strict;
 
 sub new {
     my $class = shift;
