e9c0f3b18aeb6c067e6176bfba2da146347cf54e
Clay Birkett <clb343@cornell.edu>
2025-08-01 13:11:17 -0400

    indicate if marker found im multiple protocols

--- lib/CXGN/List/Validate/Plugin/Markers.pm
+++ lib/CXGN/List/Validate/Plugin/Markers.pm
@@ -1,9 +1,7 @@
-
 package CXGN::List::Validate::Plugin::Markers;
 
 use strict;
 use warnings;
-use Moose;
 use CXGN::Marker::Search;
 use JSON;
 
@@ -22,10 +20,16 @@ sub validate {
     my $marker;
     my $term;
     my $count;
+    my $name;
     my $protocol_id;
+    my $protocol_name;
+    my $protocol_id_all;
+    my %other_protocols;
     my $found;
     my @row;
+    my $val;
     my @missing = ();
+    my @warning = ();
 
     my $q = "select cvterm_id from public.cvterm where name = 'vcf_map_details'";
     my $h = $schema->storage->dbh()->prepare($q);
@@ -34,36 +38,43 @@ sub validate {
 
     #if protocol_id found get data from nd_protocolprop
     foreach $term (@$list) {
-	eval {$str = decode_json $term;};
+	eval { $str = decode_json($term); 1; } or next;
+        next unless ref($str) eq 'HASH';
         foreach $key ( keys %$str ) {
 	    if ($key eq "genotyping_protocol_id") {
 		$protocol_id = $str->{$key};
 		$q = "SELECT value from nd_protocolprop where type_id = ? AND nd_protocol_id = ?";
 		$h = $schema->storage->dbh()->prepare($q);
 		$h->execute($type_id, $protocol_id);
-		if (@row = $h->fetchrow_array()) {
-                    $data = decode_json($row[0]);
+		($val) = $h->fetchrow_array();
+		if (defined $val) {
+                    $data = decode_json($val);
                 } else {
 		    print STDERR "error protocol $protocol_id not found\n";
 		    push @missing, "protocol $protocol_id not found";
 		}
+		$q = "SELECT name from nd_protocol where nd_protocol_id = ?";
+		$h = $schema->storage->dbh()->prepare($q);
+		$h->execute($protocol_id);
+                ($protocol_name) = $h->fetchrow_array();
 	    }
         }
     }
     $q = "SELECT nd_protocol_id from materialized_markerview where marker_name = ?";
     $h = $schema->storage->dbh()->prepare($q);
+    my $q2 = "SELECT materialized_markerview.nd_protocol_id, name from materialized_markerview, nd_protocol where materialized_markerview.nd_protocol_id = nd_protocol.nd_protocol_id and marker_name = ?";
+    my $h2 = $schema->storage->dbh()->prepare($q2);
 
     foreach $term (@$list) {
         eval {$str = decode_json $term;};
         if ($@) {			#simple list
-	    foreach $marker ( $term ) {
-	        $h->execute($marker);
-		if (@row = $h->fetchrow_array()) {
-		} else {
-		    push @missing, $term;
-		}
-            }
+	    $h->execute($term);
+	    ($protocol_id_all) = $h->fetchrow_array();
+            if (!defined $protocol_id_all) {
+	        push @missing, $term;
+	    }
 	} else {			#json list
+	    next unless ref($str) eq 'HASH';
 	    foreach $key ( keys %$str ) {
 	        if ($key eq "marker_name") {
 		    $marker = $str->{$key};
@@ -77,12 +88,29 @@ sub validate {
 		    if (!$found) {
 			push @missing, $marker;
 		    }
+		    $h2->execute($marker);      #search other protocols
+		    while (@row = $h2->fetchrow_array()) {
+		        ($protocol_id_all, $name) = @row;
+			if ($protocol_id_all != $protocol_id ) {
+			   next if $protocol_id_all == $protocol_id;
+			   push @{ $other_protocols{$marker} }, $name;
+		        } 
+                    }
 	        }
 	    }
 	}
-        #$schema->storage->debug(1);
     }
-    return { missing => \@missing };
+	if (%other_protocols) {
+	    my $formatted = "selected protocol $protocol_name, markers also found in\n";
+	    foreach my $marker (keys %other_protocols) {
+                $formatted .= "$marker\n\t" . join("\n\t", @{$other_protocols{$marker}}) . "\n";
+            }
+            push @warning, $formatted;
+	}
+        #$schema->storage->debug(1);
+
+    return { missing => \@missing,
+             warning => \@warning };
 
 }
 
