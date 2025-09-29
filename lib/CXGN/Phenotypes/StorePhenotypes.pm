42c1bb35ee917e55ac626744714e61d603c586f6
Lukas Mueller <lam87@cornell.edu>
2025-09-29 18:03:15 -0400

    report the total number of skipped phenotypes and also the ones skipped due to values already being present in the database.

--- lib/CXGN/Phenotypes/StorePhenotypes.pm
+++ lib/CXGN/Phenotypes/StorePhenotypes.pm
@@ -655,7 +655,7 @@ sub check_measurement {
         }
 
         #check that trait value is valid for trait name (but only if we have a trait_value)
-        if ((defined($trait_value) && $trait_value ne '') && exists($self->check_trait_format()->{$trait_cvterm_id})) { 
+        if ((defined($trait_value) && $trait_value ne '' && $trait_value ne 'NA' && $trait_value ne '.') && exists($self->check_trait_format()->{$trait_cvterm_id})) { 
             # print STDERR "Trait minimum value checks if it exists: " . $self->check_trait_min_value->{$trait_cvterm_id} . "\n";
             if ($self->check_trait_format()->{$trait_cvterm_id} eq 'numeric') {
                 my $trait_format_checked = looks_like_number($trait_value);
@@ -900,6 +900,7 @@ sub store {
         my @overwritten_values;
         my $new_count = 0;
         my $skip_count = 0;
+	my $previously_stored_skip_count = 0;
         my $overwrite_count = 0;
         my $remove_count = 0;
 
@@ -1129,6 +1130,7 @@ sub store {
                         print STDERR "RESULT FROM STORE: ".Dumper($result)."\n";
 
                         if (exists($result->{skip_count}) && $result->{skip_count} > 0) { $skip_count++; }
+			if (exists($result->{previously_stored_skip_count}) && $result->{previously_stored_skip_count} > 0) { $previously_stored_skip_count++ };
                         if (exists($result->{remove_count}) && $result->{remove_count} > 0) { $remove_count++; }
                         if (exists($result->{new_count})   && $result->{new_count}   > 0) { $new_count++; }
                         if (exists($result->{overwrite_count}) && $result->{overwrite_count} > 0) { $overwrite_count++; }
@@ -1264,7 +1266,9 @@ sub store {
         $success_message = 'All values in your file have been successfully processed!<br><br>';
         $success_message .= "$new_count new values stored<br>";
         $success_message .= "$skip_count previously stored values skipped<br>";
+	$success_message .= "of which $previously_stored_skip_count were skipped because a value had previously been stored.";
         $success_message .= "$overwrite_count previously stored values overwritten<br>";
+
         $success_message .= "$remove_count previously stored values removed<br><br>";
         my %files_with_overwritten_values = map {$_->[0] => 1} @overwritten_values;
         my $obsoleted_files = $self->check_overwritten_files_status(keys %files_with_overwritten_values);
