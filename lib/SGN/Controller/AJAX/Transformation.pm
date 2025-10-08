9d90b5fe7ee1ad994b54d32cbaf2b84a09abae72
titima15 <tt15@cornell.edu>
2025-10-08 18:27:24 -0400

    fixed file id

--- lib/SGN/Controller/AJAX/Transformation.pm
+++ lib/SGN/Controller/AJAX/Transformation.pm
@@ -1529,10 +1529,8 @@ sub upload_qPCR_data_POST : Args(0) {
     my $assay_date = $c->req->param('assay_date');
     my $upload;
     my $plugin_type;
-    my $CT_upload = $c->req->upload('CT_qPCR_data_file');
+    my $CT_upload = $c->req->upload('Cq_qPCR_data_file');
     my $normalized_upload = $c->req->upload('normalized_qPCR_data_file');
-    print STDERR "CT UPLOAD =".Dumper($CT_upload)."\n";
-    print STDERR "NORMALIZED UPLOAD =".Dumper($normalized_upload)."\n";
 
     if ($CT_upload) {
         $upload = $CT_upload;
@@ -1541,12 +1539,10 @@ sub upload_qPCR_data_POST : Args(0) {
         $upload = $normalized_upload;
         $plugin_type = 'NormalizedqPCRData';
     }
-    print STDERR "PLUGIN TYPE =".Dumper($plugin_type)."\n";
 
     my $parser;
     my $parsed_data;
     my $upload_original_name = $upload->filename();
-    print STDERR "ORIGINAL NAME = =".Dumper($upload_original_name)."\n";
 
     my $upload_tempfile = $upload->tempname;
     my $subdirectory = "qPCR_data_upload";
@@ -1659,7 +1655,6 @@ sub upload_qPCR_data_POST : Args(0) {
                     $relative_expression_data = $parsed_data->{$transformant_name};
                 }
                 print STDERR "CT UPLOAD =".Dumper($CT_expression_data)."\n";
-                print STDERR "RELATIVE UPLOAD =".Dumper($relative_expression_data)."\n";
 
                 my $expression_data = CXGN::Transformation::StoreTransgeneExpressionData->new({
                     chado_schema => $schema,
