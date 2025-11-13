26239ed9ef03ee7bab191a68039b9100fcf74f6c
Lukas Mueller <lam87@cornell.edu>
2025-11-12 22:08:40 -0500

    remove debug alerts.

--- js/source/entries/htmltree.js
+++ js/source/entries/htmltree.js
@@ -6,25 +6,29 @@ var version = '0.01';
 
 export function init(tree_type, hard_refresh) {
 
-    alert("STARTING..."+tree_type+" refresh="+hard_refresh);
+    //alert("STARTING..."+tree_type+" refresh="+hard_refresh);
 
+
+    
     var html = '';
+
+    
     
     init_events(tree_type);
     // check if we have no last refresh date or hard_refresh is true
     // fetch the tree from the back end and store it in the browser
     //
-    alert("HARD REFRESH: "+hard_refresh);
+//    alert("HARD REFRESH: "+hard_refresh);
     var last_refresh_date = localStorage.getItem(tree_type+'_last_refresh');
-    alert("Last refresh timestamp: "+last_refresh_date);
+    //alert("Last refresh timestamp: "+last_refresh_date);
 
     if (hard_refresh === 1 || last_refresh_date === null) {
-	alert("refresh date is not set or hard_refresh is 1");
+	//alert("refresh date is not set or hard_refresh is 1");
 	jQuery.ajax({ 
 	    url: '/ajax/breeders/recently_modified_projects',
 	    data: { 'type' : tree_type, 'hard_refresh' : hard_refresh },
 	}).then( function(r) {
-	    alert("NEW TRIALS 1: "+JSON.stringify(r));
+	    //alert("NEW TRIALS 1: "+JSON.stringify(r));
 	    
 	    if (r.data.length > 0) {
 		get_html_tree(tree_type);
@@ -34,14 +38,14 @@ export function init(tree_type, hard_refresh) {
     }
 
     else {
-	alert('retrieving from local storage');
+	//alert('retrieving from local storage');
 	html = localStorage.getItem(tree_type);
-	alert('html retrieved from local storage: '+html);
+	//alert('html retrieved from local storage: '+html);
 	format_html_tree(html, tree_type);
     }
 
     if (html === null) {
-	alert('HTML NOT DEFINED! FETCHING...');
+	//alert('HTML NOT DEFINED! FETCHING...');
 	get_html_tree(tree_type);
 
     }
@@ -50,19 +54,20 @@ export function init(tree_type, hard_refresh) {
 
 export function init_events(tree_type) {
 
+    jQuery('#'+tree_type+'_search').keyup(function() {
+        var v = jQuery('#'+tree_type+'_search').val();
+        jQuery('#'+tree_type+'_list').jstree(true).search(v);
+    });
+
     jQuery('#refresh_'+tree_type+'_button').click(function() {
-	alert('hello!');
 	get_html_tree(tree_type);
-	    //.then( function(r) {
-	    //alert('now loading new tree');
-	    //format_html_tree(r.html, tree_type) });
-	
     });
     
 
     jQuery('#'+tree_type+'_list').on("changed.jstree", function (e, data) {
-    //console.log(data);
+	//alert('CLICK!!! ON type '+tree_type+ ' NODE: '+data.node.data.jstree.type);
 	if (jQuery('#'+tree_type+'_list').jstree('is_leaf', data.node) && data.node.data.jstree.type == tree_type) {
+	    //alert('tree_type = '+tree_type);
             jQuery('#'+tree_type+'_download_phenotypes_button').removeAttr('disabled');
             jQuery("#folder_edit_options").hide();
 	}
@@ -119,11 +124,11 @@ export function get_timestamp() {
 
     
 export function get_html_tree(tree_type) {
-    alert('get_html_tree with tree type '+tree_type);
+    //alert('get_html_tree with tree type '+tree_type);
     return jQuery.ajax( {
 	url: '/ajax/breeders/get_trials_with_folders?type='+tree_type,  
     }).then(  function(r) {
-	alert("adding new html and timestamp to localstorage" + r.html);
+	//alert("adding new html and timestamp to localstorage" + r.html);
 	localStorage.setItem(tree_type, r.html);
 	localStorage.setItem(tree_type+'_last_refresh', get_timestamp());	
 	format_html_tree(r.html, tree_type);
@@ -134,13 +139,8 @@ export function format_html_tree(treehtml, tree_type) {
 
     var html = '<ul>'+treehtml+'</ul>';
     
-    alert('format_html_tree with TREE TYPE = '+tree_type);
-    
-    alert('html now: '+html);
-    
     jQuery('#'+tree_type+'_list').html(html);
     
-    alert("NEW HTML = "+html);
     jQuery('#'+tree_type+'_list').jstree( {
 	"core": { 'themes': { 'name': 'proton', 'responsive': true}},
 	"valid_children" : [ "folder", "trial", "breeding_program", "analyses", "sampling_trial" ],
@@ -151,7 +151,7 @@ export function format_html_tree(treehtml, tree_type) {
 	    "folder" : {
 		"icon": 'glyphicon glyphicon-folder-open text-danger',
 	    },
-	    "trial" : {
+	    "phenotyping_trial" : {
 		"icon": 'glyphicon glyphicon-leaf text-success',
 	    },
 	    "analyses" : {
