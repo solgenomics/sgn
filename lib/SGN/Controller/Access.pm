8e564f69ebd52701c032e52b0ae7808f386c74f8
Lukas Mueller <lam87@cornell.edu>
2025-11-18 13:33:49 -0500

    redirect manage privileges page to login page if not logged in.

--- lib/SGN/Controller/Access.pm
+++ lib/SGN/Controller/Access.pm
@@ -13,17 +13,12 @@ sub access_table_page :Path('/access/table') {
     my $self = shift;
     my $c = shift;
 
-#    if (! (my $user = $c->user())) {
-#	$c->stash->{rest} = { error => "You must be logged in to use this resource" };
-#	$c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
- #   }
+    if (! $c->user()) {
+	$c->res->redirect( uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+	$c->detach();
+    }
 
-#    if (! $c->user()) {
-#	$c->stash->{rest} = { error => "NOT LOGGED IN!\n" };
-#	return;
-#    }
-    
-    if (my $message = $c->stash->{access}->denied( $c->stash->{user_id}, "read", "privileges")) { 
+    if (my $message = $c->stash->{access}->denied( $c->stash->{user_id}, "read", "privileges")) {
     	$c->response->status(401);
 	$c->stash->{data_type} = 'privilege';
 	$c->stash->{message} = $message;
@@ -35,26 +30,26 @@ sub access_table_page :Path('/access/table') {
     $c->stash->{template} = '/access/table.mas';
 }
 
-sub access :Path('/access') Args(0) {
-    my $self = shift;
-    my $c = shift;
+# sub access :Path('/access') Args(0) {
+#     my $self = shift;
+#     my $c = shift;
 
-    my $message;
-    my $sp_person_id = $c->user() ? $c->user()->get_object->get_sp_person_id() : undef;
-    if (!$c->stash->{access}->check_user($sp_person_id, "accesstest")) {
-	$message = "You don't have sufficient privileges to access this page.";
-	
-    }
+#     my $message;
+#     my $sp_person_id = $c->user() ? $c->user()->get_object->get_sp_person_id() : undef;
+#     if (!$c->stash->{access}->check_user($sp_person_id, "accesstest")) {
+# 	$message = "You don't have sufficient privileges to access this page.";
 
-    else {
-	$message = "YOU HAVE ALL THE RIGHTS IN THE WORLD!\n";
-    }
+#     }
 
-    $c->stash->{message} = $message;
+#     else {
+# 	$message = "YOU HAVE ALL THE RIGHTS IN THE WORLD!\n";
+#     }
 
-    $c->stash->{template} = '/site/access/manage.mas';
+#     $c->stash->{message} = $message;
 
-}
+#     $c->stash->{template} = '/site/access/manage.mas';
+
+# }
 
 
 
