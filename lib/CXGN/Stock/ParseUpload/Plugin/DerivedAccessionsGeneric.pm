8089e3922e122fd03f203b8a5682e7a2acdd59bf
titima15 <tt15@cornell.edu>
2025-11-20 10:41:13 -0500

    upload plugin

---
+++ lib/CXGN/Stock/ParseUpload/Plugin/DerivedAccessionsGeneric.pm
@@ -0,0 +1,114 @@
+package CXGN::Stock::ParseUpload::Plugin::DerivedAccessionsGeneric;
+
+use Moose::Role;
+use CXGN::File::Parse;
+use CXGN::Stock::StockLookup;
+use SGN::Model::Cvterm;
+use Data::Dumper;
+use CXGN::List::Validate;
+
+sub _validate_with_plugin {
+    my $self = shift;
+
+    my $filename = $self->get_filename();
+    my $schema = $self->get_chado_schema();
+
+    my @error_messages;
+    my %errors;
+
+    my $parser = CXGN::File::Parse->new (
+        file => $filename,
+        required_columns => [ 'original_name', 'derived_accession_name', 'description'],
+        optional_columns => [],
+        column_aliases => {
+            'original_name' => ['original name', 'Original Name'],
+            'derived_accession_name' => ['derived accession name', 'Derived Accession Name'],
+        }
+    );
+    my $parsed = $parser->parse();
+    my $parsed_errors = $parsed->{errors};
+    my $parsed_columns = $parsed->{columns};
+    my $parsed_data = $parsed->{data};
+    my $parsed_values = $parsed->{values};
+    my $additional_columns = $parsed->{additional_columns};
+
+    # return if parsing error
+    if ( $parsed_errors && scalar(@$parsed_errors) > 0 ) {
+        $errors{'error_messages'} = $parsed_errors;
+        $self->_set_parse_errors(\%errors);
+        return;
+    }
+
+    if ( $additional_columns && scalar(@$additional_columns) > 0 ) {
+        $errors{'error_messages'} = [
+            "The following columns are not recognized: " . join(', ', @$additional_columns) . ". Please check the spreadsheet format for the allowed columns."
+        ];
+        $self->_set_parse_errors(\%errors);
+        return;
+    }
+
+    my $seen_original_names = $parsed_values->{'original_name'};
+    my $seen_derived_accession_names = $parsed_values->{'derived_accession_name'};
+
+    my $original_names_validator = CXGN::List::Validate->new();
+    my @original_names_missing = @{$original_names_validator->validate($schema,'accessions_or_plants_or_tissue_samples',$seen_original_name)->{'missing'}};
+
+    if (scalar(@original_names_missing) > 0) {
+        push @error_messages, "The following original names are not in the database, or are not in the database as accession names, plant names or tissue sample names: ".join(',',@original_names_missing);
+    }
+
+    my $rs = $schema->resultset("Stock::Stock")->search({
+        'is_obsolete' => { '!=' => 't' },
+        'uniquename' => { -in => $seen_derived_accession_names }
+    });
+
+    while (my $r=$rs->next){
+        push @error_messages, "Derived accession name already exists in database: ".$r->uniquename;
+    }
+
+    if (scalar(@error_messages) >= 1) {
+        $errors{'error_messages'} = \@error_messages;
+        $self->_set_parse_errors(\%errors);
+        return;
+    }
+
+    $self->_set_parsed_data($parsed);
+    return 1;
+
+}
+
+sub _parse_with_plugin {
+    my $self = shift;
+    my $schema = $self->get_chado_schema();
+    my $parsed = $self->_parsed_data();
+    my $parsed_data = $parsed->{data};
+    my %parsed_result;
+    my @derived_accession_info;
+
+    my $accession_stock_type_id  =  SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
+    my $plot_stock_type_id  =  SGN::Model::Cvterm->get_cvterm_row($schema, 'plot', 'stock_type')->cvterm_id();
+    my $plant_stock_type_id  =  SGN::Model::Cvterm->get_cvterm_row($schema, 'plant', 'stock_type')->cvterm_id();
+    my $plot_of_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plot_of', 'stock_relationship')->cvterm_id();
+    my $plant_of_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'plant_of', 'stock_relationship')->cvterm_id();
+
+    foreach my $row (@$parsed_data) {
+        my $original_name = $row->{'original_name'};
+        my $derived_accession_name = $row->{'derived_accession_name'};
+        my $description = $row->{'description'};
+        push @derived_accession_info, {
+            'stock_name' => $original_name,
+            'derived_accession_name' => $derived_accession_name,
+            'accession_description' => $description,
+        }
+    }
+
+    $parsed_result{'derived_accession_info'} = \@derived_accession_info;
+
+    $self->_set_parsed_data(\%parsed_result);
+
+    return 1;
+
+}
+
+
+1;
