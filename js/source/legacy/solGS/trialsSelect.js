f06904a61d5b7cbf3e0d91d61cf340dafad6015c
chris263 <ccs263@cornell.edu>
2026-04-23 21:49:12 -0400

    fixing conflicts

--- js/source/legacy/solGS/trialsSelect.js
+++ js/source/legacy/solGS/trialsSelect.js
@@ -99,7 +99,7 @@ jQuery(document).ready(function () {
                 // Process and display the trials
                 console.log("Trials for dataset " + datasetName + ": ", trials);
                 var trialSelect = jQuery("#trial_select");
-                trialSelect.empty(); 
+                trialSelect.empty();
                 var datasetOption = jQuery("<option></option>")
                     .attr("value", datasetId)
                     .text("Dataset: " + datasetName);
@@ -144,6 +144,4 @@ jQuery(document).on("change", "#trial_select", function () {
     } else {
         console.log("No trial selected.");
     }
-=======
->>>>>>> 3e8418880e (populate dataset trials select menu)
 });
