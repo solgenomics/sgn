f38a01ae2f9e7d74c27c56cb9f4e84740386990e
Clay Birkett <clb343@cornell.edu>
2026-05-14 16:24:32 -0400

    fix javascript errors on trial details page

--- js/source/legacy/solGS/loadAcronyms.js
+++ js/source/legacy/solGS/loadAcronyms.js
@@ -4,65 +4,61 @@
 *
 */
 
-
 var solGS = solGS || function solGS () {};
 
 solGS.acronyms = {
 
     loadAcronyms: function() {
-       var id;
        var page = document.URL;
+       var id = page.match(/breeders\/trial\//)
+            ? jQuery('#trial_id').val()
+            : jQuery('#training_pop_id').val();
 
-       if (page.match(/breeders\/trial\//)) {
-           id = jQuery('#trial_id').val();
-       } else {
-          id = jQuery('#training_pop_id').val();
-       }
-
-       var acros = jQuery.ajax({
+       return jQuery.ajax({
             type    : 'POST',
             dataType: 'json',
-            data    : {'id': id},
+            data    : { id: id },
             url     : '/solgs/load/trait/acronyms/',
         });
-
-        return acros;
     },
-
 
     displayAcronyms: function(data) {
 
-        var table = jQuery('#trait_acronyms_table').DataTable({
-        'searching' : false,
-        'ordering'  : false,
-        'processing': true,
-        'paging'    : false,
-        'info'      : false,
-        });
+        if (!data || !data.length) return;
+
+        var table;
+        if (jQuery.fn.DataTable.isDataTable('#trait_acronyms_table')) {
+            table = jQuery('#trait_acronyms_table').DataTable();
+        } else {
+            table = jQuery('#trait_acronyms_table').DataTable({
+                searching  : false,
+                ordering   : false,
+                processing : true,
+                paging     : false,
+                info       : false,
+            });
+        }
 
         table.rows.add(data).draw();
+    },
 
-    }
+    showError: function() {
+        jQuery('#trait_acronyms_table').hide();
+        jQuery('#acronyms_message')
+            .html('Error occurred loading acronyms')
+            .show()
+            .fadeOut(5000);
+    },
+};
 
-/////////////
-}
-////////////
-
-
-jQuery(document).ready( function() {
+jQuery(document).ready(function() {
     jQuery('#trait_acronyms_div').show();
 
-    solGS.acronyms.loadAcronyms().done( function(res) {
-        solGS.acronyms.displayAcronyms(res.acronyms);
-    });
-
-    solGS.acronyms.loadAcronyms().fail( function(res) {
-         jQuery('#trait_acronyms_table').hide();
-        var errorMsg = 'Error occured loading acronyms';
-        jQuery('#acronyms_message')
-            .html(errorMsg)
-            .show()
-            .fadeOut(50000);
-    });
-
+    solGS.acronyms.loadAcronyms()
+        .done(function(res) {
+            solGS.acronyms.displayAcronyms(res.acronyms);
+        })
+        .fail(function() {
+            solGS.acronyms.showError();
+        });
 });
