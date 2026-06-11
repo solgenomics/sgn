ecbe80c0278c10e28d2b7f0d3e0235b480804547
bm743 <bm743@cornell.edu>
2026-06-11 10:52:06 -0400

    add observationUnitDbId as valid input for phenotype spreadsheet parser

--- lib/CXGN/Phenotypes/ParseUpload/Plugin/PhenotypeSpreadsheetSimpleGeneric.pm
+++ lib/CXGN/Phenotypes/ParseUpload/Plugin/PhenotypeSpreadsheetSimpleGeneric.pm
@@ -75,7 +75,7 @@ sub validate {
         file => $filename,
         required_columns => [ 'observationunit_name' ],
         column_aliases => {
-            'observationunit_name' => [ 'plot_name', 'subplot_name', 'plant_name', 'observationUnitName', 'plotName', 'subplotName', 'plantName' ]
+            'observationunit_name' => [ 'plot_name', 'subplot_name', 'plant_name', 'observationUnitName', 'plotName', 'subplotName', 'plantName', 'observationUnitDbId' ]
         }
     );
     my $parsed = $parser->parse();
@@ -129,18 +129,95 @@ sub parse {
     my $nd_protocol_id = shift; #not relevant for this plugin
     my $nd_protocol_filename = shift; #not relevant for this plugin
 
-    # Open and read the file
+    # Try parsing with observationUnitDbId first
     my $parser = CXGN::File::Parse->new(
-        file => $filename,
-        required_columns => [ "observationunit_name" ],
-        column_aliases => {
-            'observationunit_name' => [ 'plot_name', 'subplot_name', 'plant_name', 'observationUnitName', 'plotName', 'subplotName', 'plantName' ]
+        file             => $filename,
+        required_columns => ["observationunit_id"],
+        column_aliases   => {
+            'observationunit_id' => ['observationUnitDbId']
         }
     );
     my $parsed = $parser->parse();
+
+    if ( $parsed->{errors} && scalar( @{ $parsed->{errors} } ) > 0 ) {
+
+        # Fall back to name-based parsing
+        $parser = CXGN::File::Parse->new(
+            file             => $filename,
+            required_columns => ["observationunit_name"],
+            column_aliases   => {
+                'observationunit_name' => [
+                    'plot_name',    'subplot_name',
+                    'plant_name',   'observationUnitName',
+                    'plotName',     'subplotName',
+                    'plantName'
+                ]
+            }
+        );
+        $parsed = $parser->parse();
+
+        # If name-based also fails, return the errors
+        if ( $parsed->{errors} && scalar( @{ $parsed->{errors} } ) > 0 ) {
+            my %parse_result;
+            $parse_result{'error'} =
+              "File must have a plot name or observationUnitDbId column. Errors: "
+              . join( ", ", @{ $parsed->{errors} } );
+            return \%parse_result;
+        }
+    }
+
     my $parsed_data = $parsed->{data};
     my $parsed_values = $parsed->{values};
     my $trait_columns = $parsed->{optional_columns};
+    my $columns = $parsed->{columns};
+
+    my $observationunit_type;
+    if ( grep { $_ eq 'observationunit_id' } @$columns ) {
+        $observationunit_type = 'id';
+    }
+    elsif ( grep { $_ eq 'observationunit_name' } @$columns ) {
+        $observationunit_type = 'name';
+    }
+    else {
+        my %parse_result;
+        $parse_result{'error'} =
+          "File must have a plot name or observationUnitDbId column.";
+        return \%parse_result;
+    }
+
+    print STDERR "obsunit type: $observationunit_type\n";
+
+    # If IDs used, convert to uniquenames
+    my %id_to_name;
+    if ( $observationunit_type eq 'id' ) {
+
+        # Collect all stock ids from the parsed data
+        my @stock_ids =
+          map  { $_->{'observationunit_id'} }
+          grep { $_ && ref($_) eq 'HASH' && defined $_->{'observationunit_id'} }
+          @$parsed_data;
+
+        if ( !@stock_ids ) {
+            my %parse_result;
+            $parse_result{'error'} =
+              "No valid observationUnitDbId values found in the file.";
+            return \%parse_result;
+        }
+
+        my $transform_plugin = CXGN::List::Transform->new();
+        my $transform_result = $transform_plugin->transform( $schema, 'stock_ids_2_stocks', \@stock_ids );
+
+        if ( $transform_result->{missing} && @{ $transform_result->{missing} } ) {
+            my %parse_result;
+            $parse_result{'error'} =
+                "The following observationUnitDbId values could not be found in the database: "
+              . join( ", ", @{ $transform_result->{missing} } );
+            return \%parse_result;
+        }
+
+        my @transformed_names = @{ $transform_result->{transform} };
+        @id_to_name{@stock_ids} = @transformed_names;
+    }
 
     my %data;
     my %units_seen;
@@ -148,8 +225,21 @@ sub parse {
     for my $row (@$parsed_data) {
         next unless $row && ref($row) eq 'HASH';
 
-        my $observationunit_name = $row->{'observationunit_name'};
-        next unless defined $observationunit_name && $observationunit_name ne '';
+        my $observationunit_name;
+        if ( $observationunit_type eq 'id' ) {
+            my $stock_id = $row->{'observationunit_id'};
+            next unless defined $stock_id && $stock_id ne '';
+            $observationunit_name = $id_to_name{$stock_id};
+            next
+              unless defined $observationunit_name
+              && $observationunit_name ne '';
+        }
+        else {
+            $observationunit_name = $row->{'observationunit_name'};
+            next
+              unless defined $observationunit_name
+              && $observationunit_name ne '';
+        }
 
         $units_seen{$observationunit_name} = 1;
 
@@ -188,10 +278,10 @@ sub parse {
             }
 
             push @{ $data{$observationunit_name}->{$trait_name} }, @trait_values
-                if @trait_values;
+              if @trait_values;
         }
     }
-         
+
     my @sorted_units = sort keys %units_seen;
     my @sorted_variables = sort @$trait_columns;
 
