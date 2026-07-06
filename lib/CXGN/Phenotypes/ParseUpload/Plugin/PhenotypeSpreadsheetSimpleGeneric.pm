0bd9bf8c69eca8ba141a56a1ed995de2d5423955
Lukas Mueller <lam87@cornell.edu>
2026-07-06 10:39:14 -0400

    edit error message to reflect new accepted formats.

--- lib/CXGN/Phenotypes/ParseUpload/Plugin/PhenotypeSpreadsheetSimpleGeneric.pm
+++ lib/CXGN/Phenotypes/ParseUpload/Plugin/PhenotypeSpreadsheetSimpleGeneric.pm
@@ -133,7 +133,7 @@ sub validate {
                     my ($value, $timestamp) = @$measurement;
                     next if !$timestamp;
                     if ($timestamp !~ m/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}(:\d{2})?(?:[+-]\d{4})?$/) {
-                        $parse_result{'error'} = "Timestamp needs to be of form YYYY-MM-DD HH:MM:SS-0000 or YYYY-MM-DD HH:MM:SS+0000";
+                        $parse_result{'error'} = "Timestamp needs to be of form YYYY-MM-DD HH:MM:SS-0000 or YYYY-MM-DD HH:MM:SS+0000 or YYYY-MM-DD HH:MM format";
                         return \%parse_result;
                     }
                 }
