43b7cfccf17ea73def7de4c5928d10eeabe5526a
Lukas Mueller <lam87@cornell.edu>
2026-07-29 18:19:49 -0400

    add a script for scanning barcodes in images.

--- bin/scan_barcodes.pl
+++ bin/scan_barcodes.pl
@@ -32,6 +32,8 @@ Lukas Mueller <lam87@cornell.edu>
 =cut
 
 use strict;
+use Barcode::ZBar;
+use Image::Magick;
 
 my $dir = shift;
 
@@ -39,8 +41,54 @@ my @files = glob "$dir/*";
 
 my $step;
 
+my $scanner = Barcode::ZBar::ImageScanner ->new();
+my $image = Barcode::ZBar::Image->new();
 foreach my $f (@files) {
     chomp($f);
+
+    print STDERR "PROCESSING IMAGE $f...\n";
+
+    # 2. Read the image file using Image::Magick
+    my $magick = Image::Magick->new();
+    my $status = $magick->Read($f);
+    die "Error reading image: $status\n" if $status;
+
+    # 3. Extract the image metadata (width and height)
+    my $width  = $magick->Get('columns');
+    my $height = $magick->Get('rows');
+
+    # 4. Convert the JPG data to a raw 8-bit greyscale blob (Y800 format)
+    my $raw_blob = $magick->ImageToBlob(magick => 'GRAY', depth => 8);
+
+    # 5. Pack the raw bits into the Barcode::ZBar::Image wrapper
+    my $image = Barcode::ZBar::Image->new();
+
+    $image->set_size($width, $height);
+
+
+
+    $scanner->parse_config('disable');
+    $scanner->parse_config('qrcode.enable');
+
+    $image->set_data($raw_blob);
+    $image->set_format("Y800");
+
+    print STDERR "SCANNING IMAGE...\n";
+    my $symbol_count = $scanner->scan_image($image);
+
+    print STDERR "SYMBOLD COUNT : $symbol_count\n";
+    print STDERR "GETTING RESULT...\n";
+
+    foreach my $symbol ($image->get_symbols()) {
+
+	print STDERR join(", ", $symbol->get_data())."\n";
+    }
+
+    print STDERR "DONE\n";
+
+#    $scanner->recycle_image($scanner, $image);
+    next();
+
     print STDERR "PROCESSING IMAGE $f\n";
     $step = 1;
     my $output = `zbarimg --raw $f`;
