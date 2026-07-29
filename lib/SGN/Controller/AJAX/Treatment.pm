738f7ed236fe2f177340bb8f5df66ba260c786b9
ryan-preble <rsp98@cornell.edu>
2026-07-29 09:30:16 -0400

    Allow unicode characters in trait names and trait compositions

--- lib/SGN/Controller/AJAX/Treatment.pm
+++ lib/SGN/Controller/AJAX/Treatment.pm
@@ -44,7 +44,6 @@ sub create_treatment :Path('/ajax/treatment/create') {
 
     $name =~ s/^\s+//;
     $name =~ s/\s+$//;
-    $name =~ s/[^[:ascii:]]//g;
     $name =~ s/\|//g;
 
     $definition =~ s/^\s+//;
