918c8d64a98fab2947ed3188e8c7dbd336d6c884
Clay Birkett <clb343@cornell.edu>
2026-09-08 15:28:46 -0400

    fix output to match unit test

--- lib/CXGN/List/Validate/Plugin/Locations.pm
+++ lib/CXGN/List/Validate/Plugin/Locations.pm
@@ -15,72 +15,73 @@ sub validate {
 
     my %seen;
     my @unique = grep { defined && length && !$seen{$_}++ } @$list;
-    return { missing => [], wrong_case => [], multiple_wrong_case => [], codes => [] }
-        unless @unique;
 
-    my $location_code_type_id = SGN::Model::Cvterm
-        ->get_cvterm_row($schema, 'abbreviation', 'geolocation_property')->cvterm_id();
-
-    # 1. exact matches
-    my %is_exact = map { $_ => 1 }
-        $schema->resultset("NaturalDiversity::NdGeolocation")
-               ->search({ description => { in => \@unique } })
-               ->get_column('description')->all();
-
-    my @not_exact = grep { !$is_exact{$_} } @unique;
-
-    my @missing;
+    my %is_exact;
     my @wrong_case;
     my @multiple_wrong_case;
     my @codes;
 
-    if (@not_exact) {
-        # 2. case-insensitive description matches - one query
-        my %ci;   # lc(name) => [ descriptions ]
-        my $ci_rs = $schema->resultset("NaturalDiversity::NdGeolocation")->search(
-            { 'lower(description)' => { in => [ map { lc } @not_exact ] } }
-        );
-        while ( my $row = $ci_rs->next ) {
-            push @{ $ci{ lc $row->description } }, $row->description;
-        }
+    if (@unique) {
+        my $location_code_type_id = SGN::Model::Cvterm
+            ->get_cvterm_row($schema, 'abbreviation', 'geolocation_property')->cvterm_id();
 
-        # 3. location-code matches - one query
-        my %by_code;   # lc(code) => [ descriptions ]
-        my $code_rs = $schema->resultset("NaturalDiversity::NdGeolocationprop")->search(
-            {
-                'me.type_id'      => $location_code_type_id,
-                'lower(me.value)' => { in => [ map { lc } @not_exact ] },
-            },
-            {
-                join      => 'nd_geolocation',
-                '+select' => [ 'me.value', 'nd_geolocation.description' ],
-                '+as'     => [ 'code_value', 'description' ],
-            },
-        );
-        while ( my $row = $code_rs->next ) {
-            push @{ $by_code{ lc $row->get_column('code_value') } },
-                 $row->get_column('description');
-        }
+        # 1. exact matches
+        %is_exact = map { $_ => 1 }
+            $schema->resultset("NaturalDiversity::NdGeolocation")
+                   ->search({ description => { in => \@unique } })
+                   ->get_column('description')->all();
 
-        foreach my $item (@not_exact) {
-            my $ci_hits   = $ci{ lc $item }      || [];
-            my $code_hits = $by_code{ lc $item } || [];
+        my @not_exact = grep { !$is_exact{$_} } @unique;
 
-            if ( @$ci_hits == 1 && $ci_hits->[0] ne $item ) {
-                push @wrong_case, [ $item, $ci_hits->[0] ];
-            }
-            elsif ( @$ci_hits > 1 ) {
-                push @multiple_wrong_case, [ $item, $_ ] for @$ci_hits;
+        if (@not_exact) {
+            # 2. case-insensitive description matches - one query
+            my %ci;   # lc(name) => [ descriptions ]
+            my $ci_rs = $schema->resultset("NaturalDiversity::NdGeolocation")->search(
+                { 'lower(description)' => { in => [ map { lc } @not_exact ] } }
+            );
+            while ( my $row = $ci_rs->next ) {
+                push @{ $ci{ lc $row->description } }, $row->description;
             }
 
-            # only substitute an unambiguous code match
-            if ( @$code_hits == 1 ) {
-                push @codes, [ $item, $code_hits->[0] ];
+            # 3. location-code matches - one query
+            my %by_code;   # lc(code) => [ descriptions ]
+            my $code_rs = $schema->resultset("NaturalDiversity::NdGeolocationprop")->search(
+                {
+                    'me.type_id'      => $location_code_type_id,
+                    'lower(me.value)' => { in => [ map { lc } @not_exact ] },
+                },
+                {
+                    join      => 'nd_geolocation',
+                    '+select' => [ 'me.value', 'nd_geolocation.description' ],
+                    '+as'     => [ 'code_value', 'description' ],
+                },
+            );
+            while ( my $row = $code_rs->next ) {
+                push @{ $by_code{ lc $row->get_column('code_value') } },
+                     $row->get_column('description');
             }
 
-            push @missing, $item;
+            foreach my $item (@not_exact) {
+                my $ci_hits   = $ci{ lc $item }      || [];
+                my $code_hits = $by_code{ lc $item } || [];
+
+                if ( @$ci_hits == 1 && $ci_hits->[0] ne $item ) {
+                    push @wrong_case, [ $item, $ci_hits->[0] ];
+                }
+                elsif ( @$ci_hits > 1 ) {
+                    push @multiple_wrong_case, [ $item, $_ ] for @$ci_hits;
+                }
+
+                # only substitute an unambiguous code match
+                if ( @$code_hits == 1 ) {
+                    push @codes, [ $item, $code_hits->[0] ];
+                }
+            }
         }
     }
+
+    # Report against the original list so blank/undef/duplicate entries are still surfaced
+    my @missing = grep { !defined($_) || $_ eq '' || !$is_exact{$_} } @$list;
 
     return {
         missing             => \@missing,
--- lib/CXGN/List/Validate/Plugin/Trials.pm
+++ lib/CXGN/List/Validate/Plugin/Trials.pm
@@ -11,45 +11,49 @@ sub name {
 sub validate {
     my $self   = shift;
     my $schema = shift;
-    my $list   = shift;
+    my $list   = shift || [];
 
     my %seen;
     my @unique = grep { defined && length && !$seen{$_}++ } @$list;
-    return { missing => [] } unless @unique;
 
-    # projectprop / stock_type cvterms that mean "this project is NOT a trial"
-    my @not_trial_type_ids = map {
-        SGN::Model::Cvterm->get_cvterm_row($schema, $_->[0], $_->[1])->cvterm_id()
-    } (
-        [ 'breeding_program',       'project_property' ],
-        [ 'trial_folder',           'project_property' ],
-        [ 'cross',                  'stock_type'       ],  # crossing experiment
-        [ 'analysis_metadata_json', 'project_property' ],
-    );
+    my %found;
+    if (@unique) {
+        # projectprop / stock_type cvterms that mean "this project is NOT a trial"
+        my @not_trial_type_ids = map {
+            SGN::Model::Cvterm->get_cvterm_row($schema, $_->[0], $_->[1])->cvterm_id()
+        } (
+            [ 'breeding_program',       'project_property' ],
+            [ 'trial_folder',           'project_property' ],
+            [ 'cross',                  'stock_type'       ],  # crossing experiment
+            [ 'analysis_metadata_json', 'project_property' ],
+        );
 
-    # candidate projects carrying one of the requested names
-    my $project_rs = $schema->resultset("Project::Project")->search(
-        { name => { -in => \@unique } },
-        { columns => [qw/ project_id name /] },
-    );
-    my %name_by_id;
-    while ( my $p = $project_rs->next ) {
-        $name_by_id{ $p->project_id } = $p->name;
-    }
-
-    # drop the ones that are breeding programs / folders / crossing experiments / analyses
-    if (%name_by_id) {
-        my $dq_rs = $schema->resultset("Project::Projectprop")->search({
-            project_id => { -in => [ keys %name_by_id ] },
-            type_id    => { -in => \@not_trial_type_ids },
-        });
-        while ( my $r = $dq_rs->next ) {
-            delete $name_by_id{ $r->project_id };
+        # candidate projects carrying one of the requested names
+        my $project_rs = $schema->resultset("Project::Project")->search(
+            { name => { -in => \@unique } },
+            { columns => [qw/ project_id name /] },
+        );
+        my %name_by_id;
+        while ( my $p = $project_rs->next ) {
+            $name_by_id{ $p->project_id } = $p->name;
         }
+
+        # drop the ones that are breeding programs / folders / crossing experiments / analyses
+        if (%name_by_id) {
+            my $dq_rs = $schema->resultset("Project::Projectprop")->search({
+                project_id => { -in => [ keys %name_by_id ] },
+                type_id    => { -in => \@not_trial_type_ids },
+            });
+            while ( my $r = $dq_rs->next ) {
+                delete $name_by_id{ $r->project_id };
+            }
+        }
+
+        %found = map { $_ => 1 } values %name_by_id;
     }
 
-    my %found = map { $_ => 1 } values %name_by_id;
-    my @missing = grep { !$found{$_} } @unique;
+    # Report against the original list so blank/undef/duplicate entries are still surfaced
+    my @missing = grep { !defined($_) || $_ eq '' || !$found{$_} } @$list;
 
     return { missing => \@missing };
 }
