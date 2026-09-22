c720edc9ff9f13ddca351d9fed108c6054791db2
titima15 <tt15@cornell.edu>
2026-09-22 15:03:09 -0400

    fix a bug in stock validation

--- lib/CXGN/List/Validate/Plugin/AccessionsOrSynonymsOrCrosses.pm
+++ lib/CXGN/List/Validate/Plugin/AccessionsOrSynonymsOrCrosses.pm
@@ -19,7 +19,7 @@ sub validate {
     my $accession_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'accession', 'stock_type')->cvterm_id();
     my $cross_type_id = SGN::Model::Cvterm->get_cvterm_row($schema, 'cross', 'stock_type')->cvterm_id();
 
-    my $q = "SELECT stock.uniquename, stockprop.value, stockprop.type_id FROM stock LEFT JOIN stockprop USING(stock_id) WHERE (stock.type_id=? OR stock.type_id=? OR stock.type_id=?) AND stock.is_obsolete = 'F';";
+    my $q = "SELECT stock.uniquename, stockprop.value, stockprop.type_id FROM stock LEFT JOIN stockprop USING(stock_id) WHERE (stock.type_id=? OR stock.type_id=?) AND stock.is_obsolete = 'F';";
     my $h = $schema->storage->dbh()->prepare($q);
     $h->execute($accession_type_id, $cross_type_id);
     my %result;
